39502a89fa49ffd3a60fd550e9d2cbd2134f813e
jh <jh@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-07-09 18:12:40 +0000

    First version of setup.py
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ setup.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+"""
+Package installer for Twisted
+
+Copyright (c) 2001 by Twisted Matrix Laboratories
+All rights reserved, see LICENSE for details.
+
+$Id: setup.py,v 1.1 2001/07/09 18:12:40 jh Exp $
+"""
+
+import distutils
+from distutils.core import setup
+from twisted import copyright
+
+#############################################################################
+### Call setup()
+#############################################################################
+
+setup_args = {
+    'name': "Twisted",
+    'version': copyright.version,
+    'description': "Twisted %s is a framework to build frameworks" % (copyright.version,),
+    'author': "Twisted Matrix Laboratories",
+    'author_email': "twisted-python@twistedmatrix.com",
+    'maintainer': "Glyph Lefkowitz",
+    'maintainer_email': "glyph@twistedmatrix.com",
+    'url': "http://twistedmatrix.com/",
+    'licence': "GNU LGPL",
+    'long_description': """
+Twisted is a framework to build frameworks. It is expected that one day
+the project has expanded to the point that the Twisted Reality framework
+(a very small part of the codebase, even now) can seamlessly integrate
+with mail, web, DNS, netnews, IRC, RDBMSs, desktop environments, and
+your toaster. 
+""",
+    'packages': [
+        'twisted',
+        'twisted.internet',
+        'twisted.persisted',
+        'twisted.pim',
+        'twisted.protocols',
+        'twisted.python',
+        'twisted.reality',
+        'twisted.spread',
+        'twisted.test',
+        'twisted.web',
+    ],
+}
+
+if hasattr(distutils.dist.DistributionMetadata, 'get_keywords'):
+    setup_args['keywords'] = "internet www tcp framework games"
+
+if hasattr(distutils.dist.DistributionMetadata, 'get_platforms'):
+    setup_args['platforms'] = "win32 posix"
+
+apply(setup, (), setup_args)
+
