d8b14bc70bc60b62a3d6300540f3a4c0db1c488d
carmstro <carmstro@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-08-06 14:36:56 +0000

    initial manhole checikin -- not functional yet.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@107 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/manhole/service.py
@@ -0,0 +1,40 @@
+from twisted.spread import pb
+
+import cStringIO
+StringIO = cStringIO
+del cStringIO
+
+class Perspective(pb.Perspective):
+    def perspective_do(self, mesg):
+        fn = "$manhole"
+        try:
+            code = compile(mesg, fn, 'eval')
+        except:
+            try:
+                code = compile(mesg, fn, 'exec')
+            except:
+                io = StringIO.StringIO()
+                traceback.print_exc(file=io)
+                return io.getvalue()
+        try:
+            val = eval(code, self.factory.namespace)
+        except:
+            io = StringIO.StringIO()
+            traceback.print_exc(file=io)
+            return io.getvalue()
+        return val
+
+class Service(pb.Service):
+    # By default, "guest"/"guest" will work as login and password, though you
+    # must implement something to retrieve a perspective.
+    def getPerspectiveNamed(self, name):
+        return Perspective()
+
+if __name__ == '__main__':
+    import service
+    from twisted.internet.main import Application
+    bf = pb.BrokerFactory()
+    bf.addService("manhole", service.Service())
+    app = Application("manhole")
+    app.listenOn(pb.portno, bf)
+    app.save()
---
+++ twisted/manhole/ui/gtkmanhole.py
@@ -0,0 +1,7 @@
+import gtk
+
+from twisted.words.ui import gtkim
+from twisted.spread.ui import gtkutil
+
+class Interaction(gtk.GtkWindow):
+    
