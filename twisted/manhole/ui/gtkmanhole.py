bf1c4ae5a460262dab7dbbdb24cc115a4a8e8dfb
carmstro <carmstro@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-08-08 02:30:19 +0000

    ....?
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@129 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/manhole/ui/gtkmanhole.py
+++ twisted/manhole/ui/gtkmanhole.py
@@ -6,6 +6,11 @@ from twisted.internet import ingtkernet
 from twisted.spread import pb
 ingtkernet.install()
 
+normalFont = gtk.load_font("-adobe-courier-medium-r-normal-*-*-120-*-*-m-*-iso8859-1")
+font = normalFont
+boldFont = gtk.load_font("-adobe-courier-bold-r-normal-*-*-120-*-*-m-*-iso8859-1")
+errorFont = gtk.load_font("-adobe-courier-medium-o-normal-*-*-120-*-*-m-*-iso8859-1")
+
 class Interaction(gtk.GtkWindow):
     def __init__(self):
         gtk.GtkWindow.__init__(self, gtk.WINDOW_TOPLEVEL)
@@ -20,20 +25,36 @@ class Interaction(gtk.GtkWindow):
 
         self.input = gtk.GtkText()
         self.input.set_editable(gtk.TRUE)
+        self.input.connect("key_press_event", self.processKey)
         vp.pack2(gtkim.scrolltxt(self.input), gtk.FALSE, gtk.TRUE)
-
-        self.send = gtkim.cbutton("Send", self.sendMessage)
         vb.pack_start(vp, 1,1,0)
-        vb.pack_start(self.send, 0,0,0)
 
         self.add(vb)
         self.signal_connect('destroy', sys.exit, None)
 
+    def processKey(self, entry, event):
+        if event.keyval == gtk.GDK.Return:
+#            if self.input.get_chars(self.input.get_length()-1,-1) == ":":
+#                print "woo!"
+#            else:
+                self.sendMessage(entry)
+                self.input.delete_text(0,-1)
+                self.input.emit_stop_by_name("key_press_event")
+
     def messageReceived(self, message):
+        print "received: ", message
+        win = self.get_window()
+        blue = win.colormap.alloc(0x0000, 0x0000, 0xffff)
+        red = win.colormap.alloc(0xffff, 0x0000, 0x0000)
+        green = win.colormap.alloc(0x0000, 0xffff, 0x0000)
+        black = win.colormap.alloc(0x0000, 0x0000, 0x0000)
+
         t = self.output
         t.set_point(t.get_length())
         t.freeze()
-        t.insert_defaults('\n'+message)
+        styles = {"out": black, "err": green, "result": blue, "error": red}
+        for element in message:
+            t.insert(font, styles[element[0]], None, element[1])
         a = t.get_vadjustment()
         t.thaw()
         a.set_value(a.upper - a.page_size)
