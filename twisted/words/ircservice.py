8c2297e016ca8eb7c0cd290514bb09820299e020
acapnotic <acapnotic@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-08-11 08:23:14 +0000

    RoboHak spots the problem: an extra colon in MYINFO
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@207 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/ircservice.py
+++ twisted/words/ircservice.py
@@ -130,9 +130,8 @@ class IRCChatter(irc.IRC):
                 #  I'll have to guess at all the values"
                 #    -- epic
                 self.sendMessage(irc.RPL_MYINFO,
-                                 ":%s %s %s %s" %
-                                 (self.servicename, copyright.version,
-                                  'w', 'n')) # user and channel modes
+                                 self.servicename, copyright.version,
+                                  'w', 'n') # user and channel modes
                 if self.passwd is None:
                     self.receiveDirectMessage("*login*", "Password?")
 ##                self.sendLine(":*login*!*login*@%s NOTICE %s :You 'must /msg *login* <your password>' to use this server" %
