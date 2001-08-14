cbe3cfd7cca917d9ffa35f74ef7a1a4201a47a8b
phed <phed@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-08-14 03:39:46 +0000

    Based on tap.telnet.py. Note that the server is still not operational.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@249 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/tap/ftp.py
@@ -0,0 +1,32 @@
+"""
+I am the support module for making a ftp server with mktap.
+"""
+
+from twisted.protocols import ftp2
+from twisted.internet import tcp
+from twisted.python import usage
+import sys
+
+
+usage_message = """
+usage: mktap ftp [OPTIONS]
+
+Options are as follows:
+        --port <#>, -p:         set the port number to <#>.
+"""
+
+class Options(usage.Options):
+    def opt_port(self, opt):
+        try:
+	    self.portno = int(opt)
+	except ValueError:
+	    raise usage.error("Invalid argument to 'port'!")
+    opt_p = opt_port
+
+def getPorts(app, config):
+    t = ftp2.ShellFactory()
+    try:
+        portno = config.portno
+    except AttributeError:
+        portno = 2121
+    return [(portno, t)]
