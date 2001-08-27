f278b92e4e32a7782b270ad2d24b26e9b4caa884
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-08-27 17:30:30 +0000

    A demo file to twistd -y if you want a simple reverse proxy
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@327 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/examples/proxy.py
@@ -0,0 +1,22 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from twisted.internet import main
+from twisted.web import proxy, server
+site = server.Site(proxy.ReverseProxyResource('www.yahoo.com', 80, '/'))
+application = main.Application('web-proxy')
+application.listenOn(8080, site)
