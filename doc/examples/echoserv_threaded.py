ac971d229afa1f3e4e8751299778f40ceec52f98
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-08-29 23:53:43 +0000

    added some explanatory docs
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@341 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/echoserv_threaded.py
+++ doc/examples/echoserv_threaded.py
@@ -15,6 +15,28 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+#  Threading in twisted
+# ======================
+#
+# Many operations in twisted are not thread safe. Therefore, when using threads
+# you need to do a number of things.
+#
+# When you want to call a non thread-safe operation, you don't call it, but
+# instead you add the operation to the threadtask scheduler. The main thread
+# running the event loop will then read these tasks from the scheduler and
+# execute them.
+#
+# Additionally, you must make sure to run twisd in threaded mode (by using the
+# '-t' option.) If you are not using twistd to run your app but are running it
+# directly, make sure to do:
+#
+#    from twisted.python import threadable
+#    threadable.init(1)
+#
+# The following example server has a thread for each connections that does the
+# actual processing of the protocol, in this case echoing back all received
+# data.
+
 import threading, Queue
 
 from twisted.protocols.protocol import Protocol, Factory
@@ -37,6 +59,8 @@ class Echo(Protocol):
 
     def send(self, data):
         """Schedule data to be written in a thread-safe manner"""
+        # instead of doing self.transport.write(data), which is not
+        # thread safe, we do:
         threadtask.schedule(self.transport.write, (data,))
 
     def connectionLost(self):
