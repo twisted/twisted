cb1823c3815b40f10dd15c3cfd1a9653f495fc5e
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-09-20 13:32:47 +0000

    added initial implementation of deferred results
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@427 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/threadtask.py
+++ twisted/internet/threadtask.py
@@ -22,6 +22,19 @@ scheduler. This module should not be used by non-threaded modules,
 instead they should use twisted.internet.task.
 
 Tasks added to this scheduler will *not* be stored persistently.
+
+This is an implementation of the Active Object pattern, and can be used
+as part of the queueing layer for the Async/Half-Async pattern. For more
+details:
+
+  1) POSA2 book - http://www.cs.wustl.edu/~schmidt/POSA/    
+  
+  2) Active Object - http://www.cs.wustl.edu/~schmidt/PDF/Act-Obj.pdf
+  
+  3) Async/Half-Async - http://www.cs.wustl.edu/~schmidt/PDF/PLoP-95.pdf
+
+Note: the API for DeferredResults is not final - I'm open to suggestions
+on how to improve it.  --itamar
 """
 
 # twisted import
@@ -42,6 +55,10 @@ class Scheduler:
         return dict
 
     def addTask(self, function, args=[], kwargs={}):
+        """Schedule a function to be called by the main event-loop thread.
+        
+        The result of the function will not be returned.
+        """
         threadTasks = self.threadTasks
         hadNoTasks = (threadTasks == {})
         thread = reflect.currentThread()
@@ -52,6 +69,17 @@ class Scheduler:
         if hadNoTasks:
             main.wakeUp()
 
+    def addTaskWithResult(self, function, args=[], kwargs={}):
+        """Schedule a function, returning a DeferredResult object.
+        
+        The result of the function can be gotten by calling the get()
+        method of this DeferredResult object.
+        """
+        args = [function,] + list(args)
+        dresult = DeferredResult()
+        self.addTask(dresult._doAction, args, kwargs)
+        return dresult
+    
     def timeout(self):
         """Either I have work to do immediately, or no work to do at all.
         """
@@ -70,11 +98,72 @@ class Scheduler:
     synchronized = ["addTask", "runUntilCurrent"]
 
 threadable.synchronize(Scheduler)
+
+
+class NotReady(RuntimeError):
+    """The DeferredResult does not yet have a result."""
+
+
+class DeferredResult:
+    """Result of a scheduled operation that is deferred.
+    
+    We can get the result of the operation by calling the get() method, which
+    will raise a NotReady exception if the result is not available. Otherwise it
+    returns the result of the operation, or if it raised an exception, reraise
+    that exception.
+    
+    The exception catching will only work for class-based exceptions, but
+    hopefully by now string exceptions are rare.
+    """
+    
+    def __init__(self):
+        self._finished = 0
+    
+    def haveResult(self):
+        """Did we get the result yet?"""
+        return self._finished
+    
+    def get(self):
+        """Return the result or if it's not available raise NotReady"""
+        if not self._finished:
+            raise NotReady, "we don't have the result yet."
+        
+        if hasattr(self, '_exception'):
+            raise self._exception
+        else:
+            return self._result
+    
+    def _doAction(self, function, *args, **kwargs):
+        """Run the action whose result we are. 
+        
+        Should only be called by the scheduler."""
+        try:
+            result = apply(function, args, kwargs)
+        except Exception, e:
+            self._exception = exception
+        else:
+            self._result = result
+        self._finished = 1
+
+
 theScheduler = Scheduler()
 
 def schedule(function, args=[], kwargs={}):
+    """Schedule a function to be called by the main event loop thread"""
     global theScheduler
     theScheduler.addTask(function, args, kwargs)
+
+def scheduleWithResult(function, args=[], kwargs={}):
+    """Schedule a function to be called by the main event loop thread.
+    
+    Returns a DeferredResult object, allowing us to get the result of the
+    function.
+    
+    XXX this name sucks. Someone think of a new one please.
+    """
+    global theScheduler
+    return theScheduler.addTaskWithResult(function, args, kwargs)
+
 
 # sibling import - done here to prevent circular import
 import main
