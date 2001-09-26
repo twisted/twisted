73c8cee005bb006d272c3b4393d5c4380689a27d
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-09-26 09:00:25 +0000

    Another version of the prototype -- a bit more fleshed out
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@474 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/mail/pb.py
+++ twisted/mail/pb.py
@@ -1,3 +1,21 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+from twisted.spread import pb, banana
+import types
+
 class Maildir(pb.Referencable):
 
     def __init__(self, directory, rootDirectory):
@@ -61,3 +79,45 @@ class Maildir(pb.Referencable):
         return (not os.path.isdir(os.path.join(self.rootDirectory, name)) or
                 not os.path.isfile(os.path.join(self.rootDirectory, name, 
                                                 'maildirfolder'))
+
+
+class MaildirCollection(pb.Referencable):
+
+    def __init__(self, root):
+        self.root = root
+
+    def getSubFolders(self):
+        return os.listdir(self.getRoot())
+    remote_getSubFolders = getSubFolders
+
+    def getSubFolder(self, name):
+        if '/' in name or name[0] == '.':
+            raise IOError("invalid name")
+        return Maildir('.', os.path.join(self.getRoot(), name))
+    remote_getSubFolder = getSubFolder
+
+
+class MaildirBroker(pb.Broker):
+
+    def proto_getCollection(self, requestID, name, domain, password):
+        collection = self._getCollection()
+        if collection is None:
+            self.sendError(requestID, "permission denied")
+        else:
+            self.sendAnswer(requestID, collection)
+
+    def getCollection(self, name, domain, password):
+        if not self.domains.has_key(domain):
+            return
+        domain = self.domains[domain]
+        if (domain.dbm.has_key(name) and 
+            domain.dbm[name] == password):
+            return MaildirCollection(domain.userDirectory(name))
+
+
+class MaildirClient(pb.Broker):
+
+    def getCollection(self, name, domain, password, callback, errback):
+        requestID = self.newRequestID()
+        self.waitingForAnswers[requestID] = callback, errback
+        self.sendCall("getCollection", requestID, name, domain, password)
