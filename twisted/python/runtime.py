232d918ecebd0d0dcfda5cbfce310cefb868180e
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-10-04 10:57:51 +0000

    information that is only available at runtime, e.g. what OS we are running
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@495 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/python/runtime.py
@@ -0,0 +1,52 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# System imports
+import os
+
+
+class Platform:
+    """Gives us information about the platform we're running on"""
+    
+    def __init__(self):
+        if os.name is 'nt':
+            self.type = 'win32'
+        elif os.name is 'posix':
+            self.type = 'posix'
+        elif os.name in ('java', 'org.python.modules.os'):
+            self.type = 'java'
+        else:
+            self.type = None
+    
+    def isKnown(self):
+        """Do we know about this platform?"""
+        return self.type != None
+    
+    def getType(self):
+        """Return 'posix', 'win32' or 'java'"""
+        return self.type
+    
+    def isWinNT(self):
+        """Are we running in Windows NT?"""
+        if self.getType() == 'win32':
+            import win32api, win32con
+            return win32api.GetVersionEx()[3] == win32con.VER_PLATFORM_WIN32_NT
+        else:
+            raise ValueError, "this is not a Windows platform"
+
+
+platform = Platform()
