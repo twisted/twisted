43b501987de184bce28cd9d98e5a6618b5ce808b
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-10-17 17:23:58 +0000

    Added xml-rpc support and an example
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@554 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/examples/xmlrpc.py
@@ -0,0 +1,37 @@
+"""Hook up an object to XML-RPC. An example session:
+    
+    >>> s = xmlrpclib.Server('http://localhost:7080/')
+    >>> s.echo("lala")
+    ['lala']
+    >>> s.echo("lala", 1)
+    ['lala', 1]
+    >>> s.echo("lala", 4)
+    ['lala', 4]
+    >>> s.echo("lala", 4, 3.4)
+    ['lala', 4, 3.3999999999999999]
+    >>> s.echo("lala", 4, [1, 2])
+    ['lala', 4, [1, 2]]
+"""
+
+from twisted.web import xmlrpc
+
+
+class Echoer:
+    """An example object to be published."""
+    
+    def perspective_echo(self, *args):
+        """Return all passed args."""
+        return args
+
+
+def main():
+    from twisted.internet.main import Application
+    from twisted.web import server
+    app = Application("xmlrpc")
+    r = xmlrpc.PB(Echoer())
+    app.listenOn(7080, server.Site(r))
+    app.run(0)
+
+
+if __name__ == '__main__':
+    main()
---
+++ twisted/web/xmlrpc.py
@@ -0,0 +1,83 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""A generic resource for publishing objects via XML-RPC.
+
+Requires xmlrpclib (comes standard with Python 2.2 and later, otherwise can be
+downloaded from http://www.pythonware.com/products/xmlrpc/).
+"""
+
+# System Imports
+import xmlrpclib, sys, traceback
+
+# Sibling Imports
+from twisted.web import resource
+
+
+class NoSuchFunction(Exception):
+    """There is no function by the given name."""
+    pass
+
+
+class XMLRPC(resource.Resource):
+    """A resource that implements XML-RPC.
+    
+    You probably want to connect this to '/RPC2'.
+    """
+    
+    isLeaf = 1
+    
+    def render(self, request):
+        args, functionPath = xmlrpclib.loads(request.content)
+        try:
+            function = self._getFunction(functionPath)
+        except NoSuchFunction:
+            result = xmlrpclib.Fault(1, "no such function")
+        else:
+            try:
+                result = (apply(function, args), )
+            except:
+                traceback.print_exc(file=sys.stdout)
+                result = xmlrpclib.Fault(2, "error")
+        return xmlrpclib.dumps(result, methodresponse=1)
+    
+    def _getFunction(self, functionPath):
+        """Given a string, return a function, or raise NoSuchFunction.
+        
+        Override in subclasses.
+        """
+        raise NotImplementedError, "implement in subclass"
+
+
+class PB(XMLRPC):
+    """Publish a pb.Perspective instance using XML-RPC."""
+    
+    def __init__(self, perspective):
+        XMLRPC.__init__(self)
+        self.perspective = perspective
+    
+    def _getFunction(self, functionPath):
+        """Convert the functionPath to a method beginning with 'perspective_'.
+        
+        For example, 'echo' returns the method 'perspective_echo'
+        of self.perspective.
+        """
+        f = getattr(self.perspective, "perspective_%s" % functionPath, None)
+        if f:
+            return f
+        else:
+            raise NoSuchFunction
