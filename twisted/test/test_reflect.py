a57f52c9ebce3c38d56307950e538fd29530fb19
acapnotic <acapnotic@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-10-18 22:25:05 +0000

    sorry dash, but the fact that append only takes one argument is /not/ a Python 2.x bug.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@560 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_reflect.py
+++ twisted/test/test_reflect.py
@@ -1,16 +1,16 @@
 
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001 Matthew W. Lefkowitz
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of version 2.1 of the GNU Lesser General Public
 # License as published by the Free Software Foundation.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -42,13 +42,13 @@ class AccessorTest(unittest.TestCase):
             def set_x(self, x):
                 self.y = x
                 self.reallySet('x',x)
-                
+
             def get_z(self):
                 self.q = 1
                 return 1
 
         self.tester = Tester()
-        
+
     def testSet(self):
         self.tester.x = 1
         assert self.tester.x == 1
@@ -64,12 +64,12 @@ class PromiseTest(unittest.TestCase):
             def __init__(self):
                 self.results = []
             def a(self, arg):
-                self.results.append("a", arg)
+                self.results.append(("a", arg))
             def b(self, arg):
-                self.results.append("b", arg)
+                self.results.append(("b", arg))
         self.slowObj = SlowObject()
         self.obj = reflect.Promise()
-        
+
     def testSingle(self):
         print 'Promise: single'
         self.obj.a(0)
