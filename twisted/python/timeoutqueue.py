223d6ae01eaa870b95c3e9164cfeaaa8523b8301
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-10-21 14:46:13 +0000

    a queue that supports timeouts
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@569 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/python/timeoutqueue.py
@@ -0,0 +1,74 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""A Queue subclass that supports timeouts."""
+
+# System Imports
+import Queue, time
+
+
+_time = time.time
+_sleep = time.sleep
+
+
+class TimedOut(Exception):
+    pass
+
+
+class TimeoutQueue(Queue.Queue):
+    """A thread-safe queue that supports timeouts"""
+    
+    def wait(self, timeout):
+        """Wait until the queue isn't empty. Raises TimedOut if still empty."""
+        endtime = _time() + timeout
+        delay = 0.0005 # 500 us -> initial delay of 1 ms
+        while 1:
+            gotit = not self.empty()
+            if gotit:
+                break
+            remaining = endtime - _time()
+            if remaining <= 0:
+                raise TimedOut, "timed out."
+            delay = min(delay * 2, remaining, .05)
+            _sleep(delay)
+
+
+__all__ = ["TimeoutQueue", "TimedOut"]
+
+
+if __name__ == '__main__':
+    import threading
+    
+    q = TimeoutQueue()
+    
+    try:
+        q.wait(5)
+    except TimedOut:
+        print "OK, timed out correctly."
+    
+    def put():
+        time.sleep(3)
+        q.put(1)
+    
+    start = time.time()
+    threading.Thread(target=put).start()
+    q.wait(3.5)
+    assert time.time() - start < 4
+    
+    result = q.get()
+    if result == 1:
+        print "OK, got data correctly."
