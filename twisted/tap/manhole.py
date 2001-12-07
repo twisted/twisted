a1faea71d874f5e114c45722c01460b028cf0f46
carmstro <carmstro@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-12-07 15:24:15 +0000

    Hmph. Manhole, not telnet.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@777 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/tap/manhole.py
+++ twisted/tap/manhole.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """
-I am the support module for making a telnet server with mktap.
+I am the support module for making a manhole server with mktap.
 """
 
 from twisted.manhole import service
