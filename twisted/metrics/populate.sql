93a6827d6d27bcad26cae317d63457cdfbc3637a
sean <sean@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2001-12-26 20:57:37 +0000

    added metrics populate.sql file.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@808 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/metrics/populate.sql
@@ -0,0 +1,14 @@
+INSERT INTO metrics_variables VALUES ('cpu', 80);
+INSERT INTO metrics_variables VALUES ('memory', 80);
+INSERT INTO metrics_variables VALUES ('connections', 80);
+INSERT INTO metrics_variables VALUES ('syscalls', 80);
+INSERT INTO metrics_variables VALUES ('processes', 80);
+
+
+INSERT INTO metrics_variables VALUES ('counter1', 2);
+INSERT INTO metrics_variables VALUES ('counter2', 5);
+INSERT INTO metrics_variables VALUES ('counter3', 8);
+
+
+INSERT INTO metrics_perspectives VALUES ('localMachine', 'localhost', 'thisgroup', 1);
+INSERT INTO metrics_perspectives VALUES ('remoteMachine', 'somehost', 'thisgroup', 1);
