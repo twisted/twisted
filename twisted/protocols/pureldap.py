2f4c08de8e2f97ee98260abedc69c51e9a9af89e
tv <tv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-01-03 22:39:24 +0000

    Esthetic cleanup.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@834 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/pureldap.py
+++ twisted/protocols/pureldap.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 """LDAP protocol handling"""
 
 import sys
