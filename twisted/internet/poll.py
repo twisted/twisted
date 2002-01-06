8d79c3e2be6b999a43280789c4cbde8b82419b58
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-01-06 11:55:08 +0000

    poll event loop now registers threaded waker
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@836 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/poll.py
+++ twisted/internet/poll.py
@@ -1,10 +1,34 @@
-"""A poll() based implementation of the twisted main loop."""
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""A poll() based implementation of the twisted main loop.
+
+To install the event loop (and you should do this before any connections,
+listeners or connectors are added):
+
+    from twisted.internet import poll
+    poll.install()
+
+"""
 
 # System imports
 import select, traceback
 
 # Twisted imports
-from twisted.python import log
+from twisted.python import log, threadable
 from twisted.internet import main
 
 # globals
@@ -106,3 +130,15 @@ def install():
     main.removeReader = removeReader
     main.removeWriter = removeWriter
     main.doSelect = doPoll
+
+
+def initThreads():
+    """Do initialization for threads."""
+    if main.wakerInstalled:
+        # make sure waker is registered with us
+        removeReader(main.waker)
+        addReader(main.waker)
+
+threadable.whenThreaded(initThreads)
+
+__all__ = ["install"]
