42ba70a6aab45b88e3ccb2a976ba53f103c76b96
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-01-30 13:40:30 +0000

    SOAP is lower level than PB
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@943 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/spread/flavors.py
+++ twisted/spread/flavors.py
@@ -1,10 +1,26 @@
 
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 """
 This module represents flavors of remotely acessible objects.
 
 Currently this is only objects accessible through Perspective Broker, but will
 hopefully encompass all forms of remote access which can emulate subsets of PB
-(such as XMLRPC) or those which PB expresses a subset of (SOAP).
+(such as XMLRPC or SOAP).
 
 """
 
