00a3379136e69ae987698536f9e0e9dea008bfb5
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-02-12 21:10:34 +0000

    checking printing tracebacks doesn't raise its own exception
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1016 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_failure.py
+++ twisted/test/test_failure.py
@@ -1,16 +1,55 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+"""
+Test cases for failure module.
+"""
+
 from pyunit import unittest
+import StringIO
 
 from twisted.python import failure
 
-class TestFailure(unittest.TestCase):
+
+class FailureTestCase(unittest.TestCase):
+    
     def testFailAndTrap(self):
         try:
             raise NotImplementedError('test')
         except:
             f = failure.Failure()
         f.trap(SystemExit, RuntimeError)
+        
         try:
             raise ValueError()
         except:
             f = failure.Failure()
         self.assertRaises(failure.Failure,f.trap,OverflowError)
+
+    def testPrinting(self):
+        out = StringIO.StringIO()
+        try:
+            1/0
+        except:
+            f = failure.Failure()
+        f.printDetailedTraceback(out)
+        f.printBriefTraceback(out)
+        f.printTraceback(out)
+    
+    
+testCases = [FailureTestCase]
