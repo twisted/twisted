cb024b72f12ca828b149e6eace61043d14cc1b76
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-02-13 01:18:55 +0000

    test running processes
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1020 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/test/test_process.py
@@ -0,0 +1,49 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Test running processes.
+"""
+
+from pyunit import unittest
+
+import cStringIO, gzip, os
+
+# Twisted Imports
+from twisted.internet import process, main
+
+s = "there's no place like home!\n" * 3
+
+
+class ProcessTestCase(unittest.TestCase):
+    """Test running processes."""
+    
+    def testProcess(self):
+        f = cStringIO.StringIO()
+        p = process.Process("/bin/gzip", ["-"], {}, "/tmp")
+        p.handleChunk = f.write
+        p.write(s)
+        p.closeStdin()
+        while hasattr(p, 'writer'):
+            main.iterate()
+        f.seek(0, 0)
+        gf = gzip.GzipFile(fileobj=f)
+        self.assertEquals(gf.read(), s)
+
+
+if os.name != 'posix':
+    del ProcessTestCase
