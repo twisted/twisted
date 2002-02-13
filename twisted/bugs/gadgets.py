a664561c197bb6b3479731fa81c7ee3aefcfcf60
acapnotic <acapnotic@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-02-13 18:49:31 +0000

    of course, it'd help if I actually reloaded the module after changing the code.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1033 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/bugs/gadgets.py
+++ twisted/bugs/gadgets.py
@@ -1,15 +1,15 @@
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001 Matthew W. Lefkowitz
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of version 2.1 of the GNU Lesser General Public
 # License as published by the Free Software Foundation.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -25,14 +25,14 @@ from twisted.web import widgets
 
 class BugsPage(widgets.WidgetPage):
     """Default look for bug pages."""
-    
+
     stylesheet = '''
     P, BODY, TD, OL, UL, MENU, BLOCKQUOTE, DIV
     {
         font-family: Lucida, Verdana, Helvetica, Arial;
     }
     '''
-    
+
     template = '''
     <html>
     <head>
@@ -54,16 +54,16 @@ class BugsPage(widgets.WidgetPage):
 
 class BugsGadget(widgets.StreamWidget, widgets.Gadget):
     """The main bugs gadget."""
-    
+
     title = "List of Bugs"
     page = BugsPage
-    
+
     def __init__(self, database):
         widgets.Gadget.__init__(self)
         self.database = database
         self.putWidget('create', CreateNewForm(self.database))
         self.putWidget('view_bug', ViewBug(self.database))
-    
+
     def stream(self, write, request):
         """Display the intro list of bugs. This is only called if there is no URI.
         """
@@ -88,9 +88,9 @@ class BugsGadget(widgets.StreamWidget, widgets.Gadget):
 
 class CreateNewForm(widgets.Form):
     """Form for creating new bugs."""
-    
+
     title = "Create a new bug report:"
-    
+
     def __init__(self, database):
         self.database = database
 
@@ -109,9 +109,9 @@ class CreateNewForm(widgets.Form):
             ['string', 'Summary:', 'summary', ''],
             ['text', 'Description:', 'description', ''],
             ]
-        
+
         return widgets.Form.display(self, self.request)
-    
+
     def process(self, write, request, submit, name, email, version, os, security, type, summary, description):
         write(self.database.createBug(name, email, version, os, security, type, summary, description).addCallback(self._cbPosted))
 
@@ -121,10 +121,10 @@ class CreateNewForm(widgets.Form):
 
 class AddCommentsForm(widgets.Form):
     """Form for adding comments to a bug."""
-    
+
     def __init__(self, database):
         self.database = database
-    
+
     def getFormFields(self, request, fieldSet=[]):
         bug_id = int(request.args.get('bug_id',[0])[0])
         formFields = [['string', 'Name: ', 'name', ''],
@@ -133,7 +133,7 @@ class AddCommentsForm(widgets.Form):
                       ['hidden', '', 'bug_id', bug_id],
                      ]
         return widgets.Form.getFormFields(self, request, formFields)
-        
+
     def process(self, write, request, submit, bug_id, name, email, comment):
         write(self.database.createComment(bug_id, name, email, comment).addCallback(self._cbPosted))
 
@@ -143,18 +143,18 @@ class AddCommentsForm(widgets.Form):
 
 class ViewBug(widgets.Widget):
     """Display a bug."""
-    
+
     def __init__(self, database):
         self.database = database
         self.addComments = AddCommentsForm(database)
-    
+
     def display(self, request):
         bug_id = int(request.args.get('bug_id',[0])[0])
         db = self.database
         return [db.getBug(bug_id).addCallback(self._cbBug), "\n<p>&nbsp; </p>\n<h3>Add a comment</h3>\n"] + \
                self.addComments.display(request) + \
                ['<p>&nbsp; </p>\n', db.getBugComments(bug_id).addCallback(self._cbComments)]
-    
+
     def _cbBug(self, result):
         l = []
         desc = result[0][12]
@@ -170,7 +170,7 @@ class ViewBug(widgets.Widget):
         l.append('<b>Assigned to:</b> %s<br>\n</p>\n' % result[3])
         l.append('<pre>\n%s\n</pre>' % desc)
         return l
-    
+
     def _cbComments(self, result):
         l  = ["<h3>Comments</h3>\n"]
         for id, name, email, date, comment in result:
