ff1b6b05f9ba6230f045b1857ecd22a2a3feb98e
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-02-22 04:24:04 +0000

    more tests for coil
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1085 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_coil.py
+++ twisted/test/test_coil.py
@@ -13,20 +13,43 @@ class E(C, D): pass
 class F(A): pass
 
 class MyConfig(coil.Configurable):
+    
     configTypes = {
         "foo": types.IntType,
         "bar": types.StringType
         }
 
+class CustomConfig(coil.Configurable):
+
+    configTypes = {
+        "foo": types.StringType,
+    }
+    
+    def configInit(self, container, name):
+        self.name = name
+    
+    def config_foo(self, foo):
+        self.foo = foo
+
+
 class CoilTest(unittest.TestCase):
+    
     def testConfigurable(self):
         c = MyConfig()
         c.configure({'foo':1, 'bar':'hi'})
+        self.assertEquals(c.configuration['foo'], 1)
+        self.assertEquals(c.configuration['bar'], 'hi')
         self.failUnlessRaises(coil.InvalidConfiguration,
                               c.configure, {'foo':'hi'})
         self.failUnlessRaises(coil.InvalidConfiguration,
                               c.configure, {'bar':1})
 
+    def testCustomConfigurable(self):
+        c = coil.createConfigurable(CustomConfig, None, "testName")
+        self.assertEquals(c.name, "testName")
+        c.configure({'foo' : 'test'})
+        self.assertEquals(c.foo, 'test')
+    
     def testClassHierarchy(self):
         r = coil.ClassHierarchy()
         map(r.registerClass, (A, B, C, D, E, F))
