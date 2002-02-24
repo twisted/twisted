5471f1ce51ed73d7286d4cbbd07040389a4355b4
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-02-24 16:24:18 +0000

    small cleanups
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1130 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/socks.py
+++ twisted/protocols/socks.py
@@ -1,44 +1,82 @@
-#sibling imports
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Implementation of the SOCKSv4 protocol.
+"""
+
+# sibling imports
 import protocol
+
+# twisted imports
 from twisted.internet import tcp
 
-#python imports
+# python imports
 import struct
 import string
 import socket
 import time
 
+
 class SOCKSv4Outgoing(protocol.Protocol):
+    
     def __init__(self,socks):
         self.socks=socks
+    
     def connectionMade(self):
         self.socks.makeReply(90)
         self.socks.otherConn=self
+    
     def connectionFailed(self):
         self.socks.makeReply(91)
+    
     def connectionLost(self):
         self.socks.transport.loseConnection()
+    
     def dataReceived(self,data):
         self.socks.write(data)
+    
     def write(self,data):
         self.socks.log(self,data)
         self.transport.write(data)
 
+
 class SOCKSv4Incoming(protocol.Protocol):
+    
     def __init__(self,socks):
         self.socks=socks
         self.socks.otherConn=self
+    
     def connectionLost(self):
         self.socks.transport.loseConnection()
+    
     def dataReceived(self,data):
         self.socks.write(data)
+    
     def write(self,data):
         self.socks.log(self,data)
         self.transport.write(data)
 
+
 class SOCKSv4(protocol.Protocol):
+    
     def __init__(self,logging=None):
         self.logging=logging
+    
     def connectionMade(self):
         self.buf=""
         self.otherConn=None
@@ -54,7 +92,7 @@ class SOCKSv4(protocol.Protocol):
             try:
                 version,code,port=struct.unpack("!BBH",head[:4])
             except struct.error:
-                assert 0,"struct error with head='%s' and buf='%s'"%(repr(head),repr(self.buf))
+                raise RuntimeError, "struct error with head='%s' and buf='%s'"%(repr(head),repr(self.buf))
             user,self.buf=string.split(self.buf,"\000",1)
             if head[4:7]=="\000\000\000": # domain is after
                 server,self.buf=string.split(self.buf,'\000',1)
@@ -75,7 +113,7 @@ class SOCKSv4(protocol.Protocol):
                 ourip,ourport=self.serv.socket.getsockname()
                 self.makeReply(90,0,ourport,ourip)
             else:
-                assert 0, "Bad Connect Code: %s" % code
+                raise RuntimeError, "Bad Connect Code: %s" % code
             assert self.buf=="","hmm, still stuff in buffer... '%s'"
 
     def authorize(self,code,server,port,user):
@@ -122,14 +160,26 @@ class SOCKSv4(protocol.Protocol):
             self.ip=""
             return 0
 
+
 class SOCKSv4Factory(protocol.Factory):
-    def __init__(self,log):
-        self.logging=log
+    """A factory for a SOCKSv4 proxy.
+    
+    Constructor accepts one argument, a logfile.
+    """
+    
+    def __init__(self, log):
+        self.logging = log
+    
     def buildProtocol(self,addr):
         return SOCKSv4(self.logging)
 
+
 class SOCKSv4IncomingFactory(protocol.Factory):
+    """A utility class for building protocols for incoming connections."""
+    
     def __init__(self,socks):
         self.socks=socks
+    
     def buildProtocol(self,addr):
         return SOCKSv4Incoming(self.socks)
+
