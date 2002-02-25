544b35e363df9e2f2ab970572b72b03f8562f753
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-02-25 00:49:25 +0000

    better default result code
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1142 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/socks.py
+++ twisted/protocols/socks.py
@@ -123,7 +123,7 @@ class SOCKSv4(protocol.Protocol):
         log.msg("code %s connection to %s:%s (user %s) authorized" % (code,server,port,user))
         return 1
 
-    def makeReply(self,reply,version=4,port=0,ip="0.0.0.0"):
+    def makeReply(self,reply,version=0,port=0,ip="0.0.0.0"):
         self.transport.write(struct.pack("!BBH",version,reply,port)+socket.inet_aton(ip))
         if reply!=90: self.transport.loseConnection()
 
