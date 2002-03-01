807b161f9c9d99e1011f229ff7af18ef8ff4ff12
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-03-01 16:15:33 +0000

    fixed DNS example to work with latest code
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1172 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/dnstest.py
+++ doc/examples/dnstest.py
@@ -15,7 +15,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from twisted.protocols import dns
+from twisted.names import dns
 from twisted.internet import main
 
 def printMessage( message ):
