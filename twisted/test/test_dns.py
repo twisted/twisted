074553e44d8970822a7b6904442c51cb2f47dfd5
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-03-03 22:24:32 +0000

    beginning of DNS protocol tests
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1191 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/test/test_dns.py
@@ -0,0 +1,61 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Tests for twisted.protocols.dns.
+"""
+
+from pyunit import unittest
+from StringIO import StringIO
+
+from twisted.protocols import dns
+
+
+class RountripDNSTestCase(unittest.TestCase):
+    """Decoding and then encoding various objects."""
+    
+    names = ["example.org", "go-away.fish.tv", "23strikesback.net"]
+    
+    def testName(self):
+        for n in self.names:
+            # encode the name
+            f = StringIO()
+            dns.Name(n).encode(f)
+            
+            # decode the name
+            f.seek(0, 0)
+            result = dns.Name()
+            result.decode(f)
+            self.assertEquals(result.name, n)
+    
+    def testQuery(self):
+        for n in self.names:
+            for dnstype in range(1, 17):
+                for dnscls in range(1, 5):
+                    # encode the query
+                    f = StringIO()
+                    dns.Query(n, dnstype, dnscls).encode(f)
+                    
+                    # decode the result
+                    f.seek(0, 0)
+                    result = dns.Query()
+                    result.decode(f)
+                    self.assertEquals(result.name.name, n)
+                    self.assertEquals(result.type, dnstype)
+                    self.assertEquals(result.cls, dnscls)
+    
+
