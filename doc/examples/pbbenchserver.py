8757e874f86ed99f0f8532c9b49f4da1f38020a7
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-04-08 04:50:44 +0000

    server calls per second
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1450 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/pbbenchserver.py
+++ doc/examples/pbbenchserver.py
@@ -3,8 +3,15 @@ from twisted.spread import pb
 from twisted.internet import main, app
 
 class PBBenchPerspective(pb.Perspective):
+    callsPerSec = 0
     def perspective_simple(self):
+        self.callsPerSec = self.callsPerSec + 1
         return None
+
+    def printCallsPerSec(self):
+        print '(s) cps:', self.callsPerSec
+        self.callsPerSec = 0
+        main.addTimeout(self.printCallsPerSec, 1)
 
     def perspective_complexTypes(self):
         return ['a', 1, 1l, 1.0, [], ()]
@@ -15,5 +22,7 @@ class PBBenchService(pb.Service):
 a = app.Application("pbbench")
 a.listenTCP(8787, pb.BrokerFactory(pb.AuthRoot(a)))
 b = PBBenchService("benchmark", a)
-b.createPerspective("benchmark").makeIdentity("benchmark")
+u = b.createPerspective("benchmark")
+u.makeIdentity("benchmark")
+u.printCallsPerSec()
 a.run()
