8611cf1a5b54bc4fdde00205b438c1c8d143563d
dp <dp@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-05-13 06:08:31 +0000

    Initial stab at a mvc architecture for twisted. Suggestions/comments requested. Todo: Create wmvc, web model view controller with some additional conveniences for use building web pages, and refactoring Blog to use the new interfaces.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1722 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/python/mvc.py
@@ -0,0 +1,212 @@
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+A simple Model-View-Controller framework for separating
+presentation, business logic, and data.
+
+A google search reveals several interesting pages to refer
+to while designing this implementation:
+
+http://www.object-arts.com/EducationCentre/Overviews/MVC.htm
+
+Model-View-Presenter is a slightly newer concept developed by IBM
+in the early-mid nineties and now used extensively in Dolphin SmallTalk:
+
+http://www.object-arts.com/EducationCentre/Overviews/ModelViewPresenter.htm
+
+Pretty pictures, for sure. I'll try to keep this implementation simple
+while trying to learn as much as possible from previous implementations.
+"""
+
+# TODO: Warning, Danger, Danger, Warning
+# This module creates lots of circular references
+# Do we care?
+
+from twisted.python import components
+
+# Should these interfaces be somewhere else?
+
+class IModel(components.Interface):
+    """A MVC Model."""
+    def addView(self, view):
+        """Add a view for the model to keep track of."""
+        raise NotImplementedError
+
+    def removeView(self, view):
+        """Remove a view that the model no longer should keep track of."""
+        raise NotImplementedError
+
+    def notify(self, changed=None):
+        """
+        Notify all views that something was changed on me.
+        Passing a dictionary of {'attribute': 'new value'} in changed
+        will pass this dictionary to the view for increased performance.
+        If you don't want to do this, don't, and just use the traditional
+        MVC paradigm of querying the model for things you're interested
+        in.
+        """
+        raise NotImplementedError
+
+class IView(components.Interface):
+    """A MVC View"""
+    def __init__(self, model, controller=None):
+        """
+        A view must be told what it's model is, and may be told what it's
+        controller is, but can also look up it's controller if none specified.
+        """
+        raise NotImplementedError
+
+    def draw(self):
+        """Hook subclasses can override to implement drawing the whole view"""
+
+    def modelChanged(self, changed):
+        """
+        Dispatch changed messages to any update_* methods which
+        may have been defined, then pass the update notification on
+        to the controller.
+        """
+
+    def controllerFactory(self):
+        """
+        Hook for subclasses to customize the controller that is associated
+        with the model associated with this view.
+        
+        Default behavior: Look up a component that implements IController
+        for the self.model instance.
+        """
+
+class IController(components.Interface):
+    """A MVC Controller"""
+
+
+# Should all these docstrings be duplicated in the implementation
+# of the interfaces?
+
+class Model:
+    """
+    A Model which keeps track of views which are looking at it in order
+    to notify them when the model changes.
+    """
+    __implements__ = IModel
+    
+    def __init__(self):
+        self.views = []
+
+    def addView(self, view):
+        """
+        Add a view for the model to keep track of.
+        """
+        self.views.append(view)
+    
+    def removeView(self, view):
+        """
+        Remove a view that the model no longer should keep track of.
+        """
+        self.views.remove(view)
+    
+    def notify(self, changed=None):
+        """
+        Notify all views that something was changed on me.
+        Passing a dictionary of {'attribute': 'new value'} in changed
+        will pass this dictionary to the view for increased performance.
+        If you don't want to do this, don't, and just use the traditional
+        MVC paradigm of querying the model for things you're interested
+        in.
+        """
+        for view in self.views:
+            view.modelChanged(changed)
+
+    def __eq__(self, other):
+        for elem in self.__dict__.keys():
+            if elem is "views": continue
+            if getattr(self, elem) != getattr(other, elem):
+                return 0
+        else:
+            return 1
+    
+    def __ne__(self, other):
+        for elem in self.__dict__.keys():
+            if elem is "views": continue
+            if getattr(self, elem) == getattr(other, elem):
+                return 0
+        else:
+            return 1
+
+
+class View:
+    """
+    A View which tracks a model and displays it's contents to the user.
+    """
+    __implements__ = IView
+    
+    def __init__(self, model, controller=None):
+        """
+        A view must be told what it's model is, and may be told what it's
+        controller is, but can also look up it's controller if none specified.
+        """
+        model.addView(self)
+        self.model = model
+        if controller:
+            self.controller = controller
+        else:
+            self.controller = self.controllerFactory(model, self)
+        self.draw()
+    
+    def draw(self):
+        """
+        Hook subclasses can override to implement drawing the initial view
+        after the class is done initializing, without overriding __init__
+        """
+        pass
+
+    def modelChanged(self, changed):
+        """
+        Dispatch changed messages to any update_* methods which
+        may have been defined, then pass the update notification on
+        to the controller.
+        """
+        for name in changed.keys():
+            handler = getattr(self, 'update_' + name, None)
+            if handler:
+                apply(handler, (changed[name],))
+
+    def controllerFactory(self, model, view):
+        """
+        Hook for subclasses to customize the controller that is associated
+        with the model associated with this view.
+        """
+        # TODO: Decide what to do as a default Controller
+        # if you don't need to use one...
+        # Something that ignores all messages?
+        controller = components.getAdapter(model, IController, None)
+        controller.setView(self)
+        return controller
+
+class Controller:
+    """
+    A Controller which translates commands the user executed on the
+    view into explicit manipulations of the model. This is where the
+    business logic lives.
+    """
+    __implements__ = IController
+    
+    def __init__(self, model):
+        self.model = model
+
+    def setView(self, view):
+        self.view = view
---
+++ twisted/test/test_mvc.py
@@ -0,0 +1,139 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Test cases for Twisted Model-View-Controller architecture."""
+
+import random
+
+from pyunit import unittest
+
+from twisted.python import mvc
+from twisted.python import components
+
+# simple pickled string storage to test persistence
+persisted_model = ""
+
+class MyModel(mvc.Model):
+    def __init__(self, foo, random=None):
+        # I hate having to explicitly initialize the super
+        mvc.Model.__init__(self)
+        self.foo=foo
+        self.random=random
+
+class MyView(mvc.View):        
+    def draw(self):
+        # pretend self.foo is what the user now sees on their screen
+        self.foo = self.model.foo
+        self.random = self.model.random
+
+    def update_foo(self, newValue):
+        # pretend self.foo is what the user actually sees on the screen
+        self.foo = newValue
+   
+    def update_random(self, newValue):
+        # pretend self.random is what the user actually sees on the screen
+        self.random = newValue
+    
+    def twiddleControl(self, newValue):
+        """
+        The user twiddled a control onscreen, causing this event
+        """
+        self.controller.setFoo(newValue)
+    
+    def pushButton(self):
+        """
+        The user hit a button onscreen, causing this event
+        """
+        return self.controller.doRandom()
+
+# Register MyView as the view for instances of type MyModel
+components.registerAdapter(MyView, MyModel, mvc.IView)
+
+class MyController(mvc.Controller):
+    def setFoo(self, newValue):
+        self.model.foo = newValue
+        self.model.notify({'foo': newValue})
+        self.persist()
+    
+    def doRandom(self):
+        rnd = random.choice(range(100))
+        self.model.random = rnd
+        self.model.notify({'random': rnd})
+        self.persist()
+        return rnd
+    
+    def persist(self):
+        """
+        Save the model object to persistent storage
+        """
+        global persisted_model
+        
+        from cPickle import dumps
+        persisted_model = dumps(self.model)
+
+# Register MyController as the controller for instances of type MyModel
+components.registerAdapter(MyController, MyModel, mvc.IController)
+
+class MVCTestCase(unittest.TestCase):
+    """Test MVC."""
+    def setUp(self):
+        self.model = MyModel("foo")
+
+    def getView(self):
+        return components.getAdapter(self.model, mvc.IView, None)
+
+    def testViewConstruction(self):
+        view = self.getView()
+        self.assert_(isinstance(view, MyView))
+
+    def testControllerConstruction(self):
+        view = self.getView()
+        self.assert_(isinstance(view.controller, MyController))
+    
+    def testModelManipulation(self):
+        view = self.getView()
+        view.twiddleControl("bar")
+        self.assertEquals("bar", self.model.foo)
+    
+    def testMoreModelManipulation(self):
+        view = self.getView()
+        value = view.pushButton()
+        self.assertEquals(value, self.model.random)
+
+    def testViewManipulation(self):
+        """When the model updates the view should too"""
+        view = self.getView()
+        view.twiddleControl("bar")
+        self.assertEquals("bar", view.foo)
+    
+    def testMoreViewManipulation(self):
+        """When the model updates the view should too"""
+        view = self.getView()
+        value = view.pushButton()
+        self.assertEquals(value, view.random)
+
+    def testPersistence(self):
+        """See if the automatically-persisting model (persisted by the
+        controller) matches our live model"""
+        global persisted_model
+        
+        view = self.getView()
+        view.twiddleControl("ASDFASDF")
+        from cPickle import loads
+        loadedModel = loads(persisted_model)
+        self.assertEquals(loadedModel, self.model)
+
+testCases = [MVCTestCase]
