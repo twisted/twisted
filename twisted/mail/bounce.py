ca4a1a21b56f8c74b046393cb24b50035da51612
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-05-24 12:00:14 +0000

    Preliminary bounce message code.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@1802 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/mail/bounce.py
@@ -0,0 +1,57 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import cStringIO, rfc822, string, time, os
+
+
+def generateBounce(message, failedFrom, failedTo, transcript=''):
+    if not transcript:
+        transcript = '''\
+I'm sorry, the following address has permanent errors: %(failedTo)s.
+I've given up, and I will not retry the message again.
+''' % vars()
+    boundary = "%s_%s_%s" % (time.time(), os.getpid(), 'XXXXX')
+    fp = cStringIO.StringIO()
+    failedAddress = rfc822.AddressList(failedTo)[0][1]
+    failedDomain = string.split(failedAddress, '@', 1)[1]
+    fp.write('From: postmaster@'+failedDomain+'\n')
+    fp.write('To: '+failedFrom+'\n')
+    fp.write('Subject: Returned Mail: see transcript for details\n')
+    fp.write('Content-Type: multipart/report; report-type=delivery-status; boundary="%s"\n' % boundary)
+    fp.write('\n')
+    fp.write('--%s\n' % boundary)
+    fp.write('\n')
+    fp.write(transcript)
+    fp.write('\n')
+    fp.write('--%s\n' % boundary)
+    fp.write('Content-Type: message/delivery-status')
+    fp.write('\n')
+    fp.write('Arrival-Date: %s\n' % time.ctime(time.time()))
+    fp.write('\n')
+    fp.write('Final-Recipient: RFC822; ' + failedTo + '\n')
+    orig = message.tell()
+    message.seek(2, 0)
+    sz = message.tell()
+    message.seek(0, orig)
+    if sz > 10000:
+        while 1:
+            line = message.readline()
+            if len(line)<=1:
+                break
+            fp.write(line)
+    else:
+        fp.write(message.read())
+    return '', failedFrom, fp.getvalue()
---
+++ twisted/test/test_bounce.py
@@ -0,0 +1,44 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Test cases for bounce message generation
+"""
+
+from pyunit import unittest
+from twisted.mail import bounce
+import rfc822, cStringIO
+
+class BounceTestCase(unittest.TestCase):
+    """
+    testcases for bounce message generation
+    """
+
+    def testBounceFormat(self):
+        from_, to, s = bounce.generateBounce(cStringIO.StringIO('''\
+From: Moshe Zadka <moshez@example.com>
+To: nonexistant@example.org
+Subject: test
+
+'''), 'moshez@example.com', 'nonexistant@example.org')
+        self.assertEquals(from_, '')
+        self.assertEquals(to, 'moshez@example.com')
+        mess = rfc822.Message(cStringIO.StringIO(s))
+	self.assertEquals(mess['To'], 'moshez@example.com')
+	self.assertEquals(mess['From'], 'postmaster@example.org')
+	self.assertEquals(mess['subject'], 'Returned Mail: see transcript for details')
+
+    def testBounceMIME(self):
+        pass
