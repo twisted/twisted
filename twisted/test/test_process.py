aef8650b158e02fb79e9203e377bd1bf41d3d948
carmstro <carmstro@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-07-07 07:36:06 +0000

    make the Stdio test a bit easier to debug
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2084 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_process.py
+++ twisted/test/test_process.py
@@ -119,7 +119,7 @@ class PosixProcessTestCase(unittest.TestCase):
         p.transport.closeStdin()
         while not p.closed:
             reactor.iterate()
-        self.assertEquals(p.outF.getvalue(), "hello, worldabc123")
+        self.assertEquals(p.outF.getvalue(), "hello, worldabc123", p.errF.getvalue())
     
     def testProcess(self):
         if os.path.exists('/bin/gzip'): cmd = '/bin/gzip'
