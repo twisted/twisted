3717163be73669526e75aa6f9787116ada819ffc
carmstro <carmstro@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-07-07 07:02:30 +0000

    preparing for 0.19.0rc1
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2082 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/copyright.py
+++ twisted/copyright.py
@@ -21,7 +21,7 @@ Copyright information for Twisted.
 """
 
 
-version="0.18.0-post"
+version="0.19.0rc1"
 longversion="Twisted %s" % version
 copyright="Copyright (c) 2000-2002 Matthew William Lefkowitz, all rights reserved."
 
