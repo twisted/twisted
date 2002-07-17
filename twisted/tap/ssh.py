7471f312a6a93ae62b6abcceb7dc157ed577b50a
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-07-17 16:21:58 +0000

    get rid of irrelevant warning
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2207 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/tap/ssh.py
+++ twisted/tap/ssh.py
@@ -25,21 +25,14 @@ import sys
 
 class Options(usage.Options):
     synopsis = "Usage: mktap sshd [-i <interface>] [-p <port>] [-d <dir>]"
-    optParameters = [["interface", "i", "127.0.0.1", "local interface to which we listen"],
+    optParameters = [["interface", "i", "", "local interface to which we listen"],
                   ["port", "p", 5822],
                   ["data", "d", "/etc", "directory to look for host keys in"]]
 
     longdesc = "Makes a SSH server.."
 
 def updateApplication(app, config):
-    if config.interface != "127.0.0.1":
-        print
-        print "WARNING:"
-        print "  You have chosen to listen on a non-local interface."
-        print "  This may allow intruders to access your local network"
-        print "  if you run this on a firewall."
-        print
-    t =factory.OpenSSHFactory()
+    t = factory.OpenSSHFactory()
     t.dataRoot = config.opts['data']
     portno = int(config.opts['port'])
     app.listenTCP(portno, t, interface=config.opts['interface'])
