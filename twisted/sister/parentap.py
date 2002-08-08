94fd485f5832ce3441385fba03db8e559433f7ef
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-08-08 19:53:47 +0000

    Renamed ParentService to MotherService in tap file (already renamed elsewhere)
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2352 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/sister/parentap.py
+++ twisted/sister/parentap.py
@@ -1,15 +1,15 @@
 
 from twisted.python import usage
 from twisted.spread import pb
-from twisted.sister.parentserv import ParentService
+from twisted.sister.parentserv import MotherService
 
 class Options(usage.Options):
     optParameters = [
         ["port", "p", 7879, "Port number to listen on."],
         ["secret", "s", "qux", "Shared Secret"],
-        ["service", "", "twisted.sister.parent", "Service Name of Parent"]
+        ["service", "", "twisted.sister.parent", "Service Name of Mother"]
         ]
 
 def updateApplication(app, config):
-    app.addService(ParentService(config['secret'], config['service'], app))
+    app.addService(MotherService(config['secret'], config['service'], app))
     app.listenTCP(config['port'], pb.BrokerFactory(pb.AuthRoot(app)))
