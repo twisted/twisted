e4a409b7fb75924cbf5f5e649d68f6989165b3b2
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-09-01 09:10:27 +0000

    ignored some files I shouldn't have been paying attention to, added a non-ssl echo client that might be useful in some cases.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2585 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/examples/echoclient.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from twisted.internet.protocol import ClientFactory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet.app import Application
+from twisted.internet import reactor
+import sys
+
+class EchoClient(LineReceiver):
+    end="Bye-bye!"
+    def connectionMade(self):
+        self.sendLine("Hello, world!")
+        self.sendLine("What a fine day it is.")
+        self.sendLine(self.end)
+
+    def lineReceived(self, line):
+        print "receive:", line
+        if line==self.end:
+            self.transport.loseConnection()
+
+class EchoClientFactory(ClientFactory):
+    protocol = EchoClient
+
+    def clientConnectionFailed(self, connector, reason):
+        print 'connection failed:', reason.getErrorMessage()
+        reactor.stop()
+
+    def clientConnectionLost(self, connector, reason):
+        print 'connection lost:', reason.getErrorMessage()
+        reactor.stop()
+
+factory = EchoClientFactory()
+reactor.connectTCP('localhost', 8000, factory)
+reactor.run()
