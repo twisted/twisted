062e416953c9302a3ca11e1e8cd3c1f7d37e08c9
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-09-10 06:46:42 +0000

    Adding tap support to inetd
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2675 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/inetd/inetd.py
+++ twisted/inetd/inetd.py
@@ -103,119 +103,15 @@ class InetdFactory(ServerFactory):
         self.service = service
 
 
-class InetdOptions(usage.Options):
-    optParameters = [['file', 'f', '/etc/inetd.conf'],]
-
-
 def main(options=None):
+    from twisted.inetd import tap
     # Parse options, read various config files
     if not options:
-        options = InetdOptions()
+        options = tap.Options()
         options.parseOptions()
     
-    conf = inetdconf.InetdConf()
-    conf.parseFile(open(options['file']))
-
-    rpcConf = inetdconf.RPCServicesConf()
-    try:
-        rpcConf.parseFile()
-    except:
-        # We'll survive even if we can't read /etc/rpc
-        log.deferr()
-    
-    app = Application('tinetd')
-
-    for service in conf.services:
-        rpc = service.protocol.startswith('rpc/')
-        protocol = service.protocol
-
-        if rpc and not rpcOk:
-            log.msg('Skipping rpc service due to lack of rpc support')
-            continue
-
-        if rpc:
-            # RPC has extra options, so extract that
-            protocol = protocol[4:]     # trim 'rpc/'
-            if not protocolDict.has_key(protocol):
-                log.msg('Bad protocol: ' + protocol)
-                continue
-            
-            try:
-                name, rpcVersions = service.name.split('/')
-            except ValueError:
-                log.msg('Bad RPC service/version: ' + service.name)
-                continue
-
-            if not rpcConf.services.has_key(name):
-                log.msg('Unknown RPC service: ' + repr(service.name))
-                continue
-
-            try:
-                if '-' in rpcVersions:
-                    start, end = map(int, rpcVersions.split('-'))
-                    rpcVersions = range(start, end+1)
-                else:
-                    rpcVersions = [int(rpcVersions)]
-            except ValueError:
-                log.msg('Bad RPC versions: ' + str(rpcVersions))
-                continue
-            
-        if (protocol, service.socketType) not in [('tcp', 'stream'),
-                                                  ('udp', 'dgram')]:
-            log.msg('Skipping unsupported type/protocol: %s/%s'
-                    % (service.socketType, service.protocol))
-            continue
-
-        # Convert the username into a uid (if necessary)
-        try:
-            service.user = int(service.user)
-        except ValueError:
-            try:
-                service.user = pwd.getpwnam(service.user)[2]
-            except KeyError:
-                log.msg('Unknown user: ' + service.user)
-                continue
-
-        # Convert the group name into a gid (if necessary)
-        if service.group is None:
-            # If no group was specified, use the user's primary group
-            service.group = pwd.getpwuid(service.user)[3]
-        else:
-            try:
-                service.group = int(service.group)
-            except ValueError:
-                try:
-                    service.group = grp.getgrnam(service.group)[2]
-                except KeyError:
-                    log.msg('Unknown group: ' + service.group)
-                    continue
-
-        log.msg('Adding service:', service.name, service.port, protocol)
-        if service.program == 'internal':
-            # Internal services can use a standard ServerFactory
-            if not internalProtocols.has_key(service.name):
-                log.msg('Unknown internal service: ' + service.name)
-                continue
-            factory = ServerFactory()
-            factory.protocol = internalProtocols[service.name]
-        elif rpc:
-            proto = protocolDict[protocol]
-            p = reactor.listenTCP(0, ServerFactory())
-            portNo = p.getHost()[2]
-            for version in rpcVersions:
-                portmap.set(rpcConf.services[name], version, proto, portNo)
-            forkPassingFD(service.program, service.programArgs, os.environ,
-                          service.user, service.group, p)
-            continue
-        else:
-            # Non-internal non-rpc services use InetdFactory
-            factory = InetdFactory(service)
-
-        if protocol == 'tcp':
-            app.listenTCP(service.port, factory)
-        elif protocol == 'udp':
-            app.listenUDP(service.port, factory)
-    
+    app = Application('tinet')
+    tap.updateApplications(app, options)
     app.run(save=0)
 
 
