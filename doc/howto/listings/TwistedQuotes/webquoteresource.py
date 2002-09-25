9e14ee4168311da3be4e5ba5752229a268e93a7b
dp <dp@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-09-25 20:49:32 +0000

    Update DOMTemplate example to use web.woven.template instead of web.domtemplate
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2972 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/howto/listings/TwistedQuotes/webquoteresource.py
+++ doc/howto/listings/TwistedQuotes/webquoteresource.py
@@ -1,17 +1,17 @@
-from twisted.web import domtemplate
+from twisted.web.woven import template
 from twisted.python import domhelpers #helpers for munging the DOM
 
 
 from TwistedQuotes import quoters
 
-class QuoteResource(domtemplate.DOMTemplate):
+class QuoteResource(template.DOMTemplate):
     """I am a DOMTemplate that displays a fancy quote page."""
     
     #The template; this must be valid XML (parsable by Python's DOM implementation)
     templateFile = "WebQuotes.xhtml"
     
     def __init__(self, filenames):
-        domtemplate.DOMTemplate.__init__(self)
+        template.DOMTemplate.__init__(self)
         self.quoter = quoters.FortuneQuoter(filenames)        
     
     def factory_getQuote(self, request, node):
