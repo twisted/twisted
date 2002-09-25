df14b8fb360dc272c93681fe8d7a9800d0188566
spiv <spiv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-09-25 00:34:32 +0000

    twisted.inetd is semi-stable.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2938 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/inetd/__init__.py
+++ twisted/inetd/__init__.py
@@ -1,1 +1,8 @@
-"""Twisted inetd: classic inetd(8) support"""
+"""Twisted inetd: classic inetd(8) support
+
+Maintainer: Andrew Bennetts <spiv@twistedmatrix.com>
+
+This module is semi-stable:
+  - The basic design will probably remain, but there are some bugs that need
+    fixing that affects UDP and Sun-RPC support.
+"""
