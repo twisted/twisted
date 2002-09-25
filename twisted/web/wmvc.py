beafbc86a54be4ba35984d0259ee601206db02f3
dp <dp@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-09-25 19:40:59 +0000

    Deprecate wmvc
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@2965 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/wmvc.py
+++ twisted/web/wmvc.py
@@ -20,23 +20,28 @@ webmvc.py
 The webmvc module collects together most of the modules and functions you will need to use Twisted's model view controller architecture for the web.
 """
 
-from twisted.web import domtemplate
+from twisted.python import log
+log.write("DeprecationWarning: twisted.web.wmvc has been renamed twisted.web.woven. It has also been split into template, model, view, and controller modules.\n")
+
 from twisted.python import mvc
 from twisted.python import components
-from twisted.web import domwidgets
-from twisted.web import domhandlers
 from twisted.web import resource
+from twisted.web.woven import template
+from twisted.web.woven import widgets
+from twisted.web.woven import input
 
+domtemplate = template
+domwidgets = widgets
+domhandlers = input
+dominput = input
 
-WModel = mvc.Model
-WView = domtemplate.DOMTemplate
-WController = domtemplate.DOMController
+from twisted.web.woven import model
+from twisted.web.woven import view
+from twisted.web.woven import controller
 
+WModel = model.WModel
+WView = view.WView
+WController = controller.WController
 
-def registerViewForModel(view, model):
-    components.registerAdapter(view, model, mvc.IView)
-
-def registerControllerForModel(controller, model):
-    components.registerAdapter(controller, model, mvc.IController)
-    components.registerAdapter(controller, model, resource.IResource)
-
+registerViewForModel = view.registerViewForModel
+registerControllerForModel = controller.registerControllerForModel
