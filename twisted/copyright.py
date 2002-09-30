cc9c667857a775937c39e1d1220a7f37e96173e1
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-09-30 08:34:23 +0000

    Preparing for 0.99.3alpha4
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3079 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/copyright.py
+++ twisted/copyright.py
@@ -21,7 +21,7 @@ Copyright information for Twisted.
 """
 
 
-version="0.99.3alpha3"
+version="0.99.3alpha4"
 longversion="Twisted %s" % version
 copyright="Copyright (c) 2000-2002 Matthew William Lefkowitz, all rights reserved."
 
