d1d0b517712a56ac6ce96fc17c645c992a7ab8d3
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-06 14:27:13 +0000

    Bring back patches from 0.99.3 branch
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3224 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/copyright.py
+++ twisted/copyright.py
@@ -21,7 +21,7 @@ Copyright information for Twisted.
 """
 
 
-version="0.99.3rc1"
+version="0.99.3"
 longversion="Twisted %s" % version
 copyright="Copyright (c) 2000-2002 Matthew William Lefkowitz, all rights reserved."
 
