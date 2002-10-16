126140d76cf90a014a5dd3d5e4174c7cabde9d17
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-16 03:49:44 +0000

    much cleaner script install.
    For example, mktap on Windows is now c:\Python22\Scripts\mktap.py
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3370 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- setup.py
+++ setup.py
@@ -22,14 +22,14 @@ Package installer for Twisted
 Copyright (C) 2001 Matthew W. Lefkowitz
 All rights reserved, see LICENSE for details.
 
-$Id: setup.py,v 1.65 2002/10/13 11:47:00 spiv Exp $
+$Id: setup.py,v 1.66 2002/10/16 03:49:44 itamarst Exp $
 """
 
 import distutils, os, sys, string
 from glob import glob
 
 from distutils.core import setup, Extension
-from distutils.command.build_scripts import build_scripts
+from distutils.command.install_scripts import install_scripts
 from distutils.command.install_data import install_data
 from distutils.ccompiler import new_compiler
 from distutils.errors import CompileError
@@ -42,102 +42,14 @@ from twisted import copyright
 ### Helpers and distutil tweaks
 #############################################################################
 
-script_preamble = """
-### Twisted Preamble
-# This makes sure that users don't have to set up their environment
-# specially in order to run these programs from bin/.
-import sys, os, string
-if string.find(os.path.abspath(sys.argv[0]), os.sep+'Twisted') != -1:
-    sys.path.insert(0, os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]), os.pardir, os.pardir)))
-sys.path.insert(0, os.curdir)
-### end of preamble
-"""
+class install_scripts_twisted(install_scripts):
+    """Renames scripts so they end with '.py' on Windows."""
 
-class build_scripts_create(build_scripts):
-    """ Overload the build_scripts command and create the scripts
-        from scratch, depending on the target platform.
-
-        You have to define the name of your package in an inherited
-        class (due to the delayed instantiation of command classes
-        in distutils, this cannot be passed to __init__).
-
-        The scripts are created in an uniform scheme: they start the
-        run() function in the module
-
-            <packagename>.scripts.<mangled_scriptname>
-
-        The mangling of script names replaces '-' and '/' characters
-        with '_' and '.', so that they are valid module paths. 
-    """
-    package_name = None
-
-    def copy_scripts(self):
-        """ Create each script listed in 'self.scripts'
-        """
-        if not self.package_name:
-            raise Exception("You have to inherit build_scripts_create and"
-                " provide a package name")
-        
-        to_module = string.maketrans('-/', '_.')
-
-        self.mkpath(self.build_dir)
-        for script in self.scripts:
-            outfile = os.path.join(self.build_dir, os.path.basename(script))
-
-            #if not self.force and not newer(script, outfile):
-            #    self.announce("not copying %s (up-to-date)" % script)
-            #    continue
-
-            if self.dry_run:
-                self.announce("would create %s" % outfile)
-                continue
-
-            module = os.path.splitext(os.path.basename(script))[0]
-            module = string.translate(module, to_module)
-            script_vars = {
-                'python': os.path.normpath(sys.executable),
-                'package': self.package_name,
-                'module': module,
-                'preamble': script_preamble,
-            }
-
-            self.announce("creating %s" % outfile)
-            file = open(outfile, 'w')
-
-            try:
-                if sys.platform == "win32":
-                    file.write('@echo off\n'
-                        'if NOT "%%_4ver%%" == "" %(python)s -c "from %(package)s.scripts.%(module)s import run; run()" %%$\n'
-                        'if     "%%_4ver%%" == "" %(python)s -c "from %(package)s.scripts.%(module)s import run; run()" %%*\n'
-                        % script_vars)
-                else:
-                    file.write('#! %(python)s\n'
-                        '%(preamble)s\n'
-                        'from %(package)s.scripts.%(module)s import run\n'
-                        'run()\n'
-                        % script_vars)
-            finally:
-                file.close()
-                os.chmod(outfile, 0755)
-
-
-class build_scripts_twisted(build_scripts_create):
-    package_name = 'twisted'
-
-
-def scriptname(path):
-    """ Helper for building a list of script names from a list of
-        module files.
-    """
-    script = os.path.splitext(os.path.basename(path))[0]
-    script = string.replace(script, '_', '-')
-    if sys.platform == "win32":
-        script = script + ".bat"
-    return script
-
-
-# build list of scripts from their implementation modules
-twisted_scripts = map(scriptname, glob('twisted/scripts/[!_]*.py'))
+    def run(self):
+        install_scripts.run(self)
+        if os.name == "nt":
+            for file in self.get_outputs():
+                os.rename(file, file + ".py")
 
 
 # make sure data files are installed in twisted package
@@ -302,10 +214,13 @@ your toaster.
         "twisted.words",
         "twisted.zoot",
     ],
-
+    'scripts' : [
+        'bin/manhole', 'bin/mktap', 'bin/twistd',
+        'bin/im', 'bin/t-im', 'bin/tap2deb',
+        'bin/coil', 'bin/tapconvert',
+    ],
     'cmdclass': {
-        ## NOT YET for all platforms, see below for win32 test code!
-        ##'build_scripts': build_scripts_twisted,
+        'install_scripts': install_scripts_twisted,
         'install_data': install_data_twisted,
         'build_ext' : build_ext_twisted,
     },
@@ -317,18 +232,6 @@ if hasattr(distutils.dist.DistributionMetadata, 'get_keywords'):
 if hasattr(distutils.dist.DistributionMetadata, 'get_platforms'):
     setup_args['platforms'] = "win32 posix"
 
-if os.name == 'posix':
-    setup_args['scripts'] = [
-        'bin/manhole', 'bin/mktap', 'bin/twistd',
-        'bin/im', 'bin/t-im', 'bin/tap2deb',
-        'bin/coil', 'bin/tapconvert',
-    ]
-else:
-    # new script schema only for win32, for now
-    setup_args['scripts'] = twisted_scripts
-    setup_args['cmdclass']['build_scripts'] = build_scripts_twisted
-    
-
 imPath = os.path.join('twisted', 'im')
 setup_args['data_files'] = [(imPath, [os.path.join(imPath, 'instancemessenger.glade')]),
                             ('twisted', [os.path.join('twisted', 'plugins.tml')])]
@@ -338,7 +241,7 @@ if os.name == 'nt':
     define_macros = [("WIN32", 1)]
 else:
     define_macros = []
-        
+
 # Include all extension modules here, whether they are built or not.
 # The custom built_ext command will wipe out this list anyway, but it
 # is required for sdist to work.
@@ -353,4 +256,3 @@ setup_args['ext_modules'] = [
 
 if __name__ == '__main__':
     apply(setup, (), setup_args)
-
