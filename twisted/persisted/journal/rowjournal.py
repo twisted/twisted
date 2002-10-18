c86527e2965dbada6941444370f93ed0d27a740c
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-18 01:01:26 +0000

    store straight list of queued SQL commands instead of trying to be more efficient
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3411 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/persisted/journal/rowjournal.py
+++ twisted/persisted/journal/rowjournal.py
@@ -37,33 +37,8 @@ from twisted.internet import defer
 import base
 
 
-class OrderedSet:
-    """Store keys in order they were inserted in."""
-
-    def __init__(self):
-        self.keys = []
-        self.map = {}
-
-    def add(self, key):
-        if self.map.has_key(key):
-            return
-        else:
-            self.keys.append(key)
-            self.map[key] = len(self.keys) - 1
-
-    def __delitem__(self, key):
-        i = self.map[key]
-        del self.keys[i:i+1]
-        del self.map[key]
-
-    def has_key(self, key):
-        return self.map.has_key(key)
-
-    def keys(self):
-        return self.keys
-
-    def clear(self):
-        self.__init__(self)
+# constants for command list
+INSERT, DELETE, UPDATE = range(3)
 
 
 class RowJournal(base.Journal):
@@ -76,35 +51,21 @@ class RowJournal(base.Journal):
 
     def __init__(self, log, journaledService, reflector):
         self.reflector = reflector
-        self.dirtyRows = OrderedSet()
-        self.insertedRows = OrderedSet()
-        self.deletedRows = OrderedSet()
+        self.commands = []
         self.syncing = 0
         base.Journal.__init__(self, log, journaledService)
     
     def updateRow(self, obj):
         """Mark on object for updating when sync()ing."""
-        if self.insertedRows.has_key(obj):
-            self.insertedRows.add(obj)
-        else:
-            self.dirtyRows.add(obj)
+        self.commands.append((UPDATE, obj))
 
     def insertRow(self, obj):
         """Mark on object for inserting when sync()ing."""
-        if self.deletedRows.has_key(obj):
-            del self.deletedRows[obj]
-            self.dirtyRows.add(obj)
-        else:
-            self.insertedRows.add(obj)
+        self.commands.append((INSERT, obj))
 
     def deleteRow(self, obj):
         """Mark on object for deleting when sync()ing."""
-        if self.insertedRows.has_key(obj):
-            del self.insertedRows[obj]
-            return
-        if self.dirtyRows.has_key(obj):
-            del self.dirtyRows[obj]
-        self.deletedRows.add(obj)
+        self.commands.append((DELETE, obj))
 
     def loadObjectsFrom(self, tableName, parentRow=None, data=None, whereClause=None, forceChildren=0):
         """Flush all objects to the database and then load objects."""
@@ -118,19 +79,16 @@ class RowJournal(base.Journal):
         """Commit changes to database."""
         if self.syncing:
             raise ValueError, "sync already in progress"
-        commands = []
-        for i in self.insertedRows.keys():
-            commands.append(self.reflector.insertRowSQL(i))
-        for i in self.dirtyRows.keys():
-            commands.append(self.reflector.updateRowSQL(i))
-        for i in self.deletedRows.keys():
-            commands.append(self.reflector.deleteRowSQL(i))
-        self.insertedRows.clear()
-        self.dirtyRows.clear()
-        self.deletedRows.clear()
-        if commands:
+        comandMap = {INSERT : self.reflector.insertRowSQL,
+                     UPDATE : self.reflector.updateRowSQL,
+                     DELETE : self.reflector.deleteRowSQL}
+        sqlCommands = []
+        for kind, obj in self.commands:
+            sqlCommands.append(comandMap[kind](obj))
+        self.commands = []
+        if sqlCommands:
             self.syncing = 1
-            d = self.reflector.dbpool.runInteraction(self._sync, self.latestIndex, commands)
+            d = self.reflector.dbpool.runInteraction(self._sync, self.latestIndex, sqlCommands)
             d.addCallback(self._syncDone)
             return d
         else:
