95e0ac7863eb153634304b97e688163feec55c8b
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-20 07:42:31 +0000

    Added some comments
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3466 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/web.py
+++ doc/examples/web.py
@@ -15,6 +15,12 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+# This web server makes it possible to put it behind a reverse proxy
+# transparently. Just have the reverse proxy proxy to 
+# host:port/vhost/http/external-host:port/
+# and on redirects and other link calculation, the external-host:port will
+# be transmitted to the client.
+
 from twisted.internet import app
 from twisted.web import static, server, vhost, twcgi, script, trp
 
