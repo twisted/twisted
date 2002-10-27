5d00e6d2cd3ca3d566222f24ceaff81fc072d030
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-27 10:32:19 +0000

    Move to a DOM based implementation rather than SUX. The SUX version was already creaking at the seams.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3695 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/latex.py
+++ twisted/lore/latex.py
@@ -15,19 +15,32 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # 
 
-from twisted.protocols.sux import XMLParser
+from twisted.web import microdom, domhelpers
 import os, re
+from cStringIO import StringIO
 
 escapingRE = re.compile(r'([#$%&_{}^~])')
 lowerUpperRE = re.compile(r'([a-z])([A-Z])')
 
 def latexEscape(text):
-    return escapingRE.sub(r'\\\1{}', text.replace('\\', '$\\backslash$'))
+    text = escapingRE.sub(r'\\\1{}', text.replace('\\', '$\\backslash$'))
+    return text.replace('\n', ' ')
 
-class LatexSpitter(XMLParser):
+entities = { 'amp': '&', 'gt': '>', 'lt': '<', 'quot': '"',
+             'copy': '\\copyright'}
 
-    entities = { 'amp': '&', 'gt': '>', 'lt': '<', 'quot': '"',
-                 'copy': '\\copyright'}
+def getLatexText(node, writer, filter=lambda x:x):
+    if isinstance(node, microdom.EntityReference):
+        writer(entities.get(node.data, ''))
+        return
+    if hasattr(node, 'data'):
+        writer(filter(node.data))
+    for child in node.childNodes :
+        getLatexText(child, writer, filter)
+
+
+class LatexSpitter:
+
 
     ignoring = 0
     normalizing = 1
@@ -36,51 +49,47 @@ class LatexSpitter(XMLParser):
     raw = 0
     hyphenateCode = 0
 
-    def __init__(self, writer, currDir='.'):
+    def __init__(self, writer, currDir='.', filename=''):
         self.writer = writer
         self.currDir = currDir
-        self.spans = []
-        self.divs = []
+        self.filename = filename
 
-    def gotTagStart(self, name, attributes):
-        s = getattr(self, "mapStart_"+name, None)
-        if s and not self.raw:
-            self.writer(s)
-        m = getattr(self, "start_"+name, None)
-        m and m(name, attributes)
-
-    def gotText(self, data):
-        if self.ignoring:
+    def visitNode(self, node):
+        if not hasattr(node, 'tagName'):
+            getLatexText(node, self.writer, latexEscape)
             return
-        if self.normalizing:
-            data = data.replace('\n', ' ')
-        if self.escaping:
-            data = latexEscape(data)
-        if self.hyphenateCode:
-            olddata = data
-            data = lowerUpperRE.sub(r'\1\\textrm{\\-}\2', data)
-            if data == olddata:
-                # No hyphenation points were added, so fall back to adding
-                # hyphenation points at dots (except not for leading dots)
-                data = data[:1] + data[1:].replace('.', '.\\textrm{\\-}')
+        m = getattr(self, 'visitNode_'+node.tagName, self.visitNodeDefault)
+        m(node)
 
-        self.writer(data)
-
-    def gotEntityReference(self, entityRef):
-        self.writer(self.entities.get(entityRef, ''))
-
-    def gotTagEnd(self, name):
-        s = getattr(self, "mapEnd_"+name, None)
-        if s and not self.raw:
+    def visitNodeDefault(self, node):
+        s = getattr(self, 'mapStart_'+node.tagName, None)
+        if s:
             self.writer(s)
-        m = getattr(self, "end_"+name, None)
-        m and m(name)
+        for child in node.childNodes:
+            self.visitNode(child)
+        s = getattr(self, 'mapEnd_'+node.tagName, None)
+        if s:
+            self.writer(s)
 
-    def gotDoctype(self, dt):
-        pass
+    def visitNode_pre(self, node):
+        self.writer('\\begin{verbatim}\n')
+        getLatexText(node, self.writer, lambda x:x)
+        self.writer('\\end{verbatim}\n')
 
-    def start_img(self, _, attributes):
-        fileName = os.path.join(self.currDir, attributes['src'])
+    def visitNode_code(self, node):
+        fout = StringIO()
+        getLatexText(node, fout.write, latexEscape)
+        data = fout.getvalue()
+        olddata = data
+        data = lowerUpperRE.sub(r'\1\\textrm{\\-}\2', data)
+        if data == olddata:
+            # No hyphenation points were added, so fall back to adding
+            # hyphenation points at dots (except not for leading dots)
+            data = data[:1] + data[1:].replace('.', '.\\textrm{\\-}')
+        self.writer('\\texttt{'+data+'}')
+
+    def visitNode_img(self, node):
+        fileName = os.path.join(self.currDir, node.getAttribute('src'))
         target, ext = os.path.splitext(fileName)
         ext = ext[1:]
         m = getattr(self, 'convert_'+ext, None)
@@ -94,95 +103,69 @@ class LatexSpitter(XMLParser):
     def convert_png(self, src, target):
         os.system("pngtopnm %s | pnmtops > %s" % (src, target))
 
-    def _headerStart(self, h, attributes):
-        level = int(h[1])-2
+    def visitNodeHeader(self, node):
+        level = int(node.tagName[1])-2
         level += self.baseLevel
         self.writer('\n\n\\'+level*'sub'+'section{')
+        self.visitNodeDefault(node)
+        self.writer('}\n')
 
-    def start_h1(self, _, _1):
-        self.ignoring = 1
+    def visitNode_h1(self, node):
+        pass
 
-    def end_h1(self, _):
-        self.ignoring = 0
+    def visitNode_a(self, node):
+        if node.hasAttribute('class'):
+            if node.getAttribute('class').endswith('listing'):
+                return self.visitNode_a_listing(node)
+        if node.hasAttribute('href'):
+            return self.visitNode_a_href(node)
+        if node.hasAttribute('name'):
+            return self.visitNode_a_name(node)
+        self.visitNodeDefault(node)
 
-    def start_pre(self, _, _1):
-        self.normalizing = self.escaping = 0
-        self.raw = 1
+    def visitNode_a_listing(self, node):
+        fileName = os.path.join(self.currDir, node.getAttribute('href'))
+        self.writer('\\begin{verbatim}')
+        self.writer(open(fileName).read())
+        self.writer('\\end{verbatim}')
+        # Write a caption for this source listing
+        self.writer('\\begin{center}\\raisebox{1ex}[1ex]{Source listing for '
+                    '\\begin{em}%s\\end{em}}\\end{center}' 
+                    % latexEscape(fileName))
 
-    def end_pre(self, _):
-        self.normalizing = self.escaping = 1
-        self.raw = 0
-        self.writer(self.mapEnd_pre)
+    def visitNode_a_href(self, node):
+        self.visitNodeDefault(node)
+        href = node.getAttribute('href')
+        ref = None
+        if href.startswith('#'):
+            ref = self.filename + 'HASH' + href[1:]
+        elif href.find('#') != 1 and not href.startswith('http:'):
+            ref = href.replace('#', 'HASH')
+        elif not href.startswith('http:'):
+            ref = href
+        if ref:
+            self.writer(' (page \\pageref{%s})' % ref)
 
-    def start_code(self, _, _1):
-        self.hyphenateCode = 1
+    def visitNode_a_name(self, node):
+        self.writer('\\label{%sHASH%s}' % (self.filename,
+                                           node.getAttribute('name')))
+        self.visitNodeDefault(node)
 
-    def end_code(self, _):
-        self.hyphenateCode = 0
+    def visitNode_style(self, node):
+        pass
 
-    def start_a(self, _, attrs):
-        if attrs.get('class') == "py-listing":
-            fileName = os.path.join(self.currDir, attrs['href'])
-            self.writer('\\begin{verbatim}')
-            self.writer(open(fileName).read())
-            self.writer('\\end{verbatim}')
-            # Write a caption for this source listing
-            self.writer('\\begin{center}\\raisebox{1ex}[1ex]{Source listing for '
-                        '\\begin{em}%s\\end{em}}\\end{center}' 
-                        % latexEscape(fileName))
-            self.ignoring = 1
-        elif attrs.has_key('href'):
-            if attrs.get('class', '').find('listing') != -1:
-                # Source listings aren't cross-references.
-                return
-            href = attrs['href']
-            if href.startswith('#'):
-                self.ref = self.filename + 'HASH' + href[1:]
-            elif href.find('#') != 1 and not href.startswith('http:'):
-                self.ref = href.replace('#', 'HASH')
-            elif not href.startswith('http:'):
-                self.ref = href
-        elif attrs.has_key('name'):
-            self.writer('\\label{%sHASH%s}' % (self.filename, attrs['name']))
+    def visitNode_span(self, node):
+        if not node.hasAttribute('class'):
+            return self.visitNodeDefault(node)
+        node.tagName += node.getAttribute('class')
+        self.visitNode(node)
 
-    def end_a(self, _):
-        self.ignoring = 0
-        if hasattr(self, 'ref'):
-            self.writer(' (page \\pageref{%s})' % self.ref)
-            del self.ref
+    visitNode_div = visitNode_span
 
-    def start_style(self, _, _1):
-        self.ignoring = 1
-
-    def end_style(self, _):
-        self.ignoring = 0
-
-    def start_span(self, _, attributes):
-        class_ = attributes.get('class')
-        self.spans.append(class_)
-        if class_:
-            self.gotTagStart('span_'+class_, attributes)
-
-    def end_span(self, _):
-        class_ = self.spans.pop()
-        if class_:
-            self.gotTagEnd('span_'+class_)
-
-    def start_div(self, _, attributes):
-        class_ = attributes.get('class')
-        self.divs.append(class_)
-        if class_:
-            self.gotTagStart('div_'+class_, attributes)
-
-    def end_div(self, _):
-        class_ = self.divs.pop()
-        if class_:
-            self.gotTagEnd('div_'+class_)
-
-    start_h2 = start_h3 = start_h4 = _headerStart
+    visitNode_h2 = visitNode_3 = visitNode_h4 = visitNodeHeader
 
     mapStart_title = '\\title{'
-    mapEnd_title = mapEnd_h2 = mapEnd_h3 = mapEnd_h4 = '}\n'
+    mapEnd_title = '}\n'
 
     mapStart_html = '\\documentclass{article}\n'
     mapStart_body = '\\begin{document}\n\\maketitle\n'
@@ -203,12 +186,6 @@ class LatexSpitter(XMLParser):
 
     mapStart_p = '\n\n'
 
-    mapStart_code = '\\texttt{'
-    mapEnd_code = '}'
-
-    mapStart_pre = '\\begin{verbatim}'
-    mapEnd_pre = '\\end{verbatim}'
-
     mapStart_strong = mapStart_em = '\\begin{em}'
     mapEnd_strong = mapEnd_em = '\\end{em}'
 
@@ -225,7 +202,16 @@ class LatexSpitter(XMLParser):
 class SectionLatexSpitter(LatexSpitter):
 
     baseLevel = 1
-    mapStart_title = '\\section{'
-    def end_title(self, _):
-        self.writer('\\label{%s}\n' % self.filename)
-    mapEnd_body = mapStart_body = mapStart_html = None
+
+    def visitNode_title(self, node):
+        self.writer('\\section{')
+        self.visitNodeDefault(node)
+        self.writer('\\label{%s}}\n' % self.filename)
+
+    mapStart_title = mapEnd_title = mapEnd_body = mapStart_body = \
+    mapStart_html = None
+
+
+def processFile(spitter, fin):
+    dom = microdom.parse(fin).documentElement
+    spitter.visitNode(dom)
