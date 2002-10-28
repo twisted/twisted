fb9a821742b4a4ae500ea6688bb4d34a4fe3665b
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-28 04:44:03 +0000

    Moving hlint to a part of the twisted.lore package
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3750 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/lint.py
+++ twisted/lore/lint.py
@@ -1,4 +1,19 @@
-#!/usr/bin/python
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001-2002 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
 
 from twisted.lore import tree
 from twisted.web import domhelpers
@@ -60,14 +75,9 @@ tags = list2dict(["html", "title", "head", "body", "h1", "h2", "h3", "ol", "ul",
                   "dl", "li", "dt", "dd", "p", "code", "img", "blockquote", "a",
                   "cite", "div", "span", "strong", "em", "pre", "q", "table",
                   "tr", "td", "th", "style"])
-def main():
-    import sys
-    checker = TagChecker(tags.has_key, classes.has_key)
-    for file in sys.argv[1:]:
-        dom = tree.parseFileAndReport(file)
-        if not dom:
-            continue
-        checker.check(dom, file)
 
-if __name__ == '__main__':
-   main()
+def doFile(file, checker):
+    dom = tree.parseFileAndReport(file)
+    if not dom:
+        return
+    checker.check(dom, file)
