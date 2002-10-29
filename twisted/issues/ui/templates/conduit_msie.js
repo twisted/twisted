3eafea7fbe4da373c64839c5473b6be1325d759f
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-10-29 10:59:09 +0000

    reindent, fix to actually work
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3806 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/issues/ui/templates/conduit_msie.js
+++ twisted/issues/ui/templates/conduit_msie.js
@@ -1,50 +1,44 @@
+
 // JAVASCRIPT FOR MSIE
 
 function onkeyevent(theEvent)
-{ 	
-    if (!theEvent) {
-        theEvent = event
-    }
-	code = theEvent.keyCode;
-	if (code==13 || code==10) {
-		var inputText = document.getElementById('inputText')
-		send('None', inputText.value)
-	}
-        
+{
+  if (!theEvent) {
+    theEvent = event;
+  }
+  code = theEvent.keyCode;
+  if (code==13 || code==10) {
+    var inputText = document.getElementById('inputText');
+    send('None', inputText.value);
+  }
 }
 
 document.onkeypress = onkeyevent
 
 function send(recipient, text) {
-    if (text) {
-      document.getElementById("input").src = "?input=" + escape(text) + "&recipient=" + escape(recipient)
-        // what would be really cool is if we did it roman here and 
-        // and changed it to italics on receive :D
-        var inputText = document.getElementById("inputText")
-        inputText.value = ""
-        focusInput() // inputText.focus()
-    }
+  if (text) {
+    document.getElementById("input").src = "?input=" + escape(text) + "&recipient=" + escape(recipient);
+    // what would be really cool is if we did it roman here and
+      // and changed it to italics on receive :D
+        var inputText = document.getElementById("inputText");
+    inputText.value = "";
+    focusInput(); // inputText.focus()
+  }
 }
-//function scrollTextFrame()
-//{
-//        document.getElementById("initialtextFrame").contentDocument.defaultView.scroll(0, 50000);
-//}
 
 function recv(stuff) {
-    var output = document.getElementById("content")
-// Works on ie mac and mozilla but not as well on ie win
-    output.document.documentElement.appendChild(output.document.createTextNode(unescape(stuff)))
-//    output.appendChild(document.createElement("br"))
-//    output.contentDocument.defaultView.scrollBy(0, 5000)
-// Works on ie win & mac, and mozilla, but is a bit slower
-    alert(stuff)
-//    output.document.innerHTML = output.document.documentElement.innerHTML + unescape(stuff) + '<br \>'
-    output.contentWindow.scrollBy(0, output.innerHeight)
+  var output = document.getElementById("content");
+
+  var fc = frames.content;
+  var fcd = fc.document;
+  var fcdb = fcd.body;
+  fcdb.insertAdjacentHTML("beforeEnd", stuff + "<br/>");
+  output.contentWindow.scrollBy(0, 5000); //output.innerHeight
 }
 
 function focusInput() {
-    foo = document.getElementById('inputText')
-    foo.focus()
-    foo.onkeypress = onkeyevent
+  foo = document.getElementById('inputText');
+  foo.focus();
+  foo.onkeypress = onkeyevent;
 }
 
