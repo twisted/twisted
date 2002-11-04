6b258338d18a3417c574b169a413b4ab01fd84ef
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-04 06:22:22 +0000

    by default, stop twisted event loop when tkinter window is closed
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@3913 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/tksupport.py
+++ twisted/internet/tksupport.py
@@ -26,9 +26,6 @@ To use, do::
 
 and then run your reactor as usual - do *not* call Tk's mainloop(),
 use Twisted's regular mechanism for running the event loop.
-
-Note that the Twisted event loop will not stop when the root widget
-is destroyed - you'll need to stop it yourself.
 """
 
 # system imports
@@ -47,10 +44,13 @@ def _guiUpdate(reactor, widget, delay):
     reactor.callLater(delay, _guiUpdate, reactor, widget, delay)
 
 
-def install(widget, ms=10, reactor=None):
+def install(widget, ms=10, doShutdown=1, reactor=None):
     """Install a Tkinter.Tk() object into the reactor."""
     if reactor is None:
         from twisted.internet import reactor
+    if doShutdown:
+        # close twisted on tkinter root destruction
+        widget.protocol("WM_DELETE_WINDOW", reactor.stop)
     _guiUpdate(reactor, widget, ms/1000.0)
 
 
