049cba07cd54ffc3a8d534eef492755ba10cf968
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-09 07:39:42 +0000

    Make dispathing events explicit, support multiple callbacks per event
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4021 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/dispatch.py
+++ twisted/python/dispatch.py
@@ -15,72 +15,25 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-## The following comment is from dispatch.py's original home, Original Gamer.
-## http://sf.net/projects/originalgamer
-
-## Justification for following evilness::
-#
-# At first I simply had the EventDispatcher class implement a bunch of callback
-# methods that explicitly called the object that the event needed to get to,
-# but this was still a lot of needless code. (wrapper methods are teh sux0r)
-#
-# So I tried to just do a lot of `self.foo = obj.bar' in `setWhateverObject'
-# methods of EventDispatcher that WhateverObjects (i.e., ClientFrame,
-# NetworkText) would call on initialization, but this had problems with
-# objects trying to grab references to a callback method before the callback
-# had been bound to the EventDispatcher.
-#
-# Thus, the next logical step is to create Handler, The Method That Wasn't.
-# Now WhateverObjects simply call registerMethod on the dispatcher object,
-# with an event-name to respond to and their callback method. Allowing
-# disparate objects to talk to each other through a well-defined interface
-# shouldn't be such a PITA any more.
-#
-# -- radix, feeling guilty for writing __getattr__/__call__ hacks
-
-
-class Handler:
-    """
-    I'm a method that doesn't exist yet.
-    """
-    def __init__(self, name, method=None):
-        self.name = name
-
-    def __call__(self, *args, **kwargs):
-        raise RuntimeError("Eep! A method hasn't been registered for the "
-                           "%s event yet." % self.name)
-
-
-
 class EventDispatcher:
     """
-    A global event dispatcher for events in OGC.
+    A global event dispatcher for events.
     I'm used for any events that need to span disparate objects in the client.
 
     I should only be used when one object needs to signal an object that it's
     not got a direct reference to (unless you really want to pass it through
     here, in which case I won't mind).
+
+    I'm mainly useful for complex GUIs.
     """
 
-
     def __init__(self):
-        self.notRegisteredYet = {}
+        self.callbacks = {}
 
 
     def registerHandler(self, name, meth):
-        # If we've given out any Handlers yet, we need to tell them
-        # about the new method.
-        if hasattr(self, name):
-            import warnings
-            warnings.warn("Honk! Already registered method %s for event %s, "
-                          "but continuing to replace it with %s" %
-                          (getattr(self, name), name, meth))
-        if self.notRegisteredYet.has_key(name):
-            for x in self.notRegisteredYet[name]:
-                x.__call__ = meth
-            del self.notRegisteredYet[name]
-
-        setattr(self, name, meth)
+        self.callbacks.setdefault(name, []).append(meth)
+            
 
     def autoRegister(self, obj):
         from twisted.python import reflect
@@ -90,10 +43,7 @@ class EventDispatcher:
             self.registerHandler(k, v)
 
 
-    def __getattr__(self, name):
-        # if no one's registered a callback for this name, we'll give out
-        # a Handler and make sure to remember it so registerHandler
-        # can update it later.
-        h = Handler(name)
-        self.notRegisteredYet.setdefault(name, []).append(h)
-        return h
+    def publishEvent(self, name, *args, **kwargs):
+        for cb in self.callbacks[name]:
+            cb(*args, **kwargs)
+    
