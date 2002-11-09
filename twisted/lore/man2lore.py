8f2794e6949d3888cfa1a2235fce5a874d935739
spiv <spiv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-09 15:13:13 +0000

    We love you, glyph.  Have some copyright.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4038 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/man2lore.py
+++ twisted/lore/man2lore.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001-2002 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 """man2lore: Converts man page source (i.e. groff) into lore-compatible html.
 
 This is nasty and hackish (and doesn't support lots of real groff), but is good
