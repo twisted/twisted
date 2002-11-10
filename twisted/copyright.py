2a09b0ca98346989954c0de2371794475fe2f57d
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-10 13:37:28 +0000

    Preparing for 1.0.1alpha2
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4073 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/copyright.py
+++ twisted/copyright.py
@@ -21,7 +21,7 @@ Copyright information for Twisted.
 """
 
 
-version="1.0.1alpha1"
+version="1.0.1alpha2"
 longversion="Twisted %s" % version
 copyright="Copyright (c) 2000-2002 Matthew William Lefkowitz, all rights reserved."
 
