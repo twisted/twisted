85cafdf804bad2f945ea1eeaef032afb43bf67ce
washort <washort@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-11 08:52:31 +0000

    LITTLE GIRL I CAN AFFORD TO RIDE IN THE FRONT!!!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4091 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/woven/controller.py
+++ twisted/web/woven/controller.py
@@ -15,6 +15,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from __future__ import nested_scopes
+
 from twisted.python import log
 from twisted.python import components
 from twisted.web import resource
