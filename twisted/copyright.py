2007225c5c842de8a7ab4558951cafd25e34168e
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-12 13:24:55 +0000

    Preparing for 1.0.1alpha6
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4113 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/copyright.py
+++ twisted/copyright.py
@@ -21,7 +21,7 @@ Copyright information for Twisted.
 """
 
 
-version="1.0.1alpha5"
+version="1.0.1alpha6"
 longversion="Twisted %s" % version
 copyright="Copyright (c) 2000-2002 Matthew William Lefkowitz, all rights reserved."
 
