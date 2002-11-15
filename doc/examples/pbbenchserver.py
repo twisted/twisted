8263836053f52e05eb8e69016685c5b23c532f47
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-15 20:38:30 +0000

    fixes for deprecation warnings
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4144 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/pbbenchserver.py
+++ doc/examples/pbbenchserver.py
@@ -1,6 +1,24 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001, 2002 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Server for PB benchmark."""
 
 from twisted.spread import pb
 from twisted.internet import reactor, app
+from twisted.cred import authorizer
 
 class PBBenchPerspective(pb.Perspective):
     callsPerSec = 0
@@ -20,8 +38,9 @@ class PBBenchService(pb.Service):
     perspectiveClass = PBBenchPerspective
 
 a = app.Application("pbbench")
-a.listenTCP(8787, pb.BrokerFactory(pb.AuthRoot(a)))
-b = PBBenchService("benchmark", a)
+auth = authorizer.DefaultAuthorizer(a)
+a.listenTCP(8787, pb.BrokerFactory(pb.AuthRoot(auth)))
+b = PBBenchService("benchmark", a, authorizer=auth)
 u = b.createPerspective("benchmark")
 u.makeIdentity("benchmark")
 u.printCallsPerSec()
