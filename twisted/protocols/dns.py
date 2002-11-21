0dd046758d8b4129534d8d2f03010f0b5258c872
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-21 18:03:27 +0000

    typo
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4225 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/dns.py
+++ twisted/protocols/dns.py
@@ -37,7 +37,7 @@ PORT = 53
 QUERY_TYPES = {
     1:  'A',     2:  'NS',    3:  'MD',   4:   'MF',
     5:  'CNAME', 6:  'SOA',   7:  'MB',   8:   'MG',
-    9:  'MR',    10: 'NULL',  11: 'MKS',  12:  'PTR',
+    9:  'MR',    10: 'NULL',  11: 'WKS',  12:  'PTR',
     13: 'HINFO', 14: 'MINFO', 15: 'MX',   16: 'TXT',
     
     252: 'AFRX', 253: 'MAILB', 254: 'MAILA', 255: 'ALL_RECORDS'
