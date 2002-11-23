b68a47d4ae2d1f79e082b0726e4ac86fcde97f84
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-23 20:54:39 +0000

    The beginning of a client module for web
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4249 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/web/client.py
@@ -0,0 +1,81 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001-2002 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+from twisted.protocols import http
+from twisted.internet import defer, protocol, reactor
+import urlparse
+
+class HTTPPageGetter(http.HTTPClient):
+
+    def connectionMade(self):
+        self.sendCommand('GET', self.factory.url)
+        self.sendHeader('Host', self.factory.host)
+        self.sendHeader('User-Agent', self.factory.agent)
+        self.endHeaders()
+
+    def handleResponse(self, response):
+        self.factory.page(response)
+
+
+class HTTPClientFactory(protocol.ClientFactory):
+
+    protocol = HTTPPageGetter
+
+    def __init__(self, host, url, agent="Twisted PageGetter"):
+        self.host=host
+        self.url=url
+        self.agent=agent
+        self.waiting = 1
+        self.deferred = defer.Deferred()
+
+    def page(self, page):
+        self.waiting = 0
+        self.deferred.callback(page)
+
+    def clientConnectionFailed(self, _, reason):
+        if self.waiting:
+            self.deferred.errback(reason)
+
+    clientConnectionLost = clientConnectionFailed 
+
+
+def getPage(url):
+    '''download a web page
+
+    Download a page. Return a deferred, which will
+    callback with a page or errback with a description
+    of the error.
+    '''
+    parsed = urlparse.urlparse(url)
+    url = urlparse.urlunparse(('','')+parsed[2:])
+    host, port = parsed[1], 80
+    if ':' in host:
+        host, port = host.split(':')
+        port = int(port)
+    factory = HTTPClientFactory(host, url)
+    reactor.connectTCP(host, port, factory)
+    return factory.deferred
+
+if __name__ == '__main__':
+    d = getPage('http://moshez.org/links.html')
+    def printValue(value):
+        print value
+        reactor.stop()
+    def printError(error):
+        print "an error occured"
+        reactor.stop()
+    d.addCallbacks(callback=printValue, errback=printError)
+    reactor.run()
