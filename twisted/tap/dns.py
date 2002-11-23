51c2a2193df6933a2514b98eb9852f99e468e4de
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-23 00:16:11 +0000

    don't forget this
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4234 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/tap/dns.py
@@ -0,0 +1,63 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""
+Domain Name Server
+"""
+
+import os
+
+from twisted.python import usage
+from twisted.names import server
+from twisted.protocols import dns
+
+import traceback
+
+class Options(usage.Options):
+    zonefiles = None
+    
+    def __init__(self):
+        usage.Options.__init__(self)
+        self.zonefiles = []
+    
+    
+    def opt_zonefile(self, filename):
+        if not os.path.exists(filename):
+            raise usage.UsageError(filename + ": No such file")
+        self.zonefiles.append(filename)
+    
+    
+    def postOptions(self):
+        zones = []
+        if not len(self.zonefiles):
+            raise usage.UsageError("At least one zone file must be specified")
+        for f in self.zonefiles:
+            try:
+                zones.append(server.Authority(f))
+            except Exception, e:
+                traceback.print_exc()
+                raise usage.UsageError(e)
+        if not len(zones):
+            raise usage.UsageError("At least one zone file must contain a valid zone")
+        self.zones = zones
+
+
+def updateApplication(app, config):
+    f = server.DNSServerFactory(config.zones)
+    p = dns.DNSClientProtocol(f)
+    app.listenUDP(dns.PORT, p)
+    app.listenTCP(dns.PORT, f)
+    
