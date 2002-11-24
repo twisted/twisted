e74df0f2bf6b348844bd404cc3d830eb2a5ee97a
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-11-24 01:25:42 +0000

    format foo
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4252 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/dns.py
+++ twisted/protocols/dns.py
@@ -40,9 +40,10 @@ QUERY_TYPES = {
     1:  'A',     2:  'NS',    3:  'MD',   4:   'MF',
     5:  'CNAME', 6:  'SOA',   7:  'MB',   8:   'MG',
     9:  'MR',    10: 'NULL',  11: 'WKS',  12:  'PTR',
-    13: 'HINFO', 14: 'MINFO', 15: 'MX',   16: 'TXT',
+    13: 'HINFO', 14: 'MINFO', 15: 'MX',   16:  'TXT',
     
-    252: 'AFRX', 253: 'MAILB', 254: 'MAILA', 255: 'ALL_RECORDS'
+    252: 'AFRX',  253: 'MAILB',
+    254: 'MAILA', 255: 'ALL_RECORDS'
 }
 REV_TYPES = dict([
     (v, k) for (k, v) in QUERY_TYPES.items()
