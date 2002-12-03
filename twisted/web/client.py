1252555f74be5f2b6673d1b1c15edf2a10577f45
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-03 09:51:58 +0000

    Downloader for pages we don't wnat in memory
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4340 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/client.py
+++ twisted/web/client.py
@@ -39,6 +39,18 @@ class HTTPPageGetter(http.HTTPClient):
         self.factory.page(response)
 
 
+class HTTPPageDownloader(HTTPPageGetter):
+
+    def handleEndHeaders(self):
+        self.factory.pageStart()
+
+    def handleResponsePart(self, data):
+        self.factory.pagePart(data)
+
+    def handleResponseEnd(self):
+        self.factory.pageEnd()
+
+
 class HTTPClientFactory(protocol.ClientFactory):
 
     protocol = HTTPPageGetter
@@ -65,6 +77,42 @@ class HTTPClientFactory(protocol.ClientFactory):
             self.waiting = 0
             self.deferred.errback(reason)
 
+class HTTPDownloader(HTTPClientFactory):
+
+    protocol = HTTPPageDownloader
+
+    def __init__(self, host, url, fileName, agent="Twisted client"):
+        self.host = host
+        self.url = url
+        self.fileName = fileName
+        self.agent = agent
+        self.deferred = defer.Deferred()
+        self.waiting = 1
+        self.file = None
+
+    def pageStart(self):
+        if self.waiting:
+            self.waiting = 0
+            self.file = open(self.fileName, 'w')
+
+    def pageEnd(self):
+        if self.file:
+            self.file.close()
+        self.deferred.callback(None)
+
+    def pagePart(self, data):
+        if self.file:
+            self.file.write(data)
+
+    
+def _parse(url):
+    parsed = urlparse.urlparse(url)
+    url = urlparse.urlunparse(('','')+parsed[2:])
+    host, port = parsed[1], 80
+    if ':' in host:
+        host, port = host.split(':')
+        port = int(port)
+    return host, port, url
 
 def getPage(url):
     '''download a web page
@@ -73,13 +121,14 @@ def getPage(url):
     callback with a page or errback with a description
     of the error.
     '''
-    parsed = urlparse.urlparse(url)
-    url = urlparse.urlunparse(('','')+parsed[2:])
-    host, port = parsed[1], 80
-    if ':' in host:
-        host, port = host.split(':')
-        port = int(port)
+    host, port, url = _parse(url)
     factory = HTTPClientFactory(host, url)
+    reactor.connectTCP(host, port, factory)
+    return factory.deferred
+
+def downloadPage(url, file):
+    host, port, url = _parse(url)
+    factory = HTTPDownloader(host, url, file)
     reactor.connectTCP(host, port, factory)
     return factory.deferred
 
