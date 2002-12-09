e6dbeaf3d4e3ce05aface4149af007d9573bbb89
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-09 04:21:11 +0000

    be an IResolver
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4394 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/resolve.py
+++ twisted/names/resolve.py
@@ -25,18 +25,20 @@ Future plans: Other types of queries
 @author: U{Jp Calderone<mailto:exarkun@twistedmatrix.com}
 """
 
-from twisted.internet import defer
+from twisted.internet import defer, interfaces
 
 class ResolverChain:
     """Lookup an address using multiple C{IResolver}s
     """
+    
+    __implements__ = (interfaces.IResolver,)
 
     def __init__(self, resolvers):
         self.resolvers = resolvers
         self.resolvers.reverse()
 
 
-    def resolve(self, name, timeout=10):
+    def lookupAddress(self, name, timeout=10):
         d = r.lookupAddress(name, timeout)
         for r in self.resolvers:
             d = d.addErrback(r.lookupAddress, name, timeout)
