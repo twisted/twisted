9bca935860bfe3344224513e2c4506ba93abe13b
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-15 18:14:14 +0000

    Remove icky stuff
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4521 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/dlpage.py
+++ doc/examples/dlpage.py
@@ -1,13 +1,12 @@
-if __name__ == '__main__':
-    from twisted.internet import reactor
-    from twisted.web.client import downloadPage
-    import sys
-    d = downloadPage(sys.argv[1], "foo")
-    def printValue(value):
-        print "done"
-        reactor.stop()
-    def printError(error):
-        print "an error occured", error
-        reactor.stop()
-    d.addCallbacks(callback=printValue, errback=printError)
-    reactor.run()
+from twisted.internet import reactor
+from twisted.web.client import downloadPage
+import sys
+d = downloadPage(sys.argv[1], "foo")
+def printValue(value):
+    print "done"
+    reactor.stop()
+def printError(error):
+    print "an error occured", error
+    reactor.stop()
+d.addCallbacks(callback=printValue, errback=printError)
+reactor.run()
--- doc/examples/getpage.py
+++ doc/examples/getpage.py
@@ -1,13 +1,12 @@
-if __name__ == '__main__':
-    from twisted.internet import reactor
-    from twisted.web.client import getPage
-    import sys
-    d = getPage(sys.argv[1])
-    def printValue(value):
-        print value
-        reactor.stop()
-    def printError(error):
-        print "an error occured", error
-        reactor.stop()
-    d.addCallbacks(callback=printValue, errback=printError)
-    reactor.run()
+from twisted.internet import reactor
+from twisted.web.client import getPage
+import sys
+d = getPage(sys.argv[1])
+def printValue(value):
+    print value
+    reactor.stop()
+def printError(error):
+    print "an error occured", error
+    reactor.stop()
+d.addCallbacks(callback=printValue, errback=printError)
+reactor.run()
