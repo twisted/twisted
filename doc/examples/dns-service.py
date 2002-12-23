137276af7d6fcf7ad51557c439fa0f0efb5725b3
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-23 22:22:53 +0000

    DNS service location w/ new API
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4614 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/examples/dns-service.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+"""Sample app to lookup SRV records in DNS."""
+
+from twisted.names import client
+from twisted.internet import reactor
+import sys
+
+def printAnswer(answers):
+    if not len(answers):
+        print 'No answers'
+    else:
+        print '\n'.join(map(str, answers))
+    reactor.stop()
+
+def printFailure(arg):
+    print "error: could not resolve:", arg
+    reactor.stop()
+
+try:
+    service, proto, domain = sys.argv[1:]
+except ValueError:
+    sys.stderr.write('%s: usage:\n' % sys.argv[0] +
+                     '  %s SERVICE PROTO DOMAIN\n' % sys.argv[0])
+    sys.exit(1)
+
+resolver = client.Resolver('/etc/resolv.conf')
+d = resolver.lookupService('_%s._%s.%s' % (service, proto, domain), 1)
+d.addCallbacks(printAnswer, printFailure)
+
+reactor.run()
