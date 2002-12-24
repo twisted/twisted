1dd8ca15d932e8b48c227e2643dcfce5ddac0b30
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-24 07:24:39 +0000

    Adding an example of adding advogato diaries via XML-RPC See http://www.advogato.org/person/moshez/ for a proof
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4628 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/examples/advogato.py
@@ -0,0 +1,35 @@
+from twisted.web.xmlrpc import Proxy
+from twisted.internet import reactor
+from getpass import getpass
+import sys
+
+class AddDiary:
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        self.proxy = Proxy('http://advogato.org/XMLRPC')
+
+    def __call__(self, filename):
+        self.data = open(filename).read()
+        d = self.proxy.callMethod('authenticate', self.name, self.password)
+        d.addCallbacks(self.login, self.noLogin)
+
+    def noLogin(self, reason):
+        print "could not login"
+        reactor.stop()
+
+    def login(self, cookie):
+        d = self.proxy.callMethod('diary.set', cookie, -1, self.data)
+        d.addCallbacks(self.setDiary, self.errorSetDiary)
+
+    def setDiary(self, response):
+        reactor.stop()
+
+    def errorSetDiary(self, error):
+        print "could not set diary", error
+        reactor.stop()
+
+diary = AddDiary(sys.argv[1], getpass())
+diary(sys.argv[2])
+reactor.run()
