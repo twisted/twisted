6bd13e36faec340515e530ea7e2a58b25ca8eb7a
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-24 06:52:28 +0000

    Hrm, remove examples which only work when running your own server
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4626 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/xmlrpcclient.py
+++ doc/examples/xmlrpcclient.py
@@ -9,13 +9,6 @@ def printError(error):
     print 'error', error
     reactor.stop()
 
-proxy = Proxy('http://localhost:10999/')
-proxy.callMethod('echo', 41).addCallbacks(printValue, printError)
-reactor.run()
-proxy.callMethod('defer').addCallbacks(printValue, printError)
-reactor.run()
-proxy.callMethod('fail').addCallbacks(printValue, printError)
-reactor.run()
 proxy = Proxy('http://advogato.org/XMLRPC')
 proxy.callMethod('test.sumprod', 3, 5).addCallbacks(printValue, printError)
 reactor.run()
