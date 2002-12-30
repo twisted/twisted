74dc222bdd49e5b0799ee1e7df83ebb331b6c9ac
z3p <z3p@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2002-12-30 21:38:47 +0000

    just for you Pahan, conch now prints 'Connection to <host> lost.'
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4720 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/conch.py
+++ twisted/scripts/conch.py
@@ -14,7 +14,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: conch.py,v 1.29 2002/12/27 23:56:00 z3p Exp $
+# $Id: conch.py,v 1.30 2002/12/30 21:38:47 z3p Exp $
 
 #""" Implementation module for the `conch` command.
 #"""
@@ -149,6 +149,7 @@ def run():
     finally:
         if old:
             tty.tcsetattr(fd, tty.TCSADRAIN, old)
+    print 'Connection to %s closed.' % options['host']
     sys.exit(exitStatus)
 
 def handleError():
