10ad2571bd726284d604225e49c1d62fe3f30110
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-04 00:47:01 +0000

    POKEY IT IS A TEST
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4798 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/woven/model.py
+++ twisted/web/woven/model.py
@@ -15,6 +15,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+__version__ = "$Revision: 1.30 $"[11:-2]
+
 import types
 import weakref
 import warnings
