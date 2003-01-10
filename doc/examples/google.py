acd4be5a7ca8e30f5de6bd0ef8f10dfa1198e0a7
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-10 10:14:27 +0000

    Change callback style a litt.e
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4975 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/google.py
+++ doc/examples/google.py
@@ -1,13 +1,14 @@
 from twisted.web import google
 from twisted.internet import reactor 
-
 import sys
+
 def printValue(location):
      print location
      reactor.stop()
+
 def printError(error):
      print 'error', error
      reactor.stop()
-d = google.checkGoogle(sys.argv[1:])
-d.addCallbacks(printValue, printError)
+
+google.checkGoogle(sys.argv[1:]).addCallbacks(printValue, printError)
 reactor.run()
