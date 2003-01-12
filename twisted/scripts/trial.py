3c87b1c8de0f17505889c9ff7a3c55b0a6a7892c
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-12 03:11:42 +0000

    Removed outdated comment.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@4991 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/trial.py
+++ twisted/scripts/trial.py
@@ -16,9 +16,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-# FIXME
-# - Hangs.
-
 from twisted.python import usage
 from twisted.trial import unittest
 import sys, os
