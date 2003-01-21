2cdd7b9c2346d40045770133374ef6a4bd4c6968
moonfallen <moonfallen@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-21 20:35:28 +0000

    compensate for win32 directory reorg
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5128 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_postinstall.py
+++ twisted/test/test_postinstall.py
@@ -2,23 +2,31 @@
 """
 
 from twisted.trial import unittest
+import sys
 
 import os
 
 if os.name == 'nt':
-    from twisted.scripts import postinstall
+    sys.path.insert(0, "win32")
+    import twisted_postinstall
+    sys.path.remove("win32")
     import os.path
 
     def noop(name):
         pass
 
-    postinstall.directory_created=postinstall.file_created=noop
+    twisted_postinstall.directory_created=twisted_postinstall.file_created=noop
 
     class PostinstallTest(unittest.TestCase):
         def testInstall(self):
-            files=postinstall.install()
+            stdout_save=sys.stdout
+            output=open("install.stdout.txt", 'w')
+            sys.stdout=output
+            files=twisted_postinstall.install()
+            sys.stdout=stdout_save
+            output.close()
             for file in files:
                 assert os.path.exists(file)
 
         def testRemove(self):
-            assert postinstall.remove() is None
+            assert twisted_postinstall.remove() is None
