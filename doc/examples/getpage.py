f254c385ec0ed5f86f21c5dfeb0ee2dbb9d828c2
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-26 07:18:29 +0000

    Use println
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5170 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/getpage.py
+++ doc/examples/getpage.py
@@ -1,12 +1,9 @@
 from twisted.internet import reactor
 from twisted.web.client import getPage
+from twisted.python.util import println
 import sys
-d = getPage(sys.argv[1])
-def printValue(value):
-    print value
-    reactor.stop()
-def printError(error):
-    print "an error occured", error
-    reactor.stop()
-d.addCallbacks(callback=printValue, errback=printError)
+
+getPage(sys.argv[1]).addCallbacks(
+    callback=lambda value:(println(value),reactor.stop()),
+    errback=lambda error:(println("an error occured", error),reactor.stop()))
 reactor.run()
--- doc/examples/spider.py
+++ doc/examples/spider.py
@@ -1,15 +1,11 @@
 from twisted.web import spider
 from twisted.internet import app
-
-def start(uri):
-    print "starting", uri
-def stop(uri):
-    print "stopping", uri
+from twisted.python.util import println
 
 a = app.Application("spider")
 s = spider.SpiderSender("spider", a)
 s.addTargets(['http://twistedmatrix.com/'])
 s.maxDepth = 1
-s.notifyDownloadStart = start
-s.notifyDownloadEnd = stop
+s.notifyDownloadStart = lambda uri: println('starting', uri)
+s.notifyDownloadEnd = lambda uri: println('stopping', uri)
 a.run(save=0)
