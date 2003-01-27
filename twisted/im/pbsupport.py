8381e36ce777d2b374ad3b92cf9f93f988d2239e
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-27 09:47:22 +0000

    nested_scopes
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5196 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/im/pbsupport.py
+++ twisted/im/pbsupport.py
@@ -14,6 +14,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from __future__ import nested_scopes
 
 from twisted.internet import defer
 from twisted.python import log
