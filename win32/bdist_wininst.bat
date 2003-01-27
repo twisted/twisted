d2d361f2e754648206ae981ee1385f47dc408191
moonfallen <moonfallen@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-27 16:38:19 +0000

    optionally upload to sf at the end
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5201 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- win32/bdist_wininst.bat
+++ win32/bdist_wininst.bat
@@ -1,3 +1,7 @@
 attrib /s -r
+python setup.py clean --all
 python setup.py build --compiler=mingw32
 python setup.py bdist_wininst --install-script=twisted_postinstall.py
+@echo Press ^C to skip uploading
+pause
+scp dist/Twisted-*.exe shell.sf.net:/home/groups/t/tw/twisted/htdocs/
