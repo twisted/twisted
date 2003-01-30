a6d4055d97c09f8c53fdfa8f49ffc15e6d494ec6
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-30 06:55:13 +0000

    Check everything is installed. Might be platform-specific -- please try this on other platforms
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5232 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/test/test_setup.py
@@ -0,0 +1,43 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import os
+import setup
+from twisted.trial import unittest
+import twisted
+
+class CheckingPackagesTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.curDir = os.getcwd()
+
+    def tearDown(self):
+        os.chdir(self.curDir)
+
+    def testListen(self):
+        l = []
+        os.chdir(os.path.dirname(setup.__file__))
+        os.path.walk('twisted', 
+                     lambda l,d,n:not d.endswith('CVS') and l.append(d),
+                     l)
+        l = [x.replace('/', '.') for x in l]
+        p = setup.setup_args['packages']
+
+        for package in p:
+            l.remove(package)
+        # special treatment of cReactor
+        l.remove('twisted.internet.cReactor')
+        self.failUnlessEqual(l, [])
