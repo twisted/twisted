91a8071f5b37bf0692842e04b2dbd00360a43a9a
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-01-30 21:23:55 +0000

    update to use new names, argument order, etc
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5241 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/ipv6.py
+++ sandbox/ipv6.py
@@ -11,9 +11,8 @@ def isIPv6Address(ip):
         return 0
     return 1
 
-class IPv6Client(tcp.TCPClient):
-    def createInternetSocket(self):
-        return socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+class Client(tcp.Client):
+    addressFamily = socket.AF_INET6
 
     def resolveAddress(self):
         if isIPv6Address(self.addr[0]):
@@ -30,31 +29,25 @@ class IPv6Client(tcp.TCPClient):
         return ('INET6',) + self.socket.getsockname()
 
 
-class IPv6Connector(default.TCPConnector):
+class Connector(tcp.Connector):
     def _makeTransport(self):
-        return IPv6Client(self.host, self.port, self.bindAddress, self, self.reactor)
+        return Client(self.host, self.port, self.bindAddress, self, self.reactor)
     
     def getDestination(self):
         return ('INET6', self.host, self.port)
 
-class IPv6Server(tcp.Server):
+class Server(tcp.Server):
     def getHost(self):
         return ('INET6',) + self.socket.getsockname()
 
     def getPeer(self):
-        if isinstance(self.client, tuple):
-            return ('INET6',) + self.client
-        else:
-            return ('INET6', self.client)
+        return ('INET6',) + self.client
 
-class IPv6Port(tcp.Port):
-    transport = IPv6Server
+class Port(tcp.Port):
+    addressFamily = socket.AF_INET6
 
-    def createInternetSocket(self):
-        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s
-    
+    transport = Server
+
     def getHost(self):
         return ('INET6',) + self.socket.getsockname()
     
@@ -65,13 +58,13 @@ def connectTCP6(host, port, factory, timeout=30, bindAddress=None, reactor=None)
     if reactor is None:
         from twisted.internet import reactor
     
-    c = IPv6Connector(reactor, host, port, factory, timeout, bindAddress)
+    c = Connector(host, port, factory, timeout, bindAddress, reactor)
     c.connect()
     return c
 
 
 def listenTCP6(port, factory, backlog=5, interface='::'):
-    p = IPv6Port(port, factory, backlog, interface)
+    p = Port(port, factory, backlog, interface)
     p.startListening()
     return p
 
