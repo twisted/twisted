44e0dd4a6acd9c0c4e6da9da4975ee6ab7713744
acapnotic <acapnotic@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-02-06 20:10:31 +0000

    gtk2 manhole is coming along
    Output window and log-in are working fairly well,
    but the input stuff isn't ported yet.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5307 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/manhole/ui/gtk2manhole.py
+++ twisted/manhole/ui/gtk2manhole.py
@@ -1,17 +1,28 @@
 # -*- Python -*-
 
-__version__ = '$Revision: 1.2 $'[11:-2]
+"""Manhole client with a GTK v2.x front-end.
+"""
+# Note: Because GTK 2.x Python bindings are only available for Python 2.2,
+# this code may use Python 2.2-isms.
 
+__version__ = '$Revision: 1.3 $'[11:-2]
+
+from twisted import copyright
+from twisted.internet import reactor
 from twisted.python import components, failure, util
 from twisted.spread import pb
 from twisted.spread.ui import gtk2util
 
 from twisted.manhole.service import IManholeClient
 
+# The pygtk.require for version 2.0 has already been done by the reactor.
 import gtk
+
+import types
 
 # TODO:
 #  Make wrap-mode a run-time option.
+
 
 class ManholeWindow(components.Componentized, gtk2util.GladeKeeper):
     gladefile = util.sibpath(__file__, "gtk2manhole.glade")
@@ -48,9 +59,30 @@ class ManholeWindow(components.Componentized, gtk2util.GladeKeeper):
         self.output.append("Login FAILED %s\n" % (reason.value,), "exception")
         return None
 
+    def _on_aboutMenuItem_activate(self, widget, *unused):
+        from os import path
+        self.output.append("""\
+a Twisted Manhole client
+  Versions:
+    %(twistedVer)s
+    GTK %(gtkVer)s / PyGTK %(pygtkVer)s
+    %(module)s %(modVer)s
+http://twistedmatrix.com/
+""" % {'twistedVer': copyright.longversion,
+       'gtkVer': ".".join(map(str, gtk.gtk_version)),
+       'pygtkVer': ".".join(map(str, gtk.pygtk_version)),
+       'module': path.basename(__file__),
+       'modVer': __version__,
+       }, "local")
+
     def _on_openMenuItem_activate(self, widget, userdata=None):
-        self.output.append("You said open!\n")
         self.login()
+
+    def _on_manholeWindow_delete_event(self, widget, *unused):
+        reactor.stop()
+
+    def _on_quitMenuItem_activate(self, widget, *unused):
+        reactor.stop()
 
 tagdefs = {
     'default': {"family": "monospace"},
@@ -63,6 +95,7 @@ tagdefs = {
 
 
 class ConsoleOutput:
+    _willScroll = None
     def __init__(self, textView):
         self.textView = textView
         self.buffer = textView.get_buffer()
@@ -70,7 +103,7 @@ class ConsoleOutput:
         # TODO: Make this a singleton tag table.
         for name, props in tagdefs.iteritems():
             tag = self.buffer.create_tag(name)
-            # This can be done in the constructor in newer pygtk.
+            # This can be done in the constructor in newer pygtk (post 1.99.14)
             for k, v in props.iteritems():
                 tag.set_property(k, v)
 
@@ -86,24 +119,30 @@ class ConsoleOutput:
         self.buffer.insert_with_tags_by_name(self.buffer.get_end_iter(),
                                              text, *tags)
         # Silly things, the TextView needs to update itself before it knows
-        # where the bottom is.  This probably is hell on many-line updates.
-        while gtk.events_pending():
-            gtk.main_iteration_do(False)
+        # where the bottom is.
+        if self._willScroll is None:
+            self._willScroll = gtk.idle_add(self._scrollDown)
 
+    def _scrollDown(self, *unused):
         self.textView.scroll_to_iter(self.buffer.get_end_iter(), 0,
                                      True, 1.0, 1.0)
+        self._willScroll = None
+        return False
+
 
 class ConsoleInput:
     def __init__(self, textView):
         pass
 
-class Notafile:
+class _Notafile:
+    """Curry to make failure.printTraceback work with the output widget."""
     def __init__(self, output, kind):
         self.output = output
         self.kind = kind
 
     def write(self, txt):
         self.output.append(txt, self.kind)
+
 
 class ManholeClient(components.Adapter, pb.Referenceable):
     __implements__ = (IManholeClient,)
@@ -118,11 +157,11 @@ class ManholeClient(components.Adapter, pb.Referenceable):
 
     def remote_console(self, messages):
         for kind, content in messages:
-            if isinstance(content, (str, unicode)):
+            if isinstance(content, types.StringTypes):
                 self.original.output.append(content, kind)
             elif (kind == "exception") and isinstance(content, failure.Failure):
-                content.printTraceback(Notafile(self.original.output,
-                                                "exception"))
+                content.printTraceback(_Notafile(self.original.output,
+                                                 "exception"))
             else:
                 self.original.output.append(str(content), kind)
 
