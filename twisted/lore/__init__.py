f1eade9976077650a2ac859cfc3fbebb2f0936a3
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-02-09 10:32:18 +0000

    More TODO items
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5371 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/__init__.py
+++ twisted/lore/__init__.py
@@ -27,3 +27,5 @@ Maintainer: U{Andrew Bennetts<spiv@twistedmatrix.com>}
 # Allow non-web image formats (EPS, specifically)
 # Allow pickle output and input to minimize parses
 # If the input name is not a valid tap, try treating it as a module name
+# Numbered headers
+# Navigational aides
