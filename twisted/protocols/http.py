063ef6c393828fe1364ccdb335d9f555a3f47cdb
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-02-15 14:44:43 +0000

    M R I vars *and* C vars!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5475 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/http.py
+++ twisted/protocols/http.py
@@ -344,7 +344,16 @@ class HTTPClient(basic.LineReceiver):
 NO_BODY_CODES = (204, 304)
 
 class Request:
-    """A HTTP request."""
+    """A HTTP request.
+
+    @cvar method: The HTTP method that was used.
+    @cvar uri: The full URI that was requested (includes arguments).
+    @ivar path: The path only (arguments not included).
+    @ivar args: All of the arguments, including URL and POST arguments.
+    @type args: A mapping of strings (the argument names) to lists of values.
+                i.e., ?foo=bar&foo=baz&quux=spam results in
+                {'foo': ['bar', 'baz'], 'quux': ['spam']}.
+    """
 
     __implements__ = interfaces.IConsumer
 
