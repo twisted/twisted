dedb39e9a208871aab2160518d97e4886dccf076
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-03-04 21:26:25 +0000

    SSL really should be tested more...
    This doesn't pass.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5631 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/test/test_ssl.py
@@ -0,0 +1,36 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from twisted.trial import unittest
+from twisted.internet import protocol, reactor
+from twisted.internet import ssl
+import test_tcp
+
+class ProperlyCloseFilesTestCase(test_tcp.ProperlyCloseFilesTestCase):
+    def setUp(self):
+        f = protocol.ServerFactory()
+        f.protocol = protocol.Protocol
+        self.listener = reactor.listenSSL(
+            0, f, ssl.DefaultOpenSSLContextFactory('', '')
+        )
+        
+        f = protocol.ClientFactory()
+        f.protocol = test_tcp.ConnectionLosingProtocol
+        f.protocol.master = self
+        self.connector = (lambda p=self.listener.getHost()[2]:
+            reactor.connectSSL('localhost', p, f, ssl.ClientContextFactory()))
+        
+        self.totalConnections = 0
