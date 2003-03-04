a34746470b353f1f480579ff67f3d7e87835e156
moonfallen <moonfallen@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-03-04 22:49:11 +0000

    reminder to fix this later
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5635 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/test.py
+++ twisted/web/test.py
@@ -16,6 +16,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """I am a test application for twisted.web.
+
+FIXME -- move into twisted.test or doc/examples?
 """
 
 from twisted.python import log
