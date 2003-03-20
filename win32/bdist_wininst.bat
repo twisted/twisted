9b6e9f094779a7d7776e59f2ed8aeb1b28e8a9c1
moonfallen <moonfallen@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-03-20 16:54:19 +0000

    choke if not libpython22.a, and explain
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5788 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- win32/bdist_wininst.bat
+++ win32/bdist_wininst.bat
@@ -1,7 +1,15 @@
 attrib /s -r
+if not exist c:\python22\libs\libpython22.a goto :choke
 python setup.py clean --all
 python setup.py build --compiler=mingw32
 python setup.py bdist_wininst --install-script=twisted_postinstall.py
 @echo Press Ctrl+C to skip uploading
 pause
 scp dist/Twisted-*.exe shell.sf.net:/home/groups/t/tw/twisted/htdocs/
+goto :done
+
+:choke
+rem libpython22.a must be present to build Twisted.  See 
+rem http://sebsauvage.net/python/mingw.html
+
+:done
