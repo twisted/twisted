9ab47f08b868d6c379c23fcebf25a2138005b1e1
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-03-26 15:30:51 +0000

    Add IFinishableConsumer (not stable) and IProtocol
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5868 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/interfaces.py
+++ twisted/internet/interfaces.py
@@ -656,6 +656,13 @@ class IConsumer(Interface):
     def write(self, data):
         """The producer will write data by calling this method."""
 
+class IFinishableConsumer(IConsumer):
+    """A Consumer for producers that finish.
+
+    This interface is semi-stable.
+    """
+    def finish(self):
+        """The producer has finished producing."""
 
 class IProducer(Interface):
     """A producer produces data for a consumer.
@@ -677,6 +684,8 @@ class IPushProducer(IProducer):
     A push producer, also known as a streaming producer is expected to
     produce (write to this consumer) data in the main IO thread of
     some process as the result of a read operation.
+
+    This interface is semi-stable.
     """
     
     def pauseProducing(self):
@@ -696,6 +705,8 @@ class IPullProducer(IProducer):
     """
     A pull producer, also known as a non-streaming producer, is
     expected to produce data each time resumeProducing() is called.
+
+    This interface is semi-stable.
     """
         
     def resumeProducing(self):
@@ -705,7 +716,46 @@ class IPullProducer(IProducer):
         more data for its consumer.
         """
     
-    
+class IProtocol(Interface):
+    def dataReceived(self, data):
+        """Called whenever data is received.
+
+        Use this method to translate to a higher-level message.  Usually, some
+        callback will be made upon the receipt of each complete protocol
+        message.
+
+        @param data: a string of indeterminate length.  Please keep in mind
+            that you will probably need to buffer some data, as partial
+            (or multiple) protocol messages may be received!  I recommend
+            that unit tests for protocols call through to this method with
+            differing chunk sizes, down to one byte at a time.
+        """
+
+    def connectionLost(self, reason="A sensible default value goes here."):
+        """Called when the connection is shut down.
+
+        Clear any circular references here, and any external references
+        to this Protocol.  The connection has been closed.
+
+        @type reason: L{twisted.python.failure.Failure}
+        """
+
+    def makeConnection(self, transport):
+        """Make a connection to a transport and a server.
+        """
+
+    def connectionMade(self):
+        """Called when a connection is made.
+
+        This may be considered the initializer of the protocol, because
+        it is called when the connection is completed.  For clients,
+        this is called once the connection to the server has been
+        established; for servers, this is called after an accept() call
+        stops blocking and a socket has been received.  If you need to
+        send any greeting or initial message, do it here.
+        """
+
+
 
 class IProtocolFactory(Interface):
     """Interface for protocol factories.
