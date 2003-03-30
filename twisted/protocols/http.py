64e4fa2ca467cca92758135030ed24eaaac3b7c2
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-03-30 23:26:33 +0000

    flushing slows things down and is not necessary (these aren't important logs)
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5916 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/http.py
+++ twisted/protocols/http.py
@@ -1035,4 +1035,3 @@ class HTTPFactory(protocol.ServerFactory):
             request.getHeader("referer") or "-",
             request.getHeader("user-agent") or "-")
         self.logFile.write(line)
-        self.logFile.flush()
