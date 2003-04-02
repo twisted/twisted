f90c94262a97055671f372a0571b1908b15a199a
acapnotic <acapnotic@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-04-02 18:43:36 +0000

    test_rebuild: a later(do) run run goodbye because delayed is "so deprecated you will die."
    I'm changing a *rebuild* test omg ph33r.  But I'm not too worried
    about not fully understading how testRebuild was written because
    testFileRebuild seems to cover everything that this simpler test does.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5970 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_rebuild.py
+++ twisted/test/test_rebuild.py
@@ -1,20 +1,21 @@
 
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001 Matthew W. Lefkowitz
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of version 2.1 of the GNU Lesser General Public
 # License as published by the Free Software Foundation.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from twisted.python.compat import *
 
 import sys
 
@@ -23,7 +24,6 @@ from twisted.python import rebuild
 
 import crash_test_dummy
 from twisted.web import server
-from twisted.python import delay
 f = crash_test_dummy.foo
 
 if sys.version_info >= (2, 2, 0):
@@ -77,24 +77,21 @@ class RebuildTestCase(unittest.TestCase):
         # assert c.b() == 'c'
 
     def testRebuild(self):
+        """Rebuilding an unchanged module."""
+        # This test would actually pass if rebuild was a no-op, but it
+        # ensures rebuild doesn't break stuff while being a less
+        # complex test than testFileRebuild.
+        
         x = crash_test_dummy.X('a')
-        x = [x]
-        d = delay.Delayed()
-        d.later(x[0].do, 1)
-        d.run()
-        d.run()
-        d.run()
 
-        d.later(x[0].do, 1)
-        rebuild.rebuild(crash_test_dummy,0)
-        d.run()
-        d.run()
-        d.run()
+        rebuild.rebuild(crash_test_dummy, doLog=False)
+        # Instance rebuilding is triggered by attribute access.
+        x.do()
+        self.failUnlessIdentical(x.__class__, crash_test_dummy.X)
 
-        rebuild.rebuild(server,0)
-        assert f is crash_test_dummy.foo, 'huh?'
-        #x[0].do()
-        assert x[0].__class__ is crash_test_dummy.X, 'huh?'
+        # Just for kicks?
+        rebuild.rebuild(server, doLog=False)
+        self.failUnlessIdentical(f, crash_test_dummy.foo)
 
     def testComponentInteraction(self):
         x = crash_test_dummy.XComponent()
