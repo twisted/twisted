9db5670b43b37ddd6a6effb6452474c0cdb7b402
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-04-04 21:09:28 +0000

    Add test-case-name tags
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@5984 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/trial/remote.py
+++ twisted/trial/remote.py
@@ -1,3 +1,5 @@
+# -*- test-case-name: twisted.test.test_trial -*-
+#
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001-2002 Matthew W. Lefkowitz
 #
--- twisted/trial/unittest.py
+++ twisted/trial/unittest.py
@@ -1,3 +1,5 @@
+# -*- test-case-name: twisted.test.test_trial -*-
+
 """
 Twisted Test Framework
 """
