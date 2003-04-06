b284e4ef51ce15178d241b9e25a3f3d873318c1e
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-04-06 22:56:40 +0000

    Add tests for the various failUnless/etc methods.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6021 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_trial.py
+++ twisted/test/test_trial.py
@@ -2,9 +2,10 @@
 
 from __future__ import nested_scopes
 
-__version__ = "$Revision: 1.4 $"[11:-2]
+__version__ = "$Revision: 1.5 $"[11:-2]
 
 from twisted.python.compat import *
+from twisted.python import reflect
 from twisted.trial import unittest
 from StringIO import StringIO
 import sys
@@ -137,3 +138,129 @@ class LoopbackTests(unittest.TestCase):
         self.failUnlessEqual(self.reporter.importErrors[0][0],
                              "nosuchmoduleasthis")
         self.failUnlessEqual(f.type, ImportError)
+
+class TestTests(unittest.TestCase):
+    # first, the things we're going to test
+    class Tests(unittest.TestCase):
+        def __init__(self):
+            self.setupRun = 0
+            self.teardownRun = 0
+        def setUp(self):
+            self.setupRun += 1
+        def tearDown(self):
+            self.teardownRun += 1
+        def testSuccess_pass(self):
+            pass
+        def testFail_fail(self):
+            self.fail("failed")
+        def testFailIf_pass(self):
+            self.failIf(0, "failed")
+        def testFailIf_fail(self):
+            self.failIf(1, "failed")
+        def testFailUnless_pass(self):
+            self.failUnless(1, "failed")
+        def testFailUnless_fail(self):
+            self.failUnless(0, "failed")
+        def testFailUnlessRaises_pass(self):
+            def boom():
+                raise ValueError
+            self.failUnlessRaises(ValueError, boom)
+        def testFailUnlessRaises1_fail(self):
+            def boom():
+                raise IndexError
+            self.failUnlessRaises(ValueError, boom)
+        def testFailUnlessRaises2_fail(self):
+            def boom():
+                pass
+            self.failUnlessRaises(ValueError, boom)
+        def testFailUnlessEqual_pass(self):
+            self.failUnlessEqual(1, 1, "failed")
+        def testFailUnlessEqual_fail(self):
+            self.failUnlessEqual(1, 2, "failed")
+        def testFailIfEqual_fail(self):
+            self.failIfEqual(1, 1, "failed")
+        def testFailIfEqual_pass(self):
+            self.failIfEqual(1, 2, "failed")
+        def testFailUnlessIdentical_pass(self):
+            a = [1,2]
+            b = a
+            self.failUnlessIdentical(a, b, "failed")
+        def testFailUnlessIdentical1_fail(self):
+            a = [1,2]
+            b = [1,2]
+            self.failUnlessIdentical(a, b, "failed")
+        def testFailUnlessIdentical2_fail(self):
+            a = [1,2]
+            b = [3,4]
+            self.failUnlessIdentical(a, b, "failed")
+        def testApproximates1_pass(self):
+            a = 1.0
+            b = 1.2
+            self.assertApproximates(a, b, .3, "failed")
+        def testApproximates2_pass(self):
+            a = 1.0
+            b = 1.2
+            self.assertApproximates(b, a, .3, "failed")
+        def testApproximates3_fail(self):
+            a = 1.0
+            b = 1.2
+            self.assertApproximates(a, b, .1, "failed")
+        def testApproximates4_fail(self):
+            a = 1.0
+            b = 1.2
+            self.assertApproximates(b, a, .1, "failed")
+            
+    def checkResults(self, reporter, method):
+        self.failIf(reporter.imports, "%s caused import error" % method)
+        self.failUnless(reporter.numTests == 1,
+                        "%s had multiple tests" % method)
+        if method[-5:] == "_pass":
+            self.failIf(reporter.errors, "%s had error" % method)
+            self.failIf(reporter.failures, "%s had failure" % method)
+            self.failIf(reporter.skips, "%s had skips" % method)
+        if method[-5:] == "_fail":
+            self.failIf(reporter.errors, "%s had error" % method)
+            self.failUnless(len(reporter.failures) == 1,
+                            "%s had %d failures" % (method,
+                                                    len(reporter.failures)))
+            self.failIf(reporter.skips, "%s had skips" % method)
+        if method[-6:] == "_error":
+            self.failUnless(len(reporter.errors) == 1,
+                            "%s had %d errors" % (method,
+                                                  len(reporter.errors)))
+            self.failIf(reporter.failures, "%s had failure" % method)
+            self.failIf(reporter.skips, "%s had skips" % method)
+        if method[-5:] == "_skip":
+            self.failIf(reporter.errors, "%s had error" % method)
+            self.failIf(reporter.failures, "%s had failure" % method)
+            self.failUnless(len(reporter.skips) == 1,
+                            "%s had %d skips" % (method,
+                                                 len(reporter.skips)))
+    def testTests(self):
+        suite = unittest.TestSuite()
+        suffixes = reflect.prefixedMethodNames(TestTests.Tests, "test")
+        for suffix in suffixes:
+            method = "test" + suffix
+            testCase = self.Tests()
+
+            # if one of these test cases fails, switch to TextReporter to
+            # see what happened
+
+            #reporter = unittest.TextReporter()
+            #print "running '%s'" % method
+            reporter = unittest.Reporter()
+            reporter.start(1)
+
+            suite.runOneTest(testCase.__class__, testCase,
+                             getattr(self.Tests, method),
+                             reporter)
+            # TODO: verify that case.setUp == 1 and case.tearDown == 1
+            try:
+                self.checkResults(reporter, method)
+            except unittest.FailTest:
+                # with TextReporter, this will show the traceback
+                print
+                print "problem in method '%s'" % method
+                reporter.stop()
+                raise
+            
