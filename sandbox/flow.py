4fc62b7da7425779858e6be7abe5d8233513e46a
cce <cce@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-04-11 04:09:44 +0000

    updates for commenting, clarity, standards
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6117 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/flow.py
+++ sandbox/flow.py
@@ -1,218 +1,177 @@
-# Twisted, the Framework of Your Internet
-# Copyright (C) 2003 Matthew W. Lefkowitz
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of version 2.1 of the GNU Lesser General
-# Public License as published by the Free Software Foundation.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-# USA
-#
-''' Flow -- async data flow
+""" Flow -- async data flow
 
     This module provides a mechanism for using async data flows through
     the use of generators.  While this module does not use generators in
-    its implementation, it isn't very useable without them.
+    its implementation, it isn't very useable without them.   A data flow
+    is constructed with a top level generator, which can have three 
+    types of yield statements:  flow.Cooperate, flow.Generator, or
+    any other return value with exceptions wrapped using failure.Failure
+    An example program...
 
-    A data flow starts with a top level generator, which has numerous
-    yield statements.   Each yield can return one of several types:
+    from __future__ import generators
+    import flow
+    def producer():
+        lst = flow.Generator([1,2,3])
+        nam = flow.Generator(['one','two','three'])
+        while 1:
+            yield lst; yield nam
+            if lst.stop or nam.stop: 
+                return
+            yield (lst.result, nam.result)
 
-        flow.Cooperate  This (singleton) value should be returned when 
-                        the flow would like to give up control of the 
-                        call stack to allow other events to be handled.
+    def consumer():
+        title = flow.Generator(['Title'])
+        yield title
+        print title.getResult()
+        lst = flow.Generator(producer)
+        try:
+            while 1:
+                yield lst
+                print lst.getResult()
+        except flow.StopIteration: pass
 
-        flow.Generator  This is a sub-flow (iterator) to be executed.  
-                        This object has a 'result' value which can be 
-                        checked for each value yielded.   If the last
-                        iteration of the sub-flow produced an exception,
-                        then a failure.Failure object will be returned.
+    flow.Flow(consumer).execute()
 
-                        While the sub-flow is generating (it has not
-                        returned or raised StopIteration), it will have
-                        an 'active' state of true.  Once it is finished,
-                        the 'active' state will be false.
-
-       <anything>       Any other return value, which is not a FlowItem
- 
-'''
-from __future__ import nested_scopes
+"""
 from twisted.python import failure
 from twisted.python.compat import StopIteration, iter
 
-class FlowItem: pass
-Cooperate = FlowItem() 
+class FlowCommand: 
+    """ Objects given special meaning when returned from yield """
+    pass
 
-class Generator(FlowItem):
+class Cooperate(FlowCommand):
+    """ Represents a request to delay and let other events process
+
+        Objects of this type are returned within a flow when
+        the flow would block, or needs to sleep.  This object
+        is then used as a signal to the flow mechanism to pause
+        and perhaps let other delayed operations to proceed.
+    """
+    def __init__(self, timeout = 0):
+        self.timeout = timeout
+
+class Generator(FlowCommand):
+    """ Wraps a generator or other iterator for use in a flow 
+
+        Creates a nested generation stage (a producer) which can provide
+        zero or more values to the current stage (the consumer).  After 
+        a yield of this object when control has returned to the caller,
+        this object will have two attributes:
+
+            stop    This is true if the underlying generator has not 
+                    been started (a yield is needed) or if the underlying
+                    generator has raised StopIteration
+
+            result  This is the result of the generator if it is active, 
+                    the result may be a fail.Failure object if an 
+                    exception was thrown in the nested generator.
+    """      
     def __init__(self, iterable):
-        self.__next  = iter(iterable).next
-        self.result  = None
-        self.active  = 1
+        self._next  = iter(iterable).next
+        self.result = None
+        self.stop   = 1
     def isFailure(self):
+        """ return a boolean value if the result is a Failure """ 
+        if self.stop: raise StopIteration()
         return isinstance(self.result, failure.Failure)
     def getResult(self):
+        """ return the result, or re-throw an exception on Failure """
         if self.isFailure():
-            res = self.result
-            if res.value:  raise res.value
-            raise res.type
+            raise (self.result.value or self.result.type)
         return self.result
-    def generate(self):
+    def _generate(self):
+        """ update the active and result member variables """ 
         try:
-            self.result = self.__next()
+            self.result = self._next()
+            self.stop = 0
         except StopIteration:
-            self.active = 0
+            self.stop = 1
             self.result = None
         except:
-            self.active = 0
+            self.stop = 1
             self.result = failure.Failure()
 
-class Flow(Generator):
-    ''' a top-level generator which can handle subordinates '''
+class Flow:
+    """ A flow contruct, created with a top-level generator/iterator
+
+        The iterable provided to this flow is the top-level consumer
+        object.  From within the consumer, multiple 'yield' calls can
+        be made returning either Cooperate or Generate.  If a Generate
+        object is returned, then it becomes the current context and
+        the process is continued.  Communication from the producer 
+        back to the consumer is done by yield of a non FlowItem
+    """
     def __init__(self, iterable):
-        Generator.__init__(self, iterable)
-        self._stack = [self]
-    def execute(self, cooperate = 0):
+        self.results = []
+        self._stack  = [Generator(iterable)]
+    def _execute(self):
+        """ private execute, execute flow till a Cooperate is found """
         while self._stack:
             head = self._stack[-1]
-            head.generate()
-            if head.active:
-                if isinstance(result, FlowItem):
-                    if result is Cooperate:
-                        if cooperate: return 1
-                    self._stack.append(result)
-            else:
+            head._generate()
+            if head.stop:
                 self._stack.pop()
-                
-#
-# This code below belongs in twisted.internet.defer
-#
+            else:
+                if isinstance(result, FlowCommand):
+                    if isinstance(result, Cooperate):
+                        return result.timeout
+                    assert(isinstance(result, Generator))
+                    self._stack.append(result)
+                else:
+                    self.results.append(result)
+    def execute(self):
+        """ continually execute, using sleep for Cooperate """
+        from time import sleep
+        while 1:
+            timeout = self._execute()
+            if timeout is None: break
+            sleep(timeout)
 
 from twisted.internet import defer, reactor
 class DeferredFlow(Flow, defer.Deferred):
-   ''' a version of Flow using Twisted's reactor and Deferreds '''
-   def __init__(self, iterable):
+   """ a version of Flow using Twisted's reactor and Deferreds
+
+       In this version, a call to execute isn't required.  Instead,
+       the iterable is scheduled right away using the reactor.  And,
+       the Cooperate is implemented through the reactor's callLater.
+
+       Since more than one (possibly failing) result could be returned,
+       this uses the same semantics as DeferredList
+   """
+   def __init__(self, iterable, delay = 0, 
+                fireOnOneCallback=0, fireOnOneErrback=0):
+       """initialize a DeferredFlow
+       @param iterable:          top level iterator / generator
+       @param delay:             delay when scheduling reactor.callLater
+       @param fireOnOneCallback: a flag indicating that the first good 
+                                 yielded result should be sent via Callback
+       @param fireOnOneErrback:  a flag indicating that the first failing
+                                 yield result should be sent via Errback
+       """
        defer.Deferred.__init__(self)
        Flow.__init__(iterable)
-       reactor.callLater(0, self.execute)
-   def execute(self):
-       if Flow.execute(self, cooperate = 1):
-           reactor.callLater(0, self.execute)
+       self.fireOnOneCallback = fireOnOneCallback
+       self.fireOnOneErrback  = fireOnOneErrback
+       reactor.callLater(delay, self._execute)
+   def execute(self): 
+       raise TypeError("Deferred Flow is auto-executing") 
+   def _execute(self):
+       timeout = Flow._execute(self)
+       if timeout is None:
+           # emulate DeferredList behavior rather than invent...
+           if self.fireOnOneErrback or self.fireOnOneCallback:
+               idx = 0
+               for result in self.results:
+                    isFail = isinstance(result, failure.Failure)
+                    if self.fireOnOneErrback and isFail:
+                        self.errback(fail.Failure((result,idx)))
+                        return
+                    if self.fireOnOneCallback and not isFail:
+                        self.callback((result,idx))
+                        return
+                    idx += 1
+           self.callback(self.results)
        else:
-           if self.isFailure():
-               self.errback(self.result)
-           else:
-               self.callback(self.result)
-
-#
-# The following is a thread package which really is othogonal to
-# Flow.  Flow does not depend on it, and it does not depend on Flow,
-# with the exception of 'Cooperate'.  Although, if you are trying 
-# to bring the output of a thread into a Flow, it is exactly what 
-# you want.   The QueryIterator is just an obvious application 
-# of the ThreadedIterator.
-#
-
-class ThreadedIterator:
-    """
-       This is an iterator base class which can be used to build
-       iterators which are constructed and run within a Flow
-    """
-     
-    def __init__(self, data = None):
-        self.data = data  
-        tunnel = _TunnelIterator(self)
-        self._tunnel = tunnel
-     
-    def __iter__(self): 
-        from twisted.internet.reactor import callInThread
-        callInThread(self._tunnel.process)
-        return self._tunnel
-     
-    def next(self):
-        """ 
-            The method used to fetch the next value, make sure
-            to return a list of rows, not just a row
-        """
-        raise StopIteration
-
-class _TunnelIterator:
-    """
-       This is an iterator which tunnels output from an iterator
-       executed in a thread to the main thread.   Note, unlike
-       regular iterators, this one throws a PauseFlow exception
-       which must be handled by calling reactor.callLater so that
-       the producer threads can have a chance to send events to 
-       the main thread.
-    """
-    def __init__(self, source):
-        """
-            This is the setup, the source argument is the iterator
-            being wrapped, which exists in another thread.
-        """
-        self.source     = source
-        self.isFinished = 0
-        self.failure    = None
-        self.buff       = []
-     
-    def process(self):
-        """
-            This is called in the 'source' thread, and 
-            just basically sucks the iterator, appending
-            items back to the main thread.
-        """
-        from twisted.internet.reactor import callFromThread
-        try:
-            while 1:
-                val = self.source.next()
-                self.buff.extend(val)    # lists are thread safe
-        except StopIteration:
-            callFromThread(self.stop)
-        self.source = None
-     
-    def setFailure(self, failure):
-        self.failure = failure
-     
-    def stop(self):
-        self.isFinished = 1
-     
-    def next(self):
-        if self.buff:
-           return self.buff.pop(0)
-        if self.isFinished:  
-            raise StopIteration
-        if self.failure:
-            raise self.failure
-        raise Cooperation
-
-class QueryIterator(ThreadedIterator):
-    def __init__(self, pool, sql, fetchall=0):
-        ThreadedIterator.__init__(self)
-        self.curs = None
-        self.sql  = sql
-        self.pool = pool
-        self.data = None
-        self.fetchall = fetchall
-     
-    def __call__(self,data):
-        self.data = data
-        return self
-     
-    def next(self):
-        if not self.curs:
-            conn = self.pool.connect()
-            self.curs = conn.cursor()
-            if self.data: self.curs.execute(self.sql % self.data) 
-            else: self.curs.execute(self.sql)
-        if self.fetchall:
-            res = self.curs.fetchall()
-        else:
-            res = self.curs.fetchmany()
-        if not(res): 
-            raise StopIteration
-        return res
+           reactor.callLater(timeout, self._execute)
+ 
