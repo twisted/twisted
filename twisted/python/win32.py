45581a14f01c7d6fd5775b5e1d3333e7c31e113c
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-04-29 20:32:03 +0000

    module for win32 utility functions and classes - add your own today
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6446 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/python/win32.py
@@ -0,0 +1,41 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Win32 utilities.
+
+See also twisted.python.shortcut.
+"""
+
+# system imports
+import win32api, win32con
+
+# sibling import
+from runtime import platform
+
+
+def getProgramsMenuPath():
+    """Get the path to the Programs menu.
+    
+    @return the filesystem location of the common Start Menu.
+
+    Probably will break on non-US Windows.
+    """
+    if not platform.isWinNT():
+        return "C:\\Windows\\Start Menu\\Programs"
+    keyname = 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders'
+    hShellFolders = win32api.RegOpenKeyEx(win32con.HKEY_LOCAL_MACHINE, 
+                                          keyname, 0, win32con.KEY_READ)
+    return win32api.RegQueryValueEx(hShellFolders, 'Common Programs')[0]
