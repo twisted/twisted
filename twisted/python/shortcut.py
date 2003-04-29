158f9e9dab4e14629e7afe619f86b0d9c37edfd0
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-04-29 20:32:22 +0000

    copyright
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6448 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/shortcut.py
+++ twisted/python/shortcut.py
@@ -1,11 +1,27 @@
-"""
-A windows shortcut.
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-win32 only.
+"""Creation of  Windows shortcuts.
+
+Requires win32all.
 """
 
 from win32com.shell import shell
 import pythoncom
+
 
 def open(filename):
     """Open an existing shortcut for reading.
@@ -15,6 +31,7 @@ def open(filename):
     sc=Shortcut()
     sc.load(filename)
     return sc
+
 
 class Shortcut:
     """A shortcut on Win32.
