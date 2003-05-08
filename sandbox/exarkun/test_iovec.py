d09c6c09bd6f15982f0cbba17c0580e94ac6323b
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-05-08 19:17:44 +0000

    and a testcase for stuff
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6658 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/test_iovec.py
+++ sandbox/exarkun/test_iovec.py
@@ -3,22 +3,23 @@
 import iovec
 
 import tempfile
+import socket
 
 from twisted.trial import unittest
 
 class IOVectorTestCase(unittest.TestCase):
     def testAllocDealloc(self):
-        v = iovec.IOVectorType()
+        v = iovec.iovec()
         del v
     
     def testAdd(self):
-        v = iovec.IOVectorType()
+        v = iovec.iovec()
         for s in [chr(i + ord('a')) * i for i in range(1, 11)]:
             v.add(s)
 
         
     def testWriteToFile(self):
-        v = iovec.IOVectorType()
+        v = iovec.iovec()
         for s in [chr(i + ord('a')) * i for i in range(1, 11)]:
             v.add(s)
         
@@ -29,7 +30,7 @@ class IOVectorTestCase(unittest.TestCase):
         self.assertEquals(f.read(), ''.join([chr(i + ord('a')) * i for i in range(1, 11)]))
 
     def testWriteToFileDescriptor(self):
-        v = iovec.IOVectorType()
+        v = iovec.iovec()
         for s in [chr(i + ord('a')) * i for i in range(1, 11)]:
             v.add(s)
         f = tempfile.TemporaryFile('w+')
@@ -40,7 +41,7 @@ class IOVectorTestCase(unittest.TestCase):
         self.assertEquals(f.read(), ''.join([chr(i + ord('a')) * i for i in range(1, 11)]))
 
     def testIllegalWrites(self):
-        v = iovec.IOVectorType()
+        v = iovec.iovec()
         for s in ['x' * i for i in range(100)]:
             v.add(s)
 
@@ -53,3 +54,29 @@ class IOVectorTestCase(unittest.TestCase):
         del Pah.fileno
         self.assertRaises(AttributeError, v.write, Pah())
         self.assertRaises(AttributeError, v.write, 'a string')
+
+    def testIncompleteWrites(self):
+        server = socket.socket()
+        server.setblocking(0)
+        server.bind(('', 0))
+        server.listen(5)
+        
+        port = server.getsockname()[1]
+        
+        client = socket.socket()
+        client.setblocking(0)
+        try:
+            client.connect(('', port))
+        except:
+            pass
+        
+        s, _ = server.accept()
+        
+        v = iovec.iovec()
+        for i in range(1000, 2000):
+            v.add(chr(ord('a') + i % 26) * i)
+        
+        written = v.write(client)
+        # print
+        # print written
+        # print
