8bd5b156c8a7436958a95e283ce352c99f51756f
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-05-12 15:47:09 +0000

    isLeaf for redirect
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6700 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/util.py
+++ twisted/web/util.py
@@ -1,3 +1,21 @@
+# -*- test-case-name: twisted.test.test_web -*-
+
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 from cStringIO import StringIO
 
 from twisted.python import failure
@@ -26,6 +44,9 @@ def redirectTo(URL, request):
 """ % {'url': URL}
 
 class Redirect(resource.Resource):
+
+    isLeaf = 1
+    
     def __init__(self, url):
         resource.Resource.__init__(self)
         self.url = url
