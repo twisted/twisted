3b9b5ac8a24b0f28e615be638e6caeda2449c572
dp <dp@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-05-21 19:08:07 +0000

    Properly check to see if there is a slash at the end of a url
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6815 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/woven/controller.py
+++ twisted/web/woven/controller.py
@@ -18,7 +18,7 @@
 
 from __future__ import nested_scopes
 
-__version__ = "$Revision: 1.59 $"[11:-2]
+__version__ = "$Revision: 1.60 $"[11:-2]
 
 import os
 import cgi
@@ -190,7 +190,7 @@ class Controller(resource.Resource):
         DOM. gatheredControllers will render the page to the browser
         when it is done.
         """
-        if self.addSlash and request.uri[-1] != '/':
+        if self.addSlash and request.uri.split('?')[0][-1] != '/':
             return redirectTo(addSlash(request), request)
         # Handle any inputhandlers that were passed in to the controller first
         for ih in self._inputhandlers:
