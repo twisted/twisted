8971addedc5869daa939b07711b59b07348f78ce
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-05-26 05:42:56 +0000

    Don't divide by zero, matey!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6865 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/smtp.py
+++ twisted/protocols/smtp.py
@@ -114,7 +114,8 @@ def rfc822date(timeinfo=None,local=1):
             tz = -time.timezone
             
         (tzhr, tzmin) = divmod(abs(tz), 3600)
-        tzhr *= int(abs(tz)/tz)
+        if tz:
+            tzhr *= int(abs(tz)/tz)
         (tzmin, tzsec) = divmod(tzmin, 60)
     else:
         (tzhr, tzmin) = (0,0)
