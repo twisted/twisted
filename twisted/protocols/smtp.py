9367c8de2c1b7cd1f30f4f7a6599b4cf3158ae2e
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-05-26 05:48:15 +0000

    -another- test case name!  woot woot
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6867 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/smtp.py
+++ twisted/protocols/smtp.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twisted.test.test_smtp -*-
+# -*- test-case-name: twisted.test.test_smtp, twisted.test.test_unix -*-
 #
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001 Matthew W. Lefkowitz
