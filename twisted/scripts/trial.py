e2f3090f380d7eab6559a6eb555c5d91b5421e76
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-06-02 15:46:26 +0000

    jump into debugger on exceptions
    someone really out to take this code from here and twistd.y and move it into failure.py as a utility function
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@6956 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/trial.py
+++ twisted/scripts/trial.py
@@ -263,6 +263,17 @@ def run():
     if config['debug']:
         reporter.debugger = 1
         import pdb
+        from twisted.python import failure
+        def newinit(self, exc_value=None, exc_type=None, exc_tb=None,
+                    init=failure.Failure.__init__.im_func):
+            r = init(self, exc_value, exc_type, exc_tb)
+            if (exc_value, exc_type, exc_tb) == (None, None, None):
+                exc = sys.exc_info()
+                if not exc[0] == failure.Failure:
+                    print "Jumping into debugger - %s" % exc[1]
+                    pdb.post_mortem(exc[2])
+            return r
+        failure.Failure.__init__ = newinit
         dbg = pdb.Pdb()
         try:
             rcFile = open("../.pdbrc")
