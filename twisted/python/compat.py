9f00096e4e070dbfe580b48ca4d2ffd18fb5a2bc
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-06-10 14:30:03 +0000

    Get rid of 2.1 compat (i'm watching the buildbot :)
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7051 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/compat.py
+++ twisted/python/compat.py
@@ -26,33 +26,6 @@ the latest version of Python directly from your code, if possible.
 
 import sys, types, socket, struct, __builtin__, exceptions
 
-_dict_doc = (
-    """dict() -> new empty dictionary.
-    dict(mapping) -> new dictionary initialized from a mapping object's
-        (key, value) pairs.
-    dict(seq) -> new dictionary initialized as if via:
-        d = {}
-        for k, v in seq:
-            d[k] = v
-    dict(**kwargs) -> new dictionary initialized with the name=value pairs
-        in the keyword argument list.  For example:  dict(one=1, two=2)
-    """)
-if sys.version_info[:2] == (2, 1):
-    def dict(*arg, **kwargs):
-        r = {}
-        if arg:
-            assert len(arg) == 1
-            arg = arg[0]
-            if hasattr(arg, 'items'):
-                r.update(arg)
-            else:
-                for k, v in arg:
-                    r[k] = v
-        for k, v in kwargs.items():
-            r[k] = v
-        return r
-    dict.__doc__ = _dict_doc
-    __builtin__.dict = dict
 #elif sys.version_info[:2] == (2, 2):
 #    def dict(*arg, **kwargs):
 #        d = types.DictType(*arg)
@@ -61,19 +34,6 @@ if sys.version_info[:2] == (2, 1):
 #    dict.__doc__ = _dict_doc
 #    __builtin__.dict = dict
 
-
-if sys.version_info[:2] == (2, 1):
-    types.StringTypes = types.StringType, types.UnicodeType
-
-    def isinstance(object, class_or_type_or_tuple, _f = __builtin__.isinstance):
-        if type(class_or_type_or_tuple) == types.TupleType:
-            for t in class_or_type_or_tuple:
-                if _f(object, t):
-                    return 1
-            return 0
-        else:
-            return _f(object, class_or_type_or_tuple)
-    __builtin__.isinstance = isinstance
 
 if sys.version_info[:3] == (2, 2, 0):
     __builtin__.True = (1 == 1)
@@ -87,96 +47,6 @@ if sys.version_info[:3] == (2, 2, 0):
         return not not value
     __builtin__.bool = bool
     del bool
-
-if sys.version_info[:2] == (2, 1):
-    class StopIteration(Exception):
-        """Signal the end from iterator.next()."""
-        pass
-
-    class _SequenceIterator:
-        """
-        Facilitates implicit sequence/dict iterability
-        """
-        def __init__(self, seq):
-            self.idx = 0
-            # dictionary behavior
-            if hasattr(seq, 'keys'):
-                seq = seq.keys()
-            self.seq = seq
-
-        def __iter__(self):
-            return self
-
-        def next(self):
-            idx = self.idx
-            self.idx += 1
-            try:
-                return self.seq[idx]
-            except IndexError: 
-                raise StopIteration
-
-    class _CompatIterator:
-        """
-        Facilitates for x in iter(iterable)
-        """
-        def __init__(self, iterable):
-            self.iterable = iterable
-
-        def next(self):
-            return self.iterable.next()
-
-        def __getitem__(self, index):
-            try:
-                return self.next()
-            except StopIteration, s:
-                raise IndexError, s
-
-    class _CompatSentinelIterator(_CompatIterator):
-        """
-        Facilitates for x in iter(callable, sentinel)
-        """
-        def __init__(self, callable, sentinel):
-            self.callable = callable
-            self.sentinel = sentinel
-
-        def next(self):
-            res = self.callable()
-            if res == self.sentinel:
-                raise StopIteration
-            return res
-    
-    def _iter(iterable):
-        if isinstance(iterable, _CompatIterator):
-            # already iterable
-            return iterable
-        if not hasattr(iterable, '__iter__'):
-            # implicit iteration, dependent on sequence or dict behavior
-            iterable = _SequenceIterator(iterable)
-        return _CompatIterator(iterable.__iter__())
-
-    def _iter_sentinel(fn, sentinel):
-        if not callable(fn):
-            raise TypeError, 'iter(v, w): v must be callable'
-        return _CompatSentinelIterator(fn, sentinel)
-
-    def iter(*args): 
-        """
-        iter(collection) -> iterator
-        iter(callable, sentinel) -> iterator
-
-        Get an iterator from an object.  In the first form, the argument must
-        supply its own iterator, or be a sequence.
-        In the second form, the callable is called until it returns the sentinel.
-        """
-        if len(args) == 1:
-            return _iter(*args)
-        elif len(args) == 2:
-            return _iter_sentinel(*args)
-        raise TypeError, "iter() takes at most 2 arguments (%d given)" % (len(args),)
-
-    exceptions.StopIteration = __builtin__.StopIteration = StopIteration
-    __builtin__.iter = iter
-
 
 
 if not hasattr(socket, 'inet_pton'):
