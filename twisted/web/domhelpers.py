4355e471c4701f0085c00ece35f6349db1175914
moonfallen <moonfallen@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-06-13 01:40:59 +0000

    gatherTextNodes can unescape as it goes
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7088 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/domhelpers.py
+++ twisted/web/domhelpers.py
@@ -139,12 +139,13 @@ def superAppendAttribute(node, key, value):
         for child in node.childNodes:
             superAppendAttribute(child, key, value)
 
-def gatherTextNodes(iNode):
+def gatherTextNodes(iNode, dounescape=0):
     """Visit each child node and collect its text data, if any, into a string.
 For example::
     >>> doc=microdom.parseString('<a>1<b>2<c>3</c>4</b></a>')
     >>> gatherTextNodes(doc.documentElement)
     '1234'
+With dounescape=1, also convert entities back into normal characters.
 @return: the gathered nodes as a single string
 @rtype: str
 """
@@ -154,7 +155,11 @@ For example::
     while len(slice)>0:
         c=slice.pop(0)
         if hasattr(c, 'nodeValue') and c.nodeValue is not None:
-            gathered_append(c.nodeValue)
+            if dounescape:
+                val=unescape(c.nodeValue)
+            else:
+                val=c.nodeValue
+            gathered_append(val)
         slice[:0]=c.childNodes
     return ''.join(gathered)
 
