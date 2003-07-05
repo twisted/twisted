7ff3f04de5f624a6261b281a755f5790c90566bb
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-05 16:48:29 +0000

    emacs didn't like multiple values for test-case-name
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7469 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/smtp.py
+++ twisted/protocols/smtp.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twisted.test.test_smtp, twisted.test.test_unix -*-
+# -*- test-case-name: twisted.test.test_smtp -*-
 #
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001 Matthew W. Lefkowitz
