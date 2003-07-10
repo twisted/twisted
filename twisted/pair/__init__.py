3878a159286acbae51c51af57d40ff83ebeb5959
tv <tv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-10 16:51:06 +0000

    Pun indented.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7526 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/pair/__init__.py
+++ twisted/pair/__init__.py
@@ -17,7 +17,12 @@
 
 """
 
-Twisted Pair: Low-level networking transports and utilities.
+Twisted Pair: The framework of your ethernet.
+
+Low-level networking transports and utilities.
+
+See also twisted.protocols.ethernet, twisted.protocols.ip,
+twisted.protocols.raw and twisted.protocols.rawudp.
 
 Maintainer: U{Tommi Virtanen <mailto:tv@twistedmatrix.com>}
 
