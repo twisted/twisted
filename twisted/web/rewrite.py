f0bf07ea224331c28fb3cf006cfbb5968c5d9ec6
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-12 16:11:52 +0000

    An insufficient aliaser.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7539 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/rewrite.py
+++ twisted/web/rewrite.py
@@ -44,3 +44,21 @@ def tildeToUsers(request):
     if request.postpath and request.postpath[0][:1]=='~':
         request.postpath[:1] = ['users', request.postpath[0][1:]]
         request.path = '/'+'/'.join(request.prepath+request.postpath)
+
+def alias(aliasPath, sourcePath):
+    """
+    I am not a very good aliaser. But I'm the best I can be. If I'm
+    aliasing to a Resource that generates links, and it uses any parts
+    of request.prepath to do so, the links will not be relative to the
+    aliased path, but rather to the aliased-to path. That I can't
+    alias static.File directory listings that nicely. However, I can
+    still be useful, as many resources will play nice.
+    """
+    sourcePath = sourcePath.split('/')
+    aliasPath = aliasPath.split('/')
+    def rewriter(request):
+        if request.postpath[:len(aliasPath)] == aliasPath:
+            after = request.postpath[len(aliasPath):]
+            request.postpath = sourcePath + after
+            request.path = '/'+'/'.join(request.prepath+request.postpath)
+    return rewriter
