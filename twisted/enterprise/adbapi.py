9183bcab2d9fc4645d9db687068d87b9bfbbabe5
cyli <cyli@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-19 06:55:40 +0000

    Mortal limitations: back with a vengeance (accidental commit - sorry)
    Accidentally changed adbapi and committed.  Oopsie - this is the previous
    (1.50) version
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7648 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/enterprise/adbapi.py
+++ twisted/enterprise/adbapi.py
@@ -32,15 +32,6 @@ class Transaction:
 
     def __init__(self, pool, connection):
         self._connection = connection
-<<<<<<< adbapi.py
-        self.reopen()
-
-    def reopen(self):
-        if self._cursor is not None:
-            self._cursor.close()
-        self._cursor = self._connection.cursor()
-        
-=======
         self.reopen()
 
     def reopen(self):
@@ -48,7 +39,6 @@ class Transaction:
             self._cursor.close()
         self._cursor = self._connection.cursor()
 
->>>>>>> 1.48
     def __getattr__(self, name):
         return getattr(self._cursor, name)
 
