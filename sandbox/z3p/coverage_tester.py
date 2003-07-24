ec283a89c80a47b6866d45d0e33ce0640e26c009
z3p <z3p@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-24 04:31:18 +0000

    initial stab at code-coverage testing
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7750 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ sandbox/z3p/coverage_tester.py
@@ -0,0 +1,9 @@
+def test():
+    print 'hello'
+    1+1==2
+    print 'goodbye'
+
+def test2():
+    print "this won't get covered"
+    print 'making only half of the program covered'
+    print 'wow'
