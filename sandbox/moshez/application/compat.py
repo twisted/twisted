e5f55f1b553593e8b056c212de6d25758916a5fd
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-27 04:50:40 +0000

    Add adapter to old behaviour
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7800 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/compat.py
+++ sandbox/moshez/application/compat.py
@@ -1,4 +1,5 @@
 from twisted.python import components
+from twisted.application import servers, clients
 
 class IOldApplication(components.Interface):
 
@@ -44,3 +45,67 @@ class IOldApplication(components.Interface):
 
     def connectUNIX(self, address, factory, timeout=30):
         pass
+
+
+class ServiceNetwork:
+
+    __implements__ = IOldApplication,
+
+    def __init__(self, app):
+        self.app = app
+
+    def listenWith(self, portType, *args, **kw):
+        servers.GenericServer(portType, *args, **kw).setParent(self.app)
+
+    def unlistenWith(self, portType, *args, **kw):
+        raise NotImplementedError()
+
+    def listenTCP(self, port, factory, backlog=5, interface=''):
+        servers.TCPServer(port, factory, backlog, interface).setParent(self.app)
+        pass
+
+    def unlistenTCP(self, port, interface=''):
+        raise NotImplementedError()
+
+    def listenUNIX(self, filename, factory, backlog=5, mode=0666):
+        servers.UNIXServer(filename, factory, backlog, mode).setParent(self.app)
+
+    def unlistenUNIX(self, filename):
+        raise NotImplementedError()
+
+    def listenUDP(self, port, proto, interface='', maxPacketSize=8192):
+        s = servers.UDPServer(port, proto, interface, maxPacketSize)
+        s.setParent(self.app)
+
+    def unlistenUDP(self, port, interface=''):
+        raise NotImplementedError()
+
+    def listenSSL(self, port, factory, ctxFactory, backlog=5, interface=''):
+        s = servers.SSLServer(port, factory, ctxFactory, backlog, interface)
+        s.setParent(self.app)
+
+    def unlistenSSL(self, port, interface=''):
+        raise NotImplementedError()
+
+    def connectWith(self, connectorType, *args, **kw):
+        s = clients.GenericClient(connectorType,  *args, **kw)
+        s.setParent(self.app)
+
+    def unlistenSSL(self, port, interface=''):
+        pass
+
+    def connectUDP(self, remotehost, remoteport, protocol, localport=0,
+                  interface='', maxPacketSize=8192):
+        s = clients.GenericClient(connectorType,  *args, **kw)
+        s.setParent(self.app)
+
+    def connectTCP(self, host, port, factory, timeout=30, bindAddress=None):
+        s = clients.TCPClient(host, port, factory, timeout, bindAddress)
+        s.setParent(self.app)
+
+    def connectUNIX(self, address, factory, timeout=30):
+        s = clients.UNIXClient(address, factory, timeout)
+        s.setParent(self.app)
+
+
+components.registerAdapter(service.IServiceCollection, ServiceNetwork)
