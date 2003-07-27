84a7f8cf011b8249dda90d44cd045f57440aab64
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-27 00:29:35 +0000

    Clients
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7791 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ sandbox/moshez/application/clients.py
@@ -0,0 +1,46 @@
+from twisted.application import service
+
+class _AbstractClient(service.Service):
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __getstate__(self):
+        d = service.Service.__getstate__(self)
+        if d.has_key('_connection'):
+            del d['_connection']
+        return d
+
+    def startService(self):
+        service.Service.startService(self)
+        self._connection = self.getConnection()
+
+    def stopService(self):
+        service.Service.stopService(self)
+        self._connection.stopListening()
+
+    def getConnection(self):
+        return getattr(reactor, self.method)(*self.args, **self.kwargs)
+
+
+class GenericClient(_AbstractClient):
+    method = 'connectWith'
+
+class TCPClient(_AbstractClient):
+    method = 'connectTCP'
+
+class UNIXClient(_AbstractClient):
+    method = 'connectUNIX'
+
+class SSLClient(_AbstractClient):
+    method = 'connectSSL'
+
+class UDPClient(_AbstractClient):
+    method = 'connectUDP'
+
+class UNIXDatagramClient(_AbstractClient):
+    method = 'connectUNIXDatagram'
+
+class MulticastClient(_AbstractClient):
+    method = 'connectMulticast'
