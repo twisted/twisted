bc27232d5408b0893c806670188b7cb662bb92ad
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-30 07:54:47 +0000

    Move convert stuff into compat
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7902 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/compat.py
+++ sandbox/moshez/application/compat.py
@@ -15,7 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 from twisted.python import components, log
-from twisted.application import servers, clients, service
+from twisted.application import servers, clients, service, app
 from twisted.persisted import sob
 
 class IOldApplication(components.Interface):
@@ -180,3 +180,34 @@ class ServiceNetwork:
 
 components.registerAdapter(ServiceNetwork,
                            service.IServiceCollection, IOldApplication)
+
+
+mapping = []
+for tran in 'tcp unix udp ssl'.split():
+    mapping.append((tran+'Ports', getattr(servers, tran.upper()+'Server')))
+    mapping.append((tran+'Connectors', getattr(clients, tran.upper()+'Client')))
+
+def convert(oldApp):
+    '''
+    This function might damage oldApp beyond repair: services
+    that other parts might be depending on might be missing.
+    It is not safe to use oldApp after it has been converted.
+    In case this behaviour is not desirable, pass a deep copy
+    of the old application
+    '''
+    ret = app.Application(oldApp.name, oldApp.uid, oldApp.gid)
+    ret.processName = oldApp.processName
+    for (pList, klass) in [(oldApp.extraPorts, servers.GenericServer),
+                           (oldApp.extraConnectors, clients.GenericClient),]:
+        for (portType, args, kw) in pList:
+            klass(portType, *args, **kw).setServiceParent(ret)
+    for (name, klass) in mapping:
+        for args in getattr(oldApp, name):
+            klass(*args).setServiceParent(ret)
+    for service in ret:
+        if isinstance(service, servers._AbstractServer):
+            service.privileged = 1
+    for service in oldApp.services.values():
+        service.disownServiceParent()
+        service.setServiceParent(ret)
+    return ret
