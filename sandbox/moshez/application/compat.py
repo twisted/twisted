2f2745e99fdde6e40978091a5801206e178fc37c
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-07-30 08:42:36 +0000

    Remove unnecessary import
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@7907 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/compat.py
+++ sandbox/moshez/application/compat.py
@@ -14,7 +14,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-from twisted.python import components, log
+from twisted.python import components
 from twisted.application import internet, service
 from twisted.persisted import sob
 
