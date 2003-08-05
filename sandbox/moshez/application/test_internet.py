e959d75cb086ae510c0dafd074fd0a31215187ca
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-05 07:44:32 +0000

    More complete coverage
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8047 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/test_internet.py
+++ sandbox/moshez/application/test_internet.py
@@ -18,6 +18,7 @@ from twisted.trial import unittest
 from twisted.application import service, internet
 from twisted.protocols import wire, basic
 from twisted.internet import protocol, reactor
+import copy
 
 class TestInternet(unittest.TestCase):
 
@@ -41,6 +42,73 @@ class TestInternet(unittest.TestCase):
         while factory.line is None:
             reactor.iterate(0.1)
         self.assertEqual(factory.line, 'lalala')
+
+    def testPrivileged(self):
+        factory = protocol.ServerFactory()
+        factory.protocol = wire.Echo
+        t = internet.TCPServer(0, factory)
+        t.privileged = 1
+        t.privilegedStartService()
+        num = t._port.getHost()[2]
+        class Foo(basic.LineReceiver):
+            def connectionMade(self):
+                self.transport.write('lalala\r\n')
+            def lineReceived(self, line):
+                self.factory.line = line
+        factory = protocol.ClientFactory()
+        factory.protocol = Foo
+        factory.line = None
+        internet.TCPClient('localhost', num, factory).startService()
+        while factory.line is None:
+            reactor.iterate(0.1)
+        self.assertEqual(factory.line, 'lalala')
+
+    def testConnectionGettingRefused(self):
+        factory = protocol.ServerFactory()
+        factory.protocol = wire.Echo
+        t = internet.TCPServer(0, factory)
+        t.startService()
+        num = t._port.getHost()[2]
+        t.stopService()
+        l = []
+        factory = protocol.ClientFactory()
+        factory.clientConnectionFailed = lambda *args: l.append(None)
+        c = internet.TCPClient('localhost', num, factory)
+        c.startService()
+        while not l:
+            reactor.iterate(0.1)
+        self.assertEqual(l, [None])
+
+    def testUNIX(self):
+        s = service.MultiService()
+        s.startService()
+        factory = protocol.ServerFactory()
+        factory.protocol = wire.Echo
+        t = internet.UNIXServer('echo.skt', factory)
+        t.setServiceParent(s)
+        class Foo(basic.LineReceiver):
+            def connectionMade(self):
+                self.transport.write('lalala\r\n')
+            def lineReceived(self, line):
+                self.factory.line = line
+        factory = protocol.ClientFactory()
+        factory.protocol = Foo
+        factory.line = None
+        internet.UNIXClient('echo.skt', factory).setServiceParent(s)
+        while factory.line is None:
+            reactor.iterate(0.1)
+        self.assertEqual(factory.line, 'lalala')
+        s.stopService()
+
+    def testVolatile(self):
+        factory = protocol.ServerFactory()
+        factory.protocol = wire.Echo
+        t = internet.UNIXServer('echo.skt', factory)
+        t.startService()
+        self.assert_(hasattr(t, '_port'))
+        t1 = copy.copy(t)
+        self.assert_(not hasattr(t1, '_port'))
+        t.stopService()
 
     def testTimer(self):
         l = []
