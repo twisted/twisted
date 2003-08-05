9e4b78068a79dd6e443fd33dcc2fcfca6713e49c
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-05 06:01:34 +0000

    Fix imports
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8039 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/twistd.py
+++ sandbox/moshez/application/twistd.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-from twisted.python import util, log, logfile, syslog
+from twisted.python import util, log, syslog
 from twisted.application import app, service
 import sys, os, errno, signal
 
--- sandbox/moshez/application/twistw.py
+++ sandbox/moshez/application/twistw.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-from twisted.python import util, log, logfile
+from twisted.python import util, log
 from twisted.application import app, service, internet
 import sys, os
 
