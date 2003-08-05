ff37e2d55595a943e63e8ec7e96681cb5e094b17
moonfallen <moonfallen@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-05 20:47:17 +0000

    no dependency on path
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8056 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moonfallen/PASS_unzip.py
+++ sandbox/moonfallen/PASS_unzip.py
@@ -1,36 +1,44 @@
 from __future__ import generators
 
 import zipfile
-from path import path
+import os.path
 
 
 DIR_BIT=16
-def unzip(filename, directory=".", overwrite=0, generator=1):
+def unzip(filename, directory=".", overwrite=0):
     """Unzip the file
     @param filename: the name of the zip file
-    @param directory: the directory into which the files will be extracted
+    @param directory: the directory into which the files will be
+    extracted
+    @param overwrite: if on, overwrite files when they exist.  You can
+    still get an error if you try to create a directory over a file
+    with the same name or vice-versa.
+    @param generator: yield after every file
     """
+    for i in unzipiter(filename, directory, overwrite):
+        pass
+
+def unzipiter(filename, directory='.', overwrite=0):
     zf=zipfile.ZipFile(filename, 'r')
     names=zf.namelist()
-    remaining=count=len(names)
+    if not os.path.exists(directory): os.makedirs(directory)
+    remaining=len(names)
     for entry in names:
-        remaining=remaining-1
+        remaining=remaining - 1
         isdir=zf.getinfo(entry).external_attr & DIR_BIT
-        f=(path(directory)/entry).abspath()
-        fdir=f.dirname()
-        try:
-            fdir.makedirs()
-        except OSError, e:
-            if e.args[1]=='File exists':
-                pass
-            else:
-                raise e
+        f=os.path.join(directory, entry)
+        if isdir:
+            # overwrite flag only applies to files
+            if not os.path.exists(f): os.makedirs(f)
+        else:
+            # create the directory the file will be in first,
+            # since we can't guarantee it exists
+            fdir=os.path.split(f)[0]
+            if not os.path.exists(fdir):
+                os.makedirs(f)
+            if overwrite or not os.path.exists(f):
+                outfile=file(f, 'wb')
+                outfile.write(zf.read(entry))
+                outfile.close()
+        yield remaining
 
-        if isdir and not f.exists():
-            f.mkdir()
-        elif (overwrite or not f.exists()):
-            outfile=file(f, 'wb')
-            outfile.write(zf.read(entry))
-            outfile.close()
-        if generator:
-            yield remaining
