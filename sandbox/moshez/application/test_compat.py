ee21a4ff4c31a5ec655fdf5ab750d721a70c1f8c
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-06 05:40:17 +0000

    Test convertion old application -> new application
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8061 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/test_compat.py
+++ sandbox/moshez/application/test_compat.py
@@ -15,10 +15,10 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 from twisted.trial import unittest
-from twisted.application import service, compat
+from twisted.application import service, compat, internet
 from twisted.protocols import wire, basic
 from twisted.internet import protocol, reactor
-import os
+import os, pickle
 
 class TestInternet(unittest.TestCase):
 
@@ -95,3 +95,29 @@ class TestInternet(unittest.TestCase):
         ch.setServiceParent(c)
         self.assertEqual(c.getServiceNamed("lala"), ch)
         ch.disownServiceParent()
+
+class Dummy:
+    processName = None
+    
+
+class TestConvert(unittest.TestCase):
+
+    def testSimpleInternet(self):
+        s = "(dp0\nS'udpConnectors'\np1\n(lp2\nsS'unixConnectors'\np3\n(lp4\nsS'twisted.internet.app.Application.persistenceVersion'\np5\nI12\nsS'name'\np6\nS'web'\np7\nsS'sslConnectors'\np8\n(lp9\nsS'sslPorts'\np10\n(lp11\nsS'tcpPorts'\np12\n(lp13\n(I8080\n(itwisted.web.server\nSite\np14\n(dp16\nS'resource'\np17\n(itwisted.web.test\nTest\np18\n(dp19\nS'files'\np20\n(lp21\nsS'paths'\np22\n(dp23\nsS'tmpl'\np24\n(lp25\nS'\\n    Congratulations, twisted.web appears to work!\\n    <ul>\\n    <li>Funky Form:\\n    '\np26\naS'self.funkyForm()'\np27\naS'\\n    <li>Exception Handling:\\n    '\np28\naS'self.raiseHell()'\np29\naS'\\n    </ul>\\n    '\np30\nasS'widgets'\np31\n(dp32\nsS'variables'\np33\n(dp34\nsS'modules'\np35\n(lp36\nsS'children'\np37\n(dp38\nsbsS'logPath'\np39\nNsS'timeOut'\np40\nI43200\nsS'sessions'\np41\n(dp42\nsbI5\nS''\np43\ntp44\nasS'unixPorts'\np45\n(lp46\nsS'services'\np47\n(dp48\nsS'gid'\np49\nI1000\nsS'tcpConnectors'\np50\n(lp51\nsS'extraConnectors'\np52\n(lp53\nsS'udpPorts'\np54\n(lp55\nsS'extraPorts'\np56\n(lp57\nsS'persistStyle'\np58\nS'pickle'\np59\nsS'uid'\np60\nI1000\ns."
+        d = pickle.loads(s)
+        a = Dummy()
+        a.__dict__ = d
+        appl = compat.convert(a)
+        self.assertEqual(service.IProcess(appl).uid, 1000)
+        self.assertEqual(service.IProcess(appl).gid, 1000)
+        self.assertEqual(service.IService(appl).name, "web")
+        services = list(service.IServiceCollection(appl))
+        self.assertEqual(len(services), 1)
+        s = services[0]
+        self.assertEqual(s.parent, service.IServiceCollection(appl))
+        self.assert_(s.privileged)
+        self.assert_(isinstance(s, internet.TCPServer))
+        args = s.args
+        self.assertEqual(args[0], 8080)
+        args[1].resource.template
+        self.assertEqual(args[3], '')
