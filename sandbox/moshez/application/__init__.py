b2b2cd24e421b0ce7109ff01d94d4a5e6bc4f96b
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-08 14:28:17 +0000

    Docstringify __init__
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8123 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/moshez/application/__init__.py
+++ sandbox/moshez/application/__init__.py
@@ -14,3 +14,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+"""
+Configuration objects for Twisted Applications
+"""
