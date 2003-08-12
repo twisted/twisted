d9bf9403f9ba7e1a025f2e5634564849e036b4bb
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-12 14:41:43 +0000

    mention RFC
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8178 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/sip.py
+++ twisted/protocols/sip.py
@@ -16,7 +16,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-"""Session Initialization Protocol."""
+"""Session Initialization Protocol.
+
+Documented in RFC 2543.
+"""
 
 # system imports
 import socket, time
