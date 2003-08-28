9aa7c675874987874b7b1cd548d8b0735b677e52
cce <cce@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-28 02:11:43 +0000

    sandbox: update flow "webserver" example
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8333 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/cce/flow/webserv.py
+++ sandbox/cce/flow/webserv.py
@@ -1,153 +1,39 @@
-# Why flow?
-#
-#   The flow module provides a mechanism for delivering results
-#   incrementally and long computations in a manner which 
-#   cooperates with other simoutaneous requests.
-#
-#   This is an example, two data sources (as generators, but
-#   they really could be any Python 2.1 iterable producer), one
-#   of which simulates a long-computation which is made to 
-#   cooperate with other requests, and the other which blocks,
-#   perhaps simulating a database result that delivers content
-#   in small chunks as it pages through the query results.
-#
-#   Using these two data sources is a simple HTML web page,
-#   which weaves the data together onto a single output
-#   stream, via request.write  
-#
-
 from __future__ import generators
 from twisted.internet import reactor
 from twisted.web import server, resource
-from twisted.python import util
 from twisted.flow import flow
-from twisted.flow.threads import Threaded
-
-def cooperative():
-    """ simulate a cooperative resource, that doesn't block """
-    from random import random
-    while 1:
-        val = random()
-        yield flow.Cooperate(val/5)
-        yield str(val)[-5:]
-
-def blocking():
-    """ simulate a blocking database resource """
-    from time import sleep
-    sleep(1)
-    yield (3,'AL','Alabama')
-    yield (4,'AK','Alaska')
-    yield (6,'AZ','Arizona')
-    yield (7,'AR','Arkansas')
-    yield (8,'CA','California')
-    yield (9,'CO','Colorado')
-    yield (1,'CT','Connecticut')
-    sleep(1)
-    yield (1,'DE','Delaware')
-    yield (2,'DC','District of Columbia')
-    yield (4,'FL','Florida')
-    yield (5,'GA','Georgia')
-    yield (7,'HI','Hawaii')
-    yield (8,'ID','Idaho')
-    yield (9,'IL','Illinois')
-    yield (3,'IN','Indiana')
-    yield (1,'IA','Iowa')
-    yield (2,'KS','Kansas')
-    yield (3,'KY','Kentucky')
-    sleep(1)
-    yield (4,'LA','Louisiana')
-    yield (5,'ME','Maine')
-    yield (7,'MD','Maryland')
-    yield (8,'MA','Massachusetts')
-    yield (9,'MI','Michigan')
-    yield (0,'MN','Minnesota')
-    yield (1,'MS','Mississippi')
-    yield (2,'MO','Missouri')
-    yield (3,'MT','Montana')
-    yield (4,'NE','Nebraska')
-    yield (5,'NV','Nevada')
-    yield (6,'NH','New Hampshire')
-    sleep(1)
-    yield (7,'NJ','New Jersey')
-    yield (8,'NM','New Mexico')
-    yield (9,'NY','New York')
-    yield (0,'NC','North Carolina')
-    yield (1,'ND','North Dakota')
-    yield (3,'OH','Ohio')
-    yield (4,'OK','Oklahoma')
-    yield (5,'OR','Oregon')
-    sleep(1)
-    yield (7,'PA','Pennsylvania')
-    yield (9,'RI','Rhode Island')
-    yield (0,'SC','South Carolina')
-    yield (1,'SD','South Dakota')
-    yield (2,'TN','Tennessee')
-    yield (3,'TX','Texas')
-    yield (4,'UT','Utah')
-    yield (5,'VT','Vermont')
-    yield (7,'VA','Virginia')
-    yield (8,'WA','Washington')
-    yield (9,'WV','West Virginia')
-    yield (3,'WI','Wisconsin')
-    yield (8,'WY','Wyoming')
 
 def render(req):
     req.write("""
        <html>
          <head>
-           <title>Icremental Webpage</title></head>
+           <title>Delayed Webpage</title></head>
          <body>
-           <h1>Incremental Webpage</h1>
-           <table>
-             <tr>
-                <td>State</td>
-                <td>Zipcodes</td>
-             <tr>
+           <h1>Delayed Webpage</h1>
     """)
-    zips   = flow.wrap(cooperative())
-    states = flow.wrap(Threaded(blocking()))
-    yield states
-    for cnt, abbr, state in states:
-        req.write("""
-            <tr><td>%s</td><td>%s</td>
-                <td>
-        """ % (abbr, state))
-        for x in range(cnt):
-            yield zips; 
-            if x: req.write(", ")
-            for result in zips.results:
-               req.write(result)
-               zips.results = []
-        req.write("""
-            </td></tr>
-        """)
-        yield states
-    req.finish()
+    yield flow.Cooperate(5)
+    req.write("""
+           <p>Delayed content</p>
+         </body>
+        </html>
+    """)
 
 class FlowResource(resource.Resource):
+    def __init__(self, gen):
+        resource.Resource.__init__(self)
+        self.gen = gen
     def isLeaf(self): return true
     def render(self, req):
-        self.d = flow.Deferred(render(req))
+        self.d = flow.Deferred(self.gen(req))
+        self.d.addCallback(lambda _: req.finish())
         return server.NOT_DONE_YET
 
-# compute a factorial just for fun
-def fact(n):
-    acc = 1
-    while n > 1:
-        acc = acc*n
-        n -= 1
-        if n % 3: 
-           yield flow.Cooperate()
-    yield acc
-d = flow.Deferred(fact(10))
-d.addCallback(util.println)
-for x in range(3):
-    reactor.iterate()
-
-if __name__=='__main__':
-    print "visit http://localhost:8081/ to view the example"
-    root = FlowResource()
+def run(gen):
+    print "visit http://localhost:8081/ to view"
+    root = FlowResource(gen)
     site = server.Site(root)
     reactor.listenTCP(8081,site)
     reactor.run()
 
+if __name__=='__main__':
+    run(render)
