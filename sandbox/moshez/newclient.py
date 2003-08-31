e194e52fde2071f4836bd38ae685b7b09a885844
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-08-31 04:14:32 +0000

    new client architecture
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8361 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ sandbox/moshez/newclient.py
@@ -0,0 +1,255 @@
+"""
+Example usage:
+o = opener()
+o.open("http://www.yahoo.com/").addCallback(readData).addCallback(util.println)
+o.open("http://www.yahoo.com/").addCallback(download(sys.stdout.write))
+d = o.open("http://www.yahoo.com/")
+d.addCallback(download(file("yahoo.html", 'wb'))
+d.addCallback(close)
+"""
+from twisted.internet import protocol
+from twisted.protocols import http
+import urllib2, urlparse
+
+class Request(urllib2.Request):
+
+    def __init__(self, url, data, headers, visited=None):
+        urllib2.Request.__init__(url, data, headers)
+        if visited is None:
+            visited = {}
+        self.visited = visited
+
+
+class Response:
+
+    def __init__(self, version, status, message):
+        self.version = version
+        self.status = status
+        self.message = message
+        self._events = []
+
+    def handleHeader(self, key, value):
+        key = key.lower()
+        l = self.headers[key] = self.headers.get(key, [])
+        l.append(value)
+
+    def setHandler(self, dataReceived, connectionLost, dataDone):
+        self.dataReceived = dataReceived
+        self.connectionLost = connectionLost
+        self.dataDone = dataDone
+        for event in self._events:
+            getattr(self, event[0])(*event[1:])
+        del self._events
+
+    def dataReceived(self, data):
+        self._events.append(('dataReceived', data))
+    def connectionLost(self, reason):
+        self._events.append(('connectionLost', reason))
+    def dataDone(self):
+        self._events.append(('dataDone',))
+
+class HTTPClient(http.HTTPClient):
+
+    def connectionMade(self):
+        self.factory.connection.callback(fobj)
+
+    def setResponseSink(self, deferred):
+        self.sink = deferred
+
+    def handleStatus(self, version, status, message):
+        self.response = Response(version, status, message)
+        self.handleHeader = self.response.handleHeader
+
+    def handleEndHeaders(self):
+        self.sink.callback(self.response)
+        def _(reason):
+            HTTPClient.connectionLost(self, reason)
+            if not self.done:
+                self.response.connectionLost(reason)
+        self.connectionLost = _
+
+    def connectionLost(self, reason):
+        HTTPClient.connectionLost(self, reason)
+        self.sink.errback(reason)
+
+    def handleResponsePart(self, data):
+        self.response.dataReceived(data)
+
+    def handleResponseEnd(self):
+        self.done = 1
+        self.response.dataDone()
+
+class ClientFactory(protocol.ClientFactory):
+    protocol = HTTPClient
+    def __init__(self):
+        self.connection = defer.Deferred
+    def connectionFailed(self, _, reason):
+        self.connection.errback(reason)
+
+class NoHandler(LookupError):
+    pass
+
+def sendHeaders(connection, req):
+    if req.has_data():
+        data = req.get_data()
+        connection.sendCommand('POST', requ.get_selector())
+        if not req.headers.has_key('Content-type'):
+            connection.sendHeader('Content-type',
+                                  'application/x-www-form-urlencoded')
+        if not req.headers.has_key('Content-length'):
+            connection.sendHeader('Content-length', str(len(data)))
+    else:
+        connection.sendCommand('GET', req.get_selector())
+    connection.sendHeader('Host', req.get_host())
+    for el in req.headers.iteritems():
+        connection.sendHeader(*el)
+    connection.endHeaders()
+    if req.has_data():
+        connection.transport.write(data)
+    return connection
+
+class Opener:
+
+    def __init__(self, *handlers):
+        self.handlers = list(handlers)
+        for handler in self.handlers:
+            handler.setOpener(self)
+
+    def callMethod(self, name, *args, **kw):
+        value = None
+        for handler in self.handlers:
+            value = getattr(handler, name, lambda *args,**kw:NEXT)(*args, **kw)
+            if value is not None:
+                break
+        if value is None:
+            raise NoHandler("No handlers found for method %s" % name)
+        return value
+
+    def open(self, request, factory=None):
+        try:
+            self.callMethod('transform', request)
+        except NoHandler:
+            pass
+        factory = ClientFactory()
+        self.callMethod('connect_'+request.type, request, factory)
+        d = factory.connection
+        d.addCallback(sendHeaders, request)
+        def _(connection):
+            d = defer.Deferred()
+            connection.setResponseSink(d)
+            return d
+        d.addCallback(_)
+        def _(response):
+            try:
+                response = self.callMethod('response_%s' % response.status,
+                                           response, request)
+            except NoHandler:
+                response = self.callMethod('responseDefault', response, request)
+            return response
+        d.addCallback(_)
+        return d
+
+class BaseHandler:
+
+    def setOpener(self, parent):
+        self.parent = parent
+
+class BaseHTTPHandler(BaseHandler):
+
+    def connect_http(self, request, factory):
+        host, port = request.get_host(), request.get_port()
+        reactor.connectTCP(host, port, factory)
+        return 1 # handled
+
+    def connect_https(self, request, factory):
+        host, port = request.get_host(), request.get_port()
+        reactor.connectSSL(host, port, factory)
+        return 1 # handled
+
+    def responseDefault(self, response, request):
+        raise response
+
+    def response_200(self, response, request):
+        return response
+
+class HTTPRedirect(BaseHandler):
+
+    def response_301(self, response, req):
+        h = response.headers
+        url = (h.get('location') or h.get('uri') or [None])[0]
+        if url is None:
+            raise response
+        url = urlparse.urljoin(req.get_full_url(), url)
+        if len(req.visited)>10 or url in req.visited:
+            raise ValueError("redirection loop detected", url)
+        return self.parent.open(Request(url, req.get_data(),
+                                        req.headers, req.visited))
+
+    response_307 = response_302 = response_301
+
+    def response_303(self, response, req):
+        req.add_data(None)
+        return response_302(self, response, req)
+
+
+class ProxyHandler(BaseHandler):
+
+    def __init__(self, proxies=None):
+        if proxies is None:
+            proxies = urllib2.getproxies()
+        self.proxies = proxies
+
+    def connect_http(self, request, factory):
+        origType = request.get_type()
+        proxy = self.proxies.get(origType)
+        if not proxy:
+            return
+        type, url = urllib2.splittype(proxy)
+        host, _ = urllib2.splithost(url)
+        if '@' in host:
+            user, host = host.split('@', 1)
+            user = base64.encodestring(unquote(user_pass)).strip()
+            request.add_header('Proxy-Authorization', 'Basic '+user)
+        host = unquote(host)
+        request.set_proxy(host, type)
+        if origType == type:
+            return
+        else:
+            return self.callMethod('connect_'+request.type, request, factory)
+
+    connect_https = connect_http
+
+def opener():
+    return Opener(ProxyHandler(), BaseHTTPHandler(), HTTPRedirect())
+
+def gatherResults(response):
+    d, l = defer.Deferred(), []
+    response.setHandler(
+        dataReceived=l.append
+        dataDone=lambda:d.callback(''.join(l))
+        connectionLost=d.errback
+    )
+    return d
+
+def download(fp):
+    def _(response):
+        d = defer.Deferred()
+        response.setHandler(
+            dataReceived=fp.write
+            dataDone=lambda:d.callback(fp)
+            connectionLost=d.errback
+        )
+        return processDeferred(d)
+    return _
+
+def close(fp):
+    fp.close()
+
+def urlopen(url, data=None):
+    return opener().open(Request(url, data))
+
+def urlretrieve(name, url, data=None):
+    d = opener().open(Request(url, data))
+    d.addCallback(download(file(name, 'wb')))
+    d.addCallback(close)
+    return d
