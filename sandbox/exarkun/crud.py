6e27126ba5bcc9ea56140f536b45503cc65708a4
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-09-27 23:36:20 +0000

    another RESP implementation, this one monolithic
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8661 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/crud.py
+++ sandbox/exarkun/crud.py
@@ -2,15 +2,44 @@
 
 import md5
 
+##
+correct = '8cf8b637395da8475d65aaf45e4cfad5'
 user = "exarkun"
 passwd = "password"
 realm = "intarweb.us"
 nonce = "abcdefg"
 qop = "auth"
 method = "REGISTER"
-uri = "sip:exarkun@intarweb.us"
+uri = "sip:intarweb.us"
+nc = None
+cnonce = None
+##
 
-nc = cnonce = None
+##
+correct = "d388dad90d4bbd760a152321f2143af7"
+method = "LOGIN"
+user = "chris"
+passwd = "secret"
+realm = "elwood.innosoft.com"
+nonce = "OA6MG9tEQGm2hh"
+nc = "00000001"
+cnonce = "OA6MHXh6VqTrRk"
+uri = "imap/elwood.innosoft.com"
+qop = "auth"
+##
+
+##
+correct = ''
+method = "GET"
+user = "Mufasa"
+passwd = "Circle Of Life"
+realm = "testrealm@host.com"
+nonce = "dcd98b7102dd2f0e8b11d0f600bfb0c093"
+nc = "00000001"
+cnonce = "0a4f113b"
+uri = "/dir/index.html"
+qop = "auth"
+##
 
 def H(s):       
     return md5.md5(s).digest()
@@ -32,16 +61,36 @@ def A1(user, realm, passwd, nonce, cnonce):
 def A2(uri, method):    
     return method + ":" + uri
 
-def RESP(a1, nonce, nc, cnonce, qop, a2):
+def RESP(user, realm, pwd, nonce, cnonce, nc, qop, uri, method):
+    a1 = H(A1(user, realm, pwd, nonce, cnonce))
+    a2 = HEX(H(A2(uri, method)))
+
     if qop == "auth":
-        return HEX(KD(H(a1), JOIN(nonce, HEX(H(a2)))))
+        return HEX(KD(a1, JOIN(nonce, a2)))
     else:
-        return HEX(KD(H(a1), JOIN(nonce, nc, cnonce, qop, HEX(H(a2)))))
+        return HEX(KD(a1, JOIN(nonce, nc, cnonce, qop, a2)))
+
+def RESP(user, realm, pwd, nonce, cnonce, nc, qop, uri, method, algo='md5', bodyHash=''):
+    front = md5.md5("%s:%s:%s" % (user, realm, pwd)).digest()
+    if algo == "md5-sess":
+        front = md5.md5("%s:%s:%s" % (front, nonce, cnonce)).hexdigest()
+    
+    back = "%s:%s" % (method, uri)
+    if qop == "auth-int":
+        back = "%s:%s" % (back, bodyHash)
+    back = md5.md5(back).digest()
+    
+    response = "%s:%s" % (front, nonce)
+    if qop:
+        response = "%s:%s:%s:%s" % (response, nc, cnonce, qop)
+    response = "%s:%s" % (response, back)
+    response = md5.md5(response).hexdigest()
+    return response
+        
 
 def g():
-    return RESP(A1(user, realm, passwd, nonce, cnonce), nonce, nc, cnonce, qop, A2(uri, method))
+    return RESP(user, realm, passwd, nonce, cnonce, nc, qop, uri, method)
 
-correct = '8cf8b637395da8475d65aaf45e4cfad5'
 
 v = g()
 print v == correct
