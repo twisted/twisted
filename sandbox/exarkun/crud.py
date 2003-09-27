3ea14a73e3a86cfc02db53fffcd502f33e8f35c5
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-09-27 14:46:19 +0000

    crud
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8653 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/crud.py
+++ sandbox/exarkun/crud.py
@@ -10,31 +10,40 @@ qop = "auth"
 method = "REGISTER"
 uri = "sip:exarkun@intarweb.us"
 
-nc = cnonce = ''
+nc = cnonce = None
 
 def H(s):       
     return md5.md5(s).digest()
 
 def KD(k, s):   
-    return H(k + ":" + s)
+    return H(JOIN(k, s))
 
 def HEX(s):     
     return s.encode('hex')
 
+def JOIN(*a):
+    return ":".join(a)
+
 def A1(user, realm, passwd, nonce, cnonce):
-    return H(user + ":" + realm + ":" + passwd) + ":" + nonce
-    return H(user + ":" + realm + ":" + passwd) + ":" + nonce + ":" + cnonce
+    if cnonce:
+        return JOIN(H(JOIN(user, realm, passwd)), nonce, cnonce)
+    return JOIN(H(JOIN(user, realm, passwd)), nonce)
 
 def A2(uri, method):    
     return method + ":" + uri
 
 def RESP(a1, nonce, nc, cnonce, qop, a2):
-    return HEX(KD(H(a1), nonce + ":" + qop + ":" + HEX(H(a2))))
-    return HEX(KD(H(a1), nonce + ":" + nc + ":" + cnonce + ":" + qop + ":" + HEX(H(a2))))
+    if qop == "auth":
+        return HEX(KD(H(a1), JOIN(nonce, HEX(H(a2)))))
+    else:
+        return HEX(KD(H(a1), JOIN(nonce, nc, cnonce, qop, HEX(H(a2)))))
 
 def g():
     return RESP(A1(user, realm, passwd, nonce, cnonce), nonce, nc, cnonce, qop, A2(uri, method))
 
 correct = '8cf8b637395da8475d65aaf45e4cfad5'
 
-print g(), correct, g() == correct
+v = g()
+print v == correct
+print correct
+print v
