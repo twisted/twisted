ab676ef69a3658bf3755af9584c9035d516e9beb
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-09-29 21:19:05 +0000

    Typo in docstring
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8703 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/cred/credentials.py
+++ twisted/cred/credentials.py
@@ -54,7 +54,7 @@ class IUsernameHashedPassword(ICredentials):
         """Validate these credentials against the correct password.
 
         @param password: The correct, plaintext password against which to
-        @check.
+        check.
 
         @return: a deferred which becomes, or a boolean indicating if the
         password matches.
