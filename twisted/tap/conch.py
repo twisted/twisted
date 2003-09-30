a69a8702d4000f6bd828a646a8f7fe25360d43de
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-09-30 08:58:13 +0000

    Port to strports
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8714 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/tap/conch.py
+++ twisted/tap/conch.py
@@ -37,19 +37,20 @@ I am a support module for making SSH servers with mktap.
 from twisted.conch import checkers, realm
 from twisted.conch.ssh import factory
 from twisted.cred import portal
-from twisted.python import reflect, usage
-from twisted.application import internet
-import sys
+from twisted.python import usage
+from twisted.application import strports
 
 
 class Options(usage.Options):
     synopsis = "Usage: mktap conch [-i <interface>] [-p <port>] [-d <dir>] "
-    optParameters = [["interface", "i", "", "local interface to which we listen"],
-                  ["port", "p", 5822, "Port on which to listen"],
-                  ["data", "d", "/etc", "directory to look for host keys in"],
-                  ["moduli", "", None, "directory to look for moduli in (if different from --data)"]]
-
     longdesc = "Makes a Conch SSH server.."
+    optParameters = [
+         ["interface", "i", "", "local interface to which we listen"],
+         ["port", "p", "5822", "Port on which to listen"],
+         ["data", "d", "/etc", "directory to look for host keys in"],
+         ["moduli", "", None, "directory to look for moduli in "
+                              "(if different from --data)"]
+    ]
 
 
 def makeService(config):
@@ -61,5 +62,8 @@ def makeService(config):
     #    t.portal.registerChecker(checkers.PluggableAuthenticationModulesChecker())
     t.dataRoot = config['data']
     t.moduliRoot = config['moduli'] or config['data']
-    portno = int(config['port'])
-    return internet.TCPServer(portno, t, interface=config['interface'])
+    port = config['port']
+    if config['interface']:
+        # Add warning here
+        port += ':interface='+config['interface']
+    return strports.service(port, t)
