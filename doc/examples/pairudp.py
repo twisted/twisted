5389f051ba810f426f3197d47d0d5635b2a926e6
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-02 06:15:31 +0000

    Add a pair example, to close issue 292
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8746 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/examples/pairudp.py
@@ -0,0 +1,18 @@
+from twisted.internet import reactor, protocol
+from twisted.protocols import ethernet, ip, rawudp
+from twisted.pair import tuntap
+
+class MyProto(protocol.DatagramProtocol):
+    def datagramReceived(self, *a, **kw):
+        print a, kw
+
+p_udp = rawudp.RawUDPProtocol()
+p_udp.addProto(42, MyProto())
+p_ip = ip.IPProtocol()
+p_ip.addProto(17, p_udp)
+p_eth = ethernet.EthernetProtocol()
+p_eth.addProto(0x800, p_ip)
+
+reactor.listenWith(tuntap.TuntapPort,
+                   interface='tap0', proto=p_eth, reactor=reactor)
+reactor.run()
