05bcd32a148c5417fa6a67d2bb37d40320fb7256
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-08 23:00:48 +0000

    todo
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8822 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_pb.py
+++ twisted/test/test_pb.py
@@ -16,6 +16,9 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 """Tests for Perspective Broker module.
+
+TODO: update protocol level tests to use new connection API, leaving
+only specific tests for old API.
 """
 
 import sys, os
