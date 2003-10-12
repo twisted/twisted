80982c8c34245376b7fd2ba470bcb8f3c399fbe6
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-12 16:12:42 +0000

    don't clutter docstrings with irrelevant info
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8895 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/cred/checkers.py
+++ twisted/cred/checkers.py
@@ -35,18 +35,20 @@ class ICredentialsChecker(components.Interface):
         @return: a Deferred which will fire a string which identifies an
         avatar, an empty tuple to specify an authenticated anonymous user
         (provided as checkers.ANONYMOUS) or fire a Failure(UnauthorizedLogin).
-        A note on anonymity - We do not want None as the value for anonymous
-        because it is too easy to accidentally return it.  We do not want the
-        empty string, because it is too easy to mistype a password file.  For
-        example, an .htpasswd file may contain the lines: ['hello:asdf',
-        'world:asdf', 'goodbye', ':world'].  This misconfiguration will have an
-        ill effect in any case, but accidentally granting anonymous access is a
-        worse failure mode than simply granting access to an untypeable
-        username.  We do not want an instance of 'object', because that would
-        create potential problems with persistence.
         """
+        
+# A note on anonymity - We do not want None as the value for anonymous
+# because it is too easy to accidentally return it.  We do not want the
+# empty string, because it is too easy to mistype a password file.  For
+# example, an .htpasswd file may contain the lines: ['hello:asdf',
+# 'world:asdf', 'goodbye', ':world'].  This misconfiguration will have an
+# ill effect in any case, but accidentally granting anonymous access is a
+# worse failure mode than simply granting access to an untypeable
+# username.  We do not want an instance of 'object', because that would
+# create potential problems with persistence.
 
 ANONYMOUS = ()
+
 
 class AllowAnonymousAccess:
     __implements__ = ICredentialsChecker
@@ -54,6 +56,7 @@ class AllowAnonymousAccess:
 
     def requestAvatarId(self, credentials):
         return defer.succeed(ANONYMOUS)
+
 
 class InMemoryUsernamePasswordDatabaseDontUse:
     __implements__ = ICredentialsChecker
