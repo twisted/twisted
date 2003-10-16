da517c663a53e92e12a1e8ffd1365026a31e777c
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-16 15:53:45 +0000

    Explain how to override the login form.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8965 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/howto/listings/guard/login.py
+++ doc/howto/listings/guard/login.py
@@ -1,42 +1,13 @@
-"""Root of forms."""
+from twisted.web.woven import simpleguard, page, guard
+from twisted.web import resource, util, microdom
+from twisted.cred import checkers, portal
 
-from twisted.web.woven import guard, page
-from twisted.web import resource, static, util, microdom
-from twisted.cred import checkers, portal, credentials
+class Authenticated(page.Page):
 
+    template='<html>Hello <span model="name"/>!</html>'
 
-class User:
-    """User info for current person."""
-    
-    def __init__(self, key):
-        self.key = key
-
-
-class RootPage(resource.Resource):
-    """Root page users see when logged in."""
-    
-    def __init__(self, user):
-        resource.Resource.__init__(self)
-        self.user = user
-        
-    def render(self, request):
-        return "Hello, %s!" % self.user.key
-
-
-class UserRealm:
-    """Realm for user pages."""
-    
-    def __init__(self, anonymousResource):
-        self.service = service
-        self.anonymousResource = anonymousResource
-    
-    def requestAvatar(self, avatarID, mind, interface):
-        if avatarID == checkers.ANONYMOUS:
-            return (resource.IResource, self.anonymousResource, lambda: None)
-        else:
-            user = User(avatarID)
-            return (resource.IResource, RootPage(user), lambda: None)
-
+    def wmfactory_name(self, request):
+        return request.getComponent(simpleguard.Authenticated).name
 
 class LoginPage(page.Page):
     """This is the page that is shown to non-logged in users."""
@@ -77,7 +48,8 @@ class LoginPage(page.Page):
         self.formModel = formModel
 
     def wvupdate_loginform(self, request, widget, model):
-        microdom.lmx(widget.node).form(action=guard.INIT_PERSPECTIVE, model="form")
+        microdom.lmx(widget.node).form(action=guard.INIT_PERSPECTIVE,
+                                       model="form")
 
     def wmfactory_form(self, request):
         if self.formModel:
@@ -90,11 +62,8 @@ def callback(_):
     return util.Redirect(".")
 
 def buildGuardedResource():
-    """Build guarded resource for the site."""
-    myPortal = portal.Portal(UserRealm(LoginPage()))
-    # hardcoded to single username/password - admin/password
-    c = checkers.InMemoryUsernamePasswordDatabaseDontUse(admin="password")
-    myPortal.registerChecker(c)
-    myPortal.registerChecker(checkers.AllowAnonymousAccess(), credentials.IAnonymous)
-    un = guard.UsernamePasswordWrapper(myPortal, callback=callback, errback=LoginPage)
-    return guard.SessionWrapper(un)
+    return simpleguard.guardResource(
+               Authenticated(),
+               [checkers.InMemoryUsernamePasswordDatabaseDontUse(bob="12345")],
+               nonauthenticated=LoginPage(),
+               callback=callback, errback=LoginPage)
