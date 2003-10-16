39d832015e904d5be48a7d27c9ca82e2fd2867c3
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-16 21:51:00 +0000

    Finished newcredifying PB docs (closes issue172).
    Added examples, removed confusing pb.py-internals walkthrough. Explain
    Avatars and common use patterns. Replace old jokes with entirely new ones :)
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@8981 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/howto/listings/pb/chatclient.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+
+from twisted.spread import pb
+from twisted.internet import reactor
+from twisted.cred import credentials
+
+class Client(pb.Referenceable):
+
+    def remote_print(self, message):
+        print message
+
+    def connect(self):
+        factory = pb.PBClientFactory()
+        reactor.connectTCP("localhost", 8800, factory)
+        def1 = factory.login(credentials.UsernamePassword("alice", "1234"),
+                             client=self)
+        def1.addCallback(self.connected)
+        reactor.run()
+
+    def connected(self, perspective):
+        print "connected, joining group #lookingForFourth"
+        # this perspective is a reference to our User object
+        d = perspective.callRemote("joinGroup", "#lookingForFourth")
+        d.addCallback(self.gotGroup)
+
+    def gotGroup(self, group):
+        print "joined group, now sending a message to all members"
+        # 'group' is a reference to the Group object (through a ViewPoint)
+        d = group.callRemote("send", "You can call me Al.")
+        d.addCallback(self.shutdown)
+
+    def shutdown(self, result):
+        reactor.stop()
+
+
+Client().connect()
+
---
+++ doc/howto/listings/pb/chatserver.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python
+
+from twisted.cred import portal, checkers
+from twisted.spread import pb
+from twisted.internet import reactor
+
+class ChatServer:
+    def __init__(self):
+        self.groups = {} # indexed by name
+
+    def joinGroup(self, groupname, user, allowMattress):
+        if not self.groups.has_key(groupname):
+            self.groups[groupname] = Group(groupname, allowMattress)
+        self.groups[groupname].addUser(user)
+        return self.groups[groupname]
+
+class ChatRealm:
+    __implements__ = portal.IRealm
+    def requestAvatar(self, avatarID, mind, *interfaces):
+        assert pb.IPerspective in interfaces
+        avatar = User(avatarID)
+        avatar.server = self.server
+        avatar.attached(mind)
+        return pb.IPerspective, avatar, lambda a=avatar:a.detached(mind)
+
+class User(pb.Avatar):
+    def __init__(self, name):
+        self.name = name
+    def attached(self, mind):
+        self.remote = mind
+    def detached(self, mind):
+        self.remote = None
+    def perspective_joinGroup(self, groupname, allowMattress=True):
+        return self.server.joinGroup(groupname, self, allowMattress)
+    def send(self, message):
+        self.remote.callRemote("print", message)
+
+class Group(pb.Viewable):
+    def __init__(self, groupname, allowMattress):
+        self.name = groupname
+        self.allowMattress = allowMattress
+        self.users = []
+    def addUser(self, user):
+        self.users.append(user)
+    def view_send(self, from_user, message):
+        if not self.allowMattress and message.find("mattress") != -1:
+            raise ValueError, "Don't say that word"
+        for user in self.users:
+            user.send("<%s> says: %s" % (from_user.name, message))
+
+realm = ChatRealm()
+realm.server = ChatServer()
+checker = checkers.InMemoryUsernamePasswordDatabaseDontUse()
+checker.addUser("alice", "1234")
+checker.addUser("bob", "secret")
+checker.addUser("carol", "fido")
+p = portal.Portal(realm, [checker])
+
+reactor.listenTCP(8800, pb.PBServerFactory(p))
+reactor.run()
