e34ee33c630ac6d31d5dcb56bd4e5814b3436dea
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-19 08:17:36 +0000

    Removing stuff for which copyright isn't assigned to glyph
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9039 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/Cocoa/WebServicesTool/Main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyObjCTools import AppHelper
-
-# import classes required to start application
-import WSTApplicationDelegateClass
-import WSTConnectionWindowControllerClass
-
-# pass control to the AppKit
-AppHelper.runEventLoop()
