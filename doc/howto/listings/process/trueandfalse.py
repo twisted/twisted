1d77215732fc614f1b84c3e88ae75b7f0e3116a5
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-10-23 18:33:43 +0000

    make simple-process example a bit more meaningful
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9100 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/howto/listings/process/trueandfalse.py
+++ doc/howto/listings/process/trueandfalse.py
@@ -1,14 +1,14 @@
 from twisted.internet import utils, reactor
 
 def printTrueValue(val):
-    print val
-    output = utils.getProcessValue('false')
+    print "/bin/true exits with rc=%d" % val
+    output = utils.getProcessValue('/bin/false')
     output.addCallback(printFalseValue)
 
 def printFalseValue(val):
-    print val
+    print "/bin/false exits with rc=%d" % val
     reactor.stop()
 
-output = utils.getProcessValue('true')
+output = utils.getProcessValue('/bin/true')
 output.addCallback(printTrueValue)
 reactor.run()
