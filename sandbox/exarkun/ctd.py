dfde30fc8d62b8cb76c61a44ec89357986863f50
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-01 14:34:20 +0000

    crash test dummy
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9169 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ sandbox/exarkun/ctd.py
@@ -0,0 +1,15 @@
+# -*- coding: Latin-1 -*-
+
+class Class:
+    def method(self):
+        pass
+    def smethod(self):
+        pass
+    smethod = staticmethod(smethod)
+    
+    def cmethod(self):
+        pass
+    cmethod = classmethod(cmethod)
+
+def Function():
+    pass
