54c80d4d2ee743e9a855a46e0fa1ee5fac4290d3
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-01 13:57:18 +0000

    Adapters and such things
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9165 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/rebuild.py
+++ sandbox/exarkun/rebuild.py
@@ -1,26 +1,62 @@
-# -*- coding: Latin-1 -*-
+# -*- test-case-name: test_rebuild -*-
 
 from twisted.python import components
 
 class IRebuildable(components.Interface):
-    def rebuild(self):
+    def rebuild(self, memo):
         """Build a new copy of this object and return it.
         """
 
-def rebuild(obj):
-    return IRebuildable(obj).rebuild()
+def latest(klass):
+    return reflect.namedAny(reflect.qual(klass))
 
-def ImmutableObjectAdapter(original):
+def rebuild(obj):
+    return IRebuildable(obj).rebuild({})
+
+def ImmutableObjectAdapter(original, memo):
     return original
+
+class ListAdapter(components.Adapter):
+    def rebuild(self, memo):
+        if id(original) in memo:
+            return memo[id(original)]
+        r = [IRebuildable(e).rebuild(memo) for e in original]
+        memo[id(original)] = r
+        return r
 
 class _foo:
     pass
+    
+class _Cache(components.Adapter):
+    def cret(self, memo, original, result):
+        memo[id(original)] = result
+        return result
 
-def ClassicClassAdapter(original):
-    d = original.__dict__.copy()
-    for k in d.keys():
-        d[k] = IRebuildable(d[k]).rebuild()
-    f = _foo()
-    f.__class__ = reflect.namedAny(reflect.qual(original.__class__))
-    f.__dict__ = d
-    return f
+class ClassicClassAdapter(_Cache):
+    def rebuild(self, memo):
+        if id(original) in memo:
+            return memo[id(original)]
+        d = original.__dict__.copy()
+        for k in d.keys():
+            d[k] = IRebuildable(d[k]).rebuild(memo)
+        f = _foo()
+        f.__class__ = latest(original.__class__)
+        f.__dict__ = d
+        return self.cret(memo, original, f)
+
+class BoundMethodAdapter(components.Adapter):
+    def rebuild(self, memo):
+        if id(original) in memo:
+            return memo[id(original)]
+        klass = latest(original.im_class)
+        self = original.im_self
+        name = original.im_func.func_name
+        return self.cret(memo, original, getattr(IRebuildable(self).rebuild(memo), name))
+
+class UnboundMethodAdapter(components.Adapter):
+    def rebuild(self, memo):
+        if id(original) in memo:
+            return memo[id(original)]
+        klass = latest(original.im_class)
+        name = original.im_func.func_name
+        return self.cret(memo, original, getattr(latest, name))
