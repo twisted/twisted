69467920cfc2ab8b4a1f237386bc314c5a4efc7b
pahan <pahan@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-08 02:45:50 +0000

    whew
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9248 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/pahan/iocp/iocpreactor/abstract.py
+++ sandbox/pahan/iocp/iocpreactor/abstract.py
@@ -1,7 +1,10 @@
 from twisted.python import log
 from twisted.persisted import styles
+from twisted.internet import interfaces
 
 class IoHandle(log.Logger, styles.Ephemeral):
+    __implements__ = (interfaces.ITransport,)
+
     def __init__(self, reactor=None):
         if not reactor:
             from twisted.internet import reactor
