27758b9ea0fd4d6e79eecafc1b448dad7e4c1487
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-10 04:22:49 +0000

    Fear my progress.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9264 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ sandbox/exarkun/copyover/client.py
@@ -0,0 +1,146 @@
+import sys
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+import fdpass
+
+from twisted.internet import reactor
+from twisted.internet import defer
+from twisted.spread import pb
+from twisted.python import log
+from twisted.cred import credentials
+from twisted.internet import protocol
+from twisted.internet import unix
+from twisted.application import internet
+
+sys.path.append("../../pahan/sendmsg")
+from sendmsg import recvmsg
+
+class Client(unix.Client):
+    connected = True
+    
+    def connectionLost(self, reason):
+        self.connected = False
+        unix.Client.connectionLost(self, reason)
+
+    def doRead(self):
+        if not self.connected:
+            return
+        try:
+            msg, flags, ancillary = recvmsg(self.fileno())
+        except:
+            log.msg('recvmsg():')
+            log.err()
+        else:
+            buf = ancillary[0][2]
+            fds = []
+            while buf:
+                fd, buf = buf[:4], buf[4:]
+                fds.append(struct.unpack("i", fd)[0])
+            try:
+                self.protocol.fileDescriptorsReceived(fds)
+            except:
+                log.msg('protocol.fileDescriptorsReceived')
+                log.err()
+        return unix.Client.doRead(self)
+
+class Connector(unix.Connector):
+    def _makeTransport(self):
+        return Client(self.address, self, self.reactor)
+
+class UNIXClient(internet._AbstractClient):
+    def _getConnection(self):
+        print 'get connection'
+        from twisted.internet import reactor
+        return reactor.connectWith(Connector, *self.args, **self.kwargs)
+
+class _FileDescriptorUnpickler:
+    def __init__(self, fdmap):
+        self.fdmap = fdmap
+        self.fdmemo = {}
+
+    def persistent_load(self, id):
+        r = None
+        id = int(id)
+        kname, mode, id = id.split(":")
+        if id in self.fdmemo:
+            return self.fdmemo[id]
+        if kname == "file":
+            r = self.fdmemo[id] = os.fdopen(self.fdmap[id], mode)
+        elif kname == "socket":
+            r = self.fdmemo[id] = socket.fromfd(self.fdmap[id])
+        return r
+
+def FileDescriptorUnpickler(s, fdmap):
+    ph = _FileDescriptorUnpickler(fdmap)
+    p = pickle.Unpickler(s)
+    p.persistent_load = ph.persistent_load
+    return p
+
+class FileDescriptorReceivingProtocol(protocol.Protocol):
+    """
+    Must be used with L{Port} as the transport.
+    """
+    
+    def __init__(self, id, d):
+        print 'hi'
+        self.id = id
+        self.d = d
+
+    def connectionMade(self):
+        print 'fie'
+        self.transport.write("%s\r\n" % (self.id,))
+
+    def dataReceived(self, data):
+        print 'Got some random data', repr(data)
+
+    def fileDescriptorsReceived(self, fds):
+        print 'guy'
+        self.d.callback(fds)
+        self.transport.loseConnection()
+
+class FileDescriptorRequestFactory(protocol.ClientFactory):
+    protocol = FileDescriptorReceivingProtocol
+
+    def __init__(self, id, d):
+        print 'request factory'
+        self.id = id
+        self.gotFDs = d
+
+    def buildProtocol(self, addr):
+        print 'buildProtocol'
+        p = self.protocol(self.id, self.gotFDs)
+        return p
+
+class UserStateReceiver(pb.Referenceable):
+    def unproxyFileDescriptors(self, fds, state):
+        print 'unproxying'
+        s = StringIO.StringIO(state)
+        p = FileDescriptorUnpickler(s, fds)
+        return p.load()
+
+    def remote_takeUser(self, id, state, path):
+        print 'takeUser', id, state, path
+        d = defer.Deferred()
+        f = FileDescriptorRequestFactory(id, d)
+        client = UNIXClient(path, f, 60).startService()
+        d.addCallback(self.unproxyFileDescriptors, state)
+        return d
+
+def main():
+    log.startLogging(sys.stdout)
+    f = pb.PBClientFactory()
+    reactor.connectTCP("localhost", 10301, f)
+    f.login(credentials.UsernamePassword("username", "password"), UserStateReceiver())
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
---
+++ sandbox/exarkun/copyover/server.py
@@ -0,0 +1,192 @@
+import os
+import sys
+import struct
+import socket
+import tempfile
+from os.path import join as opj
+
+from types import FileType
+from socket import SocketType
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+from twisted.internet import reactor
+from twisted.internet import protocol
+from twisted.internet import abstract
+from twisted.python import log
+from twisted.spread import pb
+from twisted.cred import portal
+from twisted.cred import checkers
+from twisted.protocols import basic
+from twisted.internet import unix
+from twisted.application import internet
+
+sys.path.append("../../pahan/sendmsg")
+from sendmsg import sendmsg
+from sendmsg import SCM_RIGHTS
+
+class Server(unix.Server):
+    def sendFileDescriptors(self, fileno, data="Filler"):
+        """
+        @param fileno: An iterable of the file descriptors to pass.
+        """
+        payload = struct.pack("%di" % len(fileno), *fileno)
+        r = sendmsg(self.fileno(), data, 0, (socket.SOL_SOCKET, SCM_RIGHTS, payload))
+        return r
+
+class Port(unix.Port):
+    transport = Server
+
+class UNIXServer(internet._AbstractServer):
+    def getHost(self):
+        return self._port.getHost()
+
+    def _getPort(self):
+        from twisted.internet import reactor
+        return reactor.listenWith(Port, *self.args, **self.kwargs)
+
+class _FileDescriptorPickler:
+    def __init__(self, fdmap):
+        self.fdmap = fdmap
+
+    def __id(self, fileno):
+        self.fdmap[len(self.fdmap)] = fileno
+
+    def persistent_id(self, obj):
+        if isinstance(obj, FileType):
+            return 'file:%s:%d' % (obj.mode, self.__id(obj.fileno()))
+        elif isinstance(obj, SocketType):
+            return 'socket::%d' % (self.__id(obj.fileno()),)
+        return None
+
+def FileDescriptorPickler(s, fdmap):
+    ph = _FileDescriptorPickler(fdmap)
+    p = pickle.Pickler(s)
+    p.persistent_id = ph.persistent_id
+    return p
+
+class _FileDescriptorUnpickler:
+    def __init__(self, fdmap):
+        self.fdmap = fdmap
+        self.fdmemo = {}
+
+    def persistent_load(self, id):
+        r = None
+        id = int(id)
+        kname, mode, id = id.split(":")
+        if id in self.fdmemo:
+            return self.fdmemo[id]
+        if kname == "file":
+            r = self.fdmemo[id] = os.fdopen(self.fdmap[id], mode)
+        elif kname == "socket":
+            r = self.fdmemo[id] = socket.fromfd(self.fdmap[id])
+        return r
+
+def FileDescriptorUnpickler(s, fdmap):
+    ph = _FileDescriptorUnpickler(fdmap)
+    p = pickle.Unpickler(s)
+    p.persistent_load = ph.persistent_load
+    return p
+
+class FileDescriptorSendingProtocol(basic.LineReceiver):
+    """
+    Must be used with L{Port} as the transport.
+    """
+
+    def __init__(self, idmap):
+        self.idmap = idmap
+
+    def lineReceived(self, line):
+        try:
+            files = self.idmap[int(line)]
+        except ValueError:
+            log.msg("Peer sent us a bogus ID")
+        except KeyError:
+            log.msg("Peer sent us an unassigned ID")
+        except:
+            log.err()
+        else:
+            self.transport.sendFileDescriptors(files)
+            return
+        
+        self.transport.loseConnection()
+
+class FileDescriptorSendingFactory(protocol.ServerFactory):
+    protocol = FileDescriptorSendingProtocol
+
+    def __init__(self, idmap):
+        self.idmap = idmap
+
+    def buildProtocol(self, addr):
+        p = self.protocol(self.idmap)
+        return p
+
+class UserStateSender(pb.Avatar):
+    id = 0
+    
+    def __init__(self, client):
+        self.idmap = {}
+
+        # PB Object for talking to our peer
+        self.client = client
+        
+        self.dir = tempfile.mkdtemp()
+        self.path = tempfile.mktemp(dir=self.dir)
+        self.factory = FileDescriptorSendingFactory(self.idmap)
+        self.server = UNIXServer(opj(self.dir, self.path), self.factory)
+        self.server.startService()
+
+        reactor.callLater(1, sendSomeState, self)
+
+    def logout(self):
+        self.server.stopService()
+        # This is BUNK with a capital BUNK
+        reactor.callLater(0, os.rmdir, self.dir)
+
+    def sendUserState(self, state):
+        """Transmit the given state to this service's peer.
+        """
+        d = {}
+        s = StringIO.StringIO()
+        p = FileDescriptorPickler(s, d)
+        p.dump(state)
+        s = s.getvalue()
+        self.id += 1
+        self.idmap[self.id] = d
+        return self._sendUserStateWithID(self.id, state, opj(self.dir, self.path))
+    
+    def _sendUserStateWithID(self, id, state, path):
+        return self.client.callRemote("takeUser", id, state, path)
+
+class Realm:
+    __implements__ = (portal.IRealm,)
+
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        if pb.IPerspective not in interfaces:
+            raise NotImplementedError
+        a = UserStateSender(mind)
+        return pb.IPerspective, a, a.logout
+
+def sendSomeState(avatar):
+    avatar.sendUserState([1, 2, 3])
+
+def main():
+    log.startLogging(sys.stdout)
+    r = Realm()
+    p = portal.Portal(r)
+    c = checkers.InMemoryUsernamePasswordDatabaseDontUse(username="password")
+    p.registerChecker(c)
+    f = pb.PBServerFactory(p)
+    reactor.listenTCP(10301, f)
+    reactor.run()
+
+if __name__ == '__main__':
+    main()
