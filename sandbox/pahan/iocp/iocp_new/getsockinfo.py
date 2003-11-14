802c1a3b071ac1fbd876e49eabb60c01324e2f3b
pahan <pahan@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-14 17:12:03 +0000

    Better exec usage
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9352 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/pahan/iocp/iocp_new/getsockinfo.py
+++ sandbox/pahan/iocp/iocp_new/getsockinfo.py
@@ -1,8 +1,13 @@
 def getsockinfo(sock):
-    s = `sock`
+    s = `sock._sock`
     sp = s[1:-1].split(",")[1:]
+    g = {}
     d = {}
     for i in sp:
-        exec(i.strip(), None, d)
+        exec i.strip() in g, d
     return (d["family"], d["type"], d["protocol"])
+
+if __name__ == "__main__":
+    from socket import socket, AF_INET, SOCK_DGRAM
+    print getsockinfo(socket(AF_INET, SOCK_DGRAM))
 
