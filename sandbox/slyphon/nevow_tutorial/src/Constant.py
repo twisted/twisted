510d842e8fd759f2e2094f6be6dc8bcbce42504d
slyphon <slyphon@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-22 02:22:57 +0000

    yeah, we movin on up! (movin on up!) to a deee-lux apartment...etc, etc.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9485 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/slyphon/nevow_tutorial/src/Constant.py
+++
@@ -1,9 +0,0 @@
-DEBUG = False
-TRACE = False
-PDB = True
-
-# Pickle File Paths
-PFPath = "picklejar"
-CategoryPF = PFPath + "/category.pkl"
-MenuPF = PFPath + "/menu.pkl"
-BookmarkPF = PFPath + "/bookmark.pkl"
--- sandbox/slyphon/nevow_tutorial/src/woven-links.rpy
+++
@@ -1,5 +0,0 @@
-from HomePage import HomePage
-from storage import CSVStorage
-
-resource = HomePage(storage=CSVStorage.CSVStorage('storage/'))
-
