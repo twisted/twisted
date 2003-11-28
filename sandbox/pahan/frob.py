9f2402f86d1db07c0dad292bb7f1d1f0323face0
pahan <pahan@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-11-28 02:27:11 +0000

    Move to statefulprotocol/, rename.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9546 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/pahan/frob.py
+++
@@ -1,6 +0,0 @@
-from implicitstate import ImplicitStateProtocol as isp
-
-def f(msg):
-    print "f got:", msg
-p = isp()
-p.implicit_state = (f, 4)
--- sandbox/pahan/test_implicitstate.py
+++
@@ -1,9 +0,0 @@
-from twisted.test import test_protocols
-from implicitstate import MyInt32StringReceiver
-
-class TestInt32(test_protocols.TestMixin, MyInt32StringReceiver):
-    MAX_LENGTH = 50
-
-class Int32TestCase(test_protocols.Int32TestCase):
-    protocol = TestInt32
-
