e48b7f4fa97ca16419bae7816bb17eb9e1c1599b
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-12-05 04:54:03 +0000

    Resolver and tap support for acting as a secondary DNS
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9622 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/names/secondary.py
@@ -0,0 +1,94 @@
+# -*- coding: Latin-1 -*-
+
+from twisted.internet import task
+from twisted.protocols import dns
+from twisted.names import common
+from twisted.names import client
+from twisted.names import server
+from twisted.names import resolve
+from twisted.python import log
+from twisted.application import service
+
+class SecondaryAuthorityService(service.Service):
+    calls = None
+
+    def __init__(self, primary, domains):
+        """
+        @param primary: The IP address of the server from which to perform
+        zone transfers.
+
+        @param domains: A sequence of domain names for which to perform
+        zone transfers.
+        """
+        self.primary = primary
+        self.domains = [SecondaryAuthority(primary, d) for d in domains]
+
+    def getAuthority(self):
+        return resolve.ResolverChain(self.domains)
+
+    def startService(self):
+        service.Service.startService(self)
+        self.calls = [task.LoopingCall(d.transfer) for d in self.domains]
+        i = 0
+        from twisted.internet import reactor
+        for c in self.calls:
+            # XXX Add errbacks, respect proper timeouts
+            reactor.callLater(i, c.start, 60 * 60)
+            i += 1
+
+    def stopService(self):
+        service.Service.stopService(self)
+        for c in self.calls:
+            c.stop()
+
+class SecondaryAuthority(common.ResolverBase):
+    """An Authority that keeps itself updated by performing zone transfers"""
+
+    transferring = False
+
+    def __init__(self, primaryIP, domain):
+        common.ResolverBase.__init__(self)
+        self.primary = primaryIP
+        self.domain = domain
+
+    def transfer(self):
+        if self.transferring:
+            return
+        self.transfering = True
+
+        print 'do a transfer'
+        return client.Resolver(servers=[(self.primary, dns.PORT)]
+            ).lookupZone(self.domain
+            ).addCallback(self._cbZone
+            ).addErrback(self._ebZone
+            )
+
+    from twisted.names.authority import FileAuthority
+    _lookup = FileAuthority.__dict__['_lookup']
+
+    def _cbZone(self, zone):
+        ans, _, _ = zone
+        print 'gawt', zone
+        self.records = r = {}
+        for rec in ans:
+            if rec.type == dns.SOA:
+                self.soa = (str(rec.name).lower(), rec.payload)
+            else:
+                r.setdefault(str(rec.name).lower(), []).append(rec.payload)
+
+    def _ebZone(self, failure):
+        log.msg("Updating %s from %s failed during zone transfer" % (self.domain, self.primary))
+        log.err(failure)
+
+    def update(self):
+        print 'uperdate'
+        self.transfer().addCallbacks(self._cbTransferred, self._ebTransferred)
+
+    def _cbTransferred(self, result):
+        print 'done xferred'
+        self.transferring = False
+
+    def _ebTransferred(self, failure):
+        self.transferred = False
+        log.msg("Transferring %s from %s failed after zone transfer" % (self.domain, self.primary))
+        log.err(failure)
--- twisted/names/tap.py
+++ twisted/names/tap.py
@@ -1,15 +1,15 @@
 # Twisted, the Framework of Your Internet
 # Copyright (C) 2001 Matthew W. Lefkowitz
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of version 2.1 of the GNU Lesser General Public
 # License as published by the Free Software Foundation.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -24,7 +24,9 @@ from twisted.python import usage
 from twisted.protocols import dns
 from twisted.application import internet, service
 
-import server, authority
+from twisted.names import server
+from twisted.names import authority
+from twisted.names import secondary
 
 class Options(usage.Options):
     optParameters = [
@@ -33,7 +35,7 @@ class Options(usage.Options):
         ["resolv-conf", None, None,
             "Override location of resolv.conf (implies --recursive)"],
     ]
-    
+
     optFlags = [
         ["cache",       "c", "Enable record caching"],
         ["recursive",   "r", "Perform recursive lookups"],
@@ -43,14 +45,15 @@ class Options(usage.Options):
 
     zones = None
     zonefiles = None
-    
+
     def __init__(self):
         usage.Options.__init__(self)
         self['verbose'] = 0
         self.bindfiles = []
         self.zonefiles = []
-    
-    
+        self.secondaries = []
+
+
     def opt_pyzone(self, filename):
         """Specify the filename of a Python syntax zone definition"""
         if not os.path.exists(filename):
@@ -64,6 +67,15 @@ class Options(usage.Options):
         self.bindfiles.append(filename)
 
 
+    def opt_secondary(self, ip_domain):
+        """Act as secondary for the specified domain, performing
+        zone transfers from the specified IP (IP/domain)
+        """
+        args = ip_domain.split('/', 1)
+        if len(args) != 2:
+            raise usage.UsageError("Argument must be of the form IP/domain")
+        self.secondaries.append((args[0], [args[1]]))
+
     def opt_verbose(self):
         """Increment verbosity level"""
         self['verbose'] += 1
@@ -75,6 +87,7 @@ class Options(usage.Options):
         if self['iterative'] and self['recursive']:
             raise usage.UsageError("--iterative and --recursive are mutually exclusive.")
 
+        self.svcs = []
         self.zones = []
         for f in self.zonefiles:
             try:
@@ -88,6 +101,9 @@ class Options(usage.Options):
             except Exception, e:
                 traceback.print_exc()
                 raise usage.UsageError("Invalid syntax in " + f)
+        for f in self.secondaries:
+            self.svcs.append(secondary.SecondaryAuthorityService(*f))
+            self.zones.append(self.svcs[-1].getAuthority())
         try:
             self['port'] = int(self['port'])
         except ValueError:
@@ -110,4 +126,6 @@ def makeService(config):
     for (klass, arg) in [(internet.TCPServer, f), (internet.UDPServer, p)]:
         s = klass(config['port'], arg, interface=config['interface'])
         s.setServiceParent(ret)
+    for svc in config.svcs:
+        svc.setServiceParent(ret)
     return ret
