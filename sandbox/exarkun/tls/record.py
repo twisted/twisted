75281fdcfb6f5d31a28b163a00ad0c05c9efe678
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-12-06 21:07:36 +0000

    IMPROVE
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9630 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/tls/record.py
+++ sandbox/exarkun/tls/record.py
@@ -1,24 +1,100 @@
+# -*- test-case-name: twisted.test.test_record -*-
+
 import struct
 
+class Integer:
+    def __init__(self, bits, signed=False):
+        if signed:
+            bits -= 1
+        self.bits = bits
+        self.signed = signed
+
+    def verify(self, value):
+        if not self.signed and value < 0:
+            return False
+        return 2 ** self.bits > abs(value)
+
+    def sign(self, value):
+        return value >= 0 or self.signed
+
+    def range(self, value):
+        return 2 ** self.bits > value
+
+class Int255String:
+    pass
+
 class RecordType(type):
-    def __init__(klass, name, bases, attrs):
+    FORMAT_SPECIFIERS = {
+        Integer(8, False): 'B',
+        Integer(8, True): 'b',
+        Integer(16, False): 'H',
+        Integer(16, True): 'h',
+        Integer(32, False): 'I',
+        Integer(32, True): 'i',
+        Int255String(): 'p',
+        }
+
+    def __new__(klass, name, bases, attrs):
         if '__format__' in attrs:
             attrs['encode'] = klass.makeEncoder(attrs['__format__'])
             attrs['decode'] = klass.makeDecoder(attrs['__format__'])
         return type.__new__(klass, name, bases, attrs)
 
     def makeEncoder(klass, format):
-        attrs, fmts = zip(*format)
-        fmts = '>' + fmts
         def encode(self):
-            return struct.pack(fmts, *[getattr(self, a) for a in attrs])
+            result = []
+            subbytes = []
+            for (attr, type) in format:
+                if type in klass.FORMAT_SPECIFIERS:
+                    offset = 0
+                    accum = 0
+                    subbytes.reverse()
+                    for (v, t) in subbytes:
+                        accum |= unsignedNegation(v) << offset
+                        offset += t.bits
+                    subbytes = []
+                    for (n, fmt) in ((32, 'I'), (16, 'H'), (8, 'B')):
+                        while offset >= n:
+                            result.append(struct.pack('>' + fmt, accum & (2 ** n - 1)))
+                            offset -= n
+                            accum >>= n
+
+                    if offset:
+                        raise ValueError("Non-byte-aligned values in format")
+
+                    fmt = klass.FORMAT_SPECIFIERS[type]
+                    result.append(struct.pack('>' + fmt, getattr(self, attr)))
+                elif isinstance(type, Integer):
+                    subbytes.append((getattr(self, attr), type))
+                else:
+                    raise NotImplementedError((type, attr))
+            return ''.join(result)
         return encode
+    makeEncoder = classmethod(makeEncoder)
 
     def makeDecoder(klass, format):
-        attrs, fmts = zip(*format)
-        fmts = '>' + fmts
-        def decode(cls, s):
-            values = struct.unpack(fmts, s)
-            attrs = dict(zip(attrs, values))
-            return types.InstanceType(cls, attrs)
+        def decode(cls, bytes):
+            d = {}
+            for (attr, type) in format:
+                if type in klass.FORMAT_SPECIFIERS:
+                    fmt = klass.FORMAT_SPECIFIERS[type]
+                    size = struct.calcsize('>' + fmt)
+                    d[attr] = struct.unpack('>' + fmt, bytes[:size])[0]
+                    bytes = bytes[size:]
+                else:
+                    if type.bits % 8 == 0:
+                        # Yay, easy
+                        accum = 0
+                        for b in bytes[:type.bits % 8]:
+                            accum <<= 8
+                            accum |= ord(b)
+                        d[attr] = accum
+                        bytes = bytes[type.bits % 8:]
+                    else:
+                        raise NotImplementedError("Decoding sub-byte fields is hard")
+            return types.InstanceType(cls, d)
         return classmethod(decode)
+    makeDecoder = classmethod(makeDecoder)
+
+class Record(object):
+    __metaclass__ = RecordType
---
+++ sandbox/exarkun/tls/test_record.py
@@ -0,0 +1,77 @@
+
+import random
+
+from record import Record, Integer, Int255String
+
+class SingleSignedByte(Record):
+    __format__ = [('a', Integer(8, True))]
+    EXPECTED_ENCODED_SIZE = 1
+
+class SingleUnsignedByte(Record):
+    __format__ = [('a', Integer(8, False))]
+    EXPECTED_ENCODED_SIZE = 1
+
+class SingleSignedShort(Record):
+    __format__ = [('a', Integer(16, True))]
+    EXPECTED_ENCODED_SIZE = 2
+
+class SingleUnsignedShort(Record):
+    __format__ = [('a', Integer(16, False))]
+    EXPECTED_ENCODED_SIZE = 1
+
+class SingleSignedLong(Record):
+    __format__ = [('a', Integer(32, True))]
+    EXPECTED_ENCODED_SIZE = 4
+
+class SingleUnsignedLong(Record):
+    __format__ = [('a', Integer(32, False))]
+    EXPECTED_ENCODED_SIZE = 4
+
+class MultiByte(Record):
+    __format__ = [('a', Integer(8, True)),
+                  ('b', Integer(8, False))]
+    EXPECTED_ENCODED_SIZE = 2
+
+class MultiType(Record):
+    __format__ = [('a', Integer(16, True)),
+                  ('b', Integer(8, True))]
+    EXPECTED_ENCODED_SIZE = 3
+
+class SubByteFields(Record):
+    __format__ = [('a', Integer(2, False)),
+                  ('b', Integer(3, False)),
+                  ('c', Integer(3, False))]
+    EXPECTED_ENCODED_SIZE = 1
+
+class MiddleSubByteFields(Record):
+    __format__ = [('a', Integer(32)),
+                  ('b', Integer(4)),
+                  ('c', Integer(4)),
+                  ('d', Integer(24)),
+                  ('e', Integer(8))]
+    EXPECTED_ENCODED_SIZE = 9
+
+records = [SingleSignedByte, SingleUnsignedByte, SingleSignedShort,
+           SingleUnsignedShort, SingleSignedLong, SingleUnsignedLong,
+           MultiByte, MultiType, SubByteFields, MiddleSubByteFields]
+
+def randomValue(type):
+    return random.randrange(2 ** type.bits)
+
+from twisted.trial import unittest
+class RecordPacking(unittest.TestCase):
+    def testMethodsAdded(self):
+        for rt in records:
+            self.failUnless(hasattr(rt, 'encode'))
+            self.failUnless(hasattr(rt, 'decode'))
+
+    def testEncoding(self):
+        for rt in records:
+            fmt = rt.__format__
+            inst = rt()
+            for (k, t) in fmt:
+                # Pick a random value that the attribute can take on
+                setattr(inst, k, randomValue(t))
+            s = inst.encode()
+            self.assertEquals(len(s), rt.EXPECTED_ENCODED_SIZE)
+            self.assertEquals(vars(rt.decode(s)), vars(inst))
