13c3e15bec803a41aa427973f2479130d2277b29
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2003-12-31 06:54:19 +0000

    itertools isn't really used; also add copyright and author information
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9747 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/tls/record.py
+++ sandbox/exarkun/tls/record.py
@@ -1,6 +1,27 @@
 # -*- test-case-name: twisted.test.test_record -*-
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001 Matthew W. Lefkowitz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-import types, math, struct, itertools
+"""
+Framework for decoding complex byte streams.
+
+@author: U{Jp Calderone<mailto:exarkun@twistedmatrix.com}
+"""
+
+import types, math, struct
 
 class Integer:
     def __init__(self, bits, signed=False):
