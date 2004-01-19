b1bb4e0d5b1d63b3cbdfc4b558a58a529efcc1d6
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-01-19 05:22:45 +0000

    import on windows, small fixes
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@9858 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/realm.py
+++ twisted/conch/realm.py
@@ -1,5 +1,23 @@
+# Twisted, the Framework of Your Internet
+# Copyright (C) 2001-2002 Matthew W. Lefkowitz
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General Public
+# License as published by the Free Software Foundation.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# 
+
 from twisted.cred import portal
-import pwd
+from twisted.python import components
+
 
 class UnixSSHRealm:
     __implements__ = portal.IRealm
@@ -7,7 +25,8 @@ class UnixSSHRealm:
     def requestAvatar(self, username, mind, *interfaces):
         return interfaces[0], UnixSSHUser(username), lambda: None
 
-class ISSHUser:
+
+class ISSHUser(components.Interface):
     """A user for an SSH service.  This lets the server get access to things
     like the users uid/gid, their home directory, and their shell.
     """
@@ -27,11 +46,13 @@ class ISSHUser:
         @return: a string containing the path to the users shell.
         """
 
+
 class UnixSSHUser:
     __implements__ = ISSHUser
 
     def __init__(self, username):
         self.username = username
+        import pwd
         self.pwdData = pwd.getpwnam(self.username)
 
     def getUserGroupId(self):
