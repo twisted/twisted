23c88f8eb607fb1b23520e72b45832deae213223
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-03-03 04:57:30 +0000

    Half refactor; half support for Connections
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10062 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/papers/2004/pycon/migration/oldpb/jelliers.py
+++ sandbox/exarkun/papers/2004/pycon/migration/oldpb/jelliers.py
@@ -10,6 +10,26 @@ from twisted.internet import interfaces as iinternet
 from twisted.internet import defer
 
 #
+# Blah MetaInterface is not type
+#
+class MetaInterfaceJellier(components.Adapter):
+    __implements__ = (ispread.IJellyable,)
+
+    def jellyFor(self, jellier):
+        sxp = jellier.prepare(self.original)
+        sxp.extend([
+            'twisted.python.components.MetaInterface',
+            reflect.qual(self.original.__class__)])
+        return jellier.preserve(self.original, sxp)
+
+components.registerAdapter(MetaInterfaceJellier, components.MetaInterface, ispread.IJellyable)
+
+def MetaInterfaceUnjellier(unjellier, jellyList):
+    return reflect.namedAny(jellyList[1])
+
+jelly.setUnjellyableForClass('twisted.python.components.MetaInterface', MetaInterfaceUnjellier)
+
+#
 # Reactor jelly!
 #
 class ReactorJellier(components.Adapter):
@@ -70,7 +90,7 @@ class FileDescriptorJellier(components.Adapter):
 
     def getStateFor(self, jellier):
         state = self.original.__dict__.copy()
-        state['socketHandle'] = ISocketStorage(jellier).put(state.pop('socket'))
+        ISocketStorage(jellier).put(state.pop('socket'))
         jellier.invoker.serializingPerspective.dChannel.transport.sendFileDescriptors([state['fileno']()])
         del state['fileno']
         return state
@@ -94,31 +114,42 @@ def handleToSocket(handle, addressFamily, socketType):
         ).addCallback(socket.fromfd, addressFamily, socketType
         )
 
-def socketInMyPocket(skt, instance, attribute):
+READ = 1
+WRITE = 2
+def socketInMyPocket(skt, instance, attribute, mode):
     setattr(instance, attribute, skt)
     instance.fileno = skt.fileno
-    instance.startReading()
-    # instance.startWriting()
+    if mode & READ:
+        instance.startReading()
+    if mode & WRITE:
+        instance.startWriting()
 
 class _DummyClass:
     pass
 
-def FileDescriptorUnjellier(unjellier, jellyList):
-    # Second half of the icky hack!
-    fdproto = unjellier.invoker.fdproto
-    klass = reflect.namedAny(jellyList[0])
-    inst = _DummyClass()
-    inst.__class__ = klass
-    state = unjellier.unjelly(jellyList[1])
-    socketHandle = state.pop('socketHandle')
-    inst.__dict__ = state
-    handleToSocket(fdproto.fds.pop(0), klass.addressFamily, klass.socketType
-        ).addCallback(socketInMyPocket, inst, 'socket'
-        ).addErrback(log.err
-        )
-    return inst
+class FileDescriptorUnjellier:
+    def __init__(self, mode):
+        self.mode = mode
+
+    def __call__(self, unjellier, jellyList):
+        # Second half of the icky hack!
+        fdproto = unjellier.invoker.fdproto
+        klass = reflect.namedAny(jellyList[0])
+        inst = _DummyClass()
+        inst.__class__ = klass
+        state = unjellier.unjelly(jellyList[1])
+        inst.__dict__ = state
+        handleToSocket(fdproto.fds.pop(0), klass.addressFamily, klass.socketType,
+            ).addCallback(socketInMyPocket, inst, 'socket', self.mode
+            ).addErrback(log.err
+            )
+        return inst
 
 portBase = 'twisted.internet.%s.Port'
 for mod in ('tcp',):
-    jelly.setUnjellyableForClass(portBase % mod, FileDescriptorUnjellier)
+    jelly.setUnjellyableForClass(portBase % mod, FileDescriptorUnjellier(READ))
 del portBase, mod
+
+connBase = 'twisted.internet.%s.Connection'
+for mod in ('tcp',):
+    jelly.setUnjellyableForClass(connBase % mod, FileDescriptorUnjellier(READ | WRITE))
--- sandbox/exarkun/papers/2004/pycon/migration/oldpb/test_server.py
+++ sandbox/exarkun/papers/2004/pycon/migration/oldpb/test_server.py
@@ -1,114 +1,24 @@
 
-import tempfile
-
-from twisted.spread import pb
 from twisted.python import log
-from twisted.internet import defer
-from twisted.internet import reactor
-from twisted.internet import protocol
+from twisted.spread import pb
 from twisted.application import service
-from twisted.cred import portal
+from twisted.application import internet
 from twisted.cred import checkers
+from twisted.cred import portal
 
+import unix
 import pbold
 import jelliers
-import unix
+
+from server import MigrationRealm
 
 def makeAFactory():
     from twisted.web import server, static
     pb.globalSecurity.allowInstancesOf(server.Site)
     return server.Site(static.File('.'))
 
-class ServerFactory(protocol.ServerFactory):
-    protocol = protocol.Protocol
-
-    def __init__(self, d):
-        self.onConnect = d
-
-    def buildProtocol(self, addr):
-        p = protocol.ServerFactory.buildProtocol(self, addr)
-        self.onConnect.callback(p)
-        return p
-
-class MigrationError(Exception):
-    pass
-
-class DescriptorChannelNotAllocated(MigrationError):
-    pass
-
-#
-# Sequence of events:
-#
-# Server listens on 'migrate' unix socket
-# Client connects to 'migrate' unix socket
-# New Avatar allocated for this connection
-# Client asks for a new descriptor channel via PB
-# Server allocates a temporary file, listens on it as a unix socket,
-#   and tells client name of file
-# Client connects to temporary file
-# Client requests list of services available for transfer
-# Server responds with list
-# Client selects services and requests it from server
-# Server moves service into `in transit' holding pen
-# Server returns service:
-#   Objects reachable from service are jellied
-#   FileDescriptors encountered are jellied using the adapters
-#     in jelliers.py; their file descriptors are sent over the
-#     descriptor channel with sendmsg()
-# Client acknowledges receipt of service and associated file
-#   descriptors
-# Server dumps service from `in transit' holding pen
-# If no services remain on server, server terminates
-
-class MigrationServer(pb.Avatar):
-    descriptorChannelAllocated = False
-
-    def __init__(self, servers):
-        self.servers = servers
-        self.transition = {}
-
-    def cbChannelAllocate(self, result):
-        self.descriptorChannelAllocated = True
-        self.dConnection = None
-        self.dChannel = result
-
-    def perspective_allocateDescriptorChannel(self):
-        tmp = tempfile.mktemp()
-        self.dConnection = defer.Deferred()
-        self.dConnection.addCallback(self.cbChannelAllocate)
-        self.dConnection.addErrback(log.err)
-        self.dChannelServer = unix.UNIXServer(tmp, ServerFactory(self.dConnection))
-        self.dChannelServer.startService()
-        return tmp
-
-    def perspective_getServerList(self):
-        return self.servers.keys()
-
-    def perspective_getServer(self, name):
-        if not self.descriptorChannelAllocated:
-            raise DescriptorChannelNotAllocated()
-        self.transition[name] = self.servers.pop(name)
-        return self.transition[name]
-
-    def perspective_gotServer(self, name):
-        server = self.transition.pop(name)
-        server.socket.close()
-        server.socket = None
-        if not self.servers and not self.transition:
-            from twisted.internet import reactor
-            # reactor.stop()
-
-class MigrationRealm:
-    __implements__ = (portal.IRealm,)
-
-    def __init__(self, servers):
-        self.servers = servers
-
-    def requestAvatar(self, avatarID, mind, *interfaces):
-        assert pb.IPerspective in interfaces
-        return pb.IPerspective, MigrationServer(self.servers), lambda: None
-
 def makeService():
+    from twisted.internet import reactor
     port = reactor.listenTCP(0, makeAFactory())
 
     r = MigrationRealm({'blah': port})
