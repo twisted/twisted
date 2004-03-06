91a17cdc22be799bba166965a7442fa2ef188eb4
mary <mary@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-03-06 01:07:43 +0000

    Fix spelling AND use non-deprecated methods!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10082 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/examples/silly-web.py
+++ doc/examples/silly-web.py
@@ -1,7 +1,7 @@
 # This shows an example of a bare-bones distributed web
 # set up.
 # The "master" and "slave" parts will usually be in different files
-# -- they are here together only for brevity of illustatiojn 
+# -- they are here together only for brevity of illustration 
 
 from twisted.internet import reactor, protocol
 from twisted.web import server, distrib, static
@@ -11,8 +11,8 @@ from twisted.spread import pb
 site = server.Site(distrib.ResourceSubscription('unix', '.rp'))
 reactor.listenTCP(19988, site)
 
-# The "slaver" server
-fact = pb.BrokerFactory(distrib.ResourcePublisher(server.Site(static.File('static'))))
+# The "slave" server
+fact = pb.PBServerFactory(distrib.ResourcePublisher(server.Site(static.File('static'))))
 
 reactor.listenUNIX('./.rp', fact)
 reactor.run()
