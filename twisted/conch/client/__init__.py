d7de574a279c8291b23136cfa8ac36c690ad4132
z3p <z3p@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-04-12 03:08:37 +0000

    add a docstring to the client __init__.py
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10526 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/client/__init__.py
+++ twisted/conch/client/__init__.py
@@ -14,3 +14,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
+"""
+Client support code for Conch.
+
+This module is unstable.
+
+Maintainer: U{Paul Swartz<mailto:z3p@twistedmatrix.com>}
+"""
