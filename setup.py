82455a99fded6d1221329a608baf7f7fa21334ff
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-04-18 17:30:12 +0000

    that was stupid
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10565 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- setup.py
+++ setup.py
@@ -237,6 +237,7 @@ desktop environments, and your toaster.
         "twisted.manhole",
         "twisted.manhole.ui",
         "twisted.names",
+        "twisted.news",
         "twisted.pair",
         "twisted.persisted",
         "twisted.persisted.journal",
