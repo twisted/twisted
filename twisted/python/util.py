5c9d2aaa12b67e062dbbbdf93918fbef0e2e0cfc
slyphon <slyphon@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-05-28 22:03:52 +0000

    a wrapper class around cStringIO to allow for subclassing
    maybe someone will find this useful. it's pretty trivial, 
    and i don't understand why the stdlib doesn't provide it.
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10816 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/util.py
+++ twisted/python/util.py
@@ -606,10 +606,61 @@ def switchUID(uid, gid, euid=False):
     initgroups(uid, gid)
     setuid(uid)
 
+
+class SubclassableCStringIO(object):
+    """A wrapper around cStringIO to allow for subclassing"""
+    __csio = None
+
+    def __init__(self, *a, **kw):
+        from cStringIO import StringIO
+        self.__csio = StringIO(*a, **kw)
+
+    def __iter__(self):
+        return self.__csio.__iter__()
+
+    def next(self):
+        return self.__csio.next()
+
+    def close(self):
+        return self.__csio.close()
+
+    def isatty(self):
+        return False
+
+    def seek(self, pos, mode=0):
+        return self.__csio.seek(pos, mode)
+
+    def tell(self):
+        return self.__csio.tell()
+
+    def read(self, n=-1):
+        return self.__csio.read(n)
+
+    def readline(self, length=None):
+        return self.__csio.readline(length)
+
+    def readlines(self, sizehint=0):
+        return self.__csio.readlines(sizehint)
+
+    def truncate(self, size=None):
+        return self.__csio.truncate(size)
+
+    def write(self, s):
+        return self.__csio.write(s)
+
+    def writelines(self, list):
+        return self.__csio.writelines(list)
+
+    def flush(self):
+        return self.__csio.flush()
+
+    def getvalue(self):
+        return self.__csio.getvalue()
+
 __all__ = [
     "uniquify", "padTo", "getPluginDirs", "addPluginDir", "sibpath",
     "getPassword", "dict", "println", "keyed_md5", "makeStatBar",
     "OrderedDict", "spewer", "searchupwards", "LineLog", "raises",
     "IntervalDifferential", "FancyStrMixin", "FancyEqMixin", "dsu",
-    "switchUID"
+    "switchUID", "SubclassableCStringIO"
 ]
