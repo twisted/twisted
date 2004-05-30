8ff90caa6deb250e9f809d19f74c42007b3aa800
slyphon <slyphon@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-05-30 20:06:50 +0000

    zope.interface.directlyProvides() example
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10830 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/slyphon/interfaces_tutorial/cmplist3.py
+++ sandbox/slyphon/interfaces_tutorial/cmplist3.py
@@ -3,46 +3,33 @@
 import os, os.path as osp, types
 from os.path import join as opj
 
-from twisted.python.util import sibpath
-
 from twisted.python import components
-import zope.interface as zi
+import zope.interface 
 
 
 # functions can be adapters too!
 
 class IFilePath(components.Interface):
-        """i return a string that the this object represents in terms of a file path"""
+   """i return a string that the this object represents in terms of a file path"""
+   pass
 
 def modulePath(original):
     return osp.abspath(original.__file__)
-zi.directlyProvides(modulePath, IFilePath)
+zope.interface.directlyProvides(modulePath, IFilePath)
 
 def listPath(original):
     return opj(*original)
-zi.directlyProvides(IFilePath)
+zope.interface.directlyProvides(IFilePath)
 
 def filePath(original):
     return original.name
-zi.directlyProvides(IFilePath)
+zope.interface.directlyProvides(IFilePath)
 
-components.registerAdapter(
-    modulePath,
-    types.ModuleType,
-    IFilePath
-)
+for _adapter, _original, _interface in [ (modulePath, types.ModuleType, IFilePath),
+                                         (listPath, types.ListType, IFilePath),
+                                         (filePath, types.FileType, IFilePath) ]:
 
-components.registerAdapter(
-    listPath,
-    types.ListType,
-    IFilePath
-)
-
-components.registerAdapter(
-    filePath,
-    types.FileType,
-    IFilePath
-)
+    components.registerAdapter(_adapter, _original, _interface)
 
 
 def main():
