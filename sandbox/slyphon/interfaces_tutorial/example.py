10f85a8d50839ca67e79f38b8af85a9f0cf3b2a7
slyphon <slyphon@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-05-31 03:21:23 +0000

    almost done!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10832 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/slyphon/interfaces_tutorial/example.py
+++ sandbox/slyphon/interfaces_tutorial/example.py
@@ -1,41 +1,34 @@
 #!/usr/bin/env python2.3
 
-import re, os, os.path as osp, time
+import re, os, os.path as osp, time, types
 from os.path import join as opj
 
 from twisted.python import components
 import zope.interface as zi
 
 
-#### our "data objects" ####
-#
-# these classes represent data and methods for acquiring the data we're interested in
-# 
+class IDisplay(zi.Interface):
+    def simple():
+        """display a simple version of data"""
+   
+    def verbose():
+        """display a verbose view of data"""
 
 
-class FileGrepper(object):
-   """I grep a file with a regex pattern and display matching lines""" 
 
-   _matchlist = None
-
-   def __init__(self, path, pattern):
-       self.path = path
-       self.pattern = pattern
-
-   def _getMatchingLines(self):
-       """return a list of all lines that match pattern in path"""
-       r = re.compile(self.pattern)
-       l = []
-       if not self._matchlist:
-           for num, line in enumerate(file(self.path, 'r')):
-               if r.search(line):
-                   l.append((num, line))
-           self._matchlist = l
-       return self._matchlist
-   matchingLines = property(_getMatchingLines)
+class IListDirItem(zi.Interface):
+   def __call__(pathAsString):
+      """@param pathAsString: the path to a file"""
+   path = zi.Attribute("the path given to this file")
+   name = zi.Attribute("the name of the file")
+   nlinks = zi.Attribute("the number of hardlinks to this file")
+   size = zi.Attribute("size in bytes of a plain file")
+   stat = zi.Attribute("the stat object returned by os.stat of path")
+   uid = zi.Attribute("the user id of the owner")
+   gid = zi.Attribute("the group id of the owner")
 
 
-class ListDirItem(object):
+class ListDirItem(components.Adapter):
     """a basic wrapper around the os.lstat object"""
     _stat = None
 
@@ -53,77 +46,42 @@ class ListDirItem(object):
             self._stat = os.lstat(self.path)
         return self._stat
     stat = property(_getstat)
-    
 
-class FileLister(object):
+components.registerAdapter(ListDirItem, types.StringType, IListDirItem)
+
+
+
+class IFileLister(components.Interface):
+   dirlist = zi.Attribute("""a file list that can be easily converted into linux-style ls -l command output""")
+   filenames = zi.Attribute("""the filenames this lister has listed (without full path info)""")
+   paths = zi.Attribute("""the full path of the items this lister has listed""")
+
+class FileLister(components.Adapter):
     """list all files in a directory"""
+    zi.implements(IFileLister)
     _dirlist = None
-    def __init__(self, path):
-        self.path = path
+
+    def __init__(self, original):
+        self.original = self.path = original
 
     def _getDirList(self):
-        """returns a file list that can be easily converted into linux-style ls -l command output"""
         print 'self.path: %s' % self.path
         if not self._dirlist:
             r = []
             for name in os.listdir(self.path):
                 full = opj(self.path, name)
                 if osp.isfile(full):
-                    r.append(ListDirItem(full))
+                    r.append(IListDirItem(full))
             self._dirlist = r
         return self._dirlist
     dirlist = property(_getDirList)
    
     filenames = property(lambda self: [i.name for i in self.dirlist])
     paths = property(lambda self: [i.path for i in self.dirlist])
+
+components.registerAdapter(FileLister, types.StringType, IFileLister)
+
     
-
-
-# Let's pretend everything up here is in one module...
-                                                                                     
-#######################################################################################
-                                                                                     
-# ...and everything down here is in another
- 
-
-
-
-#### our interfaces ####
-#
-# these are some simple interfaces that we'll make a little more complicated in
-# components, phase 3
-#
-
-class IDisplay(zi.Interface):
-    def simple():
-        """display a simple version of data"""
-   
-    def verbose():
-        """display a verbose view of data"""
-
-
-class IPrint(IDisplay):
-    """I print data to the console"""
-
-
-
-#### our Adapters ####
-#
-# these adapt other objects to the required interfaces
-
-class DisplayFileGrepper(components.Adapter):
-    zi.implements(IDisplay)
-
-    def simple(self):
-        for num, line in self.original.matchingLines:
-            print line
-
-    def verbose(self):
-        for num, line in self.original.matchingLines:
-            print "%s:    %s" % (num, line)
-
-
-
 class DisplayFileList(components.Adapter):
     zi.implements(IDisplay)
 
@@ -132,8 +90,6 @@ class DisplayFileList(components.Adapter):
 
     def verbose(self):
 
-        # these next two are exarkun's bit of magic, so don't ask, I don't know
-
         def pmask(info):
             return ''.join([info.stat.st_mode & (256 >> n) and 'rwx'[n % 3] or '-' for n in range(9)])
 
@@ -141,43 +97,70 @@ class DisplayFileList(components.Adapter):
             fmt = 'pld----'
             return [fmt[i] for i in range(7) if (info.stat.st_mode >> 12) & (1 << i)][0]
 
-        ### --end exarkun magic-- ###
-
-
         def strmtime(info):
             return time.strftime('%b %d %I:%M', time.gmtime(info.stat.st_mtime))
 
         for info in self.original.dirlist:
             print "%s%s   %s    %s    %s    %s   %s   %s" % (dtype(info), pmask(info), info.nlinks, info.uid, info.gid, info.size, strmtime(info), info.name)
 
+components.registerAdapter(DisplayFileList, IFileLister, IDisplay)
 
 
 
-#### adapter registration ####
-#
-# I always forget the order in which you call the registerAdapter function
-#
-# here are two very helpful suggestions from dash and moshez:
-#
-#  < moshez> slyphon: I always imagine it like a call
-#  < moshez> Adapter(OriginalInterface) -> AdaptedInterface
-#
-#  < dash> slyphon: AOI AOI AOI
-#
-
-                          
-components.registerAdapter(
-    DisplayFileGrepper,     # our -A-dapter
-    FileGrepper,            # our -O-riginal interface
-    IDisplay                # our -I-nterface 
-)
+class IFileGrepper(zi.Interface):
+   pattern = zi.Attribute('the patern to grep for')
+   matchingLines = zi.Attribute("""a list of (path, num, line) tuples that matched the pattern in path""")
 
 
-components.registerAdapter(
-    DisplayFileList,
-    FileLister,
-    IDisplay
-)
+class FileListerGrepper(object):
+   """I grep a file with a regex pattern and display matching lines""" 
+   zi.implements(IFileGrepper)
+
+   _pattern, _regex, _matchList = None, None, None
+
+   def __init__(self, original):
+       self.original = original
+
+   def _getPattern(self):
+       return self._pattern
+
+   def _setPattern(self, pattern):
+       print "_setPattern called"
+       self._pattern = pattern
+       self._regex = re.compile(pattern)
+
+   pattern = property(_getPattern, _setPattern)
+
+   def _getMatchingLines(self):
+       """return a list of all lines that match pattern in path"""
+       l = []
+       if not self._matchList:
+           for path in self.original.paths:
+               for num, line in enumerate(file(path, 'r')):
+                   assert self._regex
+                   if self._regex.search(line):
+                       l.append((osp.basename(path), num, line))
+           self._matchList = l
+       return self._matchList
+
+   matchingLines = property(_getMatchingLines)
+
+
+components.registerAdapter(FileListerGrepper, FileLister, IFileGrepper)
+
+
+class DisplayFileGrepper(components.Adapter):
+    zi.implements(IDisplay)
+
+    def simple(self):
+        for path, num, line in self.original.matchingLines:
+            print line
+
+    def verbose(self):
+        for path, num, line in self.original.matchingLines:
+            print "%s: %s:    %s" % (path, num, line)
+
+components.registerAdapter(DisplayFileGrepper, FileListerGrepper, IDisplay)
 
 
 
@@ -186,35 +169,42 @@ def getGoodPath():
     import twisted.python
 
     # gimme a path with a lot of good files to play with
-    return sibpath(twisted.__file__, '../doc/howto')
+    sp = sibpath(twisted.__file__, '../doc/howto')
+    return osp.normpath(sp)
 
 
 def main():
     goodpath = getGoodPath()
-    filelister = FileLister(goodpath)
+    print "we'll be playing around with goodpath: %s" % goodpath
+    print "which is an object of type: %r" % type(goodpath)
+    filelister = IFileLister(goodpath)
 
+    print "\n\n"
 
     print "first, let's see IDisplay(filelist).simple()'s output"
-    idfl = IDisplay(filelister)
-    idfl.simple()
+    IDisplay(filelister).simple()
     
+    print "\n\n"
+   
     print "now let's see IDisplay(filelist).verbose()'s output"
     IDisplay(filelister).verbose()
 
+    print "\n\n"
 
     print "okay, now let's grep around for the pattern '\<[Tt]wisted\>' in filelist's list of names"
-    pattern = r"\<[Tt]wisted\>"
-    glist = [FileGrepper(path, pattern) for path in filelister.paths]
+    p = r"[Tt]wisted"
+    fg = IFileGrepper(filelister)
 
+    print fg
     
-    print "and see IDisplay(grepper).simple()"
-    for grepper in glist:
-        IDisplay(grepper).simple()
+    fg.pattern = p
 
+    print IDisplay(fg).simple()
 
-    print "and now how about IDisplay(grepper).verbose()"
-    for grepper in glist:
-        IDisplay(grepper).simple()
+    print "\n\n"
+
+    print "and now how about IDisplay(fg).verbose()"
+    IDisplay(fg).verbose()
 
 
 if __name__ == '__main__':
