e7eb5b84d52b937f7e721324c508a39707f33787
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-06-05 16:09:20 +0000

    make test_split_compat more better
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10878 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_split_compat.py
+++ twisted/test/test_split_compat.py
@@ -12,15 +12,13 @@ movedModules = [('twisted.protocols.smtp', 'twisted.mail.smtp'),
 
 class TestCompatibility(unittest.TestCase):
     def testCompatibility(self):
-        self.expectedAssertions = len(movedModules)
         for oldName, newName in movedModules:
             try:
                 old = reflect.namedModule(oldName)
                 new = reflect.namedModule(newName)
             except ImportError, e:
-                self.expectedAssertions -= 1
                 continue
-            someName = vars(old).keys()[0]
-            self.assertIdentical(getattr(old, someName),
-                                 getattr(new, someName))
+            for someName in [x for x in vars(new).keys() if x != '__doc__']:
+                self.assertIdentical(getattr(old, someName),
+                                     getattr(new, someName))
             
