d85faa037d9726f8ec9f37711090a60108182395
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-06-21 13:52:54 +0000

    here!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10938 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ sandbox/radix/defgen.py
@@ -0,0 +1,136 @@
+# Copyright (C) 2004 Christopher Armstrong
+# This will eventually go into Twisted
+
+from twisted.python import log
+
+from twisted.internet.defer import Deferred
+
+class waitForDeferred:
+    """
+    waitForDeferred and deferredGenerator help you write
+    Deferred-using code that looks like it's blocking (but isn't really),
+    with the help of generators.
+
+    There are two important functions involved: waitForDeferred, and
+    deferredGenerator.
+
+        def thingummy():
+            thing = waitForDeferred(makeSomeRequestResultingInDeferred())
+            yield thing
+            thing = thing.getResult()
+            print thing #the result! hoorj!
+        thingummy = deferGenerator(thingummy)
+
+    waitForDeferred returns something that you should immediately yield;
+    when your generator is resumed, calling thing.getResult() will either
+    give you the result of the Deferred if it was a success, or raise an
+    exception if it was a failure.
+
+    deferredGenerator takes one of these waitForDeferred-using
+    generator functions and converts it into a function that returns a
+    Deferred. The result of the Deferred will be the last 'regular'
+    value that your generator yielded, i.e., one that's not the result
+    of waitForDeferred (remember that 'return result' won't work; use
+    'yield result; return' in place of that). The Deferred may also
+    errback if your generator raised an exception.
+
+        def thingummy():
+            thing = waitForDeferred(makeSomeRequestResultingInDeferred())
+            yield thing
+            thing = thing.getResult()
+            if thing == 'I love Twisted':
+                # will become the result of the Deferred
+                yield 'TWISTED IS GREAT!'
+                return
+            else:
+                # will trigger an errback
+                raise Exception('DESTROY ALL LIFE')
+        thingummy = deferredGenerator(thingummy)
+
+    Put concisely, waitForDeferred and deferredGenerator connect
+    deferred-using code with this 'fake blocking' style in both
+    directions: waitForDeferred converts from a Deferred to the 'blocking'
+    style, and deferredGenerator converts from the 'blocking' style to a
+    Deferred.
+    """
+    def __init__(self, d):
+        assert isinstance(d, Deferred)
+        self.d = d
+
+    def getResult(self):
+        if hasattr(self, 'failure'):
+            raise self.failure
+        return self.result
+
+def deferGenerator(g, deferred=None, result=None):
+    """
+    See L{waitForDeferred}.
+    """
+    if deferred is None:
+        deferred = Deferred()
+    try:
+        result = g.next()
+    except StopIteration:
+        deferred.callback(result)
+        return deferred
+    except:
+        deferred.errback()
+        return deferred
+    if isinstance(result, waitForDeferred):
+        def gotResult(r):
+            result.result = r
+            deferGenerator(g, deferred, r)
+        def gotError(f):
+            result.failure = f
+            deferGenerator(g, deferred, f)
+        result.d.addCallbacks(gotResult, gotError)
+        # XXX log.err???? bad
+        result.d.addErrback(log.err)
+    else:
+        deferGenerator(g, deferred, result)
+    return deferred
+
+def deferredGenerator(f):
+    return lambda *args, **kwargs: deferGenerator(f(*args, **kwargs))
+
+if __name__ == '__main__':
+    from twisted.internet import reactor
+    def testIt():
+
+        def getThing():
+            d = Deferred()
+            reactor.callLater(0.5, d.callback, "hi")
+            return d
+
+        x = waitForDeferred(getThing())
+        yield x
+        x = x.getResult()
+
+        assert x == "hi"
+
+        def getOwie():
+            d = Deferred()
+            def CRAP():
+                d.errback(Exception('OMG'))
+            reactor.callLater(0.5, CRAP)
+            return d
+
+        ow = waitForDeferred(getOwie())
+        yield ow
+        try:
+            ow.getResult()
+        except Exception, e:
+            assert str(e) == 'OMG', repr(e.args)
+        1/0 # seeing this is good
+        return
+
+
+    d = deferGenerator(testIt())
+    def _(r):
+        assert r == "there"
+        print "wee"
+    d.addCallback(_)
+    d.addErrback(log.err)
+    d.addBoth(lambda x: reactor.stop())
+    from twisted.internet import reactor
+    reactor.run()
