dcc827b2d2ebf57efd22d825aad407e85680f673
tv <tv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-06-29 19:04:24 +0000

    Make web tests cover lot more ground, refactor a bit.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@10995 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/tv/newlyguarded/login.py
+++ sandbox/tv/newlyguarded/login.py
@@ -25,6 +25,19 @@ import os
 from twisted.cred import checkers, portal, credentials
 from nevow import rend, loaders, inevow, guard, url
 
+def getActionURL(current, history):
+    action = current
+    if len(history) == 1:
+        action = action.here()
+    else:
+        for element in history[1:]:
+            action = action.parent()
+
+    action = action.child(guard.LOGIN_AVATAR)
+    for element in history:
+        action = action.child(element)
+    return action
+
 class LoginPage(rend.Page):
     """The resource that is returned when you are not logged in"""
     docFactory = loaders.xmlfile(
@@ -40,21 +53,8 @@ class LoginPage(rend.Page):
 
     def render_form(self, context, data):
         request = context.locate(inevow.IRequest)
-        action = url.URL.fromRequest(request)
-        print 'HISTORY', self.history
-        print 'ACTION', 1, action
-        #for element in self.history[1:]:
-        if len(self.history) == 1:
-            action = action.parent()
-        else:
-            for element in self.history[1:]:
-                action = action.parent()
-        print 'ACTION', 2, action
-        action = action.child(guard.LOGIN_AVATAR)
-        print 'ACTION', 3, action
-        for element in self.history:
-            action = action.child(element)
-        print 'ACTION', 4, action
+        current = url.URL.fromRequest(request)
+        action = getActionURL(current, self.history)
         context.fillSlots('action-url', str(action))
         return context.tag
 
--- sandbox/tv/newlyguarded/test/test_web.py
+++ sandbox/tv/newlyguarded/test/test_web.py
@@ -70,23 +70,104 @@ class ActualTests:
         b.follow_link(text_regex=re.compile(r'another'))
         self.assertEquals(b.title(), "Hello again!")
 
-
 # TODO self.url = "http://localhost:8081/" won't work,
 # as mechanize tries to look for cookies set by "localhost.local",
 # of which there are none.
 
-class TestWithCookies(unittest.TestCase, ActualTests):
+class WithCookies:
     def setUp(self):
         self.url = "http://127.0.0.1:8081/"
         self.browser = Browser()
+
+class TestWithCookies(ActualTests, WithCookies, unittest.TestCase):
+    pass
 
 class DenyAllCookiesPolicy(ClientCookie.DefaultCookiePolicy):
     def set_ok(self, cookie, request, unverifiable):
         return False
 
-class TestWithOutCookies(unittest.TestCase, ActualTests):
+class WithoutCookies:
     def setUp(self):
         self.url = "http://127.0.0.1:8081/"
         self.browser = Browser()
         jar = ClientCookie.CookieJar(policy=DenyAllCookiesPolicy())
         self.browser.set_cookiejar(jar)
+
+class TestWithoutCookies(ActualTests, WithoutCookies, unittest.TestCase):
+    pass
+
+class ActualLoginTest:
+    def test_login(self):
+        b = self.browser
+        b.open(self.url + self.path)
+        self.assertEquals(b.title(), 'Log In')
+        b.select_form(name='login')
+        b["username"] = "test"
+        b["password"] = "test"
+        b.submit()
+        self.assertEquals(b.title(), "Hello!")
+        url = b.geturl()
+        self.failUnless(url.startswith(self.url))
+##         end = '/%s' % self.path
+##         self.failUnless(url.endswith(end),
+##                         "URL must end with %r, but was %r" % (end, url))
+
+class LoginTest_SecretStuffStuff_noslash_cookie(ActualLoginTest,
+                                                WithCookies,
+                                                unittest.TestCase):
+    path = 'secret/stuff/stuff'
+
+class LoginTest_SecretStuffStuff_noslash_nocookie(ActualLoginTest,
+                                                  WithoutCookies,
+                                                  unittest.TestCase):
+    path = 'secret/stuff/stuff'
+
+class LoginTest_SecretStuffStuff_slash_cookie(ActualLoginTest,
+                                              WithCookies,
+                                              unittest.TestCase):
+    path = 'secret/stuff/stuff/'
+
+class LoginTest_SecretStuffStuff_slash_nocookie(ActualLoginTest,
+                                                WithoutCookies,
+                                                unittest.TestCase):
+    path = 'secret/stuff/stuff/'
+
+class LoginTest_SecretStuff_noslash_cookie(ActualLoginTest,
+                                           WithCookies,
+                                           unittest.TestCase):
+    path = 'secret/stuff'
+
+class LoginTest_SecretStuff_noslash_nocookie(ActualLoginTest,
+                                             WithoutCookies,
+                                             unittest.TestCase):
+    path = 'secret/stuff'
+
+class LoginTest_SecretStuff_slash_cookie(ActualLoginTest,
+                                         WithCookies,
+                                         unittest.TestCase):
+    path = 'secret/stuff/'
+
+class LoginTest_SecretStuff_slash_nocookie(ActualLoginTest,
+                                           WithoutCookies,
+                                           unittest.TestCase):
+    path = 'secret/stuff/'
+
+class LoginTest_Secret_noslash_cookie(ActualLoginTest,
+                                      WithCookies,
+                                      unittest.TestCase):
+    path = 'secret'
+
+class LoginTest_Secret_noslash_nocookie(ActualLoginTest,
+                                        WithoutCookies,
+                                        unittest.TestCase):
+    path = 'secret'
+
+class LoginTest_Secret_slash_cookie(ActualLoginTest,
+                                    WithCookies,
+                                    unittest.TestCase):
+    path = 'secret/'
+
+class LoginTest_Secret_slash_nocookie(ActualLoginTest,
+                                      WithoutCookies,
+                                      unittest.TestCase):
+    path = 'secret/'
