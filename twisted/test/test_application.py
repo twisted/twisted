b575a097673dd186e340cda46a684d7dc9422429
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-06-30 15:02:04 +0000

    pebble. - Use IAddress.port instead of IAddress[2] - IReactorFoo(reactor, None) as check for Foo support in reactor. Backwards/Forwards compat AND more correct (thx exarkun)
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11005 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_application.py
+++ twisted/test/test_application.py
@@ -311,7 +311,7 @@ class TestAppSupport(unittest.TestCase):
 class TestInternet(unittest.TestCase):
 
     def testUNIX(self):
-        if not components.implements(reactor, interfaces.IReactorUNIX):
+        if not interfaces.IReactorUNIX(reactor, None):
             raise unittest.SkipTest, "This reactor does not support UNIX domain sockets"
         s = service.MultiService()
         c = compat.IOldApplication(s)
@@ -344,7 +344,7 @@ class TestInternet(unittest.TestCase):
         c.listenTCP(0, factory)
         s.privilegedStartService()
         s.startService()
-        num = list(s)[0]._port.getHost()[2]
+        num = list(s)[0]._port.getHost().port
         class Foo(basic.LineReceiver):
             def connectionMade(self):
                 self.transport.write('lalala\r\n')
@@ -445,7 +445,7 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(args[3], '')
 
     def testSimpleUNIX(self):
-        if not components.implements(reactor, interfaces.IReactorUNIX):
+        if not interfaces.IReactorUNIX(reactor, None):
             raise unittest.SkipTest, "This reactor does not support UNIX domain sockets"
         s = "(dp0\nS'udpConnectors'\np1\n(lp2\nsS'unixConnectors'\np3\n(lp4\nsS'twisted.internet.app.Application.persistenceVersion'\np5\nI12\nsS'name'\np6\nS'web'\np7\nsS'sslConnectors'\np8\n(lp9\nsS'sslPorts'\np10\n(lp11\nsS'tcpPorts'\np12\n(lp13\nsS'unixPorts'\np14\n(lp15\n(S'/home/moshez/.twistd-web-pb'\np16\n(itwisted.spread.pb\nBrokerFactory\np17\n(dp19\nS'objectToBroker'\np20\n(itwisted.web.distrib\nResourcePublisher\np21\n(dp22\nS'twisted.web.distrib.ResourcePublisher.persistenceVersion'\np23\nI2\nsS'site'\np24\n(itwisted.web.server\nSite\np25\n(dp26\nS'resource'\np27\n(itwisted.web.static\nFile\np28\n(dp29\nS'ignoredExts'\np30\n(lp31\nsS'defaultType'\np32\nS'text/html'\np33\nsS'registry'\np34\n(itwisted.web.static\nRegistry\np35\n(dp36\nS'twisted.web.static.Registry.persistenceVersion'\np37\nI1\nsS'twisted.python.components.Componentized.persistenceVersion'\np38\nI1\nsS'_pathCache'\np39\n(dp40\nsS'_adapterCache'\np41\n(dp42\nS'twisted.internet.interfaces.IServiceCollection'\np43\n(itwisted.internet.app\nApplication\np44\n(dp45\ng1\ng2\nsg3\ng4\nsg5\nI12\nsg6\ng7\nsg8\ng9\nsg10\ng11\nsg12\ng13\nsg14\ng15\nsS'extraPorts'\np46\n(lp47\nsS'gid'\np48\nI1053\nsS'tcpConnectors'\np49\n(lp50\nsS'extraConnectors'\np51\n(lp52\nsS'udpPorts'\np53\n(lp54\nsS'services'\np55\n(dp56\nsS'persistStyle'\np57\nS'pickle'\np58\nsS'delayeds'\np59\n(lp60\nsS'uid'\np61\nI1053\nsbssbsS'encoding'\np62\nNsS'twisted.web.static.File.persistenceVersion'\np63\nI6\nsS'path'\np64\nS'/home/moshez/public_html.twistd'\np65\nsS'type'\np66\ng33\nsS'children'\np67\n(dp68\nsS'processors'\np69\n(dp70\nS'.php3'\np71\nctwisted.web.twcgi\nPHP3Script\np72\nsS'.rpy'\np73\nctwisted.web.script\nResourceScript\np74\nsS'.php'\np75\nctwisted.web.twcgi\nPHPScript\np76\nsS'.cgi'\np77\nctwisted.web.twcgi\nCGIScript\np78\nsS'.epy'\np79\nctwisted.web.script\nPythonScript\np80\nsS'.trp'\np81\nctwisted.web.trp\nResourceUnpickler\np82\nssbsS'logPath'\np83\nNsS'sessions'\np84\n(dp85\nsbsbsS'twisted.spread.pb.BrokerFactory.persistenceVersion'\np86\nI3\nsbI5\nI438\ntp87\nasg55\ng56\nsg48\nI1053\nsg49\ng50\nsg51\ng52\nsg53\ng54\nsg46\ng47\nsg57\ng58\nsg61\nI1053\nsg59\ng60\ns."
         d = pickle.loads(s)
@@ -491,7 +491,7 @@ class TestInternet2(unittest.TestCase):
         factory.protocol = wire.Echo
         t = internet.TCPServer(0, factory)
         t.setServiceParent(s)
-        num = t._port.getHost()[2]
+        num = t._port.getHost().port
         class Foo(basic.LineReceiver):
             def connectionMade(self):
                 self.transport.write('lalala\r\n')
@@ -506,12 +506,12 @@ class TestInternet2(unittest.TestCase):
         self.assertEqual(factory.line, 'lalala')
 
     def testUDP(self):
-        if not components.implements(reactor, interfaces.IReactorUDP):
+        if not interfaces.IReactorUDP(reactor, None):
             raise unittest.SkipTest, "This reactor does not support UDP sockets"
         p = protocol.DatagramProtocol()
         t = internet.TCPServer(0, p)
         t.startService()
-        num = t._port.getHost()[2]
+        num = t._port.getHost().port
         l = []
         defer.maybeDeferred(t.stopService).addCallback(l.append)
         while not l:
@@ -529,7 +529,7 @@ class TestInternet2(unittest.TestCase):
         t = internet.TCPServer(0, factory)
         t.privileged = 1
         t.privilegedStartService()
-        num = t._port.getHost()[2]
+        num = t._port.getHost().port
         class Foo(basic.LineReceiver):
             def connectionMade(self):
                 self.transport.write('lalala\r\n')
@@ -548,7 +548,7 @@ class TestInternet2(unittest.TestCase):
         factory.protocol = wire.Echo
         t = internet.TCPServer(0, factory)
         t.startService()
-        num = t._port.getHost()[2]
+        num = t._port.getHost().port
         t.stopService()
         l = []
         factory = protocol.ClientFactory()
@@ -560,7 +560,7 @@ class TestInternet2(unittest.TestCase):
         self.assertEqual(l, [None])
 
     def testUNIX(self):
-        if not components.implements(reactor, interfaces.IReactorUNIX):
+        if not interfaces.IReactorUNIX(reactor, None):
             raise unittest.SkipTest, "This reactor does not support UNIX domain sockets"
         s = service.MultiService()
         s.startService()
@@ -597,7 +597,7 @@ class TestInternet2(unittest.TestCase):
             reactor.iterate(0.1)
 
     def testVolatile(self):
-        if not components.implements(reactor, interfaces.IReactorUNIX):
+        if not interfaces.IReactorUNIX(reactor, None):
             raise unittest.SkipTest, "This reactor does not support UNIX domain sockets"
         factory = protocol.ServerFactory()
         factory.protocol = wire.Echo
@@ -624,7 +624,7 @@ class TestInternet2(unittest.TestCase):
         self.failIf(t.running)
 
     def testStoppingServer(self):
-        if not components.implements(reactor, interfaces.IReactorUNIX):
+        if not interfaces.IReactorUNIX(reactor, None):
             raise unittest.SkipTest, "This reactor does not support UNIX domain sockets"
         factory = protocol.ServerFactory()
         factory.protocol = wire.Echo
@@ -669,9 +669,9 @@ class TestInternet2(unittest.TestCase):
     def testEverythingThere(self):
         trans = 'TCP UNIX SSL UDP UNIXDatagram Multicast'.split()
         for tran in trans[:]:
-            if not components.implements(reactor, getattr(interfaces, "IReactor"+tran)):
+            if not getattr(interfaces, "IReactor"+tran)(reactor, None):
                 trans.remove(tran)
-        if components.implements(reactor, interfaces.IReactorArbitrary):
+        if interfaces.IReactorArbitrary(reactor, None) is not None:
             trans.insert(0, "Generic")
         for tran in trans:
             for side in 'Server Client'.split():
@@ -701,14 +701,3 @@ class TestCompat(unittest.TestCase):
         application = oapp.Application("HELLO")
         oapp.MultiService("HELLO", application)
         compat.convert(application)
-
-"""
-if not components.implements(reactor, interfaces.IReactorUNIX):
-    unixmethods = [TestInternet.testUNIX, TestConvert.testSimpleUNIX, \
-        TestInternet2.testUNIX, TestInternet2.testVolatile, TestInternet2.testStoppingServer]
-    for m in unixmethods:
-        m.im_func.skip = "This reactor does not support UNIX domain sockets"
-    del unixmethods # what's this good for?
-if not components.implements(reactor, interfaces.IReactorUDP):
-    TestInternet2.testUDP.im_func.skip = "This reactor does not support UDP"
-"""
