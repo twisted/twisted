5fef9719fdd4ec9a69b5b63e6ce0cabeab4ebfe2
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-07-29 16:06:23 +0000

    forgot to add backwards compatibility modules
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11174 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/protocols/http.py
@@ -0,0 +1,7 @@
+from twisted.python import util
+
+util.moduleMovedForSplit('twisted.protocols.http', 'twisted.web.http',
+                         'HTTP protocol support', 'Web',
+                         'http://projects.twistedmatrix.com/web',
+                         globals())
+
---
+++ twisted/protocols/sux.py
@@ -0,0 +1,7 @@
+from twisted.python import util
+
+util.moduleMovedForSplit('twisted.protocols.sux', 'twisted.web.sux',
+                         'SAX-like XML parser', 'Web',
+                         'http://projects.twistedmatrix.com/web',
+                         globals())
+
