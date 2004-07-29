b6f29ac7e07c1b323476498c76b996f2bd9237f5
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-07-29 16:18:35 +0000

    backwards compatibility module
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11177 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/protocols/jabber.py
@@ -0,0 +1,7 @@
+from twisted.python import util
+
+util.moduleMovedForSplit('twisted.protocols.jabber', 'twisted.words.protocols.jabber',
+                         'Jabber protocol support', 'Words',
+                         'http://projects.twistedmatrix.com/words',
+                         globals())
+
