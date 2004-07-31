bb402cf2e4c5924621e450597c1e44901525f2e0
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-07-31 18:06:47 +0000

    tm.com, not projects.tm.com
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11214 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/dns.py
+++ twisted/protocols/dns.py
@@ -2,5 +2,5 @@ from twisted.python import util
 
 util.moduleMovedForSplit('twisted.protocols.dns', 'twisted.names.dns',
                          'DNS protocol support', 'Names',
-                         'http://projects.twistedmatrix.com/names',
+                         'http://www.twistedmatrix.com/projects/names/',
                          globals())
