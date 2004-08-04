ca92d09e4733a90d301842c14c1182e9cf280e41
spiv <spiv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-08-04 17:38:42 +0000

    Fork twice in daemonise -- see http://www.erlenstar.demon.co.uk/unix/faq_toc.html#TOC16
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11263 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/twistd.py
+++ twisted/scripts/twistd.py
@@ -119,9 +119,12 @@ def startLogging(logfilename, sysLog, prefix, nodaemon):
 
 
 def daemonize():
+    # See http://www.erlenstar.demon.co.uk/unix/faq_toc.html#TOC16
     if os.fork():   # launch child and...
         os._exit(0) # kill off parent
     os.setsid()
+    if os.fork():   # launch child and...
+        os._exit(0) # kill off parent again.
     os.umask(077)
     null=os.open('/dev/null', os.O_RDWR)
     for i in range(3):
