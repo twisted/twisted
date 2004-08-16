86ec0b961ac84b5699047f46d5e0532486ab4efe
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-08-16 07:35:56 +0000

    Tests for receiving various numbers of bytes at a time
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11332 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/manhole/test_manhole.py
+++ sandbox/exarkun/manhole/test_manhole.py
@@ -15,12 +15,12 @@ class ControlCharacters(unittest.TestCase):
         self.parser = ServerProtocol()
         self.transport = StringTransport()
         self.parser.makeConnection(self.transport)
-        self.handler = self.parser.handler = pmock.Mock()
 
     def _simpleKeystrokeTest(self, symbolic, bytes):
-        self.handler.expects(pmock.once()).keystrokeReceived(pmock.eq(symbolic))
+        handler = self.parser.handler = pmock.Mock()
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(symbolic))
         self.parser.dataReceived(bytes)
-        self.handler.verify()
+        handler.verify()
 
     tmpl = "def test%sArrow(self): self._simpleKeystrokeTest(self.parser.%s_ARROW, '\x1b[%s')"
     for testName, byte in [('Up', 'A'), ('Down', 'B'), ('Right', 'C'), ('Left', 'D')]:
@@ -31,3 +31,28 @@ class ControlCharacters(unittest.TestCase):
         exec tmpl % (funcNum, funcNum, chr(ord('O') + funcNum))
 
     del tmpl, testName, funcNum, byte
+
+    def _setupHandler(self):
+        # All the arrow keys once, followed by 5 function keys
+        bytes = '\x1b[A\x1b[B\x1b[C\x1bD\x1bOP\x1bOQ\x1bOR\x1bOS\x1bOT'
+        handler = self.parser.handler = pmock.Mock()
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.UP_ARROW)).id("up")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.DOWN_ARROW)).id("down").after("up")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.RIGHT_ARROW)).id("right").after("down")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.LEFT_ARROW)).id("left").after("right")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.F1)).id("f1").after("left")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.F2)).id("f2").after("f1")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.F3)).id("f3").after("f2")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.F4)).id("f4").after("f3")
+        handler.expects(pmock.once()).keystrokeReceived(pmock.eq(self.parser.F5)).after("f4")
+        return handler, bytes
+
+    def testSingleBytes(self):
+        handler, bytes = self._setupHandler()
+        for b in bytes:
+            self.parser.dataReceived(b)
+        handler.verify()
+
+    tmpl = "def testByte%s(self):\n\thandler, bytes = self._setupHandler()\n\twhile bytes:\n\t\tself.parser.dataReceived(b[:%d])\n\t\tb = b[%d:]\n\thandler.verify()"
+    for word, n in [('Pairs', 2), ('Triples', 3), ('Quads', 4), ('Quints', 5), ('Sexes', 6)]:
+        exec tmpl % (word, n, n)
