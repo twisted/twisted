0503b6023f9264585315b71b360bbd7de5b27cd9
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-08-19 18:21:38 +0000

    Let people actually quit
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11375 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/manhole/stdio.py
+++ sandbox/exarkun/manhole/stdio.py
@@ -9,18 +9,32 @@ from insults import ServerProtocol
 class ConsoleManhole(Manhole):
     def __init__(self, *a, **kw):
         Manhole.__init__(self, *a, **kw)
-        self.keyHandlers['\n'] = self.keyHandlers.pop('\r')
+        self.keyHandlers['\x03'] = self.handle_INT
+        self.keyHandlers['\x04'] = self.handle_QUIT
+        self.keyHandlers['\x1c'] = self.handle_QUIT
+
+    def handle_INT(self):
+        self.proto.nextLine()
+        self.proto.write("KeyboardInterrupt")
+        self.proto.nextLine()
+        self.proto.write(self.ps[self.pn])
+        self.lineBuffer = []
+        self.lineBufferIndex = 0
+
+    def handle_QUIT(self):
+        reactor.stop()
 
 def main():
     oldSettings = termios.tcgetattr(sys.stdin.fileno())
     tty.setraw(sys.stdin.fileno())
     try:
         p = ServerProtocol()
-        p.handlerFactory = Manhole
+        p.handlerFactory = ConsoleManhole
         stdio.StandardIO(p)
         reactor.run()
     finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, oldSettings)
+        termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, oldSettings)
+        print
 
 if __name__ == '__main__':
     main()
