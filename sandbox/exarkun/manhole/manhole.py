153ceadea37486b7d60f2e9e6db0a53d3129cb57
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-08-19 20:52:18 +0000

    Factor ^C and ^\ and ^D handling back into the base class (probably should go even higher up)
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11378 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/manhole/manhole.py
+++ sandbox/exarkun/manhole/manhole.py
@@ -68,6 +68,20 @@ class Manhole(recvline.HistoricRecvLineHandler):
     def __init__(self, proto):
         recvline.HistoricRecvLineHandler.__init__(self, proto)
         self.interpreter = ManholeInterpreter(self)
+        self.keyHandlers['\x03'] = self.handle_INT
+        self.keyHandlers['\x04'] = self.handle_QUIT
+        self.keyHandlers['\x1c'] = self.handle_QUIT
+
+    def handle_INT(self):
+        self.proto.nextLine()
+        self.proto.write("KeyboardInterrupt")
+        self.proto.nextLine()
+        self.proto.write(self.ps[self.pn])
+        self.lineBuffer = []
+        self.lineBufferIndex = 0
+
+    def handle_QUIT(self):
+        self.proto.disconnect()
 
     def addOutput(self, bytes):
         self.proto.write(bytes.replace('\n', '\r\n'))
--- sandbox/exarkun/manhole/stdio.py
+++ sandbox/exarkun/manhole/stdio.py
@@ -9,17 +9,6 @@ from insults import ServerProtocol
 class ConsoleManhole(Manhole):
     def __init__(self, *a, **kw):
         Manhole.__init__(self, *a, **kw)
-        self.keyHandlers['\x03'] = self.handle_INT
-        self.keyHandlers['\x04'] = self.handle_QUIT
-        self.keyHandlers['\x1c'] = self.handle_QUIT
-
-    def handle_INT(self):
-        self.proto.nextLine()
-        self.proto.write("KeyboardInterrupt")
-        self.proto.nextLine()
-        self.proto.write(self.ps[self.pn])
-        self.lineBuffer = []
-        self.lineBufferIndex = 0
 
     def handle_QUIT(self):
         self.proto.reset()
