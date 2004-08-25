c792bf9c2366235f0a27330b80699a87dc64cb80
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-08-25 05:07:11 +0000

    Whitespace; remove prints
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11446 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/manhole/telnet.py
+++ sandbox/exarkun/manhole/telnet.py
@@ -12,19 +12,16 @@ MODE_ACK = 4
 SOFT_TAB = 8
 LIT_ECHO = 16
 
-NAWS = chr(31)
-SUPPRESS_GO_AHEAD = chr(3)
-
 # Characters gleaned from the various (and conflicting) RFCs.  Not all of these are correct.
 
-NULL =            chr(0)  # No operation.
-BEL =             chr(7)  # Produces an audible or
+NULL =           chr(0)   # No operation.
+BEL =            chr(7)   # Produces an audible or
                           # visible signal (which does
                           # NOT move the print head).
-BS =              chr(8)  # Moves the print head one
+BS =             chr(8)   # Moves the print head one
                           # character position towards
                           # the left margin.
-HT =              chr(9)  # Moves the printer to the
+HT =             chr(9)   # Moves the printer to the
                           # next horizontal tab stop.
                           # It remains unspecified how
                           # either party determines or
@@ -45,45 +42,51 @@ FF =             chr(12)  # Moves the printer to the top
 CR =             chr(13)  # Moves the printer to the left
                           # margin of the current line.
 
-ECHO  =           chr(1)  # User-to-Server:  Asks the server to send
+ECHO  =          chr(1)   # User-to-Server:  Asks the server to send
                           # Echos of the transmitted data.
-LINEMODE =      chr(34)   # Allow line buffering to be
+SGA =            chr(3)   # Suppress Go Ahead.  Go Ahead is silly
+                          # and most modern servers should suppress
+                          # it.
+NAWS =           chr(31)  # Negotiate About Window Size.  Indicate that
+                          # information about the size of the terminal
+                          # can be communicated.
+LINEMODE =       chr(34)  # Allow line buffering to be
                           # negotiated about.
 
-SE =            chr(240)  # End of subnegotiation parameters.
-NOP =           chr(241)  # No operation.
-DM =            chr(242)  # "Data Mark": The data stream portion
+SE =             chr(240) # End of subnegotiation parameters.
+NOP =            chr(241) # No operation.
+DM =             chr(242) # "Data Mark": The data stream portion
                           # of a Synch.  This should always be
                           # accompanied by a TCP Urgent
                           # notification.
-BRK =           chr(243)  # NVT character Break.
-IP =            chr(244)  # The function Interrupt Process.
-AO =            chr(245)  # The function Abort Output
-AYT =           chr(246)  # The function Are You There.
-EC =            chr(247)  # The function Erase Character.
-EL =            chr(248)  # The function Erase Line
-GA =            chr(249)  # The Go Ahead signal.
-SB =            chr(250)  # Indicates that what follows is
+BRK =            chr(243) # NVT character Break.
+IP =             chr(244) # The function Interrupt Process.
+AO =             chr(245) # The function Abort Output
+AYT =            chr(246) # The function Are You There.
+EC =             chr(247) # The function Erase Character.
+EL =             chr(248) # The function Erase Line
+GA =             chr(249) # The Go Ahead signal.
+SB =             chr(250) # Indicates that what follows is
                           # subnegotiation of the indicated
                           # option.
-WILL =          chr(251)  # Indicates the desire to begin
+WILL =           chr(251) # Indicates the desire to begin
                           # performing, or confirmation that
                           # you are now performing, the
                           # indicated option.
-WONT =          chr(252)  # Indicates the refusal to perform,
+WONT =           chr(252) # Indicates the refusal to perform,
                           # or continue performing, the
                           # indicated option.
-DO =            chr(253)  # Indicates the request that the
+DO =             chr(253) # Indicates the request that the
                           # other party perform, or
                           # confirmation that you are expecting
                           # the other party to perform, the
                           # indicated option.
-DONT =          chr(254)  # Indicates the demand that the
+DONT =           chr(254) # Indicates the demand that the
                           # other party stop performing,
                           # or confirmation that you are no
                           # longer expecting the other party
                           # to perform, the indicated option.
-IAC =           chr(255)  # Data Byte 255.  Introduces a
+IAC =            chr(255) # Data Byte 255.  Introduces a
                           # telnet command.
 
 
@@ -624,7 +627,7 @@ class TelnetBootstrapProtocol(TelnetProtocol, ProtocolTransportMixin):
     def connectionMade(self):
         self.transport.negotiationMap[NAWS] = self.telnet_NAWS
 
-        for opt in (LINEMODE, NAWS, SUPPRESS_GO_AHEAD):
+        for opt in (LINEMODE, NAWS, SGA):
             self.transport.do(opt).addErrback(log.err)
         for opt in (ECHO,):
             self.transport.will(opt).addErrback(log.err)
@@ -633,7 +636,6 @@ class TelnetBootstrapProtocol(TelnetProtocol, ProtocolTransportMixin):
         self.protocol.makeConnection(self)
 
     def allowEnable(self, opt):
-        print 'Refusing to allow to enable', repr(opt)
         return False
 
     def enable(self, opt):
@@ -642,7 +644,7 @@ class TelnetBootstrapProtocol(TelnetProtocol, ProtocolTransportMixin):
         elif opt == NAWS:
             pass
         else:
-            print 'enabling', repr(opt)
+            pass
 
     def disable(self, opt):
         print 'disabling', repr(opt)
