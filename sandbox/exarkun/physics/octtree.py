4d5679b55510306dfd5fe054f2e9c66c9426499b
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-08-29 04:45:36 +0000

    remove and __iter__
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11489 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/physics/octtree.py
+++ sandbox/exarkun/physics/octtree.py
@@ -11,6 +11,12 @@ class _TerminalNode(object):
     def add(self, obj):
         self.objects.append(obj)
 
+    def remove(self, obj):
+        self.objects.remove(obj)
+
+    def __iter__(self):
+        return iter(self.objects)
+
 class OctTree(object):
     def __init__(self, center, width, depth, height, n=0):
         self.center = center
@@ -19,7 +25,11 @@ class OctTree(object):
         self.height = height
         self.n = n
 
-    def add(self, obj):
+    def _getChild(self, p):
+        left = p[0] < self.center[0]
+        front = p[1] < self.center[1]
+        bottom = p[2] < self.center[2]
+
         # One way to implement the base case for this recursive function
         # is to stop when an empty node is found.  There is no need to
         # subdivide between a single object.  Even more, for small groups
@@ -29,12 +39,6 @@ class OctTree(object):
 
         # Another way is to use a fixed depth.  This is the way we will use.
         # Subsequent implementations may attempt to be smarter.
-
-        p = obj.position
-
-        left = p[0] < self.center[0]
-        front = p[1] < self.center[1]
-        bottom = p[2] < self.center[2]
 
         if self._children[n] is None:
             if self.n == 9:
@@ -57,5 +61,23 @@ class OctTree(object):
                     center[2] = self.center - height / 2
                 else:
                     center[2] = self.center + height / 2
-                self._children[n] = OctTree(center, width, depth, height)
-        self._children[n].add(obj)
+                self._children[n] = OctTree(tuple(center),
+                                            width,
+                                            depth,
+                                            height,
+                                            self.n + 1)
+            return self._children[n]
+
+    def add(self, obj):
+        child = self._getChild(obj.position)
+        child.add(obj)
+
+    def remove(self, obj):
+        child = self._getChild(obj.position)
+        child.remove(obj)
+
+    def __iter__(self):
+        for ch in self._children:
+            if ch is not None:
+                for obj in ch:
+                    yield obj
