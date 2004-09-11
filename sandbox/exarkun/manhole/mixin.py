fd6e36b3730298f12f5b1169052210143bb98251
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-09-11 16:09:44 +0000

    docstrings for whiney radix
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11625 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/exarkun/manhole/mixin.py
+++ sandbox/exarkun/manhole/mixin.py
@@ -2,12 +2,20 @@
 from twisted.internet import reactor
 
 class BufferingMixin:
+    """Mixin which adds write buffering.
+    """
     _delayedWriteCall = None
     bytes = None
 
     DELAY = 0.0
 
     def write(self, bytes):
+        """Buffer some bytes to be written soon.
+
+        Every call to this function delays the real write by C{self.DELAY}
+        seconds.  When the delay expires, all collected bytes are written
+        to the underlying transport using C{ITransport.writeSequence}.
+        """
         if self._delayedWriteCall is None:
             self.bytes = []
             self._delayedWriteCall = reactor.callLater(self.DELAY, self.flush)
@@ -16,6 +24,8 @@ class BufferingMixin:
         self.bytes.append(bytes)
 
     def flush(self):
+        """Flush the buffer immediately.
+        """
         self._delayedWriteCall = None
         self.transport.writeSequence(self.bytes)
         self.bytes = None
