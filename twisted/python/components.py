b58d83ea9580c5293f177b5cbb34628844878cd0
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-09-26 21:36:59 +0000

    fix issue 730
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11832 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/components.py
+++ twisted/python/components.py
@@ -330,7 +330,7 @@ class MetaInterface(interface.InterfaceClass):
         registry.register([self], to, '', using)
 
     def __getattr__(self, attr):
-        if attr == "_v_repr":
+        if attr.startswith("_v_"):
             raise AttributeError # z.i internal thing,
         if attr != "__instadapt__": # __instadapt__ is part of our own backwards compat layer
             warnings.warn("Don't get attributes (in this case, %r) off Interface, use "
