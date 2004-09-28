b4a6f86a07e843789597cc56c13ba29be90f7b5d
cce <cce@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-09-28 14:49:50 +0000

    digest authentication works with curl
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11850 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/cce/digest.py
+++ sandbox/cce/digest.py
@@ -8,7 +8,8 @@ import md5, time, random
 
 class DigestAuthentication(Resource):
     """ Simple implementation of RFC 2617 - HTTP Digest Authentication """
-    def __init__(self, realm, userfunc, authpage = None, errpage = None):
+    def __init__(self, realm, userfunc, authpage = None, 
+                 failpage = None, testall = True):
         """
             parent is the page that the getChild request is forwarded to
             realm is a globally unique URI, like tag:clarkevans.com,2004:bing
@@ -20,9 +21,16 @@ class DigestAuthentication(Resource):
         self.__nonce    = {} # list to prevent replay attacks
         self.__userfunc = userfunc
         self.__realm    = realm
+        self.__testall  = testall
         self.__authpage = authpage or \
-            ErrorPage(401,'Authentication Required','Please Authenticate')
-        self.__errpage  = errpage  or \
+            ErrorPage(401,'Authentication Required',
+              "This server could not verify that you "
+              "are authorized to access the document you "
+              "requested.  Either you supplied the wrong "
+              "credentials (e.g., bad password), or your "
+              "browser doesn't understand how to supply "
+              "the credentials required.")
+        self.__failpage  = failpage  or \
             ErrorPage(400,'Bad Authentication','Bad Authentication')
 
     def sendAuthenticateResponse(self, request, stale = ''):
@@ -35,46 +43,58 @@ class DigestAuthentication(Resource):
         request.setResponseCode(401)
         return self.__authpage
            
-    def testAuthorization(self, request):
+    def testAuthentication(self, request):
         method = request.method
         auth = request.getHeader('Authorization')
         if not auth:
             return self.sendAuthenticateResponse(request)
         (authtype, auth) = auth.split(" ", 1)
+        print auth
         if 'Digest' != authtype:
-            return self._errpage
+            return self.__failpage
         amap = {}
         for itm in auth.split(", "):
             (k,v) = [s.strip() for s in itm.split("=",1)]
             amap[k] = v.replace('"','')
         try:
             username = amap['username']
-            nonce    = amap['nonce']
-            seqno    = amap['nc']
             uri      = amap['uri']
-            cnonce   = amap['cnonce']
+            nonce    = amap['nonce']
+            realm    = amap['realm']
             assert uri == request.uri
+            assert realm == self.__realm
+            qop      = amap.get('qop','')
+            cnonce   = amap.get('cnonce','')
+            nc       = amap.get('nc','00000000')
+            if qop:
+                assert 'auth' == qop
+                assert nonce and nc
         except:
-            return self._errpage
-        ha1 = self.__userfunc(self.__realm,username)
+            return self.__failpage
+        ha1 = self.__userfunc(realm,username)
         if not ha1:
             return self.sendAuthenticateResponse(request)
         ha2 = md5.md5('%s:%s' % (method,uri)).hexdigest()
-        chk = "%s:%s:%s:%s:%s:%s" % (ha1,nonce,seqno,cnonce,'auth',ha2)
+        if qop:
+            chk = "%s:%s:%s:%s:%s:%s" % (ha1,nonce,nc,cnonce,qop,ha2)
+        else:
+            chk = "%s:%s:%s" % (ha1,nonce,ha2)
         if amap['response'] != md5.md5(chk).hexdigest():
             if nonce in self.__nonce:
                 del self.__nonce[nonce]
             return self.sendAuthenticateResponse(request)
-        if seqno <= self.__nonce.get(nonce,'00000000'):
+        if nc <= self.__nonce.get(nonce,'00000000'):
             if nonce in self.__nonce:
                 del self.__nonce[nonce]
             return self.sendAuthenticateResponse(request,stale=True)
-        self.__nonce[nonce] = seqno
+        self.__nonce[nonce] = nc
         return None # all is well
 
     def getChildWithDefault(self,path,request):
-        epage = self.testAuthorization(request)
-        if epage: return epage
+        if self.__testall:
+            epage = self.testAuthentication(request)
+            if epage: 
+                return epage
         return Resource.getChildWithDefault(self,path,request)
 
 def test():
@@ -106,6 +126,11 @@ if '__main__' == __name__:
 # broken up till urllib2.py revision 1.53.6.2 ; so, if you have Python
 # 2.3.2 you can fetch the following URL and just overwrite urllib2.py
 # http://cvs.sf.net/viewcvs.py/*checkout*/python/python/dist/src/Lib/urllib2.py?rev=1.53.6.2
+#
+# also works with curl,
+#
+# curl --user bing:gnib --digest "http://127.0.0.1:8080/data"
+#
 #
 def fetch():
     import urllib2
