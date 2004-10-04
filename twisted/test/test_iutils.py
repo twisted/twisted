dddd1f190be913088db9051913f57ceeb6a6e4df
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-10-04 05:34:40 +0000

    Use SIGKILL, not SIGHUP, because the buildslave is run under 'nohup'. Make all reactor.iterate calls a bit less CPU-slamming, and add timeouts.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11906 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_iutils.py
+++ twisted/test/test_iutils.py
@@ -27,8 +27,10 @@ class UtilsTestCase(SignalMixin, unittest.TestCase):
         exe = sys.executable
         d=utils.getProcessOutput(exe, ['-c', 'print "hello world"'])
         d.addCallback(self.saveOutput)
-        while self.output is None:
-            reactor.iterate()
+        ttl = time.time() + 5
+        while self.output is None and time.time() < ttl:
+            reactor.iterate(0.01)
+        self.failIf(self.output is None, "timeout")
         self.assertEquals(self.output, "hello world\n")
 
     def testOutputWithError(self):
@@ -38,15 +40,19 @@ class UtilsTestCase(SignalMixin, unittest.TestCase):
         # make sure stderr raises an error normally
         d = utils.getProcessOutput(exe, ['-c', sx], errortoo=0)
         d.addBoth(res1.append)
-        while len(res1) == 0:
-            reactor.iterate()
+        ttl = time.time() + 5
+        while len(res1) == 0 and time.time() < ttl:
+            reactor.iterate(0.01)
+        self.failUnless(len(res1), "timeout")
         self.failUnless(isinstance(res1.pop().value, IOError))
         # make sure the error can be turned off
         res2 = []
         d = utils.getProcessOutput(exe, ['-c', sx], errortoo=1)
         d.addBoth(res2.append)
-        while len(res2) == 0:
-            reactor.iterate()
+        ttl = time.time() + 5
+        while len(res2) == 0 and time.time() < ttl:
+            reactor.iterate(0.01)
+        self.failUnless(len(res2), "timeout")
         actual = res2[0]
         expected = 'hello world\n'
         self.assertEquals(actual, expected)
@@ -55,8 +61,10 @@ class UtilsTestCase(SignalMixin, unittest.TestCase):
         exe = sys.executable
         d=utils.getProcessValue(exe, ['-c', 'import sys;sys.exit(1)'])
         d.addCallback(self.saveValue)
-        while self.value is None:
-            reactor.iterate()
+        ttl = time.time() + 5
+        while self.value is None and time.time() < ttl:
+            reactor.iterate(0.01)
+        self.failIf(self.value is None, "timeout")
         self.assertEquals(self.value, 1)
 
     def saveValue(self, o):
@@ -72,22 +80,29 @@ class UtilsTestCase(SignalMixin, unittest.TestCase):
         result = []
         d = utils.getProcessOutputAndValue(exe, ['-c', sx])
         d.addCallback(result.append)
-        while len(result) == 0:
-            reactor.iterate()
+        ttl = time.time() + 5
+        while len(result) == 0 and time.time() < ttl:
+            reactor.iterate(0.01)
+        self.failUnless(len(result), "timeout")
         actual = result[0]
         expected = ('hello world!\n', 'goodbye world!\n', 1)
         self.assertEquals(actual, expected)
 
     def testOutputSignal(self):
+        # Use SIGKILL here because it's guaranteed to be delivered. Using
+        # SIGHUP might not work in, e.g., a buildbot slave run under the
+        # 'nohup' command.
         exe = sys.executable
-        sx = r'import os, signal;os.kill(os.getpid(), signal.SIGHUP)'
+        sx = r'import os, signal;os.kill(os.getpid(), signal.SIGKILL)'
         result = []
         d = utils.getProcessOutputAndValue(exe, ['-c', sx])
         d.addErrback(result.append)
-        while len(result) == 0:
-            reactor.iterate()
+        ttl = time.time() + 5
+        while len(result) == 0 and time.time() < ttl:
+            reactor.iterate(0.01)
+        self.failUnless(len(result), "timeout")
         actual = result[0].value
-        expected = ('', '', signal.SIGHUP)
+        expected = ('', '', signal.SIGKILL)
         self.assertEquals(actual, expected)
 
 if not interfaces.IReactorProcess(reactor, None):
