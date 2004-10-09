f9fa2877a905e38c1297d51102335cc7c00e7b51
z3p <z3p@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-10-09 03:42:06 +0000

    fix broken test-case-name lines
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@11942 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/ssh/transport.py
+++ twisted/conch/ssh/transport.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twisted.test.test_conch -*-
+# -*- test-case-name: twisted.conch.test.test_conch -*-
 #
 # Copyright (c) 2001-2004 Twisted Matrix Laboratories.
 # See LICENSE for details.
