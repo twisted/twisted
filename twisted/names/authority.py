a28b212cc1fe84d0da63087e5f4dbe7be3334584
dreid <dreid@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-10-21 20:48:53 +0000

    add support for wildcard A and CNAME records
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12125 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/authority.py
+++ twisted/names/authority.py
@@ -102,7 +102,7 @@ class FileAuthority(common.ResolverBase):
                     )
             if not results:
                 results = cnames
-            
+
             for record in results + authority:
                 if record.type == dns.NS or record.type == dns.CNAME:
                     n = str(record.payload.name)
@@ -111,8 +111,23 @@ class FileAuthority(common.ResolverBase):
                             additional.append(
                                 dns.RRHeader(n, dns.A, dns.IN, rec.ttl or default_ttl, rec, auth=True)
                             )
+            
             return defer.succeed((results, authority, additional))
         except KeyError:
+            # Attempt to locate a suitable wildcard record
+            newName = name
+            while not self.records.get('*.' + newName.lower(), ()) and len(newName.split('.')) > 1:
+                newName = '.'.join(newName.split('.')[1:])
+
+            if self.records.has_key('*.'+newName):
+                for rec in self.records['*.'+newName]:
+                    if rec.TYPE == dns.A or rec.TYPE == dns.CNAME:
+                        results.append(
+                            dns.RRHeader(name, rec.TYPE, dns.IN, rec.ttl or default_ttl, rec, auth=True)
+                        )
+                
+                return defer.succeed((results, (), ()))
+
             if name.lower().endswith(self.soa[0].lower()):
                 # We are the authority and we didn't find it.  Goodbye.
                 return defer.fail(failure.Failure(dns.AuthoritativeDomainError(name)))
