e8944df5bcd62a1074c40db82f9ff18f99df473b
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-10-27 01:03:19 +0000

    fix test-case-name tags
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12181 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/test/test_domhelpers.py
+++ twisted/web/test/test_domhelpers.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twisted.test.test_domhelpers -*-
+# -*- test-case-name: twisted.web.test.test_domhelpers -*-
 #
 # Copyright (c) 2001-2004 Twisted Matrix Laboratories.
 # See LICENSE for details.
