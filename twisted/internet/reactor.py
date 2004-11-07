6a91b02348858e60143a1c1a6a442a92ef2a0c27
foom <foom@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-11-07 22:29:54 +0000

    There is no javareactor.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12375 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/reactor.py
+++ twisted/internet/reactor.py
@@ -6,12 +6,7 @@ See twisted.internet.interfaces.IReactor*.
 """
 import sys
 del sys.modules['twisted.internet.reactor']
-from twisted.python import runtime
-if runtime.platform.getType() == 'java':
-    from twisted.internet import javareactor
-    javareactor.install()
-else:
-    #from twisted.python import log
-    #log.msg("Installing SelectReactor, since unspecified.")
-    from twisted.internet import default
-    default.install()
+#from twisted.python import log
+#log.msg("Installing SelectReactor, since unspecified.")
+from twisted.internet import default
+default.install()
