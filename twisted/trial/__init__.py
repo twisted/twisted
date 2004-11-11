75249b2f8291ffb64d73e3a7ddff2782a143b510
slyphon <slyphon@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-11-11 01:37:50 +0000

    a change to the trial's private adapter registry
    this commit makes trial's private adapter registry an instance of 
    TrialAdapterRegistry, so that when testing trial, it is possible to 
    change adapters registrations without having to worry about changing 
    the registry that is running the test itself.
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12451 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/trial/__init__.py
+++ twisted/trial/__init__.py
@@ -6,50 +6,126 @@ try:
 except ImportError:
     raise ImportError, "you need zope.interface installed (http://zope.org/Products/ZopeInterface/)"
 
-_globalRegistry = AdapterRegistry()
 
-# we define a private adapter registry here to avoid conflicts and
-# have a bit more control
+class TrialAdapterRegistry(object):
+    # we define a private adapter registry here to avoid conflicts and
+    # have a bit more control
 
-def registerAdapter(adapterFactory, origInterface, *interfaceClasses):
-    self = _globalRegistry
-    assert interfaceClasses, "You need to pass an Interface"
+    def setUpRegistry(self, hooksListIndex=-1):
+        self._registry = AdapterRegistry()
+        interface.adapter_hooks.insert(hooksListIndex, self._hook)
 
-    if not isinstance(origInterface, interface.InterfaceClass):
-        origInterface = declarations.implementedBy(origInterface)
+    def registerAdapter(self, adapterFactory, origInterface, *interfaceClasses):
+        if not hasattr(self, '_registry'):
+            self.setUpRegistry()
 
-    for interfaceClass in interfaceClasses:
-        factory = self.get(origInterface).selfImplied.get(interfaceClass, {}).get('')
-        if factory and adapterFactory is not None: 
-            raise ValueError("an adapter (%s) was already registered." % (factory, ))
+        assert interfaceClasses, "You need to pass an Interface"
 
-    for interfaceClass in interfaceClasses:
-        self.register([origInterface], interfaceClass, '', adapterFactory)
+        if not isinstance(origInterface, interface.InterfaceClass):
+            origInterface = declarations.implementedBy(origInterface)
+
+        for interfaceClass in interfaceClasses:
+            factory = self._registry.get(origInterface).selfImplied.get(interfaceClass, {}).get('')
+            if factory and adapterFactory is not None:
+                raise ValueError("an adapter (%r, %r, %r) was already registered." % (
+                                 factory, origInterface, interfaceClasses))
+
+        for interfaceClass in interfaceClasses:
+            self._registry.register([origInterface], interfaceClass, '', adapterFactory)
 
 
-def adaptWithDefault(iface, orig, default=None):
-    # GUH! zi sucks
-    face = default
-    try:
-        face = iface(orig)
-    except TypeError, e:
-        if e.args[0] == 'Could not adapt':
-            pass
+    def adaptWithDefault(self, iface, orig, default=None):
+        # GUH! zi sucks
+        face = default
+        try:
+            face = iface(orig)
+        except TypeError, e:
+            if e.args[0] == 'Could not adapt':
+                pass
+            else:
+                raise
+        return face
+
+    def _clearAdapterRegistry(self):
+        """FOR INTERNAL USE ONLY"""
+        del self._registry
+        assert self._hook in interface.adapter_hooks
+        interface.adapter_hooks.remove(self._hook)
+
+    # add global adapter lookup hook for our newly created registry
+    def _hook(self, iface, ob):
+        factory = self._registry.lookup1(declarations.providedBy(ob), iface)
+        if factory is None:
+            return None
         else:
-            raise
-    return face
+            return factory(ob)
+
+    def _setUpAdapters(self):
+        import types
+        from twisted.trial import reporter, runner, itrial, adapters, remote
+        from twisted.spread import jelly
+        from twisted.python import failure
+        for a, o, i in [# ---- ITestRunner and ITestMethod adapters -----------
+                        (runner.TestCaseRunner, itrial.ITestCaseFactory, itrial.ITestRunner),
+                        (runner.TestModuleRunner, types.ModuleType, itrial.ITestRunner),
+                        (runner.PyUnitTestCaseRunner, itrial.IPyUnitTCFactory, itrial.ITestRunner),
+                        (runner.TestCaseMethodRunner, types.MethodType, itrial.ITestRunner),
+                        (runner.TestMethod, types.MethodType, itrial.ITestMethod),
+                        (trialdoctest.ModuleDocTestsRunner, types.ListType, itrial.ITestRunner),
+                        
+                        # ---- ITestStats  ------------------------------------
+                        (reporter.TestStats, itrial.ITestSuite, itrial.ITestStats),
+                        (reporter.TestStats, runner.TestModuleRunner, itrial.ITestStats),
+                        (reporter.TestCaseStats, runner.TestCaseRunner, itrial.ITestStats),
+                        (reporter.TestCaseStats, runner.TestCaseMethodRunner, itrial.ITestStats),
+                        (reporter.TestCaseStats, runner.PyUnitTestCaseRunner, itrial.ITestStats),
+
+                        # ---- Miscellaneous 'utility' adapters ---------------
+                        (adapters.formatFailureTraceback, failure.Failure, itrial.IFormattedFailure),
+                        (adapters.formatMultipleFailureTracebacks, types.ListType, itrial.IFormattedFailure),
+                        (adapters.formatMultipleFailureTracebacks, types.TupleType, itrial.IFormattedFailure),
+                        (adapters.formatTestMethodFailures, itrial.ITestMethod, itrial.IFormattedFailure),
+                        (adapters.getModuleNameFromModuleType, types.ModuleType, itrial.IModuleName),
+                        (adapters.getModuleNameFromClassType, types.ClassType, itrial.IModuleName),
+                        (adapters.getModuleNameFromClassType, types.TypeType, itrial.IModuleName),
+                        (adapters.getModuleNameFromMethodType, types.MethodType, itrial.IModuleName),
+                        (adapters.getModuleNameFromFunctionType, types.FunctionType, itrial.IModuleName),
+                        (adapters.getClassNameFromClass, types.ClassType, itrial.IClassName),
+                        (adapters.getClassNameFromMethodType, types.MethodType, itrial.IClassName),
+                        (adapters.getClassNameFromClass, types.TypeType, itrial.IClassName),
+                        (adapters.getFQClassName, types.ClassType, itrial.IFQClassName),
+                        (adapters.getFQClassName, types.TypeType, itrial.IFQClassName),
+                        (adapters.getFQClassName, types.MethodType, itrial.IFQClassName),
+                        (adapters.getFQMethodName, types.MethodType, itrial.IFQMethodName),
+                        (adapters.getClassFromMethodType, types.MethodType, itrial.IClass),
+                        (adapters.getClassFromFQString, types.StringType, itrial.IClass),
+                        (adapters.getModuleFromMethodType, types.MethodType, itrial.IModule),
+                        (lambda x: x, types.StringType, itrial.IClassName),
+                        (lambda x: x, types.StringType, itrial.IModuleName),
+
+                        # ---- Magic Attribute Adapters -----------------------
+                        (adapters.TupleTodo, types.TupleType, itrial.ITodo),
+                        (adapters.StringTodo, types.StringType, itrial.ITodo),
+                        (adapters.TodoBase, types.NoneType, itrial.ITodo),
+                        (adapters.TupleTimeout, types.TupleType, itrial.ITimeout),
+                        (adapters.NumericTimeout, types.FloatType, itrial.ITimeout),
+                        (adapters.NumericTimeout, types.IntType, itrial.ITimeout),
+                        (adapters.TimeoutBase, types.NoneType, itrial.ITimeout),
+                        (adapters.TimeoutBase, types.MethodType, itrial.ITimeout),
+                        #(runner.UserMethodWrapper, types.MethodType, itrial.IUserMethod),
+                        (remote.JellyableTestMethod, itrial.ITestMethod, jelly.IJellyable)]:
+            self.registerAdapter(a, o, i)
 
 
-# add global adapter lookup hook for our newly created registry
-def _hook(iface, ob, lookup=_globalRegistry.lookup1):
-    factory = lookup(declarations.providedBy(ob), iface)
-    if factory is None:
-        return None
-    else:
-        return factory(ob)
-interface.adapter_hooks.append(_hook)
+#XXX: refactor this so that things which need to adjust the global registry
+#     don't use these module-level names
 
+_trialRegistry = TrialAdapterRegistry()
 
-del _hook, AdapterRegistry
+_setUpAdapters = _trialRegistry._setUpAdapters
+
+registerAdapter = _trialRegistry.registerAdapter
+adaptWithDefault = _trialRegistry.adaptWithDefault
+
 
 __all__ = ['registerAdapter', 'adaptWithDefault']
