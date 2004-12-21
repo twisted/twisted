66c0f413035d999092a1155fc27ce7b6d08399ad
spiv <spiv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-12-21 04:54:48 +0000

    Log some more info to help track down #798
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12747 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/trial/test/importErrors.py
+++ twisted/trial/test/importErrors.py
@@ -2,6 +2,16 @@
 
 See twisted.trial.test.test_adapters.TestFailureFormatting.testImportError
 """
+# Log where we were imported from, to help debugging.
+from twisted.python import log
+import traceback
+from StringIO import StringIO
+buf = StringIO()
+traceback.print_stack(file=buf)
+log.msg('importErrors imported from:')
+log.msg(buf.getvalue())
+
+# Import a module that doesn't exist.  Boom!
 import Supercalifragilisticexpialidocious
 
 from twisted.trial.test import common
