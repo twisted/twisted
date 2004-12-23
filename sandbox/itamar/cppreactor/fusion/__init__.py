5c0ffbb26e8f1615fd42926ff9f8e4903b4c312c
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2004-12-23 23:07:41 +0000

    maybe possibly work if there's no sys.setdlopenflags, or at least don't blow up
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12770 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- sandbox/itamar/cppreactor/fusion/__init__.py
+++ sandbox/itamar/cppreactor/fusion/__init__.py
@@ -3,16 +3,15 @@
 # make sure fusion._fusion is loaded in a way other C++ code can use it.
 import sys
 try:
-    import dl
-    RTLD_NOW = dl.RTLD_NOW
-    RTLD_GLOBAL = dl.RTLD_GLOBAL
+    from dl import RTLD_NOW, RTLD_GLOBAL
 except ImportError:
     RTLD_NOW = 2
     RTLD_GLOBAL = 256
 
-oldflags = sys.getdlopenflags()
-sys.setdlopenflags(RTLD_NOW | RTLD_GLOBAL)
-from fusion import _fusion
-sys.setdlopenflags(oldflags)
+if hasattr(sys, "setdlopenflags"):
+    oldflags = sys.getdlopenflags()
+    sys.setdlopenflags(RTLD_NOW | RTLD_GLOBAL)
+    from fusion import _fusion
+    sys.setdlopenflags(oldflags)
 
 __version__ = "0.1.2-post"
