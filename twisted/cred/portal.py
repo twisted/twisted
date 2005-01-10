b2ade52490771e2c1796b2a50e357e0d984bcad8
spiv <spiv@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-01-10 23:48:37 +0000

    Commit expanded docstring I had lying around
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12852 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/cred/portal.py
+++ twisted/cred/portal.py
@@ -23,12 +23,21 @@ class IRealm(components.Interface):
     def requestAvatar(self, avatarId, mind, *interfaces):
         """Return avatar implementing one of the given interfaces.
 
-        Return a deferred which will fire a tuple of (interface,
-        avatarAspect, logout), or the tuple itself.  The interface
-        will be one of the interfaces passed in the 'interfaces'
-        argument.  The 'avatarAspect' will implement that interface.
-        The 'logout' object is a callable which will detach the mind
-        from the avatar.
+        @param avatarId: a string that identifies an avatar, as returned by
+            L{ICredentialsChecker.requestAvatarId<twisted.cred.checkers.ICredentialsChecker.requestAvatarId>}
+            (via a Deferred).  Alternatively, it may be
+            C{twisted.cred.checkers.ANONYMOUS}.
+        @param mind: usually None.  See the description of mind in
+            L{Portal.login}.
+        @param interfaces: the interface(s) the returned avatar should
+            implement, e.g.  C{IMailAccount}.  See the description of
+            L{Portal.login}.
+        
+        @returns: a deferred which will fire a tuple of (interface,
+            avatarAspect, logout), or the tuple itself.  The interface will be
+            one of the interfaces passed in the 'interfaces' argument.  The
+            'avatarAspect' will implement that interface.  The 'logout' object
+            is a callable which will detach the mind from the avatar.
         """
 
 
