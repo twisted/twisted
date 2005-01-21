ed6ea18bc66d16896128a7b38c5ad074cb5aee10
z3p <z3p@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-01-21 22:12:52 +0000

    Update conch metadata to match the webbarsite
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12893 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/topfiles/setup.py
+++ twisted/conch/topfiles/setup.py
@@ -6,14 +6,15 @@ if __name__ == '__main__':
     dist.setup(
         twisted_subproject=sys.argv[0],
         # metadata
-        name="Twisted Conch",
-        version="0.1.0",
-        description="Twisted Conch is a ssh implementation.",
+        name="Conch",
+        version="0.9",
+        description="Conch is a SSHv2 implementation.",
         author="Twisted Matrix Laboratories",
         author_email="twisted-python@twistedmatrix.com",
         maintainer="Paul Swartz",
         maintainer_email="z3p@twistedmatrix.com",
         url="http://twistedmatrix.com/projects/conch/",
         license="MIT",
-        long_description="Twisted conch is a ssh implementation.",
+        long_description="Conch is an SSHv2 implementation.  It includes a "
+                        "server, client, a SFTP client, and a key generator."
     )
