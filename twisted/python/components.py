00ebf1a1b482ac1f5028e713c4547eaca848cfb3
foom <foom@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-01-28 19:27:10 +0000

    Bug fixed in z.i 3.0.1b1 and later.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12912 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/components.py
+++ twisted/python/components.py
@@ -108,14 +108,6 @@ def getAdapterFactory(fromInterface, toInterface, default):
 
 # add global adapter lookup hook for our newly created registry
 def _hook(iface, ob, lookup=globalRegistry.lookup1):
-    # FIXME: Workaround zope.interface bug:
-    # Once you look up something in AdapterRegistry,
-    # any further 'classProvides(Only)' statements will not work.
-    # Until this is fixed, make *sure* fixClassImplements was
-    # called before looking anything up in AdapterRegistry.
-    # http://collector.zope.org/Zope3-dev/349
-    if hasattr(ob, '__class__'):
-        fixClassImplements(ob.__class__)
     factory = lookup(declarations.providedBy(ob), iface)
     if factory is None:
         return None
