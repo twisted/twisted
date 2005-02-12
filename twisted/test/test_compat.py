c7d1e168f8f2b172231ddd37878c7c22a01b8886
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-02-12 23:37:11 +0000

    Revert 12988 :(  These functions need more testing in different environments, I guess.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@12990 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_compat.py
+++ twisted/test/test_compat.py
@@ -4,7 +4,7 @@
 
 from twisted.trial import unittest
 
-import os, sys, types, socket
+import os, sys, types
 
 class IterableCounter:
     def __init__(self, lim=0):
@@ -19,7 +19,7 @@ class IterableCounter:
         if self.i >= self.lim:
             raise StopIteration
         return self.i
-
+        
 class CompatTestCase(unittest.TestCase):
     def testDict(self):
         d1 = {'a': 'b'}
@@ -46,7 +46,7 @@ class CompatTestCase(unittest.TestCase):
 
     def testIteration(self):
         lst1, lst2 = range(10), []
-
+        
         for i in iter(lst1):
             lst2.append(i)
         self.assertEquals(lst1, lst2)
@@ -78,7 +78,7 @@ class CompatTestCase(unittest.TestCase):
         for i in iter(IterableCounter(20).next, 10):
             lst2.append(i)
         self.assertEquals(lst1, lst2)
-
+        
     def testIsinstance(self):
         self.assert_(isinstance(u'hi', types.StringTypes))
         self.assert_(isinstance(self, unittest.TestCase))
@@ -101,48 +101,3 @@ class CompatTestCase(unittest.TestCase):
         self.assertEquals(' \tx x'.strip('\t '), 'x x')
         self.assertEquals(' x\t '.strip(' \t'), 'x')
         self.assertEquals('x x \t'.strip(' \t'), 'x x')
-
-    def testNToP(self):
-        f = lambda a: socket.inet_ntop(socket.AF_INET6, a)
-        g = lambda a: socket.inet_ntop(socket.AF_INET, a)
-
-        self.assertEquals('::', f('\x00' * 16))
-        self.assertEquals('::1', f('\x00' * 15 + '\x01'))
-        self.assertEquals(
-            'aef:b01:506:1001:ffff:9997:55:170',
-            f('\x0a\xef\x0b\x01\x05\x06\x10\x01\xff\xff\x99\x97\x00\x55\x01\x70'))
-
-        self.assertEquals('1.0.1.0', g('\x01\x00\x01\x00'))
-        self.assertEquals('170.85.170.85', g('\xaa\x55\xaa\x55'))
-        self.assertEquals('255.255.255.255', g('\xff\xff\xff\xff'))
-
-        self.assertEquals('100::', f('\x01' + '\x00' * 15))
-        self.assertEquals('100::1', f('\x01' + '\x00' * 14 + '\x01'))
-
-    def testPToN(self):
-        f = lambda a: socket.inet_pton(socket.AF_INET6, a)
-        g = lambda a: socket.inet_pton(socket.AF_INET, a)
-
-        self.assertEquals('\x00\x00\x00\x00', g('0.0.0.0'))
-        self.assertEquals('\xff\x00\xff\x00', g('255.0.255.0'))
-        self.assertEquals('\xaa\xaa\xaa\xaa', g('170.170.170.170'))
-
-        self.assertEquals('\x00' * 16, f('::'))
-        self.assertEquals('\x00' * 16, f('0::0'))
-        self.assertEquals('\x00\x01' + '\x00' * 14, f('1::'))
-        self.assertEquals(
-            '\x45\xef\x76\xcb\x00\x1a\x56\xef\xaf\xeb\x0b\xac\x19\x24\xae\xae',
-            f('45ef:76cb:1a:56ef:afeb:bac:1924:aeae'))
-
-        self.assertEquals('\x00' * 14 + '\x00\x01', f('::1'))
-        self.assertEquals('\x00' * 12 + '\x01\x02\x03\x04', f('::1.2.3.4'))
-        self.assertEquals(
-            '\x00\x01\x00\x02\x00\x03\x00\x04\x00\x05\x00\x06\x01\x02\x03\xff',
-            f('1:2:3:4:5:6:1.2.3.255'))
-
-        for badaddr in ['1:2:3:4:5:6:7:8:', ':1:2:3:4:5:6:7:8', '1::2::3',
-                        '1:::3', ':::', '1:2', '::1.2', '1.2.3.4::',
-                        'abcd:1.2.3.4:abcd:abcd:abcd:abcd:abcd',
-                        '1234:1.2.3.4:1234:1234:1234:1234:1234:1234',
-                        '1.2.3.4']:
-            self.assertRaises(ValueError, f, badaddr)
