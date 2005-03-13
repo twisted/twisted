1c14bcc6f0d8318b6e3ca348b77e6a013edec82e
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-03-13 10:56:15 +0000

    Fix another UNIXism - sys.path contains relative pathnames on Win32
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13173 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/plugins/__init__.py
+++ twisted/plugins/__init__.py
@@ -12,6 +12,6 @@ the __path__ variable.
 """
 
 import os, sys
-__path__ = [os.path.join(x, 'twisted', 'plugins') for x in sys.path]
+__path__ = [os.path.abspath(os.path.join(x, 'twisted', 'plugins')) for x in sys.path]
 
 __all__ = []                    # nothing to see here, move along, move along
