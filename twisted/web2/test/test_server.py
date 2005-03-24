818f06a0093da03579da7cfcb735dae7d466d41a
dreid <dreid@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-03-24 19:48:14 +0000

    Beginning of a unittest harness for twisted.web2, unit tests to follow.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13302 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web2/test/test_server.py
+++ twisted/web2/test/test_server.py
@@ -2,18 +2,20 @@
 A test harness for the twisted.web2 server.
 """
 
-from zope.interfaces import implements
+from zope.interface import implements
 from twisted.web2 import http
 from twisted.web2 import http_headers
 from twisted.web2 import iweb
 from twisted.web2 import server
+from twisted.trial import unittest, util, assertions
+from twisted.internet import defer
 
 class FakeChanRequest:
     implements(iweb.IChanRequest)
 
     def __init__(self, site):
         self.request_method = 'GET'
-        self.prepathuri = ''
+        self.prepath = ''
         self.uri = ''
         self.http_version = (1, 1)
         self.site = site
@@ -26,17 +28,22 @@ class FakeChanRequest:
                                       site=self.site,
                                       prepathuri=self.prepath)
 
+        self.data = ''
+        self.responseHeaders = None
+        self.code = None
+
     def writeIntermediateResponse(code, headers=None):
         pass
     
     def writeHeaders(self, code, headers):
-        pass
-
+        self.responseHeaders = headers
+        self.code = code
+        
     def write(self, data):
-        pass
+        self.data += data
 
     def finish(self):
-        pass
+        self.finished = True
 
     def abortConnection(self):
         pass
@@ -47,4 +54,17 @@ class FakeChanRequest:
     def unregisterProducer(self):
         pass
 
-    
+from twisted.web2 import resource
+from twisted.web2 import stream
+from twisted.web2 import http
+from twisted.web2 import responsecode
+
+class FakeResource(resource.Resource):
+    def child_foo(self, ctx, segments):
+        return self, segments[1:]
+
+    def render(self, ctx):
+        return http.Response(
+            responsecode.OK,
+            stream.MemoryStream("Ok"))
+
