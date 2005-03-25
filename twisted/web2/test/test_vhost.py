b5d180e1eab1245d6cb1c3d38d75326028e65311
dreid <dreid@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-03-25 01:56:18 +0000

    Tests for NameVirtualHost
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13308 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/web2/test/test_vhost.py
@@ -0,0 +1,46 @@
+from twisted.web2.test.test_server import BaseCase
+from twisted.web2 import resource
+from twisted.web2 import vhost
+from twisted.web2 import http, responsecode
+from twisted.web2 import iweb
+from twisted.web2 import stream
+
+class HostResource(resource.Resource):
+    addSlash = True
+    
+    def child_bar(self, ctx):
+        return self
+
+    def render(self, ctx):
+        h = iweb.IRequest(ctx).getHost()
+        return http.Response(responsecode.OK, stream=stream.MemoryStream(h))
+
+class TestVhost(BaseCase):
+    root = vhost.NameVirtualHost(default=HostResource())
+
+    def setUp(self):
+        self.root.addHost('foo', HostResource())
+
+    def testNameVirtualHost(self):
+        self.assertResponse(
+            (self.root, 'http://localhost/'),
+            (200, {}, 'localhost'))
+
+        self.assertResponse(
+            (self.root, 'http://foo/'),
+            (200, {}, 'foo'))
+
+    def testNameVirtualHostWithChildren(self):
+        self.assertResponse(
+            (self.root, 'http://foo/bar'),
+            (200, {}, 'foo'))
+
+    def testNameVirtualHostWithNesting(self):
+        nested = vhost.NameVirtualHost()
+        nested.addHost('is.nested', HostResource())
+        self.root.addHost('nested', nested)
+
+        self.assertResponse(
+            (self.root, 'http://is.nested/'),
+            (200, {}, 'is.nested'))
+        
