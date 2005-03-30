003562362f7e02160fedbbba3bf21514c69a0d11
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-03-30 00:10:02 +0000

    woops, fix fqpn
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13339 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web2/resource.py
+++ twisted/web2/resource.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twisted.web2.test_server -*-
+# -*- test-case-name: twisted.web2.test.test_server -*-
 # Copyright (c) 2001-2004 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
