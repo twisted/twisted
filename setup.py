8c8c2d7bd9aea7af1b067e78c79b3ca749306942
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-04-01 20:55:05 +0000

    add news to list of subprojects
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13387 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Distutils-launcher for Twisted projects.
 import sys, os, os.path
 
 subprojects = ('core', 'conch', 'flow', 'lore', 'mail', 'names', 'pair',
-               'runner', 'web', 'words', 'xish')
+               'runner', 'web', 'words', 'xish', 'news')
 
 specialPaths = {'core': 'twisted/topfiles/setup.py'}
 specialModules = {'core': 'twisted'}
