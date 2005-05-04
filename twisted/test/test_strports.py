67b1d6b02f0fa3aad8d8e3b6f4a7ba811c22853e
foom <foom@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-05-04 01:40:34 +0000

    Fix failing tests.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13712 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_strports.py
+++ twisted/test/test_strports.py
@@ -11,16 +11,16 @@ class ParserTestCase(unittest.TestCase):
 
     def testSimpleNumeric(self):
         self.assertEqual(strports.parse('80', self.f),
-                         ('TCP', (80, self.f), {'interface':'', 'backlog':5}))
+                         ('TCP', (80, self.f), {'interface':'', 'backlog':50}))
 
     def testSimpleTCP(self):
         self.assertEqual(strports.parse('tcp:80', self.f),
-                         ('TCP', (80, self.f), {'interface':'', 'backlog':5}))
+                         ('TCP', (80, self.f), {'interface':'', 'backlog':50}))
 
     def testInterfaceTCP(self):
         self.assertEqual(strports.parse('tcp:80:interface=127.0.0.1', self.f),
                          ('TCP', (80, self.f),
-                                 {'interface':'127.0.0.1', 'backlog':5}))
+                                 {'interface':'127.0.0.1', 'backlog':50}))
 
     def testBacklogTCP(self):
         self.assertEqual(strports.parse('tcp:80:backlog=6', self.f),
@@ -30,29 +30,29 @@ class ParserTestCase(unittest.TestCase):
     def testSimpleUnix(self):
         self.assertEqual(strports.parse('unix:/var/run/finger', self.f),
                          ('UNIX', ('/var/run/finger', self.f),
-                                 {'mode':0666, 'backlog':5}))
+                                 {'mode':0666, 'backlog':50}))
 
     def testModedUNIX(self):
         self.assertEqual(strports.parse('unix:/var/run/finger:mode=0660',
                                         self.f),
                          ('UNIX', ('/var/run/finger', self.f),
-                                 {'mode':0660, 'backlog':5}))
+                                 {'mode':0660, 'backlog':50}))
 
     def testAllKeywords(self):
         self.assertEqual(strports.parse('port=80', self.f),
-                         ('TCP', (80, self.f), {'interface':'', 'backlog':5}))
+                         ('TCP', (80, self.f), {'interface':'', 'backlog':50}))
 
     def testEscape(self):
         self.assertEqual(strports.parse(r'unix:foo\:bar\=baz\:qux\\', self.f),
                          ('UNIX', ('foo:bar=baz:qux\\', self.f),
-                                 {'mode':0666, 'backlog':5}))
+                                 {'mode':0666, 'backlog':50}))
 
     def testImpliedEscape(self):
         self.assertEqual(strports.parse(r'unix:address=foo=bar', self.f),
                          ('UNIX', ('foo=bar', self.f),
-                                 {'mode':0666, 'backlog':5}))
+                                 {'mode':0666, 'backlog':50}))
 
     def testNonstandardDefault(self):
         self.assertEqual(strports.parse('filename', self.f, 'unix'),
                          ('UNIX', ('filename', self.f),
-                                 {'mode':0666, 'backlog':5}))
+                                 {'mode':0666, 'backlog':50}))
