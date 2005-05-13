8d8847b68fede653a0302a578ef584723d05ec9f
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-05-13 02:37:02 +0000

    poor typing
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13789 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/trial/test/test_adapters.py
+++ twisted/trial/test/test_adapters.py
@@ -105,7 +105,7 @@ class TestFailureFormatting(common.RegistryBaseMixin, unittest.TestCase):
         self.suite.addTestClass(erroneous.TestFailureInSetUp)
         self.suite.run()
         
-        output = self.tm,formatError().split('\n')
+        output = self.tm.formatErrors().split('\n')
         
         expect = [reporter.DOUBLE_SEPARATOR,
                   '[ERROR]: testMethod (twisted.trial.test.erroneous.TestFailureInSetUp)']
@@ -118,7 +118,7 @@ class TestFailureFormatting(common.RegistryBaseMixin, unittest.TestCase):
         self.suite.addMethod(common.FailfulTests.testFailure)
         self.suite.run()
 
-        output = self.tm.formatError().split('\n')
+        output = self.tm.formatErrors().split('\n')
         common.stringComparison(expectTestFailure, output)
 
     def testTrimFilename(self):
