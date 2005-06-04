9d151132d39d4c2d3b2ae2240082566f39107c8b
mg <mg@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-06-04 22:08:34 +0000

    Make the example work. The browser requests at least '/' anyway, so we don't want an extra '/' prepended when it's sent to the server behind the proxy.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@13925 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/web/examples/proxy.py
+++ doc/web/examples/proxy.py
@@ -6,6 +6,6 @@
 from twisted.internet import reactor
 from twisted.web import proxy, server
 
-site = server.Site(proxy.ReverseProxyResource('www.yahoo.com', 80, '/'))
+site = server.Site(proxy.ReverseProxyResource('www.yahoo.com', 80, ''))
 reactor.listenTCP(8080, site)
 reactor.run()
