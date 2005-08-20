c3792d54b50e35dce2da6153d60fd5c66b895363
mary <mary@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-08-20 07:50:09 +0000

    Add multicast UDP explanation and examples by Kyle Robertson. [fixes issue 1084]
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14277 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/core/howto/listings/udp/MulticastClient.py
@@ -0,0 +1,12 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from twisted.application.internet import MulticastServer
+
+class MulticastClientUDP(DatagramProtocol):
+
+    def datagramReceived(self, datagram, address):
+            print "Received:" + repr(datagram)
+
+# Send multicast on 224.0.0.1:8005, on our dynamically allocated port
+reactor.listenUDP(0, MulticastClientUDP()).write('UniqueID', ('224.0.0.1', 8005))
+reactor.run()
---
+++ doc/core/howto/listings/udp/MulticastServer.py
@@ -0,0 +1,24 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from twisted.application.internet import MulticastServer
+
+class MulticastServerUDP(DatagramProtocol):
+    def startProtocol(self):
+        print 'Started Listening'
+        # Join a specific multicast group, which is the IP we will respond to
+        self.transport.joinGroup('224.0.0.1')
+
+    def datagramReceived(self, datagram, address):
+        # The uniqueID check is to ensure we only service requests from ourselves
+        if datagram == 'UniqueID':
+            print "Server Received:" + repr(datagram)
+            self.transport.write("data", address)
+
+# Note that the join function is picky about having a unique object
+# on which to call join.  To avoid using startProtocol, the following is
+# sufficient:
+#reactor.listenMulticast(8005, MulticastServerUDP()).join('224.0.0.1')
+
+# Listen for multicast on 224.0.0.1:8005
+reactor.listenMulticast(8005, MulticastServerUDP())
+reactor.run()
