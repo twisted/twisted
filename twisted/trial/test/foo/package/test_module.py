8688ab34d64ec5c938f168ac601f6277fada64a0
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-09-05 08:57:34 +0000

    Stack of tests for trial's output for various forms of failed import
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14359 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/trial/test/foo/package/test_module.py
@@ -0,0 +1,5 @@
+from twisted.trial import unittest
+
+class FooTest(unittest.TestCase):
+    def testFoo(self):
+        pass
--- twisted/trial/test/test_output.py
+++ twisted/trial/test/test_output.py
@@ -1,5 +1,6 @@
 from twisted.trial import unittest
 from twisted.trial.assertions import FailTest
+from twisted.python import util
 from twisted.internet import utils, reactor, interfaces
 import os, re
 
@@ -8,11 +9,6 @@ def getTrialPath():
     trialPath = fp.split(os.path.sep)[:-3] + ['bin', 'trial']
     return os.path.normpath(os.path.join(fp, os.pardir, os.pardir,
                                          os.pardir, 'bin', 'trial'))
-
-
-def runTrial(*args):
-    return utils.getProcessOutput(getTrialPath(), args=args, errortoo=1,
-                                  env=os.environ)
 
 
 if not interfaces.IReactorProcess.providedBy(reactor):
@@ -26,25 +22,124 @@ class TestImportErrors(unittest.TestCase):
     ## XXX -- ideally refactor the trial top-level stuff so we don't have to
     ## shell out for this stuff.
 
+    debug = False
+
+    def runTrial(self, *args):
+        path = self._getMockPath()
+        env = os.environ.copy()
+        if not env.has_key('PYTHONPATH'):
+            env['PYTHONPATH'] = path
+        else:
+            env['PYTHONPATH'] += os.pathsep + path
+        d = utils.getProcessOutput(getTrialPath(), args=args, errortoo=1,
+                                   env=env)
+        if self.debug:
+            d.addCallback(self._print)
+        return d
+
+    def _print(self, stuff):
+        print stuff
+        return stuff
+
+    def _getMockPath(self):
+        from twisted.trial import test
+        return os.path.normpath(util.sibpath(test.__file__, 'foo'))
+
+    def failUnlessIn(self, container, containee, *args, **kwargs):
+        # redefined to be useful in callbacks
+        unittest.TestCase.failUnlessIn(self, containee, container,
+                                       *args, **kwargs)
+        return container
+
+    def failIfIn(self, container, containee, *args, **kwargs):
+        # redefined to be useful in callbacks
+        unittest.TestCase.failIfIn(self, containee, container,
+                                   *args, **kwargs)
+        return container
+
     def test_trialFound(self):
         self.failUnless(os.path.isfile(getTrialPath()), getTrialPath())
 
+    def test_mockPathCorrect(self):
+        # This doesn't test a feature.  This tests that we are accurately finding
+        # the directory with all of the mock modules and packages.
+        path = self._getMockPath()
+        self.failUnless(path.endswith('twisted/trial/test/foo'))
+        self.failUnless(os.path.isdir(path))
+
     def test_trialRun(self):
-        d = runTrial('--help')
-        d.addCallback(lambda x : self.failUnless('trial' in x))
+        d = self.runTrial('--help')
+        d.addCallback(self.failUnless, 'trial')
         return d
 
     def test_nonexistentModule(self):
-        d = runTrial('twisted.doesntexist')
-        d.addCallback(lambda x : self.failUnlessIn('IMPORT ERROR', x) and x)
-        d.addCallback(lambda x : self.failUnlessIn('twisted.doesntexist', x))
+        d = self.runTrial('twisted.doesntexist')
+        d.addCallback(self.failUnlessIn, 'IMPORT ERROR')
+        d.addCallback(self.failUnlessIn, 'twisted.doesntexist')
         return d
 
+    def test_nonexistentPackage(self):
+        d = self.runTrial('doesntexist')
+        d.addCallback(self.failUnlessIn, 'doesntexist')
+        d.addCallback(self.failUnlessIn, 'IOError')
+        d.addCallback(self.failIfIn, 'IMPORT ERROR')
+        return d
+
+    def test_nonexistentPackageWithModule(self):
+        d = self.runTrial('doesntexist.barney')
+        d.addCallback(self.failUnlessIn, 'doesntexist.barney')
+        d.addCallback(self.failUnlessIn, 'IOError')
+        d.addCallback(self.failIfIn, 'IMPORT ERROR')
+        return d
+
+    def test_badpackage(self):
+        d = self.runTrial('badpackage')
+        d.addCallback(self.failUnlessIn, 'IMPORT ERROR')
+        d.addCallback(self.failUnlessIn, 'badpackage')
+        d.addCallback(self.failIfIn, 'IOError')
+        return d
+
+    def test_moduleInBadpackage(self):
+        d = self.runTrial('badpackage.test_module')
+        d.addCallback(self.failUnlessIn, "IMPORT ERROR")
+        d.addCallback(self.failUnlessIn, "badpackage.test_module")
+        d.addCallback(self.failIfIn, 'IOError')
+        return d
+
+    def test_badmodule(self):
+        d = self.runTrial('package.test_bad_module')
+        d.addCallback(self.failUnlessIn, 'IMPORT ERROR')
+        d.addCallback(self.failUnlessIn, 'package.test_bad_module')
+        d.addCallback(self.failIfIn, 'IOError')
+        d.addCallback(self.failIfIn, '<module')
+        return d
+
+    def test_badimport(self):
+        d = self.runTrial('package.test_import_module')
+        d.addCallback(self.failUnlessIn, 'IMPORT ERROR')
+        d.addCallback(self.failUnlessIn, 'package.test_import_module')
+        d.addCallback(self.failIfIn, 'IOError')
+        d.addCallback(self.failIfIn, '<module')
+        return d
+
+    def test_recurseImport(self):
+        d = self.runTrial('-R', 'package')
+        d.addCallback(self.failUnlessIn, 'IMPORT ERROR')
+        d.addCallback(self.failUnlessIn, 'package.test_bad_module')
+        d.addCallback(self.failUnlessIn, 'package.test_import_module')
+        d.addCallback(self.failIfIn, '<module')
+        d.addCallback(self.failIfIn, 'IOError')
+        return d
+    test_recurseImport.todo = "This is a regression, I think -- jml"
+
+    def test_regularRun(self):
+        d = self.runTrial('package.test_module')
+        d.addCallback(self.failIfIn, 'IMPORT ERROR')
+        d.addCallback(self.failIfIn, 'IOError')
+        d.addCallback(self.failUnlessIn, 'OK')
+        d.addCallback(self.failUnlessIn, 'PASSED (successes=1)')
+        return d
+    
     ## XXX -- needs tests for
-    ## - nonexistent toplevel package
-    ## - nonexistent toplevel package with module
-    ## - package that doesn't import
-    ## - module inside package that doesn't import
-    ## - module that doesn't import inside package
     ## - recursive on package that contains module that doesn't import
     ## - package / modules that _do_ import
