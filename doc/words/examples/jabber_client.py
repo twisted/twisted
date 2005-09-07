70a70a39706333770817cf7d1cebf770c41dfbae
ralphm <ralphm@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-09-07 19:54:10 +0000

    Add jabber client example, originally by Vandorp.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14374 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/words/examples/jabber_client.py
@@ -0,0 +1,27 @@
+#
+# Originally written by Darryl Vandorp
+# http://randomthoughts.vandorp.ca/
+
+from twisted.words.protocols.jabber import client, jid
+from twisted.xish import domish
+from twisted.internet import reactor
+        
+def authd(xmlstream):
+    print "authenticated"
+
+    presence = domish.Element(('jabber:client','presence'))
+    xmlstream.send(presence)
+    
+    xmlstream.addObserver('/message',  debug)
+    xmlstream.addObserver('/presence', debug)
+    xmlstream.addObserver('/iq',       debug)   
+
+def debug(elem):
+    print elem.toXml().encode('utf-8')
+    print "="*20
+    
+myJid = jid.JID('username@server.jabber/twisted_words')
+factory = client.basicClientFactory(myJid, 'password')
+factory.addBootstrap('//event/stream/authd',authd)
+reactor.connectTCP('server.jabber',5222,factory)
+reactor.run()
