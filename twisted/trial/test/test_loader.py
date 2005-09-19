e8060c15c5759a6d610d32bc553d6976bf277882
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-09-19 11:30:11 +0000

    Comments describing the tests I want.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14489 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/trial/test/test_loader.py
+++ twisted/trial/test/test_loader.py
@@ -2,6 +2,32 @@ from twisted.trial import unittest
 from twisted.trial import runner, reporter
 
 class LoaderTest(unittest.TestCase):
+
+    ## FIXME -- Need tests for:
+    ## * loading package
+    ##   * including packages that contain modules with errors
+    ## * loading package recursively
+    ##   * including packages that contain modules with errors
+    ## * loading with custom sorter
+    ## * the default sort order (alphabetic)
+    ## * loading doctests
+    ## * loading individual methods
+    ## * the created suites are of the right kind
+    ##   (e.g. ModuleSuite for modules)
+    ## * Calling loadModule on a non-module
+    ## * Calling loadClass on a non-class
+    ## * Calling loadClass on a non-TestCase
+
+    ## FIXME -- Need tests (and implementations) for:
+    ## * loadAnything loads arbitrary python objects correctly
+    ## * Loading from a file
+    ##   * when it refers to a module (in sys.path and out of sys.path)
+    ##   * when it refers to a package (in sys.path and out of sys.path,
+    ##     a directory or __init__.py)
+    ## * Loading from a string
+    ##   * could be a file / directory
+    ##   * could be name of a python object
+    
     def setUp(self):
         self.reporter = reporter.Reporter()
         self.loader = runner.TestLoader(self.reporter)
