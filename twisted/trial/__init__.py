997957339ef4e023b14b3f288591361d220cc6d0
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-09-24 02:09:03 +0000

    Remove twisted.trial.remote.  Logic in __init__ no longer necessary
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14546 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/trial/__init__.py
+++ twisted/trial/__init__.py
@@ -1,29 +1,6 @@
-# -*- test-case-name: twisted.trial.test.test_trial -*-
-#
 # Copyright (c) 2001-2004 Twisted Matrix Laboratories.
 # See LICENSE for details.
 #
-# Author: Jonathan D. Simms <slyphon@twistedmatrix.com>
+# Maintainer: Jonathan Lange <jml@twistedmatrix.com>
 
 """Unit testing framework."""
-
-tbformat = 'plain'
-
-from twisted.python import components
-try:
-    from zope.interface import interface, declarations
-    from zope.interface.adapter import AdapterRegistry
-except ImportError:
-    raise ImportError, "you need zope.interface installed (http://zope.org/Products/ZopeInterface/)"
-
-def _setUpAdapters():
-    from twisted.spread import jelly
-    # sibling imports
-    import itrial, remote
-    components.registerAdapter(remote.JellyableTestMethod,
-                               itrial.ITestMethod,
-                               jelly.IJellyable)
-
-
-_setUpAdapters()
-
