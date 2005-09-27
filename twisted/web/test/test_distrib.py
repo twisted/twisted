5775a978b1879c95017eac2d050957d5c597371b
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-09-27 06:46:47 +0000

    Cease using trial.util
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14579 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/test/test_distrib.py
+++ twisted/web/test/test_distrib.py
@@ -2,9 +2,9 @@
 
 import sys
 
-from twisted.trial import unittest, util
+from twisted.trial import unittest
 from twisted.web import http, distrib, client, resource, static, server
-from twisted.internet import reactor, base
+from twisted.internet import reactor, base, defer
 from twisted.spread import pb
 from twisted.python import log, util as tputil
 
@@ -18,18 +18,21 @@ class MySite(server.Site):
 class DistribTest(unittest.TestCase):
     port1 = None
     port2 = None
+    sub = None
 
     def setUp(self):
         base.DelayedCall.debug = True
 
     def tearDown(self):
+        if self.sub is not None:
+            self.sub.publisher.broker.transport.loseConnection()
         http._logDateTimeStop()
+        dl = []
         if self.port1 is not None:
-            d = self.port1.stopListening()
-            unittest.wait(d)
+            dl.append(self.port1.stopListening())
         if self.port2 is not None:
-            d = self.port2.stopListening()
-            unittest.wait(d)
+            dl.append(self.port2.stopListening())
+        return defer.gatherResults(dl)
 
     def testDistrib(self):
         # site1 is the publisher
@@ -38,26 +41,14 @@ class DistribTest(unittest.TestCase):
         site1 = server.Site(r1)
         f1 = pb.PBServerFactory(distrib.ResourcePublisher(site1))
         self.port1 = reactor.listenTCP(0, f1)
-
-        util.spinUntil(lambda :self.port1.connected)
-
-        # site2 is the subscriber
-        sub = distrib.ResourceSubscription("127.0.0.1",
-                                           self.port1.getHost().port)
+        self.sub = distrib.ResourceSubscription("127.0.0.1",
+                                                self.port1.getHost().port)
         r2 = resource.Resource()
-        r2.putChild("here", sub)
+        r2.putChild("here", self.sub)
         f2 = MySite(r2)
         self.port2 = reactor.listenTCP(0, f2)
-
-        util.spinUntil(lambda :self.port2.connected)
-
-        # then we hit site2 with a client
         d = client.getPage("http://127.0.0.1:%d/here/there" % \
                            self.port2.getHost().port)
-        res = util.wait(d, timeout=1.0)
-        self.failUnlessEqual(res, "root")
-
-        # A bit of a hack: force the pb client to disconnect, for cleanup
-        # purposes.
-        sub.publisher.broker.transport.loseConnection()
+        d.addCallback(self.failUnlessEqual, 'root')
+        return d
 
