375f621b528968ec78885dfa2fb859c2746baa22
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-10-02 16:45:55 +0000

    move this code someplace importable
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@14629 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ doc/conch/examples/window.tac
@@ -0,0 +1,173 @@
+from __future__ import division
+
+import string, random
+
+from twisted.python import log
+from twisted.internet import protocol
+from twisted.application import internet, service
+from twisted.cred import checkers, portal
+
+from twisted.conch.insults import insults, window
+from twisted.conch.telnet import TelnetTransport, TelnetBootstrapProtocol
+from twisted.conch.manhole_ssh import ConchFactory, TerminalRealm
+
+from twisted.internet import reactor
+
+class DrawableCanvas(window.Canvas):
+    x = 0
+    y = 0
+
+    def func_LEFT_ARROW(self, modifier):
+        self.x -= 1
+
+    def func_RIGHT_ARROW(self, modifier):
+        self.x += 1
+
+    def func_UP_ARROW(self, modifier):
+        self.y -= 1
+
+    def func_DOWN_ARROW(self, modifier):
+        self.y += 1
+
+    def characterReceived(self, keyID, modifier):
+        self[self.x, self.y] = keyID
+        self.x += 1
+
+    def keystrokeReceived(self, keyID, modifier):
+        if keyID == '\r' or keyID == '\v':
+            return
+        window.Canvas.keystrokeReceived(self, keyID, modifier)
+        if self.x >= self.width:
+            self.x = 0
+        elif self.x < 0:
+            self.x = self.width - 1
+
+        if self.y >= self.height:
+            self.y = 0
+        elif self.y < 0:
+            self.y = self.height - 1
+
+    def render(self, width, height, terminal):
+        window.Canvas.render(self, width, height, terminal)
+        if self.focused:
+            terminal.cursorPosition(self.x, self.y)
+            window.cursor(terminal, self[self.x, self.y])
+
+
+class CanvasDemo(insults.TerminalProtocol):
+    width = 80
+    height = 24
+
+    def connectionMade(self):
+        self.terminal.eraseDisplay()
+        self.terminal.resetPrivateModes([insults.privateModes.CURSOR_MODE])
+        self.canvases = []
+        self.window = window.TopWindow()
+        self.packer = window.Packer()
+        self.addCanvas()
+        self.window.addChild(self.packer)
+        self.terminalSize(self.width, self.height)
+
+    def terminalSize(self, width, height):
+        self.width = width
+        self.height = height
+        self.window.render(self.width, self.height, self.terminal)
+
+    def addCanvas(self):
+        self.canvases.append(DrawableCanvas(self.width, self.height))
+        self.packer.addChild(self.canvases[-1])
+
+    def remCanvas(self):
+        self.packer.remChild(self.canvases.pop())
+
+    def keystrokeReceived(self, keyID, modifier):
+        if keyID == '\r':
+            self.addCanvas()
+        elif keyID == self.terminal.BACKSPACE:
+            self.remCanvas()
+        else:
+            self.window.keystrokeReceived(keyID, modifier)
+        self.window.render(self.width, self.height, self.terminal)
+
+
+class ButtonDemo(insults.TerminalProtocol):
+    width = 80
+    height = 24
+
+    def connectionMade(self):
+        self.terminal.eraseDisplay()
+        self.terminal.resetPrivateModes([insults.privateModes.CURSOR_MODE])
+
+        self.window = window.TopWindow()
+        self.output = window.TextOutput((15, 1))
+        self.input = window.TextInput(15, self._setText)
+        self.select1 = window.Selection(map(str, range(100)), self._setText, 10)
+        self.select2 = window.Selection(map(str, range(200, 300)), self._setText, 10)
+        self.button = window.Button("Clear", self._clear)
+        self.canvas = DrawableCanvas()
+
+        hbox = window.HBox()
+        hbox.addChild(self.input)
+        hbox.addChild(self.output)
+        hbox.addChild(window.Border(self.button))
+        hbox.addChild(window.Border(self.select1))
+        hbox.addChild(window.Border(self.select2))
+
+        vbox = window.VBox()
+        vbox.addChild(hbox)
+        vbox.addChild(window.Border(self.canvas))
+        self.window.addChild(vbox)
+        self.terminalSize(self.width, self.height)
+
+    def terminalSize(self, width, height):
+        self.width = width
+        self.height = height
+        self.terminal.eraseDisplay()
+        self.window.render(self.width, self.height, self.terminal)
+
+
+    def keystrokeReceived(self, keyID, modifier):
+        self.window.keystrokeReceived(keyID, modifier)
+        self.window.render(self.width, self.height, self.terminal)
+
+    def _clear(self):
+        self.canvas.clear()
+
+    def _setText(self, text):
+        self.input.setText('')
+        self.output.setText(text)
+
+
+def makeService(args):
+    checker = checkers.InMemoryUsernamePasswordDatabaseDontUse(username="password")
+
+    f = protocol.ServerFactory()
+    f.protocol = lambda: TelnetTransport(TelnetBootstrapProtocol,
+                                         insults.ServerProtocol,
+                                         args['protocolFactory'],
+                                         *args.get('protocolArgs', ()),
+                                         **args.get('protocolKwArgs', {}))
+    tsvc = internet.TCPServer(args['telnet'], f)
+
+    def chainProtocolFactory():
+        return insults.ServerProtocol(
+            args['protocolFactory'],
+            *args.get('protocolArgs', ()),
+            **args.get('protocolKwArgs', {}))
+
+    rlm = TerminalRealm()
+    rlm.chainedProtocolFactory = chainProtocolFactory
+    ptl = portal.Portal(rlm, [checker])
+    f = ConchFactory(ptl)
+    csvc = internet.TCPServer(args['ssh'], f)
+
+    m = service.MultiService()
+    tsvc.setServiceParent(m)
+    csvc.setServiceParent(m)
+    return m
+
+application = service.Application("Window Demo")
+
+makeService({'protocolFactory': ButtonDemo,
+             'telnet': 6023,
+             'ssh': 6022}).setServiceParent(application)
---
+++ twisted/conch/insults/window.py
@@ -0,0 +1,528 @@
+import array
+
+from twisted.conch.insults import insults
+
+class YieldFocus(Exception):
+    """Input focus manipulation exception
+    """
+
+class BoundedTerminalWrapper(object):
+    def __init__(self, terminal, width, height, xoff, yoff):
+        self.width = width
+        self.height = height
+        self.xoff = xoff
+        self.yoff = yoff
+        self.terminal = terminal
+        self.cursorForward = terminal.cursorForward
+        self.selectCharacterSet = terminal.selectCharacterSet
+        self.selectGraphicRendition = terminal.selectGraphicRendition
+        self.saveCursor = terminal.saveCursor
+        self.restoreCursor = terminal.restoreCursor
+
+    def cursorPosition(self, x, y):
+        return self.terminal.cursorPosition(
+            self.xoff + min(self.width, x),
+            self.yoff + min(self.height, y)
+            )
+
+    def cursorHome(self):
+        return self.terminal.cursorPosition(
+            self.xoff, self.yoff)
+
+    def write(self, bytes):
+        return self.terminal.write(bytes)
+
+class Widget(object):
+    focused = False
+
+    def render(self, terminal):
+        pass
+
+    def sizeHint(self):
+        return None
+
+    def keystrokeReceived(self, keyID, modifier):
+        if keyID == '\t':
+            self.tabReceived(modifier)
+        elif keyID == '\x7f':
+            self.backspaceReceived()
+        elif keyID in insults.FUNCTION_KEYS:
+            self.functionKeyReceived(keyID, modifier)
+        else:
+            self.characterReceived(keyID, modifier)
+
+    def tabReceived(self, modifier):
+        # XXX TODO - Handle shift+tab
+        raise YieldFocus()
+
+    def focusReceived(self):
+        """Called when focus is being given to this widget.
+
+        May raise YieldFocus is this widget does not want focus.
+        """
+        self.focused = True
+
+    def focusLost(self):
+        self.focused = False
+
+    def backspaceReceived(self):
+        pass
+
+    def functionKeyReceived(self, keyID, modifier):
+        func = getattr(self, 'func_' + keyID.name, None)
+        if func is not None:
+            func(modifier)
+
+    def characterReceived(self, keyID, modifier):
+        pass
+
+class ContainerWidget(Widget):
+    """
+    @ivar focusedChild: The contained widget which currently has
+    focus, or None.
+    """
+    focusedChild = None
+    focused = False
+
+    def __init__(self):
+        Widget.__init__(self)
+        self.children = []
+
+    def addChild(self, child):
+        self.children.append(child)
+        if self.focusedChild is None and self.focused:
+            try:
+                child.focusReceived()
+            except YieldFocus:
+                pass
+            else:
+                self.focusedChild = child
+
+    def remChild(self, child):
+        self.children.remove(child)
+
+    def render(self, width, height, terminal):
+        for ch in self.children:
+            ch.render(width, height, terminal)
+
+    def changeFocus(self):
+        if self.focusedChild is not None:
+            self.focusedChild.focusLost()
+            focusedChild = self.focusedChild
+            self.focusedChild = None
+            try:
+                curFocus = self.children.index(focusedChild) + 1
+            except ValueError:
+                raise YieldFocus()
+        else:
+            curFocus = 0
+        while curFocus < len(self.children):
+            try:
+                self.children[curFocus].focusReceived()
+            except YieldFocus:
+                curFocus += 1
+            else:
+                self.focusedChild = self.children[curFocus]
+                return
+        # None of our children wanted focus
+        raise YieldFocus()
+
+
+    def focusReceived(self):
+        self.changeFocus()
+        self.focused = True
+
+
+    def keystrokeReceived(self, keyID, modifier):
+        if self.focusedChild is not None:
+            try:
+                self.focusedChild.keystrokeReceived(keyID, modifier)
+            except YieldFocus:
+                self.changeFocus()
+        else:
+            Widget.keystrokeReceived(self, keyID, modifier)
+
+
+class TopWindow(ContainerWidget):
+    focused = True
+
+    def changeFocus(self):
+        try:
+            ContainerWidget.changeFocus(self)
+        except YieldFocus:
+            try:
+                ContainerWidget.changeFocus(self)
+            except YieldFocus:
+                pass
+
+    def keystrokeReceived(self, keyID, modifier):
+        try:
+            ContainerWidget.keystrokeReceived(self, keyID, modifier)
+        except YieldFocus:
+            self.changeFocus()
+
+
+class AbsoluteBox(ContainerWidget):
+    def moveChild(self, child, x, y):
+        for n in range(len(self.children)):
+            if self.children[n][0] is child:
+                self.children[n] = (child, x, y)
+                break
+        else:
+            raise ValueError("No such child", child)
+
+    def render(self, width, height, terminal):
+        for (ch, x, y) in self.children:
+            wrap = BoundedTerminalWrapper(terminal, width - x, height - y, x, y)
+            ch.render(width, height, wrap)
+
+
+class _Box(ContainerWidget):
+    TOP, CENTER, BOTTOM = range(3)
+
+    def __init__(self, gravity=CENTER):
+        ContainerWidget.__init__(self)
+        self.gravity = gravity
+
+    def sizeHint(self):
+        height = 0
+        width = 0
+        for ch in self.children:
+            hint = ch.sizeHint()
+            if hint is None:
+                return None
+
+            if self.variableDimension == 0:
+                width += hint[0]
+                height = max(height, hint[1])
+            else:
+                width = max(width, hint[0])
+                height += hint[1]
+
+        return width, height
+
+
+    def render(self, width, height, terminal):
+        if not self.children:
+            return
+
+        greedy = 0
+        wants = []
+        for ch in self.children:
+            hint = ch.sizeHint()
+            if hint is None:
+                greedy += 1
+                wants.append(None)
+            else:
+                wants.append(hint[self.variableDimension])
+
+        length = (width, height)[self.variableDimension]
+        totalWant = sum([w for w in wants if w is not None])
+        if greedy:
+            leftForGreedy = int((length - totalWant) / greedy)
+
+        widthOffset = heightOffset = 0
+
+        for want, ch in zip(wants, self.children):
+            if want is None:
+                want = leftForGreedy
+
+            subWidth, subHeight = width, height
+            if self.variableDimension == 0:
+                subWidth = want
+            else:
+                subHeight = want
+
+            wrap = BoundedTerminalWrapper(
+                terminal,
+                subWidth,
+                subHeight,
+                widthOffset,
+                heightOffset,
+                )
+            ch.render(subWidth, subHeight, wrap)
+            if self.variableDimension == 0:
+                widthOffset += want
+            else:
+                heightOffset += want
+
+
+class HBox(_Box):
+    variableDimension = 0
+
+class VBox(_Box):
+    variableDimension = 1
+
+
+class Packer(ContainerWidget):
+    def render(self, width, height, terminal):
+        if not self.children:
+            return
+
+        root = int(len(self.children) ** 0.5 + 0.5)
+        boxes = [VBox() for n in range(root)]
+        for n, ch in enumerate(self.children):
+            boxes[n % len(boxes)].addChild(ch)
+        h = HBox()
+        map(h.addChild, boxes)
+        return h.render(width, height, terminal)
+
+
+class Canvas(Widget):
+    focused = False
+
+    contents = None
+
+    def __init__(self):
+        Widget.__init__(self)
+        self.resize(1, 1)
+
+    def resize(self, width, height):
+        contents = array.array('c', ' ' * width * height)
+        if self.contents is not None:
+            for x in range(min(width, self.width)):
+                for y in range(min(height, self.height)):
+                    contents[width * y + x] = self[x, y]
+        self.contents = contents
+        self.width = width
+        self.height = height
+        if self.x >= width:
+            self.x = width - 1
+        if self.y >= height:
+            self.y = height - 1
+
+    def __getitem__(self, (x, y)):
+        return self.contents[(self.width * y) + x]
+
+    def __setitem__(self, (x, y), value):
+        self.contents[(self.width * y) + x] = value
+
+    def clear(self):
+        self.contents = array.array('c', ' ' * len(self.contents))
+
+    def render(self, width, height, terminal):
+        if not width or not height:
+            return
+
+        if width != self.width or height != self.height:
+            self.resize(width, height)
+        for i in range(height):
+            terminal.cursorPosition(0, i)
+            terminal.write(''.join(self.contents[self.width * i:self.width * i + self.width])[:width])
+
+
+def rectangle(terminal, (top, left), (width, height)):
+    terminal.selectCharacterSet(insults.CS_DRAWING, insults.G0)
+
+    terminal.cursorPosition(top, left)
+    terminal.write(chr(0154))
+    terminal.write(chr(0161) * (width - 2))
+    terminal.write(chr(0153))
+    for n in range(height - 2):
+        terminal.cursorPosition(left, top + n + 1)
+        terminal.write(chr(0170))
+        terminal.cursorForward(width - 2)
+        terminal.write(chr(0170))
+    terminal.cursorPosition(0, top + height - 1)
+    terminal.write(chr(0155))
+    terminal.write(chr(0161) * (width - 2))
+    terminal.write(chr(0152))
+
+    terminal.selectCharacterSet(insults.CS_US, insults.G0)
+
+class Border(Widget):
+    def __init__(self, containee):
+        Widget.__init__(self)
+        self.containee = containee
+
+    def focusReceived(self):
+        return self.containee.focusReceived()
+
+    def focusLost(self):
+        return self.containee.focusLost()
+
+    def keystrokeReceived(self, keyID, modifier):
+        return self.containee.keystrokeReceived(keyID, modifier)
+
+    def sizeHint(self):
+        hint = self.containee.sizeHint()
+        if hint is not None:
+            return hint[0] + 2, hint[1] + 2
+        return None
+
+    def render(self, width, height, terminal):
+        rectangle(terminal, (0, 0), (width, height))
+        wrap = BoundedTerminalWrapper(terminal, width - 2, height - 2, 1, 1)
+        self.containee.render(width - 2, height - 2, wrap)
+
+
+class Button(Widget):
+    def __init__(self, label, onPress):
+        Widget.__init__(self)
+
+        self.label = label
+        self.onPress = onPress
+
+    def sizeHint(self):
+        return len(self.label), 1
+
+    def characterReceived(self, keyID, modifier):
+        if keyID == '\r':
+            self.onPress()
+
+    def render(self, width, height, terminal):
+        terminal.cursorPosition(0, 0)
+        if self.focused:
+            terminal.write('\x1b[1m' + self.label + '\x1b[0m')
+        else:
+            terminal.write(self.label)
+
+class TextInput(Widget):
+    def __init__(self, maxwidth, onSubmit):
+        Widget.__init__(self)
+        self.onSubmit = onSubmit
+        self.maxwidth = maxwidth
+        self.buffer = ''
+        self.cursor = 0
+
+    def setText(self, text):
+        self.buffer = text[:self.maxwidth]
+        self.cursor = len(self.buffer)
+
+    def func_LEFT_ARROW(self, modifier):
+        if self.cursor > 0:
+            self.cursor -= 1
+
+    def func_RIGHT_ARROW(self, modifier):
+        if self.cursor < len(self.buffer):
+            self.cursor += 1
+
+    def backspaceReceived(self):
+        if self.cursor > 0:
+            self.buffer = self.buffer[:self.cursor - 1] + self.buffer[self.cursor:]
+            self.cursor -= 1
+
+    def characterReceived(self, keyID, modifier):
+        if keyID == '\r':
+            self.onSubmit(self.buffer)
+        else:
+            if len(self.buffer) < self.maxwidth:
+                self.buffer = self.buffer[:self.cursor] + keyID + self.buffer[self.cursor:]
+                self.cursor += 1
+
+    def sizeHint(self):
+        return self.maxwidth + 1, 1
+
+    def render(self, width, height, terminal):
+        terminal.cursorPosition(0, 0)
+        if self.focused:
+            terminal.write(self.buffer[:self.cursor])
+            cursor(terminal, self.buffer[self.cursor:self.cursor+1] or ' ')
+            terminal.write(self.buffer[self.cursor+1:])
+            terminal.write(' ' * (self.maxwidth - len(self.buffer) + 1))
+        else:
+            more = self.maxwidth - len(self.buffer)
+            terminal.write(self.buffer + '_' * more)
+
+class TextOutput(Widget):
+    text = ''
+
+    def __init__(self, size=None):
+        Widget.__init__(self)
+        self.size = size
+
+    def sizeHint(self):
+        return self.size
+
+    def render(self, width, height, terminal):
+        terminal.cursorPosition(0, 0)
+        text = self.text[:width]
+        terminal.write(text + ' ' * (width - len(text)))
+
+    def setText(self, text):
+        self.text = text
+
+    def focusReceived(self):
+        raise YieldFocus()
+
+def cursor(terminal, ch):
+    terminal.saveCursor()
+    terminal.selectGraphicRendition(str(insults.REVERSE_VIDEO))
+    terminal.write(ch)
+    terminal.restoreCursor()
+    terminal.cursorForward()
+
+class Selection(Widget):
+    # Index into the sequence
+    focusedIndex = 0
+
+    # Offset into the displayed subset of the sequence
+    renderOffset = 0
+
+    def __init__(self, sequence, onSelect, minVisible=None):
+        Widget.__init__(self)
+        self.sequence = sequence
+        self.onSelect = onSelect
+        self.minVisible = minVisible
+        if minVisible is not None:
+            self._width = max(map(len, self.sequence))
+
+    def sizeHint(self):
+        if self.minVisible is not None:
+            return self._width, self.minVisible
+
+    def func_UP_ARROW(self, modifier):
+        if self.focusedIndex > 0:
+            self.focusedIndex -= 1
+            if self.renderOffset > 0:
+                self.renderOffset -= 1
+
+    def func_PGUP(self, modifier):
+        if self.renderOffset != 0:
+            self.focusedIndex -= self.renderOffset
+            self.renderOffset = 0
+        else:
+            self.focusedIndex = max(0, self.focusedIndex - self.height)
+
+    def func_DOWN_ARROW(self, modifier):
+        if self.focusedIndex < len(self.sequence) - 1:
+            self.focusedIndex += 1
+            if self.renderOffset < self.height - 1:
+                self.renderOffset += 1
+
+
+    def func_PGDN(self, modifier):
+        if self.renderOffset != self.height - 1:
+            change = self.height - self.renderOffset - 1
+            if change + self.focusedIndex >= len(self.sequence):
+                change = len(self.sequence) - self.focusedIndex - 1
+            self.focusedIndex += change
+            self.renderOffset = self.height - 1
+        else:
+            self.focusedIndex = min(len(self.sequence) - 1, self.focusedIndex + self.height)
+
+    def characterReceived(self, keyID, modifier):
+        if keyID == '\r':
+            self.onSelect(self.sequence[self.focusedIndex])
+
+    def render(self, width, height, terminal):
+        self.height = height
+        start = self.focusedIndex - self.renderOffset
+        if start > len(self.sequence) - height:
+            start = max(0, len(self.sequence) - height)
+
+        elements = self.sequence[start:start+height]
+
+        for n, ele in enumerate(elements):
+            terminal.cursorPosition(0, n)
+            if n == self.renderOffset:
+                terminal.saveCursor()
+                if self.focused:
+                    modes = str(insults.REVERSE_VIDEO), str(insults.BOLD)
+                else:
+                    modes = str(insults.REVERSE_VIDEO),
+                terminal.selectGraphicRendition(*modes)
+            text = ele[:width]
+            terminal.write(text + (' ' * (width - len(text))))
+            if n == self.renderOffset:
+                terminal.restoreCursor()
