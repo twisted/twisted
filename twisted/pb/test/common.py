570480b486c489158d62071abca29499bc966eae
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-12-31 08:10:28 +0000

    whoops, missed an added file
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@15453 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/pb/test/common.py
@@ -0,0 +1,54 @@
+
+from zope.interface import implements
+from twisted.internet import defer, reactor
+from twisted.pb import pb, schema
+
+class RIHelper(pb.RemoteInterface):
+    def set(obj=schema.Any()): return bool
+    def set2(obj1=schema.Any(), obj2=schema.Any()): return bool
+    def append(obj=schema.Any()): return schema.Any()
+    def get(): return schema.Any()
+    def echo(obj=schema.Any()): return schema.Any()
+    def defer(obj=schema.Any()): return schema.Any()
+    def hang(): return schema.Any()
+
+class HelperTarget(pb.Referenceable):
+    implements(RIHelper)
+    d = None
+    def __init__(self, name="unnamed"):
+        self.name = name
+    def __repr__(self):
+        return "<HelperTarget %s>" % self.name
+    def waitfor(self):
+        self.d = defer.Deferred()
+        return self.d
+
+    def remote_set(self, obj):
+        self.obj = obj
+        if self.d:
+            self.d.callback(obj)
+        return True
+    def remote_set2(self, obj1, obj2):
+        self.obj1 = obj1
+        self.obj2 = obj2
+        return True
+
+    def remote_append(self, obj):
+        self.calls.append(obj)
+
+    def remote_get(self):
+        return self.obj
+
+    def remote_echo(self, obj):
+        self.obj = obj
+        return obj
+
+    def remote_defer(self, obj):
+        d = defer.Deferred()
+        reactor.callLater(1, d.callback, obj)
+        return d
+
+    def remote_hang(self):
+        self.d = defer.Deferred()
+        return self.d
+
