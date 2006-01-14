f0b1577a6438bccd36425023194de9be87bf3392
foom <foom@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-01-14 22:31:36 +0000

    Exarkun forgot to remove this code as part of the last change.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@15585 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web2/test/__init__.py
+++ twisted/web2/test/__init__.py
@@ -1,22 +1,9 @@
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2006 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 """
 
-twisted.web.test: unittests for the Twisted Web, Web Server Framework
+twisted.web2.test: unittests for the Twisted Web2, Web Server Framework
 
 """
 
-# Heh heh. Too Evil to pass up. ;)
-import __builtin__
-__builtin__._http_headers_isBeingTested=True
-import sys
-import twisted.web2
-import twisted.python.rebuild
-if sys.modules.has_key('twisted.web2.http_headers'):
-    twisted.python.rebuild.rebuild(twisted.web2.http_headers)
-else:
-    from twisted.web2 import http_headers
-del __builtin__._http_headers_isBeingTested
-del sys
-del twisted
