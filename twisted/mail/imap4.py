db0c877b844b84274d03bd93eb3fe0f4282de610
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-03-11 23:24:35 +0000

    Apply patch from cablehead/therve - adds support for recognizing unsolicited FLAGS responses to IMAP4Client; Fixes #1105
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@16253 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/mail/imap4.py
+++ twisted/mail/imap4.py
@@ -364,8 +364,14 @@ class Command:
             names = parseNestedParens(L)
             N = len(names)
             if (N >= 1 and names[0] in self._1_RESPONSES or
-                N >= 2 and names[1] in self._2_RESPONSES or
                 N >= 2 and names[0] == 'OK' and isinstance(names[1], types.ListType) and names[1][0] in self._OK_RESPONSES):
+                send.append(L)
+            elif N >= 3 and names[1] in self._2_RESPONSES:
+                if isinstance(names[2], list) and len(names[2]) >= 1 and names[2][0] == 'FLAGS' and 'FLAGS' not in self.args:
+                    unuse.append(L)
+                else:
+                    send.append(L)
+            elif N >= 2 and names[1] in self._2_RESPONSES:
                 send.append(L)
             else:
                 unuse.append(L)
--- twisted/mail/test/test_imap.py
+++ twisted/mail/test/test_imap.py
@@ -1659,6 +1659,55 @@ class HandCraftedTestCase(IMAP4HelperMixin, unittest.TestCase):
 
         self.server.connectionLost(error.ConnectionDone("Connection done."))
 
+    def testUnsolicitedResponseMixedWithSolicitedResponse(self):
+
+        class StillSimplerClient(imap4.IMAP4Client):
+            events = []
+            def flagsChanged(self, newFlags):
+                self.events.append(['flagsChanged', newFlags])
+
+        transport = StringTransport()
+        c = StillSimplerClient()
+        c.makeConnection(transport)
+        c.lineReceived('* OK [IMAP4rev1]')
+
+        def login():
+            d = c.login('blah', 'blah')
+            c.dataReceived('0001 OK LOGIN\r\n')
+            return d
+        def select():
+            d = c.select('inbox')
+            c.lineReceived('0002 OK SELECT')
+            return d
+        def fetch():
+            d = c.fetchSpecific('1:*',
+                headerType='HEADER.FIELDS',
+                headerArgs=['SUBJECT'])
+            c.dataReceived('* 1 FETCH (BODY[HEADER.FIELDS ("SUBJECT")] {38}\r\n')
+            c.dataReceived('Subject: Suprise for your woman...\r\n')
+            c.dataReceived('\r\n')
+            c.dataReceived(')\r\n')
+            c.dataReceived('* 1 FETCH (FLAGS (\Seen))\r\n')
+            c.dataReceived('* 2 FETCH (BODY[HEADER.FIELDS ("SUBJECT")] {75}\r\n')
+            c.dataReceived('Subject: What you been doing. Order your meds here . ,. handcuff madsen\r\n')
+            c.dataReceived('\r\n')
+            c.dataReceived(')\r\n')
+            c.dataReceived('0003 OK FETCH completed\r\n')
+            return d
+        def test(res):
+            self.assertEquals(res, {
+                1: [['BODY', ['HEADER.FIELDS', ['SUBJECT']],
+                    'Subject: Suprise for your woman...\r\n\r\n']],
+                2: [['BODY', ['HEADER.FIELDS', ['SUBJECT']],
+                    'Subject: What you been doing. Order your meds here . ,. handcuff madsen\r\n\r\n']]
+            })
+
+            self.assertEquals(c.events, [['flagsChanged', {1: ['\\Seen']}]])
+
+        return login(
+            ).addCallback(strip(select)
+            ).addCallback(strip(fetch)
+            ).addCallback(test)
 
 class FakeyServer(imap4.IMAP4Server):
     state = 'select'
