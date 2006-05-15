7884b7aab67f8311a18d9baef816121f7d16c411
teratorn <teratorn@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-05-15 00:35:27 +0000

    Merge testWhich-win32-1717
    Author: teratorn 
    Review: exarkun 
    Fixes #1717
    
    os.access() ignores all flags except for os.F_OK on MS-Windows. Make test cases aware of how this changes the result of procutils.which().
    Better doc string for procutils.which()
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@16817 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/procutils.py
+++ twisted/python/procutils.py
@@ -13,6 +13,14 @@ import os
 def which(name, flags=os.X_OK):
     """Search PATH for executable files with the given name.
     
+    On newer versions of MS-Windows, the PATHEXT environment variable will be
+    set to the list of file extensions for files considered executable. This
+    will normally include things like ".EXE". This fuction will also find files
+    with the given name ending with any of these extensions.
+
+    On MS-Windows the only flag that has any meaning is os.F_OK. Any other
+    flags will be ignored.
+    
     @type name: C{str}
     @param name: The name for which to search.
     
