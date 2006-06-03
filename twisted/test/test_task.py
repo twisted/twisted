c7970bb20e6fc7b78796083b165678b19cfcfdc9
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-06-03 22:19:20 +0000

    Merge test-clock-1757
    Authors: exarkun, radix
    Reviewer: glyph, radix
    Fixes #1757
    
    
    This adds a twisted.internet.task.Clock class which
    simulates time as an IReactor.callLater replacement.
    It is useful for unit tests.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@17032 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_task.py
+++ twisted/test/test_task.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2001-2004 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-
 from twisted.trial import unittest
 
-from twisted.internet import task, reactor, defer
+from twisted.internet import interfaces, task, reactor, defer
+
+# Be compatible with any jerks who used our private stuff
+Clock = task.Clock
 
 from twisted.python import failure
 
@@ -16,61 +18,100 @@ class TestableLoopingCall(task.LoopingCall):
 
 
 
-class FakeDelayedCall(object):
-    def __init__(self, when, clock, what, a, kw):
-        self.clock = clock
-        self.when = when
-        self.what = what
-        self.a = a
-        self.kw = kw
-
-
-    def __call__(self):
-        return self.what(*self.a, **self.kw)
-
-
-    def __repr__(self):
-        return "<FakeDelayedCall of %r>" % (self.what,)
-
-
-    def cancel(self):
-        self.clock.calls.remove((self.when, self))
-
-
-
-class Clock(object):
-    rightNow = 0.0
-
-    def __init__(self):
-        self.calls = []
-
-    def seconds(self):
-        return self.rightNow
-
-    def callLater(self, when, what, *a, **kw):
-        self.calls.append((self.seconds() + when, FakeDelayedCall(self.seconds() + when, self, what, a, kw)))
-        return self.calls[-1][1]
-
-    def adjust(self, amount):
-        self.rightNow += amount
-
-    def runUntilCurrent(self):
-        while self.calls and self.calls[0][0] <= self.seconds():
-            when, call = self.calls.pop(0)
-            call()
-
-    def pump(self, timings):
-        timings = list(timings)
-        timings.reverse()
-        self.calls.sort()
-        while timings:
-            self.adjust(timings.pop())
-            self.runUntilCurrent()
-
-
-
 class TestException(Exception):
     pass
+
+
+
+class ClockTestCase(unittest.TestCase):
+    """
+    Test the non-wallclock based clock implementation.
+    """
+    def testSeconds(self):
+        """
+        Test that the L{seconds} method of the fake clock returns fake time.
+        """
+        c = task.Clock()
+        self.assertEquals(c.seconds(), 0)
+
+
+    def testCallLater(self):
+        """
+        Test that calls can be scheduled for later with the fake clock and
+        hands back an L{IDelayedCall}.
+        """
+        c = task.Clock()
+        call = c.callLater(1, lambda a, b: None, 1, b=2)
+        self.failUnless(interfaces.IDelayedCall.providedBy(call))
+        self.assertEquals(call.getTime(), 1)
+        self.failUnless(call.active())
+
+
+    def testCallLaterCancelled(self):
+        """
+        Test that calls can be cancelled.
+        """
+        c = task.Clock()
+        call = c.callLater(1, lambda a, b: None, 1, b=2)
+        call.cancel()
+        self.failIf(call.active())
+
+
+    def testAdvance(self):
+        """
+        Test that advancing the clock will fire some calls.
+        """
+        events = []
+        c = task.Clock()
+        call = c.callLater(2, lambda: events.append(None))
+        c.advance(1)
+        self.assertEquals(events, [])
+        c.advance(1)
+        self.assertEquals(events, [None])
+
+
+    def testCallLaterDelayed(self):
+        """
+        Test that calls can be delayed.
+        """
+        events = []
+        c = task.Clock()
+        call = c.callLater(1, lambda a, b: events.append((a, b)), 1, b=2)
+        call.delay(1)
+        self.assertEquals(call.getTime(), 2)
+        c.advance(1.5)
+        self.assertEquals(events, [])
+        c.advance(1.0)
+        self.assertEquals(events, [(1, 2)])
+
+
+    def testCallLaterResetLater(self):
+        """
+        Test that calls can have their time reset to a later time.
+        """
+        events = []
+        c = task.Clock()
+        call = c.callLater(2, lambda a, b: events.append((a, b)), 1, b=2)
+        c.advance(1)
+        call.reset(3)
+        self.assertEquals(call.getTime(), 4)
+        c.advance(2)
+        self.assertEquals(events, [])
+        c.advance(1)
+        self.assertEquals(events, [(1, 2)])
+
+
+    def testCallLaterResetSooner(self):
+        """
+        Test that calls can have their time reset to an earlier time.
+        """
+        events = []
+        c = task.Clock()
+        call = c.callLater(4, lambda a, b: events.append((a, b)), 1, b=2)
+        call.reset(3)
+        self.assertEquals(call.getTime(), 3)
+        c.advance(3)
+        self.assertEquals(events, [(1, 2)])
 
 
 
@@ -82,7 +123,7 @@ class LoopTestCase(unittest.TestCase):
         # happens before any time has elapsed.
         timings = [0.05, 0.1, 0.1]
 
-        clock = Clock()
+        clock = task.Clock()
 
         L = []
         def foo(a, b, c=None, d=None):
@@ -117,7 +158,7 @@ class LoopTestCase(unittest.TestCase):
     def testDelayedStart(self):
         timings = [0.05, 0.1, 0.1]
 
-        clock = Clock()
+        clock = task.Clock()
 
         L = []
         lc = TestableLoopingCall(clock, L.append, None)
@@ -149,7 +190,7 @@ class LoopTestCase(unittest.TestCase):
         def foo():
             ran.append(None)
 
-        clock = Clock()
+        clock = task.Clock()
         lc = TestableLoopingCall(clock, foo)
         d = lc.start(delay, now=False)
         lc.stop()
@@ -222,7 +263,7 @@ class ReactorLoopTestCase(unittest.TestCase):
         # Tests if the callable isn't scheduled again before the returned
         # deferred has fired.
         timings = [0.2, 0.8]
-        clock = Clock()
+        clock = task.Clock()
 
         def foo():
             d = defer.Deferred()
@@ -238,12 +279,12 @@ class ReactorLoopTestCase(unittest.TestCase):
     def testFailurePropagation(self):
         # Tests if the failure of the errback of the deferred returned by the
         # callable is propagated to the lc errback.
-        # 
+        #
         # To make sure this test does not hang trial when LoopingCall does not
         # wait for the callable's deferred, it also checks there are no
         # calls in the clock's callLater queue.
         timings = [0.3]
-        clock = Clock()
+        clock = task.Clock()
 
         def foo():
             d = defer.Deferred()
