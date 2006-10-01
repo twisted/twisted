d5007a5c75a26958fd5539d502596061d10ff574
warner <warner@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-10-01 19:16:04 +0000

    Remove newpb (twisted/pb) from trunk.
    Author: warner
    
    Reviewer: TimothyFitz
    
    Fixes #1999
    
    It has been moved into warner's sandbox, and renamed Foolscap for now.
    See http://twistedmatrix.com/trac/wiki/FoolsCap for details.
    
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@18362 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/pb/_version.py
+++
@@ -1,3 +0,0 @@
-# This is an auto-generated file. Use admin/change-versions to update.
-from twisted.python import versions
-version = versions.Version(__name__[:__name__.rfind('.')], 0, 0, 1)
--- twisted/pb/test/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- test-case-name: twisted.pb.test -*-
-"""perspective broker tests"""
