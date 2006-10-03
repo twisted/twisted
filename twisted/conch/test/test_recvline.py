34e213ae0a5897dc346c9e452fa481892795df80
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-10-03 00:01:42 +0000

    Remove dead code in insults tests
    Author: therve
    Reviewer: exarkun
    Fixes #1185
    
    Several insults test helpers invoked reactor.iterate(), however these helpers had
    not been used for quite some time.  Remove them.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@18376 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/test/test_recvline.py
+++ twisted/conch/test/test_recvline.py
@@ -3,15 +3,12 @@
 # See LICENSE for details.
 
 import sys, os
-from pprint import pformat
-
-from zope.interface import implements
 
 from twisted.conch.insults import insults
 from twisted.conch import recvline
 
-from twisted.python import log, reflect, components
-from twisted.internet import defer, error, task
+from twisted.python import reflect, components
+from twisted.internet import defer, error
 from twisted.trial import unittest
 from twisted.cred import portal
 from twisted.test.proto_helpers import StringTransport
@@ -381,12 +378,6 @@ class _BaseMixin:
                 " != " +
                 str(expectedLines[max(0, i-1):i+1]))
 
-    def _test(self, s, lines):
-        self._testwrite(s)
-        def asserts(ignored):
-            self._assertBuffer(lines)
-        return defer.maybeDeferred(self._emptyBuffers).addCallback(asserts)
-
     def _trivialTest(self, input, output):
         done = self.recvlineClient.expect("done")
 
@@ -439,9 +430,6 @@ class _SSHMixin(_BaseMixin):
     def _testwrite(self, bytes):
         self.sshClient.write(bytes)
 
-    def _emptyBuffers(self):
-        pass
-
 from twisted.conch.test import test_telnet
 
 class TestInsultsClientProtocol(insults.ClientProtocol,
@@ -480,10 +468,6 @@ class _TelnetMixin(_BaseMixin):
 
     def _testwrite(self, bytes):
         self.telnetClient.write(bytes)
-
-    def _emptyBuffers(self):
-        self.clientTransport.clearBuffer()
-        self.serverTransport.clearBuffer()
 
 try:
     from twisted.conch import stdio
@@ -548,11 +532,6 @@ class _StdioMixin(_BaseMixin, SignalMixin):
     def _testwrite(self, bytes):
         self.clientTransport.write(bytes)
 
-    def _emptyBuffers(self):
-        from twisted.internet import reactor
-        for i in range(100):
-            reactor.iterate(0.01)
-
 class RecvlineLoopbackMixin:
     serverProtocol = EchoServer
 
@@ -611,8 +590,6 @@ class RecvlineLoopbackMixin:
             [">>> home line",
              "home line",
              ">>> done"])
-
-        return done.addCallback(finished)
 
     def testEnd(self):
         return self._trivialTest(
