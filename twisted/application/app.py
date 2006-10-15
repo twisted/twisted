d7a67e8e9cb822e852b9e0a536cf237fe82ec147
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-10-15 15:19:04 +0000

    Merge epollreactor-1953
    Author: therve, exarkun
    Reviewer: PenguinOfDoom
    Fixes #1953
    
    Add a level-triggered epoll-based reactor.
    
    Also re-order the removal and the shutdown of sockets from the reactor
    in the tcp code shared between reactors in order to avoid possibly
    passing a no-longer valid file descriptor to a control API such as
    epoll_ctl() or kevent().
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@18464 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/application/app.py
+++ twisted/application/app.py
@@ -21,6 +21,7 @@ reactorTypes = {
     'win32': 'twisted.internet.win32eventreactor',
     'win': 'twisted.internet.win32eventreactor',
     'poll': 'twisted.internet.pollreactor',
+    'epoll': 'twisted.internet.epollreactor',
     'cf' : 'twisted.internet.cfreactor',
     'kqueue': 'twisted.internet.kqreactor',
     'iocp': 'twisted.internet.iocpreactor',
