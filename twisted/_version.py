023c9f598fbae50a4618736f6700f720aacd4c02
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2007-01-06 20:24:42 +0000

    Merge releases/twisted-core-2.5.x-2329-2
    Reviewer: exarkun
    Author: release automation, radix
    Fixes #2329
    
    Release shrapnel from the 2.5.0 release of twisted-core, 
    plus a minor fix to an URL in README.
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@19208 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/_version.py
+++ twisted/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Use admin/change-versions to update.
 from twisted.python import versions
-version = versions.Version(__name__[:__name__.rfind('.')], 2, 4, 0)
+version = versions.Version(__name__[:__name__.rfind('.')], 2, 5, 0)
