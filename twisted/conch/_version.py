e8f04628fa2911657e578a12be17a68bf15ec3fa
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2007-01-07 01:54:03 +0000

    Merge releases/twisted-conch-0.8-2359
    Author: release automation, radix
    Reviewer: exarkun
    Fixes #2359
    
    Version numbers and NEWS from release of conch 0.8.0
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@19226 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/_version.py
+++ twisted/conch/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Use admin/change-versions to update.
 from twisted.python import versions
-version = versions.Version(__name__[:__name__.rfind('.')], 0, 7, 0)
+version = versions.Version(__name__[:__name__.rfind('.')], 0, 8, 0)
