6417d6dfec523792541212f1d3769a485cbd8d2f
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2007-01-07 01:46:56 +0000

    Merge releases/twisted-lore-0.3.x-2363
    Author: release automation, radix
    Reviewer: exarkun
    Fixes #2363
    
    Version numbers and NEWS from the release of lore 0.3.0
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@19224 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/_version.py
+++ twisted/lore/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Use admin/change-versions to update.
 from twisted.python import versions
-version = versions.Version(__name__[:__name__.rfind('.')], 0, 2, 0)
+version = versions.Version(__name__[:__name__.rfind('.')], 0, 3, 0)
