eee46a536e51b04ebe8d761bb9a078f1eaf0f46b
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2007-02-09 09:24:07 +0000

    Merge web2-test-log-2102
    Author: jknight, therve
    Reviewer: jerub, dreid
    Fixes #2102
    
    Update web2 log tests to be sure there were no duplicate messages. Also
    make some cleanups in the process.
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@19587 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web2/test/test_log.py
+++ twisted/web2/test/test_log.py
@@ -1,29 +1,38 @@
-from twisted.trial import unittest
-from twisted.web2 import log, iweb, resource, http
+# Copyright (c) 2001-2007 Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+from twisted.web2 import log, resource, http
 from twisted.web2.test.test_server import BaseCase, BaseTestResource
 
 from twisted.python import log as tlog
-from twisted.test.test_log import FakeFile
 
 class BufferingLogObserver(log.BaseCommonAccessLoggingObserver):
-    messages = ['']
+    """
+    A web2 log observer that buffer messages.
+    """
+    messages = []
     def logMessage(self, message):
         self.messages.append(message)
 
-class TestLogWrapperResource(resource.WrapperResource):
+class SetDateWrapperResource(resource.WrapperResource):
+    """
+    A resource wrapper which sets the date header.
+    """
     def hook(self, req):
-        def _logFilter(req, resp):
+        def _filter(req, resp):
             resp.headers.setHeader('date', 0.0)
-            return log.logFilter(req, resp)
+            return resp
+        _filter.handleErrors = True
 
-        _logFilter.handleErrors = True
-                                 
-        req.addResponseFilter(_logFilter, atEnd=True)
+        req.addResponseFilter(_filter, atEnd=True)
 
 class NoneStreamResource(resource.Resource):
+    """
+    A basic empty resource.
+    """
     def render(self, req):
         return http.Response(200)
-    
+
 class TestLogging(BaseCase):
     def setUp(self):
         self.blo = BufferingLogObserver()
@@ -32,22 +41,26 @@ class TestLogging(BaseCase):
         # some default resource setup
         self.resrc = BaseTestResource()
         self.resrc.child_emptystream = NoneStreamResource()
-        self.resrc.addSlash = True
-        self.resrc.responseHeaders = {'Date': 0.0}
-        
-        self.root = TestLogWrapperResource(self.resrc)
+
+        self.root = SetDateWrapperResource(log.LogWrapperResource(self.resrc))
+
+    def tearDown(self):
+        tlog.removeObserver(self.blo.emit)
 
     def assertLogged(self, **expected):
+        """
+        Check that logged messages matches expected format.
+        """
         if 'date' not in expected:
             epoch = log.BaseCommonAccessLoggingObserver().logDateString(0)
             expected['date'] = epoch
 
         if 'user' not in expected:
             expected['user'] = '-'
-            
+
         if 'referer' not in expected:
             expected['referer'] = '-'
-            
+
         if 'user-agent' not in expected:
             expected['user-agent'] = '-'
 
@@ -56,74 +69,80 @@ class TestLogging(BaseCase):
 
         if 'remotehost' not in expected:
             expected['remotehost'] = 'remotehost'
-        
-        if 'message' in expected:
-            message = message
-        else:
-            message = self.blo.messages[-1]
+
+        messages = self.blo.messages[:]
+        del self.blo.messages[:]
 
         expectedLog = ('%(remotehost)s - %(user)s [%(date)s] "%(method)s '
                        '%(uri)s HTTP/%(version)s" %(status)d %(length)d '
                        '"%(referer)s" "%(user-agent)s"')
 
         if expected.get('logged', True):
-            self.assertEquals(message, expectedLog % expected)
+            # Ensure there weren't other messages hanging out
+            self.assertEquals(len(messages), 1, "len(%r) != 1" % (messages, ))
+            self.assertEquals(messages[0], expectedLog % expected)
         else:
-            self.failIfEquals(message, expectedLog % expected)
+            self.assertEquals(len(messages), 0, "len(%r) != 0" % (messages, ))
 
-    def testLogSimpleRequest(self):
+    def test_logSimpleRequest(self):
+        """
+        Check the log for a simple request.
+        """
         uri = 'http://localhost/'
         method = 'GET'
-        
+
         def _cbCheckLog(response):
             self.assertLogged(method=method, uri=uri, status=response[0],
                               length=response[1].getHeader('content-length'))
-            
+
         d = self.getResponseFor(self.root, uri, method=method)
         d.addCallback(_cbCheckLog)
 
         return d
 
-    def testLogErrors(self):
-
+    def test_logErrors(self):
+        """
+        Test the error log.
+        """
         def test(_, uri, method, **expected):
             expected['uri'] = uri
             expected['method'] = method
-            
+
             def _cbCheckLog(response):
                 self.assertEquals(response[0], expected['status'])
                 self.assertLogged(
                     length=response[1].getHeader('content-length'), **expected)
-            
+
             return self.getResponseFor(self.root,
                                        uri,
                                        method=method).addCallback(_cbCheckLog)
-        
+
 
         uri = 'http://localhost/foo' # doesn't exist
         method = 'GET'
-    
+
         d = test(None, uri, method, status=404, logged=True)
 
         # no host. this should result in a 400 which doesn't get logged
-        uri = 'http:///' 
+        uri = 'http:///'
 
         d.addCallback(test, uri, method, status=400, logged=False)
 
         return d
 
-    def testLogNoneResponseStream(self):
+    def test_logNoneResponseStream(self):
+        """
+        Test the log of an empty resource.
+        """
         uri = 'http://localhost/emptystream'
         method = 'GET'
-        
+
         def _cbCheckLog(response):
             self.assertLogged(method=method, uri=uri, status=200,
                               length=0)
-            
+
         d = self.getResponseFor(self.root, uri, method=method)
         d.addCallback(_cbCheckLog)
 
         return d
-
-        
 
