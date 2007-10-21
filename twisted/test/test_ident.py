e8bb918393f70a12cae8a5a2480ef045174d7291
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2007-10-21 06:49:20 +0000

    Merge test-ident-cleanups-2767
    Author: therve
    Reviewer: exarkun
    Fixes #2767
    
    Replace a call of log.flushErrors by flushLoggedErrors in
    twisted.test.test_ident, and make a bunch of cleanups in the process.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@21374 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_ident.py
+++ twisted/test/test_ident.py
@@ -1,5 +1,4 @@
-
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2007 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 
@@ -13,64 +12,89 @@ from twisted.protocols import ident
 from twisted.python import failure
 from twisted.internet import error
 from twisted.internet import defer
-from twisted.python import log
 
 from twisted.trial import unittest
 from twisted.test.proto_helpers import StringTransport
 
+
+
 class ClassParserTestCase(unittest.TestCase):
-    def testErrors(self):
-        p = ident.IdentClient()
-        
-        L = []
+    """
+    Test parsing of ident responses.
+    """
+
+    def setUp(self):
+        """
+        Create a ident client used in tests.
+        """
+        self.client = ident.IdentClient()
+
+
+    def test_indentError(self):
+        """
+        'UNKNOWN-ERROR' error should map to the L{ident.IdentError} exception.
+        """
         d = defer.Deferred()
-        d.addErrback(L.append)
-        p.queries.append((d, 123, 456))
-        p.lineReceived('123, 456 : ERROR : UNKNOWN-ERROR')
-        self.failUnless(L[0].value.__class__ is ident.IdentError, "%s is the wrong exception" % (L[0],))
-        
-        L = []
+        self.client.queries.append((d, 123, 456))
+        self.client.lineReceived('123, 456 : ERROR : UNKNOWN-ERROR')
+        return self.assertFailure(d, ident.IdentError)
+
+
+    def test_noUSerError(self):
+        """
+        'NO-USER' error should map to the L{ident.NoUser} exception.
+        """
         d = defer.Deferred()
-        d.addErrback(L.append)
-        p.queries.append((d, 234, 456))
-        p.lineReceived('234, 456 : ERROR : NO-USER')
-        self.failUnless(L[0].value.__class__ is ident.NoUser, "%s is the wrong exception" % (L[0],))
-        
-        L = []
+        self.client.queries.append((d, 234, 456))
+        self.client.lineReceived('234, 456 : ERROR : NO-USER')
+        return self.assertFailure(d, ident.NoUser)
+
+
+    def test_invalidPortError(self):
+        """
+        'INVALID-PORT' error should map to the L{ident.InvalidPort} exception.
+        """
         d = defer.Deferred()
-        d.addErrback(L.append)
-        p.queries.append((d, 345, 567))
-        p.lineReceived('345, 567 :  ERROR : INVALID-PORT')
-        self.failUnless(L[0].value.__class__ is ident.InvalidPort, "%s is the wrong exception" % (L[0],))
-        
-        L = []
+        self.client.queries.append((d, 345, 567))
+        self.client.lineReceived('345, 567 :  ERROR : INVALID-PORT')
+        return self.assertFailure(d, ident.InvalidPort)
+
+
+    def test_hiddenUserError(self):
+        """
+        'HIDDEN-USER' error should map to the L{ident.HiddenUser} exception.
+        """
         d = defer.Deferred()
-        d.addErrback(L.append)
-        p.queries.append((d, 567, 789))
-        p.lineReceived('567, 789 : ERROR : HIDDEN-USER')
-        self.failUnless(L[0].value.__class__ is ident.HiddenUser, "%s is the wrong exception" % (L[0],))
-    
-    def testLostConnection(self):
-        p = ident.IdentClient()
-        
-        L = []
+        self.client.queries.append((d, 567, 789))
+        self.client.lineReceived('567, 789 : ERROR : HIDDEN-USER')
+        return self.assertFailure(d, ident.HiddenUser)
+
+
+    def test_lostConnection(self):
+        """
+        A pending query which failed because of a ConnectionLost should
+        receive an L{ident.IdentError}.
+        """
         d = defer.Deferred()
-        d.addErrback(L.append)
-        p.queries.append((d, 765, 432))
-        p.connectionLost(failure.Failure(error.ConnectionLost()))
-        self.failUnless(L[0].value.__class__ is ident.IdentError)
+        self.client.queries.append((d, 765, 432))
+        self.client.connectionLost(failure.Failure(error.ConnectionLost()))
+        return self.assertFailure(d, ident.IdentError)
+
 
 
 class TestIdentServer(ident.IdentServer):
     def lookup(self, serverAddress, clientAddress):
         return self.resultValue
 
+
 class TestErrorIdentServer(ident.IdentServer):
     def lookup(self, serverAddress, clientAddress):
         raise self.exceptionType()
 
+
 class NewException(RuntimeError):
     pass
+
 
 class ServerParserTestCase(unittest.TestCase):
     def testErrors(self):
@@ -82,23 +106,23 @@ class ServerParserTestCase(unittest.TestCase):
         p.exceptionType = ident.IdentError
         p.lineReceived('123, 345')
         self.assertEquals(L[0], '123, 345 : ERROR : UNKNOWN-ERROR')
-        
+
         p.exceptionType = ident.NoUser
         p.lineReceived('432, 210')
         self.assertEquals(L[1], '432, 210 : ERROR : NO-USER')
-        
+
         p.exceptionType = ident.InvalidPort
         p.lineReceived('987, 654')
         self.assertEquals(L[2], '987, 654 : ERROR : INVALID-PORT')
-        
+
         p.exceptionType = ident.HiddenUser
         p.lineReceived('756, 827')
         self.assertEquals(L[3], '756, 827 : ERROR : HIDDEN-USER')
-        
+
         p.exceptionType = NewException
         p.lineReceived('987, 789')
         self.assertEquals(L[4], '987, 789 : ERROR : UNKNOWN-ERROR')
-        errs = log.flushErrors(NewException)
+        errs = self.flushLoggedErrors(NewException)
         self.assertEquals(len(errs), 1)
 
         for port in -1, 0, 65536, 65537:
@@ -114,10 +138,11 @@ class ServerParserTestCase(unittest.TestCase):
         p.makeConnection(StringTransport())
         L = []
         p.sendLine = L.append
-        
+
         p.resultValue = ('SYS', 'USER')
         p.lineReceived('123, 456')
         self.assertEquals(L[0], '123, 456 : USERID : SYS : USER')
+
 
 if struct.pack('=L', 1)[0] == '\x01':
     _addr1 = '0100007F'
@@ -125,6 +150,7 @@ if struct.pack('=L', 1)[0] == '\x01':
 else:
     _addr1 = '7F000001'
     _addr2 = '01020304'
+
 
 class ProcMixinTestCase(unittest.TestCase):
     line = ('4: %s:0019 %s:02FA 0A 00000000:00000000 '
@@ -137,7 +163,8 @@ class ProcMixinTestCase(unittest.TestCase):
 
     def testUnpackAddress(self):
         p = ident.ProcServerMixin()
-        self.assertEquals(p.unpackAddress(_addr1 + ':0277'), ('127.0.0.1', 631))
+        self.assertEquals(p.unpackAddress(_addr1 + ':0277'),
+                          ('127.0.0.1', 631))
 
     def testLineParser(self):
         p = ident.ProcServerMixin()
@@ -158,6 +185,10 @@ class ProcMixinTestCase(unittest.TestCase):
     def testNonExistingAddress(self):
         p = ident.ProcServerMixin()
         p.entries = lambda: iter([self.line])
-        self.assertRaises(ident.NoUser, p.lookup, ('127.0.0.1', 26), ('1.2.3.4', 762))
-        self.assertRaises(ident.NoUser, p.lookup, ('127.0.0.1', 25), ('1.2.3.5', 762))
-        self.assertRaises(ident.NoUser, p.lookup, ('127.0.0.1', 25), ('1.2.3.4', 763))
+        self.assertRaises(ident.NoUser, p.lookup, ('127.0.0.1', 26),
+                                                  ('1.2.3.4', 762))
+        self.assertRaises(ident.NoUser, p.lookup, ('127.0.0.1', 25),
+                                                  ('1.2.3.5', 762))
+        self.assertRaises(ident.NoUser, p.lookup, ('127.0.0.1', 25),
+                                                  ('1.2.3.4', 763))
+
