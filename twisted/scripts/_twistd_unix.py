7df574a49fb6b9e4fa4f953e14c3e3ae6f362d12
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-02-03 08:40:18 +0000

    Merge twistd-profiler-tests-2921
    Author: therve
    Reviewer: exarkun
    Fixes #2921
    
    Refactor twistd profiling options to be more easily testable, and add tests for
    them. This refactor leads to a bug correction of hotshot under Python 2.5 and
    to some cleanups.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@22450 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/_twistd_unix.py
+++ twisted/scripts/_twistd_unix.py
@@ -1,5 +1,5 @@
 # -*- test-case-name: twisted.test.test_twistd -*-
-# Copyright (c) 2001-2007 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 import warnings
@@ -18,7 +18,7 @@ class ServerOptions(app.ServerOptions):
     synopsis = "Usage: twistd [options]"
 
     optFlags = [['nodaemon','n',  "don't daemonize"],
-                ['quiet', 'q', "No-op for backwards compatability."],
+                ['quiet', 'q', "No-op for backwards compatibility."],
                 ['originalname', None, "Don't try to change the process name"],
                 ['syslog', None,   "Log to syslog, not to file"],
                 ['euid', '',
@@ -257,6 +257,7 @@ class UnixApplicationRunner(app.ApplicationRunner):
         clean up PID files and such.
         """
         startApplication(self.config, self.application)
-        app.runReactorWithLogging(self.config, self.oldstdout, self.oldstderr)
+        app.runReactorWithLogging(self.config, self.oldstdout, self.oldstderr,
+                                  self.profiler)
         removePID(self.config['pidfile'])
         log.msg("Server Shut Down.")
