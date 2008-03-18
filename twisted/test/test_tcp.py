a577f9d07429c2120b3dddada15d768e4a2b2d8d
pahan <pahan@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-03-18 22:11:41 +0000

    Merge lose-order-3055: add test to check order of reactor callbacks
    Checks that protocol.connectionLost and factory.clientConnectionLost are
    called in the right order.
    
    Author: PenguinOfDoom
    Reviewer: exarkun
    Fixes #3055
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@22972 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_tcp.py
+++ twisted/test/test_tcp.py
@@ -1710,6 +1710,63 @@ class PauseProducingTestCase(unittest.TestCase):
         test_pauseProducingInConnectionMade.skip = "Reactor not providing IReactorFDSet"
 
 
+
+class CallBackOrderTestCase(unittest.TestCase):
+    """
+    Test the order of reactor callbacks
+    """
+
+    def test_loseOrder(self):
+        """
+        Check that Protocol.connectionLost is called before factory's
+        clientConnectionLost
+        """
+        server = MyServerFactory()
+        server.protocolConnectionMade = (defer.Deferred()
+                .addCallback(lambda proto: self.addCleanup(
+                             proto.transport.loseConnection)))
+
+        client = MyClientFactory()
+        client.protocolConnectionLost = defer.Deferred()
+        client.protocolConnectionMade = defer.Deferred()
+
+        def _cbCM(res):
+            """
+            protocol.connectionMade callback
+            """
+            reactor.callLater(0, client.protocol.transport.loseConnection)
+
+        client.protocolConnectionMade.addCallback(_cbCM)
+
+        port = reactor.listenTCP(0, server, interface='127.0.0.1')
+        self.addCleanup(port.stopListening)
+
+        connector = reactor.connectTCP(
+            port.getHost().host, port.getHost().port, client)
+        self.addCleanup(connector.disconnect)
+
+        def _cbCCL(res):
+            """
+            factory.clientConnectionLost callback
+            """
+            return 'CCL'
+
+        def _cbCL(res):
+            """
+            protocol.connectionLost callback
+            """
+            return 'CL'
+
+        def _cbGather(res):
+            self.assertEquals(res, ['CL', 'CCL'])
+
+        d = defer.gatherResults([
+                client.protocolConnectionLost.addCallback(_cbCL),
+                client.deferred.addCallback(_cbCCL)])
+        return d.addCallback(_cbGather)
+
+
+
 try:
     import resource
 except ImportError:
