e5804cac91bc56fad00b185430d562971003edf0
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-05-27 14:57:03 +0000

    Merge test-ssl-iterate-2374
    Author: therve
    Reviewer: exarkun
    Fixes #2374
    Fixes #2918
    
    Remove reactor.iterate calls from test_ssl.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@23717 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_ssl.py
+++ twisted/test/test_ssl.py
@@ -1,11 +1,14 @@
-# Copyright (c) 2001-2007 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
+"""
+Tests for twisted SSL support.
+"""
 
 from twisted.trial import unittest, util as trial_util
 from twisted.internet import protocol, reactor, interfaces, defer
 from twisted.protocols import basic
-from twisted.python import util, log
+from twisted.python import util
 from twisted.python.runtime import platform
 from twisted.test.test_tcp import WriteDataTestCase, ProperlyCloseFilesMixin
 
@@ -25,7 +28,19 @@ except ImportError:
 
 certPath = util.sibpath(__file__, "server.pem")
 
+
+
 class UnintelligentProtocol(basic.LineReceiver):
+    """
+    @ivar deferred: a deferred that will fire at connection lost.
+    @type deferred: L{defer.Deferred}
+
+    @cvar pretext: text sent before TLS is set up.
+    @type pretext: C{str}
+
+    @cvar posttext: text sent after TLS is set up.
+    @type posttext: C{str}
+    """
     pretext = [
         "first line",
         "last thing before tls starts",
@@ -35,9 +50,14 @@ class UnintelligentProtocol(basic.LineReceiver):
         "first thing after tls started",
         "last thing ever"]
 
+    def __init__(self):
+        self.deferred = defer.Deferred()
+
+
     def connectionMade(self):
         for l in self.pretext:
             self.sendLine(l)
+
 
     def lineReceived(self, line):
         if line == "READY":
@@ -47,21 +67,41 @@ class UnintelligentProtocol(basic.LineReceiver):
             self.transport.loseConnection()
 
 
+    def connectionLost(self, reason):
+        self.deferred.callback(None)
+
+
+
 class LineCollector(basic.LineReceiver):
-    def __init__(self, doTLS, fillBuffer=0):
+    """
+    @ivar deferred: a deferred that will fire at connection lost.
+    @type deferred: L{defer.Deferred}
+
+    @ivar doTLS: whether the protocol is initiate TLS or not.
+    @type doTLS: C{bool}
+
+    @ivar fillBuffer: if set to True, it will send lots of data once
+        C{STARTTLS} is received.
+    @type fillBuffer: C{bool}
+    """
+
+    def __init__(self, doTLS, fillBuffer=False):
         self.doTLS = doTLS
         self.fillBuffer = fillBuffer
+        self.deferred = defer.Deferred()
+
 
     def connectionMade(self):
         self.factory.rawdata = ''
         self.factory.lines = []
+
 
     def lineReceived(self, line):
         self.factory.lines.append(line)
         if line == 'STARTTLS':
             if self.fillBuffer:
                 for x in range(500):
-                    self.sendLine('X'*1000)
+                    self.sendLine('X' * 1000)
             self.sendLine('READY')
             if self.doTLS:
                 ctx = ServerTLSContext(
@@ -72,49 +112,68 @@ class LineCollector(basic.LineReceiver):
             else:
                 self.setRawMode()
 
+
     def rawDataReceived(self, data):
         self.factory.rawdata += data
-        self.factory.done = 1
+        self.transport.loseConnection()
+
 
     def connectionLost(self, reason):
-        self.factory.done = 1
+        self.deferred.callback(None)
+
 
 
 class SingleLineServerProtocol(protocol.Protocol):
+    """
+    A protocol that sends a single line of data at C{connectionMade}.
+    """
+
     def connectionMade(self):
-        self.transport.identifier = 'SERVER'
         self.transport.write("+OK <some crap>\r\n")
         self.transport.getPeerCertificate()
 
 
+
 class RecordingClientProtocol(protocol.Protocol):
+    """
+    @ivar deferred: a deferred that will fire with first received content.
+    @type deferred: L{defer.Deferred}
+    """
+
+    def __init__(self):
+        self.deferred = defer.Deferred()
+
+
     def connectionMade(self):
-        self.transport.identifier = 'CLIENT'
-        self.buffer = []
         self.transport.getPeerCertificate()
 
+
     def dataReceived(self, data):
-        self.factory.buffer.append(data)
+        self.deferred.callback(data)
+
 
 
 class ImmediatelyDisconnectingProtocol(protocol.Protocol):
+    """
+    A protocol that disconnect immediately on connection. It fires the
+    C{connectionDisconnected} deferred of its factory on connetion lost.
+    """
+
     def connectionMade(self):
         self.transport.loseConnection()
+
 
     def connectionLost(self, reason):
         self.factory.connectionDisconnected.callback(None)
 
 
-class AlmostImmediatelyDisconnectingProtocol(protocol.Protocol):
-    def connectionMade(self):
-        # Twisted's SSL support is terribly broken.
-        reactor.callLater(0.1, self.transport.loseConnection)
-
-    def connectionLost(self, reason):
-        self.factory.connectionDisconnected.callback(reason)
-
 
 def generateCertificateObjects(organization, organizationalUnit):
+    """
+    Create a certificate for given C{organization} and C{organizationalUnit}.
+
+    @return: a tuple of (key, request, certificate) objects.
+    """
     pkey = crypto.PKey()
     pkey.generate_key(crypto.TYPE_RSA, 512)
     req = crypto.X509Req()
@@ -137,7 +196,12 @@ def generateCertificateObjects(organization, organizationalUnit):
     return pkey, req, cert
 
 
+
 def generateCertificateFiles(basename, organization, organizationalUnit):
+    """
+    Create certificate files key, req and cert prefixed by C{basename} for
+    given C{organization} and C{organizationalUnit}.
+    """
     pkey, req, cert = generateCertificateObjects(organization, organizationalUnit)
 
     for ext, obj, dumpFunc in [
@@ -150,7 +214,27 @@ def generateCertificateFiles(basename, organization, organizationalUnit):
         fObj.close()
 
 
+
 class ContextGeneratingMixin:
+    """
+    Offer methods to create L{ssl.DefaultOpenSSLContextFactory} for both client
+    and server.
+
+    @ivar clientBase: prefix of client certificate files.
+    @type clientBase: C{str}
+
+    @ivar serverBase: prefix of server certificate files.
+    @type serverBase: C{str}
+
+    @ivar clientCtxFactory: a generated context factory to be used in
+        C{reactor.connectSSL}.
+    @type clientCtxFactory: L{ssl.DefaultOpenSSLContextFactory}
+
+    @ivar serverCtxFactory: a generated context factory to be used in
+        C{reactor.listenSSL}.
+    @type serverCtxFactory: L{ssl.DefaultOpenSSLContextFactory}
+    """
+
     def makeContextFactory(self, org, orgUnit, *args, **kwArgs):
         base = self.mktemp()
         generateCertificateFiles(base, org, orgUnit)
@@ -161,19 +245,27 @@ class ContextGeneratingMixin:
 
         return base, serverCtxFactory
 
-    def setupServerAndClient(self, clientArgs, clientKwArgs, serverArgs, serverKwArgs):
+
+    def setupServerAndClient(self, clientArgs, clientKwArgs, serverArgs,
+                             serverKwArgs):
         self.clientBase, self.clientCtxFactory = self.makeContextFactory(
             *clientArgs, **clientKwArgs)
         self.serverBase, self.serverCtxFactory = self.makeContextFactory(
             *serverArgs, **serverKwArgs)
 
 
+
 if SSL is not None:
     class ServerTLSContext(ssl.DefaultOpenSSLContextFactory):
-        isClient = 0
+        """
+        A context factory with a default method set to L{SSL.TLSv1_METHOD}.
+        """
+        isClient = False
+
         def __init__(self, *args, **kw):
             kw['sslmethod'] = SSL.TLSv1_METHOD
             ssl.DefaultOpenSSLContextFactory.__init__(self, *args, **kw)
+
 
 
 class StolenTCPTestCase(ProperlyCloseFilesMixin, WriteDataTestCase):
@@ -181,6 +273,7 @@ class StolenTCPTestCase(ProperlyCloseFilesMixin, WriteDataTestCase):
     For SSL transports, test many of the same things which are tested for
     TCP transports.
     """
+
     def createServer(self, address, portNumber, factory):
         contextFactory = ssl.CertificateOptions()
         return reactor.listenSSL(
@@ -204,82 +297,109 @@ class StolenTCPTestCase(ProperlyCloseFilesMixin, WriteDataTestCase):
         return ProperlyCloseFilesMixin.getHandleErrorCode(self)
 
 
-class TLSTestCase(unittest.TestCase):
-    fillBuffer = 0
 
-    port = None
+class TLSTestCase(unittest.TestCase):
+    """
+    Tests for startTLS support.
+
+    @ivar fillBuffer: forwarded to L{LineCollector.fillBuffer}
+    @type fillBuffer: C{bool}
+    """
+    fillBuffer = False
+
     clientProto = None
     serverProto = None
 
+
     def tearDown(self):
-        if self.clientProto is not None and self.clientProto.transport is not None:
+        if self.clientProto.transport is not None:
             self.clientProto.transport.loseConnection()
-        if self.serverProto is not None and self.serverProto.transport is not None:
+        if self.serverProto.transport is not None:
             self.serverProto.transport.loseConnection()
 
-        if self.port is not None:
-            return defer.maybeDeferred(self.port.stopListening)
 
     def _runTest(self, clientProto, serverProto, clientIsServer=False):
+        """
+        Helper method to run TLS tests.
+
+        @param clientProto: protocol instance attached to the client
+            connection.
+        @param serverProto: protocol instance attached to the server
+            connection.
+        @param clientIsServer: flag indicated if client should initiate
+            startTLS instead of server.
+
+        @return: a L{defer.Deferred} that will fire when both connections are
+            lost.
+        """
         self.clientProto = clientProto
         cf = self.clientFactory = protocol.ClientFactory()
         cf.protocol = lambda: clientProto
         if clientIsServer:
-            cf.server = 0
+            cf.server = False
         else:
-            cf.client = 1
+            cf.client = True
 
         self.serverProto = serverProto
         sf = self.serverFactory = protocol.ServerFactory()
         sf.protocol = lambda: serverProto
         if clientIsServer:
-            sf.client = 0
+            sf.client = False
         else:
-            sf.server = 1
+            sf.server = True
 
-        if clientIsServer:
-            inCharge = cf
-        else:
-            inCharge = sf
-        inCharge.done = 0
+        port = reactor.listenTCP(0, sf, interface="127.0.0.1")
+        self.addCleanup(port.stopListening)
 
-        port = self.port = reactor.listenTCP(0, sf, interface="127.0.0.1")
-        portNo = port.getHost().port
+        reactor.connectTCP('127.0.0.1', port.getHost().port, cf)
 
-        reactor.connectTCP('127.0.0.1', portNo, cf)
-
-        i = 0
-        while i < 1000 and not inCharge.done:
-            reactor.iterate(0.01)
-            i += 1
-        self.failUnless(
-            inCharge.done,
-            "Never finished reading all lines: %s" % (inCharge.lines,))
+        return defer.gatherResults([clientProto.deferred, serverProto.deferred])
 
 
-    def testTLS(self):
-        self._runTest(UnintelligentProtocol(), LineCollector(1, self.fillBuffer))
-        self.assertEquals(
-            self.serverFactory.lines,
-            UnintelligentProtocol.pretext + UnintelligentProtocol.posttext
-        )
+    def test_TLS(self):
+        """
+        Test for server and client startTLS: client should received data both
+        before and after the startTLS.
+        """
+        def check(ignore):
+            self.assertEquals(
+                self.serverFactory.lines,
+                UnintelligentProtocol.pretext + UnintelligentProtocol.posttext
+            )
+        d = self._runTest(UnintelligentProtocol(),
+                          LineCollector(True, self.fillBuffer))
+        return d.addCallback(check)
 
 
-    def testUnTLS(self):
-        self._runTest(UnintelligentProtocol(), LineCollector(0, self.fillBuffer))
-        self.assertEquals(
-            self.serverFactory.lines,
-            UnintelligentProtocol.pretext
-        )
-        self.failUnless(self.serverFactory.rawdata, "No encrypted bytes received")
+    def test_unTLS(self):
+        """
+        Test for server startTLS not followed by a startTLS in client: the data
+        received after server startTLS should be received as raw.
+        """
+        def check(ignored):
+            self.assertEquals(
+                self.serverFactory.lines,
+                UnintelligentProtocol.pretext
+            )
+            self.failUnless(self.serverFactory.rawdata,
+                            "No encrypted bytes received")
+        d = self._runTest(UnintelligentProtocol(),
+                          LineCollector(False, self.fillBuffer))
+        return d.addCallback(check)
 
 
-    def testBackwardsTLS(self):
-        self._runTest(LineCollector(1, self.fillBuffer), UnintelligentProtocol(), True)
-        self.assertEquals(
-            self.clientFactory.lines,
-            UnintelligentProtocol.pretext + UnintelligentProtocol.posttext
-        )
+    def test_backwardsTLS(self):
+        """
+        Test startTLS first initiated by client.
+        """
+        def check(ignored):
+            self.assertEquals(
+                self.clientFactory.lines,
+                UnintelligentProtocol.pretext + UnintelligentProtocol.posttext
+            )
+        d = self._runTest(LineCollector(True, self.fillBuffer),
+                          UnintelligentProtocol(), True)
+        return d.addCallback(check)
 
 
 
@@ -289,39 +409,41 @@ _bufferedSuppression = trial_util.suppress(
     category=RuntimeWarning)
 
 
+
 class SpammyTLSTestCase(TLSTestCase):
     """
     Test TLS features with bytes sitting in the out buffer.
     """
-    fillBuffer = 1
+    fillBuffer = True
 
-    def testTLS(self):
-        return TLSTestCase.testTLS(self)
-    testTLS.suppress = [_bufferedSuppression]
-    testTLS.todo = "startTLS doesn't empty buffer before starting TLS. :("
+    def test_TLS(self):
+        return TLSTestCase.test_TLS(self)
+
+    test_TLS.suppress = [_bufferedSuppression]
+    test_TLS.todo = "startTLS doesn't empty buffer before starting TLS. :("
 
 
-    def testBackwardsTLS(self):
-        return TLSTestCase.testBackwardsTLS(self)
-    testBackwardsTLS.suppress = [_bufferedSuppression]
-    testBackwardsTLS.todo = "startTLS doesn't empty buffer before starting TLS. :("
+    def test_backwardsTLS(self):
+        return TLSTestCase.test_backwardsTLS(self)
+
+    test_backwardsTLS.suppress = [_bufferedSuppression]
+    test_backwardsTLS.todo = "startTLS doesn't empty buffer before starting TLS. :("
+
 
 
 class BufferingTestCase(unittest.TestCase):
-    port = None
-    connector = None
     serverProto = None
     clientProto = None
 
-    def tearDown(self):
-        if self.serverProto is not None and self.serverProto.transport is not None:
-            self.serverProto.transport.loseConnection()
-        if self.clientProto is not None and self.clientProto.transport is not None:
-            self.clientProto.transport.loseConnection()
-        if self.port is not None:
-            return defer.maybeDeferred(self.port.stopListening)
 
-    def testOpenSSLBuffering(self):
+    def tearDown(self):
+        if self.serverProto.transport is not None:
+            self.serverProto.transport.loseConnection()
+        if self.clientProto.transport is not None:
+            self.clientProto.transport.loseConnection()
+
+
+    def test_openSSLBuffering(self):
         serverProto = self.serverProto = SingleLineServerProtocol()
         clientProto = self.clientProto = RecordingClientProtocol()
 
@@ -330,20 +452,18 @@ class BufferingTestCase(unittest.TestCase):
 
         server.protocol = lambda: serverProto
         client.protocol = lambda: clientProto
-        client.buffer = []
 
         sCTX = ssl.DefaultOpenSSLContextFactory(certPath, certPath)
         cCTX = ssl.ClientContextFactory()
 
-        port = self.port = reactor.listenSSL(0, server, sCTX, interface='127.0.0.1')
+        port = reactor.listenSSL(0, server, sCTX, interface='127.0.0.1')
+        self.addCleanup(port.stopListening)
+
         reactor.connectSSL('127.0.0.1', port.getHost().port, client, cCTX)
 
-        i = 0
-        while i < 5000 and not client.buffer:
-            i += 1
-            reactor.iterate()
+        return clientProto.deferred.addCallback(
+            self.assertEquals, "+OK <some crap>\r\n")
 
-        self.assertEquals(client.buffer, ["+OK <some crap>\r\n"])
 
 
 class ConnectionLostTestCase(unittest.TestCase, ContextGeneratingMixin):
@@ -369,6 +489,7 @@ class ConnectionLostTestCase(unittest.TestCase, ContextGeneratingMixin):
 
         return clientProtocolFactory.connectionDisconnected.addCallback(
             lambda ignoredResult: self.serverPort.stopListening())
+
 
     def testFailedVerify(self):
         org = "twisted.test.test_ssl"
@@ -399,6 +520,7 @@ class ConnectionLostTestCase(unittest.TestCase, ContextGeneratingMixin):
         dl = defer.DeferredList([serverConnLost, clientConnLost], consumeErrors=True)
         return dl.addCallback(self._cbLostConns)
 
+
     def _cbLostConns(self, results):
         (sSuccess, sResult), (cSuccess, cResult) = results
 
@@ -425,7 +547,11 @@ class ConnectionLostTestCase(unittest.TestCase, ContextGeneratingMixin):
         return self.serverPort.stopListening()
 
 
+
 if interfaces.IReactorSSL(reactor, None) is None:
     for tCase in [StolenTCPTestCase, TLSTestCase, SpammyTLSTestCase,
                   BufferingTestCase, ConnectionLostTestCase]:
         tCase.skip = "Reactor does not support SSL, cannot run SSL tests"
+
+# Otherwise trial will run this test here
+del WriteDataTestCase
