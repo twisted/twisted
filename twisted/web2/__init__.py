4abf3a4948eba517dff1cbe1ee7b59361e39e143
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-06-18 13:32:34 +0000

    Apply doc patch clarifying the state of twisted.web2
    Author: exarkun
    Reviewer: jml
    Fixes: #2028
    
    Explain what web2 is, what is being done with it, and link to the wiki page
    for more details.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@23951 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web2/__init__.py
+++ twisted/web2/__init__.py
@@ -1,12 +1,14 @@
 # -*- test-case-name: twisted.web2.test -*-
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-
 """
+Twisted Web2: an experimental web server implementation.
 
-Twisted Web2: a better Twisted Web Server.
+Features from this package are being merged into L{twisted.web}.  Once that
+is complete, this package will be removed.
 
+See U{http://twistedmatrix.com/trac/wiki/WebDevelopmentWithTwisted}.
 """
 
 from twisted.web2._version import version
