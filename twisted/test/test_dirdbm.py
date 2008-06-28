8a0e74cdf5a9e11f9eb47b1154475dd6191d23b0
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-06-28 15:25:16 +0000

    Remove useless tearDown method of twisted.test.test_dirdmb.DirDbmTestCase because it's causing intermittent failures under windows.
    Author: therve
    Reviewer: exarkub
    Fixes #3319
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@24131 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_dirdbm.py
+++ twisted/test/test_dirdbm.py
@@ -1,15 +1,16 @@
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
 # See LICENSE for details.
-
-
 
 """
 Test cases for dirdbm module.
 """
 
+import os, shutil, glob
+
 from twisted.trial import unittest
 from twisted.persisted import dirdbm
-import os, shutil, glob
+
+
 
 class DirDbmTestCase(unittest.TestCase):
 
@@ -18,27 +19,27 @@ class DirDbmTestCase(unittest.TestCase):
         self.dbm = dirdbm.open(self.path)
         self.items = (('abc', 'foo'), ('/lalal', '\000\001'), ('\000\012', 'baz'))
 
-    def tearDown(self):
-        shutil.rmtree(self.path)
 
     def testAll(self):
         k = "//==".decode("base64")
         self.dbm[k] = "a"
         self.dbm[k] = "a"
         self.assertEquals(self.dbm[k], "a")
-        
+
+
     def testRebuildInteraction(self):
         from twisted.persisted import dirdbm
         from twisted.python import rebuild
-        
+
         s = dirdbm.Shelf('dirdbm.rebuild.test')
         s['key'] = 'value'
         rebuild.rebuild(dirdbm)
         # print s['key']
 
+
     def testDbm(self):
         d = self.dbm
-        
+
         # insert keys
         keys = []
         values = []
@@ -48,12 +49,12 @@ class DirDbmTestCase(unittest.TestCase):
             values.append(v)
         keys.sort()
         values.sort()
-        
+
         # check they exist
         for k, v in self.items:
             assert d.has_key(k), "has_key() failed"
             assert d[k] == v, "database has wrong value"
-        
+
         # check non existent key
         try:
             d["XXX"]
@@ -61,7 +62,7 @@ class DirDbmTestCase(unittest.TestCase):
             pass
         else:
             assert 0, "didn't raise KeyError on non-existent key"
-        
+
         # check keys(), values() and items()
         dbkeys = list(d.keys())
         dbvalues = list(d.values())
@@ -74,25 +75,25 @@ class DirDbmTestCase(unittest.TestCase):
         assert keys == dbkeys, ".keys() output didn't match: %s != %s" % (repr(keys), repr(dbkeys))
         assert values == dbvalues, ".values() output didn't match: %s != %s" % (repr(values), repr(dbvalues))
         assert items == dbitems, "items() didn't match: %s != %s" % (repr(items), repr(dbitems))
-        
+
         copyPath = self.mktemp()
         d2 = d.copyTo(copyPath)
-        
+
         copykeys = list(d.keys())
         copyvalues = list(d.values())
         copyitems = list(d.items())
         copykeys.sort()
         copyvalues.sort()
         copyitems.sort()
-        
+
         assert dbkeys == copykeys, ".copyTo().keys() didn't match: %s != %s" % (repr(dbkeys), repr(copykeys))
         assert dbvalues == copyvalues, ".copyTo().values() didn't match: %s != %s" % (repr(dbvalues), repr(copyvalues))
         assert dbitems == copyitems, ".copyTo().items() didn't match: %s != %s" % (repr(dbkeys), repr(copyitems))
-        
+
         d2.clear()
         assert len(d2.keys()) == len(d2.values()) == len(d2.items()) == 0, ".clear() failed"
         shutil.rmtree(copyPath)
-        
+
         # delete items
         for k, v in self.items:
             del d[k]
@@ -100,7 +101,7 @@ class DirDbmTestCase(unittest.TestCase):
         assert len(d.keys()) == 0, "database has keys"
         assert len(d.values()) == 0, "database has values"
         assert len(d.items()) == 0, "database has items"
-        
+
 
     def testModificationTime(self):
         import time
@@ -115,14 +116,15 @@ class DirDbmTestCase(unittest.TestCase):
         # -warner
         self.dbm["k"] = "v"
         self.assert_(abs(time.time() - self.dbm.getModificationTime("k")) <= 3)
-    
+
+
     def testRecovery(self):
-        """DirDBM: test recovery from directory after a faked crash""" 
+        """DirDBM: test recovery from directory after a faked crash"""
         k = self.dbm._encode("key1")
         f = open(os.path.join(self.path, k + ".rpl"), "wb")
         f.write("value")
         f.close()
-        
+
         k2 = self.dbm._encode("key2")
         f = open(os.path.join(self.path, k2), "wb")
         f.write("correct")
@@ -130,11 +132,11 @@ class DirDbmTestCase(unittest.TestCase):
         f = open(os.path.join(self.path, k2 + ".rpl"), "wb")
         f.write("wrong")
         f.close()
-        
+
         f = open(os.path.join(self.path, "aa.new"), "wb")
         f.write("deleted")
         f.close()
-        
+
         dbm = dirdbm.DirDBM(self.path)
         assert dbm["key1"] == "value"
         assert dbm["key2"] == "correct"
