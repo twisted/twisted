da7b4156ab8d5686e2dce2ada65ccc73a75e011e
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-06-28 15:01:36 +0000

    Apply twistedquotes.diff, improving the organization of code in the quote example
    Author: exarkun
    Reviewer: radix
    Fixes: #1489
    
    Move the `IQuoter` interface to be close to the factory which uses it,
    instead of being close to the one implementation of it.  This more
    closely resembles the organization of a real program, where the user
    of an interface does not know or care where the implementation comes
    from, only that it behaves correctly, and where there may be several
    implementations of the interface in different places.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@24129 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/core/howto/listings/TwistedQuotes/quoters.py
+++ doc/core/howto/listings/TwistedQuotes/quoters.py
@@ -1,34 +1,39 @@
-from zope.interface import Interface, implements
-
 from random import choice
 
+from zope.interface import implements
 
-class IQuoter(Interface):
-    """An object that returns quotes."""
-    
-    def getQuote():
-        """Return a quote."""
+from TwistedQuotes import quoteproto
+
 
 
 class StaticQuoter:
-    """Return a static quote."""
-    
-    implements(IQuoter)
-    
+    """
+    Return a static quote.
+    """
+
+    implements(quoteproto.IQuoter)
+
     def __init__(self, quote):
         self.quote = quote
-    
+
+
     def getQuote(self):
         return self.quote
 
 
+
 class FortuneQuoter:
-    """Load quotes from a fortune-format file."""
-    
-    implements(IQuoter)
-    
+    """
+    Load quotes from a fortune-format file.
+    """
+    implements(quoteproto.IQuoter)
+
     def __init__(self, filenames):
         self.filenames = filenames
 
+
     def getQuote(self):
-        return choice(open(choice(self.filenames)).read().split('\n%\n'))
+        quoteFile = file(choice(self.filenames))
+        quotes = quoteFile.read().split('\n%\n')
+        quoteFile.close()
+        return choice(quotes)
