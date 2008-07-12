ccf20f378ccd78be0a884fd3d00ced90fb30c5b4
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-07-12 15:47:44 +0000

    Merge twisted.internet.reactor-doc-1144
    Author: exarkun
    Reviewer: thijs
    Fixes: #1144
    
    Add a module docstring to twisted.internet.reactor so that pydoctor has something
    to pick up and include on the page for that module.  The docstring briefly explains
    the reactor and points to the documentation for a number reactor interfaces.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@24262 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/reactor.py
+++ twisted/internet/reactor.py
@@ -1,12 +1,38 @@
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 """
-See twisted.internet.interfaces.IReactor*.
+The reactor is the Twisted event loop within Twisted, the loop which drives
+applications using Twisted. The reactor provides APIs for networking,
+threading, dispatching events, and more.
+
+The default reactor is based on C{select(2)} and will be installed if this
+module is imported without another reactor being explicitly installed.
+Regardless of which reactor is installed, importing this module is the correct
+way to get a reference to it.
+
+New application code should prefer to pass and accept the reactor as a
+parameter where it is needed, rather than relying on being able to import this
+module to get a reference.  This simplifies unit testing and may make it easier
+to one day support multiple reactors (as a performance enhancement), though
+this is not currently possible.
+
+@see: L{IReactorCore<twisted.internet.interfaces.IReactorCore>}
+@see: L{IReactorTime<twisted.internet.interfaces.IReactorTime>}
+@see: L{IReactorProcess<twisted.internet.interfaces.IReactorProcess>}
+@see: L{IReactorTCP<twisted.internet.interfaces.IReactorTCP>}
+@see: L{IReactorSSL<twisted.internet.interfaces.IReactorSSL>}
+@see: L{IReactorUDP<twisted.internet.interfaces.IReactorUDP>}
+@see: L{IReactorMulticast<twisted.internet.interfaces.IReactorMulticast>}
+@see: L{IReactorUNIX<twisted.internet.interfaces.IReactorUNIX>}
+@see: L{IReactorUNIXDatagram<twisted.internet.interfaces.IReactorUNIXDatagram>}
+@see: L{IReactorFDSet<twisted.internet.interfaces.IReactorFDSet>}
+@see: L{IReactorThreads<twisted.internet.interfaces.IReactorThreads>}
+@see: L{IReactorArbitrary<twisted.internet.interfaces.IReactorArbitrary>}
+@see: L{IReactorPluggableResolver<twisted.internet.interfaces.IReactorPluggableResolver>}
 """
+
 import sys
 del sys.modules['twisted.internet.reactor']
-#from twisted.python import log
-#log.msg("Installing SelectReactor, since unspecified.")
 from twisted.internet import selectreactor
 selectreactor.install()
