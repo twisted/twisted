8a9693e76e34f95806dfcaa1439b051e7bdeb8f7
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-07-16 12:37:41 +0000

    Merge clean-admin-3165
    Author: radix
    Reviewer: exarkun
    Fixes: #3165
    
    Delete most of the files in admin/, because they are now unused.
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@24278 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- admin/echo.py
+++
@@ -1,9 +0,0 @@
-from twisted.internet import app, protocol
-from twisted.protocols import wire
-
-f = protocol.Factory()
-f.protocol = wire.Echo 
-
-application = app.Application("echo")
-
-application.listenTCP(18899, f)
