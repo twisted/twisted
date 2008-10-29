2a5458c93c3132295da27a34a5e53a028e937910
teratorn <teratorn@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-10-29 15:15:13 +0000

    merge delete-t-im-3507 Author: teratorn Reviewer: exarkun Fixes: #3507
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@25178 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- win32/twistenv.bat
+++ win32/twistenv.bat
@@ -10,6 +10,6 @@ echo -:- -:- -:- -:- -:--:- -:- -:- -:- -:--:- -:- -:- -:- -:-
 
 rem  # The following scripts are not advertised for the following reasons
 rem  #  conch - issues an exception when run with no arguments
-rem  #  im, t-im - issue exceptions for missing gtk when run
+rem  #  im - issue exceptions for missing gtk when run
 rem  #  tap2deb - platform-specific
 rem  #  tk* - no need; the ones that work have icons in the start menu
