a949bd0f896b9c6c85e43d160092810eb67b86cf
ralphm <ralphm@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-11-02 11:10:26 +0000

    Add XMPP router functionality and twistd plugin 'xmpp-router'.
    Author: ralphm.
    Reviewer: therve.
    Fixes #3407.
    
    An XMPP router connects parts of a Jabber server known as 'components' and
    routes XML Stanzas between components based on addressing on stanzas.
    Components usually connect over a TCP socket, so this change adds a factory to
    listen for incoming component connections that are tied to the router. Using
    the new XmlPipe, components can also be connected to the router within the same
    process without using a socket. Finally, a random stream ID is generated for
    all incoming XML Streams.
    
    The new xmpp-router twistd plugin allows for easy deployment of a stand-alone
    XMPP router.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@25269 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/words/test/test_xmpproutertap.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Tests for L{twisted.words.xmpproutertap}.
+"""
+
+from twisted.application import internet
+from twisted.trial import unittest
+from twisted.words import xmpproutertap as tap
+from twisted.words.protocols.jabber import component
+
+class XMPPRouterTapTest(unittest.TestCase):
+
+    def test_port(self):
+        """
+        The port option is recognised as a parameter.
+        """
+        opt = tap.Options()
+        opt.parseOptions(['--port', '7001'])
+        self.assertEquals(opt['port'], '7001')
+
+
+    def test_portDefault(self):
+        """
+        The port option has '5347' as default value
+        """
+        opt = tap.Options()
+        opt.parseOptions([])
+        self.assertEquals(opt['port'], 'tcp:5347:interface=127.0.0.1')
+
+
+    def test_secret(self):
+        """
+        The secret option is recognised as a parameter.
+        """
+        opt = tap.Options()
+        opt.parseOptions(['--secret', 'hushhush'])
+        self.assertEquals(opt['secret'], 'hushhush')
+
+
+    def test_secretDefault(self):
+        """
+        The secret option has 'secret' as default value
+        """
+        opt = tap.Options()
+        opt.parseOptions([])
+        self.assertEquals(opt['secret'], 'secret')
+
+
+    def test_verbose(self):
+        """
+        The verbose option is recognised as a flag.
+        """
+        opt = tap.Options()
+        opt.parseOptions(['--verbose'])
+        self.assertTrue(opt['verbose'])
+
+
+    def test_makeService(self):
+        """
+        The service gets set up with a router and factory.
+        """
+        opt = tap.Options()
+        opt.parseOptions([])
+        s = tap.makeService(opt)
+        self.assertIsInstance(s, internet.TCPServer)
+        self.assertEquals('127.0.0.1', s.kwargs['interface'])
+        self.assertEquals(2, len(s.args))
+        self.assertEquals(5347, s.args[0])
+        factory = s.args[1]
+        self.assertIsInstance(factory, component.XMPPComponentServerFactory)
+        self.assertIsInstance(factory.router, component.Router)
+        self.assertEquals('secret', factory.secret)
+        self.assertFalse(factory.logTraffic)
+
+
+    def test_makeServiceVerbose(self):
+        """
+        The verbose flag enables traffic logging.
+        """
+        opt = tap.Options()
+        opt.parseOptions(['--verbose'])
+        s = tap.makeService(opt)
+        factory = s.args[1]
+        self.assertTrue(factory.logTraffic)
--- twisted/words/xish/utility.py
+++ twisted/words/xish/utility.py
@@ -27,6 +27,7 @@ class _MethodWrapper(object):
         self.method(*nargs, **nkwargs)
 
 
+
 class CallbackList:
     """
     Container for callbacks.
@@ -333,3 +334,39 @@ class EventDispatcher:
             self._updateQueue = []
 
         return foundTarget
+
+
+
+class XmlPipe(object):
+    """
+    XML stream pipe.
+
+    Connects two objects that communicate stanzas through an XML stream like
+    interface. Each of the ends of the pipe (sink and source) can be used to
+    send XML stanzas to the other side, or add observers to process XML stanzas
+    that were sent from the other side.
+
+    XML pipes are usually used in place of regular XML streams that are
+    transported over TCP. This is the reason for the use of the names source
+    and sink for both ends of the pipe. The source side corresponds with the
+    entity that initiated the TCP connection, whereas the sink corresponds with
+    the entity that accepts that connection. In this object, though, the source
+    and sink are treated equally.
+
+    Unlike Jabber
+    L{XmlStream<twisted.words.protocols.jabber.xmlstream.XmlStream>}s, the sink
+    and source objects are assumed to represent an eternal connected and
+    initialized XML stream. As such, events corresponding to connection,
+    disconnection, initialization and stream errors are not dispatched or
+    processed.
+
+    @since: 8.2
+    @ivar source: Source XML stream.
+    @ivar sink: Sink XML stream.
+    """
+
+    def __init__(self):
+        self.source = EventDispatcher()
+        self.sink = EventDispatcher()
+        self.source.send = lambda obj: self.sink.dispatch(obj)
+        self.sink.send = lambda obj: self.source.dispatch(obj)
