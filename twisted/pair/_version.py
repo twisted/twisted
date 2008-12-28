595b6b751b9a8e26fb4a80761b01065eee7c66d8
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2008-12-28 19:24:26 +0000

    Merge release-8.2.0-3540
    Author: radix
    Reviewer: exarkun
    Fixes: #3540
    
    NEWS file and version updates from the release of Twisted 8.2.0.
    
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@25735 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/pair/_version.py
+++ twisted/pair/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.pair', 8, 0, 0)
+version = versions.Version('twisted.pair', 8, 2, 0)
--- twisted/runner/_version.py
+++ twisted/runner/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.runner', 8, 0, 0)
+version = versions.Version('twisted.runner', 8, 2, 0)
