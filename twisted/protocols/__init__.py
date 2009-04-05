7f9fd813d512af26c206000ed87051326455c0d9
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2009-04-05 13:11:46 +0000

    Merge ref-split-code-3713: remove reference to split code
    Author: thijs
    Reviewer: exarkun
    Fixes: #3713
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@26647 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/__init__.py
+++ twisted/protocols/__init__.py
@@ -1,12 +1,7 @@
-# Copyright (c) 2001-2008 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2009 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 
 """
 Twisted Protocols: a collection of internet protocol implementations.
-
-Many of the protocols found in this package have been moved after the
-package split in Twisted 2.0. This means you will get a C{DeprecationWarning}
-if you try to import, e.g., L{twisted.protocols.http} notifying you
-that it has been moved to L{twisted.web.http}.
 """
--- twisted/protocols/xmlstream.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn("twisted.protocols.xmlstream is DEPRECATED. import twisted.words.xish.xmlstream instead.",
-              DeprecationWarning, stacklevel=2)
-
-from twisted.words.xish.xmlstream import *
