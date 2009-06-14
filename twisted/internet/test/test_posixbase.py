76df5b3e24fc988b7fdaf0083f9b45cec4a828fc
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2009-06-14 08:36:02 +0000

    Merge tcp-test-posixbase-3677
    Author: therve
    Reviewer: mwh
    Fixes #3677
    
    Skip tests expecting a PosixReactorBase variant on non-PosixReactorBase
    reactors (ie iocp).
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@27018 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_posixbase.py
+++ twisted/internet/test/test_posixbase.py
@@ -10,6 +10,7 @@ from twisted.trial.unittest import TestCase
 from twisted.internet.posixbase import PosixReactorBase, _Waker
 from twisted.internet.protocol import ServerFactory
 from twisted.internet.tcp import Port
+from twisted.internet import reactor
 
 
 class TrivialReactor(PosixReactorBase):
@@ -40,6 +41,7 @@ class PosixReactorBaseTests(TestCase):
     """
     Tests for L{PosixReactorBase}.
     """
+
     def _checkWaker(self, reactor):
         self.assertIsInstance(reactor.waker, _Waker)
         self.assertIn(reactor.waker, reactor._internalReaders)
@@ -92,14 +94,15 @@ class TCPPortTests(TestCase):
     """
     Tests for L{twisted.internet.tcp.Port}.
     """
+
+    if not isinstance(reactor, PosixReactorBase):
+        skip = "Non-posixbase reactor"
+
     def test_connectionLostFailed(self):
         """
         L{Port.stopListening} returns a L{Deferred} which errbacks if
         L{Port.connectionLost} raises an exception.
         """
-        class UniqueExceptionType(Exception):
-            pass
-
         port = Port(12345, ServerFactory())
         port.connected = True
         port.connectionLost = lambda reason: 1 / 0
