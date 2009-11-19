b08a5021bd83d5d938c608a9071fd3c413d2d945
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2009-11-19 21:23:24 +0000

    Fix copyright dates
    refs #3956
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/adbapi-arraysize-3956@27558 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/enterprise/adbapi.py
+++ twisted/enterprise/adbapi.py
@@ -1,5 +1,5 @@
 # -*- test-case-name: twisted.test.test_adbapi -*-
-# Copyright (c) 2009 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2009 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 """
