66adb7a7b7b1400dcf9f85efa7d7340b4afad1e5
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2009-11-20 02:50:27 +0000

    Update versions.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-9.0.x-3860-3@27573 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/_version.py
+++ twisted/lore/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.lore', 8, 2, 0)
+version = versions.Version('twisted.lore', 9, 0, 0, prerelease=1)
