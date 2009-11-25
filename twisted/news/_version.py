90f4c5d15ff720ba3b43b9b91ab2d6efd135f781
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2009-11-25 22:11:32 +0000

    Merge releases/release-9.0.x-3860-5
    Author: radix
    Reviewer: exarkun
    Fixes: #3860
    
    Update all the version numbers and add NEWS entries for the 9.0 release.
    In addition, LoopingCall.withCount's "@since" property was changed to
    say 9.0 instead of 9.1.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@27607 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/news/_version.py
+++ twisted/news/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.news', 8, 2, 0)
+version = versions.Version('twisted.news', 9, 0, 0)
--- twisted/web/_version.py
+++ twisted/web/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.web', 8, 2, 0)
+version = versions.Version('twisted.web', 9, 0, 0)
