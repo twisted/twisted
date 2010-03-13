7855df3faf21f0746e21cb003a0b0850bd5828ee
jesstess <jesstess@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2010-03-13 21:15:29 +0000

    Merge contact-changed-nick-229
    Author: jesstess
    Reviewer: glyph
    Fixes: #229
    
    twisted.words.im.basechat.ChatUI now has a functional
    contactChangedNick with unit tests.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@28721 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/im/basechat.py
+++ twisted/words/im/basechat.py
@@ -1,11 +1,13 @@
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# -*- test-case-name: twisted.words.test.test_basechat -*-
+# Copyright (c) 2001-2010 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-#
-
-"""Base classes for Instance Messenger clients."""
+"""
+Base classes for Instance Messenger clients.
+"""
 
 from twisted.words.im.locals import OFFLINE, ONLINE, AWAY
+
 
 class ContactsList:
     """A GUI object that displays a contacts list"""
@@ -103,6 +105,7 @@ class Conversation:
         self.person.name = newnick
 
 
+
 class GroupConversation:
     """A conversation with a group of people."""
     def __init__(self, group, chatui):
@@ -184,17 +187,43 @@ class GroupConversation:
 
 
 class ChatUI:
-    """A GUI chat client"""
+    """
+    A GUI chat client.
+
+    @type conversations: C{dict} of L{Conversation}.
+    @ivar conversations: A cache of all the direct windows.
+
+    @type groupConversations: C{dict} of L{GroupConversation}.
+    @ivar groupConversations: A cache of all the group windows.
+
+    @type persons: C{dict} with keys that are a C{tuple} of (C{str},
+        L{basesupport.AbstractAccount}) and values that are
+        L{Person<interfaces.IPerson>}.
+    @ivar persons: A cache of all the users associated with this client.
+
+    @type groups: C{dict} with keys that are a C{tuple} of (C{str},
+        L{basesupport.AbstractAccount}) and values that are
+        L{Group<interfaces.IGroup>}
+    @ivar groups: A cache of all the user groups associated with this client.
+
+    @type onlineClients: C{list} of L{Client<interfaces.IClient>}
+    @ivar onlineClients: A list of message sources currently online.
+
+    @type contactsList: L{ContactsList}
+    @ivar contactsList: A contacts list.
+    """
     def __init__(self):
-        self.conversations = {}      # cache of all direct windows
-        self.groupConversations = {} # cache of all group windows
-        self.persons = {}            # keys are (name, client)
-        self.groups = {}             # cache of all groups
-        self.onlineClients = []      # list of message sources currently online
+        self.conversations = {}
+        self.groupConversations = {}
+        self.persons = {}
+        self.groups = {}
+        self.onlineClients = []
         self.contactsList = ContactsList(self)
 
+
     def registerAccountClient(self, client):
-        """Notifies user that an account has been signed on to.
+        """
+        Notifies user that an account has been signed on to.
 
         @type client: L{Client<interfaces.IClient>}
         @returns: client, so that I may be used in a callback chain
@@ -204,8 +233,10 @@ class ChatUI:
         self.contactsList.registerAccountClient(client)
         return client
 
+
     def unregisterAccountClient(self, client):
-        """Notifies user that an account has been signed off or disconnected
+        """
+        Notifies user that an account has been signed off or disconnected
 
         @type client: L{Client<interfaces.IClient>}
         """
@@ -213,14 +244,17 @@ class ChatUI:
         self.onlineClients.remove(client)
         self.contactsList.unregisterAccountClient(client)
 
+
     def getContactsList(self):
         """
         @returntype: L{ContactsList}
         """
         return self.contactsList
 
+
     def getConversation(self, person, Class=Conversation, stayHidden=0):
-        """For the given person object, returns the conversation window
+        """
+        For the given person object, returns the conversation window
         or creates and returns a new conversation window if one does not exist.
 
         @type person: L{Person<interfaces.IPerson>}
@@ -239,8 +273,10 @@ class ChatUI:
             conv.show()
         return conv
 
+
     def getGroupConversation(self,group,Class=GroupConversation,stayHidden=0):
-        """For the given group object, returns the group conversation window or
+        """
+        For the given group object, returns the group conversation window or
         creates and returns a new group conversation window if it doesn't exist
 
         @type group: L{Group<interfaces.IGroup>}
@@ -259,8 +295,10 @@ class ChatUI:
             conv.show()
         return conv
 
+
     def getPerson(self, name, client):
-        """For the given name and account client, returns the instance of the
+        """
+        For the given name and account client, returns the instance of the
         AbstractPerson subclass, or creates and returns a new AbstractPerson
         subclass of the type Class
 
@@ -276,8 +314,10 @@ class ChatUI:
             self.persons[name, account] = p
         return p
 
+
     def getGroup(self, name, client):
-        """For the given name and account client, returns the instance of the
+        """
+        For the given name and account client, returns the instance of the
         AbstractGroup subclass, or creates and returns a new AbstractGroup
         subclass of the type Class
 
@@ -296,21 +336,25 @@ class ChatUI:
             self.groups[name, account] = g
         return g
 
-    def contactChangedNick(self, oldnick, newnick):
-        """For the given person, changes the person's name to newnick, and
-        tells the contact list and any conversation windows with that person
-        to change as well.
 
-        @type oldnick: string
-        @type newnick: string
+    def contactChangedNick(self, person, newnick):
         """
-        if self.persons.has_key((person.name, person.account)):
+        For the given C{person}, change the C{person}'s C{name} to C{newnick}
+        and tell the contact list and any conversation windows with that
+        C{person} to change as well.
+
+        @type person: L{Person<interfaces.IPerson>}
+        @param person: The person whose nickname will get changed.
+
+        @type newnick: C{str}
+        @param newnick: The new C{name} C{person} will take.
+        """
+        oldnick = person.name
+        if self.persons.has_key((oldnick, person.account)):
             conv = self.conversations.get(person)
             if conv:
                 conv.contactChangedNick(person, newnick)
-
             self.contactsList.contactChangedNick(person, newnick)
-
-            del self.persons[person.name, person.account]
+            del self.persons[oldnick, person.account]
             person.name = newnick
             self.persons[person.name, person.account] = person
