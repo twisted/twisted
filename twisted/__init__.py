c994a6694875048a3d2e8a47915b41a48c901a3d
jml <jml@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2010-03-21 12:20:06 +0000

    Merge ten-problems-4346
     * Author: jml
     * Reviewer: jesstess
     * Fixes #4346
    
    Fix some small but important defects in release-level documentation
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@28759 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/__init__.py
+++ twisted/__init__.py
@@ -1,6 +1,6 @@
 # -*- test-case-name: twisted -*-
 
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2010 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 
@@ -10,8 +10,8 @@ Twisted: The Framework Of Your Internet.
 
 # Ensure the user is running the version of python we require.
 import sys
-if not hasattr(sys, "version_info") or sys.version_info < (2,3):
-    raise RuntimeError("Twisted requires Python 2.3 or later.")
+if not hasattr(sys, "version_info") or sys.version_info < (2, 4):
+    raise RuntimeError("Twisted requires Python 2.4 or later.")
 del sys
 
 # Ensure compat gets imported
