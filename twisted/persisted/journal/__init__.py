85c04475f677670dbe56c34e65ff72c1c5319b5a
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2010-06-09 02:07:11 +0000

    Revert r29264 - missing news file
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@29265 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/persisted/journal/__init__.py
+++ twisted/persisted/journal/__init__.py
@@ -1,20 +1,10 @@
-# Copyright (c) 2010 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-#
+# 
 
 """
 Command-journalling persistence framework inspired by Prevayler.
 
-This package is deprecated since Twisted 10.1.
-
 Maintainer: Itamar Shtull-Trauring
-
 """
-
-import warnings
-warnings.warn(
-    "twisted.persisted.journal is deprecated since Twisted 10.1",
-    category=DeprecationWarning,
-    stacklevel=2)
-
