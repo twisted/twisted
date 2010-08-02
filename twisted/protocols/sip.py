559b056d2b60b57900ba4a221b9b0b5dea18c388
mithrandi <mithrandi@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2010-08-02 18:03:30 +0000

    Author: ivank, exarkun, mithrandi Reviewer: exarkun, zooko, Screwtape Fixes: #4536
    Change comparisons of sensitive materials to use a helper function which is
    resistent against leaking timing information.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@29906 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/sip.py
+++ twisted/protocols/sip.py
@@ -1,6 +1,5 @@
 # -*- test-case-name: twisted.test.test_sip -*-
-
-# Copyright (c) 2001-2009 Twisted Matrix Laboratories.
+# Copyright (c) 2001-2010 Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 
@@ -1083,7 +1082,9 @@ class DigestedCredentials(UsernameHashedPassword):
             nonce, nc, cnonce, qop, method, uri, None,
         )
 
-        return expected == response
+        return util.slowStringCompare(expected, response)
+
+
 
 class DigestAuthorizer:
     CHALLENGE_LIFETIME = 15
