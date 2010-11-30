875638dba0bd09513444c8491173e2d0de369d2a
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2010-11-30 02:27:23 +0000

    ./bin/admin/change-versions 10.2.0!!!!!
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-10.2.0-4651-2@30362 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/news/_version.py
+++ twisted/news/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.news', 10, 2, 0, prerelease=3)
+version = versions.Version('twisted.news', 10, 2, 0)
