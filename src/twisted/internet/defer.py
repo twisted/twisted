1dfae6f0222535c4fa0409e37a9568016cb76a9f
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-01-17 00:54:57 +0000

    I did not realize how tortured that docstring fragment was.  Also, it documented the behavior I said was a bad idea.  So, remove that and edit it so it reads a bit better.  Finally, correct C() syntax (that's not epytext, C{} is).
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/cascade-4632@30512 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- src/twisted/internet/defer.py
+++ src/twisted/internet/defer.py
@@ -1329,11 +1329,12 @@ def inlineCallbacks(f):
         def loadData(url):
             response = yield makeRequest(url)
             return json.loads(response)
-    You can cancel (or even errback or callback) the L{Deferred} returned from
-    your C(inlineCallbacks) generator before it errback or callback by generator
-    result. In this case yield throws standard C(GeneratorExit) exception. And
-    L{Deferred} that yield was waiting for will be cancelled too and it's
-    C(CancelledError) will be trapped.
+    You can cancel the L{Deferred} returned from your L{inlineCallbacks}
+    generator before it is fired by your generator completing (either by
+    reaching its end, a C{return} statement, or by calling L{returnValue}).
+    When it is cancelled, a 'yield' waiting for a L{Deferred} result will throw
+    a C{GeneratorExit} exception. The L{Deferred} that yield was waiting for
+    will be cancelled too and its C{CancelledError} will be trapped.
     """
     @wraps(f)
     def unwindGenerator(*args, **kwargs):
