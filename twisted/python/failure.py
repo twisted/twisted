82118e736e83b06fe995fff4370e2a4287559772
lvh <lvh@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-03-14 16:00:34 +0000

    Reverting 31020, apparently *does* fail on some buildbots.
    Reopens: #4928
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@31023 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/failure.py
+++ twisted/python/failure.py
@@ -17,7 +17,6 @@ import linecache
 import inspect
 import opcode
 from cStringIO import StringIO
-from inspect import getmro
 
 from twisted.python import reflect
 
@@ -281,7 +280,7 @@ class Failure:
                 ])
             tb = tb.tb_next
         if inspect.isclass(self.type) and issubclass(self.type, Exception):
-            parentCs = getmro(self.type)
+            parentCs = reflect.allYourBase(self.type)
             self.parents = map(reflect.qual, parentCs)
             self.parents.append(reflect.qual(self.type))
         else:
