e5e852f977f78c572e14e93cd0d1a02ad5e950ad
lewq <lewq@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-03-21 02:43:05 +0000

    Merge fd-closer-4881: Fix spawnProcess on FreeBSD.
    Author: lewq
    Reviewer: exarkun, glyph, jesstess, lvh
    Fixes: #4881
    
    twisted.internet.process now detects the most appropriate mechanism to use for
    detecting the open file descriptors on a system, getting Twisted working on
    FreeBSD even when fdescfs is not mounted.
    
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@31322 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/process.py
+++ twisted/internet/process.py
@@ -467,34 +467,124 @@ class _BaseProcess(BaseProcess, object):
                                           self.pid, self.status)
 
 
+class _FDDetector(object):
+    """
+    This class contains the logic necessary to decide which of the available
+    system techniques should be used to detect the open file descriptors for
+    the current process. The chosen technique gets monkey-patched into the
+    _listOpenFDs method of this class so that the detection only needs to occur
+    once.
+
+    @ivars listdir: The implementation of listdir to use. This gets overwritten
+        by the test cases.
+    @ivars getpid: The implementation of getpid to use, returns the PID of the
+        running process.
+    @ivars openfile: The implementation of open() to use, by default the Python
+        builtin.
+    """
+    # So that we can unit test this
+    listdir = os.listdir
+    getpid = os.getpid
+    openfile = open
+
+
+    def _listOpenFDs(self):
+        """
+        Figure out which implementation to use, then run it.
+        """
+        self._listOpenFDs = self._getImplementation()
+        return self._listOpenFDs()
+
+
+    def _getImplementation(self):
+        """
+        Check if /dev/fd works, if so, use that.  Otherwise, check if
+        /proc/%d/fd exists, if so use that.
+        
+        Otherwise, ask resource.getrlimit, if that throws an exception, then
+        fallback to _fallbackFDImplementation.
+        """
+        try:
+            self.listdir("/dev/fd")
+            if self._checkDevFDSanity(): # FreeBSD support :-)
+                return self._devFDImplementation
+            else:
+                return self._fallbackFDImplementation
+        except:
+            try:
+                self.listdir("/proc/%d/fd" % (self.getpid(),))
+                return self._procFDImplementation
+            except:
+                try:
+                    self._resourceFDImplementation() # Imports resource
+                    return self._resourceFDImplementation
+                except:
+                    return self._fallbackFDImplementation
+
+
+    def _checkDevFDSanity(self):
+        """
+        Returns true iff opening a file modifies the fds visible
+        in /dev/fd, as it should on a sane platform.
+        """
+        start = self.listdir("/dev/fd")
+        fp = self.openfile("/dev/null", "r")
+        end = self.listdir("/dev/fd")
+        return start != end
+
+
+    def _devFDImplementation(self):
+        """
+        Simple implementation for systems where /dev/fd actually works.
+        See: http://www.freebsd.org/cgi/man.cgi?fdescfs
+        """
+        dname = "/dev/fd"
+        result = [int(fd) for fd in os.listdir(dname)]
+        return result
+
+
+    def _procFDImplementation(self):
+        """
+        Simple implementation for systems where /proc/pid/fd exists (we assume
+        it works).
+        """
+        dname = "/proc/%d/fd" % (os.getpid(),)
+        return [int(fd) for fd in os.listdir(dname)]
+
+
+    def _resourceFDImplementation(self):
+        """
+        Fallback implementation where the resource module can inform us about
+        how many FDs we can expect.
+
+        Note that on OS-X we expect to be using the /dev/fd implementation.
+        """
+        import resource
+        maxfds = resource.getrlimit(resource.RLIMIT_NOFILE)[1] + 1
+        # OS-X reports 9223372036854775808. That's a lot of fds
+        # to close
+        if maxfds > 1024:
+            maxfds = 1024
+        return xrange(maxfds)
+
+
+    def _fallbackFDImplementation(self):
+        """
+        Fallback-fallback implementation where we just assume that we need to
+        close 256 FDs.
+        """
+        maxfds = 256
+        return xrange(maxfds)
+
+
+detector = _FDDetector()
 
 def _listOpenFDs():
     """
-    Return an iterable of potentially open file descriptors.
-
-    This function returns an iterable over the contents of /dev/fd or
-    /proc/<pid>/fd, if they're available on the platform. If they're not, the
-    returned value is the range [0, maxfds], where 'maxfds' is at least 256.
+    Use the global detector object to figure out which FD implementation to
+    use.
     """
-    dname = "/dev/fd"
-    try:
-        return [int(fd) for fd in os.listdir(dname)]
-    except:
-        dname = "/proc/%d/fd" % (os.getpid(),)
-        try:
-            return [int(fd) for fd in os.listdir(dname)]
-        except:
-            try:
-                import resource
-                maxfds = resource.getrlimit(resource.RLIMIT_NOFILE)[1] + 1
-                # OS-X reports 9223372036854775808. That's a lot of fds
-                # to close
-                if maxfds > 1024:
-                    maxfds = 1024
-            except:
-                maxfds = 256
-            return xrange(maxfds)
-
+    return detector._listOpenFDs()
 
 
 class Process(_BaseProcess):
