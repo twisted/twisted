194d4358bbe582aabcd01a6274310b98a7dfe050
jesstess <jesstess@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-03-22 22:51:24 +0000

    Update versions for 11.0.0pre1 release.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-11.0.0-4980@31391 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/_version.py
+++ twisted/web/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.web', 10, 2, 0)
+version = versions.Version('twisted.web', 11, 0, 0, prerelease=1)
