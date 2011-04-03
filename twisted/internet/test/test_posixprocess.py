a455c1d2ffc09d5ac9b8785b8916fbd6dbbb985a
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-04-03 18:17:31 +0000

    Merge expectedfds-test-5012
    Author: exarkun
    Reviewer: mithrandi
    Fixes: #5012
    
    Change `twisted.internet.test.test_posixprocess.FileDescriptorTests.test_expectedFDs`
    to not rely on `sys.stdin`, `sys.stdout`, and `sys.stderr`.  Instead, just manipulate
    a newly opened file descriptor.
    
    This helps reduce the dependence of this test on its execution environment, and in
    particular lets it work if stdio is not connected to anything.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@31540 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_posixprocess.py
+++ twisted/internet/test/test_posixprocess.py
@@ -5,7 +5,7 @@
 Tests for POSIX-based L{IReactorProcess} implementations.
 """
 
-import errno, os, sys
+import errno, os
 
 try:
     import fcntl
@@ -45,16 +45,40 @@ class FileDescriptorTests(TestCase):
         """
         L{_listOpenFDs} lists expected file descriptors.
         """
-        openfds = process._listOpenFDs()
-        for f in sys.stdin, sys.stdout, sys.stderr:
-            self.assertIn(f.fileno(), openfds)
+        # This is a tricky test.  A priori, there is no way to know what file
+        # descriptors are open now, so there is no way to know what _listOpenFDs
+        # should return.  Work around this by creating some new file descriptors
+        # which we can know the state of and then just making assertions about
+        # their presence or absence in the result.
 
-        # See http://twistedmatrix.com/trac/ticket/4522#comment:17
+        # Expect a file we just opened to be listed.
         f = file(os.devnull)
+        openfds = process._listOpenFDs()
+        self.assertIn(f.fileno(), openfds)
+
+        # Expect a file we just closed not to be listed - with a caveat.  The
+        # implementation may need to open a file to discover the result.  That
+        # open file descriptor will be allocated the same number as the one we
+        # just closed.  So, instead, create a hole in the file descriptor space
+        # to catch that internal descriptor and make the assertion about a
+        # different closed file descriptor.
+
+        # This gets allocated a file descriptor larger than f's, since nothing
+        # has been closed since we opened f.
         fd = os.dup(f.fileno())
+
+        # But sanity check that; if it fails the test is invalid.
+        self.assertTrue(
+            fd > f.fileno(),
+            "Expected duplicate file descriptor to be greater than original")
+
         try:
+            # Get rid of the original, creating the hole.  The copy should still
+            # be open, of course.
             f.close()
             self.assertIn(fd, process._listOpenFDs())
         finally:
+            # Get rid of the copy now
             os.close(fd)
+        # And it should not appear in the result.
         self.assertNotIn(fd, process._listOpenFDs())
