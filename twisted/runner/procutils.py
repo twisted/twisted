709e10cabf8653c3a2290b2d4e1e7e65377bb352
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-04-18 15:26:33 +0000

    Merge procutils-runner-5049: Removed the twisted.runner.procutils module that was deprecated in Twisted 2.3.
    Author: thijs
    Reviewer: jesstess
    Fixes: #5049
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@31695 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/runner/procutils.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn("twisted.runner.procutils is DEPRECATED. import twisted.python.procutils instead.",
-              DeprecationWarning, stacklevel=2)
-
-from twisted.python.procutils import which
