45cfe12fb165f2c190d206bbdd06707cd6777173
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-06-05 22:00:29 +0000

    Apply aqua-quit-3776.patch: document a workaround for the Aqua Tcl/Tk Quit menu on Mac OS X.
    Author: thijs
    Reviewer: jesstess
    Fixes: #3776 
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@32048 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/tksupport.py
+++ twisted/internet/tksupport.py
@@ -20,6 +20,13 @@ stop Twisted::
 
     | root.protocol('WM_DELETE_WINDOW', reactor.stop)
 
+When using Aqua Tcl/Tk on Mac OS X the standard Quit menu item in
+your application might become unresponsive without the additional
+fix::
+
+    | root.createcommand("::tk::mac::Quit", reactor.stop)
+
+@see: U{Tcl/TkAqua FAQ for more info<http://wiki.tcl.tk/12987>}
 """
 
 # system imports
