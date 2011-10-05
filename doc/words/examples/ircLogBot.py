9362061e6f79ac93563a8457ffea60e7f9a0b8d1
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-10-05 01:11:53 +0000

    Merge explicit-buildProtocol-5044. Fixes: #5044 Author: itamar Review: exarkun
    The clients howto now uses buildProtocol more explicitly, hopefully making it clearer where protocols come from.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@32757 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/words/examples/ircLogBot.py
+++ doc/words/examples/ircLogBot.py
@@ -126,12 +126,14 @@ class LogBotFactory(protocol.ClientFactory):
     A new protocol instance will be created each time we connect to the server.
     """
 
-    # the class of the protocol to build when new connection is made
-    protocol = LogBot
-
     def __init__(self, channel, filename):
         self.channel = channel
         self.filename = filename
+
+    def buildProtocol(self, addr):
+        p = LogBot()
+        p.factory = self
+        return p
 
     def clientConnectionLost(self, connector, reason):
         """If we get disconnected, reconnect to server."""
