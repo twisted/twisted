d903b3761b208890e9f944cdbcbae72e1731ef12
jesstess <jesstess@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-12-10 23:00:35 +0000

    Show that you can use the reactor/Twisted beyond starting and stopping.
    refs #5422
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/pygtk-example-5422@33334 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/core/examples/twgtk2helloworld.py
+++ doc/core/examples/twgtk2helloworld.py
@@ -8,18 +8,6 @@ import gtk
 from twisted.internet import reactor
 
 class HelloWorld(object):
-    def hello(self, widge, data=None):
-        print "Hello World"
-
-    def destroy(self, widget, data=None):
-        # Stopping the reactor also stops the gtk.main() loop by calling
-        # gtk.main_quit().
-        reactor.stop()
-
-    def delete_event(self, widget, event, data=None):
-        print "delete event occurred"
-        return False
-
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.connect("delete_event", self.delete_event)
@@ -31,6 +19,29 @@ class HelloWorld(object):
         self.window.add(self.button)
         self.button.show()
         self.window.show()
+        self.counter = 10
+        self.countdown()
+
+    def hello(self, widget, data=None):
+        print "Hello World"
+
+    def countdown(self):
+        if self.counter > 0:
+            print "Countdown", self.counter
+            self.counter -= 1
+            reactor.callLater(.5, self.countdown)
+        else:
+            print "Boom!"
+            self.window.destroy()
+
+    def destroy(self, widget, data=None):
+        # Stopping the reactor also stops the gtk.main() loop by calling
+        # gtk.main_quit().
+        reactor.stop()
+
+    def delete_event(self, widget, event, data=None):
+        print "delete event occurred"
+        return False
 
 if __name__ == "__main__":
     HelloWorld()
