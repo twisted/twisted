1f1dd8ac95d23e5e3ab70ccbda8e9308261035e9
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-12-31 18:36:12 +0000

    Merge twiddle-5404
    Author: exarkun
    Reviewer: thijs
    Fixes: #5404
    
    Correct the `Element` example to use the correct tag API (rather than the old
    API provided by Nevow).
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@33406 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/_element.py
+++ twisted/web/_element.py
@@ -9,6 +9,7 @@ from twisted.web.iweb import IRenderable
 from twisted.web.error import MissingRenderMethod, UnexposedMethodError
 from twisted.web.error import MissingTemplateLoader
 
+
 class Expose(object):
     """
     Helper for exposing methods for various uses using a simple decorator-style
@@ -106,7 +107,7 @@ def renderer():
         class Foo(Element):
             @renderer
             def twiddle(self, request, tag):
-                return tag['Hello, world.']
+                return tag('Hello, world.')
 
         <div xmlns:t="http://twistedmatrix.com/ns/twisted.web.template/0.1">
             <span t:render="twiddle" />
