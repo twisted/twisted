8f759ecbcfc603fd1d4de9cec97d6c278ba1fe31
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2012-02-10 14:56:39 +0000

    Change version
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-12.0.0-5457@33531 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/_version.py
+++ twisted/lore/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.lore', 12, 0, 0, prerelease=1)
+version = versions.Version('twisted.lore', 12, 0, 0)
