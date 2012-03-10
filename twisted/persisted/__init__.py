4a9c932af44e10a5098ea52e715910db31752713
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2012-03-10 15:22:17 +0000

    Merge remove-twisted-persisted-journal-4805-2: twisted.persisted.journal is removed, deprecated since Twisted 11.0.
    Author: thijs
    Reviewer: jesstess
    Fixes: #4805
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@33674 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/persisted/__init__.py
+++ twisted/persisted/__init__.py
@@ -4,12 +4,3 @@
 """
 Twisted Persisted: utilities for managing persistence.
 """
-
-
-from twisted.python import versions, deprecate
-
-deprecate.deprecatedModuleAttribute(versions.Version('twisted', 11, 0, 0),
-                                    "Use a different persistence library. This one "
-                                    "is no longer maintained.",
-                                    __name__, 'journal')
-
