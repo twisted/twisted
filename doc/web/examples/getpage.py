a5d1b7b472688a44e5d3ddde2599ff0c90ff99a5
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2012-04-07 14:57:28 +0000

    Apply firstpatch.patch: The google and getpage twisted.web examples now have improved documentation that better explains how to run the script.
    Author: rahoberg
    Reviewer: thijs
    Fixes: #5609
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@34123 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/web/examples/getpage.py
+++ doc/web/examples/getpage.py
@@ -1,3 +1,11 @@
+# Copyright (c) Twisted Matrix Laboratories
+# See LICENSE for details.
+
+# Run this example with:
+#    python getpage.py <URL>
+
+# This program will retrieve and print the resource at the given URL.
+
 from twisted.internet import reactor
 from twisted.web.client import getPage
 from twisted.python.util import println
--- doc/web/examples/google.py
+++ doc/web/examples/google.py
@@ -1,3 +1,12 @@
+# Copyright (c) Twisted Matrix Laboratories
+# See LICENSE for details.
+
+# Run this example with:
+#   python google.py <keyword(s)>.
+
+# This program will print out the URL corresponding 
+# to the first webpage given by a Google search.
+
 from twisted.web.google import checkGoogle
 from twisted.python.util import println
 from twisted.internet import reactor 
