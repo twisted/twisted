9941914803014a1de40bdb01b489ac2ad6f30f4e
z3p <z3p@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2012-07-08 18:00:52 +0000

    Merge getCipher-pycrypto26-5700: fix test failure with PyCrypto 2.6
    Author: marienz
    Reviewer: therve
    Fixes: #5700
    
    PyCrypto 2.6 changed the str() of some of its classes.  This caused a failure
    in a test which was checking the str() to verify that we received the correct
    encryption class.  This branch changes the test to check the __name__ of the
    received class.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@34697 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/test/test_transport.py
+++ twisted/conch/test/test_transport.py
@@ -37,7 +37,7 @@ from twisted.trial import unittest
 from twisted.internet import defer
 from twisted.protocols import loopback
 from twisted.python import randbytes
-from twisted.python.reflect import qual
+from twisted.python.reflect import qual, getClass
 from twisted.python.hashlib import md5, sha1
 from twisted.conch.ssh import address, service, common
 from twisted.test import proto_helpers
@@ -1858,7 +1858,7 @@ class SSHCiphersTestCase(unittest.TestCase):
             if cipName == 'none':
                 self.assertIsInstance(cip, transport._DummyCipher)
             else:
-                self.assertTrue(str(cip).startswith('<' + modName))
+                self.assertTrue(getClass(cip).__name__.startswith(modName))
 
 
     def test_getMAC(self):
