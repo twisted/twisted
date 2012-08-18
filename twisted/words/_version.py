c987b1fb425175d6b74b37c8f1c832c424c520c9
ashfall <ashfall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2012-08-18 12:05:22 +0000

    change-versions
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-12.2.0-5879@35280 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/_version.py
+++ twisted/words/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.words', 12, 1, 0)
+version = versions.Version('twisted.words', 12, 2, 0, prerelease=1)
