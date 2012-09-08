86e6a20bb01eca577f11129ef0dbd9c984cb14ef
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2012-09-08 21:43:43 +0000

    Apply testdns-output-5594.patch.
    Author: rwall
    Reviewer: thijs
    Fixes: #5594
    
    Pretty print the output of the testdns example.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@35558 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/names/examples/testdns.py
+++ doc/names/examples/testdns.py
@@ -4,45 +4,65 @@
 # See LICENSE for details.
 
 """
-Prints the results of an Address record lookup, Mail-Exchanger record lookup,
-and Nameserver record lookup for the given hostname for a given hostname.
+Prints the results of an Address record lookup, Mail-Exchanger record
+lookup, and Nameserver record lookup for the given hostname for a
+given hostname.
 
 To run this script:
 $ python testdns.py <hostname>
 e.g.:
 $ python testdns.py www.google.com
 """
-
 import sys
+
 from twisted.names import client
-from twisted.internet import reactor
-from twisted.names import dns
+from twisted.internet import defer, reactor
+from twisted.names import dns, error
+
 
 r = client.Resolver('/etc/resolv.conf')
 
-def gotAddress(a):
-    print 'Addresses: ', ', '.join(map(str, a))
 
-def gotMails(a):
-    print 'Mail Exchangers: ', ', '.join(map(str, a))
+def formatResult(a, heading):
+    answer, authority, additional = a
+    lines = ['# ' + heading]
+    for a in answer:
+        line = [
+            a.name,
+            dns.QUERY_CLASSES.get(a.cls, 'UNKNOWN (%d)' % (a.cls,)),
+            a.payload]
+        lines.append(' '.join(str(word) for word in line))
 
-def gotNameservers(a):
-    print 'Nameservers: ', ', '.join(map(str, a))
+    return '\n'.join(line for line in lines)
 
-def gotError(f):
-    print 'gotError'
-    f.printTraceback()
 
-    from twisted.internet import reactor
-    reactor.stop()
+def printError(f):
+    f.trap(defer.FirstError)
+    f = f.value.subFailure
+    f.trap(error.DomainError)
+    print f.value.__class__.__name__, f.value.message.queries
+
+
+def printResults(res):
+    for r in res:
+        print r
+        print
 
 
 if __name__ == '__main__':
-    import sys
+    domainname = sys.argv[1]
 
-    r.lookupAddress(sys.argv[1]).addCallback(gotAddress).addErrback(gotError)
-    r.lookupMailExchange(sys.argv[1]).addCallback(gotMails).addErrback(gotError)
-    r.lookupNameservers(sys.argv[1]).addCallback(gotNameservers).addErrback(gotError)
+    d = defer.gatherResults([
+            r.lookupAddress(domainname).addCallback(
+                formatResult, 'Addresses'),
+            r.lookupMailExchange(domainname).addCallback(
+                formatResult, 'Mail Exchangers'),
+            r.lookupNameservers(domainname).addCallback(
+                formatResult, 'Nameservers'),
+            ], consumeErrors=True)
 
-    reactor.callLater(4, reactor.stop)
+    d.addCallbacks(printResults, printError)
+
+    d.addBoth(lambda ign: reactor.stop())
+
     reactor.run()
