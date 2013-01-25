f1a91e94a20f911c2cb52ca834ea032131152803
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-01-25 10:06:56 +0000

    check against parserErrors
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/python-2.3-5386-2@36920 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/lore/lint.py
+++ twisted/lore/lint.py
@@ -130,11 +130,11 @@ class DefaultTagChecker(TagChecker):
                     text = '\n'.join(lines) + '\n'
                     try:
                         parser.suite(text)
-                    except SyntaxError:
+                    except parserErrors:
                         # Pretend the "..." idiom is syntactically valid
                         text = text.replace("...","'...'")
                         parser.suite(text)
-                except SyntaxError as e:
+                except parserErrors as e:
                     self._reportError(filename, node,
                                       'invalid python code:' + str(e))
 
