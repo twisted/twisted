190cef17644edcafaaa8b338a6122a06e8efa5e9
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-02-02 22:47:33 +0000

    Add a couple unit tests for the bug
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/constants-foreign-attribute-5797@37051 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/test/test_constants.py
+++ twisted/python/test/test_constants.py
@@ -242,6 +242,29 @@ class NamesTests(TestCase, _ConstantsTestsMixin):
         self.assertIdentical(first, second)
 
 
+    def test_asForeignClassAttribute(self):
+        """
+        A constant defined on a L{Names} subclass may be set as an attribute of
+        another class and then retrieved using that attribute.
+        """
+        class Another(object):
+            something = self.METHOD.GET
+
+        self.assertIdentical(self.METHOD.GET, Another.something)
+
+
+    def test_asForeignClassAttributeViaInstance(self):
+        """
+        A constant defined on a L{Names} subclass may be set as an attribute of
+        another class and then retrieved from an instance of that class using
+        that attribute.
+        """
+        class Another(object):
+            something = self.METHOD.GET
+
+        self.assertIdentical(self.METHOD.GET, Another().something)
+
+
 
 class ValuesTests(TestCase, _ConstantsTestsMixin):
     """
