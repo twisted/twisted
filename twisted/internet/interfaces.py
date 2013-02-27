9cb1c82b9131380ab2ad1389a201dca7bf7ec320
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-02-27 22:10:18 +0000

    complete getHostByName docstring and more consistent with the lookup methods
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/moduleprovides-iresolver-6328@37294 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/interfaces.py
+++ twisted/internet/interfaces.py
@@ -61,18 +61,25 @@ class IResolverSimple(Interface):
         Resolve the domain name C{name} into an IP address.
 
         @type name: C{str}
-        @type timeout: C{tuple}
+        @param name: DNS name to resolve.
+
+        @type timeout: Sequence of C{int}
+        @param timeout: Number of seconds after which to reissue the query.
+            When the last timeout expires, the query is considered failed.
+
         @rtype: L{twisted.internet.defer.Deferred}
         @return: The callback of the Deferred that is returned will be
-        passed a string that represents the IP address of the specified
-        name, or the errback will be called if the lookup times out.  If
-        multiple types of address records are associated with the name,
-        A6 records will be returned in preference to AAAA records, which
-        will be returned in preference to A records.  If there are multiple
-        records of the type to be returned, one will be selected at random.
+            passed a string that represents the IP address of the
+            specified name, or the errback will be called if the
+            lookup times out.  If multiple types of address records
+            are associated with the name, A6 records will be returned
+            in preference to AAAA records, which will be returned in
+            preference to A records.  If there are multiple records of
+            the type to be returned, one will be selected at random.
 
-        @raise twisted.internet.defer.TimeoutError: Raised (asynchronously)
-        if the name cannot be resolved within the specified timeout period.
+        @raise L{twisted.internet.defer.TimeoutError}: Raised
+            (asynchronously) if the name cannot be resolved within the
+            specified timeout period.
         """
 
 
