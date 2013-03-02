35c77f8ffca38741e04379ee1b53c4147746094e
tomprince <tomprince@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-03-02 02:16:31 +0000

    Merge TimerService-wait-for-finish-6290: Make TimerService.stopService wait for a running call to finish.
    Author: tom.prince
    Reviewers: rwall
    Fixes: #6290
    
    There was no public way to wait for the running call
    to finish which meant that reactor wouldn't wait for
    it to complete, and that test couldn't clean up.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@37359 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/application/test/test_internet.py
+++ twisted/application/test/test_internet.py
@@ -11,9 +11,13 @@ from zope.interface.verify import verifyClass
 
 from twisted.internet.protocol import Factory
 from twisted.trial.unittest import TestCase
-from twisted.application.internet import StreamServerEndpointService
+from twisted.application import internet
+from twisted.application.internet import (
+        StreamServerEndpointService, TimerService)
 from twisted.internet.interfaces import IStreamServerEndpoint, IListeningPort
 from twisted.internet.defer import Deferred, CancelledError
+from twisted.internet import task
+from twisted.python.failure import Failure
 
 class FakeServer(object):
     """
@@ -250,3 +254,106 @@ class TestEndpointService(TestCase):
         self.assertEqual(len(stoppingErrors), 1)
 
 
+
+class TestTimerService(TestCase):
+    """
+    Tests for L{twisted.application.internet.TimerService}.
+
+    @type timer: L{TimerService}
+    @ivar timer: service to test
+
+    @type clock: L{task.Clock}
+    @ivar clock: source of time
+
+    @type deferred: L{Deferred}
+    @ivar deferred: deferred returned by L{TestTimerService.call}.
+    """
+
+    def setUp(self):
+        self.timer = TimerService(2, self.call)
+        self.clock = self.timer.clock = task.Clock()
+        self.deferred = Deferred()
+
+
+    def call(self):
+        """
+        Function called by L{TimerService} being tested.
+
+        @returns: C{self.deferred}
+        @rtype: L{Deferred}
+        """
+        return self.deferred
+
+
+    def test_startService(self):
+        """
+        When L{TimerService.startService} is called, it marks itself
+        as running, creates a L{task.LoopingCall} and starts it.
+        """
+        self.timer.startService()
+        self.assertTrue(self.timer.running, "Service is started")
+        self.assertIsInstance(self.timer._loop, task.LoopingCall)
+        self.assertIdentical(self.clock, self.timer._loop.clock)
+        self.assertTrue(self.timer._loop.running, "LoopingCall is started")
+
+
+    def test_startServiceRunsCallImmediately(self):
+        """
+        When L{TimerService.startService} is called, it calls the function
+        immediately.
+        """
+        result = []
+        self.timer.call = (result.append, (None,), {})
+        self.timer.startService()
+        self.assertEqual([None], result)
+
+
+    def test_startServiceUsesGlobalReactor(self):
+        """
+        L{TimerService.startService} uses L{internet._maybeGlobalReactor} to
+        choose the reactor to pass to L{task.LoopingCall}
+        uses the global reactor.
+        """
+        otherClock = task.Clock()
+        def getOtherClock(maybeReactor):
+            return otherClock
+        self.patch(internet, "_maybeGlobalReactor", getOtherClock)
+        self.timer.startService()
+        self.assertIdentical(otherClock, self.timer._loop.clock)
+
+
+    def test_stopServiceWaits(self):
+        """
+        When L{TimerService.stopService} is called while a call is in progress.
+        the L{Deferred} returned doesn't fire until after the call finishes.
+        """
+        self.timer.startService()
+        d = self.timer.stopService()
+        self.assertNoResult(d)
+        self.assertEqual(True, self.timer.running)
+        self.deferred.callback(object())
+        self.assertIdentical(self.successResultOf(d), None)
+
+
+    def test_stopServiceImmediately(self):
+        """
+        When L{TimerService.stopService} is called while a call isn't in progress.
+        the L{Deferred} returned has already been fired.
+        """
+        self.timer.startService()
+        self.deferred.callback(object())
+        d = self.timer.stopService()
+        self.assertIdentical(self.successResultOf(d), None)
+
+
+    def test_failedCallLogsError(self):
+        """
+        When function passed to L{TimerService} returns a deferred that errbacks,
+        the exception is logged, and L{TimerService.stopService} doesn't raise an error.
+        """
+        self.timer.startService()
+        self.deferred.errback(Failure(ZeroDivisionError()))
+        errors = self.flushLoggedErrors(ZeroDivisionError)
+        self.assertEqual(1, len(errors))
+        d = self.timer.stopService()
+        self.assertIdentical(self.successResultOf(d), None)
