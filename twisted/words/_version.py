d5af30ed63d9d8974f34696b4fe9a6ab8fb76e56
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-03-06 09:42:31 +0000

    Bump version
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-13.0.0-6353@37419 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/_version.py
+++ twisted/words/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.words', 12, 3, 0)
+version = versions.Version('twisted.words', 13, 0, 0, prerelease=1)
