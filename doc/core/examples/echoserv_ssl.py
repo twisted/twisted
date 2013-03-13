9746297cbc8cea72395842708b353824e3566215
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-03-13 18:17:06 +0000

    Cleanup ssl server example and make it use CertificateOptions instead of a custom context factory
      * Give it a #! so it is potentially runnable
      * Avoid running so much code from `__main__`
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/ssl-contextfactory-docs-6273-2@37495 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/core/examples/echoserv_ssl.py
+++ doc/core/examples/echoserv_ssl.py
@@ -1,30 +1,28 @@
-
+#!/usr/bin/env python
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
+if __name__ == '__main__':
+    import echoserv_ssl
+    raise SystemExit(echoserv_ssl.main())
+
+import sys
 
 from OpenSSL import SSL
 
-class ServerContextFactory:
-    
-    def getContext(self):
-        """Create an SSL context.
-        
-        This is a sample implementation that loads a certificate from a file 
-        called 'server.pem'."""
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        ctx.use_certificate_file('server.pem')
-        ctx.use_privatekey_file('server.pem')
-        return ctx
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory
+from twisted.internet import ssl, reactor
+from twisted.python import log
 
+import echoserv
 
-if __name__ == '__main__':
-    import echoserv, sys
-    from twisted.internet.protocol import Factory
-    from twisted.internet import ssl, reactor
-    from twisted.python import log
+def main():
+    with open('server.pem') as keyAndCert:
+        cert = ssl.PrivateCertificate.loadPEM(keyAndCert.read())
+
     log.startLogging(sys.stdout)
     factory = Factory()
     factory.protocol = echoserv.Echo
-    reactor.listenSSL(8000, factory, ServerContextFactory())
+    reactor.listenSSL(8000, factory, cert.options())
     reactor.run()
