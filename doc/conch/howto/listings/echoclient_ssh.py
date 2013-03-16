c159a023d74619445f5b8687b2a605d1d5576449
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-03-16 18:18:01 +0000

    Refactor the example a bit to make some of the code more easily reusable
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/sshcommandendpoint-4698@37518 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/conch/howto/listings/echoclient_ssh.py
+++ doc/conch/howto/listings/echoclient_ssh.py
@@ -58,6 +58,7 @@ class NoiseProtocol(Protocol):
         self.finished.callback(None)
 
 
+
 def readKey(path):
     try:
         return Key.fromFile(path)
@@ -67,36 +68,63 @@ def readKey(path):
 
 
 
+class ConnectionParameters(object):
+    def __init__(self, reactor, ui, host, port, username, password, keys, knownHosts, agent):
+        self.reactor = reactor
+        self.ui = ui
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.keys = keys
+        self.knownHosts = knownHosts
+        self.agent = agent
+
+
+    @classmethod
+    def fromCommandLine(cls, reactor, argv):
+        config = EchoOptions()
+        config.parseOptions(argv)
+
+        ui = ConsoleUI(lambda: open("/dev/tty", "r+"))
+
+        keyPath = os.path.expanduser(config["identity"])
+        if os.path.exists(keyPath):
+            keys = [readKey(keyPath)]
+        else:
+            keys = []
+        knownHostsPath = FilePath(os.path.expanduser(config["knownhosts"]))
+        if knownHostsPath.exists():
+            knownHosts = KnownHostsFile.fromPath(knownHostsPath)
+        else:
+            knownHosts = None
+
+        if config["no-agent"] or "SSH_AUTH_SOCK" not in os.environ:
+            agentEndpoint = None
+        else:
+            agentEndpoint = UNIXClientEndpoint(reactor, os.environ["SSH_AUTH_SOCK"])
+
+        return cls(
+            reactor, ui, config["host"], config["port"],
+            config["username"], config["password"], keys,
+            knownHosts, agentEndpoint)
+
+
+    def endpointForCommand(self, command):
+        return SSHCommandEndpoint(
+            self.reactor,
+            self.host, self.port,
+            command, username=self.username,
+            keys=self.keys, password=self.password,
+            agentEndpoint=self.agent,
+            knownHosts=self.knownHosts,
+            ui=self.ui)
+
+
+
 def main(reactor, *argv):
-    config = EchoOptions()
-    config.parseOptions(argv)
-
-    ui = ConsoleUI(lambda: open("/dev/tty", "r+"))
-
-    keyPath = os.path.expanduser(config["identity"])
-    if os.path.exists(keyPath):
-        keys = [readKey(keyPath)]
-    else:
-        keys = []
-    knownHostsPath = FilePath(os.path.expanduser(config["knownhosts"]))
-    if knownHostsPath.exists():
-        knownHosts = KnownHostsFile.fromPath(knownHostsPath)
-    else:
-        knownHosts = None
-
-    if config["no-agent"] or "SSH_AUTH_SOCK" not in os.environ:
-        agentEndpoint = None
-    else:
-        agentEndpoint = UNIXClientEndpoint(reactor, os.environ["SSH_AUTH_SOCK"])
-
-    endpoint = SSHCommandEndpoint(
-        reactor,
-        config["host"], config["port"],
-        command="/bin/cat", username=config["username"],
-        keys=keys, password=config["password"],
-        agentEndpoint=agentEndpoint,
-        knownHosts=knownHosts,
-        ui=ui)
+    parameters = ConnectionParameters.fromCommandLine(reactor, argv)
+    endpoint = parameters.endpointForCommand(b"/bin/cat")
 
     factory = Factory()
     factory.protocol = NoiseProtocol
