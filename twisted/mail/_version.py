b5f587eca4887692942277e98127cf051343a51d
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-03-19 23:23:09 +0000

    Bump version
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-13.0.0-6353@37709 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/mail/_version.py
+++ twisted/mail/_version.py
@@ -1,3 +1,3 @@
 # This is an auto-generated file. Do not edit it.
 from twisted.python import versions
-version = versions.Version('twisted.mail', 13, 0, 0, prerelease=2)
+version = versions.Version('twisted.mail', 13, 0, 0)
