1efcbb9199eee84c4452e98fab98ddeecad695fc
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-03-28 15:12:02 +0000

    Add epydoc for all parameters
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/factory-for-5016@37863 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/protocol.py
+++ twisted/internet/protocol.py
@@ -41,7 +41,13 @@ class Factory:
         It sets the C{protocol} attribute and returns the constructed factory
         instance.
 
-        @param protocol: a L{Protocol} subclass
+        @param protocol: A L{Protocol} subclass
+
+        @param args: Positional arguments for the factory.
+
+        @param kwargs: Keyword arguments for the factory.
+
+        @return: A L{Factory} instance wired up to C{protocol}.
         """
         factory = cls(*args, **kwargs)
         factory.protocol = protocol
