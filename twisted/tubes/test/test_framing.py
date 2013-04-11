52afcedea3182ee4e7c9f1150707248b4c85e451
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-04-11 23:43:13 +0000

    Multi-shot test.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tubes-1956-3@38020 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/tubes/test/test_framing.py
+++ twisted/tubes/test/test_framing.py
@@ -28,6 +28,22 @@ class NetstringTests(TestCase):
         )])
 
 
+    def test_stringsToNetstrings(self):
+        """
+        L{stringsToNetstrings} works on subsequent inputs as well.
+        """
+        ff = FakeFount()
+        fd = FakeDrain()
+        ff.flowTo(Tube(stringsToNetstrings())).flowTo(fd)
+        ff.drain.receive("hello")
+        ff.drain.receive("world")
+        self.assertEquals(b"".join(fd.received), 
+            "{len:d}:{data:s},{len2:d}:{data2:s},".format(
+            len=len("hello"), data="hello",
+            len2=len("world"), data2="world",
+        ))
+
+
     def test_netstringToString(self):
         """
         Length prefix is stripped off.
