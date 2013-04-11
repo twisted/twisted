f1ee9b89373bcf36a6967c273f12a0c3d38afd19
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-04-11 23:42:04 +0000

    Failing test.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tubes-1956-3@38011 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/tubes/test/test_framing.py
+++ twisted/tubes/test/test_framing.py
@@ -7,6 +7,7 @@ from twisted.tubes.framing import stringsToNetstrings
 from twisted.tubes.test.util import FakeFount
 from twisted.tubes.test.util import FakeDrain
 from twisted.tubes.tube import Tube
+from twisted.tubes.framing import netstringsToStrings
 from twisted.trial.unittest import TestCase
 
 class NetstringTests(TestCase):
@@ -23,5 +24,16 @@ class NetstringTests(TestCase):
         ff.flowTo(Tube(stringsToNetstrings())).flowTo(fd)
         ff.drain.receive("hello")
         self.assertEquals(fd.received, ["{len:d}:{data:s},".format(
-                    len=len("hello"), data="hello"
-                )])
+            len=len("hello"), data="hello"
+        )])
+
+
+    def test_netstringToString(self):
+        """
+        Length prefix is stripped off.
+        """
+        ff = FakeFount()
+        fd = FakeDrain()
+        ff.flowTo(Tube(netstringsToStrings())).flowTo(fd)
+        ff.drain.receive("1:x,2:yz,3:")
+        self.assertEquals(fd.received, ["x", "yz"])
