c8f15f9d5cac0097b1e7014c16c62becb5399233
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-05-03 09:30:20 +0000

    tests for adoptDatagramPort errors
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/udp-port-from-fd-5574@38331 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_socket.py
+++ twisted/internet/test/test_socket.py
@@ -15,7 +15,7 @@ import errno, socket
 from twisted.python.log import err
 from twisted.internet.interfaces import IReactorSocket
 from twisted.internet.error import UnsupportedAddressFamily
-from twisted.internet.protocol import ServerFactory
+from twisted.internet.protocol import DatagramProtocol, ServerFactory
 from twisted.internet.test.reactormixins import (
     ReactorBuilder, needsRunningReactor)
 
@@ -135,5 +135,87 @@ class AdoptStreamConnectionErrorsTestsBuilder(ReactorBuilder):
 
 
 
+class AdoptDatagramPortErrorsTestsBuilder(ReactorBuilder):
+    """
+    Builder for testing L{IReactorSocket.adoptDatagramPort} implementations.
+
+    Generally only tests for failure cases are found here.  Success cases for
+    this interface are tested elsewhere.  For example, the success case for
+    I{AF_INET} is in L{twisted.internet.test.test_tcp}, since that case should
+    behave exactly the same as L{IReactorTCP.listenTCP}.
+    """
+    requiredInterfaces = [IReactorSocket]
+
+    def test_invalidDescriptor(self):
+        """
+        An implementation of L{IReactorSocket.adoptDatagramPort} raises
+        L{socket.error} if passed an integer which is not associated with a
+        socket.
+        """
+        reactor = self.buildReactor()
+
+        probe = socket.socket()
+        fileno = probe.fileno()
+        probe.close()
+
+        exc = self.assertRaises(
+            socket.error,
+            reactor.adoptDatagramPort, fileno, socket.AF_INET, DatagramProtocol())
+        self.assertEqual(exc.args[0], errno.EBADF)
+
+
+    def test_invalidAddressFamily(self):
+        """
+        An implementation of L{IReactorSocket.adoptDatagramPort} raises
+        L{UnsupportedAddressFamily} if passed an address family it does not
+        support.
+        """
+        reactor = self.buildReactor()
+
+        port = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.addCleanup(port.close)
+
+        arbitrary = 2 ** 16 + 7
+
+        self.assertRaises(
+            UnsupportedAddressFamily,
+            reactor.adoptDatagramPort, port.fileno(), arbitrary, DatagramProtocol())
+
+
+    def test_stopOnlyCloses(self):
+        """
+        When the L{IListeningPort} returned by L{IReactorSocket.adoptDatagramPort}
+        is stopped using C{stopListening}, the underlying socket is closed but
+        not shutdown.  This allows another process which still has a reference
+        to it to continue reading and writing to it.
+        """
+        reactor = self.buildReactor()
+
+        portSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.addCleanup(portSocket.close)
+
+        portSocket.setblocking(False)
+
+        # The file descriptor is duplicated by adoptDatagramPort
+        port = reactor.adoptDatagramPort(
+            portSocket.fileno(), portSocket.family, DatagramProtocol())
+        d = port.stopListening()
+        def stopped(ignored):
+            # Should still be possible to recv on portSocket.  If
+            # it was shutdown, the exception would be EINVAL instead.
+            exc = self.assertRaises(socket.error, portSocket.recvfrom, 1)
+            self.assertEqual(exc.args[0], errno.EAGAIN)
+        d.addCallback(stopped)
+        d.addErrback(err, "Failed to read on original port.")
+
+        needsRunningReactor(
+            reactor,
+            lambda: d.addCallback(lambda ignored: reactor.stop()))
+
+        reactor.run()
+
+
+
 globals().update(AdoptStreamPortErrorsTestsBuilder.makeTestCaseClasses())
 globals().update(AdoptStreamConnectionErrorsTestsBuilder.makeTestCaseClasses())
+globals().update(AdoptDatagramPortErrorsTestsBuilder.makeTestCaseClasses())
