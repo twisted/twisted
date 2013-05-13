fcaf805d09d2f86dcbfe9ba6cbc979324915c451
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-05-13 02:27:20 +0000

    apply bug_6480.patch, refs #6480
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/version-header-6480@38403 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/_release.py
+++ twisted/python/_release.py
@@ -258,7 +258,16 @@ def generateVersionFileData(version):
     else:
         prerelease = ""
     data = '''\
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+
 # This is an auto-generated file. Do not edit it.
+
+"""
+Gives version of Twisted: major.minor.micro
+"""
+
 from twisted.python import versions
 version = versions.Version(%r, %s, %s, %s%s)
 ''' % (version.package, version.major, version.minor, version.micro,
