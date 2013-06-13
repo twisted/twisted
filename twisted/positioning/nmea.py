9a9b33a34319d390577f70c428551381b16a20ee
lvh <lvh@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-06-13 14:15:37 +0000

    Document and make private _STATEFUL_UPDATE
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/positioning-3926-2@38786 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/positioning/nmea.py
+++ twisted/positioning/nmea.py
@@ -411,6 +411,10 @@ class NMEAAdapter(object):
     """
     An adapter from NMEAProtocol receivers to positioning receivers.
 
+    @cvar _STATEFUL_UPDATE: Information on how to update partial information
+        in the sentence data or internal adapter state. For more information,
+        see C{_statefulUpdate}'s docstring.
+    @type _STATEFUL_UPDATE: See C{_statefulUpdate}'s docstring
     @cvar _ACCEPTABLE_UNITS: A set of NMEA notations of units that are
         already acceptable (metric), and therefore don't need to be converted.
     @type _ACCEPTABLE_UNITS: C{frozenset} of bytestrings
@@ -582,8 +586,7 @@ class NMEAAdapter(object):
         self._sentenceData[destinationKey] = converter(currentValue)
 
 
-
-    STATEFUL_UPDATE = {
+    _STATEFUL_UPDATE = {
         # sentenceKey: (stateKey, factory, attributeName, converter),
         'trueHeading': ('heading', base.Heading, '_angle', float),
         'magneticVariation':
@@ -603,18 +606,32 @@ class NMEAAdapter(object):
     def _statefulUpdate(self, sentenceKey):
         """
         Does a stateful update of a particular positioning attribute.
+        Specifically, this will mutate an object in the current sentence data.
+
+        Using the C{sentenceKey}, this will get a tuple containing, in order,
+        the key name in the current state and sentence data, a factory for
+        new values, the attribute to update, and a converter from sentence
+        data (in NMEA notation) to something useful.
+
+        If the sentence data doesn't have this data yet, it is grabbed from
+        the state. If that doesn't have anything useful yet either, the
+        factory is called to produce a new, empty object. Either way, the
+        object ends up in the sentence data.
 
         @param sentenceKey: The name of the key in the sentence attributes,
-            C{NMEAAdapter.STATEFUL_UPDATE} dictionary and the adapter state.
+            C{NMEAAdapter._STATEFUL_UPDATE} dictionary and the adapter state.
         @type sentenceKey: C{str}
         """
-        state, factory, attr, converter = self.STATEFUL_UPDATE[sentenceKey]
+        key, factory, attr, converter = self._STATEFUL_UPDATE[sentenceKey]
 
-        if state not in self._sentenceData:
-            self._sentenceData[state] = self._state.get(state, factory())
+        if key not in self._sentenceData:
+            try:
+                self._sentenceData[key] = self._state[key]
+            except KeyError: # state does not have this partial data yet
+                self._sentenceData[key] = factory()
 
         newValue = converter(getattr(self.currentSentence, sentenceKey))
-        setattr(self._sentenceData[state], attr, newValue)
+        setattr(self._sentenceData[key], attr, newValue)
 
 
     _ACCEPTABLE_UNITS = frozenset(['M'])
