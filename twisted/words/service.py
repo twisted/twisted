8f50ea81e9ec670e316473a385a08586f696eae6
tomprince <tomprince@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-06-21 06:28:25 +0000

    Merge unbound-nick-5780: Fix handling of invalid UTF-8 in IRC NICK commands.
    Author: j2a, stephsolis
    Reviewers: exarkun, glyph, tom.prince
    Fixes: #5780
    
    Previously, when sent a NICK command that had invalid UTF-8,
    it would fail with an UnboundLocalError. It now properly
    reports an error to the user, instead.
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@38872 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/service.py
+++ twisted/words/service.py
@@ -289,13 +289,14 @@ class IRCUser(irc.IRC):
 
         [REQUIRED]
         """
+        nickname = params[0]
         try:
-            nickname = params[0].decode(self.encoding)
+            nickname = nickname.decode(self.encoding)
         except UnicodeDecodeError:
             self.privmsg(
                 NICKSERV,
                 nickname,
-                'Your nickname is cannot be decoded.  Please use ASCII or UTF-8.')
+                'Your nickname cannot be decoded. Please use ASCII or UTF-8.')
             self.transport.loseConnection()
             return
 
--- twisted/words/test/test_irc_service.py
+++ twisted/words/test/test_irc_service.py
@@ -42,6 +42,35 @@ class IRCUserTestCase(unittest.TestCase):
                           self.stringTransport.value())
 
 
+    def test_utf8Messages(self):
+        """
+        When a UTF8 message is sent with sendMessage and the current IRCUser
+        has a UTF8 nick and is set to UTF8 encoding, the message will be
+        written to the transport.
+        """
+        expectedResult = (u":example.com \u0442\u0435\u0441\u0442 "
+                          u"\u043d\u0438\u043a\r\n").encode('utf-8')
+
+        self.ircUser.irc_NICK("", [u"\u043d\u0438\u043a".encode('utf-8')])
+        self.stringTransport.clear()
+        self.ircUser.sendMessage(u"\u0442\u0435\u0441\u0442".encode('utf-8'))
+        self.assertEqual(self.stringTransport.value(), expectedResult)
+
+
+    def test_invalidEncodingNick(self):
+        """
+        A NICK command sent with a nickname that cannot be decoded with the
+        current IRCUser's encoding results in a PRIVMSG from NickServ
+        indicating that the nickname could not be decoded.
+        """
+        expectedResult = (b":NickServ!NickServ@services PRIVMSG "
+                          b"\xd4\xc5\xd3\xd4 :Your nickname cannot be "
+                          b"decoded. Please use ASCII or UTF-8.\r\n")
+
+        self.ircUser.irc_NICK("", [b"\xd4\xc5\xd3\xd4"])
+        self.assertEqual(self.stringTransport.value(), expectedResult)
+
+
     def response(self):
         """
         Grabs our responses and then clears the transport
