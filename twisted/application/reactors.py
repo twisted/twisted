2903d13be47bea3297e1935087e36f31aad7f102
tomprince <tomprince@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-06-25 18:36:58 +0000

    Implementation.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/install-reactor-6596@38921 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/application/reactors.py
+++ twisted/application/reactors.py
@@ -78,6 +78,8 @@ def installReactor(shortName):
     """
     for installer in getReactorTypes():
         if installer.shortName == shortName:
-            return installer.install()
+            installer.install()
+            from twisted.internet import reactor
+            return reactor
     raise NoSuchReactor(shortName)
 
