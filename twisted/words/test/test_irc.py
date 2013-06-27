cf158387f4daf08a206df333b4c1165c3b5594c6
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-06-27 02:42:44 +0000

    Merge replace-text-words-6544: Replace the usage of twisted.python.text because it will be deprecated.
    Author: thijs
    Reviewer: tom.prince
    Fixes: #6544 
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@38929 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/test/test_irc.py
+++ twisted/words/test/test_irc.py
@@ -2199,9 +2199,157 @@ class ClientTests(TestCase):
 
 
 
+class CollectorClient(irc.IRCClient):
+    """
+    A client that saves in a list the names of the methods that got called.
+    """
+    def __init__(self, methodsList):
+        """
+        @param methodsList: list of methods' names that should be replaced.
+        @type methodsList: C{list}
+        """
+        self.methods = []
+        self.nickname = 'Wolf'
+
+        for method in methodsList:
+            def fake_method(method=method):
+                """
+                Collects C{method}s.
+                """
+                def inner(*args):
+                    self.methods.append((method, args))
+                return inner
+            setattr(self, method, fake_method())
+
+
+
+class DccTests(unittest.TestCase):
+    """
+    Tests for C{dcc_*} methods.
+    """
+    def setUp(self):
+        methods = ['dccDoSend', 'dccDoAcceptResume', 'dccDoResume',
+            'dccDoChat']
+        self.user = 'Wolf!~wolf@yok.utu.fi'
+        self.channel = '#twisted'
+        self.client = CollectorClient(methods)
+
+
+    def test_dccSend(self):
+        """
+        L{irc.IRCClient.dcc_SEND} invokes L{irc.IRCClient.dccDoSend}.
+        """
+        self.client.dcc_SEND(self.user, self.channel, 'foo.txt 127.0.0.1 1025')
+        self.assertEqual(self.client.methods,
+            [('dccDoSend', (self.user, '127.0.0.1', 1025, 'foo.txt', -1,
+            ['foo.txt', '127.0.0.1', '1025']))])
+
+
+    def test_dccSendMalformedRequest(self):
+        """
+        L{irc.IRCClient.dcc_SEND} raises L{irc.IRCBadMessage} when it is passed
+        a malformed query string.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_SEND,
+            self.user, self.channel, 'foo')
+        self.assertEqual(str(result), "malformed DCC SEND request: ['foo']")
+
+
+    def test_dccSendIndecipherableAddress(self):
+        """
+        L{irc.IRCClient.dcc_SEND} raises L{irc.IRCBadMessage} when it is passed
+        a query string that doesn't contain a valid address.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_SEND,
+            self.user, self.channel, 'foo.txt #23 sd@d')
+        self.assertEqual(str(result), "Indecipherable address '#23'")
+
+
+    def test_dccSendIndecipherablePort(self):
+        """
+        L{irc.IRCClient.dcc_SEND} raises L{irc.IRCBadMessage} when it is passed
+        a query string that doesn't contain a valid port number.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_SEND,
+            self.user, self.channel, 'foo.txt 127.0.0.1 sd@d')
+        self.assertEqual(str(result), "Indecipherable port 'sd@d'")
+
+
+    def test_dccAccept(self):
+        """
+        L{irc.IRCClient.dcc_ACCEPT} invokes L{irc.IRCClient.dccDoAcceptResume}.
+        """
+        self.client.dcc_ACCEPT(self.user, self.channel, 'foo.txt 1025 2')
+        self.assertEqual(self.client.methods,
+            [('dccDoAcceptResume', (self.user, 'foo.txt', 1025, 2))])
+
+
+    def test_dccAcceptMalformedRequest(self):
+        """
+        L{irc.IRCClient.dcc_ACCEPT} raises L{irc.IRCBadMessage} when it is
+        passed a malformed query string.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_ACCEPT,
+            self.user, self.channel, 'foo')
+        self.assertEqual(str(result),
+            "malformed DCC SEND ACCEPT request: ['foo']")
+
+
+    def test_dccResume(self):
+        """
+        L{irc.IRCClient.dcc_RESUME} invokes L{irc.IRCClient.dccDoResume}.
+        """
+        self.client.dcc_RESUME(self.user, self.channel, 'foo.txt 1025 2')
+        self.assertEqual(self.client.methods,
+            [('dccDoResume', (self.user, 'foo.txt', 1025, 2))])
+
+
+    def test_dccResumeMalformedRequest(self):
+        """
+        L{irc.IRCClient.dcc_RESUME} raises L{irc.IRCBadMessage} when it is
+        passed a malformed query string.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_RESUME,
+            self.user, self.channel, 'foo')
+        self.assertEqual(str(result),
+            "malformed DCC SEND RESUME request: ['foo']")
+
+
+    def test_dccChat(self):
+        """
+        L{irc.IRCClient.dcc_CHAT} invokes L{irc.IRCClient.dccDoChat}.
+        """
+        self.client.dcc_CHAT(self.user, self.channel, 'foo.txt 127.0.0.1 1025')
+        self.assertEqual(self.client.methods,
+            [('dccDoChat', (self.user, self.channel, '127.0.0.1', 1025,
+            ['foo.txt', '127.0.0.1', '1025']))])
+
+
+    def test_dccChatMalformedRequest(self):
+        """
+        L{irc.IRCClient.dcc_CHAT} raises L{irc.IRCBadMessage} when it is
+        passed a malformed query string.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_CHAT,
+            self.user, self.channel, 'foo')
+        self.assertEqual(str(result),
+            "malformed DCC CHAT request: ['foo']")
+
+
+    def test_dccChatIndecipherablePort(self):
+        """
+        L{irc.IRCClient.dcc_CHAT} raises L{irc.IRCBadMessage} when it is passed
+        a query string that doesn't contain a valid port number.
+        """
+        result = self.assertRaises(irc.IRCBadMessage, self.client.dcc_CHAT,
+            self.user, self.channel, 'foo.txt 127.0.0.1 sd@d')
+        self.assertEqual(str(result), "Indecipherable port 'sd@d'")
+
+
+
 class DccChatFactoryTests(unittest.TestCase):
     """
-    Tests for L{DccChatFactory}
+    Tests for L{DccChatFactory}.
     """
     def test_buildProtocol(self):
         """
