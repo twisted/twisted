aa114f255c27fe3bbeb3e7176f03ff74bb4a535f
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-06-27 08:31:46 +0000

    test for lookupAllRecords
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/optionless-twistd-dns-5992-2@38937 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/test/test_resolve.py
+++ twisted/names/test/test_resolve.py
@@ -18,10 +18,21 @@ class ResolverChainTests(TestCase):
 
     def test_emptyResolversList(self):
         """
-        If L{ResolverChain} is instantiated with an empty C{resolvers}
-        list, a L{ResolverChainConstructionError} is raised.
+        L{ResolverChain._lookup} returns a L{DomainError} failure if
+        its C{resolvers} list is empty.
         """
         r = ResolverChain([])
         d = r.lookupAddress('www.example.com')
+        f = self.failureResultOf(d)
+        self.assertIs(f.trap(DomainError), DomainError)
+
+
+    def test_emptyResolversListLookupAllRecords(self):
+        """
+        L{ResolverChain.lookupAllRecords} returns a L{DomainError}
+        failure if its C{resolvers} list is empty.
+        """
+        r = ResolverChain([])
+        d = r.lookupAllRecords('www.example.com')
         f = self.failureResultOf(d)
         self.assertIs(f.trap(DomainError), DomainError)
