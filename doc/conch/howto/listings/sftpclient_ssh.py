51c135f3cd8714e1b660bca3a8bc1dd99aab1190
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-07-12 15:39:53 +0000

    Simplify example
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/subsystem-endpoint-6617-2@39147 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/conch/howto/listings/sftpclient_ssh.py
+++ doc/conch/howto/listings/sftpclient_ssh.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     from twisted.internet.task import react
     react(sftpclient_ssh.main, sys.argv[1:])
 
-from twisted.internet.defer import Deferred
+
 from twisted.internet.protocol import Protocol
 from twisted.internet.endpoints import connectProtocol
 
@@ -16,18 +16,6 @@ from twisted.conch.endpoints import SSHSubsystemClientEndpoint
 from twisted.conch.ssh.filetransfer import FileTransferClient
 
 from echoclient_ssh import ConnectionParameters
-
-
-class CreateADirectory(FileTransferClient):
-
-    def __init__(self):
-        FileTransferClient.__init__(self)
-        self.finished = Deferred()
-
-    def connectionMade(self):
-        FileTransferClient.connectionMade(self)
-        d = self.makeDirectory("/tmp/foo", {})
-        d.chainDeferred(self.finished)
 
 
 
@@ -39,10 +27,9 @@ def main(reactor, *argv):
 
     def gotConnection(proto):
         conn = proto.transport.conn
-        transferProto = CreateADirectory()
         e = SSHSubsystemClientEndpoint.existingConnection(conn, b"sftp")
-        d2 = connectProtocol(e, transferProto)
-        d2.addCallback(lambda x: transferProto.finished)
+        d2 = connectProtocol(e, FileTransferClient())
+        d2.addCallback(lambda proto: proto.makeDirectory("/tmp/foo", {}))
         return d2
 
     d.addCallback(gotConnection)
