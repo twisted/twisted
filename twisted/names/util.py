6507ec7368f10a3f8c50d2f0bdec4f11f50f0cc0
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-06 15:21:49 +0000

    and the SNA implementation from attachment:ticket:5454:addEdsn0AndDnssec5454V04.patch. Refs: #5454
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/serial-number-arithmetic-6672@39500 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/names/util.py
@@ -0,0 +1,186 @@
+# -*- test-case-name: twisted.names.test.test_util -*-
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Serial Number Arithmetic
+
+This module implements RFC 1982 DNS Serial Number Arithmetic
+(see http://tools.ietf.org/pdf/rfc1982.pdf).
+SNA is used in DNS and specifically in DNSSEC as defined in
+RFC 4034 in the DNSSEC Signature Expiration and Inception Fields.
+
+@author: Bob Novas
+"""
+
+import calendar, time
+
+
+
+class SNA(object):
+    """
+    implements RFC 1982 - DNS Serial Number Arithmetic
+    """
+    SERIAL_BITS = 32
+    MODULOVAL = 2**SERIAL_BITS
+    HLFRNG = 2**(SERIAL_BITS-1)
+    MAXADD = (2**(SERIAL_BITS-1)-1)
+
+
+    def __init__(self, number):
+        self._number = int(number)%self.MODULOVAL
+
+
+    def __repr__(self):
+        return str(self._number)
+
+
+    def asInt(self):
+        """
+        return an integer representing the object
+        """
+        return self._number
+
+
+    def __eq__(self, sna2):
+        """
+        define the equality operator
+        """
+        return sna2._number == self._number
+
+
+    def __lt__(self, sna2):
+        """
+        define the less than operator
+        """
+        return ((self != sna2) and
+               ((self._number < sna2._number) and
+                ((sna2._number - self._number) < self.HLFRNG) or
+               (self._number > sna2._number) and
+                ((self._number - sna2._number) > self.HLFRNG)))
+
+
+    def __gt__(self, sna2):
+        """
+        define the greater than operator
+        """
+        return ((self != sna2) and
+               ((self._number < sna2._number) and
+               ((sna2._number - self._number) > self.HLFRNG) or
+               (self._number > sna2._number) and
+               ((self._number - sna2._number) < self.HLFRNG)))
+
+
+    def __le__(self, sna2):
+        """
+        define the less than or equal operator
+        """
+        return self == sna2 or self < sna2
+
+
+    def __ge__(self, sna2):
+        """
+        define the greater than or equal operator
+        """
+        return self == sna2 or self > sna2
+
+
+    def __add__(self, sna2):
+        """
+        define the addition operator
+        """
+        if sna2 <= SNA(self.MAXADD):
+            return SNA( (self._number + sna2._number)%self.MODULOVAL )
+        else:
+            raise ArithmeticError
+
+
+    def __hash__(self):
+        """
+        define a hash function
+        """
+        return hash(self._number)
+
+
+
+def max(snaList):
+    """
+    takes a list of sna's from which it will pick the one
+    with the highest value
+    """
+    if len(snaList) == 0:
+        return None
+    trialMax = snaList[0]
+    for s in snaList[1:]:
+        if not trialMax:
+            trialMax = s
+        elif s and s > trialMax:
+            trialMax = s
+    return trialMax
+
+
+
+class DateSNA(SNA):
+    """
+    implements DNS Serial Number Arithmetic
+    for dates 'YYYYMMDDHHMMSS' per RFC 4034 P3.1.5
+    """
+    fmt = '%Y%m%d%H%M%S'
+
+
+    def __init__(self, utcDateTime=''):
+        """
+        accept a UTC date/time string as YYMMDDHHMMSS
+        and convert it to seconds since the epoch
+        """
+        if not utcDateTime:
+            utcDateTime = '19700101000000'
+        dtstruct = time.strptime(utcDateTime, DateSNA.fmt)
+        secondsSinceE = calendar.timegm(dtstruct)
+        super(DateSNA, self).__init__(secondsSinceE)
+
+
+    def __add__(self, sna2):
+        """
+        define the addition operator
+        """
+        if not isinstance(sna2, SNA):
+            return NotImplemented
+
+        if (sna2 <= SNA(self.MAXADD) and
+            (self._number + sna2._number < self.MODULOVAL)):
+            sna = SNA((self._number + sna2._number)%self.MODULOVAL)
+            return DateSNA.fromSNA(sna)
+        else:
+            raise ArithmeticError
+
+
+    def asDate(self):
+        """return a representation of the object as a date string"""
+        dtstruct = time.gmtime(self._number)
+        return time.strftime(DateSNA.fmt, dtstruct)
+
+
+    @classmethod
+    def fromSNA(cls, sna):
+        """
+        create an DateSNA object from an SNA
+        """
+        d = cls()
+        d._number = sna._number
+        return d
+
+
+    @classmethod
+    def fromInt(cls, i):
+        """
+        create an DateSNA object from an int
+        """
+        return cls.fromSNA(SNA(i))
+
+
+    def __str__(self):
+        """
+        return a string representation of the object
+        """
+        return self.asDate()
