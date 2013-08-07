65ba9141aee80ba837efd37ec85b608751ab9587
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-07 11:22:59 +0000

    tests for 8bit serial numbers
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/serial-number-arithmetic-6672@39506 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/test/test_util.py
+++ twisted/names/test/test_util.py
@@ -4,6 +4,7 @@
 """
 Test cases for L{twisted.names.util}.
 """
+from functools import partial
 
 from twisted.names.util import SNA, DateSNA
 from twisted.names.util import max as sna_max
@@ -162,7 +163,6 @@ def assertUndefinedComparison(testCase, a, b):
 
 
 
-from functools import partial
 sna2 = partial(SNA, serialBits=2)
 
 
@@ -212,3 +212,78 @@ class SNA2BitTests(unittest.TestCase):
         assertUndefinedComparison(self, sna2(0), sna2(2))
         assertUndefinedComparison(self, sna2(1), sna2(3))
         assertUndefinedComparison(self, sna2(3), sna2(1))
+
+
+sna8 = partial(SNA, serialBits=8)
+
+
+
+class SNA8BitTests(unittest.TestCase):
+    """
+    Consider the case where SERIAL_BITS == 8.  In this space the integers
+    that make up the serial number space are 0, 1, 2, ... 254, 255.
+    255 == 2^SERIAL_BITS - 1.
+
+    https://tools.ietf.org/html/rfc1982#section-5.2
+    """
+
+    def test_maxadd(self):
+        """
+        In this space, the largest integer that it is meaningful to add to a
+        sequence number is 2^(SERIAL_BITS - 1) - 1, or 127.
+        """
+        self.assertEqual(SNA(0, serialBits=8).MAXADD, 127)
+
+
+    def test_add(self):
+        """
+        Addition is as expected in this space, for example: 255+1 == 0,
+        100+100 == 200, and 200+100 == 44.
+        """
+        self.assertEqual(sna8(255) + sna8(1), sna8(0))
+        self.assertEqual(sna8(100) + sna8(100), sna8(200))
+        self.assertEqual(sna8(200) + sna8(100), sna8(44))
+
+
+    def test_gt(self):
+        """
+        Comparison is more interesting, 1 > 0, 44 > 0, 100 > 0, 100 > 44,
+        200 > 100, 255 > 200, 0 > 255, 100 > 255, 0 > 200, and 44 > 200.
+        """
+        self.assertGreater(sna8(1), sna8(0))
+        self.assertGreater(sna8(44), sna8(0))
+        self.assertGreater(sna8(100), sna8(0))
+        self.assertGreater(sna8(100), sna8(44))
+        self.assertGreater(sna8(200), sna8(100))
+        self.assertGreater(sna8(255), sna8(200))
+        self.assertGreater(sna8(100), sna8(255))
+        self.assertGreater(sna8(0), sna8(200))
+        self.assertGreater(sna8(44), sna8(200))
+
+
+    def test_surprisingAddition(self):
+        """
+        Note that 100+100 > 100, but that (100+100)+100 < 100.  Incrementing
+        a serial number can cause it to become "smaller".  Of course,
+        incrementing by a smaller number will allow many more increments to
+        be made before this occurs.  However this is always something to be
+        aware of, it can cause surprising errors, or be useful as it is the
+        only defined way to actually cause a serial number to decrease.
+        """
+        self.assertGreater(sna8(100) + sna8(100), sna8(100))
+
+        # XXX: This test fails
+#        self.assertLess((sna8(100) + sna8(100)) + sna8(100), sna8(100))
+        self.assertRaises(ArithmeticError, lambda: (sna8(100) + sna8(100)) + sna8(100))
+
+
+    def test_undefined(self):
+        """
+        The pairs of values 0 and 128, 1 and 129, 2 and 130, etc, to 127 and
+        255 are not equal, but in each pair, neither number is defined as
+        being greater than, or less than, the other.
+        """
+        assertUndefinedComparison(self, sna8(0), sna8(128))
+        assertUndefinedComparison(self, sna8(1), sna8(129))
+        assertUndefinedComparison(self, sna8(2), sna8(130))
+        assertUndefinedComparison(self, sna8(127), sna8(255))
