ada76d04f55bb99f4abb9dec94b5adf8a0e34069
shira <shira@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-15 21:02:00 +0000

    Fix docstring
    refs #6383
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/unit-tests-6383@39612 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/mail/test/test_mail.py
+++ twisted/mail/test/test_mail.py
@@ -575,7 +575,7 @@ class MaildirTestCase(unittest.TestCase):
 
 class TestMaildirDomain(mail.maildir.AbstractMaildirDomain):
     """
-    Maildir-backed domain that does no authentication.
+    Maildir-backed domain. 
 
     An implementation of L{mail.maildir.AbstractMaildirDomain} for testing
     purposes.
