337dc34141416a1eb08090d7e97b118bbf18093a
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-24 08:48:13 +0000

    documentation for the remaining message handler methods
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/clean-response-message-6645@39718 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/server.py
+++ twisted/names/server.py
@@ -293,6 +293,29 @@ class DNSServerFactory(protocol.ServerFactory):
 
 
     def handleInverseQuery(self, message, protocol, address):
+        """
+        Called by L{DNSServerFactory.messageReceived} when an inverse
+        query message is received.
+
+        Replies with a I{Not Implemented} error by default.
+
+        An error message will be logged if L{DNSServerFactory.verbose}
+        is C{>1}.
+
+        Override in a subclass.
+
+        @param protocol: The DNS protocol instance to which to send a
+            response message.
+        @type protocol: L{dns.DNSDatagramProtocol} or L{dns.DNSProtocol}
+
+        @param message: The original DNS query message for which a
+            response message will be constructed.
+        @type message: L{dns.Message}
+
+        @param address: The address to which the response message will
+            be sent or L{None} if C{protocol} is a stream protocol.
+        @type address: L{tuple} or L{None}
+        """
         message.rCode = dns.ENOTIMP
         self.sendReply(protocol, message, address)
         if self.verbose:
@@ -300,6 +323,29 @@ class DNSServerFactory(protocol.ServerFactory):
 
 
     def handleStatus(self, message, protocol, address):
+        """
+        Called by L{DNSServerFactory.messageReceived} when a status
+        message is received.
+
+        Replies with a I{Not Implemented} error by default.
+
+        An error message will be logged if L{DNSServerFactory.verbose}
+        is C{>1}.
+
+        Override in a subclass.
+
+        @param protocol: The DNS protocol instance to which to send a
+            response message.
+        @type protocol: L{dns.DNSDatagramProtocol} or L{dns.DNSProtocol}
+
+        @param message: The original DNS query message for which a
+            response message will be constructed.
+        @type message: L{dns.Message}
+
+        @param address: The address to which the response message will
+            be sent or L{None} if C{protocol} is a stream protocol.
+        @type address: L{tuple} or L{None}
+        """
         message.rCode = dns.ENOTIMP
         self.sendReply(protocol, message, address)
         if self.verbose:
@@ -307,6 +353,29 @@ class DNSServerFactory(protocol.ServerFactory):
 
 
     def handleNotify(self, message, protocol, address):
+        """
+        Called by L{DNSServerFactory.messageReceived} when a notify
+        message is received.
+
+        Replies with a I{Not Implemented} error by default.
+
+        An error message will be logged if L{DNSServerFactory.verbose}
+        is C{>1}.
+
+        Override in a subclass.
+
+        @param protocol: The DNS protocol instance to which to send a
+            response message.
+        @type protocol: L{dns.DNSDatagramProtocol} or L{dns.DNSProtocol}
+
+        @param message: The original DNS query message for which a
+            response message will be constructed.
+        @type message: L{dns.Message}
+
+        @param address: The address to which the response message will
+            be sent or L{None} if C{protocol} is a stream protocol.
+        @type address: L{tuple} or L{None}
+        """
         message.rCode = dns.ENOTIMP
         self.sendReply(protocol, message, address)
         if self.verbose:
@@ -314,6 +383,29 @@ class DNSServerFactory(protocol.ServerFactory):
 
 
     def handleOther(self, message, protocol, address):
+        """
+        Called by L{DNSServerFactory.messageReceived} when a message with
+        unrecognised I{OPCODE} is received.
+
+        Replies with a I{Not Implemented} error by default.
+
+        An error message will be logged if L{DNSServerFactory.verbose}
+        is C{>1}.
+
+        Override in a subclass.
+
+        @param protocol: The DNS protocol instance to which to send a
+            response message.
+        @type protocol: L{dns.DNSDatagramProtocol} or L{dns.DNSProtocol}
+
+        @param message: The original DNS query message for which a
+            response message will be constructed.
+        @type message: L{dns.Message}
+
+        @param address: The address to which the response message will
+            be sent or L{None} if C{protocol} is a stream protocol.
+        @type address: L{tuple} or L{None}
+        """
         message.rCode = dns.ENOTIMP
         self.sendReply(protocol, message, address)
         if self.verbose:
