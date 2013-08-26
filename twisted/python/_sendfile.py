070485ecb628364389584a170f0af8a875918355
therve <therve@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-26 17:38:43 +0000

    OS X support
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/sendfile-585-11@39754 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/_sendfile.py
+++ twisted/python/_sendfile.py
@@ -1,6 +1,8 @@
 from cffi import FFI
 
+import errno
 import os
+import sys
 
 ffi = FFI()
 
@@ -13,18 +15,31 @@ else:
     typedef long off_t;
 """)
 
-ffi.cdef("""
+if sys.platform == "darwin":
+    ffi.cdef("""
+    int sendfile(int fd, int s, off_t offset, off_t *len, struct sf_hdtr *hdtr, int flags);
+""")
+    libc = ffi.dlopen(None)
+
+    def sendfile(out_fd, in_fd, offset, nbytes):
+        length = ffi.new("off_t *")
+        length[0] = nbytes
+        ret = libc.sendfile(in_fd, out_fd, offset, length, ffi.NULL, 0)
+        if ret < 0:
+            if length[0] and not ffi.errno in (errno.EBUSY, errno.EAGAIN, errno.EWOULDBLOCK):
+            	raise OSError(os.strerror(ffi.errno))
+        return length[0]
+
+else:
+    ffi.cdef("""
     ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count);
 """)
+    libc = ffi.dlopen(None)
 
-libc = ffi.dlopen(None)
-
-
-#print ffi.sizeof("long")
-def sendfile(out_fd, in_fd, offset, nbytes):
-    off_t = ffi.new("off_t *")
-    off_t[0] = offset
-    sent = libc.sendfile(out_fd, in_fd, off_t, nbytes)
-    if sent == -1:
-        raise OSError(os.strerror(ffi.errno))
-    return sent
+    def sendfile(out_fd, in_fd, offset, nbytes):
+        off_t = ffi.new("off_t *")
+        off_t[0] = offset
+        sent = libc.sendfile(out_fd, in_fd, off_t, nbytes)
+        if sent == -1:
+            raise OSError(os.strerror(ffi.errno))
+        return sent
