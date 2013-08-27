0e0883deeba325508780b93efb376659afc2b6c5
shira <shira@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-27 13:57:06 +0000

    Going back to original code after discovering twistedchecker problem
    refs #6649
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/mail-doc-6649@39780 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/mail/mail.py
+++ twisted/mail/mail.py
@@ -371,7 +371,6 @@ class IDomain(Interface):
 
         @raise SMTPBadRcpt: When the given user does not exist in this domain.
         """
-        pass
 
 
     def addUser(user, password):
@@ -384,7 +383,6 @@ class IDomain(Interface):
         @type password: L{bytes}
         @param password: A password.
         """
-        pass
 
 
     def getCredentialsCheckers():
@@ -395,7 +393,6 @@ class IDomain(Interface):
             <twisted.cred.checkers.ICredentialsChecker>} provider
         @return: Credentials checkers for this domain.
         """
-        pass
 
 
 
