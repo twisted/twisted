00c06a4fb920fc39f77f14a13a8d1b3df4cea6f2
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-08-28 16:16:51 +0000

    Teach the example to report exceptions more better
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/robust-processterminated-example-6660@39797 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/core/howto/listings/process/process.py
+++ doc/core/howto/listings/process/process.py
@@ -3,14 +3,16 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-from twisted.internet import protocol
-from twisted.internet import reactor
 import re
+
+from twisted.internet import reactor, protocol, error
+
 
 class MyPP(protocol.ProcessProtocol):
     def __init__(self, verses):
         self.verses = verses
         self.data = ""
+
     def connectionMade(self):
         print "connectionMade!"
         for i in range(self.verses):
@@ -19,25 +21,40 @@ class MyPP(protocol.ProcessProtocol):
                                  "Take one down and pass it around,\n" +
                                  "Aleph-null bottles of beer on the wall.\n")
         self.transport.closeStdin() # tell them we're done
+
     def outReceived(self, data):
         print "outReceived! with %d bytes!" % len(data)
         self.data = self.data + data
+
     def errReceived(self, data):
         print "errReceived! with %d bytes!" % len(data)
+
     def inConnectionLost(self):
         print "inConnectionLost! stdin is closed! (we probably did it)"
+
     def outConnectionLost(self):
         print "outConnectionLost! The child closed their stdout!"
         # now is the time to examine what they wrote
         #print "I saw them write:", self.data
         (dummy, lines, words, chars, file) = re.split(r'\s+', self.data)
         print "I saw %s lines" % lines
+
     def errConnectionLost(self):
         print "errConnectionLost! The child closed their stderr."
+
+    def _reportEnd(self, which, reason):
+        if reason.check(error.ProcessTerminated, error.ProcessDone):
+            exc = reason.value
+            print "%s, status %d" % (which, exc.status)
+            print "\texit code %s; termination signal %s" % (exc.exitCode, exc.signal)
+        else:
+            print which, reason.getErrorMessage()
+
     def processExited(self, reason):
-        print "processExited, status %d" % (reason.value.exitCode,)
+        self._reportEnd("processExited", reason)
+
     def processEnded(self, reason):
-        print "processEnded, status %d" % (reason.value.exitCode,)
+        self._reportEnd("processEnded", reason)
         print "quitting"
         reactor.stop()
 
