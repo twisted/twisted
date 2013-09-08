ecf5fe5aaed89318a575b010cd11e02756c1efc0
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-09-08 14:19:51 +0000

    some docstrings
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/linglengthexceeded-buffer-6536@39886 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/test/test_basic.py
+++ twisted/protocols/test/test_basic.py
@@ -360,16 +360,27 @@ a'''
 
 
 class ExcessivelyLargeLineCatcher(basic.LineReceiver):
+    """
+    Helper for L{LineReceiverLineLengthExceededTests}.
+
+    @ivar longLines: A L{list} of L{bytes} giving the values
+        C{lineLengthExceeded} has been called with.
+    """
     def connectionMade(self):
         self.longLines = []
 
 
     def lineReceived(self, line):
-        pass
+        """
+        Disregard any received lines.
+        """
 
 
-    def lineLengthExceeded(self, line):
-        self.longLines.append(line)
+    def lineLengthExceeded(self, data):
+        """
+        Record any data that exceeds the line length limits.
+        """
+        self.longLines.append(data)
 
 
 
