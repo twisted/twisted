90c426a83a555c9834f751e425367a2450c893e6
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-10-20 14:57:54 +0000

    merge forward, drop some unrelated changes, resolve conflicts
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tuntap-pytun-6169-4@40411 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/pair/examples/pairudp.py
+++ doc/pair/examples/pairudp.py
@@ -1,13 +1,20 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
+from sys import stdout
+
 from twisted.internet import reactor, protocol
-from twisted.pair import ethernet, rawudp, ip
-from twisted.pair import tuntap
+from twisted.pair import ethernet, rawudp, ip, tuntap
+from twisted.python.log import startLogging
+
+startLogging(stdout, setStdout=False)
+
 
 class MyProto(protocol.DatagramProtocol):
+
     def datagramReceived(self, *a, **kw):
         print a, kw
+
 
 p_udp = rawudp.RawUDPProtocol()
 p_udp.addProto(42, MyProto())
@@ -17,5 +24,15 @@ p_eth = ethernet.EthernetProtocol()
 p_eth.addProto(0x800, p_ip)
 
 port = tuntap.TuntapPort(interface='tap0', proto=p_eth, reactor=reactor)
+
+# Ha ha!  It does not yet work.  Next you need to do:
+# $ sudo ifconfig tap0 up
+# $ sudo ip neigh add <ip address> lladdr <tap0 mac address> dev tap0
+# $ sudo ip route add to <ip network> dev tap0
+#
+# For example, to be able to send UDP to 10.0.0.1 on port 42 (which will
+# actually trigger this example to do something), use 10.0.0.1 for <ip address>
+# and 10.0.0.0/24 for <ip network>
+
 port.startListening()
 reactor.run()
