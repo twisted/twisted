707a742ce7de971773009b01c6ae5ff9f7c50fa0
lvh <lvh@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-10-28 13:47:19 +0000

    Apply patch
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/multideferred-6365@40517 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/defer.py
+++ twisted/internet/defer.py
@@ -1604,6 +1604,91 @@ class DeferredFilesystemLock(lockfile.FilesystemLock):
 
 
 
+class MultiDeferred(object):
+    """
+    An object that produces other deferreds. When this object is
+    callbacked or errbacked, those deferreds are callbacked or
+    errbacked with the same result or failure.
+    """
+    def __init__(self):
+        self._deferreds = []
+        self._result = _NO_RESULT
+        self._isFailure = None
+
+
+    def tee(self):
+        """
+        Produces a new deferred and returns it. If this C{MultiDeferred}
+        has not been fired (callbacked or errbacked) yet, the deferred
+        will not have been fired yet either, but will be fired when
+        and if this C{MultiDeferred} gets fired in the future. If this
+        C{MultiDeferred} has been fired, returns a deferred
+        synchronously fired with the same result.
+
+        @return: A deferred that will fire with whatever this object
+        is fired with.
+        @rtype: L{Deferred}
+        """
+        if self._result is not _NO_RESULT:
+            if not self._isFailure:
+                return succeed(self._result)
+            else:
+                return fail(self._result)
+
+        d = Deferred()
+        self._deferreds.append(d)
+        return d
+
+
+    def callback(self, result):
+        """
+        Callbacks the deferreds previously produced by this object.
+
+        @param result: The object which will be passed to the
+        C{callback} method of all C{Deferred}s previously produced by
+        this object's C{tee} method.
+        @raise AlreadyCalledError: If L{callback} or L{errback} has
+        already been called on this object.
+        """
+        self._setResult(result)
+        self._isFailure = False
+
+        for d in self._deferreds:
+            d.callback(result)
+
+
+    def errback(self, failure):
+        """
+        Errbacks the deferreds previously produced by this object.
+
+        @param failure: The object which will be passed to the
+        C{errback} method of all C{Deferred}s previously produced by
+        this object's C{tee} method.
+        @raise AlreadyCalledError: If L{callback} or L{errback} has
+        already been called on this object.
+        """
+        self._setResult(failure)
+        self._isFailure = True
+
+        for d in self._deferreds:
+            d.errback(failure)
+
+
+    def _setResult(self, result):
+        """
+        Sets the result. If the result is already set, raises
+        C{AlreadyCalledError}.
+
+        @raise AlreadyCalledError: The result was already set.
+        @return: C{None}, if the result was successfully set.
+        """
+        if self._result is not _NO_RESULT:
+            raise AlreadyCalledError()
+
+        self._result = result
+
+
+
 __all__ = ["Deferred", "DeferredList", "succeed", "fail", "FAILURE", "SUCCESS",
            "AlreadyCalledError", "TimeoutError", "gatherResults",
            "maybeDeferred",
--- twisted/test/test_defer.py
+++ twisted/test/test_defer.py
@@ -2329,3 +2329,139 @@ class DeferredFilesystemLockTestCase(unittest.TestCase):
         self.clock.advance(1)
 
         return d
+
+
+
+
+class MultiDeferredTests(unittest.TestCase):
+    """
+    Tests for L{defer.MultiDeferred}.
+    """
+    def setUp(self):
+        self.multiDeferred = defer.MultiDeferred()
+
+
+    def test_callback(self):
+        """
+        Any produced L{defer.Deferred}s have their callbacks called when the
+        L{defer.MultiDeferred} does.
+        """
+        a, b, c = [self.multiDeferred.tee() for _ in xrange(3)]
+
+        self.assertNoResult(a)
+        self.assertNoResult(b)
+        self.assertNoResult(c)
+
+        result = object()
+        self.multiDeferred.callback(result)
+
+        self.assertIdentical(self.successResultOf(a), result)
+        self.assertIdentical(self.successResultOf(b), result)
+        self.assertIdentical(self.successResultOf(c), result)
+
+
+    def test_errback(self):
+        """
+        Any produced L{defer.Deferred}s have their errbacks called when the
+        L{defer.MultiDeferred} does.
+        """
+        a, b, c = [self.multiDeferred.tee() for _ in xrange(3)]
+
+        self.assertNoResult(a)
+        self.assertNoResult(b)
+        self.assertNoResult(c)
+
+        error = RuntimeError()
+        self.multiDeferred.errback(error)
+
+        self.assertIdentical(self.failureResultOf(a, RuntimeError).value, error)
+        self.assertIdentical(self.failureResultOf(b, RuntimeError).value, error)
+        self.assertIdentical(self.failureResultOf(c, RuntimeError).value, error)
+
+
+    def test_callbackAfterCallback(self):
+        """
+        Calling C{callback} twice raises L{defer.AlreadyCalledError}.
+        """
+        self.multiDeferred.callback(None)
+        self.assertRaises(defer.AlreadyCalledError,
+                          self.multiDeferred.callback, None)
+
+
+    def test_callbackAfterErrback(self):
+        """
+        Calling C{callback} after C{errback} raises L{defer.AlreadyCalledError}.
+        """
+        self.multiDeferred.errback(RuntimeError())
+        self.assertRaises(defer.AlreadyCalledError,
+                          self.multiDeferred.callback, None)
+
+
+    def test_errbackAfterCallback(self):
+        """
+        Calling C{errback} after C{callback} raises L{defer.AlreadyCalledError}.
+        """
+        self.multiDeferred.callback(None)
+        self.assertRaises(defer.AlreadyCalledError,
+                          self.multiDeferred.errback, RuntimeError())
+
+
+    def test_errbackAfterErrback(self):
+        """
+        Calling C{errback} after C{errback} raises L{defer.AlreadyCalledError}.
+        """
+        self.multiDeferred.errback(RuntimeError())
+        self.assertRaises(defer.AlreadyCalledError,
+                          self.multiDeferred.errback, RuntimeError())
+
+
+    def test_synchronousCallbacks(self):
+        """
+        All callbacks are called sequentially, synchronously, and in the order
+        they were produced. If one or more of the L{defer.Deferred}s produced
+        by L{defer.MultiDeferred.tee} is waiting on a deferred that will never
+        fire, all the other deferreds produced by that method are still fired.
+        """
+        called = []
+        result = object()
+
+        def callback(r, i):
+            """
+            Checks this is the correct result, adds this deferreds index to the list
+            of called deferreds, and then returns a deferred that will never
+            fire.
+            """
+            self.assertIdentical(r, result)
+            called.append(i)
+            return defer.Deferred()
+
+        for i in range(10):
+            self.multiDeferred.tee().addCallback(callback, i=i)
+
+        self.assertEqual(called, [])
+        self.multiDeferred.callback(result)
+        self.assertEqual(called, range(10))
+
+
+    def test_alreadyFiredWithResult(self):
+        """
+        If the C{MultiDeferred} already fired, C{tee} produces a
+        C{Deferred} that has already been fired.
+        """
+        result = object()
+        self.multiDeferred.callback(result)
+        d = self.multiDeferred.tee()
+        self.assertIdentical(self.successResultOf(d), result)
+
+
+    def test_alreadyFiredWithError(self):
+        """
+        If the C{MultiDeferred} already fired with a failure, C{tee}
+        produces a C{Deferred} that has already been fired with the
+        failure.
+        """
+        error = RuntimeError()
+        self.multiDeferred.errback(error)
+        d = self.multiDeferred.tee()
+        failure = self.failureResultOf(d, RuntimeError)
+        self.assertIdentical(failure.value, error)
