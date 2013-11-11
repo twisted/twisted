b7ebfa4be986186c7298c188bebc794e42413089
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-11-11 16:59:52 +0000

    document the time format variable
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/serial-number-arithmetic-6672@40650 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/rfc1982.py
+++ twisted/names/rfc1982.py
@@ -6,6 +6,12 @@
 Utilities for handling RFC1982 Serial Number Arithmetic.
 
 @see: U{http://tools.ietf.org/html/rfc1982}
+
+@var RFC4034_TIME_FORMAT: RRSIG Time field presentation format. The Signature
+   Expiration Time and Inception Time field values MUST be represented either as
+   an unsigned decimal integer indicating seconds since 1 January 1970 00:00:00
+   UTC, or in the form YYYYMMDDHHmmSS in UTC. See U{RRSIG Presentation
+   Format<https://tools.ietf.org/html/rfc4034#section-3.2>}
 """
 
 from __future__ import division, absolute_import
