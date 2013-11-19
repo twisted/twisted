38b7479640cfdfd5f1aba83ca4996c466dfc4cbb
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-11-19 18:27:35 +0000

    tweak the comment a tiny bit
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/freebsd-posixifaces-6843@40800 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/_posixifaces.py
+++ twisted/internet/test/_posixifaces.py
@@ -145,9 +145,10 @@ def posixGetLinkLocalIPv6Addresses():
         address = nativeString(address)
         if family == socket.AF_INET6 and address.startswith('fe80:'):
             if sys.platform.startswith('freebsd'):
-                ## kill embedded interface indices in link-local scoped addresses
-                ## https://twistedmatrix.com/trac/ticket/6843
-                ## http://www.freebsd.org/doc/en/books/developers-handbook/ipv6.html#ipv6-scope-index
+                # kill embedded interface indices in link-local scoped addresses
+                # See https://twistedmatrix.com/trac/ticket/6843 and
+                # http://www.freebsd.org/doc/en/books/developers-handbook/ipv6.html#ipv6-scope-index
+                # Indications are that the need for this will be gone in FreeBSD >=10.
                 l = address.split(':')
                 l[1] = ''
                 address = ':'.join(l)
