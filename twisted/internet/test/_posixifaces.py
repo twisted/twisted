43f968761dbd7634b7319e5ade558c690fad9ab1
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-11-20 11:43:19 +0000

    Merge freebsd-posixifaces-6843
    Author: oberstet, exarkun
    Reviewer: exarkun, lvh
    Fixes: #6843
    
    Account for the embedded scope id/interface index value found in the results of
    `getifaddrs` on current versions of FreeBSD (by discarding them).  This fixes
    seemingly incorrect link local address information retrieved by the IPv6 testing
    helper.
    
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@40810 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/_posixifaces.py
+++ twisted/internet/test/_posixifaces.py
@@ -30,7 +30,7 @@ if _PY3:
 
 libc = CDLL(find_library("c"))
 
-if sys.platform == 'darwin':
+if sys.platform.startswith('freebsd') or sys.platform == 'darwin':
     _sockaddrCommon = [
         ("sin_len", c_uint8),
         ("sin_family", c_uint8),
@@ -100,6 +100,34 @@ getifaddrs.restype = c_int
 freeifaddrs = libc.freeifaddrs
 freeifaddrs.argtypes = [ifaddrs_p]
 
+
+
+def _maybeCleanupScopeIndex(family, packed):
+    """
+    On FreeBSD, kill the embedded interface indices in link-local scoped
+    addresses.
+
+    @param family: The address family of the packed address - one of the
+        I{socket.AF_*} constants.
+
+    @param packed: The packed representation of the address (ie, the bytes of a
+        I{in_addr} field).
+    @type packed: L{bytes}
+
+    @return: The packed address with any FreeBSD-specific extra bits cleared.
+    @rtype: L{bytes}
+
+    @see: U{https://twistedmatrix.com/trac/ticket/6843}
+    @see: U{http://www.freebsd.org/doc/en/books/developers-handbook/ipv6.html#ipv6-scope-index}
+
+    @note: Indications are that the need for this will be gone in FreeBSD >=10.
+    """
+    if sys.platform.startswith('freebsd') and packed[:2] == b"\xfe\x80":
+        return packed[:2] + b"\x00\x00" + packed[4:]
+    return packed
+
+
+
 def _interfaces():
     """
     Call C{getifaddrs(3)} and return a list of tuples of interface name, address
@@ -122,6 +150,7 @@ def _interfaces():
 
                 if addr:
                     packed = b''.join(map(chr, addr[0].sin_addr.in_addr[:]))
+                    packed = _maybeCleanupScopeIndex(family, packed)
                     results.append((
                             ifaddrs[0].ifa_name,
                             family,
