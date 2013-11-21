d19a2ec4aae479273fcbb53078d7d48867a90ae0
thijs <thijs@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-11-21 13:15:15 +0000

    remove tapconvert script, pyflakes fixes
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/remove-scripts-6747@40838 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/__init__.py
+++ twisted/scripts/__init__.py
@@ -7,9 +7,3 @@ Subpackage containing the modules that implement the command line tools.
 Note that these are imported by top-level scripts which are intended to be
 invoked directly from a shell.
 """
-
-from twisted.python.versions import Version
-from twisted.python.deprecate import deprecatedModuleAttribute
-
-
-del Version, deprecatedModuleAttribute
