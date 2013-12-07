fae959dfd0f98fd2177cf811a3ef3622805f0d3a
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-12-07 09:39:59 +0000

    clean up this example a bunch
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tuntap-pytun-6169-4@40914 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/pair/examples/pairudp.py
+++ doc/pair/examples/pairudp.py
@@ -1,38 +1,64 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
+"""
+This is an example of using Twisted Pair's Linux tun/tap integration to receive
+and react to ethernet, ip, and udp datagrams as if the example process were a
+network device reachable via a system network interface.
+
+Some system setup is required to successfully run this example.  As a
+privileged user (in other words, root), these commands will create the
+necessary tap network interface:
+
+$ ip tuntap add dev tap0 mode tap user ${USER_ID}
+$ ip link set dev tap0 up
+$ ip neigh add ${IP_ADDRESS} lladdr ${MAC_ADDRESS} dev tap0
+$ ip route add to ${IP_NETWORK} dev tap0
+
+Once you've done this, sending UDP datagrams to ${IP_ADDRESS} on port 42 will
+trigger a response from this example.
+
+Use the UID of the user you want to be able to run the example for ${USER_ID}
+and use something like 10.0.0.0/24 for ${IP_NETWORK}.
+
+Invent any valid value for ${MAC_ADDRESS} (though avoid re-using an address
+already in use on your network).
+
+When the tap device is configured, the example is running, and a UDP datagram
+is sent to the right address, the example will display information about the
+datagram on its standard out.
+"""
+
 from sys import stdout
 
 from twisted.internet import reactor, protocol
-from twisted.pair import ethernet, rawudp, ip, tuntap
+from twisted.pair.ethernet import EthernetProtocol
+from twisted.pair.ip import IPProtocol
+from twisted.pair.rawudp import RawUDPProtocol
+from twisted.pair.tuntap import TuntapPort
 from twisted.python.log import startLogging
 
-startLogging(stdout, setStdout=False)
 
 
 class MyProto(protocol.DatagramProtocol):
+    def datagramReceived(self, datagram, address):
+        print 'from', address, 'received', repr(datagram)
 
-    def datagramReceived(self, *a, **kw):
-        print a, kw
 
 
-p_udp = rawudp.RawUDPProtocol()
-p_udp.addProto(42, MyProto())
-p_ip = ip.IPProtocol()
-p_ip.addProto(17, p_udp)
-p_eth = ethernet.EthernetProtocol()
-p_eth.addProto(0x800, p_ip)
+def main():
+    startLogging(stdout, setStdout=False)
+    udp = RawUDPProtocol()
+    udp.addProto(42, MyProto())
+    ip = IPProtocol()
+    ip.addProto(17, udp)
+    eth = EthernetProtocol()
+    eth.addProto(0x800, ip)
 
-port = tuntap.TuntapPort(interface='tap0', proto=p_eth, reactor=reactor)
+    port = TuntapPort(interface='tap0', proto=eth, reactor=reactor)
 
-# Ha ha!  It does not yet work.  Next you need to do:
-# $ sudo ifconfig tap0 up
-# $ sudo ip neigh add <ip address> lladdr <tap0 mac address> dev tap0
-# $ sudo ip route add to <ip network> dev tap0
-#
-# For example, to be able to send UDP to 10.0.0.1 on port 42 (which will
-# actually trigger this example to do something), use 10.0.0.1 for <ip address>
-# and 10.0.0.0/24 for <ip network>
+    port.startListening()
+    reactor.run()
 
-port.startListening()
-reactor.run()
+if __name__ == '__main__':
+    main()
