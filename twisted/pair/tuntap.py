96adf62e1b740be48cd89596e1ffeda88f3b175b
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-12-07 15:36:49 +0000

    Speculate on what will happen to this code
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tuntap-pytun-6169-5@40938 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/pair/tuntap.py
+++ twisted/pair/tuntap.py
@@ -83,7 +83,7 @@ class TunnelAddress(object):
         Deprecated accessor for the tunnel name.  Use attributes instead.
         """
         warnings.warn(
-            "TunnelAddress.__getitem__ is deprecated since Twisted 13.1  "
+            "TunnelAddress.__getitem__ is deprecated since Twisted 14.0  "
             "Use attributes instead.", category=DeprecationWarning,
             stacklevel=2)
         return ('TUNTAP', self.name)[index]
@@ -375,6 +375,6 @@ class TuntapPort(abstract.FileDescriptor):
         return TunnelAddress(self._mode, self.interface)
 
 TuntapPort.loseConnection = deprecated(
-    Version("Twisted", 13, 1, 0),
+    Version("Twisted", 14, 0, 0),
     TuntapPort.stopListening)(TuntapPort.loseConnection)
 
