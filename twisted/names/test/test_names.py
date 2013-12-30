685e5546b33268e3457135eeada0f2196abb5cf6
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2013-12-30 14:32:43 +0000

    Merge forward the tests from names-server-coverage-6700
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/names-server-coverage-6887@41045 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/test/test_names.py
+++ twisted/names/test/test_names.py
@@ -1,4 +1,3 @@
-# -*- test-case-name: twisted.names.test.test_names -*-
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
@@ -432,75 +431,6 @@ class ServerDNSTestCase(unittest.TestCase):
                               "!^.*$!sip:information@domain.tld!",
                               ttl=19283784)])
 
-
-
-class DNSServerFactoryTests(unittest.TestCase):
-    """
-    Tests for L{server.DNSServerFactory}.
-    """
-    def _messageReceivedTest(self, methodName, message):
-        """
-        Assert that the named method is called with the given message when
-        it is passed to L{DNSServerFactory.messageReceived}.
-        """
-        # Make it appear to have some queries so that
-        # DNSServerFactory.allowQuery allows it.
-        message.queries = [None]
-
-        receivedMessages = []
-        def fakeHandler(message, protocol, address):
-            receivedMessages.append((message, protocol, address))
-
-        class FakeProtocol(object):
-            def writeMessage(self, message):
-                pass
-
-        protocol = FakeProtocol()
-        factory = server.DNSServerFactory(None)
-        setattr(factory, methodName, fakeHandler)
-        factory.messageReceived(message, protocol)
-        self.assertEqual(receivedMessages, [(message, protocol, None)])
-
-
-    def test_notifyMessageReceived(self):
-        """
-        L{DNSServerFactory.messageReceived} passes messages with an opcode
-        of C{OP_NOTIFY} on to L{DNSServerFactory.handleNotify}.
-        """
-        # RFC 1996, section 4.5
-        opCode = 4
-        self._messageReceivedTest('handleNotify', Message(opCode=opCode))
-
-
-    def test_updateMessageReceived(self):
-        """
-        L{DNSServerFactory.messageReceived} passes messages with an opcode
-        of C{OP_UPDATE} on to L{DNSServerFactory.handleOther}.
-
-        This may change if the implementation ever covers update messages.
-        """
-        # RFC 2136, section 1.3
-        opCode = 5
-        self._messageReceivedTest('handleOther', Message(opCode=opCode))
-
-
-    def test_connectionTracking(self):
-        """
-        The C{connectionMade} and C{connectionLost} methods of
-        L{DNSServerFactory} cooperate to keep track of all
-        L{DNSProtocol} objects created by a factory which are
-        connected.
-        """
-        protoA, protoB = object(), object()
-        factory = server.DNSServerFactory()
-        factory.connectionMade(protoA)
-        self.assertEqual(factory.connections, [protoA])
-        factory.connectionMade(protoB)
-        self.assertEqual(factory.connections, [protoA, protoB])
-        factory.connectionLost(protoA)
-        self.assertEqual(factory.connections, [protoB])
-        factory.connectionLost(protoB)
-        self.assertEqual(factory.connections, [])
 
 
 class HelperTestCase(unittest.TestCase):
