b1577fb74d9d838b874ca14db71c7ac7ca50297d
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-01-21 01:16:02 +0000

    Do the merge forward hokey pokey.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/python3-setup-6539-2@41378 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/dist.py
+++ twisted/python/dist.py
@@ -39,6 +39,8 @@ on event-based network programming and multiprotocol integration.
     classifiers=[
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
         ],
     )
 
