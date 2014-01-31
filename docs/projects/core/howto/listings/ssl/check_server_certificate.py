be7631e2db2640a9fc0e0c90d4eb0618a477abaf
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-01-31 17:07:41 +0000

    Clean listing up a bit more.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/trusted-ca-openssl-defaults-5446@41503 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/projects/core/howto/listings/ssl/check_server_certificate.py
+++ docs/projects/core/howto/listings/ssl/check_server_certificate.py
@@ -1,21 +1,15 @@
+from __future__ import print_function
 import sys
 
 from twisted.internet import defer, endpoints, protocol, ssl, task
 from pprint import pprint
 from twisted.internet.ssl import platformTrust
 
-def printCertificate((proto, reason)):
-    x509 = proto.transport.getPeerCertificate()
-    if x509 is not None:
-        cert = ssl.Certificate(x509)
-        print(cert.dumpPEM())
-    if reason.check(ssl.SSL.Error):
-        print("SSL Connection Error:")
-        pprint(reason.value)
-
 def main(reactor, host, port):
     contextFactory = ssl.CertificateOptions(peerTrust=platformTrust())
+    port = int(port)
     handshook = defer.Deferred()
+
     class HandshakeComplete(object):
         def getContext(self):
             ctx = contextFactory.getContext()
@@ -25,17 +19,34 @@ def main(reactor, host, port):
             ctx.set_info_callback(cb)
             return ctx
 
-    ep = endpoints.SSL4ClientEndpoint(reactor, host, port,
-                                      sslContextFactory=contextFactory)
-
     class DelayedDisconnectProtocol(protocol.Protocol):
         def connectionLost(self, reason):
-            self.onConnectionLost.callback((self, reason))
+            if not hasattr(handshook, 'result'):
+                handshook.errback(reason)
 
     proto = DelayedDisconnectProtocol()
 
-    return (endpoints.connectProtocol(ep, proto)
-            .addCallback(lambda connected: handshook)
-            .addCallback(printCertificate))
+    connected = endpoints.connectProtocol(
+        endpoints.SSL4ClientEndpoint(reactor, host, port,
+                                     sslContextFactory=HandshakeComplete()),
+        proto
+    )
+
+    def error(reason):
+        if reason.check(ssl.SSL.Error):
+            print("SSL Connection Error:")
+            pprint(reason.value)
+
+    def printCertificate(ignored):
+        x509 = proto.transport.getPeerCertificate()
+        if x509 is not None:
+            cert = ssl.Certificate(x509)
+            print(cert.dumpPEM())
+
+    return (
+        connected
+        .addCallback(lambda connected: handshook)
+        .addCallbacks(printCertificate, error)
+    )
 
 task.react(main, sys.argv[1:])
