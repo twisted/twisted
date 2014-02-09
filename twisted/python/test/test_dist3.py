2797fe9b8e2e29b9dd7e806534fd349bf86c9278
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-02-09 13:46:13 +0000

    Move module list into an actual module, write some minimal tests.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/setup3-missing-6929@41608 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/python/test/test_dist3.py
@@ -0,0 +1,41 @@
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Tests for L{twisted.python._dist3}.
+"""
+
+from twisted.trial.unittest import TestCase
+
+from twisted.python._dist3 import modulesToInstall
+from twisted.python.filepath import FilePath
+
+
+class ModulesToInstallTests(TestCase):
+    """
+    Tests for L{modulesToInstall}.
+    """
+    def test_sanityCheck(self):
+        """
+        L{modulesToInstall} includes some obvious module names.
+        """
+        modules = modulesToInstall()
+        self.assertIn("twisted.internet.reactor", modules)
+        self.assertIn("twisted.python.test.test_dist3", modules)
+
+
+    def test_exist(self):
+        """
+        All modules listed in L{modulesToInstall} exist.
+        """
+        import twisted
+        root = FilePath(twisted.__file__.encode("utf-8")).parent().parent()
+        for module in modulesToInstall():
+            segments = module.encode("utf-8").split(b".")
+            segments[-1] += b".py"
+            path = root.descendant(segments)
+            alternateSegments = module.encode("utf-8").split(b".") + [
+                b"__init__.py"]
+            packagePath = root.descendant(alternateSegments)
+            self.assertTrue(path.exists() or packagePath.exists(),
+                            "Module %s does not exist" % (module,))
