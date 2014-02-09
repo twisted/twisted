97e34325c1a8a6c19948c7337a153ef0224cf3cf
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-02-09 17:48:13 +0000

    Higher level tests to ensure that request attributes don't leak into the response via gotResolverResponse and gotResolverError.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/clean-response-message-6645-2@41621 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/test/test_server.py
+++ twisted/names/test/test_server.py
@@ -1015,6 +1015,54 @@ class DNSServerFactoryTests(unittest.TestCase):
             protocol=NoopProtocol(), message=dns.Message(), address=None)
 
 
+    def test_gotResolverErrorResetsResponseAttributes(self):
+        """
+        L{server.DNSServerFactory.gotResolverError} does not allow request
+        attributes to leak into the response ie it sends a response with AD, CD
+        set to 0 and empty response record sections.
+        """
+        factory = server.DNSServerFactory()
+        responses = []
+        factory.sendReply = (
+            lambda protocol, response, address: responses.append(response)
+        )
+        request = dns.Message(authenticData=True, checkingDisabled=True)
+        request.answers = [object(), object()]
+        request.authority = [object(), object()]
+        request.additional = [object(), object()]
+        factory.gotResolverError(
+            failure.Failure(error.DomainError()),
+            protocol=None, message=request, address=None
+        )
+
+        self.assertEqual([dns.Message(rCode=3, answer=True)], responses)
+
+
+    def test_gotResolverResponseResetsResponseAttributes(self):
+        """
+        L{server.DNSServerFactory.gotResolverResponse} does not allow request
+        attributes to leak into the response ie it sends a response with AD, CD
+        set to 0 and none of the records in the request answer sections are
+        copied to the response.
+        """
+        factory = server.DNSServerFactory()
+        responses = []
+        factory.sendReply = (
+            lambda protocol, response, address: responses.append(response)
+        )
+        request = dns.Message(authenticData=True, checkingDisabled=True)
+        request.answers = [object(), object()]
+        request.authority = [object(), object()]
+        request.additional = [object(), object()]
+
+        factory.gotResolverResponse(
+            ([], [], []),
+            protocol=None, message=request, address=None
+        )
+
+        self.assertEqual([dns.Message(rCode=0, answer=True)], responses)
+
+
     def test_sendReplyWithAddress(self):
         """
         If L{server.DNSServerFactory.sendReply} is supplied with a protocol
