711332f35e1e5773a6480bf2ffdb76aaf8e8363f
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-03-28 02:01:15 +0000

    Move helpers to helpers module.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tls-handshake-notification-6024-3@42009 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/ssl_helpers.py
+++ twisted/test/ssl_helpers.py
@@ -2,21 +2,37 @@
 # See LICENSE for details.
 
 """
-Helper classes for twisted.test.test_ssl.
+Helper classes for TLS test cases in L{twisted.test.test_ssl},
+L{twisted.test.test_sslverify}, L{twisted.protocols.test.test_tls}, et.  al.
 
 They are in a separate module so they will not prevent test_ssl importing if
 pyOpenSSL is unavailable.
 """
 from __future__ import division, absolute_import
 
+import itertools
+
 from twisted.python.compat import nativeString
-from twisted.internet import ssl
+from twisted.internet import ssl, protocol
 from twisted.python.filepath import FilePath
+from twisted.test.iosim import connectedServerAndClient
+from twisted.protocols.tls import TLSMemoryBIOFactory
+
 
 from OpenSSL import SSL
+from OpenSSL.crypto import PKey, X509, TYPE_RSA, FILETYPE_PEM
 
 certPath = nativeString(FilePath(__file__.encode("utf-8")
                     ).sibling(b"server.pem").path)
+
+
+
+def counter(counter=itertools.count()):
+    """
+    Each time we're called, return the next integer in the natural numbers.
+    """
+    return next(counter)
+
 
 
 class ClientTLSContext(ssl.ClientContextFactory):
@@ -35,3 +51,143 @@ class ServerTLSContext:
         ctx.use_certificate_file(self.filename)
         ctx.use_privatekey_file(self.filename)
         return ctx
+
+
+def makeCertificate(**kw):
+    """
+    Create a certificate.
+    """
+    keypair = PKey()
+    keypair.generate_key(TYPE_RSA, 768)
+
+    certificate = X509()
+    certificate.gmtime_adj_notBefore(0)
+    certificate.gmtime_adj_notAfter(60 * 60 * 24 * 365) # One year
+    for xname in certificate.get_issuer(), certificate.get_subject():
+        for (k, v) in kw.items():
+            setattr(xname, k, nativeString(v))
+
+    certificate.set_serial_number(counter())
+    certificate.set_pubkey(keypair)
+    certificate.sign(keypair, "md5")
+
+    return keypair, certificate
+
+
+
+def certificatesForAuthorityAndServer():
+    """
+    Create a self-signed CA certificate and server certificate signed by the
+    CA.
+
+    @return: a 2-tuple of C{(certificate_authority_certificate,
+        server_certificate)}
+    @rtype: L{tuple} of (L{ssl.Certificate}, L{ssl.PrivateCertificate})
+    """
+    serverDN = ssl.DistinguishedName(commonName='example.com')
+    serverKey = ssl.KeyPair.generate()
+    serverCertReq = serverKey.certificateRequest(serverDN)
+
+    caDN = ssl.DistinguishedName(commonName='CA')
+    caKey= ssl.KeyPair.generate()
+    caCertReq = caKey.certificateRequest(caDN)
+    caSelfCertData = caKey.signCertificateRequest(
+            caDN, caCertReq, lambda dn: True, 516)
+    caSelfCert = caKey.newCertificate(caSelfCertData)
+
+    serverCertData = caKey.signCertificateRequest(
+            caDN, serverCertReq, lambda dn: True, 516)
+    serverCert = serverKey.newCertificate(serverCertData)
+    return caSelfCert, serverCert
+
+
+
+def loopbackTLSConnection(trustRoot, privateKeyFile, chainedCertFile=None):
+    """
+    Create a loopback TLS connection with the given trust and keys.
+
+    @param trustRoot: the C{trustRoot} argument for the client connection's
+        context.
+    @type trustRoot: L{twisted.internet._sslverify.IOpenSSLTrustRoot}
+
+    @param privateKeyFile: The name of the file containing the private key.
+    @type privateKeyFile: L{str} (native string; file name)
+
+    @param chainedCertFile: The name of the chained certificate file.
+    @type chainedCertFile: L{str} (native string; file name)
+
+    @return: 3-tuple of server-protocol, client-protocol, and L{IOPump}
+    @rtype: L{tuple}
+    """
+    class ContextFactory(object):
+        def getContext(self):
+            """
+            Create a context for the server side of the connection.
+
+            @return: an SSL context using a certificate and key.
+            @rtype: C{OpenSSL.SSL.Context}
+            """
+            ctx = SSL.Context(SSL.TLSv1_METHOD)
+            if chainedCertFile is not None:
+                ctx.use_certificate_chain_file(chainedCertFile)
+            ctx.use_privatekey_file(privateKeyFile)
+            # Let the test author know if they screwed something up.
+            ctx.check_privatekey()
+            return ctx
+
+    class GreetingServer(protocol.Protocol):
+        greeting = b"greetings!"
+        def connectionMade(self):
+            self.transport.write(self.greeting)
+
+    class ListeningClient(protocol.Protocol):
+        data = b''
+        lostReason = None
+        def dataReceived(self, data):
+            self.data += data
+        def connectionLost(self, reason):
+            self.lostReason = reason
+
+    serverOpts = ContextFactory()
+    clientOpts = ssl.CertificateOptions(trustRoot=trustRoot)
+
+    clientFactory = TLSMemoryBIOFactory(
+        clientOpts, isClient=True,
+        wrappedFactory=protocol.Factory.forProtocol(GreetingServer)
+    )
+    serverFactory = TLSMemoryBIOFactory(
+        serverOpts, isClient=False,
+        wrappedFactory=protocol.Factory.forProtocol(ListeningClient)
+    )
+
+    sProto, cProto, pump = connectedServerAndClient(
+        lambda: serverFactory.buildProtocol(None),
+        lambda: clientFactory.buildProtocol(None)
+    )
+    return sProto, cProto, pump
+
+
+
+def pathContainingDumpOf(testCase, *dumpables):
+    """
+    Create a temporary file to store some serializable-as-PEM objects in, and
+    return its name.
+
+    @param testCase: a test case to use for generating a temporary directory.
+    @type testCase: L{twisted.trial.unittest.TestCase}
+
+    @param dumpables: arguments are objects from pyOpenSSL with a C{dump}
+        method, taking a pyOpenSSL file-type constant, such as
+        L{OpenSSL.crypto.FILETYPE_PEM} or L{OpenSSL.crypto.FILETYPE_ASN1}.
+    @type dumpables: L{tuple} of L{object} with C{dump} method taking L{int}
+        returning L{bytes}
+
+    @return: the path to a file where all of the dumpables were dumped in PEM
+        format.
+    @rtype: L{str}
+    """
+    fname = testCase.mktemp()
+    with open(fname, "wb") as f:
+        for dumpable in dumpables:
+            f.write(dumpable.dump(FILETYPE_PEM))
+    return fname
--- twisted/test/test_sslverify.py
+++ twisted/test/test_sslverify.py
@@ -8,22 +8,19 @@ Tests for L{twisted.internet._sslverify}.
 
 from __future__ import division, absolute_import
 
-import itertools
-
 from zope.interface import implementer
 
 try:
     from OpenSSL import SSL
-    from OpenSSL.crypto import PKey, X509
-    from OpenSSL.crypto import TYPE_RSA, FILETYPE_PEM
     skipSSL = False
 except ImportError:
     skipSSL = "OpenSSL is required for SSL tests."
 
-from twisted.test.iosim import connectedServerAndClient
-
-from twisted.python.compat import nativeString
 from twisted.python.constants import NamedConstant, Names
+from twisted.test.ssl_helpers import makeCertificate
+from twisted.test.ssl_helpers import certificatesForAuthorityAndServer
+from twisted.test.ssl_helpers import pathContainingDumpOf
+from twisted.test.ssl_helpers import loopbackTLSConnection
 from twisted.python.filepath import FilePath
 
 from twisted.trial import unittest
@@ -35,7 +32,6 @@ from twisted.internet import interfaces
 if not skipSSL:
     from twisted.internet.ssl import platformTrust
     from twisted.internet import _sslverify as sslverify
-    from twisted.protocols.tls import TLSMemoryBIOFactory
 
 # A couple of static PEM-format certificates to be used by various tests.
 A_HOST_CERTIFICATE_PEM = """
@@ -82,168 +78,25 @@ A_PEER_CERTIFICATE_PEM = """
 
 
 
-def counter(counter=itertools.count()):
-    """
-    Each time we're called, return the next integer in the natural numbers.
-    """
-    return next(counter)
-
-
-
-def makeCertificate(**kw):
-    keypair = PKey()
-    keypair.generate_key(TYPE_RSA, 768)
-
-    certificate = X509()
-    certificate.gmtime_adj_notBefore(0)
-    certificate.gmtime_adj_notAfter(60 * 60 * 24 * 365) # One year
-    for xname in certificate.get_issuer(), certificate.get_subject():
-        for (k, v) in kw.items():
-            setattr(xname, k, nativeString(v))
-
-    certificate.set_serial_number(counter())
-    certificate.set_pubkey(keypair)
-    certificate.sign(keypair, "md5")
-
-    return keypair, certificate
-
-
-
-def certificatesForAuthorityAndServer():
-    """
-    Create a self-signed CA certificate and server certificate signed by
-    the CA.
-
-    @return: a 2-tuple of C{(certificate_authority_certificate,
-        server_certificate)}
-    @rtype: L{tuple} of (L{sslverify.Certificate},
-        L{sslverify.PrivateCertificate})
-    """
-    serverDN = sslverify.DistinguishedName(commonName='example.com')
-    serverKey = sslverify.KeyPair.generate()
-    serverCertReq = serverKey.certificateRequest(serverDN)
-
-    caDN = sslverify.DistinguishedName(commonName='CA')
-    caKey= sslverify.KeyPair.generate()
-    caCertReq = caKey.certificateRequest(caDN)
-    caSelfCertData = caKey.signCertificateRequest(
-            caDN, caCertReq, lambda dn: True, 516)
-    caSelfCert = caKey.newCertificate(caSelfCertData)
-
-    serverCertData = caKey.signCertificateRequest(
-            caDN, serverCertReq, lambda dn: True, 516)
-    serverCert = serverKey.newCertificate(serverCertData)
-    return caSelfCert, serverCert
-
-
-
-def loopbackTLSConnection(trustRoot, privateKeyFile, chainedCertFile=None):
-    """
-    Create a loopback TLS connection with the given trust and keys.
-
-    @param trustRoot: the C{trustRoot} argument for the client connection's
-        context.
-    @type trustRoot: L{sslverify.IOpenSSLTrustRoot}
-
-    @param privateKeyFile: The name of the file containing the private key.
-    @type privateKeyFile: L{str} (native string; file name)
-
-    @param chainedCertFile: The name of the chained certificate file.
-    @type chainedCertFile: L{str} (native string; file name)
-
-    @return: 3-tuple of server-protocol, client-protocol, and L{IOPump}
-    @rtype: L{tuple}
-    """
-    class ContextFactory(object):
-        def getContext(self):
-            """
-            Create a context for the server side of the connection.
-
-            @return: an SSL context using a certificate and key.
-            @rtype: C{OpenSSL.SSL.Context}
-            """
-            ctx = SSL.Context(SSL.TLSv1_METHOD)
-            if chainedCertFile is not None:
-                ctx.use_certificate_chain_file(chainedCertFile)
-            ctx.use_privatekey_file(privateKeyFile)
-            # Let the test author know if they screwed something up.
-            ctx.check_privatekey()
-            return ctx
-
-    class GreetingServer(protocol.Protocol):
-        greeting = b"greetings!"
-        def connectionMade(self):
-            self.transport.write(self.greeting)
-
-    class ListeningClient(protocol.Protocol):
-        data = b''
-        lostReason = None
-        def dataReceived(self, data):
-            self.data += data
-        def connectionLost(self, reason):
-            self.lostReason = reason
-
-    serverOpts = ContextFactory()
-    clientOpts = sslverify.OpenSSLCertificateOptions(trustRoot=trustRoot)
-
-    clientFactory = TLSMemoryBIOFactory(
-        clientOpts, isClient=True,
-        wrappedFactory=protocol.Factory.forProtocol(GreetingServer)
-    )
-    serverFactory = TLSMemoryBIOFactory(
-        serverOpts, isClient=False,
-        wrappedFactory=protocol.Factory.forProtocol(ListeningClient)
-    )
-
-    sProto, cProto, pump = connectedServerAndClient(
-        lambda: serverFactory.buildProtocol(None),
-        lambda: clientFactory.buildProtocol(None)
-    )
-    return sProto, cProto, pump
-
-
-
-def pathContainingDumpOf(testCase, *dumpables):
-    """
-    Create a temporary file to store some serializable-as-PEM objects in, and
-    return its name.
-
-    @param testCase: a test case to use for generating a temporary directory.
-    @type testCase: L{twisted.trial.unittest.TestCase}
-
-    @param dumpables: arguments are objects from pyOpenSSL with a C{dump}
-        method, taking a pyOpenSSL file-type constant, such as
-        L{OpenSSL.crypto.FILETYPE_PEM} or L{OpenSSL.crypto.FILETYPE_ASN1}.
-    @type dumpables: L{tuple} of L{object} with C{dump} method taking L{int}
-        returning L{bytes}
-
-    @return: the path to a file where all of the dumpables were dumped in PEM
-        format.
-    @rtype: L{str}
-    """
-    fname = testCase.mktemp()
-    with open(fname, "wb") as f:
-        for dumpable in dumpables:
-            f.write(dumpable.dump(FILETYPE_PEM))
-    return fname
-
-
-
 class DataCallbackProtocol(protocol.Protocol):
     def dataReceived(self, data):
         d, self.factory.onData = self.factory.onData, None
         if d is not None:
             d.callback(data)
 
+
     def connectionLost(self, reason):
         d, self.factory.onLost = self.factory.onLost, None
         if d is not None:
             d.errback(reason)
 
+
+
 class WritingProtocol(protocol.Protocol):
     byte = b'x'
     def connectionMade(self):
         self.transport.write(self.byte)
+
 
     def connectionLost(self, reason):
         self.factory.onLost.errback(reason)
