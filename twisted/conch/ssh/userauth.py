9570a8f4a8141592910e395d8a3b32bfd8ad463e
radix <radix@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-03-28 18:08:13 +0000

    Merge remove-string-keys-7054
    Author: alex
    Reviewer: radix
    Fixes: #7054
    
    Remove deprecated code that handled public and private
    keys in string format.
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@42023 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/ssh/userauth.py
+++ twisted/conch/ssh/userauth.py
@@ -9,7 +9,7 @@ Currently implemented authentication types are public-key and password.
 Maintainer: Paul Swartz
 """
 
-import struct, warnings
+import struct
 from twisted.conch import error, interfaces
 from twisted.conch.ssh import keys, transport, service
 from twisted.conch.ssh.common import NS, getNS
@@ -676,12 +676,6 @@ class SSHUserAuthClient(service.SSHService):
 
 
     def _cbGetPublicKey(self, publicKey):
-        if isinstance(publicKey, str):
-            warnings.warn("Returning a string from "
-                          "SSHUserAuthClient.getPublicKey() is deprecated "
-                          "since Twisted 9.0.  Return a keys.Key() instead.",
-                          DeprecationWarning)
-            publicKey = keys.Key.fromString(publicKey)
         if not isinstance(publicKey, keys.Key): # failure or None
             publicKey = None
         if publicKey is not None:
@@ -770,12 +764,6 @@ class SSHUserAuthClient(service.SSHService):
         @return: the signature
         @rtype: C{str}
         """
-        if not isinstance(privateKey, keys.Key):
-            warnings.warn("Returning a PyCrypto key object from "
-                          "SSHUserAuthClient.getPrivateKey() is deprecated "
-                          "since Twisted 9.0.  Return a keys.Key() instead.",
-                          DeprecationWarning)
-            privateKey = keys.Key(privateKey)
         return privateKey.sign(signData)
 
 
