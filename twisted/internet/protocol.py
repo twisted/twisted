71c799cdd0b5a5f0fbe9cf54581290cfa2888aac
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-03-28 02:02:37 +0000

    adding stuff is fine, adding stuff is cool, adding stuff is not a problem
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tls-handshake-notification-6024-3@42017 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/protocol.py
+++ twisted/internet/protocol.py
@@ -522,28 +522,38 @@ class Protocol(BaseProtocol):
         return self.__class__.__name__
 
 
+    def handshakeCompleted(self):
+        """
+        Called when the transport's TLS handshake has completed.
+        """
+
+
     def dataReceived(self, data):
-        """Called whenever data is received.
+        """
+        Called whenever data is received.
 
         Use this method to translate to a higher-level message.  Usually, some
         callback will be made upon the receipt of each complete protocol
         message.
 
         @param data: a string of indeterminate length.  Please keep in mind
-            that you will probably need to buffer some data, as partial
-            (or multiple) protocol messages may be received!  I recommend
-            that unit tests for protocols call through to this method with
-            differing chunk sizes, down to one byte at a time.
+            that you will probably need to buffer some data, as partial (or
+            multiple) protocol messages may be received!  I recommend that unit
+            tests for protocols call through to this method with differing
+            chunk sizes, down to one byte at a time.
         """
 
-    def connectionLost(self, reason=connectionDone):
-        """Called when the connection is shut down.
 
-        Clear any circular references here, and any external references
-        to this Protocol.  The connection has been closed.
+    def connectionLost(self, reason=connectionDone):
+        """
+        Called when the connection is shut down.
+
+        Clear any circular references here, and any external references to this
+        Protocol.  The connection has been closed.
 
         @type reason: L{twisted.python.failure.Failure}
         """
+
 
 
 @implementer(interfaces.IConsumer)
