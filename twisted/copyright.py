02815d633c46b23b452b083772f77487b5da07a3
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-09 06:01:40 +0000

    changing years
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-14.0.0-7039-3@42155 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/copyright.py
+++ twisted/copyright.py
@@ -12,7 +12,7 @@ from twisted import __version__ as version, version as longversion
 longversion = str(longversion)
 
 copyright="""\
-Copyright (c) 2001-2013 Twisted Matrix Laboratories.
+Copyright (c) 2001-2014 Twisted Matrix Laboratories.
 See LICENSE for details."""
 
 disclaimer='''
