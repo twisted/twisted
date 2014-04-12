fd49e09f6966da811b5778eb42ff3cecd950dc81
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-12 08:29:23 +0000

    some pyflakes fixes while I'm in here
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tls-abstraction-7098@42159 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/tcp.py
+++ twisted/internet/tcp.py
@@ -50,16 +50,14 @@ else:
 
 
 if platformType == 'win32':
-    # no such thing as WSAEPERM or error code 10001 according to winsock.h or MSDN
+    # no such thing as WSAEPERM or error code 10001 according to winsock.h or
+    # MSDN
     EPERM = object()
     from errno import WSAEINVAL as EINVAL
     from errno import WSAEWOULDBLOCK as EWOULDBLOCK
     from errno import WSAEINPROGRESS as EINPROGRESS
     from errno import WSAEALREADY as EALREADY
-    from errno import WSAECONNRESET as ECONNRESET
     from errno import WSAEISCONN as EISCONN
-    from errno import WSAENOTCONN as ENOTCONN
-    from errno import WSAEINTR as EINTR
     from errno import WSAENOBUFS as ENOBUFS
     from errno import WSAEMFILE as EMFILE
     # No such thing as WSAENFILE, either.
@@ -76,10 +74,7 @@ else:
     from errno import EWOULDBLOCK
     from errno import EINPROGRESS
     from errno import EALREADY
-    from errno import ECONNRESET
     from errno import EISCONN
-    from errno import ENOTCONN
-    from errno import EINTR
     from errno import ENOBUFS
     from errno import EMFILE
     from errno import ENFILE
