188c3b8bb213c1576dac8109759253bc94babebd
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-14 06:53:16 +0000

    Rewrap Tkinter imports
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/pyflakes-spread-6989@42199 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/spread/ui/tktree.py
+++ twisted/spread/ui/tktree.py
@@ -16,16 +16,7 @@ What I want it to look like:
 """
 
 import os
-from Tkinter import (
-    END,
-    Listbox,
-    Tk,
-    Scrollbar,
-    LEFT,
-    BOTH,
-    RIGHT,
-    Y,
-    )
+from Tkinter import END, Listbox, Tk, Scrollbar, LEFT, BOTH, RIGHT, Y
 
 
 class Node:
--- twisted/spread/ui/tkutil.py
+++ twisted/spread/ui/tkutil.py
@@ -1,27 +1,11 @@
-
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
 """Utilities for building L{PB<twisted.spread.pb>} clients with L{Tkinter}.
 """
 from Tkinter import (
-    ACTIVE,
-    Button,
-    Canvas,
-    E,
-    END,
-    Entry,
-    Frame,
-    Label,
-    LEFT,
-    Listbox,
-    mainloop,
-    N,
-    S,
-    StringVar,
-    Toplevel,
-    Tk,
-    W,)
+    ACTIVE, Button, Canvas, E, END, Entry, Frame, Label, LEFT, Listbox,
+    mainloop, N, S, StringVar, Toplevel, Tk, W)
 from tkSimpleDialog import _QueryString
 from tkFileDialog import _Dialog
 from twisted.spread import pb
