8f6f32354a99fa3323215b6571a1755ee2f9d4ca
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-16 15:29:25 +0000

    Fix the documentation issues with `flagFunction` and a number of issues with the new tests for it.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/twisted.python.usage-python3-7038-2@42382 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/usage.py
+++ twisted/python/usage.py
@@ -948,15 +948,36 @@ def docMakeChunks(optList, width=80):
     return optChunks
 
 
+
 def flagFunction(method, name=None):
+    """
+    Determine whether a function is an optional handler for a I{flag} or an
+    I{option}.
+
+    A I{flag} handler takes no additional arguments.  It is used to handle
+    command-line arguments like I{--nodaemon}.
+
+    An I{option} handler takes one argument.  It is used to handle command-line
+    arguments like I{--path=/foo/bar}.
+
+    @param method: The bound method object to inspect.
+
+    @param name: The name of the option for which the function is a handle.
+    @type name: L{str}
+
+    @raise UsageError: If the method takes more than one argument.
+
+    @return: If the method is a flag handler, return C{True}.  Otherwise return
+        C{False}.
+    """
     reqArgs = len(inspect.getargspec(method).args)
     if reqArgs > 2:
         raise UsageError('Invalid Option function for %s' %
                          (name or method.__name__))
     if reqArgs == 2:
-        # argName = method.im_func.func_code.co_varnames[1]
-        return 0
-    return 1
+        return False
+    return True
+
 
 
 def portCoerce(value):
@@ -968,5 +989,3 @@ def portCoerce(value):
         raise ValueError("Port number not in range: %s" % (value,))
     return value
 portCoerce.coerceDoc = "Must be an int between 0 and 65535."
-
-
--- twisted/test/test_usage.py
+++ twisted/test/test_usage.py
@@ -614,12 +614,13 @@ class CompleterNotImplementedTestCase(unittest.TestCase):
                               None, "bad_shell_type")
 
 
+
 class FlagFunctionTest(unittest.TestCase):
     """
     Tests for L{usage.flagFunction}.
     """
 
-    class SomeClass:
+    class SomeClass(object):
         """
         Dummy class for L{usage.flagFunction} tests.
         """
@@ -629,13 +630,11 @@ class FlagFunctionTest(unittest.TestCase):
 
             @param a: a useless argument to satisfy the function's signature.
             """
-            pass
 
         def noArg(self):
             """
             A no argument method to be tested by L{usage.flagFunction}.
             """
-            pass
 
         def manyArgs(self, a, b, c):
             """
@@ -645,23 +644,22 @@ class FlagFunctionTest(unittest.TestCase):
             @param b: a useless argument to satisfy the function's signature.
             @param c: a useless argument to satisfy the function's signature.
             """
-            pass
 
 
     def test_hasArg(self):
         """
-        L{usage.flagFunction} returns a truth value if the method checked allows
+        L{usage.flagFunction} returns C{False} if the method checked allows
         exactly one argument.
         """
-        self.assertEqual(0, usage.flagFunction(self.SomeClass().oneArg))
+        self.assertIs(False, usage.flagFunction(self.SomeClass().oneArg))
 
 
     def test_noArg(self):
         """
-        L{usage.flagFunction} returns a false value if the method checked allows
+        L{usage.flagFunction} returns C{True} if the method checked allows
         exactly no argument.
         """
-        self.assertEqual(1, usage.flagFunction(self.SomeClass().noArg))
+        self.assertIs(True, usage.flagFunction(self.SomeClass().noArg))
 
 
     def test_tooManyArguments(self):
@@ -669,27 +667,21 @@ class FlagFunctionTest(unittest.TestCase):
         L{usage.flagFunction} raises L{usage.UsageError} if the method checked
         allows more than one argument.
         """
-        try:
-            usage.flagFunction(self.SomeClass().manyArgs)
-        except usage.UsageError as e:
-            self.assertEqual("Invalid Option function for manyArgs", str(e))
-            return
-
-        self.fail("Didn't raise UsageError")
+        exc = self.assertRaises(
+            usage.UsageError, usage.flagFunction, self.SomeClass().manyArgs)
+        self.assertEqual("Invalid Option function for manyArgs", str(exc))
 
 
     def test_tooManyArgumentsAndSpecificErrorMessage(self):
         """
-        L{usage.flagFunction} allows to specify the name of the method in its
-        error message if the method allows too many arguments.
+        L{usage.flagFunction} uses the given method name in the error message
+        raised when the method allows too many arguments.
         """
-        try:
-            usage.flagFunction(self.SomeClass().manyArgs, "flubuduf")
-        except usage.UsageError as e:
-            self.assertEqual("Invalid Option function for flubuduf", str(e))
-            return
+        exc = self.assertRaises(
+            usage.UsageError,
+            usage.flagFunction, self.SomeClass().manyArgs, "flubuduf")
+        self.assertEqual("Invalid Option function for flubuduf", str(exc))
 
-        self.fail("Didn't raise UsageError")
 
 
 class OptionsInternalTest(unittest.TestCase):
