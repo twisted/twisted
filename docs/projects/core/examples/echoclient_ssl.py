51c54bf4594bdc2e731b50c8aae6422b1c0f05e5
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-19 06:53:46 +0000

    Update examples to have more modern APIs, and also re-use more code from other examples.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tls-abstraction-7098@42474 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/projects/core/examples/echoclient_ssl.py
+++ docs/projects/core/examples/echoclient_ssl.py
@@ -2,46 +2,25 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
+from twisted.internet import ssl, task, protocol, endpoints, defer
+from twisted.python.modules import getModule
+
+import echoclient
+
+@defer.inlineCallbacks
+def main(reactor):
+    factory = protocol.Factory.forProtocol(echoclient.EchoClient)
+    certData = getModule(__name__).filePath.sibling('public.pem').getContent()
+    authority = ssl.Certificate.loadPEM(certData)
+    settings = ssl.settingsForClientTLS(u'example.com')
+    endpoint = endpoints.SSL4ClientEndpoint(reactor, 'localhost', 8000,
+                                            settings)
+    echoClient = yield endpoint.connect(factory)
+
+    done = defer.Deferred()
+    echoClient.connectionLost = lambda reason: done.callback(None)
+    yield done
+
 if __name__ == '__main__':
     import echoclient_ssl
-    raise SystemExit(echoclient_ssl.main())
-
-from twisted.internet.protocol import ClientFactory
-from twisted.protocols.basic import LineReceiver
-from twisted.internet import ssl, reactor
-
-
-class EchoClient(LineReceiver):
-    end="Bye-bye!"
-    def connectionMade(self):
-        self.sendLine("Hello, world!")
-        self.sendLine("What a fine day it is.")
-        self.sendLine(self.end)
-
-    def connectionLost(self, reason):
-        print 'connection lost (protocol)'
-
-    def lineReceived(self, line):
-        print "receive:", line
-        if line==self.end:
-            self.transport.loseConnection()
-
-class EchoClientFactory(ClientFactory):
-    protocol = EchoClient
-
-    def clientConnectionFailed(self, connector, reason):
-        print 'connection failed:', reason.getErrorMessage()
-        reactor.stop()
-
-    def clientConnectionLost(self, connector, reason):
-        print 'connection lost:', reason.getErrorMessage()
-        reactor.stop()
-
-def main():
-    factory = EchoClientFactory()
-    with open('public.pem') as certFile:
-        authority = ssl.Certificate.loadPEM(certFile.read())
-    reactor.connectSSL('localhost', 8000, factory,
-                       ssl.settingsForClientTLS(u'example.com',
-                                                authority))
-    reactor.run()
+    task.react(echoclient_ssl.main)
