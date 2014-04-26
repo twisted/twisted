198b06ca692c184f0ff639c8867932dc7ec4aa7f
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-26 03:32:27 +0000

    pre3
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/releases/release-14.0.0-7039-3@42556 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/_version.py
+++ twisted/words/_version.py
@@ -8,4 +8,4 @@ Provides Twisted version information.
 """
 
 from twisted.python import versions
-version = versions.Version('twisted.words', 14, 0, 0, prerelease=2)
+version = versions.Version('twisted.words', 14, 0, 0, prerelease=3)
