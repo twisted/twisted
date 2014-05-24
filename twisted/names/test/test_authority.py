6864cd293e26f300d4afa166be8d58c219b6c2d8
rwall <rwall@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-05-24 13:26:57 +0000

    Add tests for MX and NS records
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/memory-authority-4686@42725 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/names/test/test_authority.py
+++ twisted/names/test/test_authority.py
@@ -58,36 +58,96 @@ class MemoryAuthorityTestsMixin(object):
         )
 
 
+    def assertAdditionalRecords(self, expectedRecords, **suppliedArguments):
+        """
+        L{MemoryAuthority._additionalRecords} yields the A and AAAA records
+        corresponding to the domain name value of a supplied CNAME record.
+        """
+        expectedHeaders = []
+        for name, records in expectedRecords.items():
+            for record in records:
+                expectedHeaders.append(
+                    dns.RRHeader(name=name,
+                                 type=record.TYPE,
+                                 payload=record,
+                                 auth=True)
+                )
+        actualRecords = list(
+            self.factory(records=expectedRecords)._additionalRecords(
+                **suppliedArguments)
+        )
+
+        self.assertEqual(expectedHeaders, actualRecords)
+
+
     def test_additionalRecordsCNAME(self):
         """
         L{MemoryAuthority._additionalRecords} yields the A and AAAA records
         corresponding to the domain name value of a supplied CNAME record.
         """
-        expectedRecords = [
-            dns.Record_A(address="192.0.2.100"),
-            dns.Record_AAAA(address="::1")
-        ]
-        expectedName = b"example.com"
-        expectedHeaders = [
-            dns.RRHeader(name=expectedName, type=r.TYPE, payload=r, auth=True)
-            for r in expectedRecords
-        ]
-        actualRecords = list(
-            self.factory(
-                records={expectedName: expectedRecords}
-            )._additionalRecords(
-                answer=[
-                    dns.RRHeader(
-                        type=dns.CNAME,
-                        payload=dns.Record_CNAME(name=expectedName)
-                    )
-                ],
-                authority=[],
-                ttl=0
-            )
+        self.assertAdditionalRecords(
+            expectedRecords={
+                b'example.com': [
+                    dns.Record_A(address="192.0.2.100"),
+                    dns.Record_AAAA(address="::1")
+                ]
+            },
+            answer=[
+                dns.RRHeader(
+                    type=dns.CNAME,
+                    payload=dns.Record_CNAME(name=b'example.com')
+                )
+            ],
+            authority=[],
+            ttl=0
         )
 
-        self.assertEqual(expectedHeaders, actualRecords)
+
+    def test_additionalRecordsNS(self):
+        """
+        L{MemoryAuthority._additionalRecords} yields the A and AAAA records
+        corresponding to the domain name value of a supplied NS record.
+        """
+        self.assertAdditionalRecords(
+            expectedRecords={
+                b'ns1.example.com': [
+                    dns.Record_A(address="192.0.2.100"),
+                    dns.Record_AAAA(address="::1")
+                ]
+            },
+            answer=[
+                dns.RRHeader(
+                    type=dns.NS,
+                    payload=dns.Record_NS(name=b'ns1.example.com')
+                )
+            ],
+            authority=[],
+            ttl=0
+        )
+
+
+    def test_additionalRecordsMX(self):
+        """
+        L{MemoryAuthority._additionalRecords} yields the A and AAAA records
+        corresponding to the domain name value of a supplied MX record.
+        """
+        self.assertAdditionalRecords(
+            expectedRecords={
+                b'mail.example.com': [
+                    dns.Record_A(address="192.0.2.100"),
+                    dns.Record_AAAA(address="::1")
+                ]
+            },
+            answer=[
+                dns.RRHeader(
+                    type=dns.MX,
+                    payload=dns.Record_MX(name=b'mail.example.com')
+                )
+            ],
+            authority=[],
+            ttl=0
+        )
+
 
 
 
