eb02783a5e1adaac5f574b9c456e766d77e7cb18
khorn <khorn@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-09-12 04:10:25 +0000

    Fix erroneous path in make.bat for building Sphinx docs.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/make-bat-path-fix-7542@43041 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/make.bat
+++ docs/make.bat
@@ -4,7 +4,7 @@ REM Command file for Sphinx documentation
 
 set SPHINXBUILD=sphinx-build
 set BUILDDIR=build
-set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
+set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
 )
