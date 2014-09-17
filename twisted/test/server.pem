twisted/twisted

commit e825cd0d30adbe493cbf52f0f73093511fd6188d

Merge ninja-in-ecdhe-6586
Author: hynek
Reviewer: lvh
Fixes: #6586

OpenSSLCertificateOptions now tries best-effort to support ECDHE for servers.

git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@41888 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

twisted/internet/_sslverify.py
@@ -795,6 +795,11 @@ class OpenSSLCertificateOptions(object):
             self._options |= self._OP_NO_TICKET
         self.dhParameters = dhParameters
 
+        try:
+            self._ecCurve = _OpenSSLECCurve(_defaultCurveName)
+        except NotImplementedError:
+            self._ecCurve = None
+
         if acceptableCiphers is None:
             acceptableCiphers = defaultCiphers
         # This needs to run when method and _options are finalized.
@@ -877,7 +882,82 @@ class OpenSSLCertificateOptions(object):
             ctx.load_tmp_dh(self.dhParameters._dhFile.path)
         ctx.set_cipher_list(nativeString(self._cipherString))
 
+        if self._ecCurve is not None:
+            try:
+                self._ecCurve.addECKeyToContext(ctx)
+            except BaseException:
+                pass  # ECDHE support is best effort only.
+
         return ctx
+
+
+
+class _OpenSSLECCurve(FancyEqMixin, object):
+    """
+    A private representation of an OpenSSL ECC curve.
+    """
+    compareAttributes = ("snName", )
+
+    def __init__(self, snName):
+        """
+        @param snName: The name of the curve as used by C{OBJ_sn2nid}.
+        @param snName: L{unicode}
+
+        @raises NotImplementedError: If ECC support is not available.
+        @raises ValueError: If C{snName} is not a supported curve.
+        """
+        self.snName = nativeString(snName)
+
+        # As soon as pyOpenSSL supports ECDHE directly, attempt to use its
+        # APIs first.  See #7033.
+
+        # If pyOpenSSL is based on cryptography.io (0.14+), we use its
+        # bindings directly to set the ECDHE curve.
+        try:
+            binding = self._getBinding()
+            self._lib = binding.lib
+            self._ffi = binding.ffi
+            self._nid = self._lib.OBJ_sn2nid(self.snName)
+            if self._nid == self._lib.NID_undef:
+                raise ValueError("Unknown ECC curve.")
+        except AttributeError:
+            raise NotImplementedError(
+                "This version of pyOpenSSL does not support ECC."
+            )
+
+
+    def _getBinding(self):
+        """
+        Attempt to get cryptography's binding instance.
+
+        @raises NotImplementedError: If underlying pyOpenSSL is not based on
+            cryptography.
+
+        @return: cryptograpy bindings.
+        @rtype: C{cryptography.hazmat.bindings.openssl.Binding}
+        """
+        try:
+            from OpenSSL._util import binding
+            return binding
+        except ImportError:
+            raise NotImplementedError(
+                "This version of pyOpenSSL does not support ECC."
+            )
+
+
+    def addECKeyToContext(self, context):
+        """
+        Add an temporary EC key to C{context}.
+
+        @param context: The context to add a key to.
+        @type context: L{OpenSSL.SSL.Context}
+        """
+        ecKey = self._lib.EC_KEY_new_by_curve_name(self._nid)
+        if ecKey == self._ffi.NULL:
+            raise EnvironmentError("EC key creation failed.")
+
+        self._lib.SSL_CTX_set_tmp_ecdh(context._context, ecKey)
+        self._lib.EC_KEY_free(ecKey)
 
 
 
@@ -996,6 +1076,7 @@ defaultCiphers = OpenSSLAcceptableCiphers.fromOpenSSLCipherString(
     "ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:"
     "DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS"
 )
+_defaultCurveName = u"prime256v1"
 
 
 

twisted/test/test_sslverify.py
@@ -490,6 +490,58 @@ class OpenSSLOptions(unittest.TestCase):
         )
 
 
+    def test_ecDoesNotBreakConstructor(self):
+        """
+        Missing ECC does not break the constructor and sets C{_ecCurve} to
+        L{None}.
+        """
+        def raiser(self):
+            raise NotImplementedError
+        self.patch(sslverify._OpenSSLECCurve, "_getBinding", raiser)
+
+        opts = sslverify.OpenSSLCertificateOptions(
+            privateKey=self.sKey,
+            certificate=self.sCert,
+        )
+        self.assertIs(None, opts._ecCurve)
+
+
+    def test_ecNeverBreaksGetContext(self):
+        """
+        ECDHE support is best effort only and errors are ignored.
+        """
+        opts = sslverify.OpenSSLCertificateOptions(
+            privateKey=self.sKey,
+            certificate=self.sCert,
+        )
+        opts._ecCurve = object()
+        ctx = opts.getContext()
+        self.assertIsInstance(ctx, SSL.Context)
+
+
+    def test_ecSuccessWithRealBindings(self):
+        """
+        Integration test that checks the positive code path to ensure that we
+        use the API properly.
+        """
+        try:
+            defaultCurve = sslverify._OpenSSLECCurve(
+                sslverify._defaultCurveName
+            )
+        except NotImplementedError:
+            raise unittest.SkipTest(
+                "Underlying pyOpenSSL is not based on cryptography."
+            )
+        opts = sslverify.OpenSSLCertificateOptions(
+            privateKey=self.sKey,
+            certificate=self.sCert,
+        )
+        self.assertEqual(defaultCurve, opts._ecCurve)
+        # Exercise positive code path.  getContext swallows errors so we do it
+        # explicitly by hand.
+        opts._ecCurve.addECKeyToContext(opts.getContext())
+
+
     def test_abbreviatingDistinguishedNames(self):
         """
         Check that abbreviations used in certificates correctly map to
@@ -1142,3 +1194,253 @@ class TestDiffieHellmanParameters(unittest.TestCase):
             self.filePath
         )
         self.assertEqual(self.filePath, params._dhFile)
+
+
+
+class FakeECKey(object):
+    """
+    An introspectable fake of a key.
+
+    @ivar _nid: A free form nid.
+    """
+    def __init__(self, nid):
+        self._nid = nid
+
+
+
+class FakeNID(object):
+    """
+    An introspectable fake of a NID.
+
+    @ivar _snName: A free form sn name.
+    """
+    def __init__(self, snName):
+        self._snName = snName
+
+
+
+class FakeLib(object):
+    """
+    An introspectable fake of cryptography's lib object.
+
+    @ivar _createdKey: A set of keys that have been created by this instance.
+    @type _createdKey: L{set} of L{FakeKey}
+
+    @cvar NID_undef: A symbolic constant for undefined NIDs.
+    @type NID_undef: L{FakeNID}
+    """
+    NID_undef = FakeNID("undef")
+
+    def __init__(self):
+        self._createdKeys = set()
+
+
+    def OBJ_sn2nid(self, snName):
+        """
+        Create a L{FakeNID} with C{snName} and return it.
+
+        @param snName: a free form name that gets passed to the constructor
+            of L{FakeNID}.
+
+        @return: a new L{FakeNID}.
+        @rtype: L{FakeNID}.
+        """
+        return FakeNID(snName)
+
+
+    def EC_KEY_new_by_curve_name(self, nid):
+        """
+        Create a L{FakeECKey}, save it to C{_createdKeys} and return it.
+
+        @param nid: an arbitrary object that is passed to the constructor of
+            L{FakeECKey}.
+
+        @return: a new L{FakeECKey}
+        @rtype: L{FakeECKey}
+        """
+        key = FakeECKey(nid)
+        self._createdKeys.add(key)
+        return key
+
+
+    def EC_KEY_free(self, key):
+        """
+        Remove C{key} from C{_createdKey}.
+
+        @param key: a key object to be freed; i.e. removed from
+            C{_createdKeys}.
+
+        @raises ValueError: If C{key} is not in C{_createdKeys} and thus not
+            created by us.
+        """
+        try:
+            self._createdKeys.remove(key)
+        except KeyError:
+            raise ValueError("Unallocated EC key attempted to free.")
+
+
+    def SSL_CTX_set_tmp_ecdh(self, ffiContext, key):
+        """
+        Does not do anything.
+
+        @param ffiContext: ignored
+        @param key: ignored
+        """
+
+
+
+class TestFakeLib(unittest.TestCase):
+    """
+    Tests for FakeLib
+    """
+    def test_objSn2Nid(self):
+        """
+        Returns a L{FakeNID} with correct name.
+        """
+        nid = FakeNID("test")
+        self.assertEqual("test", nid._snName)
+
+
+    def test_emptyKeys(self):
+        """
+        A new L{FakeLib} has an empty set for created keys.
+        """
+        self.assertEqual(set(), FakeLib()._createdKeys)
+
+
+    def test_newKey(self):
+        """
+        If a new key is created, it's added to C{_createdKeys}.
+        """
+        lib = FakeLib()
+        key = lib.EC_KEY_new_by_curve_name(FakeNID("name"))
+        self.assertEqual(set([key]), lib._createdKeys)
+
+
+    def test_freeUnknownKey(self):
+        """
+        Raise L{ValueError} if an unknown key is attempted to be freed.
+        """
+        key = FakeECKey(object())
+        self.assertRaises(
+            ValueError,
+            FakeLib().EC_KEY_free, key
+        )
+
+
+    def test_freeKnownKey(self):
+        """
+        Freeing an allocated key removes it from C{_createdKeys}.
+        """
+        lib = FakeLib()
+        key = lib.EC_KEY_new_by_curve_name(FakeNID("name"))
+        lib.EC_KEY_free(key)
+        self.assertEqual(set(), lib._createdKeys)
+
+
+
+class FakeFFI(object):
+    """
+    A fake of a cryptography's ffi object.
+
+    @cvar NULL: Symbolic constant for CFFI's NULL objects.
+    """
+    NULL = object()
+
+
+
+class FakeBinding(object):
+    """
+    A fake of cryptography's binding object.
+
+    @type lib: L{FakeLib}
+    @type ffi: L{FakeFFI}
+    """
+    def __init__(self, lib=None, ffi=None):
+        self.lib = lib or FakeLib()
+        self.ffi = ffi or FakeFFI()
+
+
+
+class TestECCurve(unittest.TestCase):
+    """
+    Tests for twisted.internet._sslverify.OpenSSLECCurve.
+    """
+    if skipSSL:
+        skip = skipSSL
+
+    def test_missingBinding(self):
+        """
+        Raise L{NotImplementedError} if pyOpenSSL is not based on cryptography.
+        """
+        def raiser(self):
+            raise NotImplementedError
+        self.patch(sslverify._OpenSSLECCurve, "_getBinding", raiser)
+        self.assertRaises(
+            NotImplementedError,
+            sslverify._OpenSSLECCurve, sslverify._defaultCurveName,
+        )
+
+
+    def test_nonECbinding(self):
+        """
+        Raise L{NotImplementedError} if pyOpenSSL is based on cryptography but
+        cryptography lacks required EC methods.
+        """
+        def raiser(self):
+            raise AttributeError
+        lib = FakeLib()
+        lib.OBJ_sn2nid = raiser
+        self.patch(sslverify._OpenSSLECCurve,
+                   "_getBinding",
+                   lambda self: FakeBinding(lib=lib))
+        self.assertRaises(
+            NotImplementedError,
+            sslverify._OpenSSLECCurve, sslverify._defaultCurveName,
+        )
+
+
+    def test_wrongName(self):
+        """
+        Raise L{ValueError} on unknown sn names.
+        """
+        lib = FakeLib()
+        lib.OBJ_sn2nid = lambda self: FakeLib.NID_undef
+        self.patch(sslverify._OpenSSLECCurve,
+                   "_getBinding",
+                   lambda self: FakeBinding(lib=lib))
+        self.assertRaises(
+            ValueError,
+            sslverify._OpenSSLECCurve, u"doesNotExist",
+        )
+
+
+    def test_keyFails(self):
+        """
+        Raise L{EnvironmentError} if key creation fails.
+        """
+        lib = FakeLib()
+        lib.EC_KEY_new_by_curve_name = lambda *a, **kw: FakeFFI.NULL
+        self.patch(sslverify._OpenSSLECCurve,
+                   "_getBinding",
+                   lambda self: FakeBinding(lib=lib))
+        curve = sslverify._OpenSSLECCurve(sslverify._defaultCurveName)
+        self.assertRaises(
+            EnvironmentError,
+            curve.addECKeyToContext, object()
+        )
+
+
+    def test_keyGetsFreed(self):
+        """
+        Don't leak a key when adding it to a context.
+        """
+        lib = FakeLib()
+        self.patch(sslverify._OpenSSLECCurve,
+                   "_getBinding",
+                   lambda self: FakeBinding(lib=lib))
+        curve = sslverify._OpenSSLECCurve(sslverify._defaultCurveName)
+        ctx = FakeContext(None)
+        ctx._context = None
+        curve.addECKeyToContext(ctx)
+        self.assertEqual(set(), lib._createdKeys)

twisted/topfiles/6586.feature
@@ -1,0 +1,1 @@
+twisted.internet.ssl.CertificateOptions now supports ECDHE for servers by default on pyOpenSSL 0.14 and later, if the underlying versions of cryptography.io and OpenSSL support it.

commit 802ba0fc26e33cab068d5978b068a4ec9a37905f

Merge failing-tls-tests-7370
Author: exarkun (sponsored by Hybrid Logic, Ltd.)
Reviewer: glyph
Fixes: #7370

Generate a new and larger RSA key (and associated self-signed certificate)
for use by a number of the TLS unit tests.  This causes them to pass when
the test suite is run using pyOpenSSL 0.14 or newer and an EC-supporting
version of OpenSSL.  The tests previously failed because the newer EC-using
TLS configuration introduced in r41888 requires this larger RSA key (the
old key was 512 bits, an unreasonably small size) to successfully complete
a handshake.

Note this change is unit tested (it is really only a fix to the unit tests)
but there is presently no continuous integration coverage for this case
because the continuous integration system is not configured to run the tests
against pyOpenSSL 0.14 in any cases.


git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@43114 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

twisted/internet/test/test_tls.py
@@ -11,6 +11,8 @@ __metaclass__ = type
 
 from zope.interface import implementer
 
+from twisted.python.compat import networkString
+from twisted.python.filepath import FilePath
 from twisted.internet.test.reactormixins import ReactorBuilder
 from twisted.internet.protocol import ServerFactory, ClientFactory, Protocol
 from twisted.internet.interfaces import (
@@ -51,47 +53,18 @@ class TLSMixin:
 
 
 class ContextGeneratingMixin(object):
-    _certificateText = (
-        "-----BEGIN CERTIFICATE-----\n"
-        "MIIDBjCCAm+gAwIBAgIBATANBgkqhkiG9w0BAQQFADB7MQswCQYDVQQGEwJTRzER\n"
-        "MA8GA1UEChMITTJDcnlwdG8xFDASBgNVBAsTC00yQ3J5cHRvIENBMSQwIgYDVQQD\n"
-        "ExtNMkNyeXB0byBDZXJ0aWZpY2F0ZSBNYXN0ZXIxHTAbBgkqhkiG9w0BCQEWDm5n\n"
-        "cHNAcG9zdDEuY29tMB4XDTAwMDkxMDA5NTEzMFoXDTAyMDkxMDA5NTEzMFowUzEL\n"
-        "MAkGA1UEBhMCU0cxETAPBgNVBAoTCE0yQ3J5cHRvMRIwEAYDVQQDEwlsb2NhbGhv\n"
-        "c3QxHTAbBgkqhkiG9w0BCQEWDm5ncHNAcG9zdDEuY29tMFwwDQYJKoZIhvcNAQEB\n"
-        "BQADSwAwSAJBAKy+e3dulvXzV7zoTZWc5TzgApr8DmeQHTYC8ydfzH7EECe4R1Xh\n"
-        "5kwIzOuuFfn178FBiS84gngaNcrFi0Z5fAkCAwEAAaOCAQQwggEAMAkGA1UdEwQC\n"
-        "MAAwLAYJYIZIAYb4QgENBB8WHU9wZW5TU0wgR2VuZXJhdGVkIENlcnRpZmljYXRl\n"
-        "MB0GA1UdDgQWBBTPhIKSvnsmYsBVNWjj0m3M2z0qVTCBpQYDVR0jBIGdMIGagBT7\n"
-        "hyNp65w6kxXlxb8pUU/+7Sg4AaF/pH0wezELMAkGA1UEBhMCU0cxETAPBgNVBAoT\n"
-        "CE0yQ3J5cHRvMRQwEgYDVQQLEwtNMkNyeXB0byBDQTEkMCIGA1UEAxMbTTJDcnlw\n"
-        "dG8gQ2VydGlmaWNhdGUgTWFzdGVyMR0wGwYJKoZIhvcNAQkBFg5uZ3BzQHBvc3Qx\n"
-        "LmNvbYIBADANBgkqhkiG9w0BAQQFAAOBgQA7/CqT6PoHycTdhEStWNZde7M/2Yc6\n"
-        "BoJuVwnW8YxGO8Sn6UJ4FeffZNcYZddSDKosw8LtPOeWoK3JINjAk5jiPQ2cww++\n"
-        "7QGG/g5NDjxFZNDJP1dGiLAxPW6JXwov4v0FmdzfLOZ01jDcgQQZqEpYlgpuI5JE\n"
-        "WUQ9Ho4EzbYCOQ==\n"
-        "-----END CERTIFICATE-----\n")
-
-    _privateKeyText = (
-        "-----BEGIN RSA PRIVATE KEY-----\n"
-        "MIIBPAIBAAJBAKy+e3dulvXzV7zoTZWc5TzgApr8DmeQHTYC8ydfzH7EECe4R1Xh\n"
-        "5kwIzOuuFfn178FBiS84gngaNcrFi0Z5fAkCAwEAAQJBAIqm/bz4NA1H++Vx5Ewx\n"
-        "OcKp3w19QSaZAwlGRtsUxrP7436QjnREM3Bm8ygU11BjkPVmtrKm6AayQfCHqJoT\n"
-        "ZIECIQDW0BoMoL0HOYM/mrTLhaykYAVqgIeJsPjvkEhTFXWBuQIhAM3deFAvWNu4\n"
-        "nklUQ37XsCT2c9tmNt1LAT+slG2JOTTRAiAuXDtC/m3NYVwyHfFm+zKHRzHkClk2\n"
-        "HjubeEgjpj32AQIhAJqMGTaZVOwevTXvvHwNEH+vRWsAYU/gbx+OQB+7VOcBAiEA\n"
-        "oolb6NMg/R3enNPvS1O4UU1H8wpaF77L4yiSWlE0p4w=\n"
-        "-----END RSA PRIVATE KEY-----\n")
-
+    import twisted
+    _pem = FilePath(
+        networkString(twisted.__file__)).sibling(b"test").child(b"server.pem")
+    del twisted
 
     def getServerContext(self):
         """
         Return a new SSL context suitable for use in a test server.
         """
+        pem = self._pem.getContent()
         cert = PrivateCertificate.load(
-            self._certificateText,
-            KeyPair.load(self._privateKeyText, FILETYPE_PEM),
-            FILETYPE_PEM)
+            pem, KeyPair.load(pem, FILETYPE_PEM), FILETYPE_PEM)
         return cert.options()
 
 

twisted/protocols/test/test_tls.py
@@ -403,8 +403,9 @@ class TLSMemoryBIOTests(TestCase):
             cert = sslClientProtocol.getPeerCertificate()
             self.assertIsInstance(cert, X509Type)
             self.assertEqual(
-                cert.digest('md5'),
-                b'9B:A4:AB:43:10:BE:82:AE:94:3E:6B:91:F2:F3:40:E8')
+                cert.digest('sha1'),
+                # openssl x509 -noout -sha1 -fingerprint -in server.pem
+                b'45:DD:FD:E2:BD:BF:8B:D0:00:B7:D2:7A:BB:20:F5:34:05:4B:15:80')
         handshakeDeferred.addCallback(cbHandshook)
         return handshakeDeferred
 

twisted/test/server.pem
@@ -1,36 +1,94 @@
+# coding: utf-8
+
+from inspect import getsource
+from datetime import datetime
+
+from OpenSSL.crypto import FILETYPE_PEM, TYPE_RSA, X509, PKey, dump_privatekey, dump_certificate
+
+key = PKey()
+key.generate_key(TYPE_RSA, 2048)
+
+cert = X509()
+issuer = cert.get_issuer()
+subject = cert.get_subject()
+
+for dn in [issuer, subject]:
+    dn.C = b"TR"
+    dn.ST = b"Çorum"
+    dn.L = b"Başmakçı"
+    dn.CN = b"localhost"
+    dn.O = b"Twisted Matrix Labs"
+    dn.OU = b"Automated Testing Authority"
+    dn.emailAddress = b"security@twistedmatrix.com"
+
+cert.set_serial_number(datetime.now().toordinal())
+cert.gmtime_adj_notBefore(0)
+cert.gmtime_adj_notAfter(60 * 60 * 24 * 365 * 100)
+
+cert.set_pubkey(key)
+cert.sign(key, b"sha1")
+
+import __main__
+source = getsource(__main__)
+source = source.split(b"\n" + b"-" * 5)[0].rsplit(b"\n", 1)[0]
+with open(b"server.pem", "wb") as fObj:
+    fObj.write(source)
+    fObj.write(b"\n")
+    fObj.write(b"'''\n")
+    fObj.write(dump_privatekey(FILETYPE_PEM, key))
+    fObj.write(dump_certificate(FILETYPE_PEM, cert))
+    fObj.write(b"'''\n")
+'''
+-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDGnXh/GMCLpNNI
+AIvfBWlPrRCLFWxd2ICLYSUq3/jwh31CppGKlfaSmUYnrMxnT4hg2f6gBqlmq1gK
+jQqDkqQQtHsSljRQF58NRFtz99w45jRmrGs+F5zoggJuyv7/lKXy/BXOc40NodIl
+qRuo/uhoPjeXaUNziwpRj4rByRdLwQ6MfRcfdZ0TpVJ4J7apJ66pZt85L63u8TZi
+AZdBgcowX6giola7kUUMG66bSi2X6sIFxdXHwzrWlFNnSbKqOqKlGrjmBHUYqRFr
+gLALKkaMpF30olHn7QLqJ2592hMFVkOQAzNr8Xb15mF27BuWFYd7P6TirsxCEd+9
+BFFbhl0vAgMBAAECggEAf1ndN3GBlIi9SL/A7+GiYwpPPz8fWxVFZxmFIXa3QlM+
+CAyR6dC6Z8mL6EiuT9f5VFCzKZzb5g8bxrgk87SFKojvGT3ikTB0NaeNFFDrjjhd
+hTAtG4U8gQFL2gqjcvG3bpQgz13cJc+K1ccXC0dXce/i6Vz/eQjANwfZKuRr4Y/p
+Ml3ZtkarT7cZ2TZRDYd643U2nfrNmeLA9wwwO5mgNYu7HSpI4idpVM+rzoS9ZbU1
+Jxhrdq7GELvnd+Ko3WHTSrkQyb53GtJaLFOSAMxQlZAbRGJMv2VtXSPe+NyA0kxO
+V4O9CJ2QL1Bxqk4MQicKGtGoIsFbfR/qYQZgNEuHoQKBgQDlqEYSGpEcyKNvCjZ+
+Dv5hFUT7GZ24MNRvMBdMHPJEkmxvNpwaNkOGXL6zmhDk5Y0HOrzNv64Llmg623y0
+pn9Vh3CvMKDlq5t8910BJhXzZAM9/E9ui/YuQsFwCdWJQFYVMZUIAdylxbDkg6Mf
+WzHkx82edwWgb7hOFJhEW3h51wKBgQDdZaofTKeSLUmxIbF1yqm4iX67wyhHFFZL
+RZLEcu35ZTJqz3TRpr9KpHCtq9J2gZbqo2Dvqznwfk+yUqT2gkibtk3qvmv/qHQt
+FGX5joWLD2E22UgGc4bzonTSQcxPfPYjNZa6Iv7koPgJqO1k3aYLG3mDMgI/Yurz
+3j8udCI8aQKBgQCe8uNkfky3PkqXfNsQTnJTYTQ4EpettuYg+oj1Xgdz3F/+bS6H
+mc0Mfr5ichyFQDdmrImmYaclT/ld2zGpmp8A5FfieOazbx2T1WTieaixpyPzT+Du
+IyLFC+D0GWpYr1WlH4cFXryHY5c14cIR3r1emcc/iSM00n4eVHS6wxBUSwKBgQCQ
+fTAI20slgD7uxjF90XTwhXNbcONReDlVSKOfZ+5dbCj6QkhYJv4C5czP2yowwyRO
+H7A82j+m75htpbgZVS+zx8eUIxByumqPnRdsRhJrje8BD88MvsrdKDIbomuHmOM+
+WVP0SLCXX2JhU4kS4gDikNy9vZ5v5cu2ul37oDlTuQKBgBl4Qs6Q0xUotlvy/fMf
+L3c6GUIeRsLyQCKFfKYiUNLHqqutBSYmsAlx3XOhcK4DZ7/gFAXO6mpYJlwXOdrI
+IMcSXuJvZDPAYMXOyZUTgNjPcbQ8t0by+EKPwnTReIBf1Y17vPDfisf5IEKZEWvM
+YDXg6cfx9R5QePjvZohlItOu
+-----END PRIVATE KEY-----
 -----BEGIN CERTIFICATE-----
-MIIDBjCCAm+gAwIBAgIBATANBgkqhkiG9w0BAQQFADB7MQswCQYDVQQGEwJTRzER
-MA8GA1UEChMITTJDcnlwdG8xFDASBgNVBAsTC00yQ3J5cHRvIENBMSQwIgYDVQQD
-ExtNMkNyeXB0byBDZXJ0aWZpY2F0ZSBNYXN0ZXIxHTAbBgkqhkiG9w0BCQEWDm5n
-cHNAcG9zdDEuY29tMB4XDTAwMDkxMDA5NTEzMFoXDTAyMDkxMDA5NTEzMFowUzEL
-MAkGA1UEBhMCU0cxETAPBgNVBAoTCE0yQ3J5cHRvMRIwEAYDVQQDEwlsb2NhbGhv
-c3QxHTAbBgkqhkiG9w0BCQEWDm5ncHNAcG9zdDEuY29tMFwwDQYJKoZIhvcNAQEB
-BQADSwAwSAJBAKy+e3dulvXzV7zoTZWc5TzgApr8DmeQHTYC8ydfzH7EECe4R1Xh
-5kwIzOuuFfn178FBiS84gngaNcrFi0Z5fAkCAwEAAaOCAQQwggEAMAkGA1UdEwQC
-MAAwLAYJYIZIAYb4QgENBB8WHU9wZW5TU0wgR2VuZXJhdGVkIENlcnRpZmljYXRl
-MB0GA1UdDgQWBBTPhIKSvnsmYsBVNWjj0m3M2z0qVTCBpQYDVR0jBIGdMIGagBT7
-hyNp65w6kxXlxb8pUU/+7Sg4AaF/pH0wezELMAkGA1UEBhMCU0cxETAPBgNVBAoT
-CE0yQ3J5cHRvMRQwEgYDVQQLEwtNMkNyeXB0byBDQTEkMCIGA1UEAxMbTTJDcnlw
-dG8gQ2VydGlmaWNhdGUgTWFzdGVyMR0wGwYJKoZIhvcNAQkBFg5uZ3BzQHBvc3Qx
-LmNvbYIBADANBgkqhkiG9w0BAQQFAAOBgQA7/CqT6PoHycTdhEStWNZde7M/2Yc6
-BoJuVwnW8YxGO8Sn6UJ4FeffZNcYZddSDKosw8LtPOeWoK3JINjAk5jiPQ2cww++
-7QGG/g5NDjxFZNDJP1dGiLAxPW6JXwov4v0FmdzfLOZ01jDcgQQZqEpYlgpuI5JE
-WUQ9Ho4EzbYCOQ==
+MIID8DCCAtgCAws5AzANBgkqhkiG9w0BAQUFADCBuzELMAkGA1UEBhMCVFIxDjAM
+BgNVBAgUBcdvcnVtMRkwFwYDVQQHHhAAQgBhAV8AbQBhAGsA5wExMRIwEAYDVQQD
+Ewlsb2NhbGhvc3QxHDAaBgNVBAoTE1R3aXN0ZWQgTWF0cml4IExhYnMxJDAiBgNV
+BAsTG0F1dG9tYXRlZCBUZXN0aW5nIEF1dGhvcml0eTEpMCcGCSqGSIb3DQEJARYa
+c2VjdXJpdHlAdHdpc3RlZG1hdHJpeC5jb20wIBcNMTQwOTE2MDE0MjQ2WhgPMjEx
+NDA4MjMwMTQyNDZaMIG7MQswCQYDVQQGEwJUUjEOMAwGA1UECBQFx29ydW0xGTAX
+BgNVBAceEABCAGEBXwBtAGEAawDnATExEjAQBgNVBAMTCWxvY2FsaG9zdDEcMBoG
+A1UEChMTVHdpc3RlZCBNYXRyaXggTGFiczEkMCIGA1UECxMbQXV0b21hdGVkIFRl
+c3RpbmcgQXV0aG9yaXR5MSkwJwYJKoZIhvcNAQkBFhpzZWN1cml0eUB0d2lzdGVk
+bWF0cml4LmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMadeH8Y
+wIuk00gAi98FaU+tEIsVbF3YgIthJSrf+PCHfUKmkYqV9pKZRieszGdPiGDZ/qAG
+qWarWAqNCoOSpBC0exKWNFAXnw1EW3P33DjmNGasaz4XnOiCAm7K/v+UpfL8Fc5z
+jQ2h0iWpG6j+6Gg+N5dpQ3OLClGPisHJF0vBDox9Fx91nROlUngntqknrqlm3zkv
+re7xNmIBl0GByjBfqCKiVruRRQwbrptKLZfqwgXF1cfDOtaUU2dJsqo6oqUauOYE
+dRipEWuAsAsqRoykXfSiUeftAuonbn3aEwVWQ5ADM2vxdvXmYXbsG5YVh3s/pOKu
+zEIR370EUVuGXS8CAwEAATANBgkqhkiG9w0BAQUFAAOCAQEAK+HqbIUN6qHYYQZw
+7qqsTJWni8NOfK3aguyKAPcdCPP2DCZ6zlxxkUWL57gvsohyDu8Nr9iSI6wePjmI
+cN9eCZdc6mD9kYW4qBYhh2T48TOhEEW7zO6bWQUqWohHW+bG+GfrHnvxIx56OC2B
+eDS2djvvScYm45etlHprfrVEDIssh956O6qJCySax3D2w+i8YanXji1EbS61XTDw
+CMxDdWYmd2MDARRwlMcfcUIfKZUGl5NmqpnOx+H5MyAGwt86s647GMYZborQh+Mj
+tNHVpyKf/a8/HjqP1sCOrjCPZIjP6Qp5j4gifAjUStNmCgaBe7CpFtBSLnHqb4o/
+gU7u1w==
 -----END CERTIFICATE-----
------BEGIN RSA PRIVATE KEY-----
-MIIBPAIBAAJBAKy+e3dulvXzV7zoTZWc5TzgApr8DmeQHTYC8ydfzH7EECe4R1Xh
-5kwIzOuuFfn178FBiS84gngaNcrFi0Z5fAkCAwEAAQJBAIqm/bz4NA1H++Vx5Ewx
-OcKp3w19QSaZAwlGRtsUxrP7436QjnREM3Bm8ygU11BjkPVmtrKm6AayQfCHqJoT
-ZIECIQDW0BoMoL0HOYM/mrTLhaykYAVqgIeJsPjvkEhTFXWBuQIhAM3deFAvWNu4
-nklUQ37XsCT2c9tmNt1LAT+slG2JOTTRAiAuXDtC/m3NYVwyHfFm+zKHRzHkClk2
-HjubeEgjpj32AQIhAJqMGTaZVOwevTXvvHwNEH+vRWsAYU/gbx+OQB+7VOcBAiEA
-oolb6NMg/R3enNPvS1O4UU1H8wpaF77L4yiSWlE0p4w=
------END RSA PRIVATE KEY-----
------BEGIN CERTIFICATE REQUEST-----
-MIIBDTCBuAIBADBTMQswCQYDVQQGEwJTRzERMA8GA1UEChMITTJDcnlwdG8xEjAQ
-BgNVBAMTCWxvY2FsaG9zdDEdMBsGCSqGSIb3DQEJARYObmdwc0Bwb3N0MS5jb20w
-XDANBgkqhkiG9w0BAQEFAANLADBIAkEArL57d26W9fNXvOhNlZzlPOACmvwOZ5Ad
-NgLzJ1/MfsQQJ7hHVeHmTAjM664V+fXvwUGJLziCeBo1ysWLRnl8CQIDAQABoAAw
-DQYJKoZIhvcNAQEEBQADQQA7uqbrNTjVWpF6By5ZNPvhZ4YdFgkeXFVWi5ao/TaP
-Vq4BG021fJ9nlHRtr4rotpgHDX1rr+iWeHKsx4+5DRSy
------END CERTIFICATE REQUEST-----
+'''

twisted/test/test_tcp.py
@@ -14,7 +14,7 @@ from zope.interface import implementer
 
 from twisted.trial import unittest
 
-from twisted.python.log import msg
+from twisted.python.log import msg, err
 from twisted.internet import protocol, reactor, defer, interfaces
 from twisted.internet import error
 from twisted.internet.address import IPv4Address
@@ -1193,12 +1193,16 @@ class ProperlyCloseFilesMixin:
             cleaned up.
             """
             client, server = result
-            client.lostConnectionReason.trap(error.ConnectionClosed)
-            server.lostConnectionReason.trap(error.ConnectionClosed)
+            if not client.lostConnectionReason.check(error.ConnectionClosed):
+                err(client.lostConnectionReason,
+                    "Client lost connection for unexpected reason")
+            if not server.lostConnectionReason.check(error.ConnectionClosed):
+                err(server.lostConnectionReason,
+                    "Server lost connection for unexpected reason")
             expectedErrorCode = self.getHandleErrorCode()
-            err = self.assertRaises(
+            exception = self.assertRaises(
                 self.getHandleExceptionType(), client.handle.send, b'bytes')
-            self.assertEqual(err.args[0], expectedErrorCode)
+            self.assertEqual(exception.args[0], expectedErrorCode)
         clientDeferred.addCallback(clientDisconnected)
 
         def cleanup(passthrough):