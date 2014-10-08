1d8662d334ef3adbfb07957135b3f58775e42e59
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-10-08 19:05:52 +0000

    test limiting additional thread creation
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/desynchronize-2673@43270 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/threads/test/test_team.py
+++ twisted/threads/test/test_team.py
@@ -99,6 +99,17 @@ class TeamTests(SynchronousTestCase):
         self.assertEqual(len(self.workerPerformers), 5)
 
 
+    def test_growCreateLimit(self):
+        """
+        L{Team.grow} increases the number of available idle workers until the
+        C{createWorker} callable starts returning None.
+        """
+        self.noMoreWorkers = lambda: len(self.allWorkersEver) >= 3
+        self.team.grow(5)
+        self.performAllOutstandingWork()
+        self.assertEqual(len(self.workerPerformers), 3)
+
+
     def test_shrinkQuitsWorkers(self):
         """
         L{Team.shrink} will quit workers.
