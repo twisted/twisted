d0f17b6c994a4ebae500354e0a455fe7e1a89013
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-10-10 08:30:15 +0000

    Back to the original point:
    What exactly *does* this test do, anyway?
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/desynchronize-2673@43317 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_threadpool.py
+++ twisted/test/test_threadpool.py
@@ -544,8 +544,20 @@ class RaceConditionTestCase(unittest.SynchronousTestCase):
 
     def test_synchronization(self):
         """
-        Test a race condition: ensure that actions run in the pool synchronize
-        with actions run in the main thread.
+        If multiple threads are waiting on an event (via blocking on something
+        in a callable passed to L{threadpool.ThreadPool.callInThread}), and
+        there is spare capacity in the threadpool, sending another callable
+        which will cause those to un-block to
+        L{threadpool.ThreadPool.callInThread} will reliably run that callable
+        and un-block the blocked threads promptly.
+
+        @note: This is not really a unit test, it is a stress-test.  You may
+            need to run it with C{trial -u} to fail reliably if there is a
+            problem.  It is very hard to regression-test for this particular
+            bug - one where the thread pool may consider itself as having
+            "enough capacity" when it really needs to spin up a new thread if
+            it possibly can - in a deterministic way, since the bug can only be
+            provoked by subtle race conditions.
         """
         timeout = self.getTimeout()
         self.threadpool.callInThread(self.event.set)
@@ -557,7 +569,9 @@ class RaceConditionTestCase(unittest.SynchronousTestCase):
         self.event.wait(timeout)
         if not self.event.isSet():
             self.event.set()
-            self.fail("Actions not synchronized")
+            self.fail(
+                "'set' did not run in thread; timed out waiting on 'wait'."
+            )
 
 
     def test_singleThread(self):
