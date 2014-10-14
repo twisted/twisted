50cc4cec23ef925f94e86386f9b9e9acf9a8fbd4
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-10-14 10:35:12 +0000

    applying patch from moijes12, refs #6395
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/deprecate-msn-6395@43364 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/protocols/__init__.py
+++ twisted/words/protocols/__init__.py
@@ -5,3 +5,9 @@
 """
 Chat protocols.
 """
+
+from twisted.python import deprecate, versions
+
+deprecate.deprecatedModuleAttribute(
+    versions.Version("Twisted", 14, 1, 0), "MSN has shutdown.", __name__, 
+    "msn")
