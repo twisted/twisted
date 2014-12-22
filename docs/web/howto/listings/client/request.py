62db0bf4c4343a89023073d3e18fb9d0b548d5e2
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-12-22 18:38:47 +0000

    Fix various basic code quality issues with this example. Parameterize the URI so it can serve as an HTTPS example as well.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/agent-https-docs-7658@43652 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/web/howto/listings/client/request.py
+++ docs/web/howto/listings/client/request.py
@@ -1,21 +1,30 @@
-from twisted.internet import reactor
+#!/usr/bin/env python
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+from __future__ import print_function
+
+if __name__ == "__main__":
+    from sys import argv
+    from twisted.internet.task import react
+    from request import main
+    raise SystemExit(react(main, argv[1:]))
+
+from twisted.web.iweb import UNKNOWN_LENGTH
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 
-agent = Agent(reactor)
+def display(response):
+    print("Response code:", response.code)
+    length = response.length
+    if length is UNKNOWN_LENGTH:
+        length = "(unknown)"
+    print("Response length:", length)
 
-d = agent.request(
-    'GET',
-    'http://example.com/',
-    Headers({'User-Agent': ['Twisted Web Client Example']}),
-    None)
-
-def cbResponse(ignored):
-    print 'Response received'
-d.addCallback(cbResponse)
-
-def cbShutdown(ignored):
-    reactor.stop()
-d.addBoth(cbShutdown)
-
-reactor.run()
+def main(reactor, url=b"http://twistedmatrix.com/"):
+    agent = Agent(reactor)
+    d = agent.request(
+        b"GET", url,
+        Headers({b'User-Agent': [b'Twisted Web Client Example']}))
+    d.addCallback(display)
+    return d
