efa96ac115240ac309165b477aece8250c985ad2
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-02-02 08:22:09 +0000

    Convert comments into docstrings, make new variables private.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/extras-3696-6@43805 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/dist.py
+++ twisted/python/dist.py
@@ -8,6 +8,22 @@ Distutils convenience functionality.
 Don't use this outside of Twisted.
 
 Maintainer: Christopher Armstrong
+
+@var _EXTRA_OPTIONS: These are the actual package names and versions that will
+    be used by C{extras_require}.  This is not passed to setup directly so that
+    combinations of the packages can be created without the need to copy
+    package names multiple times.
+
+@var _EXTRAS_REQUIRE: C{extras_require} is a dictionary of items that can be
+    passed to setup.py to install optional dependencies.  For example, to
+    install the optional dev dependencies one would type::
+
+        pip install -e ".[dev]"
+
+    This has been supported by setuptools since 0.5a4.
+
+@var _PLATFORM_INDEPENDENT: A list of all optional cross-platform dependencies,
+    as setuptools version specifiers, used to populate L{_EXTRAS_REQUIRE}.
 """
 
 from distutils.command import build_scripts, install_data, build_ext
@@ -49,11 +65,7 @@ twisted_subprojects = ["conch", "lore", "mail", "names",
                        "news", "pair", "runner", "web",
                        "words"]
 
-# These are the actual package names and versions that will
-# be used by extras_require. This is not passed to setup
-# directly so that combinations of the packages can be created
-# without the need to copy package names multiple times.
-EXTRA_OPTIONS = dict(
+_EXTRA_OPTIONS = dict(
     dev=['twistedchecker >= 0.2.0',
          'pyflakes >= 0.8.1',
          'twisted-dev-tools >= 0.0.2',
@@ -71,29 +83,25 @@ EXTRA_OPTIONS = dict(
     windows=['pypiwin32']
 )
 
-PLATFORM_INDEPENDENT = (
-    EXTRA_OPTIONS['tls'] +
-    EXTRA_OPTIONS['conch'] +
-    EXTRA_OPTIONS['soap'] +
-    EXTRA_OPTIONS['serial']
+_PLATFORM_INDEPENDENT = (
+    _EXTRA_OPTIONS['tls'] +
+    _EXTRA_OPTIONS['conch'] +
+    _EXTRA_OPTIONS['soap'] +
+    _EXTRA_OPTIONS['serial']
 )
 
-# Extras_require is a dictionary of items that can be passed to setup.py
-# to install optional dependencies. For example, to install the optional
-# dev dependencies one would type `pip install -e ".[dev]"`
-# This has been supported by setuptools since 0.5a4
-EXTRAS_REQUIRE = {
-    'dev': EXTRA_OPTIONS['dev'],
-    'tls': EXTRA_OPTIONS['tls'],
-    'conch': EXTRA_OPTIONS['conch'],
-    'soap': EXTRA_OPTIONS['soap'],
-    'serial': EXTRA_OPTIONS['serial'],
-    'all_non_platform': PLATFORM_INDEPENDENT,
+_EXTRAS_REQUIRE = {
+    'dev': _EXTRA_OPTIONS['dev'],
+    'tls': _EXTRA_OPTIONS['tls'],
+    'conch': _EXTRA_OPTIONS['conch'],
+    'soap': _EXTRA_OPTIONS['soap'],
+    'serial': _EXTRA_OPTIONS['serial'],
+    'all_non_platform': _PLATFORM_INDEPENDENT,
     'osx_platform': (
-        EXTRA_OPTIONS['osx'] + PLATFORM_INDEPENDENT
+        _EXTRA_OPTIONS['osx'] + _PLATFORM_INDEPENDENT
     ),
     'windows_platform': (
-        EXTRA_OPTIONS['windows'] + PLATFORM_INDEPENDENT
+        _EXTRA_OPTIONS['windows'] + _PLATFORM_INDEPENDENT
     ),
 }
 
