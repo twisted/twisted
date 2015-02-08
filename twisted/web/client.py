037e5b41200e74df8e5e73f32c3a6afe7b3ab533
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-02-08 13:18:41 +0000

    fix and topfile
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/readbody-deprecation-spurious-7762@43887 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/client.py
+++ twisted/web/client.py
@@ -2077,20 +2077,25 @@ def readBody(response):
     def cancel(deferred):
         """
         Cancel a L{readBody} call, close the connection to the HTTP server
-        immediately.
+        immediately, if it is still open.
 
         @param deferred: The cancelled L{defer.Deferred}.
         """
-        getattr(protocol.transport, 'abortConnection', lambda: None)()
+        if protocol.transport:
+            getattr(protocol.transport, 'abortConnection', lambda: None)()
+
     d = defer.Deferred(cancel)
     protocol = _ReadBodyProtocol(response.code, response.phrase, d)
     response.deliverBody(protocol)
-    if getattr(protocol.transport, 'abortConnection', None) is None:
+
+    protocolIsAbortable = getattr(protocol.transport, 'abortConnection', None)
+    if protocol.transport and protocolIsAbortable is None:
         warnings.warn(
             'Using readBody with a transport that does not implement '
             'ITCPTransport',
             category=DeprecationWarning,
             stacklevel=2)
+
     return d
 
 
