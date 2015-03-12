a816bf05e2e4434cc9bfd09d0f06ea53ac968d35
adiroiban <adiroiban@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-03-12 15:30:09 +0000

    Sleep again.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/test-ipv6AcceptAddress-5356@44027 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_iocp.py
+++ twisted/internet/test/test_iocp.py
@@ -76,6 +76,8 @@ class SupportTests(unittest.TestCase):
                 break
             except error as socketError:
                 if socketError.errno == 10057:
+                    import time
+                    time.sleep(0.001)
                     pass
                 else:
                     # Not the excepted error so we raise the error without
