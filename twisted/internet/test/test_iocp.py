41eec8b1cece15f64af1c02b781effacd6680a27
adiroiban <adiroiban@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-03-15 14:23:02 +0000

    Retry twice and run tests 1000 times.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/test-ipv6AcceptAddress-5356@44045 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_iocp.py
+++ twisted/internet/test/test_iocp.py
@@ -64,7 +64,7 @@ class SupportTests(unittest.TestCase):
         self.assertEqual(
             0, _iocp.accept(port.fileno(), server.fileno(), buff, None))
 
-        for _ in range(1):
+        for _ in range(2):
             # Calling setsockopt after _iocp.accept might fail for both IPv4
             # and IPV6 with [Errno 10057] A request to send or receive ...
             # so we retry twice as retrying once is not enough.
@@ -119,7 +119,7 @@ class SupportTests(unittest.TestCase):
             connection
         """
         failures = 0
-        for _ in range(10000):
+        for _ in range(1000):
             # Run test a lot of times to see it fails
             # and count the failures counter.
             try:
