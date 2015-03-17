5570c7fc4509d963829b5be93cebb100d36ef3c6
adiroiban <adiroiban@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-03-17 17:48:47 +0000

    Update docstrings.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/sshsimpleserver-getPrimes-7715@44106 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/conch/examples/sshsimpleserver.py
+++ docs/conch/examples/sshsimpleserver.py
@@ -19,18 +19,19 @@ log.startLogging(sys.stderr)
 """
 Example of running a custom protocol as a shell session over an SSH channel.
 
-Server identifies itself using RSA key only.
-It is easy to extend it to also use DSA host key.
+Warning! This implementation is here to help you understand how Conch SSH
+server works. You should not use this code in production.
 
-Authenticate using username "user" and password "password" or using RSA key.
-
-The same RSA key is used as server host key.
+In this example the implemented SSH server identifies itself using an RSA host
+key and authenticates clients using username "user" and password "password" or
+using a SSH RSA key.
 """
 
-# Use as server host key as well as the key allowed for `user` account.
+# Used as server host key as well as the key allowed for `user` account.
+# In real life you should NOT reuse keys.
 publicKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az64fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkbh/C+BR3utDS555mV'
-
-# Private port is required only for host key.
+# Private part of the private key used as server's host key and as account's
+# SSH key.
 privateKey = """-----BEGIN RSA PRIVATE KEY-----
 MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
 4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
@@ -48,6 +49,23 @@ EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
 # described in RFC4419.
 # This is a short list with a single prime member and only for keys of size
 # 1024 and 2048.
+# You would need a list for each SSH key size that you plan to support in your
+# server implementation.
+# You can use OpenSSH ssh-keygen to generate these numbers.
+# See the MODULI GENERATION section from the ssh-keygen man pages.
+# See moduli man pages to find out more about the format used by the file
+# generated using ssh-keygen.
+# For Conch SSH server we only need the last 3 values:
+# * size
+# * generator
+# * modulus
+#
+# The format required by the Conch SSH server is:
+#
+# {
+#   size1: [(generator1, modulus1), (generator1, modulus2)],
+#   size2: [(generator4, modulus3), (generator1, modulus4)],
+# }
 primes = {
     1024: [(2L, 156096651313543778571595410217704559250114948553033770757855392840961183273291234883805993188033046610903022946932272851484576959192633374396690126968589997032493458342941315946790987602763346322167058862580077578954322455929446361765756279255191622023630586895206795776981175371885918231654469745514594662643L)],
     2048: [(2L, 24265446577633846575813468889658944748236936003103970778683933705240497295505367703330163384138799145013634794444597785054574812547990300691956176233759905976222978197624337271745471021764463536913188381724789737057413943758936963945487690939921001501857793275011598975080236860899147312097967655185795176036941141834185923290769258512343298744828216530595090471970401506268976911907264143910697166165795972459622410274890288999065530463691697692913935201628660686422182978481412651196163930383232742547281180277809475129220288755541335335798837173315854931040199943445285443708240639743407396610839820418936574217939L)],
@@ -72,8 +90,9 @@ class ExampleAvatar(avatar.ConchUser):
 
 class ExampleRealm(object):
     """
-    SSH server requires encapsulating all configuration for an account into
-    an L{avatar.ConchUser} instance.
+    When using Twisted Cred, the pluggable authentication framework, the
+    C{requestAvatar} method should return a L{avatar.ConchUser} instance
+    as required by the Conch SSH server.
     """
     implements(portal.IRealm)
 
@@ -108,15 +127,17 @@ class EchoProtocol(protocol.Protocol):
 
 class ExampleSession(object):
     """
-    This select what to do for each type of sessions which can be requested
-    by the client for the channel of type I{session}.
+    This selects what to do for each type of session which is requested by the
+    client via the SSH channel of type I{session}.
     """
 
     def __init__(self, avatar):
         """
-        We don't use it, but the adapter is passed the avatar as its first
-        argument.
+        In this example the avatar argument is not used for session selection,
+        but for example you can use it to limit I{shell} or I{exec} access
+        only to specific accounts.
         """
+        pass
 
     def getPty(self, term, windowSize, attrs):
         """
@@ -128,17 +149,17 @@ class ExampleSession(object):
         """
         We don't support command execution sessions.
         """
-        raise Exception("no executing commands")
+        raise Exception("not executing commands")
 
-    def openShell(self, trans):
+    def openShell(self, transport):
         """
         Use our protocol as shell session.
         """
-        ep = EchoProtocol()
-        # Connect the new new protocol to the transport and transport
-        # to the new protocol so that they can communicate.
-        ep.makeConnection(trans)
-        trans.makeConnection(session.wrapProtocol(ep))
+        protocol = EchoProtocol()
+        # Connect the new protocol to the transport and the transport
+        # to the new protocol so they can communicate in both directions.
+        protocol.makeConnection(transport)
+        transport.makeConnection(session.wrapProtocol(protocol))
 
     def eofReceived(self):
         pass
@@ -154,22 +175,22 @@ components.registerAdapter(ExampleSession, ExampleAvatar, session.ISession)
 
 class ExampleFactory(factory.SSHFactory):
     """
-    This is our SSH server.
+    This is the entry point of our SSH server implementation.
 
     The SSH transport layer is implemented by L{SSHTransport} and is the
     protocol of this factory.
 
-    Here we configure server's identity (host keys) and handlers for the SSH
-    services.
-
-
-    L{connection.SSHConnection} handles request for the channel multiplexing
-    service.
-    L{userauth.SSHUserAuthServer} handlers request for tje user authentication
-    services.
+    Here we configure the server's identity (host keys) and handlers for the
+    SSH services:
+    * L{connection.SSHConnection} handles requests for the channel multiplexing
+      service.
+    * L{userauth.SSHUserAuthServer} handlers requests for the user
+      authentication service.
     """
     protocol = SSHServerTransport
-    # Server's host keys. This server has only host key of type RSA.
+    # Server's host keys.
+    # To simplify the example this server is defined only with a host key of
+    # type RSA.
     publicKeys = {
         'ssh-rsa': keys.Key.fromString(data=publicKey)
     }
