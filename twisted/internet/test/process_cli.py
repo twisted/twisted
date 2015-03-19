c334b6a2327ba0dad25440ce3f3fda970a860f5f
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-03-19 15:42:10 +0000

    these should be here??
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tiprocess-py3-5987@44136 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ twisted/internet/test/process_cli.py
@@ -0,0 +1,30 @@
+import sys, os
+try:
+    # On Windows, stdout is not opened in binary mode by default,
+    # so newline characters are munged on writing, interfering with
+    # the tests.
+    import msvcrt
+    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+except ImportError:
+    pass
+
+if sys.version_info < (3, 0):
+    _PY3 = False
+else:
+    _PY3 = True
+
+if _PY3:
+    stdout = sys.stdout.buffer
+else:
+    stdout = sys.stdout
+
+for arg in sys.argv[1:]:
+
+    res = arg + chr(0)
+
+    if _PY3:
+        stdout.write(res.encode("utf8"))
+    else:
+        stdout.write(res)
+
+    stdout.flush()
