eae3e33367a9e95aa36f21792c5af735289a7c07
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-03-20 08:40:15 +0000

    i really hate this bug
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tiprocess-py3-5987@44163 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_process.py
+++ twisted/internet/test/test_process.py
@@ -762,6 +762,7 @@ class ProcessTestsBuilder(ProcessTestsBuilderBase):
 
         def processFinished(finishedArgs):
             output, err, code = finishedArgs
+            print(err)
             output = output.split(b'\0')
             # Drop the trailing \0.
             output.pop()
