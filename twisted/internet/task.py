e593b479a300b42e3af634c36cea75460b579e40
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-03-31 04:19:54 +0000

    make the test pass
    remove unnecessary _expectNextCallAt state, simplify things a bit
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/count-7836@44334 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/task.py
+++ twisted/internet/task.py
@@ -46,10 +46,6 @@ class LoopingCall:
         exception. In either case, it will be C{False} by the time the
         C{Deferred} returned by L{start} fires its callback or errback.
 
-    @type _expectNextCallAt: C{float}
-    @ivar _expectNextCallAt: The time at which this instance most recently
-        scheduled itself to run.
-
     @type _realLastTime: C{float}
     @ivar _realLastTime: When counting skips, the time at which the skip
         counter was last invoked.
@@ -63,7 +59,6 @@ class LoopingCall:
     running = False
     deferred = None
     interval = None
-    _expectNextCallAt = 0.0
     _runAtStart = False
     starttime = None
 
@@ -112,11 +107,12 @@ class LoopingCall:
                 lastTime = self.starttime
                 if self._runAtStart:
                     lastTime -= self.interval
-            self._realLastTime = now
             lastInterval = self._intervalOf(lastTime)
             thisInterval = self._intervalOf(now)
             count = thisInterval - lastInterval
-            return countCallable(count)
+            if count > 0:
+                self._realLastTime = now
+                return countCallable(count)
 
         self = cls(counter)
 
@@ -166,13 +162,12 @@ class LoopingCall:
         self.running = True
         d = self.deferred = defer.Deferred()
         self.starttime = self.clock.seconds()
-        self._expectNextCallAt = self.starttime
         self.interval = interval
         self._runAtStart = now
         if now:
             self()
         else:
-            self._reschedule()
+            self._scheduleFrom(self.starttime)
         return d
 
     def stop(self):
@@ -198,13 +193,13 @@ class LoopingCall:
         if self.call is not None:
             self.call.cancel()
             self.call = None
-            self._expectNextCallAt = self.clock.seconds()
-            self._reschedule()
+            self.starttime = self.clock.seconds()
+            self._scheduleFrom(self.starttime)
 
     def __call__(self):
         def cb(result):
             if self.running:
-                self._reschedule()
+                self._scheduleFrom(self.clock.seconds())
             else:
                 d, self.deferred = self.deferred, None
                 d.callback(self)
@@ -220,27 +215,20 @@ class LoopingCall:
         d.addErrback(eb)
 
 
-    def _reschedule(self):
+    def _scheduleFrom(self, when):
         """
         Schedule the next iteration of this looping call.
-        """
-        if self.interval == 0:
-            self.call = self.clock.callLater(0, self)
-            return
 
-        currentTime = self.clock.seconds()
-        # Find how long is left until the interval comes around again.
-        untilNextTime = (self._expectNextCallAt - currentTime) % self.interval
-        # Make sure it is in the future, in case more than one interval worth
-        # of time passed since the previous call was made.
-        nextTime = max(
-            self._expectNextCallAt + self.interval, currentTime + untilNextTime)
-        # If the interval falls on the current time exactly, skip it and
-        # schedule the call for the next interval.
-        if nextTime == currentTime:
-            nextTime += self.interval
-        self._expectNextCallAt = nextTime
-        self.call = self.clock.callLater(nextTime - currentTime, self)
+        @param when: The present time from whence the call is scheduled.
+        """
+        def howLong():
+            if self.interval == 0:
+                return 0
+            untilNextInterval = self.interval - ((when - self.starttime)
+                                                 % self.interval)
+            return (untilNextInterval if when != when + untilNextInterval
+                    else self.interval)
+        self.call = self.clock.callLater(howLong(), self)
 
 
     def __repr__(self):
