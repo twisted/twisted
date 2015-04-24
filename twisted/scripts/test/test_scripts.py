e4c9602d4a85b0591fca8a80ec77a752f0fa7717
adiroiban <adiroiban@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-04-24 08:32:01 +0000

    Merge scripts-testcase-names-7727 : Update twisted.scripts test to the new style of naming test classes.
    Author: eeshangarg
    Reviewers: adiroiban
    Fixes: #7727
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@44545 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/scripts/test/test_scripts.py
+++ twisted/scripts/test/test_scripts.py
@@ -187,7 +187,7 @@ class ScriptTests(TestCase, ScriptTestsMixin):
 
 
 
-class ZshIntegrationTestCase(TestCase, ZshScriptTestMixin):
+class ZshIntegrationTests(TestCase, ZshScriptTestMixin):
     """
     Test that zsh completion functions are generated without error
     """
