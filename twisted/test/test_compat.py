69867b12f34da366900792c214a5068df60839b7
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-04-27 15:27:45 +0000

    once more with feeling
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tccredentials-py3-6176-3@44587 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_compat.py
+++ twisted/test/test_compat.py
@@ -16,7 +16,7 @@ from twisted.python.compat import reduce, execfile, _PY3
 from twisted.python.compat import comparable, cmp, nativeString, networkString
 from twisted.python.compat import unicode as unicodeCompat, lazyByteSlice
 from twisted.python.compat import reraise, NativeStringIO, iterbytes, intToBytes
-from twisted.python.compat import joinBytes
+from twisted.python.compat import joinBytes, ioType
 from twisted.python.filepath import FilePath
 
 
