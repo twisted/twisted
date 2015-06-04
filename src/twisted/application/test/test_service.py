3b31412a7900eaca783bcbcc80905027cb55945a
moshez <moshez@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-06-04 03:46:34 +0000

    +add tests for IService interface
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/iservice-parent-7922@44996 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- src/twisted/application/test/test_service.py
+++ src/twisted/application/test/test_service.py
@@ -7,14 +7,65 @@ Tests for L{twisted.application.service}.
 
 from __future__ import absolute_import, division
 
+from zope.interface import implementer
+from zope.interface.exceptions import BrokenImplementation
 from zope.interface.verify import verifyObject
 
 from twisted.persisted.sob import IPersistable
 from twisted.application.service import Application, IProcess
 from twisted.application.service import IService, IServiceCollection
+from twisted.application.service import Service
+from twisted.python.compat import _PY3
 from twisted.trial.unittest import TestCase
 
 
+@implementer(IService)
+class AlmostService(object):
+
+    def setName(self, name):
+        pass
+
+    def setServiceParent(self, parent):
+        pass
+
+    def disownServiceParent(self):
+        pass
+
+    def privilegedStartService(self):
+        pass
+
+    def startService(self):
+        pass
+
+    def stopService(self):
+        pass
+
+
+class ServiceInterfaceTests(TestCase):
+
+    def setUp(self):
+        self.almostService = AlmostService()
+        self.almostService.parent = None
+        self.almostService.running = False
+        self.almostService.name = None
+
+    def test_hasAll(self):
+        verifyObject(IService, self.almostService)
+
+    def test_noName(self):
+        del self.almostService.name
+        with self.assertRaises(BrokenImplementation):
+            verifyObject(IService, self.almostService)
+
+    def test_noParent(self):
+        del self.almostService.parent
+        with self.assertRaises(BrokenImplementation):
+            verifyObject(IService, self.almostService)
+
+    def test_noRunning(self):
+        del self.almostService.running
+        with self.assertRaises(BrokenImplementation):
+            verifyObject(IService, self.almostService)
 
 class ApplicationTests(TestCase):
     """
