29e7e253600eab105026652e51020b8fd6010eba
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-07-11 15:45:27 +0000

    add python 3.4 to dist.py
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/fix-metadata-7967@45235 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/dist.py
+++ twisted/python/dist.py
@@ -57,6 +57,7 @@ on event-based network programming and multiprotocol integration.
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         ],
     )
 
