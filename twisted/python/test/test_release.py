c1ed13afaf743c7c9519e147fc0d9af00d36192e
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-08-10 13:51:50 +0000

    remove a cheesy test with an equally cheesy test
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/nopickledenv-7980@45448 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/test/test_release.py
+++ twisted/python/test/test_release.py
@@ -1452,7 +1452,7 @@ class SphinxBuilderTests(TestCase):
         # check that the html files are at least html-ish
         # this is not a terribly rigorous check
         if fpath.path.endswith('.html'):
-            parseXMLString(fcontents)
+            self.assertIn("<body", fcontents)
 
 
     def test_build(self):
