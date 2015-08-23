e20ad595b4a49e87532f6fc2d8eb7069ab7c004e
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-08-23 10:22:26 +0000

    herpa derpa
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/what-is-ssl-8006@45549 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/test/test_endpoints.py
+++ twisted/internet/test/test_endpoints.py
@@ -2903,9 +2903,9 @@ class ClientStringTests(unittest.TestCase):
 
 
 
-class SSLClientStringTests(unittest.TestCase):
+class TLSClientStringTests(unittest.TestCase):
     """
-    Tests for L{twisted.internet.endpoints.clientFromString} which require SSL.
+    Tests for L{twisted.internet.endpoints.clientFromString} which require TLS.
     """
 
     if skipTLS:
@@ -2914,13 +2914,59 @@ class SSLClientStringTests(unittest.TestCase):
     def test_ssl(self):
         """
         When passed an SSL strports description, L{clientFromString} returns a
+        L{SSL4ClientEndpoint} instance initialized with the values from the
+        string, and raises a deprecation warning.
+        """
+        reactor = object()
+        client = endpoints.clientFromString(
+            reactor,
+            "ssl:host=example.net:port=4321:privateKey=%s:"
+            "certKey=%s:bindAddress=10.0.0.3:timeout=3:caCertsDir=%s" %
+            (escapedPEMPathName, escapedPEMPathName, escapedCAsPathName))
+        self.assertIsInstance(client, endpoints.SSL4ClientEndpoint)
+        self.assertIs(client._reactor, reactor)
+        self.assertEqual(client._host, "example.net")
+        self.assertEqual(client._port, 4321)
+        self.assertEqual(client._timeout, 3)
+        self.assertEqual(client._bindAddress, ("10.0.0.3", 0))
+        certOptions = client._sslContextFactory
+        self.assertIsInstance(certOptions, CertificateOptions)
+        self.assertEqual(certOptions.method, SSLv23_METHOD)
+        self.assertTrue(certOptions._options & OP_NO_SSLv3)
+        ctx = certOptions.getContext()
+        self.assertIsInstance(ctx, ContextType)
+        self.assertEqual(Certificate(certOptions.certificate), testCertificate)
+        privateCert = PrivateCertificate(certOptions.certificate)
+        privateCert._setPrivateKey(KeyPair(certOptions.privateKey))
+        self.assertEqual(privateCert, testPrivateCertificate)
+        expectedCerts = [
+            Certificate.loadPEM(x.getContent()) for x in
+            [casPath.child("thing1.pem"), casPath.child("thing2.pem")]
+            if x.basename().lower().endswith('.pem')
+        ]
+        self.assertEqual(sorted((Certificate(x) for x in certOptions.caCerts),
+                                key=lambda cert: cert.digest()),
+                         sorted(expectedCerts,
+                                key=lambda cert: cert.digest()))
+
+        warnings = self.flushWarnings()
+        self.assertEqual(len(warnings), 1)
+        self.assertIn(
+            ("twisted.internet.endpoints.SSL4ClientEndpoint was deprecated in "
+             "Twisted 15.5.0: Use TLS4ClientEndpoint instead."),
+            warnings[0]["message"])
+
+
+    def test_tls(self):
+        """
+        When passed an TLS strports description, L{clientFromString} returns a
         L{TLS4ClientEndpoint} instance initialized with the values from the
         string.
         """
         reactor = object()
         client = endpoints.clientFromString(
             reactor,
-            "ssl:host=example.net:port=4321:privateKey=%s:"
+            "tls:host=example.net:port=4321:privateKey=%s:"
             "certKey=%s:bindAddress=10.0.0.3:timeout=3:caCertsDir=%s" %
             (escapedPEMPathName, escapedPEMPathName, escapedCAsPathName))
         self.assertIsInstance(client, endpoints.TLS4ClientEndpoint)
@@ -2950,16 +2996,16 @@ class SSLClientStringTests(unittest.TestCase):
                                 key=lambda cert: cert.digest()))
 
 
-    def test_sslPositionalArgs(self):
+    def test_tlsPositionalArgs(self):
         """
-        When passed an SSL strports description, L{clientFromString} returns a
+        When passed an TLS strports description, L{clientFromString} returns a
         L{TLS4ClientEndpoint} instance initialized with the values from the
         string.
         """
         reactor = object()
         client = endpoints.clientFromString(
             reactor,
-            "ssl:example.net:4321:privateKey=%s:"
+            "tls:example.net:4321:privateKey=%s:"
             "certKey=%s:bindAddress=10.0.0.3:timeout=3:caCertsDir=%s" %
             (escapedPEMPathName, escapedPEMPathName, escapedCAsPathName))
         self.assertIsInstance(client, endpoints.TLS4ClientEndpoint)
@@ -2970,14 +3016,14 @@ class SSLClientStringTests(unittest.TestCase):
         self.assertEqual(client._bindAddress, ("10.0.0.3", 0))
 
 
-    def test_sslWithDefaults(self):
+    def test_tlsWithDefaults(self):
         """
-        When passed an SSL strports description without extra arguments,
+        When passed an TLS strports description without extra arguments,
         L{clientFromString} returns a L{TLS4ClientEndpoint} instance
         whose context factory is initialized with default values.
         """
         reactor = object()
-        client = endpoints.clientFromString(reactor, "ssl:example.net:4321")
+        client = endpoints.clientFromString(reactor, "tls:example.net:4321")
         self.assertIsInstance(client, endpoints.TLS4ClientEndpoint)
         self.assertIs(client._reactor, reactor)
         self.assertEqual(client._host, "example.net")
@@ -3009,15 +3055,15 @@ class SSLClientStringTests(unittest.TestCase):
             [Certificate.loadPEM(casPath.child("thing1.pem").getContent())])
 
 
-    def test_sslSimple(self):
+    def test_tlsSimple(self):
         """
-        When passed an SSL strports description without any extra parameters,
+        When passed an TLS strports description without any extra parameters,
         L{clientFromString} returns a simple non-verifying endpoint that will
-        speak SSL.
+        speak TLS.
         """
         reactor = object()
         client = endpoints.clientFromString(
-            reactor, "ssl:host=simple.example.org:port=4321")
+            reactor, "tls:host=simple.example.org:port=4321")
         certOptions = client._sslContextFactory
         self.assertIsInstance(certOptions, CertificateOptions)
         self.assertEqual(certOptions.verify, False)
