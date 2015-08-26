80907b8c318765873c9db3d3ec7f8873d6bdaf3a
adiroiban <adiroiban@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-08-26 07:06:41 +0000

    Apply 7717-v7.patch - fix conflict.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/ssh-diffie-hellman-group14-sha1-7717-3@45557 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/test/test_ssh.py
+++ twisted/conch/test/test_ssh.py
@@ -17,7 +17,7 @@ try:
 except ImportError:
     pyasn1 = None
 
-from twisted.conch.ssh import common, session, forwarding
+from twisted.conch.ssh import common, session, forwarding, kex
 from twisted.conch import avatar, error
 from twisted.conch.test.keydata import publicRSA_openssh, privateRSA_openssh
 from twisted.conch.test.keydata import publicDSA_openssh, privateDSA_openssh
@@ -365,8 +365,15 @@ if Crypto is not None and pyasn1 is not None:
             }
 
         def getPrimes(self):
+            """
+            Return the Diffie-Hellman primes that can be used for the
+            diffie-hellman-group-exchange-sha1 key exchange. Prime values are
+            borrowed from diffie-hellman-group1-sha1 key exchange algorithm
+            otherwise, the test would rely on a moduli file being present. See
+            OpenSSHFactory.getPrimes.
+            """
             return {
-                2048:[(transport.DH_GENERATOR, transport.DH_PRIME)]
+                2048:[kex.getDHPrime('diffie-hellman-group1-sha1')]
             }
 
         def getService(self, trans, name):
