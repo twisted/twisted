ce6bd323c5200f12ca2e525fc9f672cdc3ce3a26
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-10-13 01:27:06 +0000

    Merge bytesurlpath-7994-2: t.p.urlpath should operate on bytes, not str
    Author: hawkowl
    Reviewers: adiroiban, glyph
    Fixes: #7994
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@45763 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/urlpath.py
+++ twisted/python/urlpath.py
@@ -2,35 +2,52 @@
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
-#
+"""
+L{URLPath}, a representation of a URL.
+"""
 
 from __future__ import division, absolute_import
-from twisted.python.compat import urllib_parse as urlparse
-from twisted.python.compat import _PY3
 
-if not _PY3:
-    from urllib import unquote as unquoteFunc
-else:
-    from urllib.parse import unquote as unquoteFunc
-
+from twisted.python.compat import unicode
+from twisted.python.compat import urllib_parse as urlparse, urlunquote
 
 
 class URLPath(object):
-    def __init__(self, scheme='', netloc='localhost', path='',
-                 query='', fragment=''):
-        self.scheme = scheme or 'http'
+    """
+    A representation of a URL.
+
+    @ivar scheme: The scheme of the URL (e.g. 'http').
+    @type scheme: L{bytes}
+    @ivar netloc: The network location ("host").
+    @type netloc: L{bytes}
+    @ivar path: The path on the network location.
+    @type path: L{bytes}
+    @ivar query: The query argument (the portion after ? in the URL).
+    @type query: L{bytes}
+    @ivar fragment: The page fragment (the portion after # in the URL).
+    @type fragment: L{bytes}
+    """
+    def __init__(self, scheme=b'', netloc=b'localhost', path=b'',
+                 query=b'', fragment=b''):
+        self.scheme = scheme or b'http'
         self.netloc = netloc
-        self.path = path or '/'
+        self.path = path or b'/'
         self.query = query
         self.fragment = fragment
 
     _qpathlist = None
     _uqpathlist = None
 
-    def pathList(self, unquote=0, copy=1):
+    def pathList(self, unquote=False, copy=True):
+        """
+        Split this URL's path into its components.
+
+        @return: The components of C{self.path}
+        @rtype: L{list} of L{bytes}
+        """
         if self._qpathlist is None:
-            self._qpathlist = self.path.split('/')
-            self._uqpathlist = map(unquoteFunc, self._qpathlist)
+            self._qpathlist = self.path.split(b'/')
+            self._uqpathlist = map(urlunquote, self._qpathlist)
         if unquote:
             result = self._uqpathlist
         else:
@@ -40,61 +57,149 @@ class URLPath(object):
         else:
             return result
 
-    def fromString(klass, st):
-        t = urlparse.urlsplit(st)
-        u = klass(*t)
-        return u
 
-    fromString = classmethod(fromString)
+    @classmethod
+    def fromString(klass, url):
+        """
+        Make a L{URLPath} from a L{str} or L{unicode}.
 
+        @param url: A L{str} representation of a URL.
+        @type url: L{str} or L{unicode}.
+
+        @rtype: L{URLPath}
+        """
+        if not isinstance(url, (str, unicode)):
+            raise ValueError("'url' must be a str or unicode")
+        url = url.encode('utf-8')
+        parts = urlparse.urlsplit(url)
+        return klass(*parts)
+
+
+    @classmethod
+    def fromBytes(klass, url):
+        """
+        Make a L{URLPath} from a L{bytes}.
+
+        @param url: A L{bytes} representation of a URL.
+        @type url: L{bytes}
+
+        @rtype: L{URLPath}
+
+        @since: 15.4
+        """
+        if not isinstance(url, bytes):
+            raise ValueError("'url' must be bytes")
+        parts = urlparse.urlsplit(url)
+        return klass(*parts)
+
+
+    @classmethod
     def fromRequest(klass, request):
-        return klass.fromString(request.prePathURL())
+        """
+        Make a L{URLPath} from a L{twisted.web.http.Request}.
 
-    fromRequest = classmethod(fromRequest)
+        @param request: A L{twisted.web.http.Request} to make the L{URLPath}
+            from.
+
+        @rtype: L{URLPath}
+        """
+        return klass.fromBytes(request.prePathURL())
+
 
     def _pathMod(self, newpathsegs, keepQuery):
         if keepQuery:
             query = self.query
         else:
-            query = ''
+            query = b''
         return URLPath(self.scheme,
-                        self.netloc,
-                        '/'.join(newpathsegs),
-                        query)
+                       self.netloc,
+                       b'/'.join(newpathsegs),
+                       query)
 
-    def sibling(self, path, keepQuery=0):
+
+    def sibling(self, path, keepQuery=False):
+        """
+        Get the sibling of the current L{URLPath}. A sibling is a file which is
+        in the same directory as the current file.
+
+        @param path: The path of the sibling.
+        @type path: L{bytes}
+
+        @param keepQuery: Whether to keep the query parameters on the returned
+            L{URLPath}.
+        @type: keepQuery: L{bool}
+
+        @rtype: L{URLPath}
+        """
         l = self.pathList()
         l[-1] = path
         return self._pathMod(l, keepQuery)
 
-    def child(self, path, keepQuery=0):
+
+    def child(self, path, keepQuery=False):
+        """
+        Get the child of this L{URLPath}.
+
+        @param path: The path of the child.
+        @type path: L{bytes}
+
+        @param keepQuery: Whether to keep the query parameters on the returned
+            L{URLPath}.
+        @type: keepQuery: L{bool}
+
+        @rtype: L{URLPath}
+        """
         l = self.pathList()
-        if l[-1] == '':
+        if l[-1] == b'':
             l[-1] = path
         else:
             l.append(path)
         return self._pathMod(l, keepQuery)
 
-    def parent(self, keepQuery=0):
+
+    def parent(self, keepQuery=False):
+        """
+        Get the parent directory of this L{URLPath}.
+
+        @param keepQuery: Whether to keep the query parameters on the returned
+            L{URLPath}.
+        @type: keepQuery: L{bool}
+
+        @rtype: L{URLPath}
+        """
         l = self.pathList()
-        if l[-1] == '':
+        if l[-1] == b'':
             del l[-2]
         else:
             # We are a file, such as http://example.com/foo/bar
             # our parent directory is http://example.com/
             l.pop()
-            l[-1] = ''
+            l[-1] = b''
         return self._pathMod(l, keepQuery)
 
-    def here(self, keepQuery=0):
+
+    def here(self, keepQuery=False):
+        """
+        Get the current directory of this L{URLPath}.
+
+        @param keepQuery: Whether to keep the query parameters on the returned
+            L{URLPath}.
+        @type: keepQuery: L{bool}
+
+        @rtype: L{URLPath}
+        """
         l = self.pathList()
-        if l[-1] != '':
-            l[-1] = ''
+        if l[-1] != b'':
+            l[-1] = b''
         return self._pathMod(l, keepQuery)
+
 
     def click(self, st):
-        """Return a path which is the URL where a browser would presumably take
+        """
+        Return a path which is the URL where a browser would presumably take
         you if you clicked on a link with an HREF as given.
+
+        @rtype: L{URLPath}
         """
         scheme, netloc, path, query, fragment = urlparse.urlsplit(st)
         if not scheme:
@@ -105,25 +210,29 @@ class URLPath(object):
                 path = self.path
                 if not query:
                     query = self.query
-            elif path[0] != '/':
+            elif path[0] != b'/':
                 l = self.pathList()
                 l[-1] = path
-                path = '/'.join(l)
+                path = b'/'.join(l)
 
         return URLPath(scheme,
-                        netloc,
-                        path,
-                        query,
-                        fragment)
-
+                       netloc,
+                       path,
+                       query,
+                       fragment)
 
 
     def __str__(self):
         x = urlparse.urlunsplit((
-            self.scheme, self.netloc, self.path,
-            self.query, self.fragment))
+            self.scheme, self.netloc, self.path, self.query, self.fragment))
+
+        if not isinstance(x, str):
+            x = x.decode('utf8')
+
         return x
+
 
     def __repr__(self):
         return ('URLPath(scheme=%r, netloc=%r, path=%r, query=%r, fragment=%r)'
-                % (self.scheme, self.netloc, self.path, self.query, self.fragment))
+                % (self.scheme, self.netloc, self.path, self.query,
+                   self.fragment))
