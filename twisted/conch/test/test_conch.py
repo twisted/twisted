f9530fb1d01bc6d8f56c418015b1e2fa7f8f5fd5
adiroiban <adiroiban@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-10-17 10:56:31 +0000

    Merge ssh-diffie-hellman-group14-sha1-7717-4: Support diffie-hellman-group14-sha1 key exchange in conch.ssh.
    Authors: cjwatson, imoore76
    Reviewer: adiroiban
    Fixes: #7717
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@45812 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/test/test_conch.py
+++ twisted/conch/test/test_conch.py
@@ -520,6 +520,57 @@ class OpenSSHClientMixin:
 
 
 
+class OpenSSHKeyExchangeTestCase(ConchServerSetupMixin, OpenSSHClientMixin,
+                                 unittest.TestCase):
+    """
+    Tests L{SSHTransportBase}'s key exchange algorithm compatibility with
+    OpenSSH.
+    """
+
+    def assertExecuteWithKexAlgorithm(self, keyExchangeAlgo):
+        """
+        Call execute() method of L{OpenSSHClientMixin} with an ssh option that
+        forces the exclusive use of the key exchange algorithm specified by
+        keyExchangeAlgo
+
+        @type keyExchangeAlgo: C{str}
+        @param keyExchangeAlgo: The key exchange algorithm to use
+
+        @return: L{defer.Deferred}
+        """
+        d = self.execute('echo hello', ConchTestOpenSSHProcess(),
+                         '-oKexAlgorithms=' + keyExchangeAlgo)
+        return d.addCallback(self.assertEqual, 'hello\n')
+
+
+    def test_DH_GROUP1(self):
+        """
+        The diffie-hellman-group1-sha1 key exchange algorithm is compatible
+        with OpenSSH.
+        """
+        return self.assertExecuteWithKexAlgorithm(
+            'diffie-hellman-group1-sha1')
+
+
+    def test_DH_GROUP14(self):
+        """
+        The diffie-hellman-group14-sha1 key exchange algorithm is compatible
+        with OpenSSH.
+        """
+        return self.assertExecuteWithKexAlgorithm(
+            'diffie-hellman-group14-sha1')
+
+
+    def test_DH_GROUP_EXCHANGE_SHA1(self):
+        """
+        The diffie-hellman-group-exchange-sha1 key exchange algorithm is
+        compatible with OpenSSH.
+        """
+        return self.assertExecuteWithKexAlgorithm(
+            'diffie-hellman-group-exchange-sha1')
+
+
+
 class OpenSSHClientForwardingTests(ForwardingMixin, OpenSSHClientMixin,
                                       unittest.TestCase):
     """
---
+++ twisted/conch/topfiles/7717.feature
@@ -0,0 +1,1 @@
+twisted.conch.ssh now supports the diffie-hellman-group14-sha1 key exchange algorithm.
