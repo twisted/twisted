236927572dd207a03ae89c119b17953bcb0b9c49
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-10-24 07:15:50 +0000

    test for pathList
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/iri-5388-3@45907 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/test/test_urlpath.py
+++ twisted/python/test/test_urlpath.py
@@ -101,6 +101,20 @@ class _BaseURLPathTests(object):
         )
 
 
+    def test_pathList(self):
+        """
+        L{urlpath.URLPath.pathList} returns a L{list} of L{bytes}.
+        """
+        self.assertEqual(
+            self.path.child(b"%00%01%02").pathList(),
+            [b"", b"foo", b"bar", b"%00%01%02"]
+        )
+        self.assertEqual(
+            self.path.child(b"%00%01%02").pathList(unquote=True),
+            [b"", b"foo", b"bar", b"\x00\x01\x02"]
+        )
+
+
 
 class BytesURLPathTests(_BaseURLPathTests, unittest.TestCase):
     """
