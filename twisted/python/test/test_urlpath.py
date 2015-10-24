e061e2e4648b35775711ff51312104296dc61e4b
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-10-24 01:23:43 +0000

    new test that fails on py3
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/iri-5388-3@45903 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/test/test_urlpath.py
+++ twisted/python/test/test_urlpath.py
@@ -82,6 +82,18 @@ class _BaseURLPathTests(object):
                          'http://example.com/foo/bar/')
 
 
+    def test_doubleSlash(self):
+        """
+        Calling L{urlpath.URLPath.click} on a L{urlpath.URLPath} with a
+        trailing slash with a relative URL containing a leading slash will
+        result in a URL with a single slash at the start of the path portion.
+        """
+        self.assertEqual(
+            str(urlpath.URLPath.fromBytes(b"http://localhost/")
+                .click(b"/hello/world")).encode("ascii"),
+            b"http://localhost/hello/world"
+        )
+
 
 class BytesURLPathTests(_BaseURLPathTests, unittest.TestCase):
     """
