a567c6a032988385eaa286d25d9ab82f6f884624
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-11-13 11:41:49 +0000

    Merge py26-assertraises-5976: Remove assertRaises backport from Trial
    Author: hawkowl
    Reviewer: adiroiban
    Fixes: #5976
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@46183 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_twisted.py
+++ twisted/test/test_twisted.py
@@ -8,13 +8,12 @@ the code in C{twisted/__init__.py}.
 
 from __future__ import division, absolute_import
 
-import re
 import sys
 from types import ModuleType, FunctionType
 
 from twisted import _checkRequirements
 from twisted.python.compat import _PY3
-from twisted.trial.unittest import TestCase
+from twisted.trial.unittest import TestCase, SkipTest
 
 
 # This is somewhat generally useful and should probably be part of a public API
@@ -53,44 +52,6 @@ class SetAsideModule(object):
     def __exit__(self, excType, excValue, traceback):
         self._unimport(self.name)
         sys.modules.update(self.modules)
-
-
-
-# Copied from 2.7 stdlib.  Delete after Python 2.6 is no longer a
-# requirement.  See #5976.
-class _AssertRaisesContext(object):
-    """A context manager used to implement TestCase.assertRaises* methods."""
-
-    def __init__(self, expected, test_case, expected_regexp=None):
-        self.expected = expected
-        self.failureException = test_case.failureException
-        self.expected_regexp = expected_regexp
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, tb):
-        if exc_type is None:
-            try:
-                exc_name = self.expected.__name__
-            except AttributeError:
-                exc_name = str(self.expected)
-            raise self.failureException(
-                "{0} not raised".format(exc_name))
-        if not issubclass(exc_type, self.expected):
-            # let unexpected exceptions pass through
-            return False
-        self.exception = exc_value # store for later retrieval
-        if self.expected_regexp is None:
-            return True
-
-        expected_regexp = self.expected_regexp
-        if isinstance(expected_regexp, basestring):
-            expected_regexp = re.compile(expected_regexp)
-        if not expected_regexp.search(str(exc_value)):
-            raise self.failureException('"%s" does not match "%s"' %
-                     (expected_regexp.pattern, str(exc_value)))
-        return True
 
 
 
@@ -207,37 +168,6 @@ class RequirementsTests(TestCase):
         supportedZopeInterfaceVersion = (4, 0, 0)
     else:
         supportedZopeInterfaceVersion = (3, 6, 0)
-
-    # Copied from 2.7 stdlib.  Delete after Python 2.6 is no longer a
-    # requirement.  See #5976.
-    def assertRaises(self, excClass, callableObj=None, *args, **kwargs):
-        """Fail unless an exception of class excClass is thrown
-           by callableObj when invoked with arguments args and keyword
-           arguments kwargs. If a different type of exception is
-           thrown, it will not be caught, and the test case will be
-           deemed to have suffered an error, exactly as for an
-           unexpected exception.
-
-           If called with callableObj omitted or None, will return a
-           context object used like this::
-
-                with self.assertRaises(SomeException):
-                    do_something()
-
-           The context manager keeps a reference to the exception as
-           the 'exception' attribute. This allows you to inspect the
-           exception after the assertion::
-
-               with self.assertRaises(SomeException) as cm:
-                   do_something()
-               the_exception = cm.exception
-               self.assertEqual(the_exception.error_code, 3)
-        """
-        context = _AssertRaisesContext(excClass, self)
-        if callableObj is None:
-            return context
-        with context:
-            callableObj(*args, **kwargs)
 
 
     def setUp(self):
@@ -666,13 +596,6 @@ class FakeZopeInterfaceTests(TestCase, ZopeInterfaceTestsMixin):
         _install(self.versions[version])
 
 
-# Python 2.6 stdlib unittest does not support skipping.  Use trial's
-# SynchronousTestCase instead.  When Python 2.6 support is dropped, this can
-# switch back to using the stdlib TestCase with its skip support.
-if sys.version_info[:2] == (2, 6):
-    from twisted.trial.unittest import SkipTest, SynchronousTestCase as TestCase
-else:
-    from unittest import SkipTest
 
 class RealZopeInterfaceTests(TestCase, ZopeInterfaceTestsMixin):
     """
