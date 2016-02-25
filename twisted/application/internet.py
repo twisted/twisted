a29e525fc271ad2b2d0999f082af1f885ba2e17c
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-02-25 16:57:41 +0000

    more conservative maxDelay.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/persistent-client-service-4735-5@46851 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/application/internet.py
+++ twisted/application/internet.py
@@ -490,7 +490,7 @@ class _DisconnectFactory(object):
 
 
 
-def backoffPolicy(initialDelay=1.0, maxDelay=3600.0, factor=1.5,
+def backoffPolicy(initialDelay=1.0, maxDelay=60.0, factor=1.5,
                   jitter=_goodEnoughRandom):
     """
     A timeout policy for L{ClientService} which computes an exponential backoff
@@ -498,18 +498,22 @@ def backoffPolicy(initialDelay=1.0, maxDelay=3600.0, factor=1.5,
 
     @param initialDelay: Delay for the first reconnection attempt (default
         1.0s).
+    @type initialDelay: L{float}
 
     @param maxDelay: Maximum number of seconds between connection attempts
-        (default 3600.0, or 1 hour).  Note that this value is before jitter is
-        applied, so the actual maximum possible delay is this value plus the
-        maximum possible result of C{jitter()}.
+        (default 60 seconds, or one minute).  Note that this value is before
+        jitter is applied, so the actual maximum possible delay is this value
+        plus the maximum possible result of C{jitter()}.
+    @type maxDelay: L{float}
 
     @param factor: A multiplicative factor by which the delay grows on each
         failed reattempt.  Default: 1.5.
+    @type factor: L{float}
 
     @param jitter: A 0-argument callable that introduces noise into the delay.
         By default, C{random.random}, i.e. a pseudorandom floating-point value
         between zero and one.
+    @type jitter: 0-argument callable returning L{float}
 
     @return: a 1-argument callable that, given an attempt count, returns a
         floating point number; the number of seconds to delay.
