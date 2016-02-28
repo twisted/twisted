7050b82208a170e00786b65f768cdaef12b597b8
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-02-28 03:16:00 +0000

    Merge old-versionpy-8219: Remove old _version.py files.
    Fixes: #8219
    Author: hawkowl
    Reviewer: mithrandi
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/release-16.0.0-8210@46861 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/__init__.py
+++ twisted/web/__init__.py
@@ -11,6 +11,13 @@ L{resource model<twisted.web.resource>}), and
 a L{web client<twisted.web.client>}.
 """
 
-from twisted.web._version import version
+from twisted.python.versions import Version
+from twisted.python.deprecate import deprecatedModuleAttribute
 
+from twisted._version import version
 __version__ = version.short()
+
+deprecatedModuleAttribute(
+    Version("Twisted", 16, 0, 0),
+    "Use twisted.__version__ instead.",
+    "twisted.web", "__version__")
