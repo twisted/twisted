c15292376f5996389ab3e32b3dd75160799dc100
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-03-08 16:22:18 +0000

    how about.......no
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/manhole-hardcodedkey-8229@46947 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/conch/manhole_ssh.py
+++ twisted/conch/manhole_ssh.py
@@ -118,29 +118,8 @@ class TerminalRealm:
         raise NotImplementedError()
 
 class ConchFactory(factory.SSHFactory):
-    publicKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az64fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkbh/C+BR3utDS555mV'
-
-    publicKeys = {
-        'ssh-rsa' : keys.Key.fromString(publicKey)
-    }
-    del publicKey
-
-    privateKey = """-----BEGIN RSA PRIVATE KEY-----
-MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
-4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
-vgUd7rQ0ueeZlQIBIwJgbh+1VZfr7WftK5lu7MHtqE1S1vPWZQYE3+VUn8yJADyb
-Z4fsZaCrzW9lkIqXkE3GIY+ojdhZhkO1gbG0118sIgphwSWKRxK0mvh6ERxKqIt1
-xJEJO74EykXZV4oNJ8sjAjEA3J9r2ZghVhGN6V8DnQrTk24Td0E8hU8AcP0FVP+8
-PQm/g/aXf2QQkQT+omdHVEJrAjEAy0pL0EBH6EVS98evDCBtQw22OZT52qXlAwZ2
-gyTriKFVoqjeEjt3SZKKqXHSApP/AjBLpF99zcJJZRq2abgYlf9lv1chkrWqDHUu
-DZttmYJeEfiFBBavVYIF1dOlZT0G8jMCMBc7sOSZodFnAiryP+Qg9otSBjJ3bQML
-pSTqy7c3a2AScC/YyOwkDaICHnnD3XyjMwIxALRzl0tQEKMXs6hH8ToUdlLROCrP
-EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
------END RSA PRIVATE KEY-----"""
-    privateKeys = {
-        'ssh-rsa' : keys.Key.fromString(privateKey)
-    }
-    del privateKey
+    publicKeys = {}
+    privateKeys = {}
 
     def __init__(self, portal):
         self.portal = portal
