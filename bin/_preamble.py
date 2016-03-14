875ae289108bf1a67b490203ce66644b72934e55
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-03-14 10:46:42 +0000

    make the bin scripts work
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/src-move-8214-2@46991 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- bin/_preamble.py
+++ bin/_preamble.py
@@ -13,7 +13,7 @@ import sys, os
 
 path = os.path.abspath(sys.argv[0])
 while os.path.dirname(path) != path:
-    if os.path.exists(os.path.join(path, 'twisted', '__init__.py')):
-        sys.path.insert(0, path)
+    if os.path.exists(os.path.join(path, 'src', 'twisted', '__init__.py')):
+        sys.path.insert(0, os.path.join(path, 'src'))
         break
     path = os.path.dirname(path)
