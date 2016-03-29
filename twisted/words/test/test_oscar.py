5d56806e4fe62a9c2c448f57092320ec674138ac
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-03-29 16:46:35 +0000

    deprecate
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/deprecate-oscar-8260@47105 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/words/test/test_oscar.py
+++ twisted/words/test/test_oscar.py
@@ -22,3 +22,22 @@ class PasswordTests(TestCase):
         self.assertEqual(
             encryptPasswordMD5('foo', 'bar').encode('hex'),
             'd73475c370a7b18c6c20386bcf1339f2')
+
+
+
+class OscarDeprecationTests(TestCase):
+    """
+    Tests for the deprecation of L{twisted.words.protocols.oscar}.
+    """
+    def test_deprecated(self):
+        """
+        L{twisted.words.protocols.oscar} is deprecated.
+        """
+        from twisted.words import protocols
+        protocols.oscar
+
+        warnings = self.flushWarnings([self.test_deprecated])
+        self.assertEqual(len(warnings), 1)
+        self.assertEqual(warnings[0]["message"], (
+            "twisted.words.protocols.oscar was deprecated in Twisted 16.2.0: "
+            "There is no replacement for this module."))
