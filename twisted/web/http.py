f1260ae58b7a12243527f571f6f7f9e48e33415e
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-04-05 16:05:55 +0000

    fix the bug
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/dropped-factory-8272@47151 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/http.py
+++ twisted/web/http.py
@@ -2262,6 +2262,16 @@ class _GenericHTTPChannelProtocol(proxyForInterface(IProtocol, "_channel")):
 
 
     @property
+    def factory(self):
+        return self._channel.factory
+
+
+    @factory.setter
+    def factory(self, value):
+        self._channel.factory = value
+
+
+    @property
     def requestFactory(self):
         return self._channel.requestFactory
 
