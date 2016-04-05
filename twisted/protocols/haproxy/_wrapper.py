b23293cad9b8864c752b1e4f09ec46a591c55786
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-04-05 22:59:19 +0000

    docstring
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/haproxy-endpoint-8203-2@47166 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/protocols/haproxy/_wrapper.py
+++ twisted/protocols/haproxy/_wrapper.py
@@ -98,6 +98,14 @@ class HAProxyWrappingFactory(policies.WrappingFactory):
 
 def proxyEndpoint(wrappedEndpoint):
     """
-    
+    Wrap an endpoint with PROXY protocol support, so that the transport's
+    C{getHost} and C{getPeer} methods reflect the attributes of the proxied
+    connection rather than the underlying connection.
+
+    @param wrappedEndpoint: The underlying listening endpoint.
+    @type wrappedEndpoint: L{IStreamServerEndpoint}
+
+    @return: a new listening endpoint that speaks the PROXY protocol.
+    @rtype: L{IStreamServerEndpoint}
     """
     return _WrapperServerEndpoint(wrappedEndpoint, HAProxyWrappingFactory)
