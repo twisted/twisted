0d6725342a0fb90c6deddd727ef3a0640e2c3fa5
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-04-06 04:44:50 +0000

    Merge dropped-factory-8272: Restore the factory being given to HTTPChannel
    Author: hawkowl
    Reviewer: lukasa
    Fixes: #8272
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/release-16.1.1-8275@47175 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/http.py
+++ twisted/web/http.py
@@ -2010,6 +2010,22 @@ class _GenericHTTPChannelProtocol(proxyForInterface(IProtocol, "_channel")):
 
 
     @property
+    def factory(self):
+        """
+        @see: L{HTTPChannel.factory}
+        """
+        return self._channel.factory
+
+
+    @factory.setter
+    def factory(self, value):
+        """
+        @see: L{HTTPChannel.factory}
+        """
+        self._channel.factory = value
+
+
+    @property
     def requestFactory(self):
         return self._channel.requestFactory
 
