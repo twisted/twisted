4008aca96b7ec7684a3d43c4791125b2b554524b
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-04-14 21:15:52 +0000

    Merge udpdocs3-8280: port udp examples to python 3
    Author: pawelmhm
    
    Reviewer: glyph
    
    Fixes: #8280
    
    The documentation examples for UDP now work on Python 3.
    
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@47236 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/core/howto/listings/udp/basic_example.py
+++ docs/core/howto/listings/udp/basic_example.py
@@ -3,9 +3,10 @@ from twisted.internet import reactor
 
 
 class Echo(DatagramProtocol):
+
     def datagramReceived(self, data, addr):
         print("received %r from %s" % (data, addr))
         self.transport.write(data, addr)
 
-reactor.listenUDP(9999, Echo(), interface='::')
+reactor.listenUDP(9999, Echo())
 reactor.run()
