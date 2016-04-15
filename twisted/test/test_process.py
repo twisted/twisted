f8469b7831b0f7398b7750db3bd342b1f2729a5b
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2016-04-15 15:25:36 +0000

    blah
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/moar-windows-8025-7@47252 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_process.py
+++ twisted/test/test_process.py
@@ -41,7 +41,7 @@ from twisted.python.log import msg
 from twisted.internet import reactor, protocol, error, interfaces, defer
 from twisted.trial import unittest
 from twisted.python import runtime, procutils
-from twisted.python.compat import _PY3, networkString, xrange, _maybeMBCS
+from twisted.python.compat import _PY3, networkString, xrange
 from twisted.python.filepath import FilePath, _asFilesystemBytes
 
 
@@ -2391,7 +2391,7 @@ class Dumbwin32procPidTests(unittest.TestCase):
 
         d = defer.Deferred()
         processProto = TrivialProcessProtocol(d)
-        comspec = _maybeMBCS(os.environ.get("COMSPEC", "cmd.exe"))
+        comspec = u"cmd.exe"
         cmd = [comspec, u"/c", pyExe, scriptPath]
 
         p = _dumbwin32proc.Process(
