d02659fe8d189854eb322ceadf33c930e6cb165b
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-05-26 01:54:41 -0700

    Replace dict.has_key() with the 'in' keyword
    This is recommended in PEP 290 for testing
    testing dictionary membership.
    
    Remove d.has_keyx() from test_mail.py, since
    it already tests for "x in d"

--- twisted/mail/pb.py
+++ twisted/mail/pb.py
@@ -98,10 +98,10 @@ class MaildirBroker(pb.Broker):
             self.sendAnswer(requestID, collection)
 
     def getCollection(self, name, domain, password):
-        if not self.domains.has_key(domain):
+        if domain not in self.domains:
             return
         domain = self.domains[domain]
-        if (domain.dbm.has_key(name) and
+        if (name in domain.dbm and
             domain.dbm[name] == password):
             return MaildirCollection(domain.userDirectory(name))
 
