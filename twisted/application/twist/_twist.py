52ec86699e044c45a4cfcabbb02de02d9e85660a
Wilfredo Sánchez Vega <wsanchez@wsanchez.net>
2016-06-03 17:08:19 -0700

    Factor into bits

--- twisted/application/twist/_twist.py
+++ twisted/application/twist/_twist.py
@@ -19,54 +19,82 @@ class Twist(object):
     Run a Twisted application.
     """
 
-    @classmethod
-    def main(cls, argv=sys.argv):
-
-        # Parse command line
-
-        twistOptions = TwistOptions()
+    @staticmethod
+    def options(argv):
+        """
+        Parse command line options.
+        """
+        options = TwistOptions()
 
         try:
-            twistOptions.parseOptions(argv[1:])
+            options.parseOptions(argv[1:])
         except UsageError as e:
-            exit(ExitStatus.EX_USAGE, "Error: {}\n\n{}".format(e, twistOptions))
+            exit(ExitStatus.EX_USAGE, "Error: {}\n\n{}".format(e, options))
 
-        # Configure the runner based on the command line options
+        return options
 
-        reactor = twistOptions["reactor"]
 
-        runnerOptions = {
-            RunnerOptions.reactor: reactor,
-        }
+    @staticmethod
+    def service(plugin, options):
+        """
+        Create the application service.
+        """
+        service = plugin.makeService(options)
+        application = Application(plugin.tapname)
+        service.setServiceParent(application)
+
+        return IService(application)
+
+
+    @staticmethod
+    def startService(reactor, service):
+        # Start the service
+        service.privilegedStartService()
+
+        # Ask the reactor to stop the service before shutting down
+        reactor.addSystemEventTrigger(
+            "before", "shutdown", service.stopService
+        )
+
+
+    @staticmethod
+    def runnerOptions(twistOptions):
+        """
+        Take options obtained from command line and configure options for the
+        application runner.
+        """
+        runnerOptions = {}
 
         for runnerOpt, twistOpt in (
+            (RunnerOptions.reactor, "reactor"),
             (RunnerOptions.defaultLogLevel, "logLevel"),
             (RunnerOptions.logFile, "logFile"),
             (RunnerOptions.fileLogObserverFactory, "fileLogObserverFactory"),
         ):
             runnerOptions[runnerOpt] = twistOptions[twistOpt]
 
-        # Set up application service
+        return runnerOptions
 
-        plugin = twistOptions.loadedPlugins[twistOptions.subCommand]
-        service = plugin.makeService(twistOptions.subOptions)
-        application = Application(plugin.tapname)
-        service.setServiceParent(application)
 
-        # Start the service
+    @staticmethod
+    def run(options):
+        runner = Runner(options)
+        runner.run()
 
-        IService(application).privilegedStartService()
 
-        # Ask the reactor to stop the service before shutting down
+    @classmethod
+    def main(cls, argv=sys.argv):
+        twistOptions = cls.options(argv)
 
-        reactor.addSystemEventTrigger(
-            "before", "shutdown", IService(application).stopService
+        reactor = twistOptions["reactor"]
+        service = cls.service(
+            plugin=twistOptions.loadedPlugins[twistOptions.subCommand],
+            options=twistOptions.subOptions,
         )
 
-        # Instantiate and run the runner
+        cls.startService(reactor, service)
 
-        runner = Runner(runnerOptions)
-        runner.run()
+        cls.run(cls.runnerOptions(twistOptions))
 
 
 
