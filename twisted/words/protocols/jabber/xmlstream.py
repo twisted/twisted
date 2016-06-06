1143f03f2b2c67059de6bfcad7bcbb7824bf1c98
Ralph Meijer <ralphm@ik.nu>
2016-06-06 10:19:52 +0200

    Port twisted.words.protocols.jabber.error to Python 3.

--- twisted/words/protocols/jabber/xmlstream.py
+++ twisted/words/protocols/jabber/xmlstream.py
@@ -17,6 +17,7 @@ from zope.interface import directlyProvides, implementer
 from twisted.internet import defer, protocol
 from twisted.internet.error import ConnectionLost
 from twisted.python import failure, log, randbytes
+from twisted.python.compat import intern
 from twisted.words.protocols.jabber import error, ijabber, jid
 from twisted.words.xish import domish, xmlstream
 from twisted.words.xish.xmlstream import STREAM_CONNECTED_EVENT
@@ -949,6 +950,7 @@ class XMPPHandlerCollection(object):
     @type handlers: C{list} of objects providing
                       L{IXMPPHandler}
     """
+
     def __init__(self):
         self.handlers = []
 
