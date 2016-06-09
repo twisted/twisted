aa691db11ddaa428384e883fa9e1529a615ac9ca
Adi Roiban <adi.roiban@chevah.com>
2016-06-09 11:53:47 +0100

    Add docstring.

--- twisted/trial/_synctest.py
+++ twisted/trial/_synctest.py
@@ -796,6 +796,15 @@ class _Assertions(pyunit.TestCase, object):
         """
         Fail the test if a C{regexp} search of C{text} fails.
 
+        @param text: Text which is under test.
+        @type text: L{str}
+
+        @param regex: A regular expression object or a string containing a
+            regular expression suitable for use by re.search().
+        @type regex: L{str} or L{re.RegexObject}
+
+        @param msg: Text used as the error message on failure.
+        @type msg: L{str}
         """
         if sys.version_info[:2] > (2, 7):
             super(_Assertions, self).assertRegex(text, regex, msg)
