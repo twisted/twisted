15049ebe1860735d4faadcd90c1f7695af2dca09
Adi Roiban <adi.roiban@chevah.com>
2016-06-29 13:30:35 +0100

    Clean the copied _pydoctor code.

--- twisted/python/_pydoctor.py
+++ twisted/python/_pydoctor.py
@@ -4,40 +4,13 @@
 
 """
 Support for a few things specific to documenting Twisted using pydoctor.
+
+FIXME: https://github.com/twisted/pydoctor/issues/106
+This documentation does not link to pydoctor API as there is no public API yet.
 """
 
-from pydoctor import model, ast_pp, zopeinterface
+from pydoctor import model, zopeinterface
 from pydoctor.sphinx import SphinxInventory
-
-
-
-class TwistedModuleVisitor(zopeinterface.ZopeInterfaceModuleVisitor):
-
-    def visitCallFunc_twisted_python_util_moduleMovedForSplit(self, funcName, node):
-        # XXX this is rather fragile...
-        origModuleName, newModuleName, moduleDesc, \
-                        projectName, projectURL, globDict = node.args
-        moduleDesc = ast_pp.pp(moduleDesc)[1:-1]
-        projectName = ast_pp.pp(projectName)[1:-1]
-        projectURL = ast_pp.pp(projectURL)[1:-1]
-        modoc = """
-%(moduleDesc)s
-
-This module is DEPRECATED. It has been split off into a third party
-package, Twisted %(projectName)s. Please see %(projectURL)s.
-
-This is just a place-holder that imports from the third-party %(projectName)s
-package for backwards compatibility. To use it, you need to install
-that package.
-""" % {'moduleDesc': moduleDesc,
-       'projectName': projectName,
-       'projectURL': projectURL}
-        self.builder.current.docstring = modoc
-
-
-
-class TwistedASTBuilder(zopeinterface.ZopeInterfaceASTBuilder):
-    ModuleVistor = TwistedModuleVisitor
 
 
 
@@ -52,7 +25,14 @@ class TwistedSphinxInventory(SphinxInventory):
 
     def getLink(self, name):
         """
-        Return URL for `name` or None if no URL is found.
+        Resolve the full URL for a cross reference.
+
+        @param name: Value of the cross reference.
+        @type name: L{str}
+
+        @return: A full URL for the I{name} reference or L{None} if no link was
+            found.
+        @rtype: L{str} or L{None}
         """
         result = super(TwistedSphinxInventory, self).getLink(name)
         if result is not None:
@@ -66,21 +46,21 @@ class TwistedSphinxInventory(SphinxInventory):
             # as an exceptional case.
             # We get the base URL from IInterface which is assume that is
             # always and already well defined in the Sphinx index.
-            base_url, _ = self._links.get(
+            baseURL, _ = self._links.get(
                 'zope.interface.interfaces.IInterface')
 
             if name  == 'zope.interface.adapter.AdapterRegistry':
                 # FIXME:
                 # https://github.com/zopefoundation/zope.interface/issues/41
-                relative_link = 'adapter.html'
+                relativeLink = 'adapter.html'
             else:
                 # Not a known exception.
-                relative_link = None
+                relativeLink = None
 
-            if relative_link is None:
+            if relativeLink is None:
                 return None
 
-            return '%s/%s' % (base_url, relative_link)
+            return '%s/%s' % (baseURL, relativeLink)
 
         return result
 
@@ -90,7 +70,6 @@ class TwistedSystem(zopeinterface.ZopeInterfaceSystem):
     """
     Maybe class used to set up all pydoctor system.
     """
-    defaultBuilder = TwistedASTBuilder
 
     def __init__(self, options=None):
         super(TwistedSystem, self).__init__(options=options)
@@ -99,18 +78,30 @@ class TwistedSystem(zopeinterface.ZopeInterfaceSystem):
         self.intersphinx = TwistedSphinxInventory(
             logger=self.msg, project_name=self.projectname)
 
+
     def privacyClass(self, obj):
+        """
+        Report the privacy level for an object.
+
+        Hide all tests with the exception of C{t.test.proto_helpers}.
+
+        param obj: Object for which the privacy is reported.
+        type obj: L{object}
+
+        rtype: C{model.PrivacyClass} member
+        """
         o = obj
+
         if o.fullName() == 'twisted.test':
             # Match this package exactly, so that proto_helpers
             # below is visible
             return model.PrivacyClass.VISIBLE
+
         while o:
-            if o.fullName() == 'twisted.words.xish.yappsrt':
-                return model.PrivacyClass.HIDDEN
             if o.fullName() == 'twisted.test.proto_helpers':
                 return model.PrivacyClass.VISIBLE
             if isinstance(o, model.Package) and o.name == 'test':
                 return model.PrivacyClass.HIDDEN
             o = o.parent
+
         return super(TwistedSystem, self).privacyClass(obj)
