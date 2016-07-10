138bd9ba8b5df98f9f2b6c49a068d64c8f555314
Adi Roiban <adi.roiban@chevah.com>
2016-07-10 22:02:36 +0100

    Allow calling for sub-dirs.

---
+++ admin/twistedchecker-trunk-diff.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+#
+# Helper for running twistedchecker and reporting only errors that are part
+# of the changes since trunk.
+#
+# Call it as ./admin/twistedchecker-trunk-diff.sh twisted
+#
+# You can also get errors only for a subset of the files using:
+#
+# ./admin/twistedchecker-trunk-diff.sh twisted/words
+#
+target=$1
+mkdir -p build/trunk-checkout
+cd build/trunk-checkout
+git --work-tree . checkout origin/trunk .
+twistedchecker $target > baseline.result
+cd ../..
+twistedchecker --diff=build/trunk-checkout/baseline.result $target
+exit $?
