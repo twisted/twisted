b8f5e507c2f19044f66ebca266d9f390edc99f95
Cory Benfield <lukasaoz@gmail.com>
2016-07-13 11:46:39 +0100

    Define ITLSContextFactory.

--- twisted/internet/interfaces.py
+++ twisted/internet/interfaces.py
@@ -2256,6 +2256,28 @@ class IProtocolNegotiationFactory(Interface):
 
 
 
+class ITLSContextFactory(Interface):
+    """
+    A provider of L{ITLSContextFactory} is capable of generating
+    L{OpenSSL.SSL.Context} classes suitable for configuring TLS on a
+    connection. A provider will store enough state to be able to generate these
+    contexts as needed for individual connections.
+
+    @see: L{twisted.internet.ssl}
+    """
+
+    def getContext():
+        """
+        Returns a TLS context object, suitable for securing a TLS connection.
+        This context object will be appropriately customized for the connection
+        based on the state in this object.
+
+        @return: A TLS context object.
+        @rtype: L{OpenSSL.SSL.Context}
+        """
+
+
+
 class ITLSTransport(ITCPTransport):
     """
     A TCP transport that supports switching to TLS midstream.
