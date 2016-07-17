b0a8f5d692acad907bbbec6a4f806b2ab2e95683
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-07-17 12:52:19 +0200

    awful hacks!!!!

--- twisted/internet/test/test_await.py
+++ twisted/internet/test/test_await.py
@@ -7,7 +7,15 @@ includes keywords not valid in Pythons before 3.5.
 """
 
 try:
-    from twisted.internet.test._awaittests import AwaitTests
+    from twisted.python.compat import execfile
+    from twisted.python.filepath import FilePath
+
+    _path = FilePath(__file__).parent().child("_awaittests.py.3only")
+
+    _g = {}
+    execfile(_path.path, _g)
+
+    AwaitTests = _g["AwaitTests"]
     __all__ = ["AwaitTests"]
 except:
-    pass
+    __all__ = []
