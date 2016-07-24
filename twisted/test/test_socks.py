11a5fb25b429eef6ba9a65e40e129367ff7d2711
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-07-24 06:11:10 -0700

    Re-order imports

--- twisted/test/test_socks.py
+++ twisted/test/test_socks.py
@@ -8,11 +8,11 @@ SOCKSv4a protocols.
 
 import struct, socket
 
-from twisted.trial import unittest
-from twisted.test import proto_helpers
 from twisted.internet import defer, address
 from twisted.internet.error import DNSLookupError
 from twisted.protocols import socks
+from twisted.test import proto_helpers
+from twisted.trial import unittest
 
 
 class StringTCPTransport(proto_helpers.StringTransport):
