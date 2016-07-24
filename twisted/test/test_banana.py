d5ac4da159a0fb291de27cdec9a0536725290be8
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-07-24 15:14:05 -0700

    Use byte strings

--- twisted/test/test_banana.py
+++ twisted/test/test_banana.py
@@ -275,7 +275,8 @@ class BananaTests(BananaTestBase):
 
 
     def test_list(self):
-        foo = [1, 2, [3, 4], [30.5, 40.2], 5, ["six", "seven", ["eight", 9]], [10], []]
+        foo = ([1, 2, [3, 4], [30.5, 40.2], 5,
+               [b"six", b"seven", [b"eight", 9]], [10], []])
         self.enc.sendEncoded(foo)
         self.enc.dataReceived(self.io.getvalue())
         assert self.result == foo, "%s!=%s" % (repr(self.result), repr(foo))
@@ -287,7 +288,7 @@ class BananaTests(BananaTestBase):
         data is not split.
         """
         foo = [1, 2, [3, 4], [30.5, 40.2], 5,
-               ["six", "seven", ["eight", 9]], [10],
+               [b"six", b"seven", [b"eight", 9]], [10],
                # TODO: currently the C implementation's a bit buggy...
                sys.maxint * 3, sys.maxint * 2, sys.maxint * -2]
         self.enc.sendEncoded(foo)
@@ -359,25 +360,25 @@ class BananaTests(BananaTestBase):
         baseIntIn = +2147483647
         baseNegIn = -2147483648
 
-        baseIntOut = '\x7f\x7f\x7f\x07\x81'
-        self.assertEqual(self.encode(baseIntIn - 2), '\x7d' + baseIntOut)
-        self.assertEqual(self.encode(baseIntIn - 1), '\x7e' + baseIntOut)
-        self.assertEqual(self.encode(baseIntIn - 0), '\x7f' + baseIntOut)
+        baseIntOut = b'\x7f\x7f\x7f\x07\x81'
+        self.assertEqual(self.encode(baseIntIn - 2), b'\x7d' + baseIntOut)
+        self.assertEqual(self.encode(baseIntIn - 1), b'\x7e' + baseIntOut)
+        self.assertEqual(self.encode(baseIntIn - 0), b'\x7f' + baseIntOut)
 
-        baseLongIntOut = '\x00\x00\x00\x08\x85'
-        self.assertEqual(self.encode(baseIntIn + 1), '\x00' + baseLongIntOut)
-        self.assertEqual(self.encode(baseIntIn + 2), '\x01' + baseLongIntOut)
-        self.assertEqual(self.encode(baseIntIn + 3), '\x02' + baseLongIntOut)
+        baseLongIntOut = b'\x00\x00\x00\x08\x85'
+        self.assertEqual(self.encode(baseIntIn + 1), b'\x00' + baseLongIntOut)
+        self.assertEqual(self.encode(baseIntIn + 2), b'\x01' + baseLongIntOut)
+        self.assertEqual(self.encode(baseIntIn + 3), b'\x02' + baseLongIntOut)
 
-        baseNegOut = '\x7f\x7f\x7f\x07\x83'
-        self.assertEqual(self.encode(baseNegIn + 2), '\x7e' + baseNegOut)
-        self.assertEqual(self.encode(baseNegIn + 1), '\x7f' + baseNegOut)
-        self.assertEqual(self.encode(baseNegIn + 0), '\x00\x00\x00\x00\x08\x83')
+        baseNegOut = b'\x7f\x7f\x7f\x07\x83'
+        self.assertEqual(self.encode(baseNegIn + 2), b'\x7e' + baseNegOut)
+        self.assertEqual(self.encode(baseNegIn + 1), b'\x7f' + baseNegOut)
+        self.assertEqual(self.encode(baseNegIn + 0), b'\x00\x00\x00\x00\x08\x83')
 
-        baseLongNegOut = '\x00\x00\x00\x08\x86'
-        self.assertEqual(self.encode(baseNegIn - 1), '\x01' + baseLongNegOut)
-        self.assertEqual(self.encode(baseNegIn - 2), '\x02' + baseLongNegOut)
-        self.assertEqual(self.encode(baseNegIn - 3), '\x03' + baseLongNegOut)
+        baseLongNegOut = b'\x00\x00\x00\x08\x86'
+        self.assertEqual(self.encode(baseNegIn - 1), b'\x01' + baseLongNegOut)
+        self.assertEqual(self.encode(baseNegIn - 2), b'\x02' + baseLongNegOut)
+        self.assertEqual(self.encode(baseNegIn - 3), b'\x03' + baseLongNegOut)
 
 
 
