31bd6cda2e71d5359e028905dd54c3ff3eea46aa
Einar Floystad Dorum <edorum@cisco.com>
2016-07-25 23:33:28 +0200

    Adding write and writeSequence test for LoopbackRelay

--- twisted/test/test_loopback.py
+++ twisted/test/test_loopback.py
@@ -426,3 +426,40 @@ class LoopbackUNIXTests(LoopbackTestCaseMixin, unittest.TestCase):
 
     if interfaces.IReactorUNIX(reactor, None) is None:
         skip = "Current reactor does not support UNIX sockets"
+
+
+class LoopbackRelayTest(unittest.TestCase):
+    """
+    Test for L{twisted.protocols.loopback.LoopbackRelay}
+    """
+    class Receiver(Protocol):
+        """
+        Simple Receiver class used for testing LoopbackRelay
+        """
+        data = b''
+        def dataReceived(self, data):
+            "Accumulate received data for verification"
+            self.data += data
+
+    def test_write(self):
+        "Test to verify that the write function works as expected"
+        receiver = self.Receiver()
+        relay = loopback.LoopbackRelay(receiver)
+        relay.write(b'abc')
+        relay.write(b'def')
+        self.assertEqual(receiver.data, b'')
+        relay.clearBuffer()
+        self.assertEqual(receiver.data, b'abcdef')
+
+    def test_writeSequence(self):
+        "Test to verify that the writeSequence function works as expected"
+        receiver = self.Receiver()
+        relay = loopback.LoopbackRelay(receiver)
+        relay.writeSequence(
+            [b'The ', b'quick ', b'brown ', b'fox '])
+        relay.writeSequence(
+            [b'jumps ', b'over ', b'the lazy dog'])
+        self.assertEqual(receiver.data, b'')
+        relay.clearBuffer()
+        self.assertEqual(
+            receiver.data, b'The quick brown fox jumps over the lazy dog')
