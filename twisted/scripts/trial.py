80b0f9aaf5f52606fecd79f89c21b9ec06cebf2c
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-07-31 06:41:05 -0700

    Use console script entry point to allow "python -m twisted.scripts.trial" to invoke trial

--- twisted/scripts/trial.py
+++ twisted/scripts/trial.py
@@ -619,3 +619,12 @@ def run():
         results.write_results(show_missing=1, summary=False,
                               coverdir=config.coverdir().path)
     sys.exit(not test_result.wasSuccessful())
+
+
+
+if __name__ == '__main__':
+    from pkg_resources import load_entry_point
+
+    sys.exit(
+        load_entry_point('Twisted', 'console_scripts', 'trial')()
+    )
