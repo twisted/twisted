b51adec12bbcb80009a9e53db5cb6e7dd2eb136f
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-08-03 08:12:52 +0800

    don't use a try block

--- twisted/internet/test/test_await.py
+++ twisted/internet/test/test_await.py
@@ -6,7 +6,11 @@ A wrapper for L{twisted.internet.test._awaittests}, as that test module
 includes keywords not valid in Pythons before 3.5.
 """
 
-try:
+from __future__ import absolute_import, division
+
+import sys
+
+if sys.version_info > (3, 5):
     from twisted.python.compat import execfile
     from twisted.python.filepath import FilePath
 
@@ -14,8 +18,19 @@ try:
 
     _g = {}
     execfile(_path.path, _g)
-
     AwaitTests = _g["AwaitTests"]
-    __all__ = ["AwaitTests"]
-except:
-    __all__ = []
+
+else:
+    from twisted.trial.unittest import TestCase
+
+    class AwaitTests(TestCase):
+        skip = "async/await is not available before Python 3.5"
+
+        def test_notAvailable(self):
+            """
+            A skipped test to show that this was not ran because the Python is
+            too old.
+            """
+            pass
+
+__all__ = ["AwaitTests"]
