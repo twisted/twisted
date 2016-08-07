b1fddffee1fcd0866a809c991169a0f65b302324
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-08-07 15:11:49 -0700

    Fix whitespace

--- twisted/protocols/wire.py
+++ twisted/protocols/wire.py
@@ -19,7 +19,9 @@ from twisted.python.compat import _PY3
 
 
 class Echo(protocol.Protocol):
-    """As soon as any data is received, write it back (RFC 862)"""
+    """
+    As soon as any data is received, write it back (RFC 862).
+    """
 
     def dataReceived(self, data):
         self.transport.write(data)
@@ -27,7 +29,9 @@ class Echo(protocol.Protocol):
 
 
 class Discard(protocol.Protocol):
-    """Discard any received data (RFC 863)"""
+    """
+    Discard any received data (RFC 863).
+    """
 
     def dataReceived(self, data):
         # I'm ignoring you, nyah-nyah
@@ -37,17 +41,22 @@ class Discard(protocol.Protocol):
 
 @implementer(interfaces.IProducer)
 class Chargen(protocol.Protocol):
-    """Generate repeating noise (RFC 864)"""
+    """
+    Generate repeating noise (RFC 864).
+    """
     noise = r'@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ !"#$%&?'
 
     def connectionMade(self):
         self.transport.registerProducer(self, 0)
 
+
     def resumeProducing(self):
         self.transport.write(self.noise)
 
+
     def pauseProducing(self):
         pass
+
 
     def stopProducing(self):
         pass
@@ -55,42 +64,56 @@ class Chargen(protocol.Protocol):
 
 
 class QOTD(protocol.Protocol):
-    """Return a quote of the day (RFC 865)"""
+    """
+    Return a quote of the day (RFC 865).
+    """
 
     def connectionMade(self):
         self.transport.write(self.getQuote())
         self.transport.loseConnection()
 
+
     def getQuote(self):
-        """Return a quote. May be overrriden in subclasses."""
+        """
+        Return a quote. May be overrriden in subclasses.
+        """
         return "An apple a day keeps the doctor away.\r\n"
 
 
 
 class Who(protocol.Protocol):
-    """Return list of active users (RFC 866)"""
+    """
+    Return list of active users (RFC 866)
+    """
 
     def connectionMade(self):
         self.transport.write(self.getUsers())
         self.transport.loseConnection()
 
+
     def getUsers(self):
-        """Return active users. Override in subclasses."""
+        """
+        Return active users. Override in subclasses.
+        """
         return "root\r\n"
 
 
 
 class Daytime(protocol.Protocol):
-    """Send back the daytime in ASCII form (RFC 867)"""
+    """
+    Send back the daytime in ASCII form (RFC 867).
+    """
 
     def connectionMade(self):
-        self.transport.write(time.asctime(time.gmtime(time.time())) + '\r\n')
+        self.transport.write(time.asctime(time.gmtime(time.time())) + b'\r\n')
         self.transport.loseConnection()
 
 
 
 class Time(protocol.Protocol):
-    """Send back the time in machine readable form (RFC 868)"""
+    """
+    Send back the time in machine readable form (RFC 868).
+    """
 
     def connectionMade(self):
         # is this correct only for 32-bit machines?
