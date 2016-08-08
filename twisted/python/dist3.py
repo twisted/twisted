626267b9b78a7f2a3b5ac87151cd03939930e25f
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-08-07 18:34:10 -0700

    Add twisted.internet.test.test_pollingfile to list of tests which run on Python 3

--- twisted/python/dist3.py
+++ twisted/python/dist3.py
@@ -438,6 +438,7 @@ testModules = [
     "twisted.internet.test.test_kqueuereactor",
     "twisted.internet.test.test_main",
     "twisted.internet.test.test_newtls",
+    "twisted.internet.test.test_pollingfile",
     "twisted.internet.test.test_posixbase",
     "twisted.internet.test.test_posixprocess",
     "twisted.internet.test.test_process",
