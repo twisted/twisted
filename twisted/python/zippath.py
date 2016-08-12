3c5debe18341be6048031fc4ae92086452b74852
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-08-11 17:08:24 -0700

    If listdir() fails, raise an UnlistableError instead of OSError.
    This fixes the test_zippath test on Windows with Python 3.5

--- twisted/python/zippath.py
+++ twisted/python/zippath.py
@@ -19,6 +19,7 @@ from zipfile import ZipFile
 from twisted.python.compat import comparable, cmp
 from twisted.python.filepath import IFilePath, FilePath, AbstractFilePath
 from twisted.python.filepath import _coerceToFilesystemEncoding
+from twisted.python.filepath import UnlistableError
 
 from zope.interface import implementer
 
@@ -128,9 +129,11 @@ class ZipPath(AbstractFilePath):
             if self.isdir():
                 return list(self.archive.childmap[self.pathInArchive].keys())
             else:
-                raise OSError(errno.ENOTDIR, "Leaf zip entry listed")
+                raise UnlistableError(
+                    OSError(errno.ENOTDIR, "Leaf zip entry listed"))
         else:
-            raise OSError(errno.ENOENT, "Non-existent zip entry listed")
+            raise UnlistableError(
+                OSError(errno.ENOENT, "Non-existent zip entry listed"))
 
 
     def splitext(self):
