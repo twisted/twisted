e9639017b28ab68dc289db18d8dbf2ca93ea0422
acabhishek942 <acabhishek942@gmail.com>
2016-08-16 19:05:49 +0530

    use args[0] instead of message for py3k compatibility

--- twisted/conch/test/test_keys.py
+++ twisted/conch/test/test_keys.py
@@ -609,7 +609,7 @@ xEm4DxjEoaIp8dW/JOzXQ2EF+WaSOgdYsw3Ac+rnnjnNptCdOEDGP6QBkt+oXj4P
         with self.assertRaises(keys.BadFingerPrintFormat) as em:
             keys.Key(self.rsaObj).fingerprint('sha256-base')
         self.assertEqual('Unsupported fingerprint format: sha256-base',
-            em.exception.message)
+            em.exception.args[0])
 
 
     def test_type(self):
