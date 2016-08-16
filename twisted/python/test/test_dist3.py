afef37488b378c0cf5223825e253f02ac96e0c6b
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-08-16 16:48:04 +1000

    fix tests

--- twisted/python/test/test_dist3.py
+++ twisted/python/test/test_dist3.py
@@ -11,6 +11,7 @@ import os
 import twisted
 
 from twisted.trial.unittest import TestCase
+from twisted.python.compat import _PY3
 from twisted.python._dist3 import notPortedModules
 
 
@@ -20,7 +21,7 @@ class ModulesToInstallTests(TestCase):
     """
     def test_exist(self):
         """
-        All modules listed in L{notPortedModules} exist.
+        All modules listed in L{notPortedModules} exist on Py2.
         """
         root = os.path.dirname(os.path.dirname(twisted.__file__))
         for module in notPortedModules:
@@ -32,3 +33,23 @@ class ModulesToInstallTests(TestCase):
             self.assertTrue(os.path.exists(path) or
                             os.path.exists(packagePath),
                             "Module {0} does not exist".format(module))
+
+    def test_notexist(self):
+        """
+        All modules listed in L{notPortedModules} do not exist on Py3.
+        """
+        root = os.path.dirname(os.path.dirname(twisted.__file__))
+        for module in notPortedModules:
+            segments = module.split(".")
+            segments[-1] += ".py"
+            path = os.path.join(root, *segments)
+            alternateSegments = module.split(".") + ["__init__.py"]
+            packagePath = os.path.join(root, *alternateSegments)
+            self.assertFalse(os.path.exists(path) or
+                             os.path.exists(packagePath),
+                             "Module {0} exists".format(module))
+
+    if _PY3:
+        test_exist.skip = "Only on Python 2"
+    else:
+        test_notexist.skip = "Only on Python 3"
