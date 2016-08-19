22b7d166503d874be35eabd36fc77e8409f27171
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-08-20 00:56:01 +0800

    don't need this anymore

--- src/twisted/python/dist3.py
+++ src/twisted/python/dist3.py
@@ -191,7 +191,6 @@ modules = [
     "twisted.mail.__init__",
     "twisted.mail.test.__init__",
     "twisted.mail.interfaces",
-    "twisted.mail.protocols",
     "twisted.mail.smtp",
     "twisted.mail._cred",
     "twisted.mail._except",
