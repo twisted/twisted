6eb32f5b440b2198dd2fb622c8e14612d3856897
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-08-25 22:08:14 +0800

    coding standards

--- src/twisted/mail/_except.py
+++ src/twisted/mail/_except.py
@@ -7,12 +7,20 @@ Exceptions in L{twisted.mail}.
 
 from __future__ import absolute_import, division
 
+
 class IMAP4Exception(Exception):
-    def __init__(self, *args):
-        Exception.__init__(self, *args)
+    pass
 
-class IllegalClientResponse(IMAP4Exception): pass
 
-class IllegalOperation(IMAP4Exception): pass
+class IllegalClientResponse(IMAP4Exception):
+    pass
 
-class IllegalMailboxEncoding(IMAP4Exception): pass
+
+
+class IllegalOperation(IMAP4Exception):
+    pass
+
+
+
+class IllegalMailboxEncoding(IMAP4Exception):
+    pass
