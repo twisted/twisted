86e6e8f60abb196ae2da5f9f9cf51ab80e4bc786
Glyph <glyph@twistedmatrix.com>
2016-09-07 15:50:23 -0700

    not that branch of the 'if'.

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,14 +8,15 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     python -m pip install --user virtualenv
     python -m virtualenv ~/.venv
 
-    # Temporary workaround for https://github.com/pypa/setuptools/issues/776;
-    # install (and thereby cache a built wheel of) cryptography.  (NB: We're
-    # already using the same Python version in this venv as in the test env,
-    # thanks to travis.yml).
-    python -m pip install 'setuptools<26'
-    python -m pip install cryptography
-
     source ~/.venv/bin/activate
 fi
+
+# Temporary workaround for https://github.com/pypa/setuptools/issues/776;
+# install (and thereby cache a built wheel of) cryptography.  (NB: We're
+# already using the same Python version in this venv as in the test env,
+# thanks to travis.yml).
+
+pip install 'setuptools<26'
+pip install cryptography
 
 pip install $@
