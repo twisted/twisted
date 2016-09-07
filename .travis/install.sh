124c536fd461686617bef96657fd5d5604dc2929
Glyph <glyph@twistedmatrix.com>
2016-09-07 15:45:52 -0700

    how about here instead

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,6 +7,14 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     python get-pip.py --user
     python -m pip install --user virtualenv
     python -m virtualenv ~/.venv
+
+    # Temporary workaround for https://github.com/pypa/setuptools/issues/776;
+    # install (and thereby cache a built wheel of) cryptography.  (NB: We're
+    # already using the same Python version in this venv as in the test env,
+    # thanks to travis.yml).
+    python -m pip install 'setuptools<26'
+    python -m pip install cryptography
+
     source ~/.venv/bin/activate
 fi
 
