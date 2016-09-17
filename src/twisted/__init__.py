e624b785b1c765ba40bbeba17240b071fc0681a5
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-09-18 04:20:25 +0800

    erm we still need this check though

--- src/twisted/__init__.py
+++ src/twisted/__init__.py
@@ -7,6 +7,22 @@
 Twisted: The Framework Of Your Internet.
 """
 
+def _checkRequirements():
+    # Don't allow the user to run a version of Python we don't support.
+    import sys
+
+    version = getattr(sys, "version_info", (0,))
+    if version < (2, 7):
+        raise ImportError("Twisted requires Python 2.7 or later.")
+    elif version >= (3, 0) and version < (3, 3):
+        raise ImportError("Twisted on Python 3 requires Python 3.3 or later.")
+    if version < (3, 0):
+        required = "3.6.0"
+    else:
+        required = "4.0.0"
+
+_checkRequirements()
+
 # setup version
 from twisted._version import __version__ as version
 __version__ = version.short()
