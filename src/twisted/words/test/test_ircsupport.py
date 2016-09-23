b5efb59725a7ca16727464452929e5b870782a7b
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-09-23 15:16:56 -0700

    Add more tests for ircsupport

--- src/twisted/words/test/test_ircsupport.py
+++ src/twisted/words/test/test_ircsupport.py
@@ -270,3 +270,23 @@ class IRCProtoTests(IRCTestCase):
         person.sendMessage("smiles", {"style": "emote"})
         self.assertEqual(self.transport.io.getvalue(),
             b'PRIVMSG alice :\x01ACTION smiles\x01\r\n')
+
+
+    def test_sendGroupMessage(self):
+        """
+        L{IRCGroup.sendGroupMessage}
+        """
+        self.proto.makeConnection(self.transport)
+        group = self.proto.chat.getGroup("#foo", self.proto)
+        self.assertRaises(OfflineError, group.sendGroupMessage, "Some message")
+
+        group.account.client = self.proto
+        self.transport.clear()
+        group.sendGroupMessage("Some message 2")
+        self.assertEqual(self.transport.io.getvalue(),
+            b'PRIVMSG #foo :Some message 2\r\n')
+
+        self.transport.clear()
+        group.sendGroupMessage("smiles", {"style": "emote"})
+        self.assertEqual(self.transport.io.getvalue(),
+            b'PRIVMSG #foo :\x01ACTION smiles\x01\r\n')
