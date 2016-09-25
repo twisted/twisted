c0cc4ec1753cd18726bbd953fc8abe0ecd583146
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-09-25 19:05:54 +0800

    any day now

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,6 +2,16 @@
 set -e
 set -x
 
-echo $PATH
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    # Initialize the virtualenv created at install time.
+    source ~/.venv/bin/activate
+
+    if [[ "${TOXENV}" == "py35-alldeps-withcov-macos,codecov-publish" ]]; then
+        # Add pyenv path
+        PYENV_ROOT="$HOME/.pyenv";
+        PATH="$PYENV_ROOT/bin:$PATH";
+        eval "$(pyenv init -)";
+    fi
+fi
 
 tox -- $TOX_FLAGS
