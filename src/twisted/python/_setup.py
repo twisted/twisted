722bcdd4fab645bfc9272bcdc93fa0c641b465ce
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-10 16:38:46 +1100

    more review comments

--- src/twisted/python/_setup.py
+++ src/twisted/python/_setup.py
@@ -40,7 +40,8 @@ from distutils.errors import CompileError
 from setuptools import Extension, find_packages
 from setuptools.command.build_py import build_py
 
-# Try not to export anything else in Twisted unless we really have to.
+# Do not replace this with t.p.compat imports, this file must not import
+# from Twisted. See the docstring.
 if sys.version_info < (3, 0):
     _PY3 = False
 else:
@@ -359,14 +360,10 @@ _isCPython = _checkCPython()
 notPortedModules = [
     "twisted.conch.client.connect",
     "twisted.conch.client.direct",
-    "twisted.conch.tap",
     "twisted.conch.test.test_cftp",
     "twisted.conch.test.test_ckeygen",
     "twisted.conch.test.test_conch",
-    "twisted.conch.test.test_helper",
     "twisted.conch.test.test_manhole",
-    "twisted.conch.test.test_tap",
-    "twisted.conch.ttymodes",
     "twisted.conch.ui.__init__",
     "twisted.conch.ui.ansi",
     "twisted.conch.ui.tkvt100",
