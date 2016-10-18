f493f553761840433203732ce1765fb112ed2996
Glyph <glyph@twistedmatrix.com>
2016-10-17 23:04:24 -0700

    wtb $NEXTVER

--- src/twisted/internet/interfaces.py
+++ src/twisted/internet/interfaces.py
@@ -89,7 +89,7 @@ class IHostResolution(Interface):
     An L{IHostResolution} represents represents an in-progress recursive query
     for a DNS name.
 
-    @since: 16.5
+    @since: 16.6
     """
 
     name = Attribute(
@@ -110,7 +110,7 @@ class IResolutionReceiver(Interface):
     An L{IResolutionReceiver} receives the results of a hostname resolution in
     progress, initiated by an L{IHostnameResolver}.
 
-    @since: 16.5
+    @since: 16.6
     """
 
     def resolutionBegan(resolutionInProgress):
@@ -147,7 +147,7 @@ class IHostnameResolver(Interface):
     An L{IHostnameResolver} can resolve a host name and port number into a
     series of L{IAddress} objects.
 
-    @since: 16.5
+    @since: 16.6
     """
 
     def resolveHostName(resolutionReceiver, hostName, portNumber=0,
