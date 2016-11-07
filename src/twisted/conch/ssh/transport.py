6f39e8910215a418669318eba883e9a98c8aa5ed
acabhishek942 <acabhishek942@gmail.com>
2016-11-07 16:58:40 +0530

    use braces for callimg keys

--- src/twisted/conch/ssh/transport.py
+++ src/twisted/conch/ssh/transport.py
@@ -474,7 +474,7 @@ class SSHTransportBase(protocol.Protocol):
 
     supportedKeyExchanges = _kex.getSupportedKeyExchanges()
     supportedPublicKeys = ([b'ssh-rsa', b'ssh-dss'] +
-                        [b'ecdsa-sha2-' + list(keys._curveTable.keys)])
+                        [b'ecdsa-sha2-' + list(keys._curveTable.keys())])
     supportedCompressions = [b'none', b'zlib']
     supportedLanguages = ()
     supportedVersions = (b'1.99', b'2.0')
