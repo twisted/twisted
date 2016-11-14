fb3c93f21d4f022179427df690635be7e03ce94c
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-11-14 12:30:21 -0800

    Move ConchOptions parsing tests to test_default
    This fixes nodeps builds

--- src/twisted/conch/test/test_default.py
+++ src/twisted/conch/test/test_default.py
@@ -293,3 +293,41 @@ class SSHUserAuthClientTests(TestCase):
         return d
 
     test_getGenericAnswers.skip = windowsSkip or ttySkip
+
+
+
+
+class ConchOptionsParsing(TestCase):
+    """
+    Options parsing.
+    """
+    def test_macs(self):
+        """
+        Specify MAC algorithms.
+        """
+        opts = ConchOptions()
+        e = self.assertRaises(SystemExit, opts.opt_macs, "invalid-mac")
+        self.assertIn("Unknown mac type", e.code)
+        opts = options.ConchOptions()
+        opts.opt_macs("hmac-sha2-512")
+        self.assertEqual(opts['macs'], [b"hmac-sha2-512"])
+        opts.opt_macs(b"hmac-sha2-512")
+        self.assertEqual(opts['macs'], [b"hmac-sha2-512"])
+        opts.opt_macs("hmac-sha2-256,hmac-sha1,hmac-md5")
+        self.assertEqual(opts['macs'], [b"hmac-sha2-256", b"hmac-sha1", b"hmac-md5"])
+
+
+    def test_host_key_algorithms(self):
+        """
+        Specify host key algorithms.
+        """
+        opts = ConchOptions()
+        e = self.assertRaises(SystemExit, opts.opt_host_key_algorithms, "invalid-key")
+        self.assertIn("Unknown host key type", e.code)
+        opts = options.ConchOptions()
+        opts.opt_host_key_algorithms("ssh-rsa")
+        self.assertEqual(opts['host-key-algorithms'], [b"ssh-rsa"])
+        opts.opt_host_key_algorithms(b"ssh-dss")
+        self.assertEqual(opts['host-key-algorithms'], [b"ssh-dss"])
+        opts.opt_host_key_algorithms("ssh-rsa,ssh-dss")
+        self.assertEqual(opts['host-key-algorithms'], [b"ssh-rsa", b"ssh-dss"])
