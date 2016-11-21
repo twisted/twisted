602b41709f7ac5729089228620a7860fdd95fb49
Moshe Zadka <zadka.moshe@gmail.com>
2016-11-20 18:32:34 -0800

    fix tests

--- src/twisted/web/test/test_tap.py
+++ src/twisted/web/test/test_tap.py
@@ -168,7 +168,7 @@ class ServiceTests(TestCase):
         path = os.path.expanduser(
             os.path.join('~', UserDirectory.userSocketName))
         self.assertEqual(
-            endpoints._parseServer(options['port'], None)[:2],
+            strports.parse(options['ports'][0], None)[:2],
             ('UNIX', (path, None)))
 
     if not IReactorUNIX.providedBy(reactor):
@@ -184,7 +184,7 @@ class ServiceTests(TestCase):
         options = Options()
         options.parseOptions([])
         self.assertEqual(
-            endpoints._parseServer(options['port'], None)[:2],
+            endpoints._parseServer(options['ports'][0], None)[:2],
             ('TCP', (8080, None)))
 
 
