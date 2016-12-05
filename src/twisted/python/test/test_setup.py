3e2d5f6a067cbf25eeb0291d154c1d2a0217f858
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-12-05 19:05:47 +1100

    correct tests

--- src/twisted/python/test/test_setup.py
+++ src/twisted/python/test/test_setup.py
@@ -17,55 +17,8 @@ from twisted.python import _setup, filepath
 from twisted.python.compat import _PY3
 from twisted.python._setup import (
     BuildPy3,
-    getSetupArgs,
-    ConditionalExtension,
     _EXTRAS_REQUIRE,
     )
-
-
-
-class SetupTests(TestCase):
-    """
-    Tests for L{getSetupArgs}.
-    """
-
-    def test_conditionalExtensions(self):
-        """
-        Will return the arguments with a custom build_ext which knows how to
-        check whether they should be built.
-        """
-        good_ext = ConditionalExtension("whatever", ["whatever.c"],
-                                        condition=lambda b: True)
-        bad_ext = ConditionalExtension("whatever", ["whatever.c"],
-                                        condition=lambda b: False)
-
-        args = getSetupArgs(extensions=[good_ext, bad_ext])
-
-        # ext_modules should be set even though it's not used.  See comment
-        # in getSetupArgs
-        self.assertEqual(args["ext_modules"], [good_ext, bad_ext])
-        cmdclass = args["cmdclass"]
-        build_ext = cmdclass["build_ext"]
-        builder = build_ext(Distribution())
-        builder.prepare_extensions()
-        self.assertEqual(builder.extensions, [good_ext])
-
-
-    def test_win32Definition(self):
-        """
-        When building on Windows NT, the WIN32 macro will be defined as 1 on
-        the extensions.
-        """
-        ext = ConditionalExtension("whatever", ["whatever.c"],
-                                   define_macros=[("whatever", 2)])
-
-        args = getSetupArgs(extensions=[ext])
-
-        builder = args["cmdclass"]["build_ext"](Distribution())
-        self.patch(os, "name", "nt")
-        builder.prepare_extensions()
-        self.assertEqual(ext.define_macros, [("whatever", 2), ("WIN32", 1)])
-
 
 
 class OptionalDependenciesTests(TestCase):
@@ -91,7 +44,7 @@ class OptionalDependenciesTests(TestCase):
         """
         L{_EXTRAS_REQUIRE} contains options for all documented extras: C{dev},
         C{tls}, C{conch}, C{soap}, C{serial}, C{all_non_platform},
-        C{osx_platform}, and C{windows_platform}.
+        C{osx_platform}, C{linux_platform}, and C{windows_platform}.
         """
         self.assertIn('dev', _EXTRAS_REQUIRE)
         self.assertIn('tls', _EXTRAS_REQUIRE)
@@ -100,6 +53,7 @@ class OptionalDependenciesTests(TestCase):
         self.assertIn('serial', _EXTRAS_REQUIRE)
         self.assertIn('all_non_platform', _EXTRAS_REQUIRE)
         self.assertIn('osx_platform', _EXTRAS_REQUIRE)
+        self.assertIn('linux_platform', _EXTRAS_REQUIRE)
         self.assertIn('windows_platform', _EXTRAS_REQUIRE)
         self.assertIn('http2', _EXTRAS_REQUIRE)
 
@@ -211,6 +165,26 @@ class OptionalDependenciesTests(TestCase):
         self.assertIn('h2 >= 2.5.0, < 3.0', deps)
         self.assertIn('priority >= 1.1.0, < 2.0', deps)
         self.assertIn('pyobjc-core', deps)
+        self.assertIn('twisted_platform_support', deps)
+
+
+    def test_extrasRequiresLinuxPlatformDeps(self):
+        """
+        L{_EXTRAS_REQUIRE}'s C{linux_platform} extra contains setuptools
+        requirements for all of Twisted's optional dependencies usable on the
+        Linux platform.
+        """
+        deps = _EXTRAS_REQUIRE['linux_platform']
+        self.assertIn('pyopenssl >= 16.0.0', deps)
+        self.assertIn('service_identity', deps)
+        self.assertIn('idna >= 0.6', deps)
+        self.assertIn('pyasn1', deps)
+        self.assertIn('cryptography >= 0.9.1', deps)
+        self.assertIn('soappy', deps)
+        self.assertIn('pyserial', deps)
+        self.assertIn('h2 >= 2.5.0, < 3.0', deps)
+        self.assertIn('priority >= 1.1.0, < 2.0', deps)
+        self.assertIn('twisted_platform_support', deps)
 
 
     def test_extrasRequiresWindowsPlatformDeps(self):
@@ -230,59 +204,7 @@ class OptionalDependenciesTests(TestCase):
         self.assertIn('h2 >= 2.5.0, < 3.0', deps)
         self.assertIn('priority >= 1.1.0, < 2.0', deps)
         self.assertIn('pypiwin32', deps)
-
-
-
-class FakeModule(object):
-    """
-    A fake module, suitable for dependency injection in testing.
-    """
-    def __init__(self, attrs):
-        """
-        Initializes a fake module.
-
-        @param attrs: The attrs that will be accessible on the module.
-        @type attrs: C{dict} of C{str} (Python names) to objects
-        """
-        self._attrs = attrs
-
-
-    def __getattr__(self, name):
-        """
-        Gets an attribute of this fake module from its attrs.
-
-        @raise AttributeError: When the requested attribute is missing.
-        """
-        try:
-            return self._attrs[name]
-        except KeyError:
-            raise AttributeError()
-
-
-
-fakeCPythonPlatform = FakeModule({"python_implementation": lambda: "CPython"})
-fakeOtherPlatform = FakeModule({"python_implementation": lambda: "lvhpy"})
-
-
-
-class WithPlatformTests(TestCase):
-    """
-    Tests for L{_checkCPython} when used with a (fake) C{platform} module.
-    """
-    def test_cpython(self):
-        """
-        L{_checkCPython} returns C{True} when C{platform.python_implementation}
-        says we're running on CPython.
-        """
-        self.assertTrue(_setup._checkCPython(platform=fakeCPythonPlatform))
-
-
-    def test_other(self):
-        """
-        L{_checkCPython} returns C{False} when C{platform.python_implementation}
-        says we're not running on CPython.
-        """
-        self.assertFalse(_setup._checkCPython(platform=fakeOtherPlatform))
+        self.assertIn('twisted_platform_support', deps)
 
 
 
