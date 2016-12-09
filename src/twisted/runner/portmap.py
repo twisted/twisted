e12e414fbf10bcb08ec43f8e759f23d765f8f7ff
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-12-09 22:39:56 +1100

    Revert "portmap is garbage"
    This reverts commit de7b65999ae118897f2bf2187afdc4404aa0e645.

---
+++ src/twisted/runner/portmap.py
@@ -0,0 +1,11 @@
+# -*- test-case-name: twisted.runner.test -*-
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Portmap support.
+"""
+
+from twisted_platform_support._portmap import set, unset
+
+__all__ = ["set", "unset"]
