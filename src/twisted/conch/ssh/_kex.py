db2a2e3d4b957c072250a48e9fcc5a5e267a29e2
Craig Rodrigues <rodrigc@FreeBSD.org>
2016-12-16 13:54:42 +0000

    Fix nodeps build

--- src/twisted/conch/ssh/_kex.py
+++ src/twisted/conch/ssh/_kex.py
@@ -10,8 +10,6 @@ from __future__ import absolute_import, division
 
 from hashlib import sha1, sha256, sha384, sha512
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import ec
 from zope.interface import Attribute, implementer, Interface
 
 from twisted.conch import error
@@ -280,14 +278,18 @@ def getSupportedKeyExchanges():
     @return: A C{list} of supported key exchange algorithm names.
     @rtype: C{list} of L{bytes}
     """
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives.asymmetric import ec
+
     backend = default_backend()
-    for keyAlgorithm in list(_kexAlgorithms):
+    kexAlgorithms = _kexAlgorithms.copy()
+    for keyAlgorithm in list(kexAlgorithms):
         if keyAlgorithm.startswith(b"ecdh"):
             keyAlgorithmDsa = keyAlgorithm.replace(b"ecdh", b"ecdsa")
             supported = backend.elliptic_curve_exchange_algorithm_supported(
                 ec.ECDH(), _curveTable[keyAlgorithmDsa])
             if not supported:
-                _kexAlgorithms.pop(keyAlgorithm)
+                kexAlgorithms.pop(keyAlgorithm)
     return sorted(
-        _kexAlgorithms,
-        key = lambda kexAlgorithm: _kexAlgorithms[kexAlgorithm].preference)
+        kexAlgorithms,
+        key = lambda kexAlgorithm: kexAlgorithms[kexAlgorithm].preference)
