c60a98df08aabcbec706c617362efc6922f5ce65
Tom Prince <tom.prince@twistedmatrix.com>
2016-12-20 22:34:51 -0700

    Automat doesn't have API docs.

--- src/twisted/application/internet.py
+++ src/twisted/application/internet.py
@@ -527,7 +527,7 @@ def _firstResult(gen):
     """
     Return the first element of a generator and exhaust it.
 
-    L{automat.MethodicalMachine.upon}'s C{collector} argument takes a generator of
+    C{MethodicalMachine.upon}'s C{collector} argument takes a generator of
     output results. If the generator is exhausted, the later outputs aren't
     actually run.
 
