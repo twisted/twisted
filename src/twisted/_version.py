14aa91e453975eeb75ced4e2c69bca2f4cef4350
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-12-23 14:16:46 +1100

    bump to 16.7.0rc1

--- src/twisted/_version.py
+++ src/twisted/_version.py
@@ -7,5 +7,5 @@ Provides Twisted version information.
 
 from incremental import Version
 
-__version__ = Version('Twisted', 16, 6, 0, dev=0)
+__version__ = Version('Twisted', 16, 7, 0, release_candidate=1)
 __all__ = ["__version__"]
--- src/twisted/test/test_sslverify.py
+++ src/twisted/test/test_sslverify.py
@@ -821,7 +821,7 @@ class OpenSSLOptionsTests(unittest.TestCase):
         )
 
         message = ("Passing method to twisted.internet.ssl.CertificateOptions "
-                   "was deprecated in Twisted NEXT. Please use a combination "
+                   "was deprecated in Twisted 16.7.0rc1. Please use a combination "
                    "of insecurelyLowerMinimumTo, raiseMinimumTo, and "
                    "lowerMaximumSecurityTo instead, as Twisted will correctly "
                    "configure the method.")
