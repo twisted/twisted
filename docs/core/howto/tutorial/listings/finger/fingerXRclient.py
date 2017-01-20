b37a623c79f852c6c98ecf5bc46b8b846c605418
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-01-20 14:56:03 -0800

    Fix example on Python 3

--- docs/core/howto/tutorial/listings/finger/fingerXRclient.py
+++ docs/core/howto/tutorial/listings/finger/fingerXRclient.py
@@ -1,7 +1,14 @@
 # testing xmlrpc finger
 
-from __future__ import print_function
+try:
+    # Python 3
+    import xmlrpc.client
+    Server = xmlrpc.client.Server
+except ImportError:
+    # Python 2
+    import xmlrpclib
+    Server = xmlrpclib.Server
 
-import xmlrpclib
-server = xmlrpclib.Server('http://127.0.0.1:8000/RPC2')
+import xmlrpc.client
+server = Server('http://127.0.0.1:8000/RPC2')
 print(server.getUser('moshez'))
