f147d21c658e0f48a93efded328326c620921377
Wilfredo Sánchez Vega <wsanchez@apple.com>
2017-01-24 11:09:37 -0800

    Add test_failureResultOfWithExpectedException and test_failureResultOfWithException

--- src/twisted/trial/test/py3_test_assertions.py
+++ src/twisted/trial/test/py3_test_assertions.py
@@ -189,6 +189,36 @@ class ResultOfCoroutineAssertionsTests(unittest.SynchronousTestCase):
         )
 
 
+    def test_failureResultOfWithExpectedException(self):
+        """
+        When passed a coroutine which currently has an exception result (ie, if
+        converted into a L{Deferred}, L{Deferred.addErrback} would cause the
+        added errback to be called before C{addErrback} returns),
+        L{SynchronousTestCase.failureResultOf} returns a L{Failure} containing
+        that exception, if the exception type is expected.
+        """
+        self.assertEqual(
+            self.failure.value,
+            self.failureResultOf(
+                self.raisesException(), self.failure.type, KeyError
+            ).value
+        )
+
+
+    def test_failureResultOfWithException(self):
+        """
+        When passed a coroutine which currently has an exception result (ie, if
+        converted into a L{Deferred}, L{Deferred.addErrback} would cause the
+        added errback to be called before C{addErrback} returns),
+        L{SynchronousTestCase.failureResultOf} returns returns a L{Failure}
+        containing that exception.
+        """
+        self.assertEqual(
+            self.failure.value,
+            self.failureResultOf(self.raisesException()).value
+        )
+
+
     def test_assertNoResultSuccess(self):
         """
         When passed a coroutine which currently has a success result (see
