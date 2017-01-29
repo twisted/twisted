77b95e9fd930151e6c583b2a27a723824c0de1db
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-01-29 14:43:35 -0800

    Use imports

--- docs/core/howto/tutorial/listings/finger/fingerXRclient.py
+++ docs/core/howto/tutorial/listings/finger/fingerXRclient.py
@@ -2,13 +2,10 @@
 
 try:
     # Python 3
-    import xmlrpc.client
-    Server = xmlrpc.client.Server
+    from xmlrpc.client import Server
 except ImportError:
     # Python 2
-    import xmlrpclib
-    Server = xmlrpclib.Server
+    from xmlrpclib import Server
 
-import xmlrpc.client
 server = Server('http://127.0.0.1:8000/RPC2')
 print(server.getUser('moshez'))
