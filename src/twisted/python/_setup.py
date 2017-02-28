42be49baec2ff541ccac689feb4f8766bc296550
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2017-02-28 08:29:21 -0500

    Avoid idna 2.3 because it's incompatible

--- src/twisted/python/_setup.py
+++ src/twisted/python/_setup.py
@@ -91,7 +91,11 @@ _EXTRA_OPTIONS = dict(
     tls=[
         'pyopenssl >= 16.0.0',
         'service_identity',
-        'idna >= 0.6'],
+        # idna 2.3 introduced some changes that break a few things.  Avoid it
+        # for the time being.  I expect we'll work around those changes and
+        # lift the < 2.3 restriction here shortly.
+        'idna >= 0.6, < 2.3',
+    ],
     conch=[
         'pyasn1',
         'cryptography >= 0.9.1',
