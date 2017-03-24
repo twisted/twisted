75133b927415c642e2773d98d6e5f095e764d565
Adi Roiban <adi.roiban@chevah.com>
2017-03-24 09:32:33 +0000

    Fix bad commit.

--- src/twisted/web/_newclient.py
+++ src/twisted/web/_newclient.py
@@ -102,7 +102,7 @@ class _WrapperException(Exception):
 
 
 
-class g(_WrapperException):
+class RequestGenerationFailed(_WrapperException):
     """
     There was an error while creating the bytes which make up a request.
 
