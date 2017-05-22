fbc8178ae60956787288c2b3cda1500f137709e5
Amber Brown <amber@atleastfornow.net>
2017-05-22 16:52:16 -0700

    hard require attrs

--- src/twisted/python/_setup.py
+++ src/twisted/python/_setup.py
@@ -229,6 +229,7 @@ def getSetupArgs(extensions=_EXTENSIONS):
     requirements.append("incremental >= 16.10.1")
     requirements.append("Automat >= 0.3.0")
     requirements.append("hyperlink >= 17.1.1")
+    requirements.append("attrs >= 17.1.0")
 
     arguments.update(dict(
         packages=find_packages("src"),
