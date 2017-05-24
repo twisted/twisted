ac430ec8ce78b018947c451c1642ba54a71673ae
Amber Brown <amber@atleastfornow.net>
2017-05-24 13:12:01 -0700

    fix windows, maybe

--- src/twisted/web/test/test_web.py
+++ src/twisted/web/test/test_web.py
@@ -1268,10 +1268,7 @@ class SiteAccessLogTests(AccessLogTestsMixin, unittest.TestCase):
     """
     Tests for L{server.Site.log}.
     """
-    if _PY3:
-        skip = "Site not ported to Python 3 yet."
-
-    linesep = os.linesep
+    linesep = b"\n"
 
     def factory(self, *args, **kwargs):
         return server.Site(resource.Resource(), *args, **kwargs)
