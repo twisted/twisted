9c6e6d2b0264f8c2a6f60fc89176e5978ea43160
Amber Brown <amber@atleastfornow.net>
2017-05-24 16:04:55 -0700

    fixy fix

--- src/twisted/internet/interfaces.py
+++ src/twisted/internet/interfaces.py
@@ -60,7 +60,7 @@ class IResolverSimple(Interface):
         """
         Resolve the domain name C{name} into an IP address.
 
-        @type name: C{bytes}
+        @type name: C{str}
         @param name: DNS name to resolve.
 
         @type timeout: Sequence of C{int}
