ea420a00589b06236e461bbd0525631e39ca1321
Mark Williams <markrwilliams@gmail.com>
2017-07-09 12:14:14 -0700

    Tests for imap4.WriterBuffer.

--- src/twisted/mail/test/test_imap.py
+++ src/twisted/mail/test/test_imap.py
@@ -6440,3 +6440,86 @@ class LiteralFileTests(LiteralTestsMixin, unittest.TestCase):
 
         dataFile, extra = result
         self.assertEqual(dataFile.read(), b"data")
+
+
+
+class WriteBufferTests(unittest.SynchronousTestCase):
+    """
+    Tests for L{imap4.WriteBuffer}.
+    """
+
+    def setUp(self):
+        self.transport = StringTransport()
+
+
+    def test_partialWrite(self):
+        """
+        L{imap4.WriteBuffer} buffers writes that are smaller than its
+        buffer size.
+        """
+        buf = imap4.WriteBuffer(self.transport)
+        data = b'x' * buf.bufferSize
+
+        buf.write(data)
+
+        self.assertFalse(self.transport.value())
+
+
+    def test_overlongWrite(self):
+        """
+        L{imap4.WriteBuffer} writes data without buffering it when
+        the size of the data exceeds the size of its buffer.
+        """
+        buf = imap4.WriteBuffer(self.transport)
+        data = b'x' * (buf.bufferSize + 1)
+
+        buf.write(data)
+
+        self.assertEqual(self.transport.value(), data)
+
+
+    def test_writesImplyFlush(self):
+        """
+        L{imap4.WriteBuffer} buffers writes until its buffer's size
+        exceeds its maximum value.
+        """
+        buf = imap4.WriteBuffer(self.transport)
+        firstData = b'x' * buf.bufferSize
+        secondData = b'y'
+
+        buf.write(firstData)
+
+        self.assertFalse(self.transport.value())
+
+        buf.write(secondData)
+
+        self.assertEqual(self.transport.value(), firstData + secondData)
+
+
+    def test_explicitFlush(self):
+        """
+        L{imap4.WriteBuffer.flush} flushes the buffer even when its
+        size is smaller than the buffer size.
+        """
+        buf = imap4.WriteBuffer(self.transport)
+        data = b'x' * (buf.bufferSize)
+
+        buf.write(data)
+
+        self.assertFalse(self.transport.value())
+
+        buf.flush()
+
+        self.assertEqual(self.transport.value(), data)
+
+
+    def test_explicitFlushEmptyBuffer(self):
+        """
+        L{imap4.WriteBuffer.flush} has no effect if when the buffer is
+        empty.
+        """
+        buf = imap4.WriteBuffer(self.transport)
+
+        buf.flush()
+
+        self.assertFalse(self.transport.value())
