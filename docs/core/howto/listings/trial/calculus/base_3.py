e7a1f62669c4d56f77486f7e0984ed3771e6fe9c
Arie van Deursen <arie.vandeursen@tudelft.nl>
2017-07-19 17:11:59 +0200

    Ensure base_3 test_error cases pass
    This fix makes the four test_error{Add,Multiply,Divide,Subtract}
    test cases in test_base_3.py pass.
    
    base_3.py relied on non-lazy evaluation of the map function.
    But in python3 map is lazy, and returns without actually applying the map.
    
    Consequently, a
    
    try:
       map(f, l)
    except:
       ...
    
    will never catch exceptions raised by f.
    
    Resolved by moving the try clause into the function.

--- docs/core/howto/listings/trial/calculus/base_3.py
+++ docs/core/howto/listings/trial/calculus/base_3.py
@@ -1,16 +1,20 @@
 # -*- test-case-name: calculus.test.test_base_3 -*-
 
 class Calculation(object):
-    def _make_ints(self, *args):
+
+    def _robust_int(self, arg):
         try:
-            return map(int, args)
+            return int(arg)
         except ValueError:
-            raise TypeError("Couldn't coerce arguments to integers: %s" % args)
+            raise TypeError("Couldn't coerce argument to integer: %s" % arg)
+
+    def _make_ints(self, *args):
+        return map(self._robust_int, args)
 
     def add(self, a, b):
         a, b = self._make_ints(a, b)
         return a + b
-    
+
     def subtract(self, a, b):
         a, b = self._make_ints(a, b)
         return a - b
