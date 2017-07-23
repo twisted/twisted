713f940b5ae51e9c793a17e5709439ef5068867a
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2017-07-23 19:12:20 +1000

    fix this version

--- cext/src/_twistedcextensions/_version.py
+++ cext/src/_twistedcextensions/_version.py
@@ -1,11 +1,11 @@
 """
-Provides _twistedcextensions version information.
+Provides twistedcextensions version information.
 """
 
 # This file is auto-generated! Do not edit!
-# Use `python -m incremental.update _twistedcextensions` to change this file.
+# Use `python -m incremental.update twistedcextensions` to change this file.
 
 from incremental import Version
 
-__version__ = Version('_twistedcextensions', 17, 5, 0)
+__version__ = Version('twistedcextensions', 17, 5, 0)
 __all__ = ["__version__"]
