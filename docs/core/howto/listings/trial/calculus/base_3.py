fd361c00d77059ee64bfeaa4671313f343e01a05
Arie van Deursen <arie.vandeursen@tudelft.nl>
2017-07-28 22:06:06 +0200

    Revert "Ensure base_3 test_error cases pass"
    This reverts commit e7a1f62669c4d56f77486f7e0984ed3771e6fe9c.
    
    Instead of moving the try-except out of the map, we will
    replace the map by a list comprehension.

--- docs/core/howto/listings/trial/calculus/base_3.py
+++ docs/core/howto/listings/trial/calculus/base_3.py
@@ -1,20 +1,16 @@
 # -*- test-case-name: calculus.test.test_base_3 -*-
 
 class Calculation(object):
-
-    def _robust_int(self, arg):
-        try:
-            return int(arg)
-        except ValueError:
-            raise TypeError("Couldn't coerce argument to integer: %s" % arg)
-
     def _make_ints(self, *args):
-        return map(self._robust_int, args)
+        try:
+            return map(int, args)
+        except ValueError:
+            raise TypeError("Couldn't coerce arguments to integers: %s" % args)
 
     def add(self, a, b):
         a, b = self._make_ints(a, b)
         return a + b
-
+    
     def subtract(self, a, b):
         a, b = self._make_ints(a, b)
         return a - b
