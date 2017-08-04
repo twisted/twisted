f2801c4c7e9475dd727ae902910d517cb2e670a9
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-08-04 04:24:29 -0700

    Add test which creates a shortcut to PYTHON.EXE

--- src/twisted/test/test_shortcut.py
+++ src/twisted/test/test_shortcut.py
@@ -3,6 +3,8 @@ Test win32 shortcut script
 """
 
 import os.path
+import sys
+import tempfile
 
 from twisted.python.runtime import platform
 from twisted.trial import unittest
@@ -23,6 +25,9 @@ class ShortcutTests(unittest.TestCase):
     skip = skipReason
 
     def test_create(self):
+        """
+        Create a simple shortcut.
+        """
         testFilename = __file__
         s1 = shortcut.Shortcut(testFilename)
         tempname = self.mktemp() + '.lnk'
@@ -30,3 +35,27 @@ class ShortcutTests(unittest.TestCase):
         self.assertTrue(os.path.exists(tempname))
         sc = shortcut.open(tempname)
         self.assertTrue(sc.GetPath(0)[0].endswith('test_shortcut.py'))
+
+
+    def test_createPythonShortcut(self):
+        """
+        Create a shortcut to the Python executable,
+        and set some values.
+        """
+        testFilename = sys.executable
+        tempDir = tempfile.gettempdir()
+        s1 = shortcut.Shortcut(path=testFilename,
+                               arguments="-V",
+                               description="The Python executable",
+			       workingdir=tempDir,
+			       iconpath=tempDir,
+                               iconidx=1)
+        tempname = self.mktemp() + '.lnk'
+        s1.save(tempname)
+        self.assertTrue(os.path.exists(tempname))
+        sc = shortcut.open(tempname)
+        self.assertTrue(sc.GetPath(0)[0].endswith(
+            os.path.basename(sys.executable)))
+        self.assertEqual(sc.GetDescription(), "The Python executable")
+        self.assertEqual(sc.GetWorkingDirectory(), tempDir)
+        self.assertEqual(sc.GetIconLocation(), (tempDir, 1))
