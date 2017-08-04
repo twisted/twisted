c60dec1fff8fe36a285bca89600eef7349927d6b
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-08-04 03:14:44 -0700

    Use twisted.python.runtime.platform.isWindows().
    Clean up indentation.

--- src/twisted/test/test_shortcut.py
+++ src/twisted/test/test_shortcut.py
@@ -1,25 +1,31 @@
-"""Test win32 shortcut script
+"""
+Test win32 shortcut script
 """
 
+import os.path
+
+from twisted.python.runtime import platform
 from twisted.trial import unittest
 
-import os
-if os.name == 'nt':
-
-    skipWindowsNopywin32 = None
+skipReason = None
+if platform.isWindows():
     try:
         from twisted.python import shortcut
     except ImportError:
-        skipWindowsNopywin32 = ("On windows, twisted.python.shortcut is not "
-                                "available in the absence of win32com.")
-    import os.path
+        skipReason = ("On Windows, twisted.python.shortcut is not "
+                      "available in the absence of win32com.")
+else:
+    skipReason = "Only runs on Windows"
 
-    class ShortcutTests(unittest.TestCase):
-        def testCreate(self):
-            s1=shortcut.Shortcut("test_shortcut.py")
-            tempname=self.mktemp() + '.lnk'
-            s1.save(tempname)
-            self.assertTrue(os.path.exists(tempname))
-            sc=shortcut.open(tempname)
-            self.assertTrue(sc.GetPath(0)[0].endswith('test_shortcut.py'))
-    ShortcutTests.skip = skipWindowsNopywin32
+
+
+class ShortcutTests(unittest.TestCase):
+    skip = skipReason
+
+    def testCreate(self):
+        s1=shortcut.Shortcut("test_shortcut.py")
+        tempname=self.mktemp() + '.lnk'
+        s1.save(tempname)
+        self.assertTrue(os.path.exists(tempname))
+        sc=shortcut.open(tempname)
+        self.assertTrue(sc.GetPath(0)[0].endswith('test_shortcut.py'))
