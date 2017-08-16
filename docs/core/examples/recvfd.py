094202d274393f0862dcf2a787f6585b2ca2ea7a
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-08-16 01:26:33 -0700

    Fix typo

--- docs/core/examples/recvfd.py
+++ docs/core/examples/recvfd.py
@@ -7,7 +7,7 @@ UNIX sockets.  This client connects to a server listening on a UNIX socket and
 waits for one file descriptor to arrive over the connection.  It displays the
 name of the file and the first 80 bytes it contains, then exits.
 
-To runb this example, run this program with one argument: a path giving the UNIX
+To run this example, run this program with one argument: a path giving the UNIX
 socket the server side of this example is already listening on.  For example:
 
     $ python recvfd.py /tmp/sendfd.sock
