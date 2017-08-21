b3ba46216f7888bdc3618dc32590acf9f66fc6e6
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-08-21 00:01:27 -0700

    Fix whitespace issues

--- src/twisted/mail/test/test_bounce.py
+++ src/twisted/mail/test/test_bounce.py
@@ -13,32 +13,34 @@ class BounceTests(unittest.TestCase):
     """
 
     def test_bounceFormatUnicode(self):
-        from_, to, s = bounce.generateBounce(StringIO(u'''\
+        fromAddress, to, s = bounce.generateBounce(StringIO(u'''\
 From: Moshe Zadka <moshez@example.com>
 To: nonexistent@example.org
 Subject: test
 
 '''), u'moshez@example.com', u'nonexistent@example.org')
-        self.assertEqual(from_, b'')
+        self.assertEqual(fromAddress, b'')
         self.assertEqual(to, b'moshez@example.com')
         emailParser = email.parser.Parser()
         mess = emailParser.parse(StringIO(s.decode("utf-8")))
         self.assertEqual(mess['To'], 'moshez@example.com')
         self.assertEqual(mess['From'], 'postmaster@example.org')
-        self.assertEqual(mess['subject'], 'Returned Mail: see transcript for details')
+        self.assertEqual(mess['subject'],
+                        'Returned Mail: see transcript for details')
 
 
     def test_bounceFormatBytes(self):
-        from_, to, s = bounce.generateBounce(BytesIO(b'''\
+        fromAddress, to, s = bounce.generateBounce(BytesIO(b'''\
 From: Moshe Zadka <moshez@example.com>
 To: nonexistent@example.org
 Subject: test
 
 '''), b'moshez@example.com', b'nonexistent@example.org')
-        self.assertEqual(from_, b'')
+        self.assertEqual(fromAddress, b'')
         self.assertEqual(to, b'moshez@example.com')
         emailParser = email.parser.Parser()
         mess = emailParser.parse(StringIO(s.decode("utf-8")))
         self.assertEqual(mess['To'], 'moshez@example.com')
         self.assertEqual(mess['From'], 'postmaster@example.org')
-        self.assertEqual(mess['subject'], 'Returned Mail: see transcript for details')
+        self.assertEqual(mess['subject'],
+                         'Returned Mail: see transcript for details')
