eee690128418d7908cf5b543f665765a1f876b1d
Mark Williams <markrwilliams@gmail.com>
2017-08-24 17:17:42 -0700

    Make an LRU out of an OrderedDict.

--- src/twisted/web/test/test_agent.py
+++ src/twisted/web/test/test_agent.py
@@ -3075,7 +3075,6 @@ class TestHostnameCachingHTTPSPolicy(TestCase):
         creator = policy.creatorForNetloc(b"foo", 1589)
         self.assertTrue(trustRoot.called)
         trustRoot.called = False
-        self.assertEquals(1, policy._nextCacheId)
         self.assertEquals(1, len(policy._cache))
         connection = creator.clientConnectionForTLS(None)
         self.assertIs(trustRoot.context, connection.get_context())
@@ -3083,31 +3082,6 @@ class TestHostnameCachingHTTPSPolicy(TestCase):
         policy.creatorForNetloc(b"foo", 1589)
         self.assertFalse(trustRoot.called)
 
-    def test_cacheIdIncremented(self):
-        """
-        Verify that the cacheId is always incremented for new and existing
-        entries.
-        """
-        policy = HostnameCachingHTTPSPolicy(trustRoot=CustomOpenSSLTrustRoot())
-        hostname = b"foo"
-        host = hostname.decode("ascii")
-
-        hostname2 = b"bard"
-        host2 = hostname2.decode("ascii")
-
-        policy.creatorForNetloc(hostname, 1589)
-        self.assertIn(host, policy._cache)
-        entry = policy._cache[host]
-        self.assertEquals(0, entry.cacheId)
-
-        policy.creatorForNetloc(hostname2, 1589)
-        self.assertIn(host2, policy._cache)
-        entry = policy._cache[host2]
-        self.assertEquals(1, entry.cacheId)
-
-        policy.creatorForNetloc(hostname, 1589)
-        entry = policy._cache[host]
-        self.assertEquals(2, entry.cacheId)
 
     def test_cacheRemovesOldest(self):
         """
@@ -3134,8 +3108,7 @@ class TestHostnameCachingHTTPSPolicy(TestCase):
 
         self.assertIn(hostn, policy._cache)
         self.assertIn(host0, policy._cache)
-        entry = policy._cache[hostn]
-        self.assertEquals(21, entry.cacheId)
+
 
     def test_changeCacheSize(self):
         """
@@ -3159,5 +3132,3 @@ class TestHostnameCachingHTTPSPolicy(TestCase):
         self.assertEquals(5, len(policy._cache))
 
         self.assertIn(hostn, policy._cache)
-        entry = policy._cache[hostn]
-        self.assertEquals(5, entry.cacheId)
