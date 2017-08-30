c17cc966546ab5f80c1e804b3fd12a102015d2cd
Moshe Zadka <zadka.moshe@gmail.com>
2017-08-29 20:39:44 -0700

    switch to testing and talking about the public class

--- src/twisted/newsfragments/9235.feature
+++ src/twisted/newsfragments/9235.feature
@@ -1,1 +1,1 @@
-twisted.web.client._HTTP11ClientFactory has a useful repr, which makes logs more readable
+twisted.web.client.HTTPConnectionPool passes the repr() of the endpoint to the client protocol factory, and the protocol factory adds that to its own repr(). This makes logs more useful.
--- src/twisted/web/test/test_client.py
+++ src/twisted/web/test/test_client.py
@@ -5,22 +5,33 @@
 Tests for various parts of L{twisted.web}.
 """
 
+from twisted.internet import defer
 from twisted.trial import unittest
 
 from twisted.web import client
 
-class HTTP11FactoryTest(unittest.TestCase):
+class DummyEndPoint(object):
+
+    def __init__(self, someString):
+        self.someString = someString
+
+    def __repr__(self):
+        return 'DummyEndPoint({})'.format(self.someString)
+
+    def connect(self, factory):
+        return defer.succeed(dict(factory=factory))
+
+class HTTPConnectionPool(unittest.TestCase):
     """
     Unit tests for L{client._HTTP11ClientFactory}.
     """
 
     def test_repr(self):
-        def a_special_name():
-            return 1
-        # Call it for coverage
-        self.assertEquals(a_special_name(), 1)
-        factory = client._HTTP11ClientFactory(a_special_name,
-                                              'this_is_kinda_unique')
-        result = repr(factory)
-        self.assertIn('a_special_name', result)
-        self.assertIn('this_is_kinda_unique', result)
+        pool = client.HTTPConnectionPool(reactor=None)
+        ep = DummyEndPoint("this_is_probably_unique")
+        d = pool.getConnection('someplace', ep)
+        l = []
+        d.addCallback(l.append)
+        result, = l
+        representation = repr(result)
+        self.assertIn("this_is_probably_unique", representation)
