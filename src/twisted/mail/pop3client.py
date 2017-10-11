d08c3dd5c4d5e62b1a47549548b0509d6c5cd76b
Craig Rodrigues <rodrigc@FreeBSD.org>
2017-10-10 21:38:06 -0700

    Use "ascii"

--- src/twisted/mail/pop3client.py
+++ src/twisted/mail/pop3client.py
@@ -900,7 +900,7 @@ class POP3Client(basic.LineOnlyReceiver, policies.TimeoutMixin):
             On a successful login, it returns the server response minus
             the status indicator.
         """
-        digest = md5(challenge + password).hexdigest().encode("utf-8")
+        digest = md5(challenge + password).hexdigest().encode("ascii")
         return self.apop(username, digest)
 
 
