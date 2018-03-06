c9cd95b2f3236700de83545a8f8a526696fedbdc
Craig Rodrigues <rodrigc@FreeBSD.org>
2018-03-05 21:31:40 -0800

    Shorten line length

--- src/twisted/web/client.py
+++ src/twisted/web/client.py
@@ -27,7 +27,8 @@ from functools import wraps
 from zope.interface import implementer
 
 from twisted.python.bytes import ensureBytes
-from twisted.python.compat import _PY3, nativeString, intToBytes, unicode, itervalues
+from twisted.python.compat import (_PY3, nativeString, intToBytes,
+    unicode, itervalues)
 from twisted.python.deprecate import deprecatedModuleAttribute, deprecated
 from twisted.python.failure import Failure
 from incremental import Version
