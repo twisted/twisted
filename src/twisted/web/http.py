64c03b8b43da8c3c63f5c2b4c5a4506b5373f6b7
Mark Williams <mrw@enotuniq.org>
2018-03-27 23:49:21 -0700

    t.w.h.Request.getRequestHostname handles UNIX domain sockets.
    A Request without a Host header received over a UNIX domain socket
    cannot have a hostname.  Previously, such a request's
    getRequestHostname method would fail with an AttributeError while
    attempting to access the host attribute of the address returned by
    getHost.
    
    This commit introduces a new public exception, UnsupportedTransport,
    that is instead raised when a Request's hostname cannot be determined.

--- src/twisted/web/http.py
+++ src/twisted/web/http.py
@@ -1322,11 +1322,16 @@ class Request:
         """
         Get the hostname that the user passed in to the request.
 
-        This will either use the Host: header (if it is available) or the
-        host we are listening on if the header is unavailable.
+        This will either use the Host: header (if it is available) or
+        the host we are listening on if the header is unavailable.
 
         @returns: the requested hostname
+
         @rtype: C{bytes}
+
+        @raises: L{UnsupportedTransport} if there is no C{Host} header
+            and the transport does have a hostname (e.g., when
+            listening on a UNIX socket)
         """
         # XXX This method probably has no unit tests.  I changed it a ton and
         # nothing failed.
@@ -1797,6 +1802,14 @@ class _ChunkedTransferDecoder(object):
             raise _DataLoss(
                 "Chunked decoder in %r state, still expecting more data to "
                 "get to 'FINISHED' state." % (self.state,))
+
+
+
+class UnsupportedTransport(Exception):
+    """
+    Raised when a L{Request} is bound to a transport that does not
+    support a given operation.
+    """
 
 
 
