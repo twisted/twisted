a6b8d9f74a5f259df71c0d790616c184b8e2f747
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-04-17 11:26:59 -0400

    Add back lost inlinecb tests

--- src/twisted/internet/test/test_inlinecb.py
+++ src/twisted/internet/test/test_inlinecb.py
@@ -14,7 +14,8 @@ from __future__ import division, absolute_import
 import sys
 
 from twisted.trial.unittest import TestCase, SynchronousTestCase
-from twisted.internet.defer import Deferred, returnValue, inlineCallbacks
+from twisted.internet.defer import (
+    Deferred, returnValue, inlineCallbacks, CancelledError)
 
 
 class StopIterationReturnTests(TestCase):
@@ -124,6 +125,7 @@ class NonLocalExitTests(TestCase):
         self.assertMistakenMethodWarning(results)
 
 
+
 class ForwardTraceBackTests(SynchronousTestCase):
 
     def test_forwardTracebacks(self):
@@ -145,3 +147,175 @@ class ForwardTraceBackTests(SynchronousTestCase):
         d = calling()
         f = self.failureResultOf(d)
         self.assertIn("in erroring", f.getTraceback())
+
+
+
+class TranslateMe(Exception):
+    """
+    Sample exception type.
+    """
+
+
+
+class TranslateResult(Exception):
+    """
+    Sample exception type.
+    """
+
+
+
+class DontFail(Exception):
+    """
+    Sample exception type.
+    """
+
+    def __init__(self, actual):
+        Exception.__init__(self)
+        self.actualValue = actual
+
+
+
+class CancellationTests(TestCase):
+    """
+    Tests for cancellation of L{Deferred}s returned by L{inlineCallbacks}.
+    For each of these tests, let:
+        - G be a generator decorated with C{inlineCallbacks}
+        - D be a L{Deferred} returned by G
+        - C be a L{Deferred} awaited by G with C{yield}
+    """
+
+    def setUp(self):
+        """
+        Set up the list of outstanding L{Deferred}s.
+        """
+        self.thingsOutstanding = []
+
+
+    def tearDown(self):
+        """
+        If any L{Deferred}s are still outstanding, fire them.
+        """
+        while self.thingsOutstanding:
+            self.thingGotten()
+
+
+    @inlineCallbacks
+    def sampleInlineCB(self, getChildThing=None):
+        """
+        Generator for testing cascade cancelling cases.
+        @param getChildThing: Some callable returning L{Deferred} that we
+            awaiting (with C{yield})
+        """
+        if getChildThing is None:
+            getChildThing = self.getThing
+        try:
+            x = yield getChildThing()
+        except TranslateMe:
+            raise TranslateResult()
+        except DontFail, df:
+            x = df.actualValue - 2
+        returnValue(x + 1)
+
+
+    def getThing(self):
+        """
+        A sample function that returns a L{Deferred} that can be fired on
+        demand, by L{CancellationTests.thingGotten}.
+        """
+        self.thingsOutstanding.append(Deferred())
+        return self.thingsOutstanding[-1]
+
+
+    def thingGotten(self, result=None):
+        """
+        Fire the L{Deferred} returned from the least-recent call to
+        L{CancellationTests.getThing}.
+        """
+        self.thingsOutstanding.pop(0).callback(result)
+
+
+    def test_cascadeCancellingOnCancel(self):
+        """
+        When D cancelled, C will be immediately cancelled too.
+        """
+        childResultHolder = ['FAILURE']
+        def getChildThing():
+            d = Deferred()
+            def _eb(result):
+                if result.check(CancelledError):
+                    childResultHolder[0] = 'SUCCESS'
+                return result
+            d.addErrback(_eb)
+            return d
+        d = self.sampleInlineCB(getChildThing=getChildThing)
+        d.addErrback(lambda result: None)
+        d.cancel()
+        self.assertEqual(
+            childResultHolder[0], 'SUCCESS', "no cascade cancelling occurs"
+        )
+
+
+    def test_trapChildCancelledErrorOnCascadeCancelling(self):
+        """
+        When D cancelled, CancelledError from cascade cancelled C will be
+        trapped
+        """
+        d = self.sampleInlineCB()
+        d.addErrback(lambda fail: None)
+        d.cancel()
+        errors = self.flushLoggedErrors(CancelledError)
+        self.assertEquals(len(errors), 0, "CancelledError not trapped")
+
+
+    def test_errorToErrorTranslation(self):
+        """
+        When D is cancelled, and C raises a particular type of error, G may
+        catch that error at the point of yielding and translate it into
+        a different error which may be received by application code.
+        """
+        def cancel(it):
+            it.errback(TranslateMe())
+        a = Deferred(cancel)
+        d = self.sampleInlineCB(lambda : a)
+        d.cancel()
+        self.failUnlessFailure(d, TranslateResult)
+
+
+    def test_errorToSuccessTranslation(self):
+        """
+        When D is cancelled, and C raises a particular type of error, G may
+        catch that error at the point of yielding and translate it into
+        a result value which may be received by application code.
+        """
+        def cancel(it):
+            it.errback(DontFail(4321))
+        a = Deferred(cancel)
+        d = self.sampleInlineCB(lambda : a)
+        results = []
+        d.addCallback(results.append)
+        d.cancel()
+        self.assertEquals(results, [4320])
+
+
+    def test_asynchronousCancellation(self):
+        """
+        When D is cancelled, it won't reach the callbacks added to it by
+        application code until C reaches the point in its callback chain where G
+        awaits it.  Otherwise, application code won't be able to track resource
+        usage that D may be using.
+        """
+        moreDeferred = Deferred()
+        def deferMeMore(result):
+            result.trap(CancelledError)
+            return moreDeferred
+        def deferMe():
+            d = Deferred()
+            d.addErrback(deferMeMore)
+            return d
+        d = self.sampleInlineCB(getChildThing=deferMe)
+        finalResult = []
+        d.addBoth(finalResult.append)
+        d.cancel()
+        self.assertEquals(finalResult, [])
+        moreDeferred.callback(6543)
+        self.assertEquals(finalResult, [6544])
