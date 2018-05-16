f473e1c7803581a2677b4c1f4f897092b1ba0363
Amber Brown <hawkowl@atleastfornow.net>
2018-05-16 13:01:26 -0400

    make htmlizer consistently work on bytes

--- src/twisted/python/htmlizer.py
+++ src/twisted/python/htmlizer.py
@@ -21,7 +21,7 @@ class TokenPrinter:
 
     currentCol, currentLine = 0, 1
     lastIdentifier = parameters = 0
-    encoding = "utf-8"
+    encoding = b"utf-8"
 
     def __init__(self, writer):
         self.writer = writer
@@ -32,12 +32,15 @@ class TokenPrinter:
             self.encoding = token
             return
 
+        if not isinstance(token, bytes):
+            token = token.encode(self.encoding)
+
         (srow, scol) = sCoordinates
         (erow, ecol) = eCoordinates
         if self.currentLine < srow:
-            self.writer('\n'*(srow-self.currentLine))
+            self.writer(b'\n' * (srow-self.currentLine))
             self.currentLine, self.currentCol = srow, 0
-        self.writer(' '*(scol-self.currentCol))
+        self.writer(b' ' * (scol-self.currentCol))
         if self.lastIdentifier:
             type = "identifier"
             self.parameters = 1
@@ -53,11 +56,11 @@ class TokenPrinter:
             type = tokenize.tok_name.get(type).lower()
         self.writer(token, type)
         self.currentCol = ecol
-        self.currentLine += token.count('\n')
+        self.currentLine += token.count(b'\n')
         if self.currentLine != erow:
             self.currentCol = 0
         self.lastIdentifier = token in ('def', 'class')
-        if token == ':':
+        if token == b':':
             self.parameters = 0
 
 
