428bd1251c59d2b85ce285c7f97cd0f8960f4ffe
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-09-16 00:57:43 -0400

    install wheel from python

--- x.py
+++ x.py
@@ -2,16 +2,19 @@ import argparse
 import glob
 import os.path
 import shutil
+import subprocess
 import sys
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory')
+    parser.add_argument('--extra', action='append', metavar='extras')
 
     args = parser.parse_args()
 
     wheel, = glob.glob(os.path.join(args.directory, '*.whl'))
+    subprocess.check_call(['pip', 'install', '{}[{}]'.format(wheel, ','.join(args.extras))])
 
     shutil.move(wheel, os.path.join(args.directory, 'twisted.whl'))
 
