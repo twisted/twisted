8b16dd2e4cfebd864822423631b75b4f8393cfa7
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-10-16 15:05:45 -0400

    update pip on appveyor and circle

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -2,6 +2,7 @@ $PYTHON --version
 $PYTHON -m pip --version
 $PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
 $PYTHON -m virtualenv -p $PYTHON venv
+venv/bin/pip install -U pip
 venv/bin/python -m pip install tox
 venv/bin/python -m pip freeze
 venv/bin/python --version
