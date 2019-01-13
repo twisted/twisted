3b7aff382beca38f7edc37a3d78d6521436e6c2c
Kyle Altendorf <kyle.altendorf@epcpower.com>
2019-01-13 17:55:18 -0500

    Review response

--- .circleci/prepare.sh
+++
@@ -1,8 +0,0 @@
-$PYTHON --version
-$PYTHON -m pip --version
-$PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
-$PYTHON -m virtualenv -p $PYTHON venv
-venv/bin/pip install -U pip
-venv/bin/python -m pip install tox
-venv/bin/python -m pip freeze
-venv/bin/python --version
