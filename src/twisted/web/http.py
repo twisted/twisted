4d7733cd0d63939c548a744f3d68a7cb5a95c2f8
James Switzer <james.t.switzer@gmail.com>
2019-05-16 11:24:15 -0400

    Update newsfragment and fix None comparison

--- src/twisted/web/http.py
+++ src/twisted/web/http.py
@@ -1593,7 +1593,7 @@ class Request:
         """
         Pass the loseConnection through to the underlying channel.
         """
-        if self.channel:
+        if self.channel is not None:
             self.channel.loseConnection()
 
 
