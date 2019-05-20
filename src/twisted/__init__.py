92598ff8f9c60c4eaa6f8d5983b68154a5460bf6
Amber Brown <hawkowl@atleastfornow.net>
2019-05-19 21:09:56 -0500

    deprecation and tests

--- src/twisted/__init__.py
+++ src/twisted/__init__.py
@@ -10,3 +10,16 @@ Twisted: The Framework Of Your Internet.
 # setup version
 from twisted._version import __version__ as version
 __version__ = version.short()
+
+
+from twisted.python.compat import _PY3
+
+if _PY3:
+    from incremental import Version
+    from twisted.python.deprecate import deprecatedModuleAttribute
+    deprecatedModuleAttribute(
+        Version("Twisted", "NEXT", 0, 0),
+        "Will not be ported to Python 3.",
+        "twisted",
+        "news"
+    )
