303d2aa3ebe229a04c4b51a672c54c5a2fe22842
Heather Delia White <white.heather33@gmail.com>
2019-06-02 15:05:29 -0500

    fix args in __init__

--- src/twisted/test/__init__.py
+++ src/twisted/test/__init__.py
@@ -8,12 +8,12 @@ Twisted's unit tests.
 
 from twisted.python.deprecate import deprecatedModuleAttribute
 from twisted.python.versions import Version
-import proto_helpers
+from twisted.test import proto_helpers
 
-for i in proto_helpers.__all__:
+for obj in proto_helpers.__all__:
     deprecatedModuleAttribute(
         Version('Twisted', 'NEXT', 0, 0),
-        'Please use twisted.internet.testing.{} instead.'.format(i),
-        __name__,
-        i)
+        'Please use twisted.internet.testing.{} instead.'.format(obj),
+        'twisted.test.proto_helpers',
+        obj)
 
