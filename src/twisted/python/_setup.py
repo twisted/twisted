e11f21d8bb1c3fdcd74a819365d7c1795dbe1bb9
Kyle Altendorf <sda@fstab.net>
2019-08-20 15:37:43 -0400

    Add python_requires to static package metadata

--- src/twisted/python/_setup.py
+++ src/twisted/python/_setup.py
@@ -72,6 +72,7 @@ STATIC_PACKAGE_METADATA = dict(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
 )
 
 
