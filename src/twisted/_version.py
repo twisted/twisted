6a3b141069528a2d81f456ee7890c77b7a7e71e4
Amber Brown <hawkowl@atleastfornow.net>
2019-10-14 08:25:13 +0100

    19.10.0rc1

--- src/twisted/_version.py
+++ src/twisted/_version.py
@@ -7,5 +7,5 @@ Provides Twisted version information.
 
 from incremental import Version
 
-__version__ = Version('Twisted', 19, 7, 0, dev=0)
+__version__ = Version('Twisted', 19, 10, 0, release_candidate=1)
 __all__ = ["__version__"]
