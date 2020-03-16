b60f198815b1d2a13dcae260f2c67a49cc125427
Kevin Azzam <kevin@azz.am>
2020-03-16 19:27:40 +0100

    Include explicit test for auto-adding mask inotify.IN_DELETE_SELF

--- src/twisted/internet/test/test_inotify.py
+++ src/twisted/internet/test/test_inotify.py
@@ -216,6 +216,28 @@ class INotifyTests(unittest.TestCase):
             inotify.IN_DELETE_SELF, operation, expectedPath=self.dirname)
 
 
+    def test_deleteSelfForced(self):
+        """
+        Deleting the monitored directory itself sends an
+        C{inotify.IN_DELETE_SELF} event to the callback
+        even if the mask isn't specified by the call to watch().
+        """
+        def cbNotified(result):
+            (watch, filename, events) = result
+            self.assertEqual(filename.asBytesMode(), self.dirname.asBytesMode())
+            self.assertTrue(events & inotify.IN_DELETE_SELF)
+
+        self.inotify.watch(
+            self.dirname, mask=0x0,
+            callbacks=[lambda *args: d.callback(args)])
+
+        d = defer.Deferred()
+        d.addCallback(cbNotified)
+
+        self.dirname.remove()
+        return d
+
+
     def test_moveSelf(self):
         """
         Renaming the monitored directory itself sends an
