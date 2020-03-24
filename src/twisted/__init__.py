036ec90eed5193c99a68d18b0e65c9214c219279
Amber Hawkie Brown <hawkowl@atleastfornow.net>
2020-03-24 17:59:48 +1100

    Remove twisted.news and related deprecations (which will never fire on Python 3)

--- src/twisted/__init__.py
+++ src/twisted/__init__.py
@@ -10,14 +10,3 @@ Twisted: The Framework Of Your Internet.
 # setup version
 from twisted._version import __version__ as version
 __version__ = version.short()
-
-
-
-from incremental import Version
-from twisted.python.deprecate import deprecatedModuleAttribute
-deprecatedModuleAttribute(
-    Version('Twisted', 20, 3, 0),
-    "morituri nolumus mori",
-    "twisted",
-    "news"
-)
