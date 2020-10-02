82ebed676fcb0242faf14d4e0a6b6e0b8ab6e628
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-10-02 15:43:48 -0700

    Enable disallow_untyped_defs and check_untyped_defs on more files in twisted.python.

--- src/twisted/python/systemd.py
+++ src/twisted/python/systemd.py
@@ -13,7 +13,7 @@ feature are supported.
 __all__ = ["ListenFDs"]
 
 from os import getpid
-from typing import List
+from typing import Iterable, List, Mapping, Optional
 
 
 class ListenFDs:
@@ -27,7 +27,6 @@ class ListenFDs:
         consecutively numbered, with a fixed lowest "starting" descriptor.  This
         gives the default starting descriptor.  Since this must agree with the
         value systemd is using, it typically should not be overridden.
-    @type _START: C{int}
 
     @ivar _descriptors: A C{list} of C{int} giving the descriptors which were
         inherited.
@@ -35,7 +34,7 @@ class ListenFDs:
 
     _START = 3
 
-    def __init__(self, descriptors):
+    def __init__(self, descriptors: List[int]) -> None:
         """
         @param descriptors: The descriptors which will be returned from calls to
             C{inheritedDescriptors}.
@@ -43,7 +42,11 @@ class ListenFDs:
         self._descriptors = descriptors
 
     @classmethod
-    def fromEnvironment(cls, environ=None, start=None):
+    def fromEnvironment(
+        cls,
+        environ: Optional[Mapping[str, str]] = None,
+        start: Optional[int] = None,
+    ) -> "ListenFDs":
         """
         @param environ: A dictionary-like object to inspect to discover
             inherited descriptors.  By default, L{None}, indicating that the
@@ -83,8 +86,8 @@ class ListenFDs:
 
         return cls(descriptors)
 
-    def inheritedDescriptors(self):
+    def inheritedDescriptors(self) -> Iterable[int]:
         """
-        @return: The configured list of descriptors.
+        @return: The configured descriptors.
         """
         return list(self._descriptors)
