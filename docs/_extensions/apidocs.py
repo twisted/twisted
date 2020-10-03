a954268c0a2db01ae99e2571dc2201d5a374f34d
Adi Roiban <adi.roiban@chevah.com>
2020-10-03 22:39:57 +0100

    Build via a parser.

--- docs/_extensions/apidocs.py
+++ docs/_extensions/apidocs.py
@@ -5,9 +5,8 @@ This is created to have API docs created on Read the docs.
 """
 import os
 
-from sphinx.builders import Builder
+from sphinx.parsers import Parser
 from sphinx.util import logging
-
 
 from twisted.python._release import BuildAPIDocsScript
 
@@ -15,41 +14,54 @@ from twisted.python._release import BuildAPIDocsScript
 logger = logging.getLogger(__name__)
 
 
-
-
-class PyDoctorBuilder(Builder):
+class PyDoctorParser(Parser):
    """
-   Trigger pydoctor HTML generation.
+   Will transform the pydoctor instruction file.
    """
-   name = 'pydoctor'
-   format = 'pydoctor'
-   out_suffix = '.html'
+   supported = ('pydoctor',)
 
-   def write(self, *args):
+   def get_transforms(self):
       """
-      Called when build process starts.
       """
-      source = self.app.config.apidocs['source']
-      destination = os.path.join(
-         self.outdir, self.app.config.apidocs['destination'])
+      return []
 
-      logger.info('pydoctor API docs build from %s to %s.' % (
-         source, destination))
+   def parse(self, inputstring, document):
+      """
+      This is called when a pydoctor marker file is found.
+      """
+      source_path = document.attributes['source']
+
+      # Let Sphinx know that the file should be ignored from the doctree.
+      docname = self.app.env.path2doc(source_path)
+      self.app.env.metadata[docname] = {'orphan': True}
+
+      destination = os.path.join(self.app.outdir, os.path.dirname(docname))
+
+      for line in inputstring.splitlines():
+         line = line.strip()
+         if not line:
+            # Empty lines are ignored.
+            continue
+
+         if line.startswith('#'):
+            # This is a comment and is ignored.
+            continue
+
+         # Stop as fist line and use it as source.
+         source = os.path.abspath(os.path.join(
+            os.path.dirname(source_path), line))
+         break
+
+      logger.info(
+         'pydoctor API docs from %s to %s (this is a long process)...' % (
+            source, destination))
       BuildAPIDocsScript().main([source, destination])
       logger.info('pydoctor API docs done.')
-
-   def finish(self):
-      pass
 
 
 def setup(app):
    """
    This is called by Sphinx extension system.
    """
-   app.add_config_value(
-      'apidocs',
-      {'source': 'src', 'destination': 'api'},
-      'env',
-      )
    app.add_source_suffix('.pydoctor', 'pydoctor')
-   app.add_builder(PyDoctorBuilder)
+   app.add_source_parser(PyDoctorParser)
