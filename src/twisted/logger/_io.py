b4184d16a77f9e2c4f94e8cb6ae56c9811b45d62
Wilfredo Sánchez <wsanchez@wsanchez.net>
2020-10-03 15:44:33 -0700

    Add type hints to twisted.logger

--- src/twisted/logger/_io.py
+++ src/twisted/logger/_io.py
@@ -7,8 +7,12 @@ File-like object that logs.
 """
 
 import sys
+from typing import AnyStr, Iterable, Optional
+
+from constantly import NamedConstant
 
 from ._levels import LogLevel
+from ._logger import Logger
 
 
 class LoggingFile:
@@ -21,23 +25,21 @@ class LoggingFile:
 
     @ivar softspace: File-like L{'softspace' attribute <file.softspace>}; zero
         or one.
-    @type softspace: L{int}
     """
 
     softspace = 0
 
-    def __init__(self, logger, level=LogLevel.info, encoding=None):
+    def __init__(
+        self,
+        logger: Logger,
+        level: NamedConstant = LogLevel.info,
+        encoding: Optional[str] = None,
+    ) -> None:
         """
         @param logger: the logger to log through.
-
         @param level: the log level to emit events with.
-
         @param encoding: The encoding to expect when receiving bytes via
             C{write()}.  If L{None}, use C{sys.getdefaultencoding()}.
-        @type encoding: L{str}
-
-        @param log: The logger to send events to.
-        @type log: L{Logger}
         """
         self.level = level
         self.log = logger
@@ -51,53 +53,48 @@ class LoggingFile:
         self._closed = False
 
     @property
-    def closed(self):
+    def closed(self) -> bool:
         """
         Read-only property.  Is the file closed?
 
         @return: true if closed, otherwise false.
-        @rtype: L{bool}
         """
         return self._closed
 
     @property
-    def encoding(self):
+    def encoding(self) -> str:
         """
         Read-only property.   File encoding.
 
         @return: an encoding.
-        @rtype: L{str}
         """
         return self._encoding
 
     @property
-    def mode(self):
+    def mode(self) -> str:
         """
         Read-only property.  File mode.
 
         @return: "w"
-        @rtype: L{str}
         """
         return "w"
 
     @property
-    def newlines(self):
+    def newlines(self) -> None:
         """
         Read-only property.  Types of newlines encountered.
 
         @return: L{None}
-        @rtype: L{None}
         """
         return None
 
     @property
-    def name(self):
+    def name(self) -> str:
         """
         The name of this file; a repr-style string giving information about its
         namespace.
 
         @return: A file name.
-        @rtype: L{str}
         """
         return "<{0} {1}#{2}>".format(
             self.__class__.__name__,
@@ -105,73 +102,69 @@ class LoggingFile:
             self.level.name,
         )
 
-    def close(self):
+    def close(self) -> None:
         """
         Close this file so it can no longer be written to.
         """
         self._closed = True
 
-    def flush(self):
+    def flush(self) -> None:
         """
         No-op; this file does not buffer.
         """
         pass
 
-    def fileno(self):
+    def fileno(self) -> int:
         """
         Returns an invalid file descriptor, since this is not backed by an FD.
 
         @return: C{-1}
-        @rtype: L{int}
         """
         return -1
 
-    def isatty(self):
+    def isatty(self) -> bool:
         """
         A L{LoggingFile} is not a TTY.
 
         @return: C{False}
-        @rtype: L{bool}
         """
         return False
 
-    def write(self, string):
+    def write(self, message: AnyStr) -> None:
         """
         Log the given message.
 
-        @param string: Data to write.
-        @type string: L{bytes} in this file's preferred encoding or L{unicode}
+        @param message: The message to write.
         """
         if self._closed:
             raise ValueError("I/O operation on closed file")
 
-        if isinstance(string, bytes):
-            string = string.decode(self._encoding)
+        if isinstance(message, bytes):
+            text = message.decode(self._encoding)
+        else:
+            text = message
 
-        lines = (self._buffer + string).split("\n")
+        lines = (self._buffer + text).split("\n")
         self._buffer = lines[-1]
         lines = lines[0:-1]
 
         for line in lines:
             self.log.emit(self.level, format="{log_io}", log_io=line)
 
-    def writelines(self, lines):
+    def writelines(self, lines: Iterable[AnyStr]) -> None:
         """
         Log each of the given lines as a separate message.
 
         @param lines: Data to write.
-        @type lines: iterable of L{unicode} or L{bytes} in this file's
-            declared encoding
         """
         for line in lines:
             self.write(line)
 
-    def _unsupported(self, *args):
+    def _unsupported(self, *args: object) -> None:
         """
         Template for unsupported operations.
 
         @param args: Arguments.
-        @type args: tuple of L{object}
         """
         raise IOError("unsupported operation")
 
