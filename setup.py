ae827aee196810739fac62ab79ab709c784c1fa8
Thomas Grainger <tagrain@gmail.com>
2020-10-14 09:28:21 +0100

    simplify setuptools configuration

--- setup.py
+++ setup.py
@@ -7,16 +7,34 @@
 Setuptools installer for Twisted.
 """
 
+import pathlib
+import platform
+import re
 import sys
+
 import setuptools
 
-if __name__ == "__main__":
 
-    _setup = {}
-    with open("src/twisted/python/_setup.py") as f:
-        exec(f.read(), _setup)
-
-    try:
-        setuptools.setup(**_setup["getSetupArgs"]())
-    except KeyboardInterrupt:
-        sys.exit(1)
+setuptools.setup(
+    # Munge links of the form `NEWS <NEWS.rst>`_ to point at the appropriate
+    # location on GitHub so that they function when the long description is
+    # displayed on PyPI.
+    long_description=re.sub(
+        r"`([^`]+)\s+<(?!https?://)([^>]+)>`_",
+        r"`\1 <https://github.com/twisted/twisted/blob/trunk/\2>`_",
+        pathlib.Path("README.rst").read_text(encoding="utf8"),
+        flags=re.I,
+    ),
+    ext_modules=[
+        setuptools.Extension(
+            "twisted.internet.iocpreactor.iocpsupport",
+            sources=[
+                "src/twisted/internet/iocpreactor/iocpsupport/iocpsupport.c",
+                "src/twisted/internet/iocpreactor/iocpsupport/winsock_pointers.c",
+            ],
+            libraries=["ws2_32"],
+        )
+    ]
+    if sys.platform == "win32" and platform.python_implementation() == "CPython"
+    else [],
+)
