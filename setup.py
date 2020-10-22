cadfb6e8fee115f5374078e5aca56072e47b7041
Thomas Grainger <tagrain@gmail.com>
2020-10-22 17:39:35 +0100

    demo pre-commit lint failure

--- setup.py
+++ setup.py
@@ -17,10 +17,5 @@ setuptools.setup(
     # Munge links of the form `NEWS <NEWS.rst>`_ to point at the appropriate
     # location on GitHub so that they function when the long description is
     # displayed on PyPI.
-    long_description=re.sub(
-        r"`([^`]+)\s+<(?!https?://)([^>]+)>`_",
-        r"`\1 <https://github.com/twisted/twisted/blob/trunk/\2>`_",
-        pathlib.Path("README.rst").read_text(encoding="utf8"),
-        flags=re.I,
-    )
+    long_description=re.sub(r"`([^`]+)\s+<(?!https?://)([^>]+)>`_", r"`\1 <https://github.com/twisted/twisted/blob/trunk/\2>`_", pathlib.Path("README.rst").read_text(encoding="utf8"), flags=re.I)
 )
