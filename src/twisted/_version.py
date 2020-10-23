c2984d01ef981f37eece332f0afb8ef65f4388ca
Adi Roiban <adi.roiban@chevah.com>
2020-10-23 18:34:26 +0100

    Revert trunk version. Enable PROD pypi.

--- src/twisted/_version.py
+++ src/twisted/_version.py
@@ -7,5 +7,5 @@ Provides Twisted version information.
 
 from incremental import Version
 
-__version__ = Version("Twisted", 20, 11, 0, dev=6)
+__version__ = Version("Twisted", 20, 3, 0, dev=0)
 __all__ = ["__version__"]
