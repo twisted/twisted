faa7d8b61443ed9d0ceb87af4b48c0a7137ded58
Adi Roiban <adi.roiban@chevah.com>
2020-10-23 18:12:50 +0100

    Use pep440 for tag version checks.

--- admin/check_tag_version_match.py
+++ admin/check_tag_version_match.py
@@ -3,11 +3,13 @@
 # tag that has the same version as the current twisted.__version.
 #
 # Designed to be conditionally called inside GitHub Actions release job.
+# Tags should use PEP440 version scheme.
 #
 # To be called as: admin/check_tag_version_match.py refs/tags/twisted-20.3.0
 #
 import sys
-from twisted import __version__
+
+import pep517.meta
 
 TAG_PREFIX = "refs/tags/twisted-"
 
@@ -15,6 +17,7 @@ if len(sys.argv) < 2:
     print("No tag check requested.")
     sys.exit(0)
 
+branch_version = pep517.meta.load(".").version
 run_version = sys.argv[1]
 
 if not run_version.startswith(TAG_PREFIX):
@@ -23,9 +26,9 @@ if not run_version.startswith(TAG_PREFIX):
 
 run_version = run_version[len(TAG_PREFIX) :]
 
-if run_version != __version__:
-    print("Branch is at '{}' while tag is '{}'".format(__version__, run_version))
+if run_version != branch_version:
+    print("Branch is at '{}' while tag is '{}'".format(branch_version, run_version))
     exit(1)
 
-print("All good. Branch and tag versions match for "%s"." % (__version__,))
+print("All good. Branch and tag versions match for '{}'.".format(branch_version))
 sys.exit(0)
