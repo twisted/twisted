80b02d87adc48f813179aaa31ff5aaaa574200e6
Adi Roiban <adi.roiban@chevah.com>
2020-10-23 20:27:26 +0100

    Initial commit.

---
+++ docs/_extensions/apidocs.py
@@ -0,0 +1,81 @@
+"""
+Extension to trigger the pydoctor API builds as part of the Sphinx build.
+
+This is created to have API docs created on Read the docs.
+
+The API docs build is ignored when this is not running outside of the
+Read The Docs environment.
+"""
+import os
+
+from sphinx.parsers import Parser
+from sphinx.util import logging
+
+from twisted.python._release import BuildAPIDocsScript
+
+
+logger = logging.getLogger(__name__)
+
+
+class PyDoctorParser(Parser):
+    """
+    Will transform the pydoctor instruction file.
+    """
+
+    supported = ("pydoctor",)
+
+    def get_transforms(self):
+        """"""
+        return []
+
+    def parse(self, inputstring, document):
+        """
+        This is called when a pydoctor marker file is found.
+        """
+        source_path = document.attributes["source"]
+
+        # Let Sphinx know that the file should be ignored from the doctree.
+        docname = self.app.env.path2doc(source_path)
+        self.app.env.metadata[docname] = {"orphan": True}
+
+        destination = os.path.join(self.app.outdir, os.path.dirname(docname))
+
+        for line in inputstring.splitlines():
+            line = line.strip()
+            if not line:
+                # Empty lines are ignored.
+                continue
+
+            if line.startswith("#"):
+                # This is a comment and is ignored.
+                continue
+
+            # Stop as fist line and use it as source.
+            source = os.path.abspath(os.path.join(os.path.dirname(source_path), line))
+            break
+
+        logger.info(
+            "pydoctor API docs from {} to {} (this is a long process)...".format(
+                source, destination
+            )
+        )
+
+        errors = BuildAPIDocsScript().main([source, destination])
+        for line in errors:
+            # Emit captured pydoctor output as warnings.
+            logger.warning(line)
+
+        logger.info("pydoctor API docs done.")
+
+
+def setup(app):
+    """
+    This is called by Sphinx extension system.
+    """
+    on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+    if not on_rtd:
+        logger.info("Skip pydoctor API docs buld without env READTHEDOCS=True")
+        return
+
+    app.add_source_suffix(".pydoctor", "pydoctor")
+    app.add_source_parser(PyDoctorParser)
