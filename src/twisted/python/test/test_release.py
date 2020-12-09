68dc0a06ad9484661dd423acca12e8c24dc99c9a
Adi Roiban <adi.roiban@chevah.com>
2020-12-09 08:30:38 +0000

    Filter at deps level.

--- src/twisted/python/test/test_release.py
+++ src/twisted/python/test/test_release.py
@@ -52,9 +52,6 @@ if sys.platform != "win32":
 else:
     skip = "Release toolchain only supported on POSIX."
 
-if sys.version_info[0:2] in [(3, 5), (3, 6), (3, 7)]:
-    skip = "Release toolchain not supported on this Python version."
-
 
 class ExternalTempdirTestCase(TestCase):
     """
