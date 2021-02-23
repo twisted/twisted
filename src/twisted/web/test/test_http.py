f0d8544a72910ff7c6659dd69c2e9456ffcbffeb
Thomas Grainger <tagrain@gmail.com>
2021-02-23 21:34:51 +0000

    skip failing twisted.web.test.test_http.QueryArgumentsTests.testParseqs

--- src/twisted/web/test/test_http.py
+++ src/twisted/web/test/test_http.py
@@ -9,6 +9,7 @@ Test HTTP support.
 import base64
 import calendar
 import random
+import sys
 
 import hamcrest
 
@@ -2319,6 +2320,7 @@ ok
 
 
 class QueryArgumentsTests(unittest.TestCase):
+    @skipIf(sys.version_info >= (3, 6), "newer py3.6 parse_qs treat ; differently")
     def testParseqs(self):
         self.assertEqual(parse_qs(b"a=b&d=c;+=f"), http.parse_qs(b"a=b&d=c;+=f"))
         self.assertRaises(ValueError, http.parse_qs, b"blah", strict_parsing=True)
