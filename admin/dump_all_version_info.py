836cfc06db90f4f966e968ec79a5fdb15359fb30
Adi Roiban <adi.roiban@chevah.com>
2021-03-03 20:38:38 +0000

    Show deps as part of normal tox run.

--- admin/dump_all_version_info.py
+++ admin/dump_all_version_info.py
@@ -1,7 +1,19 @@
+import os
 import sys
 
+from pip._internal.cli.main import main
+
+print("::group::Python info")
 print(sys.prefix)
 print(sys.exec_prefix)
 print(sys.executable)
 print(sys.version)
 print(sys.platform)
+print("::endgroup::")
+
+if os.environ.get("CI", "").lower() == "true":
+    # On CI show the exact deps found at runtime.
+    # Skipped on local dev to speed up the test execution.
+    print("::group::Deps info")
+    main(["freeze", "--all"])
+    print("::endgroup::")
