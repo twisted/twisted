cad4493471cc86dd4049842d568f13bebc62cf4d
Adi Roiban <adi.roiban@chevah.com>
2021-05-24 19:33:10 +0100

    Increase priority for ecdh-sha2-nistp521 and lower for ecdh-sha2-nistp256.

--- src/twisted/conch/ssh/_kex.py
+++ src/twisted/conch/ssh/_kex.py
@@ -88,14 +88,14 @@ class _Curve25519SHA256LibSSH:
 
 
 @implementer(_IEllipticCurveExchangeKexAlgorithm)
-class _ECDH256:
+class _ECDH512:
     """
-    Elliptic Curve Key Exchange with SHA-256 as HASH. Defined in
+    Elliptic Curve Key Exchange with SHA-512 as HASH. Defined in
     RFC 5656.
     """
 
     preference = 3
-    hashProcessor = sha256
+    hashProcessor = sha512
 
 
 @implementer(_IEllipticCurveExchangeKexAlgorithm)
@@ -110,14 +110,14 @@ class _ECDH384:
 
 
 @implementer(_IEllipticCurveExchangeKexAlgorithm)
-class _ECDH512:
+class _ECDH256:
     """
-    Elliptic Curve Key Exchange with SHA-512 as HASH. Defined in
+    Elliptic Curve Key Exchange with SHA-256 as HASH. Defined in
     RFC 5656.
     """
 
     preference = 5
-    hashProcessor = sha512
+    hashProcessor = sha256
 
 
 @implementer(_IGroupExchangeKexAlgorithm)
