66f934cc5727b25e0ff24bc7ebab6872e8e199bc
Glyph <glyph@twistedmatrix.com>
2022-03-28 20:32:57 -0700

    third implementation to compare

--- src/twisted/internet/_corohost.py
+++ src/twisted/internet/_corohost.py
@@ -1,26 +1,15 @@
+# -*- test-case-name: twisted.internet.test.test_endpoints -*-
+from __future__ import annotations
+
 from dataclasses import dataclass, field
-from typing import (
-    AsyncIterable,
-    Callable,
-    Generic,
-    List,
-    Literal,
-    Optional,
-    TYPE_CHECKING,
-    Tuple,
-    TypeVar,
-    Union,
-)
+from enum import Enum, auto
+from typing import AsyncIterable, Callable, Generic, List, Literal, Optional, TYPE_CHECKING, Tuple, TypeVar, Union
 
 from ._shutil import Outstanding
-from twisted.internet.address import IPv4Address, IPv6Address
-from twisted.internet.defer import Deferred, DeferredList
-from twisted.internet.error import TimeoutError
-from twisted.internet.interfaces import (
-    IProtocolFactory,
-    IReactorTime,
-    IStreamClientEndpoint,
-)
+from twisted.internet.address import HostnameAddress, IPv4Address, IPv6Address
+from twisted.internet.defer import CancelledError, Deferred, DeferredList, succeed
+from twisted.internet.error import ConnectError, ConnectingCancelledError, DNSLookupError, TimeoutError
+from twisted.internet.interfaces import IProtocolFactory, IReactorTime, IStreamClientEndpoint
 from twisted.internet.task import deferLater
 from twisted.python.failure import Failure
 
@@ -37,30 +26,31 @@ from twisted.internet.interfaces import (
 T = TypeVar("T")
 X = TypeVar("X")
 
+class DoneSentinel(Enum):
+    Done = auto()
 
 def push2aiter() -> Tuple[Callable[[T], None], Callable[[], None], AsyncIterable[T]]:
     """
     Create a Deferred coroutine which presents an async iterable, and a
     callable that will push values into it and a callable that will stop it.
     """
-    each: Deferred[Union[T, object]] = Deferred()
-    done = object()
+    q: List[Deferred[Union[T, Literal[DoneSentinel.Done]]]] = []
 
     async def aiter() -> AsyncIterable[T]:
         while True:
-            out = await each
-            if out is done:
+            if not q:
+                q.append(Deferred())
+            out = await q.pop(0)
+            if out is DoneSentinel.Done:
                 return
             # 'is done' is a type guard that mypy can't see
-            yield out  # type: ignore
+            yield out
 
-    def push(value: T) -> None:
-        nonlocal each
-        old, each = each, Deferred()
-        old.callback(value)
+    def push(value: Union[T, DoneSentinel]) -> None:
+        q.append(succeed(value))
 
     def stop() -> None:
-        each.callback(done)
+        push(DoneSentinel.Done)
 
     return push, stop, aiter()
 
@@ -96,6 +86,7 @@ class MultiFirer(Generic[T]):
     activeTimeout: Optional[Deferred[None]] = None
     waiting: Optional[Deferred[Tuple[bool, Optional[T]]]] = None
     hasResult: bool = False
+    hasFailure: bool = False
     finalResult: Optional[T] = None
     ended: bool = False
     failures: List[Failure] = field(default_factory=list)
@@ -114,7 +105,7 @@ class MultiFirer(Generic[T]):
 
         def failed(failure: Failure) -> None:
             self.failures.append(failure)
-            self._maybeFinallyFail()
+            self._maybeCompleteWaiting(False, None)
             return None
 
         deferred.addCallbacks(complete, failed)
@@ -130,9 +121,15 @@ class MultiFirer(Generic[T]):
         def timedOut(nothing: None) -> None:
             self._maybeCompleteWaiting(False, None)
 
-        self.activeTimeout = deferLater(clock, seconds).addCallback(timedOut)
-        self.waiting = Deferred(cancel)
-        return self.waiting
+        def ignoreCancel(f: Failure) -> None:
+            f.trap(CancelledError)
+
+        self.activeTimeout = deferLater(clock, seconds).addCallbacks(
+            timedOut, ignoreCancel
+        )
+        w = self.waiting = Deferred(cancel)
+        self._maybeFinallyFail()
+        return w
 
     def end(self) -> None:
         """
@@ -142,7 +139,6 @@ class MultiFirer(Generic[T]):
         self._maybeFinallyFail()
 
     def cancel(self) -> None:
-        assert self.ended
         self.deferreds.cancel()
 
     def _maybeFinallyFail(self) -> None:
@@ -157,9 +153,15 @@ class MultiFirer(Generic[T]):
             self.activeTimeout.cancel()
             self.activeTimeout = None
             it, self.waiting = self.waiting, None
-            it.errback(RuntimeError(f"multiple failures {self.failures}"))
+            e: Union[Failure, Exception]
+            if len(self.failures) == 1:
+                e = self.failures[0]
+            else:
+                e = RuntimeError(f"multiple failures {self.failures}")
+            it.errback(e)
 
     def _maybeCompleteWaiting(self, actuallyDone: bool, value: Optional[T]) -> None:
+        self._maybeFinallyFail()
         if self.waiting is not None and self.activeTimeout is not None:
             self.activeTimeout.cancel()
             self.activeTimeout = None
@@ -167,12 +169,14 @@ class MultiFirer(Generic[T]):
             it.callback((actuallyDone, value))
 
 
-async def start(
-    pf: IProtocolFactory, endpoint: HostnameEndpoint, reactor: IReactorTime
+async def _start(
+    endpoint: HostnameEndpoint,
+    pf: IProtocolFactory,
 ) -> TwistedProtocol:
     """
     resolver coroutine that runs
     """
+    assert endpoint is not None, "Wtf"
     p: Callable[[IAddress], None]
     s: Callable[[], None]
     ai: AsyncIterable[IAddress]
@@ -190,22 +194,44 @@ async def start(
             s()
 
     resolver = endpoint._nameResolver
-    resolver.resolveHostName(res(), endpoint._hostStr)
+    reactor = endpoint._reactor
+    resolver.resolveHostName(res(), endpoint._hostStr, portNumber=endpoint._port)
 
     mf: MultiFirer[TwistedProtocol] = MultiFirer()
-    async for addr in ai:
-        ep = addr2endpoint(endpoint, addr)
-        if ep is None:
-            continue
-        mf.add(ep.connect(pf))
-        done, result = await mf.wait(reactor, endpoint._attemptDelay)
-        if done:
+    try:
+        attempts = 0
+        async for addr in ai:
+            ep = addr2endpoint(endpoint, addr)
+            if ep is None:
+                continue
+            attempts += 1
+            mf.add(ep.connect(pf))
+            done, result = await mf.wait(reactor, endpoint._attemptDelay)
+            if done:
+                assert result is not None
+                return result
+        mf.end()
+        if not attempts:
+            raise DNSLookupError(f"no results for hostname lookup: {endpoint._hostStr}")
+        done, result = await mf.wait(reactor, 10000)
+        if done is True:
             assert result is not None
             return result
-    mf.end()
-    done, result = await mf.wait(reactor, 10000)
-    if done is True:
-        assert result is not None
-        return result
-    mf.cancel()
-    raise TimeoutError("final connection failed")
+        mf.cancel()
+    finally:
+        ty, v, tb = exc_info()
+        if ty is not GeneratorExit:
+            mf.cancel()
+from sys import exc_info
+
+def start(
+    endpoint: HostnameEndpoint,
+    pf: IProtocolFactory,
+) -> Deferred[TwistedProtocol]:
+    def translateCancel(f: Failure) -> Failure:
+        f.trap(CancelledError, ConnectingCancelledError)
+        raise ConnectingCancelledError(
+            HostnameAddress(endpoint._hostBytes, endpoint._port)
+        )
+
+    return Deferred.fromCoroutine(_start(endpoint, pf)).addErrback(translateCancel)
