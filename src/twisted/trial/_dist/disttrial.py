cc45ae4cf62a651d7900ffaf23a3f971429872d4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-04-26 12:52:52 -0400

    move the very generic stuff off to the side

--- src/twisted/trial/_dist/disttrial.py
+++ src/twisted/trial/_dist/disttrial.py
@@ -11,24 +11,13 @@ responsible for coordinating all of trial's behavior at the highest level.
 
 import os
 import sys
-from functools import partial, wraps
-from typing import (
-    Awaitable,
-    Callable,
-    Iterable,
-    List,
-    Optional,
-    Sequence,
-    TextIO,
-    TypeVar,
-    Union,
-    cast,
-)
+from functools import partial
+from typing import Iterable, List, Optional, Sequence, TextIO, Union, cast
 from unittest import TestSuite
 
 from attrs import define
 
-from twisted.internet.defer import Deferred, gatherResults, succeed
+from twisted.internet.defer import Deferred
 from twisted.internet.interfaces import IReactorCore, IReactorProcess
 from twisted.logger import Logger
 from twisted.python.failure import Failure
@@ -42,30 +31,14 @@ from ..runner import TestHolder
 from ..util import _unusedTestDirectory, openTestLog
 from . import _WORKER_AMP_STDIN, _WORKER_AMP_STDOUT
 from .distreporter import DistReporter
+from .functional import countingCalls, fromOptional, iterateWhile, parallel, void
 from .worker import LocalWorker, LocalWorkerAMP
-
-_A = TypeVar("_A")
-_B = TypeVar("_B")
-_C = TypeVar("_C")
 
 
 class IDistTrialReactor(IReactorCore, IReactorProcess):
     """
     The reactor interfaces required by disttrial.
     """
-
-
-def fromOptional(default: _A, optional: Optional[_A]) -> _A:
-    """
-    Get a definite value from an optional value.
-
-    @param default: The value to return if the optional value is missing.
-
-    @param optional: The optional value to return if it exists.
-    """
-    if optional is None:
-        return default
-    return optional
 
 
 @define
@@ -236,98 +209,6 @@ class WorkerPool:
             workers,
             ampWorkers,
         )
-
-
-async def sequence(a: Awaitable[_A], b: Awaitable[_B]) -> _B:
-    """
-    Wait for one action to complete and then another.
-
-    If either action fails, failure is propagated.  If the first action fails,
-    the second action is not waited on.
-    """
-    await a
-    return await b
-
-
-def flip(f: Callable[[_A, _B], _C]) -> Callable[[_B, _A], _C]:
-    """
-    Create a function like another but with the order of the first two
-    arguments flipped.
-    """
-
-    @wraps(f)
-    def g(b, a):
-        return f(a, b)
-
-    return g
-
-
-def compose(fx: Callable[[_B], _C], fy: Callable[[_A], _B]) -> Callable[[_A], _C]:
-    """
-    Create a function that calls one function with an argument and then
-    another function with the result of the first function.
-    """
-
-    @wraps(fx)
-    @wraps(fy)
-    def g(a):
-        return fx(fy(a))
-
-    return g
-
-
-# Discard the result of an awaitable and substitute None in its place.
-#
-# Ignore the `Cannot infer type argument 1 of "compose"`
-# https://github.com/python/mypy/issues/6220
-void: Callable[[Awaitable[_A]], Deferred[None]] = compose(  # type: ignore[misc]
-    Deferred.fromCoroutine,
-    partial(flip(sequence), succeed(None)),
-)
-
-
-async def parallel(actions: Iterable[Deferred[_A]]) -> List[_A]:
-    """
-    Await all of the awaitables at the same time.
-    """
-    return await gatherResults(actions)
-
-
-async def iterateWhile(
-    predicate: Callable[[_A], bool],
-    action: Callable[[], Awaitable[_A]],
-) -> _A:
-    """
-    Call a function repeatedly until its result fails to satisfy a predicate.
-
-    @param predicate: The check to apply.
-
-    @param action: The function to call.
-
-    @return: The result of C{action} which did not satisfy C{predicate}.
-    """
-    while True:
-        result = await action()
-        if not predicate(result):
-            return result
-
-
-def countingCalls(f: Callable[[int], _A]) -> Callable[[], _A]:
-    """
-    Wrap a function with another that automatically passes an integer counter
-    of the number of calls that have gone through the wrapper.
-    """
-    counter = 0
-
-    def g():
-        nonlocal counter
-        try:
-            result = f(counter)
-        finally:
-            counter += 1
-        return result
-
-    return g
 
 
 def shouldContinue(untilFailure: bool, result: IReporter) -> bool:
