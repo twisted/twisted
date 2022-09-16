93d8346bf950d82856623e33ab5c9ccaa4919500
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-09-16 11:50:32 -0400

    run even more examples, faster

--- src/twisted/trial/_dist/test/__init__.py
+++ src/twisted/trial/_dist/test/__init__.py
@@ -4,3 +4,8 @@
 """
 Distributed trial test runner tests.
 """
+
+from hypothesis import settings
+
+settings.register_profile("x", max_examples=100000, print_blob=True, report_multiple_bugs=True)
+settings.load_profile("x")
