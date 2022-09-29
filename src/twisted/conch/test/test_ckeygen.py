4ce8a1d5f412e8e4da5d1921df23e47a11b8d972
eevel <eevel@weezel3.weezelnet>
2022-09-28 22:59:35 -0500

    add test for omitted line

--- src/twisted/conch/test/test_ckeygen.py
+++ src/twisted/conch/test/test_ckeygen.py
@@ -174,6 +174,19 @@ class KeyGenTests(TestCase):
             "Unsupported fingerprint format: sha-base64", em.exception.args[0]
         )
 
+    def test_printFingerprintSuffixAppended(self):
+        """
+        L{printFingerprint} checks if the filename with the  '.pub' suffix
+        exists in ~/.ssh.
+        """
+        filename = self.mktemp()
+        FilePath(filename + '.pub').setContent(publicRSA_openssh)
+        printFingerprint({"filename": filename, "format": "md5-hex"})
+        self.assertEqual(
+            self.stdout.getvalue(),
+            "2048 85:25:04:32:58:55:96:9f:57:ee:fb:a8:1a:ea:69:da temp\n",
+        )
+
     def test_saveKey(self):
         """
         L{_saveKey} writes the private and public parts of a key to two
