346922714c66081def8f01716fe3a19937019468
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-10-07 13:23:26 -0400

    deprecate the other gnomish reactors

--- src/twisted/internet/__init__.py
+++ src/twisted/internet/__init__.py
@@ -10,3 +10,29 @@ observers need not care about which event loop is running. Thus, it is possible
 to use the same code for different loops, from Twisted's basic, yet portable,
 select-based loop to the loops of various GUI toolkits like GTK+ or Tk.
 """
+
+from twisted.python.deprecate import deprecatedModuleAttribute
+from twisted.python.versions import Version
+
+deprecatedModuleAttribute(
+    Version("Twisted", "NEXT", 0, 0),
+    "Please use twisted.internet.gireactor instead.",
+    "twisted.internet",
+    "gtk2reactor",
+)
+
+deprecatedModuleAttribute(
+    Version("Twisted", "NEXT", 0, 0),
+    "Please use twisted.internet.gireactor instead.",
+    "twisted.internet",
+    "gtk3reactor",
+)
+
+deprecatedModuleAttribute(
+    Version("Twisted", "NEXT", 0, 0),
+    "Please use twisted.internet.gireactor instead.",
+    "twisted.internet",
+    "glib2reactor",
+)
+
+del Version, deprecatedModuleAttribute
