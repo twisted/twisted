b177345fdc040250ef4d69ce3cd2ff7b115f542b
Glyph <glyph@twistedmatrix.com>
2022-10-19 02:42:12 -0700

    let's not be so clever

--- src/twisted/internet/gtk2reactor.py
+++ src/twisted/internet/gtk2reactor.py
@@ -51,18 +51,13 @@ except (ImportError, AttributeError):
 
 import gobject  # type: ignore[import]
 
-try:
-    gobject.IO_HUP
-except AttributeError as ae:
-    if " gi.repository " in str(ae):
-        # gi.repository's legacy compatibility helper raises an AttributeError
-        # with a custom error message rather than a useful ImportError, so
-        # things tend to fail loudly.  Things that import this module expect an
-        # ImportError if, well, something failed to import, and treat an
-        # AttributeError as an arbitrary application code failure, so we
-        # satisfy that expectation here.
-        raise ImportError("pygobject 2.x is not actually installed, use gireactor")
-    raise
+if not hasattr(gobject, "IO_HUP"):
+    # gi.repository's legacy compatibility helper raises an AttributeError with
+    # a custom error message rather than a useful ImportError, so things tend
+    # to fail loudly.  Things that import this module expect an ImportError if,
+    # well, something failed to import, and treat an AttributeError as an
+    # arbitrary application code failure, so we satisfy that expectation here.
+    raise ImportError("pygobject 2.x is not installed. Use the `gi` reactor.")
 
 if hasattr(gobject, "threads_init"):
     # recent versions of python-gtk expose this. python-gtk=2.4.1
