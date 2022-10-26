b89944c202221499d24e66e2ff7819408286e048
Ofek Lev <ofekmeister@gmail.com>
2022-10-26 14:32:38 -0400

    address
    Co-Authored-By: Glyph <glyph@twistedmatrix.com>

--- src/twisted/__main__.py
+++ src/twisted/__main__.py
@@ -8,7 +8,7 @@
 
 import sys
 
-from twisted.application.twist._twist import Twist
-
 if __name__ == "__main__":
+    from twisted.application.twist._twist import Twist
+
     sys.exit(Twist.main())
