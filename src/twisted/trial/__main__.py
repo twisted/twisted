abfd5a47960fa4d04506bc6fd190f22d46a19b6a
Ofek Lev <ofekmeister@gmail.com>
2022-10-25 20:25:38 -0400

    Remove use of pkg_resources

--- src/twisted/trial/__main__.py
+++ src/twisted/trial/__main__.py
@@ -4,6 +4,18 @@
 if __name__ == "__main__":
     import sys
 
-    from pkg_resources import load_entry_point
+    if sys.version_info >= (3, 8):
+        from importlib.metadata import distribution
+    else:
+        from importlib_metadata import distribution
 
-    sys.exit(load_entry_point("Twisted", "console_scripts", "trial")())
+    ep_name = "trial"
+    dist = distribution("twisted")
+    for ep in dist.entry_points:
+        if ep.group == "console_scripts" and ep.name == ep_name:
+            trial = ep
+            break
+    else:
+        raise OSError(f"No console_scripts entry point found for: {ep_name}")
+
+    sys.exit(trial.load()())
