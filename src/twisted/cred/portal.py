86f2041c5a3a900006099905d00d20316433d9fc
Glyph <glyph@twistedmatrix.com>
2023-02-24 16:51:04 -0800

    account for ANONYMOUS with empty tuple

--- src/twisted/cred/portal.py
+++ src/twisted/cred/portal.py
@@ -39,7 +39,7 @@ class IRealm(Interface):
     """
 
     def requestAvatar(
-        avatarId: bytes, mind: object, *interfaces: _InterfaceItself
+        avatarId: bytes | Tuple[()], mind: object, *interfaces: _InterfaceItself
     ) -> Union[Deferred[_requestResult], _requestResult]:
         """
         Return avatar which provides one of the given interfaces.
