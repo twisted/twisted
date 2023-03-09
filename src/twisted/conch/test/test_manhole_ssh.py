5b913da625c2b7fa7ec0e8df790c5255f88cfc5d
David Robertson <david.m.robertson1@gmail.com>
2023-03-09 18:19:27 +0000

    Skip test if cryptography is not available

--- src/twisted/conch/test/test_manhole_ssh.py
+++ src/twisted/conch/test/test_manhole_ssh.py
@@ -1,12 +1,21 @@
 from hamcrest import assert_that
 
-from twisted.conch.manhole_ssh import TerminalRealm
 from twisted.cred.portal import IRealm
 from twisted.trial import unittest
 from twisted.trial.test.matchers import provides
 
+try:
+    from twisted.conch.manhole_ssh import TerminalRealm
+except ImportError:
+    ssh = False
+else:
+    ssh = True
+
 
 class TerminalRealmTests(unittest.TestCase):
+    if not ssh:
+        skip = "cannot import t.conch.manhole_ssh (cryptography requirements missing?)"
+
     def test_TerminalRealm_implements_IRealm(self) -> None:
         """Regression test for #11812."""
         assert_that(TerminalRealm(), provides(IRealm))
