596bbecad484b41760f2f7d3c5055c3cb7e8e0f5
Erik Johnston <erik@matrix.org>
2023-06-25 20:14:50 +0100

    Add test

--- src/twisted/test/test_defer.py
+++ src/twisted/test/test_defer.py
@@ -12,7 +12,9 @@ import gc
 import re
 import traceback
 import types
+import unittest as pyunit
 import warnings
+import weakref
 from asyncio import (
     AbstractEventLoop,
     CancelledError,
@@ -30,6 +32,7 @@ from typing import (
     Mapping,
     NoReturn,
     Optional,
+    Set,
     Tuple,
     Type,
     TypeVar,
@@ -58,6 +61,7 @@ from twisted.internet.defer import (
 )
 from twisted.internet.task import Clock
 from twisted.python import log
+from twisted.python.compat import _PYPY
 from twisted.python.failure import Failure
 from twisted.trial import unittest
 
@@ -3796,3 +3800,29 @@ class CoroutineContextVarsTests(unittest.TestCase):
         clock.advance(1)
 
         self.assertEqual(self.successResultOf(d), True)
+
+    @pyunit.skipIf(_PYPY, "GC works differently on PyPy.")
+    def test_inlineCallbacksNoCircularReference(self) -> None:
+        """
+        Tests that there is no circular dependency when using
+        L{defer.inlineCallbacks}, so that the machinery gets cleaned up
+        immediately rather than waiting for a GC.
+        """
+
+        # Create an object and weak reference to track if its gotten freed.
+        obj: Set[Any] = set()
+        objWeakRef = weakref.ref(obj)
+
+        async def func(a: Any) -> Any:
+            return a
+
+        # Run the function
+        funcD = Deferred.fromCoroutine(func(obj))
+        assert funcD.called
+
+        # Delete the local references to obj and funcD.
+        del obj
+        del funcD
+
+        # The object has been freed if the weak reference returns None.
+        self.assertIsNone(objWeakRef())
