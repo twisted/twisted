c9907b188c35144056417b5a1d8d4674c0aacda9
Alexandr Nesterenko <kuchaspama@gmail.com>
2023-10-19 01:07:10 +0300

    test for long trailing headers without final CRLF characters

--- src/twisted/web/test/test_http.py
+++ src/twisted/web/test/test_http.py
@@ -1511,6 +1511,24 @@ class ChunkedTransferEncodingTests(unittest.TestCase):
             b"3\r\nabc\r\n0\r\nTotal-Trailer: header;greater-then=10\r\n\r\n",
         )
 
+    def test_unfinishedTrailerHeader(self):
+        r"""
+        L{_ChunkedTransferDecoder.dataReceived} raises
+        L{_MalformedChunkedDataError} when the trailing headers data is too long
+        and doesn't have final CRLF characters.
+        """
+        p = http._ChunkedTransferDecoder(
+            lambda b: None,
+            lambda b: None,  # pragma: nocov
+        )
+        p._maxTrailerHeadersSize = 10
+        p.dataReceived(b"3\r\nabc\r\n0\r\n0123456789")
+        self.assertRaises(
+            http._MalformedChunkedDataError,
+            p.dataReceived,
+            b"A",
+        )
+
 
 class ChunkingTests(unittest.TestCase, ResponseTestMixin):
     strings = [b"abcv", b"", b"fdfsd423", b"Ffasfas\r\n", b"523523\n\rfsdf", b"4234"]
