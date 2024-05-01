ec0720e7ad23c15cffcebcfe4ee077f5a6c8bab9
Glyph <code@glyph.im>
2024-05-01 16:48:22 -0700

    you can't call buildReactor twice in the same test, it's broken

--- src/twisted/internet/test/test_cfreactor.py
+++ src/twisted/internet/test/test_cfreactor.py
@@ -35,29 +35,6 @@ class CoreFoundationSpecificTests(ReactorBuilder, fakeBase):
         self.runReactor(r)
         self.assertIs(r._currentSimulator, None)
 
-    def test_callLaterLeakage(self) -> None:
-        """
-        callLater should not leak global state into CoreFoundation which will
-        be invoked by a different reactor running the main loop.
-
-        @note: this test may actually be usable for other reactors as well, so
-            we may wish to promote it to ensure this invariant across other
-            foreign-main-loop reactors.
-        """
-        r = self.buildReactor()
-        delayed = r.callLater(0, noop)
-        r2 = self.buildReactor()
-
-        def stopBlocking() -> None:
-            r2.callLater(0, r2stop)
-
-        def r2stop() -> None:
-            r2.stop()
-
-        r2.callLater(0, stopBlocking)
-        self.runReactor(r2)
-        self.assertEqual(r.getDelayedCalls(), [delayed])
-
     def test_whiteboxIterate(self) -> None:
         """
         C{.iterate()} should remove the CFTimer that will run Twisted's
