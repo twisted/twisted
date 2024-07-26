c1aa1a9572dc1282abd89399d15b56c61b37b80b
Tom Most <twm@freecog.net>
2024-07-26 13:08:29 -0700

    Move tests, fix MyPy

--- src/twisted/web/test/test_util.py
+++ src/twisted/web/test/test_util.py
@@ -5,7 +5,6 @@
 Tests for L{twisted.web.util}.
 """
 
-
 import gc
 
 from twisted.internet import defer
@@ -63,6 +62,44 @@ class RedirectToTests(TestCase):
         request.method = b"GET"
         targetURL = "http://target.example.com/4321"
         self.assertRaises(TypeError, redirectTo, targetURL, request)
+
+    def test_legitimateRedirect(self):
+        """
+        Legitimate URLs are fully interpolated in the `redirectTo` response body without transformation
+        """
+        request = DummyRequest([b""])
+        html = redirectTo(b"https://twisted.org/", request)
+        expected = b"""
+<html>
+    <head>
+        <meta http-equiv=\"refresh\" content=\"0;URL=https://twisted.org/\">
+    </head>
+    <body bgcolor=\"#FFFFFF\" text=\"#000000\">
+    <a href=\"https://twisted.org/\">click here</a>
+    </body>
+</html>
+"""
+        self.assertEqual(html, expected)
+
+    def test_maliciousRedirect(self):
+        """
+        Malicious URLs are HTML-escaped before interpolating them in the `redirectTo` response body
+        """
+        request = DummyRequest([b""])
+        html = redirectTo(
+            b'https://twisted.org/"><script>alert(document.location)</script>', request
+        )
+        expected = b"""
+<html>
+    <head>
+        <meta http-equiv=\"refresh\" content=\"0;URL=https://twisted.org/&quot;&gt;&lt;script&gt;alert(document.location)&lt;/script&gt;\">
+    </head>
+    <body bgcolor=\"#FFFFFF\" text=\"#000000\">
+    <a href=\"https://twisted.org/&quot;&gt;&lt;script&gt;alert(document.location)&lt;/script&gt;\">click here</a>
+    </body>
+</html>
+"""
+        self.assertEqual(html, expected)
 
 
 class ParentRedirectTests(SynchronousTestCase):
@@ -394,41 +431,3 @@ class DeferredResourceTests(SynchronousTestCase):
         gc.collect()
         errors = self.flushLoggedErrors(RuntimeError)
         self.assertEqual(errors, [])
-
-    def test_legitimateRedirect(self) -> None:
-        """
-        Legitimate URLs are fully interpolated in the `redirectTo` response body without transformation
-        """
-        request = DummyRequest([b""])
-        html = redirectTo(b"https://twisted.org/", request)
-        expected = b"""
-<html>
-    <head>
-        <meta http-equiv=\"refresh\" content=\"0;URL=https://twisted.org/\">
-    </head>
-    <body bgcolor=\"#FFFFFF\" text=\"#000000\">
-    <a href=\"https://twisted.org/\">click here</a>
-    </body>
-</html>
-"""
-        self.assertEqual(html, expected)
-
-    def test_maliciousRedirect(self) -> None:
-        """
-        Malicious URLs are HTML-escaped before interpolating them in the `redirectTo` response body
-        """
-        request = DummyRequest([b""])
-        html = redirectTo(
-            b'https://twisted.org/"><script>alert(document.location)</script>', request
-        )
-        expected = b"""
-<html>
-    <head>
-        <meta http-equiv=\"refresh\" content=\"0;URL=https://twisted.org/&quot;&gt;&lt;script&gt;alert(document.location)&lt;/script&gt;\">
-    </head>
-    <body bgcolor=\"#FFFFFF\" text=\"#000000\">
-    <a href=\"https://twisted.org/&quot;&gt;&lt;script&gt;alert(document.location)&lt;/script&gt;\">click here</a>
-    </body>
-</html>
-"""
-        self.assertEqual(html, expected)
