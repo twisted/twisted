ed886e87dddad64f39ae094e12628dcc255c5aab
Viktor Chuchurski <viktor@doyensec.com>
2024-07-26 10:22:48 +0200

    - "redirectTo" HTML encoding test cleanup

--- src/twisted/web/test/test_util.py
+++ src/twisted/web/test/test_util.py
@@ -394,3 +394,39 @@ class DeferredResourceTests(SynchronousTestCase):
         gc.collect()
         errors = self.flushLoggedErrors(RuntimeError)
         self.assertEqual(errors, [])
+
+    def test_legitimateRedirect(self) -> None:
+        """
+        Legitimate URLs are fully interpolated in the `redirectTo` response body without transformation
+        """
+        request = DummyRequest([b""])
+        html = redirectTo(b'https://twisted.org/', request)
+        expected = b"""
+<html>
+    <head>
+        <meta http-equiv=\"refresh\" content=\"0;URL=https://twisted.org/\">
+    </head>
+    <body bgcolor=\"#FFFFFF\" text=\"#000000\">
+    <a href=\"https://twisted.org/\">click here</a>
+    </body>
+</html>
+""" 
+        self.assertEqual(html, expected)
+
+    def test_maliciousRedirect(self) -> None:
+        """
+        Malicious URLs are HTML-escaped before interpolating them in the `redirectTo` response body
+        """
+        request = DummyRequest([b""])
+        html = redirectTo(b'https://twisted.org/"><script>alert(document.location)</script>', request)
+        expected = b"""
+<html>
+    <head>
+        <meta http-equiv=\"refresh\" content=\"0;URL=https://twisted.org/&quot;&gt;&lt;script&gt;alert(document.location)&lt;/script&gt;\">
+    </head>
+    <body bgcolor=\"#FFFFFF\" text=\"#000000\">
+    <a href=\"https://twisted.org/&quot;&gt;&lt;script&gt;alert(document.location)&lt;/script&gt;\">click here</a>
+    </body>
+</html>
+""" 
+        self.assertEqual(html, expected)
