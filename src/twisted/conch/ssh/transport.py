90252e8784c8ef058c152e40da3e9fd59e05154d
Finn Rayk Gaertner <finngaertner@protonmail.com>
2026-04-13 15:50:35 +0200

    fix: Remove pq algorithms from supported key exchanges for Client

--- src/twisted/conch/ssh/transport.py
+++ src/twisted/conch/ssh/transport.py
@@ -1864,6 +1864,14 @@ class SSHClientTransport(SSHTransportBase):
 
     isClient = True
 
+    # The client does not yet implement PQ/T Hybrid key exchange, so filter
+    # those algorithms out to prevent negotiating an unsupported method.
+    supportedKeyExchanges = [
+        kex
+        for kex in _kex.getSupportedKeyExchanges()
+        if not _kex.isPQHybrid(kex)
+    ]
+
     # Recommended minimal and maximal values from RFC 4419, 3.
     _dhMinimalGroupSize = 1024
     _dhMaximalGroupSize = 8192
