9bc873c082ac53869f31645f96e9f5727a5a3277
gudnimg <gudni.m.g@gmail.com>
2026-07-18 12:35:18 +0000

    conch: test: drop _stringRepresentation
    This function is not used by any test today

--- src/twisted/conch/test/test_address.py
+++ src/twisted/conch/test/test_address.py
@@ -6,8 +6,6 @@ Tests for L{SSHTransportAddrress} in ssh/address.py
 """
 from __future__ import annotations
 
-from typing import Callable
-
 from twisted.conch.ssh.address import SSHTransportAddress
 from twisted.internet.address import IPv4Address
 from twisted.internet.test.test_address import AddressTestCaseMixin
@@ -20,16 +18,6 @@ class SSHTransportAddressTests(unittest.TestCase, AddressTestCaseMixin):
     use to represent the other side of the SSH connection.  This tests the
     basic functionality of that class (string representation, comparison, &c).
     """
-
-    def _stringRepresentation(self, stringFunction: Callable[[object], str]) -> None:
-        """
-        The string representation of C{SSHTransportAddress} should be
-        "SSHTransportAddress(<stringFunction on address>)".
-        """
-        addr = self.buildAddress()
-        stringValue = stringFunction(addr)
-        addressValue = stringFunction(addr.address)
-        self.assertEqual(stringValue, "SSHTransportAddress(%s)" % addressValue)
 
     def buildAddress(self) -> SSHTransportAddress:
         """
