a0a708de25b308fccff4dd7a9be470a14afae92e
gudnimg <gudni.m.g@gmail.com>
2026-08-30 17:53:39 +0000

    Type annotate twisted.internet._baseprocess
    Add _getReason method to the BaseProcess class, but leave it unimplemented. Use override decorator in subclasses to make it explicit that its an override.
    
    Add a new test to handle the case were BaseProcess.proto = None. This is mainly done to make sure there arent any new coverage gaps.
    
    Added a doc string to describe _getReason

--- src/twisted/internet/test/test_baseprocess.py
+++ src/twisted/internet/test/test_baseprocess.py
@@ -7,6 +7,8 @@ functionality that is useful in all platforms supporting L{IReactorProcess}.
 """
 
 from twisted.internet._baseprocess import BaseProcess
+from twisted.internet.protocol import ProcessProtocol
+from twisted.python.failure import Failure
 from twisted.trial.unittest import TestCase
 
 
@@ -17,21 +19,31 @@ class BaseProcessTests(TestCase):
     implementations.
     """
 
-    def test_callProcessExited(self):
+    def test_callProcessExitedWithoutProtocol(self) -> None:
+        """
+        L{BaseProcess._callProcessExited} does nothing when there is no
+        process protocol.
+        """
+        process = BaseProcess(None)
+        process._callProcessExited(RuntimeError("fake reason"))
+        self.assertIsNone(process.proto)
+
+    def test_callProcessExited(self) -> None:
         """
         L{BaseProcess._callProcessExited} calls the C{processExited} method of
         its C{proto} attribute and passes it a L{Failure} wrapping the given
         exception.
         """
 
-        class FakeProto:
-            reason = None
+        class FakeProto(ProcessProtocol):
+            reason: Failure
 
-            def processExited(self, reason):
+            def processExited(self, reason: Failure) -> None:
                 self.reason = reason
 
         reason = RuntimeError("fake reason")
-        process = BaseProcess(FakeProto())
+        proto = FakeProto()
+        process = BaseProcess(proto)
         process._callProcessExited(reason)
-        process.proto.reason.trap(RuntimeError)
-        self.assertIs(reason, process.proto.reason.value)
+        proto.reason.trap(RuntimeError)
+        self.assertIs(reason, proto.reason.value)
