93010a1bd9612c42c35e342d82f4c031148936d0
Glyph <code@glyph.im>
2026-09-05 20:28:04 -0700

    type & test completeness

--- src/twisted/conch/test/test_session.py
+++ src/twisted/conch/test/test_session.py
@@ -11,10 +11,12 @@ import os
 import signal
 import struct
 import sys
+from typing import TYPE_CHECKING
 from unittest import skipIf
 
 from zope.interface import implementer
 
+from twisted.conch.interfaces import IConchUser
 from twisted.internet import defer, error, protocol
 from twisted.internet.address import IPv4Address
 from twisted.internet.error import ProcessDone, ProcessTerminated
@@ -38,6 +40,12 @@ else:
         )
 
 
+if TYPE_CHECKING:
+    LikeSSHConnection = connection.SSHConnection
+else:
+    LikeSSHConnection = object
+
+
 class SubsystemOnlyAvatar:
     """
     A stub class representing an avatar that is only useful for
@@ -53,7 +61,9 @@ class SubsystemOnlyAvatar:
         return MockProtocol()
 
 
-class StubAvatar:
+@implementer(IConchUser)
+# as-yet incomplete implementation
+class StubAvatar:  # type: ignore[misc]
     """
     A stub class representing the avatar representing the authenticated user.
     It implements the I{ISession} interface.
@@ -301,7 +311,7 @@ class MockProtocol(protocol.Protocol):
         self.reason = reason
 
 
-class StubConnection:
+class StubConnection(LikeSSHConnection):
     """
     A stub for twisted.conch.ssh.connection.SSHConnection.  Record the data
     that channels send, and when they try to close the connection.
@@ -318,7 +328,7 @@ class StubConnection:
         a close.
     """
 
-    def __init__(self, transport=None):
+    def __init__(self, transport):
         """
         Initialize our instance variables.
         """
@@ -475,14 +485,14 @@ class SessionInterfaceTests(RegistryUsingMixin, TestCase):
             )
         self.session = self.getSSHSession()
 
-    def getSSHSession(self, register_adapters=True):
+    def getSSHSession(self, register_adapters: bool = True) -> session.SSHSession:
         """
         Return a new SSH session.
         """
         return session.SSHSession(
             remoteWindow=500,
             remoteMaxPacket=100,
-            conn=StubConnection(),
+            conn=StubConnection(StubTransport()),
             avatar=StubAvatar(),
         )
 
@@ -613,7 +623,9 @@ class SessionInterfaceTests(RegistryUsingMixin, TestCase):
         adapter wasn't needed because subsystems were looked up using the
         lookupSubsystem method on the avatar.
         """
-        s = session.SSHSession(avatar=SubsystemOnlyAvatar(), conn=StubConnection())
+        s = session.SSHSession(
+            avatar=SubsystemOnlyAvatar(), conn=StubConnection(StubTransport())
+        )
         ret = s.request_subsystem(common.NS(b"subsystem") + b"data")
         self.assertTrue(ret)
         self.assertIsNotNone(s.client)
@@ -991,6 +1003,15 @@ class WrappersTests(TestCase):
         has write(), writeSequence(), loseConnection() methods which call the
         Protocol's dataReceived() and connectionLost() methods, respectively.
         """
+        # maintenance note: dataReceived is never called, *and* isn't part of
+        # ITransport, and should probably just be removed? getHost and getPeer
+        # aren't called and it's not clear to me how they would be; I don't
+        # think application code can get a direct bead on this object; the
+        # transport *of* the subsystem protocol itself, for example, is the
+        # result of wrapProcessProtocol, not this.  wrapProtocol is tested as
+        # if it ought to be a public API but it's kind of broken and probably
+        # just shouldn't be here. It should certainly be less confused about
+        # its role.
         protocol = MockProtocol()
         protocol.transport = StubTransport()
         protocol.connectionMade()
@@ -1000,6 +1021,8 @@ class WrappersTests(TestCase):
         wrapped.write(b"data")
         wrapped.writeSequence([b"1", b"2"])
         wrapped.loseConnection()
+        self.assertEqual(wrapped.getHost(), protocol.transport.getHost())
+        self.assertEqual(wrapped.getPeer(), protocol.transport.getPeer())
         self.assertEqual(protocol.data, b"data12")
         protocol.reason.trap(error.ConnectionDone)
 
