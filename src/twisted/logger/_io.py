a0c6751206aabd5b10d28662332ea980d5d513d2
Adi Roiban <adiroiban@gmail.com>
2026-09-12 07:38:42 +0100

    Fix pydoctor reference.

--- src/twisted/logger/_io.py
+++ src/twisted/logger/_io.py
@@ -130,7 +130,7 @@ class LoggingFile:
 
     def fileno(self) -> NoReturn:
         """
-        Raises a L{UnsupportedOperation} because L{LoggingFile} does not use a a file descriptor.
+        Raises a C{UnsupportedOperation} because L{LoggingFile} does not use a a file descriptor.
         """
         raise UnsupportedOperation("LoggingFile has no file descriptor.")
 
